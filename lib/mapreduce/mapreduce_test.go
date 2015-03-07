d16f03bf0b6d8da75e6abcd4fd4eb2729986ff57
Joe Doliner <jdoliner@pachyderm.io>
2015-03-07 07:25:34 +0000

    Makes mapreduce_test compile.

--- lib/mapreduce/mapreduce_test.go
+++ lib/mapreduce/mapreduce_test.go
@@ -1,11 +1,11 @@
 package mapreduce
 
 import (
+	"log"
 	"testing"
 
 	"github.com/mitchellh/goamz/aws"
 	"github.com/mitchellh/goamz/s3"
-	"github.com/pachyderm/pfs/lib/btrfs"
 )
 
 func TestS3(t *testing.T) {
@@ -16,17 +16,11 @@ func TestS3(t *testing.T) {
 		return
 	}
 	client := s3.New(auth, aws.USWest)
-
-	bucketName, err := getBucket(job.Input)
-	if err != nil {
-		log.Print(err)
-		return
-	}
 	bucket := client.Bucket("pachyderm-data")
-	inFile, err = bucket.GetReader("chess/file000000000")
+	inFile, err := bucket.GetReader("chess/file000000000")
 	if err != nil {
 		log.Print(err)
 		return
 	}
-	defer inFile.Close()
+	inFile.Close()
 }
