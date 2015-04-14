33a15b6ea649801bdace80d049d18eb512c5be9d
rw-pachyderm <robert.winslow+pachyderm_github@gmail.com>
2015-04-14 21:13:58 +0000

    btrfs_tests are green

--- lib/btrfs/btrfs_test.go
+++ lib/btrfs/btrfs_test.go
@@ -17,8 +17,8 @@ func check(err error, t *testing.T) {
 }
 
 // writeFile quickly writes a string to disk.
-func writeFile(fs *FS, name, content string, t *testing.T) {
-	f, err := fs.Create(name)
+func writeFile(name, content string, t *testing.T) {
+	f, err := Create(name)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -28,14 +28,14 @@ func writeFile(fs *FS, name, content string, t *testing.T) {
 }
 
 // checkFile checks if a file on disk contains a given string.
-func checkFile(fs *FS, name, content string, t *testing.T) {
-	exists, err := fs.FileExists(name)
+func checkFile(name, content string, t *testing.T) {
+	exists, err := FileExists(name)
 	check(err, t)
 	if !exists {
 		t.Fatalf("File %s should exist.", name)
 	}
 
-	f, err := fs.Open(name)
+	f, err := Open(name)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -51,49 +51,44 @@ func checkFile(fs *FS, name, content string, t *testing.T) {
 }
 
 // checkNoFile checks that no file is present.
-func checkNoFile(fs *FS, name string, t *testing.T) {
-	exists, err := fs.FileExists(name)
+func checkNoFile(name string, t *testing.T) {
+	exists, err := FileExists(name)
 	check(err, t)
 	if exists {
 		t.Fatalf("File %s shouldn't exist.", name)
 	}
 }
 
-func removeFile(fs *FS, name string, t *testing.T) {
-	err := fs.Remove(name)
+func removeFile(name string, t *testing.T) {
+	err := Remove(name)
 	if err != nil {
 		t.Fatal(err)
 	}
 }
 
 func TestOsOps(t *testing.T) {
-	fs := NewFSWithRandSeq("TestOsOps")
-	fs.EnsureNamespace()
-	writeFile(fs, "foo", "foo", t)
-	checkFile(fs, "foo", "foo", t)
-	removeFile(fs, "foo", t)
-	checkNoFile(fs, "foo", t)
+	writeFile("foo", "foo", t)
+	checkFile("foo", "foo", t)
+	removeFile("foo", t)
+	checkNoFile("foo", t)
 }
 
 func TestGit(t *testing.T) {
-	fs := NewFSWithRandSeq("TestGit")
-	fs.EnsureNamespace()
-
-	check(fs.Init("repo"), t)
-	writeFile(fs, "repo/master/file", "foo", t)
-	commit, err := fs.Commit("repo", "master")
+	check(Init("repo"), t)
+	writeFile("repo/master/file", "foo", t)
+	err := Commit("repo", "commit1", "master")
 	check(err, t)
-	checkFile(fs, path.Join("repo", commit, "file"), "foo", t)
+	checkFile(path.Join("repo", "commit1", "file"), "foo", t)
 
-	check(fs.Branch("repo", commit, "branch"), t)
-	checkFile(fs, "repo/branch/file", "foo", t)
+	check(Branch("repo", "commit1", "branch"), t)
+	checkFile("repo/branch/file", "foo", t)
 
-	writeFile(fs, "repo/branch/file2", "foo", t)
-	commit, err = fs.Commit("repo", "branch")
+	writeFile("repo/branch/file2", "foo", t)
+	err = Commit("repo", "commit2", "branch")
 	check(err, t)
-	checkFile(fs, path.Join("repo", commit, "file2"), "foo", t)
+	checkFile(path.Join("repo", "commit2", "file2"), "foo", t)
 
-	check(fs.Log("repo", "0", func(r io.ReadCloser) error {
+	check(Log("repo", "0", func(r io.ReadCloser) error {
 		_, err := io.Copy(os.Stdout, r)
 		return err
 	}), t)
