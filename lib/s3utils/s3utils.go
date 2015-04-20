b39b7738fab796c61e76a62227fc2e92bfff024a
Joe Doliner <jdoliner@pachyderm.io>
2015-04-20 23:46:10 +0000

    Adds a PutMulti function which wraps goamz multiputs.

--- lib/s3utils/s3utils.go
+++ lib/s3utils/s3utils.go
@@ -1,12 +1,20 @@
 package s3utils
 
 import (
+	"bytes"
+	"io"
 	"log"
 	"path"
 	"strings"
 
 	"github.com/mitchellh/goamz/aws"
 	"github.com/mitchellh/goamz/s3"
+)
+
+const (
+	// AWS says that parts must be at least 5MB, it's unclear if that means 5 *
+	// 10^6 or 5 2^10 so we went with the larger.
+	minPart = 5242880 // 5MB
 )
 
 // An s3 input looks like: s3://bucket/dir
@@ -31,4 +39,54 @@ func NewBucket(bucketName string) (*s3.Bucket, error) {
 	client := s3.New(auth, aws.USWest)
 
 	return client.Bucket(bucketName), nil
+}
+
+// PutMulti is like a smart bucket.Put in that it will automatically do a
+// multiput if the input reader has enough data that it makes sense to do so.
+func PutMulti(bucket *s3.Bucket, path string, r io.Reader, contType string, perm s3.ACL) error {
+	// A pointer to a Multi transaction, if this is non nil it means we're
+	// putting the data in parts.
+	var multi *s3.Multi = nil
+	var parts []s3.Part
+	for i := 0; ; i++ {
+		var data []byte
+		n, err := io.ReadAtLeast(r, data, minPart)
+		if err != nil && err != io.ErrUnexpectedEOF && err != io.EOF {
+			// EOF means that r was empty
+			// ErrUnexpectedEOF means that r had less than minPart bytes in it
+			// Note that we can hit these errors after having read several 5MB
+			// chunks from r already
+			return err
+		}
+		if n >= minPart && multi == nil {
+			// We have enough data to do a MultiPut but we haven't started on yet.
+			// That means it's time to start one
+			multi, err = bucket.Multi(path, contType, perm)
+			if err != nil {
+				return err
+			}
+		}
+		// Now we upload the actual data
+		if multi == nil {
+			// We're not doing a multi transaction,
+			return bucket.Put(path, data, contType, perm)
+		} else {
+			part, err := multi.PutPart(i, bytes.NewReader(data))
+			if err != nil {
+				return err
+			}
+			parts = append(parts, part)
+		}
+		if n < minPart {
+			// That means this was the last batch of data, time to break out of
+			// this loop
+			break
+		}
+	}
+	if multi != nil {
+		if err := multi.Complete(parts); err != nil {
+			return err
+		}
+	}
+	return nil
 }
