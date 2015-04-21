ad089ebf3a5932fc834ebea01a69e56e041f2a28
rw-pachyderm <robert.winslow+pachyderm_github@gmail.com>
2015-04-21 02:25:25 +0000

    SendRecv Test

--- lib/btrfs/btrfs_test.go
+++ lib/btrfs/btrfs_test.go
@@ -108,7 +108,7 @@ func TestGit(t *testing.T) {
 }
 
 func TestNewRepoIsEmpty(t *testing.T) {
-	repoName := "repo_TesNewRepoIsEmpty"
+	repoName := "repo_TestNewRepoIsEmpty"
 	check(Init(repoName), t)
 
 	// ('master' is the default branch)
@@ -155,7 +155,49 @@ func TestReplication(t *testing.T) {
 	t.Skip("implement this")
 }
 
-// TestSendRecv // low-level
+// TestSendRecv checks the Send and Recv replication primitives.
+func TestSendRecv(t *testing.T) {
+	// Create a source repo:
+	srcRepo := "repo_TestSendRecv_src"
+	check(Init(srcRepo), t)
+
+	// Create a file in the source repo:
+	writeFile(fmt.Sprintf("%s/master/myfile1", srcRepo), "foo", t)
+
+	// Create a commit in the source repo:
+	check(Commit(srcRepo, "mycommit1", "master"), t)
+
+	// Create another file in the source repo:
+	writeFile(fmt.Sprintf("%s/master/myfile2", srcRepo), "bar", t)
+
+	// Create a another commit in the source repo:
+	check(Commit(srcRepo, "mycommit2", "master"), t)
+
+	// Create a destination repo:
+	dstRepo := "repo_TestSendRecv_dst"
+	check(Init(dstRepo), t)
+
+	// Verify that the commits "mycommit1" and "mycommit2" does not exist:
+	checkNoFile(fmt.Sprintf("%s/mycommit1", dstRepo), t)
+	checkNoFile(fmt.Sprintf("%s/mycommit2", dstRepo), t)
+
+	// Run a Send/Recv operation to fetch data from the older "mycommit1".
+	// This verifies that tree copying works:
+	repo2Recv := func (r io.ReadCloser) error { return Recv(dstRepo, r) }
+        check(Send(fmt.Sprintf("%s/t0", srcRepo), fmt.Sprintf("%s/mycommit1", srcRepo), repo2Recv), t)
+
+	// Check that the file from mycommit1 exists, but not from mycommit2: 
+        checkFile(fmt.Sprintf("%s/mycommit1/myfile1", dstRepo), "foo", t)
+        checkNoFile(fmt.Sprintf("%s/mycommit2/myfile2", dstRepo), t)
+
+	// Send again, this time starting from mycommit1 and going to mycommit2:
+	repo2Recv = func (r io.ReadCloser) error { return Recv(dstRepo, r) }
+        check(Send(fmt.Sprintf("%s/mycommit1", srcRepo), fmt.Sprintf("%s/mycommit2", srcRepo), repo2Recv), t)
+
+	// Verify that files from both comits are present:
+        checkFile(fmt.Sprintf("%s/mycommit1/myfile1", dstRepo), "foo", t)
+        checkFile(fmt.Sprintf("%s/mycommit2/myfile2", dstRepo), "bar", t)
+}
 // TestSendBaseRecv // low-level
 
 // TestSendWithMissingIntermediateCommitIsCorrect(?) // ? means we don't know what the behavior is.
