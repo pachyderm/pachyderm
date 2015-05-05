52ec4f02878a767b5d6ec83a639ade6fdfaad65b
Joe Doliner <jdoliner@pachyderm.io>
2015-05-05 09:41:44 +0000

    Remove logging.

--- services/shard/shard_test.go
+++ services/shard/shard_test.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/http/httptest"
 	"path"
@@ -119,7 +118,6 @@ func TestBasic(t *testing.T) {
 }
 
 func TestPull(t *testing.T) {
-	log.SetFlags(log.Lshortfile)
 	c := 0
 	f := func(w traffic.Workload) bool {
 		_src := NewShard(fmt.Sprintf("TestPullSrc%d", c), fmt.Sprintf("TestPullSrcComp%d", c), 0, 1)
