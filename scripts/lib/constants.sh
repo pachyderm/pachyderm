986d2b431e0c38cdf782ff1ed022be77bbb58ec0
Peter Edge <peter.edge@gmail.com>
2015-06-06 14:46:23 +0200

    more cleanup, fixes mismatch in dev and prod image name

--- scripts/lib/constants.sh
+++ scripts/lib/constants.sh
@@ -10,3 +10,5 @@ PFS_DATA_IMG="${PFS_DATA_IMG:="${PFS_DIR}/data.img"}"
 PFS_CONTAINER_NAME="${PFS_CONTAINER_NAME:="pfs"}"
 PFS_IMAGE="${PFS_IMAGE:="pachyderm/pfs"}"
 PFS_PORT="${PFS_PORT:="650"}"
+# TODO: is IMAGE_BUCKET legacy?
+PFS_S3_BUCKET=${IMAGE_BUCKET:="pachyderm-images"}
