b2940b9724f1e0d37745e878d50f6827ef9a366e
peter-edge <peter.edge@gmail.com>
2015-07-02 19:23:12 +0000

    remove log file for shard binary

--- src/cmd/shard/main.go
+++ src/cmd/shard/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	stdlog "log"
 	"os"
-	"path"
 
 	"github.com/pachyderm/pachyderm/src/log"
 	"github.com/pachyderm/pachyderm/src/shard"
@@ -18,18 +16,6 @@ func main() {
 }
 
 func do() error {
-	if err := os.MkdirAll("/var/lib/pfs/log", 0777); err != nil {
-		return err
-	}
-	logF, err := os.Create(path.Join("/var/lib/pfs/log", "log-"+os.Args[1]))
-	if err != nil {
-		return err
-	}
-	defer logF.Close()
-
-	// TODO(pedge)
-	log.SetLogger(stdlog.New(logF, "", stdlog.Lshortfile))
-
 	s, err := shard.ShardFromArgs()
 	if err != nil {
 		return err
