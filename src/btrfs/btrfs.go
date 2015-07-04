44decbeef132e562701ad44df110b4e12719aa95
peter-edge <peter.edge@gmail.com>
2015-07-04 19:17:27 +0000

    cleanup of btrfs package

--- src/btrfs/btrfs.go
+++ src/btrfs/btrfs.go
@@ -19,10 +19,45 @@ import (
 	"github.com/pachyderm/pachyderm/src/util"
 )
 
+// Constants used for passing to log
+const (
+	Desc = iota
+	Asc
+)
+
 var (
 	ErrComplete  = errors.New("pfs: complete")
 	ErrCancelled = errors.New("pfs: cancelled")
 )
+
+// Pusher is an interface that wraps the Push method.
+type Pusher interface {
+	// From returns the last commit pushed.
+	// This value should be passed to Pull.
+	From() (string, error)
+	// Push applies diff to an underlying storage layer.
+	Push(diff io.Reader) error
+}
+
+// Puller is an interface that wraps the Pull method.
+type Puller interface {
+	// Pull produces binary diffs and passes them to p's Push method.
+	Pull(from string, p Pusher) error
+}
+
+// Replica is the interface that groups the Puller and Pusher methods.
+type Replica interface {
+	Pusher
+	Puller
+}
+
+func NewLocalReplica(repo string) Replica {
+	return newLocalReplica(repo)
+}
+
+func NewS3Replica(uri string) Replica {
+	return newS3Replica(uri)
+}
 
 // FilePath returns an absolute path for a file in the btrfs volume *inside*
 // the container.
@@ -462,34 +497,6 @@ func Branch(repo, commit, branch string) error {
 	return nil
 }
 
-// Constants used for passing to log
-const (
-	Desc = iota
-	Asc  = iota
-)
-
-//_log returns all of the commits the repo which have generation >= from.
-func _log(repo, from string, order int, cont func(io.Reader) error) error {
-	var sort string
-	if order == Desc {
-		sort = "-ogen"
-	} else {
-		sort = "+ogen"
-	}
-
-	if from == "" {
-		c := exec.Command("btrfs", "subvolume", "list", "-o", "-c", "-u", "-q", "--sort", sort, FilePath(path.Join(repo)))
-		return util.CallCont(c, cont)
-	} else {
-		t, err := transid(repo, from)
-		if err != nil {
-			return err
-		}
-		c := exec.Command("btrfs", "subvolume", "list", "-o", "-c", "-u", "-q", "-C", "+"+t, "--sort", sort, FilePath(path.Join(repo)))
-		return util.CallCont(c, cont)
-	}
-}
-
 // Commits is a wrapper around `Log` which parses the output in to a convenient
 // struct
 func Commits(repo, from string, order int, cont func(string) error) error {
@@ -597,6 +604,28 @@ func FindNew(repo, from, to string) ([]string, error) {
 func NewIn(repo, commit string) ([]string, error) {
 	parent := GetMeta(path.Join(repo, commit), "parent")
 	return FindNew(repo, parent, commit)
+}
+
+//_log returns all of the commits the repo which have generation >= from.
+func _log(repo, from string, order int, cont func(io.Reader) error) error {
+	var sort string
+	if order == Desc {
+		sort = "-ogen"
+	} else {
+		sort = "+ogen"
+	}
+
+	if from == "" {
+		c := exec.Command("btrfs", "subvolume", "list", "-o", "-c", "-u", "-q", "--sort", sort, FilePath(path.Join(repo)))
+		return util.CallCont(c, cont)
+	} else {
+		t, err := transid(repo, from)
+		if err != nil {
+			return err
+		}
+		c := exec.Command("btrfs", "subvolume", "list", "-o", "-c", "-u", "-q", "-C", "+"+t, "--sort", sort, FilePath(path.Join(repo)))
+		return util.CallCont(c, cont)
+	}
 }
 
 func trimFilePath(name string) string {
