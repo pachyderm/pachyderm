ab8a4a06085ea45cf0c74b9e58c57a52c18c8ab0
Joe Doliner <jdoliner@pachyderm.io>
2015-07-04 22:13:36 +0000

    First pass at the new http api.

--- src/storage/shard_http_handler.go
+++ src/storage/shard_http_handler.go
@@ -2,7 +2,10 @@ package storage
 
 import (
 	"fmt"
+	"mime/multipart"
 	"net/http"
+	"path"
+	"strings"
 )
 
 type shardHTTPHandler struct {
@@ -24,22 +27,85 @@ func newShardHTTPHandler(shard Shard) *shardHTTPHandler {
 	return shardHTTPHandler
 }
 
-func (s *shardHTTPHandler) branch(writer http.ResponseWriter, request *http.Request) {
+func resource(request *http.Request) string {
+	url := strings.Split(request.URL.Path, "/")
+	return path.Join(url[1:]...)
+}
 
+func (s *shardHTTPHandler) branch(writer http.ResponseWriter, request *http.Request) {
+	name := resource(request)
+	switch request.Method {
+	case "GET":
+		_, err := s.BranchGet(name)
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+		}
+	case "POST":
+		branch, err := s.BranchCreate(name, commitParam(request))
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+		}
+		fmt.Fprintf(writer, "Created %s.", branch.Name)
+	}
 }
 
 func (s *shardHTTPHandler) commit(writer http.ResponseWriter, request *http.Request) {
-
+	name := resource(request)
+	switch request.Method {
+	case "GET":
+		_, err := s.CommitGet(name)
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+		}
+	case "POST":
+		commit, err := s.CommitCreate(name, branchParam(request))
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+		}
+		fmt.Fprintf(writer, "Created %s.", commit.Name)
+	}
 }
 
 func (s *shardHTTPHandler) file(writer http.ResponseWriter, request *http.Request) {
-
+	name := resource(request)
+	switch request.Method {
+	case "GET":
+		file, err := s.FileGet(name, commitParam(request))
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+		}
+		http.ServeContent(writer, request, file.Name, file.ModTime, file.File)
+	case "POST":
+		if err := s.FileCreate(name, request.Body, branchParam(request)); err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+		}
+	}
 }
 
 func (s *shardHTTPHandler) pipeline(writer http.ResponseWriter, request *http.Request) {
-
+	name := path.Join("pipeline", resource(request))
+	switch request.Method {
+	case "GET":
+		file, err := s.FileGet(name, commitParam(request))
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+		}
+		http.ServeContent(writer, request, file.Name, file.ModTime, file.File)
+	case "POST":
+		if err := s.FileCreate(name, request.Body, branchParam(request)); err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+		}
+	}
 }
 
 func (s *shardHTTPHandler) pull(writer http.ResponseWriter, request *http.Request) {
-
+	from := request.URL.Query().Get("from")
+	mpw := multipart.NewWriter(writer)
+	defer mpw.Close()
+	cb := newMultipartPusher(mpw)
+	writer.Header().Add("Boundary", mpw.Boundary())
+	if err := s.Pull(from, cb); err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
