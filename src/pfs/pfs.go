1e3cf28d0e96cd37e04bc6ec5ef0f2d5d2a48a8d
peter-edge <peter.edge@gmail.com>
2015-07-10 04:03:59 +0000

    remove local api client

--- src/pfs/pfs.go
+++ src/pfs/pfs.go
@@ -4,9 +4,3 @@ package pfs
 func NewAPIServer() ApiServer {
 	return nil
 }
-
-// NewLocalAPIClient returns a local ApiClient that directly
-// makes calls to the provided ApiServer.
-func NewLocalAPIClient(apiServer ApiServer) ApiClient {
-	return nil
-}
