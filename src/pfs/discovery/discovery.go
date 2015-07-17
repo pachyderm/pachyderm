ab3c52c561bcf42f508114ef46337e4b957e6d7e
Joe Doliner <jdoliner@pachyderm.io>
2015-07-17 21:27:31 +0000

    Adds interface for discovery.

--- src/pfs/discovery/discovery.go
+++ src/pfs/discovery/discovery.go
@@ -2,6 +2,12 @@ package discovery
 
 type Client interface {
 	Close() error
+	Get(key string) (string, error)
+	GetAll(key string) ([]string, error)
+	Set(key string, value string) error
+	Create(key string, value string) error
+	Delete(key string) error
+	CheckAndSet(key string, value string, oldValue string) error
 }
 
 func NewEtcdClient(addresses ...string) Client {
