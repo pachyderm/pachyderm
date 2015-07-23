802b734ad335fd409a070f921667089d8b1f3a6a
peter-edge <peter.edge@gmail.com>
2015-07-23 10:12:56 +0000

    base implementation for pps docker client

--- src/pps/container/docker_client.go
+++ src/pps/container/docker_client.go
@@ -1,6 +1,11 @@
 package container
 
-import "github.com/fsouza/go-dockerclient"
+import (
+	"fmt"
+
+	"github.com/fsouza/go-dockerclient"
+	"github.com/pachyderm/pachyderm/src/log"
+)
 
 type dockerClient struct {
 	// confusing
@@ -31,13 +36,100 @@ func newDockerClient(dockerClientOptions DockerClientOptions) (*dockerClient, er
 }
 
 func (c *dockerClient) Build(imageName string, contextDir string, buildOptions BuildOptions) error {
-	return nil
+	return c.client.BuildImage(
+		docker.BuildImageOptions{
+			Name:           imageName,
+			Dockerfile:     buildOptions.Dockerfile,
+			SuppressOutput: true,
+			OutputStream:   log.Writer(),
+			ContextDir:     contextDir,
+		},
+	)
 }
 
 func (c *dockerClient) Pull(imageName string, pullOptions PullOptions) error {
-	return nil
+	repository, tag := docker.ParseRepositoryTag(imageName)
+	if tag == "" {
+		tag = "latest"
+	}
+	return c.client.PullImage(
+		docker.PullImageOptions{
+			Repository:   repository,
+			Tag:          tag,
+			OutputStream: log.Writer(),
+		},
+		docker.AuthConfiguration{},
+	)
 }
 
-func (c *dockerClient) Run(imageName string, runOptions RunOptions) error {
+func (c *dockerClient) Run(imageName string, runOptions RunOptions) (retErr error) {
 	return nil
+	createContainerOptions, err := getDockerCreateContainerOptions(imageName, runOptions)
+	if err != nil {
+		return err
+	}
+	container, err := c.client.CreateContainer(createContainerOptions)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := c.client.RemoveContainer(
+			docker.RemoveContainerOptions{
+				ID:    container.ID,
+				Force: true,
+			},
+		); err != nil && retErr == nil {
+			retErr = err
+		}
+	}()
+	if err := c.client.StartContainer(container.ID, createContainerOptions.HostConfig); err != nil {
+		return err
+	}
+	errC := make(chan error)
+	go func() {
+		errC <- c.client.Logs(
+			docker.LogsOptions{
+				Container:    container.ID,
+				OutputStream: log.Writer(),
+				ErrorStream:  log.Writer(),
+				Stdout:       true,
+				Stderr:       true,
+				Timestamps:   true,
+			},
+		)
+	}()
+	exitCode, err := c.client.WaitContainer(container.ID)
+	logsErr := <-errC
+	if err != nil {
+		return err
+	}
+	if exitCode != 0 {
+		return fmt.Errorf("container %s for image %s had exit code %d", container.ID, imageName, exitCode)
+	}
+	return logsErr
+}
+
+func getDockerCreateContainerOptions(imageName string, runOptions RunOptions) (docker.CreateContainerOptions, error) {
+	config, err := getDockerConfig(imageName, runOptions)
+	if err != nil {
+		return docker.CreateContainerOptions{}, err
+	}
+	hostConfig, err := getDockerHostConfig(imageName, runOptions)
+	if err != nil {
+		return docker.CreateContainerOptions{}, err
+	}
+	return docker.CreateContainerOptions{
+		Config:     config,
+		HostConfig: hostConfig,
+	}, nil
+}
+
+func getDockerConfig(imageName string, runOptions RunOptions) (*docker.Config, error) {
+	return &docker.Config{
+		Image: imageName,
+	}, nil
+}
+
+func getDockerHostConfig(imageName string, runOptions RunOptions) (*docker.HostConfig, error) {
+	return &docker.HostConfig{}, nil
 }
