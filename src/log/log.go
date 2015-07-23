65cbbb3641839ecb5a4e8f7eb510d8fe7b4de9bd
peter-edge <peter.edge@gmail.com>
2015-07-23 10:12:56 +0000

    base implementation for pps docker client

--- src/log/log.go
+++ src/log/log.go
@@ -5,6 +5,7 @@ to use other logging implementations in the future easily.
 package log
 
 import (
+	"io"
 	"log"
 	"os"
 	"sync"
@@ -12,14 +13,15 @@ import (
 
 var (
 	// DefaultLogger is the default logger used.
-	DefaultLogger Logger = log.New(os.Stderr, "", log.LstdFlags|log.Lshortfile)
+	DefaultLogger Logger = newLogger(log.New(os.Stderr, "", log.LstdFlags|log.Lshortfile))
 
-	logger = DefaultLogger
-	lock   = &sync.Mutex{}
+	globalLogger = DefaultLogger
+	lock         = &sync.Mutex{}
 )
 
 // Logger is a super simple logger.
 type Logger interface {
+	io.Writer
 	Print(args ...interface{})
 	Printf(format string, args ...interface{})
 }
@@ -28,15 +30,33 @@ type Logger interface {
 func SetLogger(l Logger) {
 	lock.Lock()
 	defer lock.Unlock()
-	logger = l
+	globalLogger = l
+}
+
+// Writer returns an io.Writer for logging.
+func Writer() io.Writer {
+	return globalLogger
 }
 
 // Print prints a log message with fmt.Print semantics using the pachyderm Logger.
 func Print(args ...interface{}) {
-	logger.Print(args...)
+	globalLogger.Print(args...)
 }
 
 // Printf prints a log message with fmt.Printf semantics using the pachyderm Logger.
 func Printf(format string, args ...interface{}) {
-	logger.Printf(format, args...)
+	globalLogger.Printf(format, args...)
+}
+
+type logger struct {
+	*log.Logger
+}
+
+func newLogger(l *log.Logger) *logger {
+	return &logger{l}
+}
+
+func (l *logger) Write(p []byte) (int, error) {
+	l.Print(string(p))
+	return len(p), nil
 }
