543b98d43e0ed2f83807c32745ec4ffa04186eb5
peter-edge <peter.edge@gmail.com>
2015-07-24 12:39:54 +0000

    TestBuildWithError

--- src/pps/graph/graph_test.go
+++ src/pps/graph/graph_test.go
@@ -3,6 +3,8 @@ package graph
 import (
 	"errors"
 	"fmt"
+	"sync"
+	"sync/atomic"
 	"testing"
 
 	"github.com/pachyderm/pachyderm/src/pps"
@@ -67,21 +69,23 @@ func TestBuild(t *testing.T) {
 			},
 		},
 	}
+	counter := int32(0)
 	nameToNodeFunc := map[string]func() error{
-		"1":   testNodeFunc(intC, "1", 1, ""),
-		"2":   testNodeFunc(intC, "2", 2, ""),
-		"3-1": testNodeFunc(intC, "3-1", 3, ""),
-		"3-2": testNodeFunc(intC, "3-2", 4, ""),
-		"3-3": testNodeFunc(intC, "3-3", 5, ""),
-		"4-1": testNodeFunc(intC, "4-1", 6, ""),
-		"4-2": testNodeFunc(intC, "4-2", 7, ""),
-		"5":   testNodeFunc(intC, "5", 8, ""),
+		"1":   testNodeFunc(&counter, intC, "1", 1, ""),
+		"2":   testNodeFunc(&counter, intC, "2", 2, ""),
+		"3-1": testNodeFunc(&counter, intC, "3-1", 3, ""),
+		"3-2": testNodeFunc(&counter, intC, "3-2", 4, ""),
+		"3-3": testNodeFunc(&counter, intC, "3-3", 5, ""),
+		"4-1": testNodeFunc(&counter, intC, "4-1", 6, ""),
+		"4-2": testNodeFunc(&counter, intC, "4-2", 7, ""),
+		"5":   testNodeFunc(&counter, intC, "5", 8, ""),
 	}
 
 	run, err := build(newTestNodeErrorRecorder(), nameToNodeInfo, nameToNodeFunc)
 	require.NoError(t, err)
 	run.Do()
 
+	require.Equal(t, int32(8), counter)
 	i := <-intC
 	require.True(t, i == 1 || i == 2)
 	i = <-intC
@@ -100,12 +104,93 @@ func TestBuild(t *testing.T) {
 	require.True(t, i == 8)
 }
 
-func testNodeFunc(intC chan int, nodeName string, i int, errString string) func() error {
+func TestBuildWithError(t *testing.T) {
+	intC := make(chan int, 5)
+	nameToNodeInfo := map[string]*NodeInfo{
+		"1": &NodeInfo{
+			Parents: []string{},
+		},
+		"2": &NodeInfo{
+			Parents: []string{},
+		},
+		"3-1": &NodeInfo{
+			Parents: []string{
+				"1",
+				"2",
+			},
+		},
+		"3-2": &NodeInfo{
+			Parents: []string{
+				"1",
+				"2",
+			},
+		},
+		"3-3": &NodeInfo{
+			Parents: []string{
+				"1",
+				"2",
+			},
+		},
+		"4-1": &NodeInfo{
+			Parents: []string{
+				"3-1",
+				"3-2",
+				"3-3",
+			},
+		},
+		"4-2": &NodeInfo{
+			Parents: []string{
+				"3-1",
+				"3-2",
+				"3-3",
+			},
+		},
+		"5": &NodeInfo{
+			Parents: []string{
+				"4-1",
+				"4-2",
+			},
+		},
+	}
+	counter := int32(0)
+	nameToNodeFunc := map[string]func() error{
+		"1":   testNodeFunc(&counter, intC, "1", 1, ""),
+		"2":   testNodeFunc(&counter, intC, "2", 2, ""),
+		"3-1": testNodeFunc(&counter, intC, "3-1", 3, "error"),
+		"3-2": testNodeFunc(&counter, intC, "3-2", 4, ""),
+		"3-3": testNodeFunc(&counter, intC, "3-3", 5, ""),
+		"4-1": testNodeFunc(&counter, intC, "4-1", 6, ""),
+		"4-2": testNodeFunc(&counter, intC, "4-2", 7, ""),
+		"5":   testNodeFunc(&counter, intC, "5", 8, ""),
+	}
+
+	testNodeErrorRecorder := newTestNodeErrorRecorder()
+	run, err := build(testNodeErrorRecorder, nameToNodeInfo, nameToNodeFunc)
+	require.NoError(t, err)
+	run.Do()
+
+	require.Equal(t, int32(5), counter)
+	i := <-intC
+	require.True(t, i == 1 || i == 2)
+	i = <-intC
+	require.True(t, i == 1 || i == 2)
+	i = <-intC
+	require.True(t, i == 3 || i == 4 || i == 5)
+	i = <-intC
+	require.True(t, i == 3 || i == 4 || i == 5)
+	i = <-intC
+	require.True(t, i == 3 || i == 4 || i == 5)
+
+	require.Equal(t, []string{"3-1:error"}, testNodeErrorRecorder.slice)
+}
+
+func testNodeFunc(counter *int32, intC chan int, nodeName string, i int, errString string) func() error {
 	var err error
 	if errString != "" {
 		err = errors.New(errString)
 	}
 	return func() error {
+		atomic.AddInt32(counter, 1)
 		intC <- i
 		fmt.Printf("ran %s, sent %d, returning %v\n", nodeName, i, err)
 		return err
@@ -113,22 +198,16 @@ func testNodeFunc(intC chan int, nodeName string, i int, errString string) func(
 }
 
 type testNodeErrorRecorder struct {
-	stringC chan string
+	slice []string
+	lock  *sync.Mutex
 }
 
 func newTestNodeErrorRecorder() *testNodeErrorRecorder {
-	return &testNodeErrorRecorder{make(chan string)}
+	return &testNodeErrorRecorder{make([]string, 0), &sync.Mutex{}}
 }
 
 func (t *testNodeErrorRecorder) Record(nodeName string, err error) {
-	t.stringC <- fmt.Sprintf("%s:%s", nodeName, err.Error())
-}
-
-func (t *testNodeErrorRecorder) strings() []string {
-	close(t.stringC)
-	var slice []string
-	for s := range t.stringC {
-		slice = append(slice, s)
-	}
-	return slice
+	t.lock.Lock()
+	defer t.lock.Unlock()
+	t.slice = append(t.slice, fmt.Sprintf("%s:%s", nodeName, err.Error()))
 }
