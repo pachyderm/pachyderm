d1f0712e386446b95f0b92aea4af2e4eb8290ef7
peter-edge <peter.edge@gmail.com>
2015-07-24 11:08:04 +0000

    add service name to pps node, move out GetNameToNodeInfo in pps graph package to own function

--- src/pps/graph/grapher.go
+++ src/pps/graph/grapher.go
@@ -3,15 +3,7 @@ package graph
 import (
 	"fmt"
 	"sync"
-
-	"github.com/pachyderm/pachyderm/src/log"
-	"github.com/pachyderm/pachyderm/src/pps"
 )
-
-type nodeInfo struct {
-	parents  []string
-	children []string
-}
 
 type run struct {
 	nodeRunners    map[string]*nodeRunner
@@ -34,37 +26,25 @@ func newGrapher() *grapher {
 	return &grapher{}
 }
 
-//func (g *grapher) GetNodeRunners(pipeline *pps.Pipeline) ([]NodeRunner, error) {
-//return getNodeRunners(pipeline)
-//}
-
-//func getNodeRunners(pipeline *pps.Pipeline) ([]NodeRunner, error) {
-//return nil, nil
-//}
-
 func (g *grapher) Build(
 	nodeErrorRecorder NodeErrorRecorder,
-	nameToNode map[string]*pps.Node,
+	nameToNodeInfo map[string]*NodeInfo,
 	nameToNodeFunc map[string]func() error,
 ) (Run, error) {
 	return build(
 		nodeErrorRecorder,
-		nameToNode,
+		nameToNodeInfo,
 		nameToNodeFunc,
 	)
 }
 
 func build(
 	nodeErrorRecorder NodeErrorRecorder,
-	nameToNode map[string]*pps.Node,
+	nameToNodeInfo map[string]*NodeInfo,
 	nameToNodeFunc map[string]func() error,
 ) (*run, error) {
-	nodeInfos, err := getNameToNodeInfo(nameToNode)
-	if err != nil {
-		return nil, err
-	}
 	cancel := make(chan bool)
-	nodeRunners, err := getNameToNodeRunner(nodeInfos, nodeErrorRecorder, cancel)
+	nodeRunners, err := getNameToNodeRunner(nameToNodeInfo, nodeErrorRecorder, cancel)
 	if err != nil {
 		return nil, err
 	}
@@ -114,7 +94,7 @@ func runNodes(
 }
 
 func getNameToNodeRunner(
-	nodeInfos map[string]*nodeInfo,
+	nodeInfos map[string]*NodeInfo,
 	nodeErrorRecorder NodeErrorRecorder,
 	cancel <-chan bool,
 ) (map[string]*nodeRunner, error) {
@@ -127,7 +107,7 @@ func getNameToNodeRunner(
 		)
 	}
 	for name, nodeInfo := range nodeInfos {
-		for _, child := range nodeInfo.children {
+		for _, child := range nodeInfo.Children {
 			errC := make(chan error, 1)
 			nodeRunner := nodeRunners[name]
 			childNodeRunner := nodeRunners[child]
@@ -136,127 +116,4 @@ func getNameToNodeRunner(
 		}
 	}
 	return nodeRunners, nil
-}
-
-func getNameToNodeInfo(nodes map[string]*pps.Node) (map[string]*nodeInfo, error) {
-	nodeToInputs := getNodeNameToInputStrings(nodes)
-	nodeToOutputs := getNodeNameToOutputStrings(nodes)
-	inputToNodes := getInputStringToNodeNames(nodes)
-	outputToNodes := getOutputStringToNodeNames(nodes)
-	nodeInfos := make(map[string](*nodeInfo))
-	for name := range nodes {
-		nodeInfo := &nodeInfo{
-			parents:  make([]string, 0),
-			children: make([]string, 0),
-		}
-		parents := make(map[string]bool)
-		for input := range nodeToInputs[name] {
-			for parent := range outputToNodes[input] {
-				if parent != name {
-					parents[parent] = true
-				}
-			}
-		}
-		for parent := range parents {
-			nodeInfo.parents = append(nodeInfo.parents, parent)
-		}
-		children := make(map[string]bool)
-		for output := range nodeToOutputs[name] {
-			for child := range inputToNodes[output] {
-				if child != name {
-					children[child] = true
-				}
-			}
-		}
-		for child := range children {
-			nodeInfo.children = append(nodeInfo.children, child)
-		}
-		nodeInfos[name] = nodeInfo
-	}
-	log.Printf("got node infos %v\n", nodeInfos)
-	return nodeInfos, nil
-}
-
-func getNodeNameToInputStrings(nodes map[string]*pps.Node) map[string]map[string]bool {
-	m := make(map[string]map[string]bool)
-	for name, node := range nodes {
-		n := make(map[string]bool)
-		if node.Input != nil {
-			for hostDir := range node.Input.Host {
-				// just need a differentiating string between types
-				n[fmt.Sprintf("host://%s", hostDir)] = true
-			}
-			for pfsRepo := range node.Input.Pfs {
-				n[fmt.Sprintf("pfs://%s", pfsRepo)] = true
-			}
-		}
-		m[name] = n
-	}
-	return m
-}
-
-func getNodeNameToOutputStrings(nodes map[string]*pps.Node) map[string]map[string]bool {
-	m := make(map[string]map[string]bool)
-	for name, node := range nodes {
-		n := make(map[string]bool)
-		if node.Output != nil {
-			for hostDir := range node.Output.Host {
-				// just need a differentiating string between types
-				n[fmt.Sprintf("host://%s", hostDir)] = true
-			}
-			for pfsRepo := range node.Output.Pfs {
-				n[fmt.Sprintf("pfs://%s", pfsRepo)] = true
-			}
-		}
-		m[name] = n
-	}
-	return m
-}
-
-func getInputStringToNodeNames(nodes map[string]*pps.Node) map[string]map[string]bool {
-	m := make(map[string]map[string]bool)
-	for name, node := range nodes {
-		if node.Input != nil {
-			for hostDir := range node.Input.Host {
-				// just need a differentiating string between types
-				s := fmt.Sprintf("host://%s", hostDir)
-				if _, ok := m[s]; !ok {
-					m[s] = make(map[string]bool)
-				}
-				m[s][name] = true
-			}
-			for pfsRepo := range node.Input.Pfs {
-				s := fmt.Sprintf("pfs://%s", pfsRepo)
-				if _, ok := m[s]; !ok {
-					m[s] = make(map[string]bool)
-				}
-				m[s][name] = true
-			}
-		}
-	}
-	return m
-}
-
-func getOutputStringToNodeNames(nodes map[string]*pps.Node) map[string]map[string]bool {
-	m := make(map[string]map[string]bool)
-	for name, node := range nodes {
-		if node.Output != nil {
-			for hostDir := range node.Output.Host {
-				// just need a differentiating string between types
-				s := fmt.Sprintf("host://%s", hostDir)
-				if _, ok := m[s]; !ok {
-					m[s] = make(map[string]bool)
-				}
-				m[s][name] = true
-			}
-			for pfsRepo := range node.Output.Pfs {
-				s := fmt.Sprintf("pfs://%s", pfsRepo)
-				if _, ok := m[s]; !ok {
-					m[s] = make(map[string]bool)
-				}
-				m[s][name] = true
-			}
-		}
-	}
-	return m
 }
---
+++ src/pps/graph/util.go
@@ -0,0 +1,131 @@
+package graph
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/pachyderm/pachyderm/src/pps"
+)
+
+func getNameToNodeInfo(nodes map[string]*pps.Node) (map[string]*NodeInfo, error) {
+	nodeToInputs := getNodeNameToInputStrings(nodes)
+	nodeToOutputs := getNodeNameToOutputStrings(nodes)
+	inputToNodes := getInputStringToNodeNames(nodes)
+	outputToNodes := getOutputStringToNodeNames(nodes)
+	nodeInfos := make(map[string](*NodeInfo))
+	for name := range nodes {
+		nodeInfo := &NodeInfo{
+			Parents:  make([]string, 0),
+			Children: make([]string, 0),
+		}
+		parents := make(map[string]bool)
+		for input := range nodeToInputs[name] {
+			for parent := range outputToNodes[input] {
+				if parent != name {
+					parents[parent] = true
+				}
+			}
+		}
+		for parent := range parents {
+			nodeInfo.Parents = append(nodeInfo.Parents, parent)
+		}
+		children := make(map[string]bool)
+		for output := range nodeToOutputs[name] {
+			for child := range inputToNodes[output] {
+				if child != name {
+					children[child] = true
+				}
+			}
+		}
+		for child := range children {
+			nodeInfo.Children = append(nodeInfo.Children, child)
+		}
+		nodeInfos[name] = nodeInfo
+	}
+	log.Printf("got node infos %v\n", nodeInfos)
+	return nodeInfos, nil
+}
+
+func getNodeNameToInputStrings(nodes map[string]*pps.Node) map[string]map[string]bool {
+	m := make(map[string]map[string]bool)
+	for name, node := range nodes {
+		n := make(map[string]bool)
+		if node.Input != nil {
+			for hostDir := range node.Input.Host {
+				// just need a differentiating string between types
+				n[fmt.Sprintf("host://%s", hostDir)] = true
+			}
+			for pfsRepo := range node.Input.Pfs {
+				n[fmt.Sprintf("pfs://%s", pfsRepo)] = true
+			}
+		}
+		m[name] = n
+	}
+	return m
+}
+
+func getNodeNameToOutputStrings(nodes map[string]*pps.Node) map[string]map[string]bool {
+	m := make(map[string]map[string]bool)
+	for name, node := range nodes {
+		n := make(map[string]bool)
+		if node.Output != nil {
+			for hostDir := range node.Output.Host {
+				// just need a differentiating string between types
+				n[fmt.Sprintf("host://%s", hostDir)] = true
+			}
+			for pfsRepo := range node.Output.Pfs {
+				n[fmt.Sprintf("pfs://%s", pfsRepo)] = true
+			}
+		}
+		m[name] = n
+	}
+	return m
+}
+
+func getInputStringToNodeNames(nodes map[string]*pps.Node) map[string]map[string]bool {
+	m := make(map[string]map[string]bool)
+	for name, node := range nodes {
+		if node.Input != nil {
+			for hostDir := range node.Input.Host {
+				// just need a differentiating string between types
+				s := fmt.Sprintf("host://%s", hostDir)
+				if _, ok := m[s]; !ok {
+					m[s] = make(map[string]bool)
+				}
+				m[s][name] = true
+			}
+			for pfsRepo := range node.Input.Pfs {
+				s := fmt.Sprintf("pfs://%s", pfsRepo)
+				if _, ok := m[s]; !ok {
+					m[s] = make(map[string]bool)
+				}
+				m[s][name] = true
+			}
+		}
+	}
+	return m
+}
+
+func getOutputStringToNodeNames(nodes map[string]*pps.Node) map[string]map[string]bool {
+	m := make(map[string]map[string]bool)
+	for name, node := range nodes {
+		if node.Output != nil {
+			for hostDir := range node.Output.Host {
+				// just need a differentiating string between types
+				s := fmt.Sprintf("host://%s", hostDir)
+				if _, ok := m[s]; !ok {
+					m[s] = make(map[string]bool)
+				}
+				m[s][name] = true
+			}
+			for pfsRepo := range node.Output.Pfs {
+				s := fmt.Sprintf("pfs://%s", pfsRepo)
+				if _, ok := m[s]; !ok {
+					m[s] = make(map[string]bool)
+				}
+				m[s][name] = true
+			}
+		}
+	}
+	return m
+}
