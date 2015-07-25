a5c7ed0b0720bfa43d6ea61e90b93c168403efec
Joe Doliner <jdoliner@pachyderm.io>
2015-07-25 00:24:18 +0000

    Adds ReadDirAll for directory.
    In theory this will suport lsing.

--- src/pfs/fuse/fuse.go
+++ src/pfs/fuse/fuse.go
@@ -62,6 +62,23 @@ func (*directory) Attr(ctx context.Context, a *fuse.Attr) error {
 	return nil
 }
 
+func nodeFromFileInfo(fs *filesystem, fileInfo *pfs.FileInfo) (fs.Node, error) {
+	switch fileInfo.FileType {
+	case pfs.FileType_FILE_TYPE_NONE:
+		log.Print("FileType_FILE_TYPE_NONE")
+		return nil, fuse.ENOENT
+	case pfs.FileType_FILE_TYPE_OTHER:
+		log.Print("FileType_FILE_TYPE_OTHER")
+		return nil, fuse.ENOENT
+	case pfs.FileType_FILE_TYPE_REGULAR:
+		return &file{fs, fileInfo.Path.Path, fileInfo.SizeBytes}, nil
+	case pfs.FileType_FILE_TYPE_DIR:
+		return &directory{fs, fileInfo.Path.Path}, nil
+	default:
+		return nil, fmt.Errorf("Unrecognized FileType.")
+	}
+}
+
 func (d *directory) Lookup(ctx context.Context, name string) (fs.Node, error) {
 	response, err := pfsutil.GetFileInfo(
 		d.fs.apiClient,
@@ -72,20 +89,28 @@ func (d *directory) Lookup(ctx context.Context, name string) (fs.Node, error) {
 	if err != nil {
 		return nil, err
 	}
-	switch response.GetFileInfo().FileType {
-	case pfs.FileType_FILE_TYPE_NONE:
-		log.Print("FileType_FILE_TYPE_NONE")
-		return nil, fuse.ENOENT
-	case pfs.FileType_FILE_TYPE_OTHER:
-		log.Print("FileType_FILE_TYPE_OTHER")
-		return nil, fuse.ENOENT
-	case pfs.FileType_FILE_TYPE_REGULAR:
-		return &file{d.fs, filepath.Join(d.path, name), response.GetFileInfo().SizeBytes}, nil
-	case pfs.FileType_FILE_TYPE_DIR:
-		return &directory{d.fs, filepath.Join(d.path, name)}, nil
-	default:
-		return nil, fmt.Errorf("Unrecognized FileType.")
+	return nodeFromFileInfo(d.fs, response.GetFileInfo())
+}
+
+func (d *directory) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
+	response, err := pfsutil.ListFiles(d.fs.apiClient, d.fs.repositoryName, d.fs.commitID, d.path, 0, 1)
+	if err != nil {
+		return nil, err
 	}
+	var result []fuse.Dirent
+	for _, fileInfo := range response.GetFileInfo() {
+		switch fileInfo.FileType {
+		case pfs.FileType_FILE_TYPE_NONE:
+			continue
+		case pfs.FileType_FILE_TYPE_OTHER:
+			continue
+		case pfs.FileType_FILE_TYPE_REGULAR:
+			result = append(result, fuse.Dirent{Inode: 3, Name: fileInfo.Path.Path, Type: fuse.DT_File})
+		case pfs.FileType_FILE_TYPE_DIR:
+			result = append(result, fuse.Dirent{Inode: 3, Name: fileInfo.Path.Path, Type: fuse.DT_Dir})
+		}
+	}
+	return result, nil
 }
 
 type file struct {
