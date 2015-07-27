58f28bcc8d980706c4fd3578931e27dc5c2013d0
Joe Doliner <jdoliner@pachyderm.io>
2015-07-27 21:56:14 +0000

    Adds checks for argument counts.

--- src/cmd/pfs/main.go
+++ src/cmd/pfs/main.go
@@ -46,35 +46,42 @@ func main() {
 		},
 	}
 
+	initUsage := "init repository-name"
 	initCmd := &cobra.Command{
-		Use:  "init repository-name",
+		Use:  initUsage,
 		Long: "Initalize a repository.",
 		Run: func(cmd *cobra.Command, args []string) {
+			checkArgs(args, 1, initUsage)
 			check(pfsutil.InitRepository(apiClient, args[0]))
 		},
 	}
-
+	mkdirUsage := "mkdir repository-name commit-id path/to/dir"
 	mkdirCmd := &cobra.Command{
-		Use:  "mkdir repository-name commit-id path/to/dir",
+		Use:  mkdirUsage,
 		Long: "Make a directory. Sub directories must already exist.",
 		Run: func(cmd *cobra.Command, args []string) {
+			checkArgs(args, 3, mkdirUsage)
 			check(pfsutil.MakeDirectory(apiClient, args[0], args[1], args[2]))
 		},
 	}
 
+	putUsage := "put repository-name branch-id path/to/file"
 	putCmd := &cobra.Command{
-		Use:  "put repository-name branch-id path/to/file",
+		Use:  putUsage,
 		Long: "Put a file from stdin. Directories must exist. branch-id must be a writeable commit.",
 		Run: func(cmd *cobra.Command, args []string) {
+			checkArgs(args, 3, putUsage)
 			_, err := pfsutil.PutFile(apiClient, args[0], args[1], args[2], os.Stdin)
 			check(err)
 		},
 	}
 
+	getUsage := "get repository-name commit-id path/to/file"
 	getCmd := &cobra.Command{
-		Use:  "get repository-name commit-id path/to/file",
+		Use:  getUsage,
 		Long: "Get a file from stdout. commit-id must be a readable commit.",
 		Run: func(cmd *cobra.Command, args []string) {
+			checkArgs(args, 3, getUsage)
 			reader, err := pfsutil.GetFile(apiClient, args[0], args[1], args[2])
 			check(err)
 			_, err = bufio.NewReader(reader).WriteTo(os.Stdout)
@@ -82,10 +89,12 @@ func main() {
 		},
 	}
 
+	lsUsage := "ls repository-name branch-id path/to/dir"
 	lsCmd := &cobra.Command{
-		Use:  "ls repository-name branch-id path/to/dir",
+		Use:  lsUsage,
 		Long: "List a directory. Directory must exist.",
 		Run: func(cmd *cobra.Command, args []string) {
+			checkArgs(args, 3, lsUsage)
 			listFilesResponse, err := pfsutil.ListFiles(apiClient, args[0], args[1], args[2], 0, 1)
 			check(err)
 			for _, fileInfo := range listFilesResponse.FileInfo {
@@ -94,38 +103,46 @@ func main() {
 		},
 	}
 
+	branchUsage := "branch repository-name commit-id"
 	branchCmd := &cobra.Command{
-		Use:  "branch repository-name commit-id",
+		Use:  branchUsage,
 		Long: "Branch a commit. commit-id must be a readable commit.",
 		Run: func(cmd *cobra.Command, args []string) {
+			checkArgs(args, 2, branchUsage)
 			branchResponse, err := pfsutil.Branch(apiClient, args[0], args[1])
 			check(err)
 			fmt.Println(branchResponse.Commit.Id)
 		},
 	}
 
+	commitUsage := "commit repository-name branch-id"
 	commitCmd := &cobra.Command{
-		Use:  "commit repository-name branch-id",
+		Use:  commitUsage,
 		Long: "Commit a branch. branch-id must be a writeable commit.",
 		Run: func(cmd *cobra.Command, args []string) {
+			checkArgs(args, 2, commitUsage)
 			check(pfsutil.Commit(apiClient, args[0], args[1]))
 		},
 	}
 
+	commitInfoUsage := "commit-info repository-name commit-id"
 	commitInfoCmd := &cobra.Command{
-		Use:  "commit-info repository-name commit-id",
+		Use:  commitInfoUsage,
 		Long: "Get info for a commit.",
 		Run: func(cmd *cobra.Command, args []string) {
+			checkArgs(args, 2, commitInfoUsage)
 			commitInfoResponse, err := pfsutil.GetCommitInfo(apiClient, args[0], args[1])
 			check(err)
 			fmt.Printf("%+v\n", commitInfoResponse.CommitInfo)
 		},
 	}
 
+	mountUsage := "mount repository-name commit-id mount-point"
 	mountCmd := &cobra.Command{
-		Use:  "mount repository-name commit-id mount-point",
+		Use:  mountUsage,
 		Long: "Mount a repository as a local file system.",
 		Run: func(cmd *cobra.Command, args []string) {
+			checkArgs(args, 3, mountUsage)
 			check(fuse.Mount(apiClient, args[0], args[1], args[2]))
 		},
 	}
@@ -156,6 +173,17 @@ The environment variable PFS_ADDRESS controls what server the CLI connects to, t
 func check(err error) {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		os.Exit(1)
+	}
+}
+
+func checkArgs(args []string, expected int, usage string) {
+	if len(args) != expected {
+		fmt.Fprintf(os.Stderr,
+			`Error, wrong number of arguments. Got %d, need %d.
+%s
+`,
+			len(args), expected, usage)
 		os.Exit(1)
 	}
 }
