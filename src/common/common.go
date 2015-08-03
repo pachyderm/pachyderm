f0aa4204503001576c2c6b2a0bbe89b2d9e76b2d
peter-edge <peter.edge@gmail.com>
2015-08-03 18:09:18 +0000

    common main function for client commands

--- src/common/common.go
+++ src/common/common.go
@@ -7,10 +7,12 @@ package common
 
 import (
 	"fmt"
+	"os"
 
 	"go.pedge.io/protolog/logrus"
 
 	stdlogrus "github.com/Sirupsen/logrus"
+	"github.com/peter-edge/go-env"
 	"github.com/satori/go.uuid"
 )
 
@@ -50,4 +52,16 @@ func ForceLogColors() {
 			},
 		},
 	)
+}
+
+func Main(do func(interface{}) error, appEnv interface{}, defaultEnv map[string]string) {
+	if err := env.Populate(appEnv, env.PopulateOptions{Defaults: defaultEnv}); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		os.Exit(1)
+	}
+	if err := do(appEnv); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		os.Exit(1)
+	}
+	os.Exit(0)
 }
