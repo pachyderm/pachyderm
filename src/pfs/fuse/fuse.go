579217c832d66f118e7dd3c54a1c611e8fc7b2d1
Joe Doliner <jdoliner@pachyderm.io>
2015-08-04 00:50:52 +0000

    Adds a benchmark for fuse filesystems.

--- src/pfs/fuse/fuse.go
+++ src/pfs/fuse/fuse.go
@@ -8,6 +8,9 @@ type Mounter interface {
 	// Mount mounts makes a repository available as a fuse filesystem at mountPoint
 	// If it succeeds Mount will block.
 	Mount(apiClient pfs.ApiClient, repositoryName string, mountPoint string, shard uint64, modulus uint64) error
+	// Unmount unmounts a mounted filesystem (duh).
+	// There's nothing special about this unmount, it's just doing a syscall under the hood
+	Unmount(mountPoint string) error
 	// Ready blocks until the filesysyem has been mounted
 	Ready()
 }
