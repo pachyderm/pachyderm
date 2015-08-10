ead15ed97eb1ba4f149e33d17bc6e66149617d55
peter-edge <peter.edge@gmail.com>
2015-08-10 19:42:37 +0200

    pps-simple example

--- etc/examples/pps-simple.sh
+++ etc/examples/pps-simple.sh
@@ -5,11 +5,18 @@ set -Ee
 DIR="$(cd "$(dirname "${0}")/../.." && pwd)"
 cd "${DIR}"
 
-func run() {
+run() {
   echo $@
   $@
 }
 
-run make launch-ppsd install
-run pps version
-run pps start github.com/pachyderm/pachyderm src/pps/server/testdata/basic
+if [ -z ${CLIENT} ]; then
+  run make launch-ppsd
+  run docker run -e CLIENT=1 --link pachyderm_ppsd_1 pachyderm_compile sh "etc/examples/$(basename "${0}")"
+  run make docker-clean-launch
+else
+  export PPS_ADDRESS="$(echo "${PACHYDERM_PPSD_1_PORT}" | sed "s/tcp:\/\///")"
+  run make install
+  run pps version
+  run pps start github.com/pachyderm/pachyderm src/pps/server/testdata/basic
+fi
