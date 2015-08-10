c8fffcfa578023afc7b2da79f562d1a7b2e5ac37
peter-edge <peter.edge@gmail.com>
2015-08-10 20:07:53 +0200

    cleanup of examples source.bash

--- etc/examples/source.bash
+++ etc/examples/source.bash
@@ -9,12 +9,37 @@ run_make() {
   run make -C "${REPO_DIR}" $@
 }
 
+do_pfs() {
+  if [ -z ${PFS_CLIENT_EXAMPLE} ]; then
+    run_make launch-pfsd
+    run docker run -e PFS_CLIENT_EXAMPLE=1 --link pachyderm_pfsd_1 pachyderm_compile bash "etc/examples/$(basename "${1}")"
+    run_make docker-clean-launch
+  else
+    export PFS_ADDRESS="$(echo "${PACHYDERM_PFSD_1_PORT}" | sed "s/tcp:\/\///")"
+    run_make install
+    ${2}
+  fi
+}
+
 do_pps() {
   if [ -z ${PPS_CLIENT_EXAMPLE} ]; then
     run_make launch-ppsd
     run docker run -e PPS_CLIENT_EXAMPLE=1 --link pachyderm_ppsd_1 pachyderm_compile bash "etc/examples/$(basename "${1}")"
     run_make docker-clean-launch
   else
+    export PPS_ADDRESS="$(echo "${PACHYDERM_PPSD_1_PORT}" | sed "s/tcp:\/\///")"
+    run_make install
+    ${2}
+  fi
+}
+
+do_pfs_pps() {
+  if [ -z ${PFS_PPS_CLIENT_EXAMPLE} ]; then
+    run_make launch
+    run docker run -e PFS_PPS_CLIENT_EXAMPLE=1 --link pachyderm_pfsd_1 --link pachyderm_ppsd_1 pachyderm_compile bash "etc/examples/$(basename "${1}")"
+    run_make docker-clean-launch
+  else
+    export PFS_ADDRESS="$(echo "${PACHYDERM_PFSD_1_PORT}" | sed "s/tcp:\/\///")"
     export PPS_ADDRESS="$(echo "${PACHYDERM_PPSD_1_PORT}" | sed "s/tcp:\/\///")"
     run_make install
     ${2}
