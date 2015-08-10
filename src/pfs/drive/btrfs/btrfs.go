c57e1bded443832e935d87a56396d8e9d5376061
peter-edge <peter.edge@gmail.com>
2015-08-10 14:20:43 +0200

    move btrfs driver to own package in drive, remove pkg/btrfs

---
+++ src/pfs/drive/btrfs/btrfs.go
@@ -0,0 +1,10 @@
+// +build linux
+
+package btrfs
+
+import "github.com/pachyderm/pachyderm/src/pfs/drive"
+
+// NewDriver constructs a new Driver for btrfs.
+func NewDriver(rootDir string) drive.Driver {
+	return newDriver(rootDir)
+}
