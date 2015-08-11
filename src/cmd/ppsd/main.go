470e4a248a8d62e9d48dba1138a62db719365b84
Peter Edge <peter.edge@gmail.com>
2015-08-11 20:39:32 +0200

    use rethink store client in ppsd

--- src/cmd/ppsd/main.go
+++ src/cmd/ppsd/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"os"
+
 	"github.com/pachyderm/pachyderm"
 	"github.com/pachyderm/pachyderm/src/pkg/grpcutil"
 	"github.com/pachyderm/pachyderm/src/pkg/mainutil"
@@ -12,13 +16,16 @@ import (
 
 var (
 	defaultEnv = map[string]string{
-		"PPS_API_PORT": "651",
+		"PPS_API_PORT":      "651",
+		"PPS_DATABASE_NAME": "pachyderm",
 	}
 )
 
 type appEnv struct {
-	APIPort   int `env:"PPS_API_PORT"`
-	TracePort int `env:"PPS_TRACE_PORT"`
+	APIPort         int    `env:"PPS_API_PORT"`
+	DatabaseAddress string `env:"PPS_DATABASE_ADDRESS"`
+	DatabaseName    string `env:"PPS_DATABASE_NAME"`
+	TracePort       int    `env:"PPS_TRACE_PORT"`
 }
 
 func main() {
@@ -27,12 +34,38 @@ func main() {
 
 func do(appEnvObj interface{}) error {
 	appEnv := appEnvObj.(*appEnv)
+	rethinkClient, err := getRethinkClient(appEnv.DatabaseAddress, appEnv.DatabaseName)
+	if err != nil {
+		return err
+	}
 	return grpcutil.GrpcDo(
 		appEnv.APIPort,
 		appEnv.TracePort,
 		pachyderm.Version,
 		func(s *grpc.Server) {
-			pps.RegisterApiServer(s, server.NewAPIServer(store.NewInMemoryClient()))
+			pps.RegisterApiServer(s, server.NewAPIServer(rethinkClient))
 		},
 	)
+}
+
+func getRethinkClient(address string, databaseName string) (store.Client, error) {
+	var err error
+	if address == "" {
+		address, err = getRethinkAddress()
+		if err != nil {
+			return nil, err
+		}
+	}
+	if err := store.InitDBs(address, databaseName); err != nil {
+		return nil, err
+	}
+	return store.NewRethinkClient(address, databaseName)
+}
+
+func getRethinkAddress() (string, error) {
+	rethinkAddr := os.Getenv("RETHINK_PORT_28015_TCP_ADDR")
+	if rethinkAddr == "" {
+		return "", errors.New("RETHINK_PORT_28015_TCP_ADDR not set")
+	}
+	return fmt.Sprintf("%s:28015", rethinkAddr), nil
 }
