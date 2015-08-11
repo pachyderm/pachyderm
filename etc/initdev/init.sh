a2938201480f816adab7e51f6bf5b3dac0823644
peter-edge <peter.edge@gmail.com>
2015-08-11 16:12:31 +0200

    update golang and docker-compose in Dockerfile and init.sh

--- etc/initdev/init.sh
+++ etc/initdev/init.sh
@@ -17,13 +17,12 @@ apt-get install -yq --no-install-recommends \
 
 # installs go
 # we use the beta for now since it will be released in a few weeks
-curl -sSL https://storage.googleapis.com/golang/go1.5beta3.linux-amd64.tar.gz | tar -C /usr/local -xz
+curl -sSL https://storage.googleapis.com/golang/go1.5rc1.linux-amd64.tar.gz | tar -C /usr/local -xz
 echo 'export PATH=${PATH}:/usr/local/go/bin' >> '/etc/profile'
-echo 'export GOROOT=/usr/local/go' >> '/etc/profile'
-# makes ~/go and sets GOPATH to ~/go
-su - ${1} -c "echo mkdir -p /home/${1}/go >> /home/${1}/.bash_aliases"
-su - ${1} -c "echo export GOPATH=/home/${1}/go >> /home/${1}/.bash_aliases"
+su - ${1} -c "echo mkdir -p /home/${1}/go >> /home/${1}/.profile"
+su - ${1} -c "echo export GOPATH=/home/${1}/go >> /home/${1}/.profile"
 
+# installs docker
 wget -qO- https://get.docker.com/ | sh
 # sudoless use of docker
 groupadd docker || true
@@ -31,7 +30,7 @@ usermod -aG docker ${1}
 service docker restart
 
 # installs docker-compose
-curl -sSL https://github.com/docker/compose/releases/download/1.4.0rc2/docker-compose-$(uname -s)-$(uname -m) > /usr/local/bin/docker-compose
+curl -sSL https://github.com/docker/compose/releases/download/1.4.0/docker-compose-$(uname -s)-$(uname -m) > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 # installs kubernetes cli
 curl -sSL https://storage.googleapis.com/kubernetes-release/release/v1.0.1/bin/linux/amd64/kubectl > /usr/local/bin/kubectl
