128999329bccdeb0794cf1bdaa88c40acd49bbd4
Peter Edge <peter.edge@gmail.com>
2015-08-13 17:21:36 +0200

    add destroyable to concurrent, add destroyable to brancher

--- src/pps/branch/brancher.go
+++ src/pps/branch/brancher.go
@@ -4,6 +4,7 @@ import (
 	"sync"
 
 	"github.com/pachyderm/pachyderm/src/pfs"
+	"github.com/pachyderm/pachyderm/src/pkg/concurrent"
 	"github.com/pachyderm/pachyderm/src/pps/store"
 )
 
@@ -11,6 +12,7 @@ type brancher struct {
 	pfsAPIClient pfs.ApiClient
 	storeClient  store.Client
 
+	destroyable          concurrent.Destroyable
 	repositoryToBranchID map[string]string
 	lock                 *sync.RWMutex
 }
@@ -22,6 +24,7 @@ func newBrancher(
 	return &brancher{
 		pfsAPIClient,
 		storeClient,
+		concurrent.NewDestroyable(),
 		make(map[string]string),
 		&sync.RWMutex{},
 	}
@@ -32,9 +35,26 @@ func (b *brancher) GetOutputCommitID(
 	inputCommitID string,
 	outputRepositoryName string,
 ) (string, error) {
-	return "", nil
+	value, err := b.destroyable.Do(func() (interface{}, error) {
+		return b.getOutputCommitID(inputRepositoryName, inputCommitID, outputRepositoryName)
+	})
+	if err != nil {
+		return "", err
+	}
+	return value.(string), nil
+}
+
+func (b *brancher) getOutputCommitID(
+	inputRepositoryName string,
+	inputCommitID string,
+	outputRepositoryName string,
+) (interface{}, error) {
+	return nil, nil
 }
 
 func (b *brancher) CommitOutstanding() error {
+	if err := b.destroyable.Destroy(); err != nil {
+		return err
+	}
 	return nil
 }
