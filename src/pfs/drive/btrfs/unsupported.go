aa26b5509aa24be7f538796f30471bdffbe04212
Peter Edge <peter.edge@gmail.com>
2015-08-25 16:29:06 -0500

    fix btrfs.NewDriver for builds other than linux (again)

--- src/pfs/drive/btrfs/unsupported.go
+++ src/pfs/drive/btrfs/unsupported.go
@@ -6,5 +6,5 @@ import "github.com/pachyderm/pachyderm/src/pfs/drive"
 
 // NewDriver constructs a new Driver for btrfs.
 func NewDriver(rootDir string, namespace string) (drive.Driver, error) {
-	return nil
+	return nil, nil
 }
