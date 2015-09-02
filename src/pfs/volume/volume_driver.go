3e52c5f32a1fc164c2ef002e503df47b8f5b62e4
Peter Edge <peter.edge@gmail.com>
2015-09-01 20:59:40 -0500

    volume driver create mountpoint in Create function and logging/cleanup

--- src/pfs/volume/volume_driver.go
+++ src/pfs/volume/volume_driver.go
@@ -8,12 +8,10 @@ import (
 	"strings"
 	"sync"
 
+	"go.pedge.io/protolog"
+
 	"github.com/pachyderm/pachyderm/src/pfs/fuse"
 	"github.com/satori/go.uuid"
-)
-
-const (
-	tempdirPrefix = "pfs-docker-volume"
 )
 
 type volume struct {
@@ -43,6 +41,7 @@ func newVolumeDriver(mounter fuse.Mounter, baseMountpoint string) *volumeDriver 
 }
 
 func (v *volumeDriver) Create(name string, opts map[string]string) error {
+	protolog.Infof("Create(name:%s, opts:%v)", name, opts)
 	repository, ok := opts["repository"]
 	if !ok {
 		return fmt.Errorf("option repository not found in %v", opts)
@@ -67,12 +66,16 @@ func (v *volumeDriver) Create(name string, opts map[string]string) error {
 	if err != nil {
 		return err
 	}
+	dir := filepath.Join(v.baseMountpoint, strings.Replace(uuid.NewV4().String(), "-", "", -1))
+	if err := os.MkdirAll(dir, 0777); err != nil {
+		return err
+	}
 	volume := &volume{
 		repository,
 		commitID,
 		shard,
 		modulus,
-		"",
+		dir,
 		&sync.RWMutex{},
 	}
 	v.lock.Lock()
@@ -82,10 +85,12 @@ func (v *volumeDriver) Create(name string, opts map[string]string) error {
 	}
 	v.nameToVolume[name] = volume
 	v.lock.Unlock()
+	protolog.Infof("Create name:%s volume:%+v", name, volume)
 	return nil
 }
 
 func (v *volumeDriver) Remove(name string) error {
+	protolog.Infof("Remove(name:%s)", name)
 	v.lock.Lock()
 	volume, ok := v.nameToVolume[name]
 	if !ok {
@@ -94,24 +99,12 @@ func (v *volumeDriver) Remove(name string) error {
 	}
 	delete(v.nameToVolume, name)
 	v.lock.Unlock()
-
-	volume.lock.RLock()
-	if volume.mountpoint != "" {
-		v.lock.Lock()
-		if _, ok := v.nameToVolume[name]; ok {
-			v.lock.Unlock()
-			return fmt.Errorf("volume already exists: %s", name)
-		}
-		v.nameToVolume[name] = volume
-		volume.lock.RUnlock()
-		v.lock.Unlock()
-		return fmt.Errorf("volume %s still has mountpoint %s", name, volume.mountpoint)
-	}
-	volume.lock.RUnlock()
+	protolog.Infof("Remove name:%s volume:%+v", name, volume)
 	return nil
 }
 
 func (v *volumeDriver) Path(name string) (string, error) {
+	protolog.Infof("Path(name:%s)", name)
 	v.lock.RLock()
 	volume, ok := v.nameToVolume[name]
 	if !ok {
@@ -119,11 +112,12 @@ func (v *volumeDriver) Path(name string) (string, error) {
 		return "", fmt.Errorf("volume does not exist: %s", name)
 	}
 	v.lock.RUnlock()
-	// TODO(pedge): should this return an error if volume.mountpoint == ""?
+	protolog.Infof("Path name:%s volume:%+v", name, volume)
 	return volume.mountpoint, nil
 }
 
 func (v *volumeDriver) Mount(name string) (string, error) {
+	protolog.Infof("Mount(name:%s)", name)
 	v.lock.RLock()
 	volume, ok := v.nameToVolume[name]
 	if !ok {
@@ -131,30 +125,21 @@ func (v *volumeDriver) Mount(name string) (string, error) {
 		return "", fmt.Errorf("volume does not exist: %s", name)
 	}
 	v.lock.RUnlock()
-
-	volume.lock.Lock()
-	defer volume.lock.Unlock()
-	if volume.mountpoint != "" {
-		return "", fmt.Errorf("volume %s already mounted at %s", name, volume.mountpoint)
-	}
-	dir := filepath.Join(v.baseMountpoint, strings.Replace(uuid.NewV4().String(), "-", "", -1))
-	if err := os.MkdirAll(dir, 0777); err != nil {
-		return "", err
-	}
 	if err := v.mounter.Mount(
 		volume.repository,
 		volume.commitID,
-		dir,
+		volume.mountpoint,
 		volume.shard,
 		volume.modulus,
 	); err != nil {
 		return "", err
 	}
-	volume.mountpoint = dir
-	return dir, nil
+	protolog.Infof("Mount name:%s volume:%+v", name, volume)
+	return volume.mountpoint, nil
 }
 
 func (v *volumeDriver) Unmount(name string) error {
+	protolog.Infof("Unmount(name:%s)", name)
 	v.lock.RLock()
 	volume, ok := v.nameToVolume[name]
 	if !ok {
@@ -162,16 +147,12 @@ func (v *volumeDriver) Unmount(name string) error {
 		return fmt.Errorf("volume does not exist: %s", name)
 	}
 	v.lock.RUnlock()
-
-	volume.lock.Lock()
-	defer volume.lock.Unlock()
-	if volume.mountpoint == "" {
-		return fmt.Errorf("volume %s not mounted", name)
-	}
-	mountpoint := volume.mountpoint
-	if err := v.mounter.Unmount(mountpoint); err != nil {
+	if err := v.mounter.Unmount(volume.mountpoint); err != nil {
 		return err
 	}
-	volume.mountpoint = ""
-	return v.mounter.Wait(mountpoint)
+	if err := v.mounter.Wait(volume.mountpoint); err != nil {
+		return err
+	}
+	protolog.Infof("Unmount name:%s volume:%+v", name, volume)
+	return nil
 }
