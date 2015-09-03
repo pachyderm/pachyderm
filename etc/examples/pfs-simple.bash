6beefa3e91f3b9d4e696dd798bab5f96c9f26fa4
Peter Edge <peter.edge@gmail.com>
2015-09-03 15:49:34 -0500

    add to pfs-simple.bash in etc/examples

--- etc/examples/pfs-simple.bash
+++ etc/examples/pfs-simple.bash
@@ -8,3 +8,6 @@ echo hello | pfs put test ${commit_id} foo.txt
 pfs commit test ${commit_id}
 pfs ls test ${commit_id} /
 docker volume create --driver=pfs --opt repository=test --opt commit_id=${commit_id} --opt shard=0 --opt modulus=1 --name foo
+docker run --name foo --volume foo:/in --volume-driver pfs ubuntu cat /in/foo.txt
+docker rm foo
+docker volume rm foo
