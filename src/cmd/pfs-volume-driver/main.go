b7549293feb0ec174e5eb3a0015e511924747c90
Peter Edge <peter.edge@gmail.com>
2015-09-03 15:29:55 -0500

    remove NewRegistry and NewProvider from pfsutil and ppsutil

--- src/cmd/pfs-volume-driver/main.go
+++ src/cmd/pfs-volume-driver/main.go
@@ -1,29 +1,26 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"os"
+	"strings"
 
 	"go.pedge.io/dockervolume"
 
 	"github.com/pachyderm/pachyderm/src/pfs"
 	"github.com/pachyderm/pachyderm/src/pfs/fuse"
-	"github.com/pachyderm/pachyderm/src/pfs/pfsutil"
 	"github.com/pachyderm/pachyderm/src/pfs/volume"
-	"github.com/pachyderm/pachyderm/src/pkg/discovery"
-	"github.com/pachyderm/pachyderm/src/pkg/grpcutil"
 	"github.com/pachyderm/pachyderm/src/pkg/mainutil"
 	"google.golang.org/grpc"
 )
 
 const (
-	volumeDriverName = "pfs"
+	volumeDriverName  = "pfs"
+	volumeDriverGroup = "root"
 )
 
 type appEnv struct {
-	EtcdAddress    string `env:"ETCD_ADDRESS"`
-	BaseMountpoint string `env:"BASE_MOUNTPOINT,required"`
+	PachydermPfsd1Port string `env:"PACHYDERM_PFSD_1_PORT"`
+	PfsAddress         string `env:"PFS_ADDRESS"`
+	BaseMountpoint     string `env:"BASE_MOUNTPOINT,required"`
 }
 
 func main() {
@@ -32,11 +29,13 @@ func main() {
 
 func do(appEnvObj interface{}) error {
 	appEnv := appEnvObj.(*appEnv)
-	discoveryClient, err := getEtcdClient(appEnv.EtcdAddress)
-	if err != nil {
-		return err
+	address := appEnv.PachydermPfsd1Port
+	if address == "" {
+		address = appEnv.PfsAddress
+	} else {
+		address = strings.Replace(address, "tcp://", "", -1)
 	}
-	clientConn, err := pfsutil.NewProvider(discoveryClient, grpcutil.NewDialer(grpc.WithInsecure())).GetClientConn()
+	clientConn, err := grpc.Dial(address, grpc.WithInsecure())
 	if err != nil {
 		return err
 	}
@@ -54,25 +53,6 @@ func do(appEnvObj interface{}) error {
 		),
 		dockervolume.ProtocolUnix,
 		volumeDriverName,
-		"root",
+		volumeDriverGroup,
 	)
-}
-
-func getEtcdClient(address string) (discovery.Client, error) {
-	etcdAddress, err := getEtcdAddress(address)
-	if err != nil {
-		return nil, err
-	}
-	return discovery.NewEtcdClient(etcdAddress), nil
-}
-
-func getEtcdAddress(address string) (string, error) {
-	if address != "" {
-		return address, nil
-	}
-	etcdAddr := os.Getenv("ETCD_PORT_2379_TCP_ADDR")
-	if etcdAddr == "" {
-		return "", errors.New("ETCD_PORT_2379_TCP_ADDR not set")
-	}
-	return fmt.Sprintf("http://%s:2379", etcdAddr), nil
 }
