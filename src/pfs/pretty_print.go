c024d3f033fa0e38e4f2b7a315d4754fb57f0766
Joe Doliner <jdoliner@gmail.com>
2015-09-09 20:04:44 -0700

    Add pretty printing for files.

--- src/pfs/pretty_print.go
+++ src/pfs/pretty_print.go
@@ -3,6 +3,8 @@ package pfs
 import (
 	"fmt"
 	"io"
+
+	"github.com/docker/docker/pkg/units"
 )
 
 func PrintCommitInfoHeader(w io.Writer) {
@@ -24,6 +26,24 @@ func PrintCommitInfo(w io.Writer, commitInfo *CommitInfo) {
 	fmt.Fprint(w, "-\t")
 	fmt.Fprint(w, "-\t")
 	fmt.Fprint(w, "-\t")
+	fmt.Fprint(w, "-\t")
+	fmt.Fprintln(w, "")
+}
+
+func PrintFileHeader(w io.Writer) {
+	fmt.Fprintln(w, "NAME\tTYPE\tMODIFIED\tLAST_COMMIT_MODIFIED\tSIZE\tPERMISSIONS\t")
+}
+
+func PrintFileInfo(w io.Writer, fileInfo *FileInfo) {
+	fmt.Fprintf(w, "%s\t", fileInfo.Path.Path)
+	if fileInfo.FileType == FileType_FILE_TYPE_REGULAR {
+		fmt.Fprint(w, "file\t")
+	} else {
+		fmt.Fprint(w, "dir\t")
+	}
+	fmt.Fprint(w, "-\t")
+	fmt.Fprintf(w, "%d\t", units.BytesSize(float64(fileInfo.SizeBytes)))
+	fmt.Fprintf(w, "%d\t", fileInfo.Perm)
 	fmt.Fprint(w, "-\t")
 	fmt.Fprintln(w, "")
 }
