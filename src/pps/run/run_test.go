6ad7791b31981ddadb38478d61ce08c7d3cf0da1
Peter Edge <peter.edge@gmail.com>
2015-09-11 16:57:10 -0700

    fix compile for src/pps/run

--- src/pps/run/run_test.go
+++ src/pps/run/run_test.go
@@ -3,19 +3,13 @@ package run
 import (
 	"testing"
 
-	"go.pedge.io/protolog/logrus"
-
 	"github.com/pachyderm/pachyderm/src/pkg/require"
 	"github.com/pachyderm/pachyderm/src/pps"
 	"github.com/pachyderm/pachyderm/src/pps/parse"
 )
 
-func init() {
-	logrus.Register()
-}
-
 func TestGetNameToNodeInfo(t *testing.T) {
-	pipeline, err := parse.NewParser().ParsePipeline("../parse/testdata/basic", "")
+	pipeline, err := parse.NewParser().ParsePipeline("../parse/testdata/basic")
 	require.NoError(t, err)
 	nodes := pps.GetNameToNode(pipeline)
 	nodeInfos, err := getNameToNodeInfo(nodes)
