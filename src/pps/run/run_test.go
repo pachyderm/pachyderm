d584d8ffbdf24de1786d9b5f5165911c35c6c89b
Peter Edge <peter.edge@gmail.com>
2015-09-11 18:59:35 -0700

    try to get tests working again pps

--- src/pps/run/run_test.go
+++ src/pps/run/run_test.go
@@ -4,15 +4,13 @@ import (
 	"testing"
 
 	"github.com/pachyderm/pachyderm/src/pkg/require"
-	"github.com/pachyderm/pachyderm/src/pps"
 	"github.com/pachyderm/pachyderm/src/pps/parse"
 )
 
 func TestGetNameToNodeInfo(t *testing.T) {
 	pipeline, err := parse.NewParser().ParsePipeline("../parse/testdata/basic")
 	require.NoError(t, err)
-	nodes := pps.GetNameToNode(pipeline)
-	nodeInfos, err := getNameToNodeInfo(nodes)
+	nodeInfos, err := getNameToNodeInfo(pipeline.NameToNode)
 	require.NoError(t, err)
 	require.Equal(t, []string{"bar-node"}, nodeInfos["baz-node-bar-in-bar-out-in"].Parents)
 }
