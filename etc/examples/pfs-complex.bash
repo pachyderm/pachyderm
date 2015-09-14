765c8505060e230b722a9f7a4ae0360c9054e940
Joe Doliner <jdoliner@gmail.com>
2015-09-14 02:54:15 -0700

    Update test script.

--- etc/examples/pfs-complex.bash
+++ etc/examples/pfs-complex.bash
@@ -2,18 +2,18 @@
 
 set -xe
 
-pfs init test
-commit1="$(pfs branch test scratch)"
-echo "hello world\n" | pfs put test ${commit1} hello.txt
-echo "lorem ipsum\n" | pfs put test ${commit1} lorem.txt
-pfs commit test ${commit1}
-commit2="$(pfs branch test $commit1)"
-commit3="$(pfs branch test $commit1)"
-echo "hello world\nthis is pfs\n" | pfs put test ${commit2} hello.txt
-echo "lorem ipsum\nNeque porro\n" | pfs put test ${commit3} lorem.txt
-pfs commit test ${commit2}
-pfs commit test ${commit3}
-commit4="$(pfs branch test $commit3)"
-echo "fizz buzz\nfizz buzz\n" | pfs put test ${commit4} fizz_buzz.txt
-echo "foo bar\nfoo bar\n" | pfs put test ${commit4} foo_bar.txt
-pfs commit test ${commit4}
+pfs repo-create test
+commit1="$(pfs commit-start test scratch)"
+echo "hello world\n" | pfs file-put test ${commit1} hello.txt
+echo "lorem ipsum\n" | pfs file-put test ${commit1} lorem.txt
+pfs commit-finish test ${commit1}
+commit2="$(pfs commit-start test $commit1)"
+commit3="$(pfs commit-start test $commit1)"
+echo "hello world\nthis is pfs\n" | pfs file-put test ${commit2} hello.txt
+echo "lorem ipsum\nNeque porro\n" | pfs file-put test ${commit3} lorem.txt
+pfs commit-finish test ${commit2}
+pfs commit-finish test ${commit3}
+commit4="$(pfs commit-start test $commit3)"
+echo "fizz buzz\nfizz buzz\n" | pfs file-put test ${commit4} fizz_buzz.txt
+echo "foo bar\nfoo bar\n" | pfs file-put test ${commit4} foo_bar.txt
+pfs commit-finish test ${commit4}
