ad84ee67cc831d1127efe74f24ba425b50b515c9
Joe Doliner <jdoliner@gmail.com>
2015-09-16 18:23:39 -0700

    Steps toward getting things compiling.

--- src/pfs/route/sharder.go
+++ src/pfs/route/sharder.go
@@ -8,15 +8,20 @@ import (
 )
 
 type sharder struct {
-	numShards int
+	numShards   int
+	numReplicas int
 }
 
-func newSharder(numShards int) *sharder {
-	return &sharder{numShards}
+func newSharder(numShards int, numReplicas int) *sharder {
+	return &sharder{numShards, numReplicas}
 }
 
 func (s *sharder) NumShards() int {
 	return s.numShards
+}
+
+func (s *sharder) NumReplicas() int {
+	return s.numReplicas
 }
 
 func (s *sharder) GetShard(file *pfs.File) (int, error) {
