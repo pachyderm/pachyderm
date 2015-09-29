8ab9b90e4bb6ca610e29ce70595e3156c6ea340f
Peter Edge <peter.edge@gmail.com>
2015-09-29 12:09:24 -0700

    update vendor

--- vendor/go.pedge.io/dockervolume/api_server.go
+++ vendor/go.pedge.io/dockervolume/api_server.go
@@ -1,7 +1,11 @@
 package dockervolume
 
 import (
+	"time"
+
+	"github.com/golang/protobuf/proto"
 	"go.pedge.io/google-protobuf"
+	"go.pedge.io/proto/rpclog"
 	"golang.org/x/net/context"
 )
 
@@ -21,11 +25,15 @@ func newAPIServer(volumeDriver VolumeDriver) *apiServer {
 	return &apiServer{volumeDriver}
 }
 
-func (a *apiServer) Activate(_ context.Context, _ *google_protobuf.Empty) (*ActivateResponse, error) {
+func (a *apiServer) Activate(_ context.Context, request *google_protobuf.Empty) (response *ActivateResponse, err error) {
+	start := time.Now()
+	defer func() { a.log("Activate", request, response, err, start) }()
 	return activateResponse, nil
 }
 
-func (a *apiServer) Create(_ context.Context, request *CreateRequest) (*CreateResponse, error) {
+func (a *apiServer) Create(_ context.Context, request *CreateRequest) (response *CreateResponse, err error) {
+	start := time.Now()
+	defer func() { a.log("Create", request, response, err, start) }()
 	if err := a.volumeDriver.Create(request.Name, request.Opts); err != nil {
 		return &CreateResponse{
 			Err: err.Error(),
@@ -34,7 +42,9 @@ func (a *apiServer) Create(_ context.Context, request *CreateRequest) (*CreateRe
 	return &CreateResponse{}, nil
 }
 
-func (a *apiServer) Remove(_ context.Context, request *RemoveRequest) (*RemoveResponse, error) {
+func (a *apiServer) Remove(_ context.Context, request *RemoveRequest) (response *RemoveResponse, err error) {
+	start := time.Now()
+	defer func() { a.log("Remove", request, response, err, start) }()
 	if err := a.volumeDriver.Remove(request.Name); err != nil {
 		return &RemoveResponse{
 			Err: err.Error(),
@@ -43,7 +53,9 @@ func (a *apiServer) Remove(_ context.Context, request *RemoveRequest) (*RemoveRe
 	return &RemoveResponse{}, nil
 }
 
-func (a *apiServer) Path(_ context.Context, request *PathRequest) (*PathResponse, error) {
+func (a *apiServer) Path(_ context.Context, request *PathRequest) (response *PathResponse, err error) {
+	start := time.Now()
+	defer func() { a.log("Path", request, response, err, start) }()
 	mountpoint, err := a.volumeDriver.Path(request.Name)
 	if err != nil {
 		return &PathResponse{
@@ -56,7 +68,9 @@ func (a *apiServer) Path(_ context.Context, request *PathRequest) (*PathResponse
 	}, nil
 }
 
-func (a *apiServer) Mount(_ context.Context, request *MountRequest) (*MountResponse, error) {
+func (a *apiServer) Mount(_ context.Context, request *MountRequest) (response *MountResponse, err error) {
+	start := time.Now()
+	defer func() { a.log("Mount", request, response, err, start) }()
 	mountpoint, err := a.volumeDriver.Mount(request.Name)
 	if err != nil {
 		return &MountResponse{
@@ -69,11 +83,17 @@ func (a *apiServer) Mount(_ context.Context, request *MountRequest) (*MountRespo
 	}, nil
 }
 
-func (a *apiServer) Unmount(_ context.Context, request *UnmountRequest) (*UnmountResponse, error) {
+func (a *apiServer) Unmount(_ context.Context, request *UnmountRequest) (response *UnmountResponse, err error) {
+	start := time.Now()
+	defer func() { a.log("Unmount", request, response, err, start) }()
 	if err := a.volumeDriver.Unmount(request.Name); err != nil {
 		return &UnmountResponse{
 			Err: err.Error(),
 		}, nil
 	}
 	return &UnmountResponse{}, nil
+}
+
+func (a *apiServer) log(methodName string, request proto.Message, response proto.Message, err error, start time.Time) {
+	protorpclog.Info("dockervolume.API", methodName, request, response, err, time.Since(start))
 }
