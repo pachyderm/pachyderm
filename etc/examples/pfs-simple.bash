9558cf149df6d05d1e04d6bdcf0859571a2e4893
Peter Edge <peter.edge@gmail.com>
2015-09-29 13:14:45 -0700

    fix pfs-simple.bash example

--- etc/examples/pfs-simple.bash
+++ etc/examples/pfs-simple.bash
@@ -3,12 +3,12 @@
 set -xe
 
 if [ -n "${SETUP}" ]; then
-  pfs init test
-  commit_id="$(pfs branch test scratch)"
-  echo hello | pfs put test ${commit_id} foo.txt
-  pfs commit test ${commit_id}
+  pfs create-repo test
+  commit_id="$(pfs start-commit test scratch)"
+  echo hello | pfs put-file test ${commit_id} foo.txt
+  pfs finish-commit test ${commit_id}
 else
-  commit_id="$(pfs list-commits test | head -1 | cut -f 5 -d : | cut -f 1 -d ' ' | sed 's/"//g')"
+  commit_id="$(pfs list-commit test | head -1 | cut -f 5 -d : | cut -f 1 -d ' ' | sed 's/"//g')"
 fi
 pfs ls test ${commit_id} /
 docker volume create --driver=pfs --opt repository=test --opt commit_id=${commit_id} --opt shard=0 --opt modulus=1 --name foo
