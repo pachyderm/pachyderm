9414f34703bc71df6a3a1d74ca69a8921a6d2246
Peter Edge <peter.edge@gmail.com>
2015-09-29 13:15:25 -0700

    fix pfs-simple.bash example

--- etc/examples/pfs-simple.bash
+++ etc/examples/pfs-simple.bash
@@ -10,7 +10,7 @@ if [ -n "${SETUP}" ]; then
 else
   commit_id="$(pfs list-commit test | head -1 | cut -f 5 -d : | cut -f 1 -d ' ' | sed 's/"//g')"
 fi
-pfs ls test ${commit_id} /
+pfs list-file test ${commit_id} /
 docker volume create --driver=pfs --opt repository=test --opt commit_id=${commit_id} --opt shard=0 --opt modulus=1 --name foo
 docker run --name foo --volume foo:/in --volume-driver pfs ubuntu cat /in/foo.txt
 docker rm foo
