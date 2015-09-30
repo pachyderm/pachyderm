963df69333d06b8d1da678d4c8506af5df59f994
Peter Edge <peter.edge@gmail.com>
2015-09-29 17:23:49 -0700

    update pfs-volume-driver for dockervolume

--- src/cmd/pfs-volume-driver/main.go
+++ src/cmd/pfs-volume-driver/main.go
@@ -77,20 +77,9 @@ func do(appEnvObj interface{}) error {
 	).Serve()
 }
 
-type volume struct {
-	repository string
-	commitID   string
-	shard      uint64
-	modulus    uint64
-	mountpoint string
-}
-
 type volumeDriver struct {
 	mounterProvider func() (fuse.Mounter, error)
 	baseMountpoint  string
-
-	nameToVolume map[string]*volume
-	lock         *sync.RWMutex
 
 	mounterOnce  *sync.Once
 	mounterValue *atomic.Value
@@ -104,114 +93,66 @@ func newVolumeDriver(
 	return &volumeDriver{
 		mounterProvider,
 		baseMountpoint,
-		make(map[string]*volume),
-		&sync.RWMutex{},
 		&sync.Once{},
 		&atomic.Value{},
 		&atomic.Value{},
 	}
 }
 
-func (v *volumeDriver) Create(name string, opts map[string]string) error {
+func (v *volumeDriver) Create(_ string, _ map[string]string) error {
+	return nil
+}
+
+func (v *volumeDriver) Remove(_ string, _ map[string]string, _ string) error {
+	return nil
+}
+
+func (v *volumeDriver) Mount(name string, opts map[string]string) (string, error) {
 	repository, err := getRequiredString(opts, "repository")
 	if err != nil {
-		return err
+		return "", err
 	}
 	commitID, err := getRequiredString(opts, "commit_id")
 	if err != nil {
-		return err
+		return "", err
 	}
 	shard, err := getOptionalUint64(opts, "shard", defaultShard)
 	if err != nil {
-		return err
+		return "", err
 	}
 	modulus, err := getOptionalUint64(opts, "modulus", defaultModulus)
 	if err != nil {
-		return err
+		return "", err
 	}
-	dir := filepath.Join(v.baseMountpoint, strings.Replace(uuid.NewV4().String(), "-", "", -1))
-	if err := os.MkdirAll(dir, 0777); err != nil {
-		return err
+	mountpoint := filepath.Join(v.baseMountpoint, fmt.Sprintf("name-%s", strings.Replace(uuid.NewV4().String(), "-", "", -1)))
+	if err := os.MkdirAll(mountpoint, 0777); err != nil {
+		return "", err
 	}
-	volume := &volume{
-		repository,
-		commitID,
-		shard,
-		modulus,
-		dir,
-	}
-	v.lock.Lock()
-	if _, ok := v.nameToVolume[name]; ok {
-		v.lock.Unlock()
-		return fmt.Errorf("pfs-volume-driver: volume already exists: %s", name)
-	}
-	v.nameToVolume[name] = volume
-	v.lock.Unlock()
-	return nil
-}
-
-func (v *volumeDriver) Remove(name string) error {
-	v.lock.Lock()
-	if _, ok := v.nameToVolume[name]; !ok {
-		v.lock.Unlock()
-		return fmt.Errorf("pfs-volume-driver: volume does not exist: %s", name)
-	}
-	delete(v.nameToVolume, name)
-	v.lock.Unlock()
-	return nil
-}
-
-func (v *volumeDriver) Path(name string) (string, error) {
-	v.lock.RLock()
-	volume, ok := v.nameToVolume[name]
-	if !ok {
-		v.lock.RUnlock()
-		return "", fmt.Errorf("pfs-volume-driver: volume does not exist: %s", name)
-	}
-	v.lock.RUnlock()
-	return volume.mountpoint, nil
-}
-
-func (v *volumeDriver) Mount(name string) (string, error) {
-	v.lock.RLock()
-	volume, ok := v.nameToVolume[name]
-	if !ok {
-		v.lock.RUnlock()
-		return "", fmt.Errorf("pfs-volume-driver: volume does not exist: %s", name)
-	}
-	v.lock.RUnlock()
 	mounter, err := v.getMounter()
 	if err != nil {
 		return "", err
 	}
 	if err := mounter.Mount(
-		volume.repository,
-		volume.commitID,
-		volume.mountpoint,
-		volume.shard,
-		volume.modulus,
+		repository,
+		commitID,
+		mountpoint,
+		shard,
+		modulus,
 	); err != nil {
 		return "", err
 	}
-	return volume.mountpoint, nil
+	return mountpoint, nil
 }
 
-func (v *volumeDriver) Unmount(name string) error {
-	v.lock.RLock()
-	volume, ok := v.nameToVolume[name]
-	if !ok {
-		v.lock.RUnlock()
-		return fmt.Errorf("pfs-volume-driver: volume does not exist: %s", name)
-	}
-	v.lock.RUnlock()
+func (v *volumeDriver) Unmount(_ string, _ map[string]string, mountpoint string) error {
 	mounter, err := v.getMounter()
 	if err != nil {
 		return err
 	}
-	if err := mounter.Unmount(volume.mountpoint); err != nil {
+	if err := mounter.Unmount(mountpoint); err != nil {
 		return err
 	}
-	return mounter.Wait(volume.mountpoint)
+	return mounter.Wait(mountpoint)
 }
 
 func (v *volumeDriver) getMounter() (fuse.Mounter, error) {
