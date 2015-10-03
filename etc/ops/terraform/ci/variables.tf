6de74a4b1e6e161be13dd1cc38d27b01cefe5203
Simon Rozet <me@simonrozet.com>
2015-10-03 05:56:40 +0000

    automated provisioning for ci

---
+++ etc/ops/terraform/ci/variables.tf
@@ -0,0 +1,18 @@
+variable "gcloud_project" {}
+variable "gcloud_region" {}
+variable "gcloud_account_file" {}
+variable "gcloud_zone" {}
+variable "gcloud_user" {}
+variable "gcloud_image" {
+  description = "Base image of the GCE Virtual Machine"
+  default = "ubuntu-1504-vivid-v20150911"
+}
+variable "gcloud_machine_type" {
+  default = "n1-standard-4"
+}
+variable "gcloud_disk_size" {
+  description = "Size of the persistent SSD disk in gigabytes"
+  default = "50"
+}
+variable "ci_buildkite_agent_token" {}
+variable "ci_count" {}
