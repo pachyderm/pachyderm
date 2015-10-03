e066bbfd5105ba14ffffa2d8303cc9f561daefc5
Peter Edge <peter.edge@gmail.com>
2015-10-02 18:55:13 -0700

    get build working

--- src/pps/run/runner.go
+++ src/pps/run/runner.go
@@ -2,6 +2,7 @@ package run
 
 import (
 	"fmt"
+	"io/ioutil"
 
 	"go.pedge.io/protolog"
 
@@ -98,28 +99,28 @@ func (r *runner) getNodeFunc(
 	if !ok {
 		return nil, fmt.Errorf("no service for name %s", node.Service)
 	}
-	// TODO(pedge): implement
-	if dockerService.Build != "" || dockerService.Dockerfile != "" {
-		return nil, fmt.Errorf("build/dockerfile not supported yet, had build: %s, dockerfile: %s", dockerService.Build, dockerService.Dockerfile)
-	}
 	return func() (retErr error) {
-		if err := r.containerClient.Pull(
+		image := dockerService.Image
+		if dockerService.Build != "" {
+			image = node.Service
+			if err := r.containerClient.Build(
+				node.Service,
+				dockerService.Build,
+				container.BuildOptions{
+					Dockerfile:   dockerService.Dockerfile,
+					OutputStream: ioutil.Discard,
+				},
+			); err != nil {
+				return err
+			}
+		} else if err := r.containerClient.Pull(
 			dockerService.Image,
-			container.PullOptions{
-			//OutputStream: newPipelineRunLogWriter(
-			//pipelineRunID,
-			//"",
-			//name,
-			//pps.OutputStream_OUTPUT_STREAM_NONE,
-			//r.timer,
-			//r.storeClient,
-			//),
-			},
+			container.PullOptions{},
 		); err != nil {
 			return err
 		}
 		containers, err := r.containerClient.Create(
-			dockerService.Image,
+			image,
 			container.CreateOptions{
 				Binds:         append(getInputBinds(node.Input), getOutputBinds(node.Output)...),
 				HasCommand:    len(node.Run) > 0,
