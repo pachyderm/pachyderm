0fe223fbf6a866623522df0697499624e6b42d04
Simon Rozet <me@simonrozet.com>
2015-10-06 19:56:01 +0000

    remove duplicate entry from PATH

--- etc/ci/run.bash
+++ etc/ci/run.bash
@@ -3,6 +3,6 @@
 set -eo pipefail
 
 export GOPATH="$(pwd)/go"
-export PATH="${GOPATH}/bin:/usr/local/go/bin:/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+export PATH="${GOPATH}/bin:/usr/local/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
 make test test-pps-extra docker-build
