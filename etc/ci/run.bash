cb4c331d7b1704d5390a95c1ad9fd890d5cb2807
Simon Rozet <me@simonrozet.com>
2015-10-06 21:02:21 +0000

    run clean deps before pretest

--- etc/ci/run.bash
+++ etc/ci/run.bash
@@ -5,4 +5,4 @@ set -eo pipefail
 export GOPATH="$(pwd)/go"
 export PATH="${GOPATH}/bin:/usr/local/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
-make test test-pps-extra docker-build
+make clean deps test test-pps-extra docker-build
