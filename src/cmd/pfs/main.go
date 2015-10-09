2efadf813ce34cc19e280063547f75dc666d3c0c
Peter Edge <peter.edge@gmail.com>
2015-10-08 17:02:08 -0700

    remove cobramainutil for pkgcobra

--- src/cmd/pfs/main.go
+++ src/cmd/pfs/main.go
@@ -8,6 +8,7 @@ import (
 	"text/tabwriter"
 
 	"go.pedge.io/env"
+	"go.pedge.io/pkg/cobra"
 	"go.pedge.io/proto/client"
 	"go.pedge.io/protolog/logrus"
 
@@ -17,7 +18,6 @@ import (
 	"go.pachyderm.com/pachyderm/src/pfs/fuse"
 	"go.pachyderm.com/pachyderm/src/pfs/pfsutil"
 	"go.pachyderm.com/pachyderm/src/pfs/pretty"
-	"go.pachyderm.com/pachyderm/src/pkg/cobramainutil"
 	"google.golang.org/grpc"
 )
 
@@ -54,22 +54,20 @@ func do(appEnvObj interface{}) error {
 	var shard int
 	var modulus int
 
-	createRepo := cobramainutil.Command{
-		Use:     "create-repo repo-name",
-		Short:   "Create a new repo.",
-		Long:    "Create a new repo.",
-		NumArgs: 1,
-		Run: func(cmd *cobra.Command, args []string) error {
+	createRepo := &cobra.Command{
+		Use:   "create-repo repo-name",
+		Short: "Create a new repo.",
+		Long:  "Create a new repo.",
+		Run: pkgcobra.RunFixedArgs(1, func(args []string) error {
 			return pfsutil.CreateRepo(apiClient, args[0])
-		},
-	}.ToCobraCommand()
+		}),
+	}
 
-	inspectRepo := cobramainutil.Command{
-		Use:     "inspect-repo repo-name",
-		Short:   "Return info about a repo.",
-		Long:    "Return info about a repo.",
-		NumArgs: 1,
-		Run: func(cmd *cobra.Command, args []string) error {
+	inspectRepo := &cobra.Command{
+		Use:   "inspect-repo repo-name",
+		Short: "Return info about a repo.",
+		Long:  "Return info about a repo.",
+		Run: pkgcobra.RunFixedArgs(1, func(args []string) error {
 			repoInfo, err := pfsutil.InspectRepo(apiClient, args[0])
 			if err != nil {
 				return err
@@ -81,15 +79,14 @@ func do(appEnvObj interface{}) error {
 			pretty.PrintRepoHeader(writer)
 			pretty.PrintRepoInfo(writer, repoInfo)
 			return writer.Flush()
-		},
-	}.ToCobraCommand()
+		}),
+	}
 
-	listRepo := cobramainutil.Command{
-		Use:     "list-repo",
-		Short:   "Return all repos.",
-		Long:    "Reutrn all repos.",
-		NumArgs: 0,
-		Run: func(cmd *cobra.Command, args []string) error {
+	listRepo := &cobra.Command{
+		Use:   "list-repo",
+		Short: "Return all repos.",
+		Long:  "Reutrn all repos.",
+		Run: pkgcobra.RunFixedArgs(0, func(args []string) error {
 			repoInfos, err := pfsutil.ListRepo(apiClient)
 			if err != nil {
 				return err
@@ -100,50 +97,46 @@ func do(appEnvObj interface{}) error {
 				pretty.PrintRepoInfo(writer, repoInfo)
 			}
 			return writer.Flush()
-		},
-	}.ToCobraCommand()
+		}),
+	}
 
-	deleteRepo := cobramainutil.Command{
-		Use:     "delete-repo repo-name",
-		Short:   "Delete a repo.",
-		Long:    "Delete a repo.",
-		NumArgs: 1,
-		Run: func(cmd *cobra.Command, args []string) error {
+	deleteRepo := &cobra.Command{
+		Use:   "delete-repo repo-name",
+		Short: "Delete a repo.",
+		Long:  "Delete a repo.",
+		Run: pkgcobra.RunFixedArgs(1, func(args []string) error {
 			return pfsutil.DeleteRepo(apiClient, args[0])
-		},
-	}.ToCobraCommand()
+		}),
+	}
 
-	startCommit := cobramainutil.Command{
-		Use:     "start-commit repo-name parent-commit-id",
-		Short:   "Start a new commit.",
-		Long:    "Start a new commit with parent-commit-id as the parent.",
-		NumArgs: 2,
-		Run: func(cmd *cobra.Command, args []string) error {
+	startCommit := &cobra.Command{
+		Use:   "start-commit repo-name parent-commit-id",
+		Short: "Start a new commit.",
+		Long:  "Start a new commit with parent-commit-id as the parent.",
+		Run: pkgcobra.RunFixedArgs(2, func(args []string) error {
 			commit, err := pfsutil.StartCommit(apiClient, args[0], args[1])
 			if err != nil {
 				return err
 			}
 			fmt.Println(commit.Id)
 			return nil
-		},
-	}.ToCobraCommand()
+		}),
+	}
 
-	finishCommit := cobramainutil.Command{
-		Use:     "finish-commit repo-name commit-id",
-		Short:   "Finish a started commit.",
-		Long:    "Finish a started commit. Commit-id must be a writeable commit.",
-		NumArgs: 2,
-		Run: func(cmd *cobra.Command, args []string) error {
+	finishCommit := &cobra.Command{
+		Use:   "finish-commit repo-name commit-id",
+		Short: "Finish a started commit.",
+		Long:  "Finish a started commit. Commit-id must be a writeable commit.",
+		Run: pkgcobra.RunFixedArgs(2, func(args []string) error {
 			return pfsutil.FinishCommit(apiClient, args[0], args[1])
-		},
-	}.ToCobraCommand()
+		}),
+	}
 
-	inspectCommit := cobramainutil.Command{
-		Use:     "inspect-commit repo-name commit-id",
-		Short:   "Return info about a commit.",
-		Long:    "Return info about a commit.",
-		NumArgs: 2,
-		Run: func(cmd *cobra.Command, args []string) error {
+	inspectCommit := &cobra.Command{
+		Use:   "inspect-commit repo-name commit-id",
+		Short: "Return info about a commit.",
+		Long:  "Return info about a commit.",
+		Run: pkgcobra.RunFixedArgs(2, func(args []string) error {
 			commitInfo, err := pfsutil.InspectCommit(apiClient, args[0], args[1])
 			if err != nil {
 				return err
@@ -155,15 +148,14 @@ func do(appEnvObj interface{}) error {
 			pretty.PrintCommitInfoHeader(writer)
 			pretty.PrintCommitInfo(writer, commitInfo)
 			return writer.Flush()
-		},
-	}.ToCobraCommand()
+		}),
+	}
 
-	listCommit := cobramainutil.Command{
-		Use:     "list-commit repo-name",
-		Short:   "Return all commits on a repo.",
-		Long:    "Return all commits on a repo.",
-		NumArgs: 1,
-		Run: func(cmd *cobra.Command, args []string) error {
+	listCommit := &cobra.Command{
+		Use:   "list-commit repo-name",
+		Short: "Return all commits on a repo.",
+		Long:  "Return all commits on a repo.",
+		Run: pkgcobra.RunFixedArgs(1, func(args []string) error {
 			commitInfos, err := pfsutil.ListCommit(apiClient, args[0])
 			if err != nil {
 				return err
@@ -174,50 +166,46 @@ func do(appEnvObj interface{}) error {
 				pretty.PrintCommitInfo(writer, commitInfo)
 			}
 			return writer.Flush()
-		},
-	}.ToCobraCommand()
+		}),
+	}
 
-	deleteCommit := cobramainutil.Command{
-		Use:     "delete-commit repo-name commit-id",
-		Short:   "Delete a commit.",
-		Long:    "Delete a commit.",
-		NumArgs: 2,
-		Run: func(cmd *cobra.Command, args []string) error {
+	deleteCommit := &cobra.Command{
+		Use:   "delete-commit repo-name commit-id",
+		Short: "Delete a commit.",
+		Long:  "Delete a commit.",
+		Run: pkgcobra.RunFixedArgs(2, func(args []string) error {
 			return pfsutil.DeleteCommit(apiClient, args[0], args[1])
-		},
-	}.ToCobraCommand()
+		}),
+	}
 
-	putBlock := cobramainutil.Command{
-		Use:     "put-block repo-name commit-id path/to/file",
-		Short:   "Put a block from stdin",
-		Long:    "Put a block from stdin. Directories must exist. commit-id must be a writeable commit.",
-		NumArgs: 3,
-		Run: func(cmd *cobra.Command, args []string) error {
+	putBlock := &cobra.Command{
+		Use:   "put-block repo-name commit-id path/to/file",
+		Short: "Put a block from stdin",
+		Long:  "Put a block from stdin. Directories must exist. commit-id must be a writeable commit.",
+		Run: pkgcobra.RunFixedArgs(3, func(args []string) error {
 			block, err := pfsutil.PutBlock(apiClient, args[0], args[1], args[2], os.Stdin)
 			if err != nil {
 				return err
 			}
 			fmt.Println(block.Hash)
 			return nil
-		},
-	}.ToCobraCommand()
+		}),
+	}
 
-	getBlock := cobramainutil.Command{
-		Use:     "get-block hash",
-		Short:   "Return the contents of a block.",
-		Long:    "Return the contents of a block.",
-		NumArgs: 1,
-		Run: func(cmd *cobra.Command, args []string) error {
+	getBlock := &cobra.Command{
+		Use:   "get-block hash",
+		Short: "Return the contents of a block.",
+		Long:  "Return the contents of a block.",
+		Run: pkgcobra.RunFixedArgs(1, func(args []string) error {
 			return pfsutil.GetBlock(apiClient, args[0], os.Stdout)
-		},
-	}.ToCobraCommand()
+		}),
+	}
 
-	inspectBlock := cobramainutil.Command{
-		Use:     "inspect-block hash",
-		Short:   "Return info about a block.",
-		Long:    "Return info about a block.",
-		NumArgs: 1,
-		Run: func(cmd *cobra.Command, args []string) error {
+	inspectBlock := &cobra.Command{
+		Use:   "inspect-block hash",
+		Short: "Return info about a block.",
+		Long:  "Return info about a block.",
+		Run: pkgcobra.RunFixedArgs(1, func(args []string) error {
 			blockInfo, err := pfsutil.InspectBlock(apiClient, args[0])
 			if err != nil {
 				return err
@@ -229,15 +217,14 @@ func do(appEnvObj interface{}) error {
 			pretty.PrintBlockInfoHeader(writer)
 			pretty.PrintBlockInfo(writer, blockInfo)
 			return writer.Flush()
-		},
-	}.ToCobraCommand()
+		}),
+	}
 
-	listBlock := cobramainutil.Command{
-		Use:     "list-block",
-		Short:   "Return the blocks in a directory.",
-		Long:    "Return the blocks in a directory.",
-		NumArgs: 0,
-		Run: func(cmd *cobra.Command, args []string) error {
+	listBlock := &cobra.Command{
+		Use:   "list-block",
+		Short: "Return the blocks in a directory.",
+		Long:  "Return the blocks in a directory.",
+		Run: pkgcobra.RunFixedArgs(0, func(args []string) error {
 			blockInfos, err := pfsutil.ListBlock(apiClient, uint64(shard), uint64(modulus))
 			if err != nil {
 				return err
@@ -248,48 +235,44 @@ func do(appEnvObj interface{}) error {
 				pretty.PrintBlockInfo(writer, blockInfo)
 			}
 			return writer.Flush()
-		},
-	}.ToCobraCommand()
+		}),
+	}
 	listBlock.Flags().IntVarP(&shard, "shard", "s", 0, "shard to read from")
 	listBlock.Flags().IntVarP(&modulus, "modulus", "m", 1, "modulus of the shards")
 
-	mkdir := cobramainutil.Command{
-		Use:     "mkdir repo-name commit-id path/to/dir",
-		Short:   "Make a directory.",
-		Long:    "Make a directory. Parent directories need not exist.",
-		NumArgs: 3,
-		Run: func(cmd *cobra.Command, args []string) error {
+	mkdir := &cobra.Command{
+		Use:   "mkdir repo-name commit-id path/to/dir",
+		Short: "Make a directory.",
+		Long:  "Make a directory. Parent directories need not exist.",
+		Run: pkgcobra.RunFixedArgs(3, func(args []string) error {
 			return pfsutil.MakeDirectory(apiClient, args[0], args[1], args[2])
-		},
-	}.ToCobraCommand()
+		}),
+	}
 
-	putFile := cobramainutil.Command{
-		Use:     "put-file repo-name commit-id path/to/file",
-		Short:   "Put a file from stdin",
-		Long:    "Put a file from stdin. Directories must exist. commit-id must be a writeable commit.",
-		NumArgs: 3,
-		Run: func(cmd *cobra.Command, args []string) error {
+	putFile := &cobra.Command{
+		Use:   "put-file repo-name commit-id path/to/file",
+		Short: "Put a file from stdin",
+		Long:  "Put a file from stdin. Directories must exist. commit-id must be a writeable commit.",
+		Run: pkgcobra.RunFixedArgs(3, func(args []string) error {
 			_, err := pfsutil.PutFile(apiClient, args[0], args[1], args[2], 0, os.Stdin)
 			return err
-		},
-	}.ToCobraCommand()
+		}),
+	}
 
-	getFile := cobramainutil.Command{
-		Use:     "get-file repo-name commit-id path/to/file",
-		Short:   "Return the contents of a file.",
-		Long:    "Return the contents of a file.",
-		NumArgs: 3,
-		Run: func(cmd *cobra.Command, args []string) error {
+	getFile := &cobra.Command{
+		Use:   "get-file repo-name commit-id path/to/file",
+		Short: "Return the contents of a file.",
+		Long:  "Return the contents of a file.",
+		Run: pkgcobra.RunFixedArgs(3, func(args []string) error {
 			return pfsutil.GetFile(apiClient, args[0], args[1], args[2], 0, math.MaxInt64, os.Stdout)
-		},
-	}.ToCobraCommand()
+		}),
+	}
 
-	inspectFile := cobramainutil.Command{
-		Use:     "inspect-file repo-name commit-id path/to/file",
-		Short:   "Return info about a file.",
-		Long:    "Return info about a file.",
-		NumArgs: 3,
-		Run: func(cmd *cobra.Command, args []string) error {
+	inspectFile := &cobra.Command{
+		Use:   "inspect-file repo-name commit-id path/to/file",
+		Short: "Return info about a file.",
+		Long:  "Return info about a file.",
+		Run: pkgcobra.RunFixedArgs(3, func(args []string) error {
 			fileInfo, err := pfsutil.InspectFile(apiClient, args[0], args[1], args[2])
 			if err != nil {
 				return err
@@ -301,15 +284,14 @@ func do(appEnvObj interface{}) error {
 			pretty.PrintFileInfoHeader(writer)
 			pretty.PrintFileInfo(writer, fileInfo)
 			return writer.Flush()
-		},
-	}.ToCobraCommand()
+		}),
+	}
 
-	listFile := cobramainutil.Command{
-		Use:     "list-file repo-name commit-id path/to/dir",
-		Short:   "Return the files in a directory.",
-		Long:    "Return the files in a directory.",
-		NumArgs: 3,
-		Run: func(cmd *cobra.Command, args []string) error {
+	listFile := &cobra.Command{
+		Use:   "list-file repo-name commit-id path/to/dir",
+		Short: "Return the files in a directory.",
+		Long:  "Return the files in a directory.",
+		Run: pkgcobra.RunFixedArgs(3, func(args []string) error {
 			fileInfos, err := pfsutil.ListFile(apiClient, args[0], args[1], args[2], uint64(shard), uint64(modulus))
 			if err != nil {
 				return err
@@ -320,27 +302,25 @@ func do(appEnvObj interface{}) error {
 				pretty.PrintFileInfo(writer, fileInfo)
 			}
 			return writer.Flush()
-		},
-	}.ToCobraCommand()
+		}),
+	}
 	listFile.Flags().IntVarP(&shard, "shard", "s", 0, "shard to read from")
 	listFile.Flags().IntVarP(&modulus, "modulus", "m", 1, "modulus of the shards")
 
-	deleteFile := cobramainutil.Command{
-		Use:     "delete-file repo-name commit-id path/to/file",
-		Short:   "Delete a file.",
-		Long:    "Delete a file.",
-		NumArgs: 2,
-		Run: func(cmd *cobra.Command, args []string) error {
+	deleteFile := &cobra.Command{
+		Use:   "delete-file repo-name commit-id path/to/file",
+		Short: "Delete a file.",
+		Long:  "Delete a file.",
+		Run: pkgcobra.RunFixedArgs(2, func(args []string) error {
 			return pfsutil.DeleteFile(apiClient, args[0], args[1], args[2])
-		},
-	}.ToCobraCommand()
+		}),
+	}
 
-	listChange := cobramainutil.Command{
-		Use:     "list-change repo-name commit-id path/to/dir",
-		Short:   "Return the changes in a directory.",
-		Long:    "Return the changes in a directory.",
-		NumArgs: 3,
-		Run: func(cmd *cobra.Command, args []string) error {
+	listChange := &cobra.Command{
+		Use:   "list-change repo-name commit-id path/to/dir",
+		Short: "Return the changes in a directory.",
+		Long:  "Return the changes in a directory.",
+		Run: pkgcobra.RunFixedArgs(3, func(args []string) error {
 			changeInfos, err := pfsutil.ListChange(apiClient, args[0], args[1], args[2], uint64(shard), uint64(modulus))
 			if err != nil {
 				return err
@@ -351,17 +331,16 @@ func do(appEnvObj interface{}) error {
 				pretty.PrintChange(writer, changeInfo)
 			}
 			return writer.Flush()
-		},
-	}.ToCobraCommand()
+		}),
+	}
 	listChange.Flags().IntVarP(&shard, "shard", "s", 0, "shard to read from")
 	listChange.Flags().IntVarP(&modulus, "modulus", "m", 1, "modulus of the shards")
 
-	inspectServer := cobramainutil.Command{
-		Use:     "inspect-server server-id",
-		Short:   "Inspect a server.",
-		Long:    "Inspect a server.",
-		NumArgs: 1,
-		Run: func(cmd *cobra.Command, args []string) error {
+	inspectServer := &cobra.Command{
+		Use:   "inspect-server server-id",
+		Short: "Inspect a server.",
+		Long:  "Inspect a server.",
+		Run: pkgcobra.RunFixedArgs(1, func(args []string) error {
 			serverInfo, err := pfsutil.InspectServer(apiClient, args[0])
 			if err != nil {
 				return err
@@ -370,15 +349,14 @@ func do(appEnvObj interface{}) error {
 			pretty.PrintServerInfoHeader(writer)
 			pretty.PrintServerInfo(writer, serverInfo)
 			return writer.Flush()
-		},
-	}.ToCobraCommand()
+		}),
+	}
 
-	listServer := cobramainutil.Command{
-		Use:     "list-server",
-		Short:   "Return all servers in the cluster.",
-		Long:    "Return all servers in the cluster.",
-		NumArgs: 0,
-		Run: func(cmd *cobra.Command, args []string) error {
+	listServer := &cobra.Command{
+		Use:   "list-server",
+		Short: "Return all servers in the cluster.",
+		Long:  "Return all servers in the cluster.",
+		Run: pkgcobra.RunFixedArgs(0, func(args []string) error {
 			serverInfos, err := pfsutil.ListServer(apiClient)
 			if err != nil {
 				return err
@@ -389,16 +367,14 @@ func do(appEnvObj interface{}) error {
 				pretty.PrintServerInfo(writer, serverInfo)
 			}
 			return writer.Flush()
-		},
-	}.ToCobraCommand()
+		}),
+	}
 
-	mount := cobramainutil.Command{
-		Use:        "mount mountpoint repo-name [commit-id]",
-		Short:      "Mount a repo as a local file system.",
-		Long:       "Mount a repo as a local file system.",
-		MinNumArgs: 2,
-		MaxNumArgs: 3,
-		Run: func(cmd *cobra.Command, args []string) error {
+	mount := &cobra.Command{
+		Use:   "mount mountpoint repo-name [commit-id]",
+		Short: "Mount a repo as a local file system.",
+		Long:  "Mount a repo as a local file system.",
+		Run: pkgcobra.RunBoundedArgs(pkgcobra.Bounds{Min: 2, Max: 3}, func(args []string) error {
 			mountPoint := args[0]
 			repo := args[1]
 			commitID := ""
@@ -410,8 +386,8 @@ func do(appEnvObj interface{}) error {
 				return err
 			}
 			return mounter.Wait(mountPoint)
-		},
-	}.ToCobraCommand()
+		}),
+	}
 	mount.Flags().IntVarP(&shard, "shard", "s", 0, "shard to read from")
 	mount.Flags().IntVarP(&modulus, "modulus", "m", 1, "modulus of the shards")
 
