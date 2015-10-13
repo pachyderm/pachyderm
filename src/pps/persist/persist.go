90d3c51c2a10f60d2e1213683e08fd59c63231b1
Peter Edge <peter.edge@gmail.com>
2015-10-13 16:51:54 -0700

    continue work on rethink api server implementation

--- src/pps/persist/persist.go
+++ src/pps/persist/persist.go
@@ -3,8 +3,9 @@ package persist // import "go.pachyderm.com/pachyderm/src/pps/persist"
 import "errors"
 
 var (
-	ErrIDSet    = errors.New("pachyderm.pps.persist: ID set")
-	ErrIDNotSet = errors.New("pachyderm.pps.persist: ID not set")
+	ErrIDSet        = errors.New("pachyderm.pps.persist: ID set")
+	ErrIDNotSet     = errors.New("pachyderm.pps.persist: ID not set")
+	ErrTimestampSet = errors.New("pachyderm.pps.persist: Timestamp set")
 )
 
 func NewRethinkAPIServer(address string, databaseName string) (APIServer, error) {
