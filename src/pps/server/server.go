9b83cac344edca0d79cbce8ea6f3c383232bf2dc
Peter Edge <peter.edge@gmail.com>
2015-10-13 20:04:54 -0700

    CreateJob and GetJob on pps api server

--- src/pps/server/server.go
+++ src/pps/server/server.go
@@ -1,9 +1,12 @@
 package server // import "go.pachyderm.com/pachyderm/src/pps/server"
 
-import "go.pachyderm.com/pachyderm/src/pps"
+import (
+	"go.pachyderm.com/pachyderm/src/pps"
+	"go.pachyderm.com/pachyderm/src/pps/persist"
+)
 
-func NewAPIServer() pps.APIServer {
-	return newLogAPIServer(newAPIServer())
+func NewAPIServer(persistAPIClient persist.APIClient) pps.APIServer {
+	return newLogAPIServer(newAPIServer(persistAPIClient))
 }
 
 func NewLocalAPIClient(apiServer pps.APIServer) pps.APIClient {
