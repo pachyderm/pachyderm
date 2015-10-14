71c788db804023d0c244bbdcb9d31d926193bd39
Peter Edge <peter.edge@gmail.com>
2015-10-13 19:43:09 -0700

    start of pps local api client

--- src/pps/server/server.go
+++ src/pps/server/server.go
@@ -5,3 +5,7 @@ import "go.pachyderm.com/pachyderm/src/pps"
 func NewAPIServer() pps.APIServer {
 	return newLogAPIServer(newAPIServer())
 }
+
+func NewLocalAPIClient(apiServer pps.APIServer) pps.APIClient {
+	return newLocalAPIClient(apiServer)
+}
