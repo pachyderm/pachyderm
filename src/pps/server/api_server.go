c88ec9b9005be3d61431a2e98133bc1057b0da4a
Peter Edge <peter.edge@gmail.com>
2015-10-13 20:04:54 -0700

    CreateJob and GetJob on pps api server

--- src/pps/server/api_server.go
+++ src/pps/server/api_server.go
@@ -2,22 +2,33 @@ package server
 
 import (
 	"go.pachyderm.com/pachyderm/src/pps"
+	"go.pachyderm.com/pachyderm/src/pps/persist"
 	"go.pedge.io/google-protobuf"
 	"golang.org/x/net/context"
 )
 
-type apiServer struct{}
+type apiServer struct {
+	persistAPIClient persist.APIClient
+}
 
-func newAPIServer() *apiServer {
-	return &apiServer{}
+func newAPIServer(persistAPIClient persist.APIClient) *apiServer {
+	return &apiServer{persistAPIClient}
 }
 
 func (a *apiServer) CreateJob(ctx context.Context, request *pps.CreateJobRequest) (response *pps.Job, err error) {
-	return nil, nil
+	persistJob, err := a.persistAPIClient.CreateJob(ctx, jobToPersist(request.Job))
+	if err != nil {
+		return nil, err
+	}
+	return persistToJob(persistJob), nil
 }
 
 func (a *apiServer) GetJob(ctx context.Context, request *pps.GetJobRequest) (response *pps.Job, err error) {
-	return nil, nil
+	persistJob, err := a.persistAPIClient.GetJobByID(ctx, &google_protobuf.StringValue{Value: request.JobId})
+	if err != nil {
+		return nil, err
+	}
+	return persistToJob(persistJob), nil
 }
 
 func (a *apiServer) GetJobsByPipelineName(ctx context.Context, request *pps.GetJobsByPipelineNameRequest) (response *pps.Jobs, err error) {
@@ -46,4 +57,40 @@ func (a *apiServer) GetPipeline(ctx context.Context, request *pps.GetPipelineReq
 
 func (a *apiServer) GetAllPipelines(ctx context.Context, request *google_protobuf.Empty) (response *pps.Pipelines, err error) {
 	return nil, nil
+}
+
+func jobToPersist(job *pps.Job) *persist.Job {
+	persistJob := &persist.Job{
+		Id:        job.Id,
+		JobInput:  job.JobInput,
+		JobOutput: job.JobOutput,
+	}
+	if job.GetTransform() != nil {
+		persistJob.Spec = &persist.Job_Transform{
+			Transform: job.GetTransform(),
+		}
+	} else if job.GetPipelineId() != "" {
+		persistJob.Spec = &persist.Job_PipelineId{
+			PipelineId: job.GetPipelineId(),
+		}
+	}
+	return persistJob
+}
+
+func persistToJob(persistJob *persist.Job) *pps.Job {
+	job := &pps.Job{
+		Id:        persistJob.Id,
+		JobInput:  persistJob.JobInput,
+		JobOutput: persistJob.JobOutput,
+	}
+	if persistJob.GetTransform() != nil {
+		job.Spec = &pps.Job_Transform{
+			Transform: persistJob.GetTransform(),
+		}
+	} else if persistJob.GetPipelineId() != "" {
+		job.Spec = &pps.Job_PipelineId{
+			PipelineId: persistJob.GetPipelineId(),
+		}
+	}
+	return job
 }
