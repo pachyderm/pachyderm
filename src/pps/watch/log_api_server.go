f8e814b0c15b5452bc41e693ec893e2908bfc630
Peter Edge <peter.edge@gmail.com>
2015-10-16 13:18:25 -0700

    watch base

---
+++ src/pps/watch/log_api_server.go
@@ -0,0 +1,23 @@
+package watch
+
+import (
+	"time"
+
+	"go.pedge.io/google-protobuf"
+	"go.pedge.io/proto/rpclog"
+	"golang.org/x/net/context"
+)
+
+type logAPIServer struct {
+	protorpclog.Logger
+	delegate APIServer
+}
+
+func newLogAPIServer(delegate APIServer) *logAPIServer {
+	return &logAPIServer{protorpclog.NewLogger("pachyderm.pps.watch.API"), delegate}
+}
+
+func (a *logAPIServer) RegisterChangeEvent(ctx context.Context, request *ChangeEvent) (response *google_protobuf.Empty, err error) {
+	defer func(start time.Time) { a.Log(request, response, err, time.Since(start)) }(time.Now())
+	return a.delegate.RegisterChangeEvent(ctx, request)
+}
