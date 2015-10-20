75d097e09f0b287b26012264deaaf3b81a108e19
Peter Edge <peter.edge@gmail.com>
2015-10-20 16:48:01 -0500

    get last commit id for pipeline in pps watch api server implementation

--- src/pps/watch/server/pipeline_controller.go
+++ src/pps/watch/server/pipeline_controller.go
@@ -1,8 +1,11 @@
 package server
 
 import (
+	"fmt"
+
 	"go.pachyderm.com/pachyderm/src/pfs"
 	"go.pachyderm.com/pachyderm/src/pps/persist"
+	"go.pedge.io/protolog"
 )
 
 type pipelineController struct {
@@ -29,7 +32,37 @@ func newPipelineController(
 }
 
 func (p *pipelineController) Start() error {
-	go p.run()
+	// TODO(pedge): do not get all jobs each time, need a limit call on persist, more
+	// generally, need all persist calls to have a limit
+	jobs, err := getJobsByPipelineName(p.persistAPIClient, p.pipeline.Name)
+	if err != nil {
+		return err
+	}
+	// TODO(pedge): use InitialCommitID
+	lastCommitID := "scratch"
+	if len(jobs) > 0 {
+		lastJob := jobs[0]
+		if len(lastJob.JobInput) == 0 {
+			return fmt.Errorf("pachyderm.pps.watch.server: had job with no JobInput, this is not currently allowed, %v", lastJob)
+		}
+		if len(lastJob.JobInput) > 0 {
+			return fmt.Errorf("pachyderm.pps.watch.server: had job with more than one JobInput, this is not currently allowed, %v", lastJob)
+		}
+		jobInput := lastJob.JobInput[0]
+		if jobInput.GetHostDir() != "" {
+			return fmt.Errorf("pachyderm.pps.watch.server: had job with host dir set, this is not allowed, %v", lastJob)
+		}
+		if jobInput.GetCommit() == nil {
+			return fmt.Errorf("pachyderm.pps.watch.server: had job without commit set, this is not allowed, %v", lastJob)
+		}
+		lastCommitID = jobInput.GetCommit().Id
+	}
+	go func() {
+		if err := p.run(lastCommitID); err != nil {
+			// TODO(pedge): what to do with error?
+			protolog.Errorln(err.Error())
+		}
+	}()
 	return nil
 }
 
@@ -39,16 +72,14 @@ func (p *pipelineController) Cancel() {
 	<-p.finishedCancelC
 }
 
-func (p *pipelineController) run() {
+func (p *pipelineController) run(lastCommitID string) error {
 	for {
 		select {
 		case <-p.cancelC:
 			p.finishedCancelC <- true
 			close(p.finishedCancelC)
-			return
+			return nil
 		default:
-			// TODO(pedge)
-			return
 		}
 	}
 }
