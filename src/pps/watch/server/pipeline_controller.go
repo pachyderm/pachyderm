7fc0d71729d265354e8a6db97d06ebaa5a5f049d
Peter Edge <peter.edge@gmail.com>
2015-10-20 17:10:43 -0500

    continue work on pps watch api server implementation

--- src/pps/watch/server/pipeline_controller.go
+++ src/pps/watch/server/pipeline_controller.go
@@ -3,6 +3,8 @@ package server
 import (
 	"fmt"
 
+	"golang.org/x/net/context"
+
 	"go.pachyderm.com/pachyderm/src/pfs"
 	"go.pachyderm.com/pachyderm/src/pps/persist"
 	"go.pedge.io/protolog"
@@ -70,14 +72,52 @@ func (p *pipelineController) Cancel() {
 
 func (p *pipelineController) run(lastCommit *pfs.Commit) error {
 	for {
+		// TODO(pedge): this is not what we want, the ListCommit
+		// context should take the cancel chan and the pfs api server implementation
+		// should handle it, for now we do this, but this also means the ListCommit
+		// call will not be cancelled and the goroutine will continue to run
+		// just overall not good, we need a discussion about handling cancel and
+		// look into if gRPC does this automatically
+		var commitInfos *pfs.CommitInfos
+		var err error
+		done := make(chan bool)
+		go func() {
+			commitInfos, err = p.pfsAPIClient.ListCommit(
+				context.Background(),
+				&pfs.ListCommitRequest{
+					Repo:  lastCommit.Repo,
+					From:  lastCommit,
+					Block: true,
+				},
+			)
+			done <- true
+			close(done)
+		}()
 		select {
 		case <-p.cancelC:
 			p.finishedCancelC <- true
 			close(p.finishedCancelC)
 			return nil
-		default:
+		case <-done:
+			if err != nil {
+				return err
+			}
+			if len(commitInfos.CommitInfo) == 0 {
+				return fmt.Errorf("pachyderm.pps.watch.server: we expected at least one *pfs.CommitInfo returned from blocking call, but no *pfs.CommitInfo structs were returned for %v", lastCommit)
+			}
+			// going in reverse order, oldest to newest
+			for _, commitInfo := range commitInfos.CommitInfo {
+				if err := p.createAndStartJobForCommitInfo(commitInfo); err != nil {
+					return err
+				}
+			}
 		}
 	}
+}
+
+// TODO(pedge): implement
+func (p *pipelineController) createAndStartJobForCommitInfo(commitInfo *pfs.CommitInfo) error {
+	return nil
 }
 
 func getRepoForPipeline(pipeline *persist.Pipeline) (*pfs.Repo, error) {
