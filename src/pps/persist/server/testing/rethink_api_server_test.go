47a715731b28ae5336f9789ab6d1d53440f5e691
Peter Edge <peter.edge@gmail.com>
2015-10-23 19:03:10 +0200

    re-add rethink_api_server_test.go which was deleted, add pps persist server testing package

---
+++ src/pps/persist/server/testing/rethink_api_server_test.go
@@ -0,0 +1,36 @@
+package testing
+
+import (
+	"testing"
+
+	"go.pachyderm.com/pachyderm/src/pkg/require"
+	"go.pachyderm.com/pachyderm/src/pps"
+	"go.pachyderm.com/pachyderm/src/pps/persist"
+	"golang.org/x/net/context"
+)
+
+func TestBasicRethink(t *testing.T) {
+	RunTestWithRethinkAPIServer(t, testBasicRethink)
+}
+
+func testBasicRethink(t *testing.T, apiServer persist.APIServer) {
+	jobInfo, err := apiServer.CreateJobInfo(
+		context.Background(),
+		&persist.JobInfo{
+			Spec: &persist.JobInfo_PipelineName{
+				PipelineName: "foo",
+			},
+		},
+	)
+	require.NoError(t, err)
+	getJobInfo, err := apiServer.GetJobInfo(
+		context.Background(),
+		&pps.Job{
+			Id: jobInfo.JobId,
+		},
+	)
+	require.NoError(t, err)
+	require.Equal(t, jobInfo.JobId, getJobInfo.JobId)
+	require.Equal(t, "foo", getJobInfo.GetPipelineName())
+	require.NotNil(t, getJobInfo.CreatedAt)
+}
