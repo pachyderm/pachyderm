efd9102baba226dbc91743175215e5aea6e04356
Joe Doliner <jdoliner@gmail.com>
2015-10-23 19:23:39 -0700

    Lots of changes to get things running.
    Still not running tho...

--- etc/kube/start-kube-docker.sh
+++ etc/kube/start-kube-docker.sh
@@ -15,5 +15,11 @@ docker run \
     --privileged=true \
     -d \
     gcr.io/google_containers/hyperkube:v1.0.6 \
-    /hyperkube kubelet --containerized --hostname-override="127.0.0.1" --address="0.0.0.0" --api-servers=http://localhost:8080 --config=/etc/kubernetes/manifests
-docker run -d --net=host --privileged gcr.io/google_containers/hyperkube:v1.0.1 /hyperkube proxy --master=http://127.0.0.1:8080 --v=2
+    /hyperkube kubelet \
+        --containerized \
+        --hostname-override="127.0.0.1" \
+        --address="0.0.0.0" \
+        --api-servers=http://localhost:8080 \
+        --config=/etc/kubernetes/manifests \
+        --allow-privileged=true
+docker run -d --net=host --privileged gcr.io/google_containers/hyperkube:v1.0.6 /hyperkube proxy --master=http://127.0.0.1:8080 --v=2
