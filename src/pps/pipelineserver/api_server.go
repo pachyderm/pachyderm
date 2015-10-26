e2e2f68762d67c56d463d1f07247a6fa80ea7c7c
Peter Edge <peter.edge@gmail.com>
2015-10-26 16:19:17 +0100

    use context.Context for cancel function in pipeline controller, add etc/golangcontext/golangcontext.go as context.Context cancel function example

--- src/pps/pipelineserver/api_server.go
+++ src/pps/pipelineserver/api_server.go
@@ -153,9 +153,8 @@ func (a *apiServer) addPipelineController(pipelineInfo *pps.PipelineInfo) error 
 func (a *apiServer) removePipelineController(name string) error {
 	pipelineController, ok := a.pipelineNameToPipelineController[name]
 	if !ok {
-		protolog.Warnf("pachyderm.pps.pipelineserver: no pipeline registered for name: %s", name)
-	} else {
-		pipelineController.Cancel()
+		return pipelineController.Cancel()
 	}
+	protolog.Warnf("pachyderm.pps.pipelineserver: no pipeline registered for name: %s", name)
 	return nil
 }
