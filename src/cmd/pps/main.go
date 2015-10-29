d4fe1fc579acce727edf3d6f1add96360ab53e1b
Joe Doliner <jdoliner@gmail.com>
2015-10-28 19:10:24 -0700

    Specs out create-job command.

--- src/cmd/pps/main.go
+++ src/cmd/pps/main.go
@@ -2,7 +2,13 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strings"
+
+	"golang.org/x/net/context"
+
+	"github.com/pachyderm/pachyderm/src/pfs"
+	"github.com/pachyderm/pachyderm/src/pps"
 
 	"go.pedge.io/env"
 
@@ -38,7 +44,7 @@ func do(appEnvObj interface{}) error {
 	if err != nil {
 		return err
 	}
-	fmt.Println(clientConn)
+	apiClient := pps.NewJobAPIClient(clientConn)
 	rootCmd := &cobra.Command{
 		Use: "pps",
 		Long: `Access the PPS API.
@@ -46,5 +52,38 @@ func do(appEnvObj interface{}) error {
 Note that this CLI is experimental and does not even check for common errors.
 The environment variable PPS_ADDRESS controls what server the CLI connects to, the default is 0.0.0.0:651.`,
 	}
+
+	createJob := &cobra.Command{
+		Use:   "create-job in-repo-name in-commit-id out-repo-name -i image -p out-parent-commit-id command [args]",
+		Short: "Create a new job. Returns the id of the created job.",
+		Long: `Create a new job. With repo-name/commit-id as input and
+out-repo-name as output. A commit will be created for the output.
+You can find out the name of the commit with inspect-job.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			job, err := apiClient.CreateJob(
+				context.Background(),
+				&pps.CreateJobRequest{
+					// Types that are valid to be assigned to Spec:
+					//	*CreateJobRequest_Transform
+					//	*CreateJobRequest_Pipeline
+					Spec: &pps.CreateJobRequest_Transform{
+						Transform: &pps.Transform{},
+					},
+					Input:        &pfs.Commit{},
+					OutputParent: &pfs.Commit{},
+				})
+			if err != nil {
+				errorAndExit("Error on CreateJob: %s", err.Error())
+			}
+			fmt.Println(job.Id)
+		},
+	}
+
+	rootCmd.AddCommand(createJob)
 	return rootCmd.Execute()
+}
+
+func errorAndExit(format string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, "%s\n", fmt.Sprintf(format, args...))
+	os.Exit(1)
 }
