b8bca93d928f789b17ac64f1a0ecad707c6514d7
Peter Edge <peter.edge@gmail.com>
2015-11-02 23:25:14 +0100

    remove global constant var map pfs.ReservedCommitIDs from src/pfs/pfs.go

--- src/pfs/pfs.go
+++ src/pfs/pfs.go
@@ -4,10 +4,3 @@ const (
 	// InitialCommitID is the initial id before any commits are made in a repository.
 	InitialCommitID = "scratch"
 )
-
-var (
-	// ReservedCommitIDs are the commit ids used by the system.
-	ReservedCommitIDs = map[string]bool{
-		InitialCommitID: true,
-	}
-)
