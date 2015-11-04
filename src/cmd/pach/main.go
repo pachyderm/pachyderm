4456f73fabf1cdcd542525c5d83f07ea1457acfe
Joe Doliner <jdoliner@gmail.com>
2015-11-03 16:32:30 -0800

    Add deploy cmds to pach.

--- src/cmd/pach/main.go
+++ src/cmd/pach/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"strings"
 
 	"go.pedge.io/env"
 
+	deploycmds "github.com/pachyderm/pachyderm/src/cmd/deploy/cmds"
 	pfscmds "github.com/pachyderm/pachyderm/src/cmd/pfs/cmds"
 	ppscmds "github.com/pachyderm/pachyderm/src/cmd/pps/cmds"
 	"github.com/spf13/cobra"
@@ -14,7 +13,14 @@ import (
 
 type appEnv struct {
 	PachydermPfsd1Port string `env:"PACHYDERM_PFSD_1_PORT"`
-	Address            string `env:"PFS_ADDRESS,default=0.0.0.0:650"`
+	PfsAddress         string `env:"PFS_ADDRESS,default=0.0.0.0:650"`
+	PachydermPpsd1Port string `env:"PACHYDERM_PPSD_1_PORT"`
+	PpsAddress         string `env:"PPS_ADDRESS,default=0.0.0.0:651"`
+	KubernetesAddress  string `env:"KUBERNETES_ADDRESS,default=http://localhost:8080"`
+	KubernetesUsername string `env:"KUBERNETES_USERNAME,default=admin"`
+	KubernetesPassword string `env:"KUBERNETES_PASSWORD"`
+	GCEProject         string `env:"GCE_PROJECT,required"`
+	GCEZone            string `env:"GCE_ZONE,required"`
 }
 
 func main() {
@@ -23,12 +29,6 @@ func main() {
 
 func do(appEnvObj interface{}) error {
 	appEnv := appEnvObj.(*appEnv)
-	address := appEnv.PachydermPfsd1Port
-	if address == "" {
-		address = appEnv.Address
-	} else {
-		address = strings.Replace(address, "tcp://", "", -1)
-	}
 	rootCmd := &cobra.Command{
 		Use: "pach",
 		Long: `Access the Pachyderm API.
@@ -36,28 +36,40 @@ func do(appEnvObj interface{}) error {
 The environment variable PFS_ADDRESS controls which PFS server the CLI connects to, the default is 0.0.0.0:650.
 The environment variable PPS_ADDRESS controls what server the CLI connects to, the default is 0.0.0.0:651.`,
 	}
-	pfscmds, err := pfscmds.Cmds(address)
+	pfsAddress := appEnv.PachydermPfsd1Port
+	if pfsAddress == "" {
+		pfsAddress = appEnv.PfsAddress
+	} else {
+		pfsAddress = strings.Replace(pfsAddress, "tcp://", "", -1)
+	}
+	pfsCmds, err := pfscmds.Cmds(pfsAddress)
 	if err != nil {
 		return err
 	}
-	for _, cmd := range pfscmds {
+	for _, cmd := range pfsCmds {
 		rootCmd.AddCommand(cmd)
 	}
-	ppscmds, err := ppscmds.Cmds(address)
+	ppsAddress := appEnv.PachydermPpsd1Port
+	if ppsAddress == "" {
+		ppsAddress = appEnv.PpsAddress
+	} else {
+		ppsAddress = strings.Replace(ppsAddress, "tcp://", "", -1)
+	}
+	ppsCmds, err := ppscmds.Cmds(ppsAddress)
 	if err != nil {
 		return err
 	}
-	for _, cmd := range ppscmds {
+	for _, cmd := range ppsCmds {
+		rootCmd.AddCommand(cmd)
+	}
+
+	deployCmds, err := deploycmds.Cmds(appEnv.KubernetesAddress, appEnv.KubernetesUsername, appEnv.KubernetesAddress, appEnv.GCEProject, appEnv.GCEZone)
+	if err != nil {
+		return err
+	}
+	for _, cmd := range deployCmds {
 		rootCmd.AddCommand(cmd)
 	}
 
 	return rootCmd.Execute()
-}
-
-func getPfsdAddress() string {
-	pfsdAddr := os.Getenv("PFSD_PORT_650_TCP_ADDR")
-	if pfsdAddr == "" {
-		return "0.0.0.0:650"
-	}
-	return fmt.Sprintf("%s:650", pfsdAddr)
 }
