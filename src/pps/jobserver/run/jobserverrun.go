0ef28b940c08a0de5dcf9eaf29bea049c7934293
Joe Doliner <jdoliner@gmail.com>
2015-11-05 01:10:04 -0800

    Remove unused code.

--- src/pps/jobserver/run/jobserverrun.go
+++ src/pps/jobserver/run/jobserverrun.go
@@ -1,52 +1,12 @@
 package jobserverrun
 
 import (
-	"github.com/pachyderm/pachyderm/src/pfs"
-	"github.com/pachyderm/pachyderm/src/pkg/container"
 	"github.com/pachyderm/pachyderm/src/pps/persist"
 	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/api/unversioned"
 	"k8s.io/kubernetes/pkg/apis/extensions"
 	kube "k8s.io/kubernetes/pkg/client/unversioned"
 )
-
-const (
-	InputMountDir  = "/pfs/in"
-	OutputMountDir = "/pfs/out"
-)
-
-type JobRunner interface {
-	Start(*persist.JobInfo) error
-}
-
-type TestJobRunner interface {
-	JobRunner
-	GetJobIDToPersistJobInfo() map[string]*persist.JobInfo
-}
-
-type JobRunnerOptions struct {
-	RemoveContainers bool
-}
-
-func NewJobRunner(
-	pfsAPIClient pfs.APIClient,
-	persistAPIClient persist.APIClient,
-	containerClient container.Client,
-	pfsMountDir string,
-	options JobRunnerOptions,
-) JobRunner {
-	return newJobRunner(
-		pfsAPIClient,
-		persistAPIClient,
-		containerClient,
-		pfsMountDir,
-		options,
-	)
-}
-
-func NewTestJobRunner() TestJobRunner {
-	return newTestJobRunner()
-}
 
 func StartJob(client *kube.Client, jobInfo *persist.JobInfo) error {
 	_, err := client.Jobs(api.NamespaceDefault).Create(job(jobInfo))
