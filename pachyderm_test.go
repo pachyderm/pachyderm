91ae07aa05a44dbc0ecff522a25f8d3bb0443d52
Joe Doliner <jdoliner@gmail.com>
2015-12-01 15:10:15 -0800

    Makes integration test actually do a little something.

--- pachyderm_test.go
+++ pachyderm_test.go
@@ -1,8 +1,45 @@
 package pachyderm
 
 import (
+	"fmt"
+	"os"
+	"strings"
 	"testing"
+
+	"github.com/pachyderm/pachyderm/src/pfs"
+	"github.com/pachyderm/pachyderm/src/pfs/pfsutil"
+	"github.com/pachyderm/pachyderm/src/pkg/require"
+	"github.com/pachyderm/pachyderm/src/pps"
+	"google.golang.org/grpc"
 )
 
-func TestGrep(t *testing.T) {
+func TestSimple(t *testing.T) {
+	pfsClient := getPfsClient(t)
+	require.NoError(t, pfsutil.CreateRepo(pfsClient, "data"))
+	require.NoError(t, pfsutil.CreateRepo(pfsClient, "output"))
+	commit, err := pfsutil.StartCommit(pfsClient, "data", "")
+	require.NoError(t, err)
+	_, err = pfsutil.PutFile(pfsClient, "data", commit.Id, "file", 0, strings.NewReader("foo"))
+	require.NoError(t, err)
+	require.NoError(t, pfsutil.FinishCommit(pfsClient, "data", commit.Id))
+}
+
+func getPfsClient(t *testing.T) pfs.APIClient {
+	pfsdAddr := os.Getenv("PFSD_PORT_650_TCP_ADDR")
+	if pfsdAddr == "" {
+		t.Error("PFSD_PORT_650_TCP_ADDR not set")
+	}
+	clientConn, err := grpc.Dial(fmt.Sprintf("%s:650", pfsdAddr), grpc.WithInsecure())
+	require.NoError(t, err)
+	return pfs.NewAPIClient(clientConn)
+}
+
+func getPpsClient(t *testing.T) pps.APIClient {
+	ppsdAddr := os.Getenv("PPSD_PORT_651_TCP_ADDR")
+	if ppsdAddr == "" {
+		t.Error("PPSD_PORT_651_TCP_ADDR not set")
+	}
+	clientConn, err := grpc.Dial(fmt.Sprintf("%s:651", ppsdAddr), grpc.WithInsecure())
+	require.NoError(t, err)
+	return pps.NewAPIClient(clientConn)
 }
