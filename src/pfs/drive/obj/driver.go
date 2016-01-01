ca66917bac60dde79846d4d556823db51e0717ae
Joe Doliner <jdoliner@gmail.com>
2016-01-01 13:19:22 -0800

    Adds sorting of commitInfos, now they need timestamps.

--- src/pfs/drive/obj/driver.go
+++ src/pfs/drive/obj/driver.go
@@ -478,6 +478,7 @@ func (d *driver) fileBlockRefsOrDir(file *pfs.File, shard uint64) (_ []*drive.Bl
 			return nil, true, nil
 		}
 		if lastRef, ok := diffInfo.LastRefs[file.Path]; ok {
+			log.Printf("Using lastRef.")
 			commit = lastRef
 			continue
 		}
@@ -486,6 +487,7 @@ func (d *driver) fileBlockRefsOrDir(file *pfs.File, shard uint64) (_ []*drive.Bl
 	if result == nil && !writeable {
 		return nil, false, fmt.Errorf("file %s/%s/%s not found", file.Commit.Repo.Name, file.Commit.Id, file.Path)
 	}
+	log.Printf("result: %+v", result)
 	return result, false, nil
 }
 
--- src/pfs/reduce.go
+++ src/pfs/reduce.go
@@ -1,5 +1,11 @@
 package pfs
 
+import (
+	"sort"
+
+	"go.pedge.io/proto/time"
+)
+
 func Reduce(commitInfos []*CommitInfo) []*CommitInfo {
 	reducedCommitInfos := make(map[string]*CommitInfo)
 	for _, commitInfo := range commitInfos {
@@ -18,5 +24,29 @@ func Reduce(commitInfos []*CommitInfo) []*CommitInfo {
 	for _, commitInfo := range reducedCommitInfos {
 		result = append(result, commitInfo)
 	}
+	sort.Sort(sortCommitInfos(result))
 	return result
+}
+
+type sortCommitInfos []*CommitInfo
+
+func (a sortCommitInfos) Len() int {
+	return len(a)
+}
+
+func (a sortCommitInfos) Less(i, j int) bool {
+	if a[i].Finished != nil && a[j].Finished != nil {
+		return prototime.TimestampToTime(a[i].Finished).After(prototime.TimestampToTime(a[j].Finished))
+	} else if a[i].Finished != nil {
+		return true
+	} else if a[j].Finished != nil {
+		return false
+	} else {
+		return prototime.TimestampToTime(a[i].Started).After(prototime.TimestampToTime(a[j].Started))
+	}
+}
+func (a sortCommitInfos) Swap(i, j int) {
+	tmp := a[i]
+	a[i] = a[j]
+	a[j] = tmp
 }
