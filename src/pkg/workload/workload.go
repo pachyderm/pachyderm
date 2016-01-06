cbaed9f005c1e586de799fa4348b8d54ac4ce98f
Joe Doliner <jdoliner@gmail.com>
2016-01-05 21:57:51 -0800

    Fixes panics. Adds newlines, they seem defective.

--- src/pkg/workload/workload.go
+++ src/pkg/workload/workload.go
@@ -67,7 +67,10 @@ func (w *worker) work(pfsClient pfs.APIClient, ppsClient pps.APIClient) error {
 		}
 		w.started = append(w.started, commit)
 	case opt < commit:
-		if len(w.started) >= maxStartedCommits {
+		if len(w.started) >= maxStartedCommits || len(w.finished) == 0 {
+			if len(w.started) == 0 {
+				return nil
+			}
 			i := w.rand.Intn(len(w.started))
 			commit := w.started[i]
 			if err := pfsutil.FinishCommit(pfsClient, commit.Repo.Name, commit.Id); err != nil {
@@ -76,6 +79,9 @@ func (w *worker) work(pfsClient pfs.APIClient, ppsClient pps.APIClient) error {
 			w.started = append(w.started[:i], w.started[i+1:]...)
 			w.finished = append(w.finished, commit)
 		} else {
+			if len(w.finished) == 0 {
+				return nil
+			}
 			commit := w.finished[w.rand.Intn(len(w.finished))]
 			commit, err := pfsutil.StartCommit(pfsClient, commit.Repo.Name, commit.Id)
 			if err != nil {
@@ -84,11 +90,17 @@ func (w *worker) work(pfsClient pfs.APIClient, ppsClient pps.APIClient) error {
 			w.started = append(w.started, commit)
 		}
 	case opt < file:
+		if len(w.started) == 0 {
+			return nil
+		}
 		commit := w.started[w.rand.Intn(len(w.started))]
 		if _, err := pfsutil.PutFile(pfsClient, commit.Repo.Name, commit.Id, w.name(), 0, w.reader()); err != nil {
 			return err
 		}
 	case opt < job:
+		if len(w.finished) == 0 {
+			return nil
+		}
 		inputs := [5]string{}
 		var inputCommits []*pfs.Commit
 		for i := range inputs {
@@ -115,6 +127,9 @@ func (w *worker) work(pfsClient pfs.APIClient, ppsClient pps.APIClient) error {
 		}
 		w.jobs = append(w.jobs, job)
 	case opt < pipeline:
+		if len(w.repos) == 0 {
+			return nil
+		}
 		inputs := [5]string{}
 		var inputRepos []*pfs.Repo
 		for i := range inputs {
@@ -141,7 +156,7 @@ func (w *worker) work(pfsClient pfs.APIClient, ppsClient pps.APIClient) error {
 }
 
 const letters = "abcdefghijklmnopqrstuvwxyz"
-const lettersAndSpaces = "abcdefghijklmnopqrstuvwxyz      \n\n"
+const lettersAndSpaces = "abcdefghijklmnopqrstuvwxyz      "
 
 func (w *worker) name() string {
 	b := make([]byte, 20)
@@ -157,7 +172,11 @@ type reader struct {
 
 func (r *reader) Read(p []byte) (int, error) {
 	for i := range p {
-		p[i] = lettersAndSpaces[r.rand.Intn(len(lettersAndSpaces))]
+		if i%128 == 0 {
+			p[i] = '\n'
+		} else {
+			p[i] = lettersAndSpaces[r.rand.Intn(len(lettersAndSpaces))]
+		}
 	}
 	if rand.Intn(10) == 0 {
 		return len(p), io.EOF
