c50028739d7bcc54159fcdc08ca274092bdb038e
Joe Doliner <jdoliner@gmail.com>
2016-01-11 04:27:33 -0800

    Remove replicas from sharder.

--- src/pfs/route/route.go
+++ src/pfs/route/route.go
@@ -11,14 +11,12 @@ import (
 type Sharder interface {
 	FileModulus() uint64
 	BlockModulus() uint64
-	NumReplicas() uint64
-	GetBlock(value []byte) *drive.Block
 	GetShard(file *pfs.File) uint64
 	GetBlockShard(block *drive.Block) uint64
 }
 
-func NewSharder(fileModulus uint64, blockModulus uint64, numReplicas uint64) Sharder {
-	return newSharder(fileModulus, blockModulus, numReplicas)
+func NewSharder(fileModulus uint64, blockModulus uint64) Sharder {
+	return newSharder(fileModulus, blockModulus)
 }
 
 type Router interface {
