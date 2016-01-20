68e78c177456706730a2d91d428c8d0460bcfdc7
Joe Doliner <jdoliner@gmail.com>
2016-01-19 18:14:39 -0800

    Adds the start of an obj block server.

--- src/pkg/obj/obj.go
+++ src/pkg/obj/obj.go
@@ -8,8 +8,10 @@ import (
 
 type Client interface {
 	Writer(name string) (io.WriteCloser, error)
-	Reader(name string) (io.ReadCloser, error)
+	//TODO size 0 means read all
+	Reader(name string, offset uint64, size uint64) (io.ReadCloser, error)
 	Delete(name string) error
+	Walk(name string, fn func(name string) error) error
 }
 
 func NewClientGoogleClient(ctx context.Context, bucket string) (Client, error) {
