1cd5d9560e15665f49cd08b95e8182605f225944
Joe Doliner <jdoliner@gmail.com>
2016-01-25 12:49:27 -0800

    Truly remove secret. Last time only 1/2 removed.

--- src/pkg/deploy/assets/assets.go
+++ src/pkg/deploy/assets/assets.go
@@ -105,14 +105,14 @@ func ObjdRc() *api.ReplicationController {
 						{
 							Name: "obj-disk",
 						},
-						{
-							Name: "amazon-secret",
-							VolumeSource: api.VolumeSource{
-								Secret: &api.SecretVolumeSource{
-									SecretName: amazonSecretName,
-								},
-							},
-						},
+						// {
+						// 	Name: "amazon-secret",
+						// 	VolumeSource: api.VolumeSource{
+						// 		Secret: &api.SecretVolumeSource{
+						// 			SecretName: amazonSecretName,
+						// 		},
+						// 	},
+						// },
 					},
 				},
 			},
