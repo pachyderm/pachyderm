4bcaf65a6e247f455513fc2219e7d2262bdec4b4
Joe Doliner <jdoliner@gmail.com>
2016-02-01 16:25:16 -0800

    Gets pfsd functionality working in pachd.

--- src/cmd/pachd/main.go
+++ src/cmd/pachd/main.go
@@ -3,16 +3,28 @@ package main
 import (
 	"fmt"
 
+	"github.com/gengo/grpc-gateway/runtime"
+	"github.com/pachyderm/pachyderm"
+	"github.com/pachyderm/pachyderm/src/pfs"
+	"github.com/pachyderm/pachyderm/src/pfs/drive"
+	"github.com/pachyderm/pachyderm/src/pfs/route"
+	"github.com/pachyderm/pachyderm/src/pfs/server"
 	"github.com/pachyderm/pachyderm/src/pkg/discovery"
+	"github.com/pachyderm/pachyderm/src/pkg/grpcutil"
 	"github.com/pachyderm/pachyderm/src/pkg/netutil"
 	"github.com/pachyderm/pachyderm/src/pkg/shard"
 	"go.pedge.io/env"
 	"go.pedge.io/lion/proto"
+	"go.pedge.io/pkg/http"
+	"go.pedge.io/proto/server"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
 	kube "k8s.io/kubernetes/pkg/client/unversioned"
 )
 
 type appEnv struct {
 	Port            uint16 `env:"PORT,default=650"`
+	HTTPPort        uint16 `env:"HTTP_PORT,default=750"`
 	NumShards       uint64 `env:"NUM_SHARDS,default=32"`
 	StorageRoot     string `env:"OBJ_ROOT,required"`
 	DatabaseAddress string `env:"RETHINK_PORT_28015_TCP_ADDR,required"`
@@ -38,13 +50,73 @@ func do(appEnvObj interface{}) error {
 		return err
 	}
 	address = fmt.Sprintf("%s:%d", address, appEnv.Port)
-	shard.NewSharder(
+	sharder := shard.NewSharder(
 		etcdClient,
 		appEnv.NumShards,
 		0,
 		appEnv.Namespace,
 	)
-	return nil
+	driver, err := drive.NewDriver(address)
+	if err != nil {
+		return err
+	}
+	apiServer := server.NewAPIServer(
+		route.NewSharder(
+			appEnv.NumShards,
+			1,
+		),
+		route.NewRouter(
+			sharder,
+			grpcutil.NewDialer(
+				grpc.WithInsecure(),
+			),
+			address,
+		),
+	)
+	go func() {
+		if err := sharder.RegisterFrontend(nil, address, apiServer); err != nil {
+			protolion.Printf("Error from sharder.RegisterFrontend %s", err.Error())
+		}
+	}()
+	internalAPIServer := server.NewInternalAPIServer(
+		route.NewSharder(
+			appEnv.NumShards,
+			1,
+		),
+		route.NewRouter(
+			sharder,
+			grpcutil.NewDialer(
+				grpc.WithInsecure(),
+			),
+			address,
+		),
+		driver,
+	)
+	go func() {
+		if err := sharder.Register(nil, address, internalAPIServer); err != nil {
+			protolion.Printf("Error from sharder.Register %s", err.Error())
+		}
+	}()
+	return protoserver.ServeWithHTTP(
+		func(s *grpc.Server) {
+			pfs.RegisterAPIServer(s, apiServer)
+			pfs.RegisterInternalAPIServer(s, internalAPIServer)
+		},
+		func(ctx context.Context, mux *runtime.ServeMux, clientConn *grpc.ClientConn) error {
+			return pfs.RegisterAPIHandler(ctx, mux, clientConn)
+		},
+		protoserver.ServeWithHTTPOptions{
+			ServeOptions: protoserver.ServeOptions{
+				Version: pachyderm.Version,
+			},
+		},
+		protoserver.ServeEnv{
+			GRPCPort: appEnv.Port,
+		},
+		pkghttp.HandlerEnv{
+			Port: appEnv.HTTPPort,
+		},
+	)
 }
 
 func getEtcdClient(env *appEnv) discovery.Client {
