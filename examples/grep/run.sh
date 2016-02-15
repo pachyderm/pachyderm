c6a2a57cc0100ab3a6ed9ae20dd35fed78962f40
Joe Doliner <jdoliner@gmail.com>
2016-02-15 03:50:40 -0800

    Mounting not needed to run grep example.

--- examples/grep/run.sh
+++ examples/grep/run.sh
@@ -4,10 +4,10 @@ set -xEe
 
 pachctl create-repo data
 commit1="$(pachctl start-commit data)"
-cat examples/grep/set1.txt >/pfs/data/"$commit1"/set1.txt
+cat examples/grep/set1.txt | pachctl put-file data "$commit1" set1.txt
 pachctl finish-commit data "$commit1"
 pachctl create-pipeline -f examples/grep/grep.json
 pachctl create-pipeline -f examples/grep/count.json
 commit2="$(pachctl start-commit data $commit1)"
-cat examples/grep/set2.txt >/pfs/data/"$commit2"/set2.txt
+cat examples/grep/set2.txt | pachctl put-file data "$commit2" set2.txt
 pachctl finish-commit data "$commit2"
