8f2efc02ceb620524299912d16b5be3123bd7bb1
Joe Doliner <jdoliner@gmail.com>
2016-02-20 19:36:39 -0800

    Consolidate deploy commands into 1 cmd.

--- src/pkg/deploy/cmds/cmds.go
+++ src/pkg/deploy/cmds/cmds.go
@@ -1,6 +1,7 @@
 package cmds
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/pachyderm/pachyderm/src/pkg/deploy/assets"
@@ -10,31 +11,26 @@ import (
 
 func Cmds() []*cobra.Command {
 	var shards int
-	var secrets bool
 	kubernetesManifest := &cobra.Command{
-		Use:   "manifest",
+		Use:   "manifest [amazon-secret bucket id secret token region]",
 		Short: "Print a kubernetes manifest for a Pachyderm cluster.",
 		Long:  "Print a kubernetes manifest for a Pachyderm cluster.",
-		Run: pkgcobra.RunFixedArgs(0, func([]string) error {
-			assets.WriteAssets(os.Stdout, uint64(shards), secrets)
+		Run: pkgcobra.RunBoundedArgs(pkgcobra.Bounds{Min: 0, Max: 5}, func(args []string) error {
+			switch len(args) {
+			case 0:
+				assets.WriteAssets(os.Stdout, uint64(shards), false)
+			case 5:
+				assets.WriteAssets(os.Stdout, uint64(shards), true)
+				assets.WriteAmazonSecret(os.Stdout, args[0], args[1], args[2], args[3], args[4])
+			default:
+				return fmt.Errorf("Expected 0 or 5 args, got %d", len(args))
+			}
 			return nil
 		}),
 	}
 	kubernetesManifest.Flags().IntVarP(&shards, "shards", "s", 1, "The static number of shards for pfs.")
-	kubernetesManifest.Flags().BoolVarP(&secrets, "secrets", "t", false, "Whether to print storage secrets.")
-
-	amazonSecret := &cobra.Command{
-		Use:   "amazon-secret bucket id secret token region",
-		Short: "Print a kubernetes secret containing amazon credentials.",
-		Long:  "Print a kubernetes secret containing amazon credentials.",
-		Run: pkgcobra.RunFixedArgs(5, func(args []string) error {
-			assets.WriteAmazonSecret(os.Stdout, args[0], args[1], args[2], args[3], args[4])
-			return nil
-		}),
-	}
 
 	var result []*cobra.Command
 	result = append(result, kubernetesManifest)
-	result = append(result, amazonSecret)
 	return result
 }
