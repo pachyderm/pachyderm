1bdda2c4834deaef8c98a3bdddabf574fe69f889
Joe Doliner <jdoliner@gmail.com>
2016-03-03 16:17:31 -0800

    Setup server_test to allow multiple tests.

--- src/pfs/server/server_test.go
+++ src/pfs/server/server_test.go
@@ -3,6 +3,7 @@ package server
 import (
 	"fmt"
 	"strings"
+	"sync"
 	"testing"
 
 	"go.pedge.io/proto/server"
@@ -24,36 +25,9 @@ const (
 )
 
 func TestSimple(t *testing.T) {
-	address := fmt.Sprintf("localhost:%d", port)
-	driver, err := drive.NewDriver(address)
-	require.NoError(t, err)
-	blockAPIServer, err := NewLocalBlockAPIServer(uniqueString("/tmp/pach_test/run"))
-	require.NoError(t, err)
-	sharder := shard.NewLocalSharder(address, shards)
-	hasher := pfs.NewHasher(shards, 1)
-	dialer := grpcutil.NewDialer(grpc.WithInsecure())
-	apiServer := NewAPIServer(hasher, shard.NewRouter(sharder, dialer, address))
-	internalAPIServer := NewInternalAPIServer(hasher, shard.NewRouter(sharder, dialer, address), driver)
-	ready := make(chan bool)
-	go func() {
-		err := protoserver.Serve(
-			func(s *grpc.Server) {
-				pfs.RegisterAPIServer(s, apiServer)
-				pfs.RegisterInternalAPIServer(s, internalAPIServer)
-				pfs.RegisterBlockAPIServer(s, blockAPIServer)
-				close(ready)
-			},
-			protoserver.ServeOptions{Version: pachyderm.Version},
-			protoserver.ServeEnv{GRPCPort: port},
-		)
-		require.NoError(t, err)
-	}()
-	<-ready
-	clientConn, err := grpc.Dial(address, grpc.WithInsecure())
-	require.NoError(t, err)
-	pfsClient := pfs.NewAPIClient(clientConn)
+	pfsClient := getPfsClient(t)
 	repo := uniqueString("TestSimple")
-	err = pfsutil.CreateRepo(pfsClient, repo)
+	err := pfsutil.CreateRepo(pfsClient, repo)
 	require.NoError(t, err)
 	_, err = pfsutil.StartCommit(pfsClient, repo, "", "master")
 	require.NoError(t, err)
@@ -61,6 +35,43 @@ func TestSimple(t *testing.T) {
 	require.NoError(t, err)
 	err = pfsutil.FinishCommit(pfsClient, repo, "master")
 	require.NoError(t, err)
+}
+
+var client pfs.APIClient
+var clientOnce sync.Once
+
+func getPfsClient(t *testing.T) pfs.APIClient {
+	clientOnce.Do(func() {
+		address := fmt.Sprintf("localhost:%d", port)
+		driver, err := drive.NewDriver(address)
+		require.NoError(t, err)
+		blockAPIServer, err := NewLocalBlockAPIServer(uniqueString("/tmp/pach_test/run"))
+		require.NoError(t, err)
+		sharder := shard.NewLocalSharder(address, shards)
+		hasher := pfs.NewHasher(shards, 1)
+		dialer := grpcutil.NewDialer(grpc.WithInsecure())
+		apiServer := NewAPIServer(hasher, shard.NewRouter(sharder, dialer, address))
+		internalAPIServer := NewInternalAPIServer(hasher, shard.NewRouter(sharder, dialer, address), driver)
+		ready := make(chan bool)
+		go func() {
+			err := protoserver.Serve(
+				func(s *grpc.Server) {
+					pfs.RegisterAPIServer(s, apiServer)
+					pfs.RegisterInternalAPIServer(s, internalAPIServer)
+					pfs.RegisterBlockAPIServer(s, blockAPIServer)
+					close(ready)
+				},
+				protoserver.ServeOptions{Version: pachyderm.Version},
+				protoserver.ServeEnv{GRPCPort: port},
+			)
+			require.NoError(t, err)
+		}()
+		<-ready
+		clientConn, err := grpc.Dial(address, grpc.WithInsecure())
+		require.NoError(t, err)
+		client = pfs.NewAPIClient(clientConn)
+	})
+	return client
 }
 
 func uniqueString(prefix string) string {
