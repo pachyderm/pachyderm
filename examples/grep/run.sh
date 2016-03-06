5efcf9094380b5c2966af4f09899387cdc59c207
Joe Doliner <jdoliner@gmail.com>
2016-03-06 14:33:51 -0800

    Update readme with multipipeline create.

--- examples/grep/run.sh
+++ examples/grep/run.sh
@@ -6,8 +6,7 @@ pachctl create-repo data
 commit1="$(pachctl start-commit data)"
 cat examples/grep/set1.txt | pachctl put-file data "$commit1" set1.txt
 pachctl finish-commit data "$commit1"
-pachctl create-pipeline -f examples/grep/grep.json
-pachctl create-pipeline -f examples/grep/count.json
+pachctl create-pipeline -f examples/grep/pipeline.json
 commit2="$(pachctl start-commit data $commit1)"
 cat examples/grep/set2.txt | pachctl put-file data "$commit2" set2.txt
 pachctl finish-commit data "$commit2"
