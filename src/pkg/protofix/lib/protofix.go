dd0ea5754ad8f9bcdc7ee91006c62f3c09df608c
Sean Jezewski <seanwjezewski@gmail.com>
2016-03-08 12:21:03 -0800

    Have 'protofix' command line util working now. Now to hook it into the makefile

--- src/pkg/protofix/lib/protofix.go
+++ src/pkg/protofix/lib/protofix.go
@@ -11,13 +11,35 @@ import(
 	"io/ioutil"
 	"path/filepath"
 	"os"
+	"os/exec"
 )
 
 func FixAllPBGOFilesInDirectory(rootPath string) {
 
 	filepath.Walk(rootPath, func (path string, f os.FileInfo, err error) error {
 		if strings.HasSuffix(f.Name(), ".pb.go") {
-			repairFile(fmt.Sprintf("%v/%v", path, f.Name()))
+			fmt.Printf("Repairing %v\n", path)
+			repairFile(path)
+		}
+		return nil
+	})
+
+}
+
+func RevertAllPBGOFilesInDirectory(rootPath string) {
+
+	filepath.Walk(rootPath, func (path string, f os.FileInfo, err error) error {
+		if f == nil {
+			return nil
+		}
+		if strings.HasSuffix(f.Name(), ".pb.go") {
+			fmt.Printf("Reverting %v\n", path)
+			args := []string{"checkout", path}
+			_, err := exec.Command("git", args... ).Output()
+			if err != nil {
+				fmt.Printf("Error reverting %v : %v\n", path, err)
+				os.Exit(1)
+			}
 		}
 		return nil
 	})
@@ -53,11 +75,14 @@ func repairFile(filename string) {
 func repairDeclaration(node ast.Node) {
 	switch node := node.(type) {
 	case *ast.Field:
-		declName := node.Names[0].Name
-		if strings.HasSuffix(declName, "Id") {
-			normalized := strings.TrimSuffix(declName, "Id")
-			node.Names[0] = ast.NewIdent(fmt.Sprintf("%vID", normalized))
+		if len(node.Names) > 0 {
+			declName := node.Names[0].Name
+			if strings.HasSuffix(declName, "Id") {
+				normalized := strings.TrimSuffix(declName, "Id")
+				node.Names[0] = ast.NewIdent(fmt.Sprintf("%vID", normalized))
+			}
 		}
+
 //	default:
 //		fmt.Printf("the type is (%T)\n", node)
 	}
--- src/pkg/protofix/protofix.go
+++ src/pkg/protofix/protofix.go
@@ -4,20 +4,27 @@ import(
 	"os"
 	"fmt"
 
-	"github.com/pachyderm/pachyderm/src/pkg/protofix/protofix"
+	protofix "github.com/pachyderm/pachyderm/src/pkg/protofix/lib"
 )
 
 func usage() {
-	fmt.Println("usage: protofix <rootdirectory>")
+	fmt.Println("usage: protofix fix <rootdirectory>")
+	fmt.Println("usage: protofix revert <rootdirectory>")
 }
 
 func main() {
 
-	if len(os.Args) < 2 {
+	if len(os.Args) < 3 {
 		fmt.Println("Missing root directory")
 		usage()
 		os.Exit(1)
 	}
 
-	protofix.FixAllPBGOFilesInDirectory(os.Args[1])
+	if os.Args[1] == "fix" {
+		protofix.FixAllPBGOFilesInDirectory(os.Args[2])
+	}
+	if os.Args[1] == "revert" {
+		protofix.RevertAllPBGOFilesInDirectory(os.Args[2])
+	}
+
 }
