14686f45edc8c42df8ab539afb39da93d030095e
Joe Doliner <jdoliner@gmail.com>
2016-03-15 13:55:58 -0700

    Remove the last remnants of grep example.

--- examples/grep/run2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -xEe
-
-commit2="$(pachctl start-commit data $commit1)"
-cat examples/grep/set2.txt | pachctl put-file data "$commit2" set2.txt
-pachctl finish-commit data "$commit2"
