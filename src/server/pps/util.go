018857b900e65955bac057a40a151ab0194f0cae
Derek Chiang <derekchiang93@gmail.com>
2016-03-24 13:03:03 -0700

    Fix dot import

--- src/server/pps/util.go
+++ src/server/pps/util.go
@@ -3,14 +3,14 @@ package pps
 import (
 	"fmt"
 
-	. "github.com/pachyderm/pachyderm/src/client/pfs"
+	"github.com/pachyderm/pachyderm/src/client/pfs"
 	ppsclient "github.com/pachyderm/pachyderm/src/client/pps"
 )
 
-func JobRepo(job *ppsclient.Job) *Repo {
-	return &Repo{Name: fmt.Sprintf("job-%s", job.ID)}
+func JobRepo(job *ppsclient.Job) *pfs.Repo {
+	return &pfs.Repo{Name: fmt.Sprintf("job-%s", job.ID)}
 }
 
-func PipelineRepo(pipeline *ppsclient.Pipeline) *Repo {
-	return &Repo{Name: pipeline.Name}
+func PipelineRepo(pipeline *ppsclient.Pipeline) *pfs.Repo {
+	return &pfs.Repo{Name: pipeline.Name}
 }
