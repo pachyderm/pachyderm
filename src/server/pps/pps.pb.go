45f6d242fbb8a7c0bde139fcc8ee8d4b77c92e04
Sean Jezewski <seanwjezewski@gmail.com>
2016-04-19 09:19:58 -0700

    Regen proto files w new 'make proto' command. Still seeing some build errors (from grpc change) though

--- src/server/pps/pps.pb.go
+++ src/server/pps/pps.pb.go
@@ -108,6 +108,7 @@ var _ grpc.ClientConn
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
+const _ = grpc.SupportPackageIsVersion2
 
 // Client API for InternalJobAPI service
 
@@ -153,28 +154,40 @@ func RegisterInternalJobAPIServer(s *grpc.Server, srv InternalJobAPIServer) {
 	s.RegisterService(&_InternalJobAPI_serviceDesc, srv)
 }
 
-func _InternalJobAPI_StartJob_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error) (interface{}, error) {
+func _InternalJobAPI_StartJob_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(StartJobRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
-	out, err := srv.(InternalJobAPIServer).StartJob(ctx, in)
-	if err != nil {
-		return nil, err
+	if interceptor == nil {
+		return srv.(InternalJobAPIServer).StartJob(ctx, in)
 	}
-	return out, nil
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/pachyderm.pps.InternalJobAPI/StartJob",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(InternalJobAPIServer).StartJob(ctx, req.(*StartJobRequest))
+	}
+	return interceptor(ctx, in, info, handler)
 }
 
-func _InternalJobAPI_FinishJob_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error) (interface{}, error) {
+func _InternalJobAPI_FinishJob_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(FinishJobRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
-	out, err := srv.(InternalJobAPIServer).FinishJob(ctx, in)
-	if err != nil {
-		return nil, err
+	if interceptor == nil {
+		return srv.(InternalJobAPIServer).FinishJob(ctx, in)
 	}
-	return out, nil
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/pachyderm.pps.InternalJobAPI/FinishJob",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(InternalJobAPIServer).FinishJob(ctx, req.(*FinishJobRequest))
+	}
+	return interceptor(ctx, in, info, handler)
 }
 
 var _InternalJobAPI_serviceDesc = grpc.ServiceDesc{
