54f91297286deb5b4f2e394d774eba6ec775c075
Derek Chiang <derekchiang93@gmail.com>
2016-04-20 13:18:36 -0700

    Fix the bug where files cannot be read from gce because the backend is not reading the correct number of bytes.

--- src/server/pkg/obj/google_client.go
+++ src/server/pkg/obj/google_client.go
@@ -47,9 +47,12 @@ func (c *googleClient) Walk(name string, fn func(name string) error) error {
 	return nil
 }
 
-//TODO size 0 means read all
+//size 0 means read all
 func (c *googleClient) Reader(name string, offset uint64, size uint64) (io.ReadCloser, error) {
-	return c.bucket.Object(name).NewReader(c.ctx)
+	if size == 0 {
+		return c.bucket.Object(name).NewReader(c.ctx)
+	}
+	return c.bucket.Object(name).NewRangeReader(c.ctx, int64(offset), int64(size))
 }
 
 func (c *googleClient) Delete(name string) error {
