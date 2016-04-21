d97c144e44f235617be394b37667998f1c18de63
Joe Doliner <jdoliner@gmail.com>
2016-04-21 14:22:03 -0700

    Report metrics to segment as well as logging them.

--- src/server/pkg/metrics/segment.go
+++ src/server/pkg/metrics/segment.go
@@ -1,9 +1,31 @@
 package metrics
 
 import (
+	"github.com/pachyderm/pachyderm/src/client/pkg/uuid"
+
 	"github.com/segmentio/analytics-go"
+	"go.pedge.io/lion"
+)
+
+var (
+	id = uuid.NewWithoutDashes()
 )
 
 func reportSegment(metrics *Metrics) {
-	_ = analytics.New("hhxbyr7x50w3jtgcwcZUyOFrTf4VNMrD")
+	client := analytics.New("hhxbyr7x50w3jtgcwcZUyOFrTf4VNMrD")
+	err := client.Track(&analytics.Track{
+		Event:       "metrics",
+		AnonymousId: id,
+		Properties: map[string]interface{}{
+			"repos":     metrics.Repos,
+			"commits":   metrics.Commits,
+			"files":     metrics.Files,
+			"bytes":     metrics.Bytes,
+			"jobs":      metrics.Jobs,
+			"pipelines": metrics.Pipelines,
+		},
+	})
+	if err != nil {
+		lion.Errorf("error reporting to Segment: %s", err.Error())
+	}
 }
