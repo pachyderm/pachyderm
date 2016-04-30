dce951e4598c5ae4a46ace30c75640502037e84b
Joe Doliner <jdoliner@gmail.com>
2016-04-29 17:35:35 -0700

    Adds client docs for pps.

--- src/client/pps/pps.go
+++ src/client/pps/pps.go
@@ -15,6 +15,22 @@ func NewPipeline(pipelineName string) *Pipeline {
 	return &Pipeline{Name: pipelineName}
 }
 
+// CreateJob creates and runs a job in PPS.
+// image is the Docker image to run the job in.
+// cmd is the command passed to the Docker run invocation.
+// NOTE as with Docker cmd is not run inside a shell that means that things
+// like wildcard globbing (*), pipes (|) and file redirects (> and >>) will not
+// work. To get that behavior you should have your command be a shell of your
+// choice and pass a shell script to stdin.
+// stdin is a slice of lines that are sent to your command on stdin. Lines need
+// not end in newline characters.
+// parallelism is how many copies of your container should run in parallel. You
+// may pass 0 for parallelism in which case PPS will set the parallelism based
+// on availabe resources.
+// inputs specifies a set of Commits that will be visible to the job during runtime.
+// parentJobID specifies the a job to use as a parent, it may be left empty in
+// which case there is no parent job. If not left empty your job will use the
+// parent Job's output commit as the parent of its output commit.
 func CreateJob(
 	client APIClient,
 	image string,
@@ -43,6 +59,7 @@ func CreateJob(
 	)
 }
 
+// GetLogs gets logs from a job (logs includes stdout and stderr).
 func GetLogs(
 	client APIClient,
 	jobID string,
@@ -60,6 +77,24 @@ func GetLogs(
 	return protostream.WriteFromStreamingBytesClient(getLogsClient, writer)
 }
 
+// CreatePipeline creates a new pipeline, pipelines are the main computation
+// object in PPS they create a flow of data from a set of input Repos to an
+// output Repo (which has the same name as the pipeline). Whenever new data is
+// committed to one of the input repos the pipelines will create jobs to bring
+// the output Repo up to data.
+// image is the Docker image to run the jobs in.
+// cmd is the command passed to the Docker run invocation.
+// NOTE as with Docker cmd is not run inside a shell that means that things
+// like wildcard globbing (*), pipes (|) and file redirects (> and >>) will not
+// work. To get that behavior you should have your command be a shell of your
+// choice and pass a shell script to stdin.
+// stdin is a slice of lines that are sent to your command on stdin. Lines need
+// not end in newline characters.
+// parallelism is how many copies of your container should run in parallel. You
+// may pass 0 for parallelism in which case PPS will set the parallelism based
+// on availabe resources.
+// inputs specifies a set of Repos that will be visible to the jobs during runtime.
+// commits to these repos will cause the pipeline to create new jobs to process them.
 func CreatePipeline(
 	client APIClient,
 	name string,
@@ -85,6 +120,7 @@ func CreatePipeline(
 	return err
 }
 
+// DeletePipeline deletes a pipeline along with its output Repo.
 func DeletePipeline(
 	client APIClient,
 	name string,
