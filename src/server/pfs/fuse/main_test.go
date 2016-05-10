4ce954b5ce9b3107ee070a955751e15c17f0de6e
Sean Jezewski <seanwjezewski@gmail.com>
2016-05-10 16:03:47 -0700

    Add final post test hook to guarantee spec reports don't change after running tests. This prevents tests from passing on CI but the spec not being checked into git

--- src/server/pfs/fuse/main_test.go
+++ src/server/pfs/fuse/main_test.go
@@ -6,6 +6,8 @@ import (
 	"testing"
 
 	"github.com/pachyderm/pachyderm/src/server/pfs/fuse/spec"
+	"os/exec"
+	"strings"
 )
 
 var OpenCommitSyscallSpec *spec.Spec
@@ -56,7 +58,28 @@ func TestMain(m *testing.M) {
 		os.Exit(1)
 	}
 
+	didSpecReportChange()
 	// Todo - if the reports changed, fail CI, because it means this wasn't run
 	// locally and couldn't have been run on linux and mac
 	os.Exit(exitVal)
+}
+
+func didSpecReportChange() {
+	// Checks to see if the generated report is different from what git expects.
+	// If so ... exit 1 ... any changes to the spec need to be done manually.
+	// This will prevent checks 'passing' on CI but the new spec not being committed
+
+	c := exec.Command("git", "status", "-s")
+	raw, err := c.Output()
+	if err != nil {
+		fmt.Printf("Couldn't query git status, so couldn't validate spec freshness")
+		os.Exit(1)
+	}
+
+	if strings.Contains(string(raw), "reports/summary") {
+		fmt.Printf("There are uncommitted changes!\nThe generated spec does not match the existing spec. Please run `make spec-generate` locally to vet and commit the changes as necessary\n")
+		fmt.Printf("Uncommitted changes:\n%v\n", string(raw))
+		os.Exit(1)
+	}
+
 }
