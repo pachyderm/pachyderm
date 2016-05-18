cf9625efc6527ee7f317826e0d5d498b80d5a1be
Derek Chiang <derekchiang93@gmail.com>
2016-05-18 13:26:09 -0700

    Bug fix

--- src/server/pfs/hash.go
+++ src/server/pfs/hash.go
@@ -20,28 +20,30 @@ func NewHasher(fileModulus uint64, blockModulus uint64) *Hasher {
 	}
 }
 
-// HashFile hashes the first part of the file path.  That is, for a path like
-// foo/bar/buzz, HashFile only considers foo
 func (s *Hasher) HashFile(file *pfs.File) uint64 {
-	parts := strings.Split(path.Clean(file.Path), "/")
-	var topLevelPath string
-	if len(parts) > 0 {
-		topLevelPath = parts[0]
-	}
-	return uint64(adler32.Checksum([]byte(topLevelPath))) % s.FileModulus
+	return uint64(adler32.Checksum([]byte(path.Clean(file.Path)))) % s.FileModulus
 }
 
 func (s *Hasher) HashBlock(block *pfs.Block) uint64 {
 	return uint64(adler32.Checksum([]byte(block.Hash))) % s.BlockModulus
 }
 
+// FileInShard checks if a given file belongs in a given shard, using only the
+// file's top-level path.  That is, for a path like foo/bar/buzz, FileInShard only
+// considers foo
 func FileInShard(shard *pfs.Shard, file *pfs.File) bool {
 	if shard == nil || shard.FileModulus == 0 {
 		// this lets us default to no filtering
 		return true
 	}
+	parts := strings.Split(path.Clean(file.Path), "/")
+	var topLevelPath string
+	if len(parts) > 0 {
+		topLevelPath = parts[0]
+	}
 	sharder := &Hasher{FileModulus: shard.FileModulus}
-	return sharder.HashFile(file) == shard.FileNumber
+	f := &pfs.File{Path: topLevelPath}
+	return sharder.HashFile(f) == shard.FileNumber
 }
 
 func BlockInShard(shard *pfs.Shard, block *pfs.Block) bool {
