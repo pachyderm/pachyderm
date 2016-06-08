b00b846470c123387a2b63383f9da2f91202a9b6
Derek Chiang <derekchiang93@gmail.com>
2016-06-07 19:11:32 -0700

    Adding pipeline spec and map implementation

---
+++ examples/word_count/map.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var (
+	reg *regexp.Regexp
+)
+
+func sanitize(word string) string {
+	sanitized := reg.ReplaceAllString(word, "")
+	return strings.ToLower(sanitized)
+}
+
+func shuffle(slice []os.FileInfo) {
+	for i := range slice {
+		j := rand.Intn(i + 1)
+		slice[i], slice[j] = slice[j], slice[i]
+	}
+}
+
+func main() {
+	flag.Parse()
+	args := flag.Args()
+	if len(args) != 2 {
+		log.Fatalf("expect two arguemnts; got %v", len(args))
+	}
+
+	var err error
+	reg, err = regexp.Compile("[^A-Za-z]+")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	inputDir := args[0]
+	outputDir := args[1]
+
+	files, err := ioutil.ReadDir(inputDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// we want to process files in a random order in order to
+	// avoid flash-crowd behavior
+	shuffle(files)
+
+	wordMap := make(map[string]int)
+
+	for _, file := range files {
+		log.Printf("scanning file %v", file.Name())
+		f, err := os.Open(filepath.Join(inputDir, file.Name()))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		scanner := bufio.NewScanner(f)
+		scanner.Split(bufio.ScanWords)
+		count := 0
+		for scanner.Scan() {
+			count += 1
+			word := sanitize(scanner.Text())
+			wordMap[word] = wordMap[word] + 1
+		}
+
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+		log.Printf("found %d words in %s", count, file.Name())
+
+		if err := f.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	for word, count := range wordMap {
+		err := ioutil.WriteFile(filepath.Join(outputDir, word), []byte(strconv.Itoa(count)+"\n"), 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
