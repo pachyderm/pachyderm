4e757a2fd7cb041663aed9c9d9b7bfac05dca41c
Joe Doliner <jdoliner@gmail.com>
2016-06-15 18:18:30 -0700

    Adds transforms to pretty printing.

--- src/server/pkg/pretty/pretty.go
+++ src/server/pkg/pretty/pretty.go
@@ -2,12 +2,20 @@ package pretty
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/docker/go-units"
+
 	"go.pedge.io/pb/go/google/protobuf"
 	"go.pedge.io/proto/time"
 )
+
+func UnescapeHTML(s string) string {
+	s = strings.Replace(s, "\\u003c", "<", -1)
+	s = strings.Replace(s, "\\u003e", ">", -1)
+	return s
+}
 
 func PrettyDuration(timestamp *google_protobuf.Timestamp) string {
 	return fmt.Sprintf("%s ago", units.HumanDuration(time.Since(prototime.TimestampToTime(timestamp))))
