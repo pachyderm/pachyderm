800dd5ec21d1eab30ab9dcb101744e46c1887ae5
Sean Jezewski <seanwjezewski@gmail.com>
2016-06-24 16:48:19 -0700

    Update additional version string build method
    Now we supply the version at link time.

--- src/client/version/version.go
+++ src/client/version/version.go
@@ -2,7 +2,6 @@ package version
 
 import (
 	"fmt"
-	"os"
 	"os/exec"
 	"strings"
 
@@ -16,8 +15,6 @@ const (
 	MinorVersion = 0
 	// MicroVersion is the patch number for pachyderm.
 	MicroVersion = 1
-	// AdditionalVersion is the string provided at release time
-	AdditionalVersion = "RC"
 )
 
 var (
@@ -28,19 +25,25 @@ var (
 		Micro:      MicroVersion,
 		Additional: getAdditionalVersion(),
 	}
+	// AdditionalVersion is the string provided at release time
+	AdditionalVersion string
 )
 
 func PrettyPrintVersion(version *protoversion.Version) string {
 	result := fmt.Sprintf("%d.%d.%d", version.Major, version.Minor, version.Micro)
 	if version.Additional != "" {
-		result += fmt.Sprintf("(%s)", version.Additional)
+		result += fmt.Sprintf("-%s", version.Additional)
 	}
 	return result
 }
 
 func getAdditionalVersion() string {
-	value := os.Getenv("VERSION_ADDITIONAL")
+	value := AdditionalVersion
 	if value == "" {
+		_, err := exec.LookPath("git")
+		if err != nil {
+			return "dirty"
+		}
 		out, err := exec.Command("git", "log", "--pretty=format:%H").Output()
 		if err != nil {
 			panic(err)
