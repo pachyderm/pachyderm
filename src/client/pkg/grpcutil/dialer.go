aca98780307d1fca3d8b62dc2ce47eb92f480189
Derek Chiang <derekchiang93@gmail.com>
2016-06-26 20:16:01 -0700

    Reuse grpc connections in pachd

--- src/client/pkg/grpcutil/dialer.go
+++ src/client/pkg/grpcutil/dialer.go
@@ -1,17 +1,48 @@
 package grpcutil
 
 import (
+	"sync"
+
 	"google.golang.org/grpc"
 )
 
 type dialer struct {
 	opts []grpc.DialOption
+	// A map from addresses to connections
+	connMap map[string]*grpc.ClientConn
+	lock    *sync.Mutex
 }
 
 func newDialer(opts ...grpc.DialOption) *dialer {
-	return &dialer{opts}
+	return &dialer{
+		opts:    opts,
+		connMap: make(map[string]*grpc.ClientConn),
+		lock:    &sync.Mutex{},
+	}
 }
 
 func (d *dialer) Dial(address string) (*grpc.ClientConn, error) {
-	return grpc.Dial(address, d.opts...)
+	d.lock.Lock()
+	defer d.lock.Unlock()
+	if conn, ok := d.connMap[address]; ok {
+		return conn, nil
+	}
+	conn, err := grpc.Dial(address, d.opts...)
+	if err != nil {
+		return nil, err
+	}
+	d.connMap[address] = conn
+	return conn, nil
+}
+
+func (d *dialer) CloseConns() error {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+	for address, conn := range d.connMap {
+		if err := conn.Close(); err != nil {
+			return err
+		}
+		delete(d.connMap, address)
+	}
+	return nil
 }
