514237c3c192561e0c58d034dc28db50624c70c6
Derek Chiang <derekchiang93@gmail.com>
2016-06-28 17:25:40 -0700

    fix test

--- src/server/pfs/drive/driver.go
+++ src/server/pfs/drive/driver.go
@@ -1108,7 +1108,7 @@ func (d *driver) inspectFile(file *pfs.File, filterShard *pfs.Shard, shard uint6
 	// We return NotFound if it's a regular file with no blocks.  Currently this
 	// only happens if the file's blocks got filtered out.  Even an empty file
 	// has one block.
-	if fileInfo.FileType == pfs.FileType_FILE_TYPE_REGULAR && len(blockRefs) == 0 {
+	if fileInfo.FileType == pfs.FileType_FILE_TYPE_REGULAR && len(blockRefs) == 0 && (filterShard != nil && filterShard.BlockNumber > 1) {
 		return nil, nil, pfsserver.NewErrFileNotFound(file.Path, file.Commit.Repo.Name, file.Commit.ID)
 	}
 	return fileInfo, blockRefs, nil
