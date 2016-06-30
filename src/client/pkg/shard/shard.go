711ec8ee4ade2844ec4ebdc67cf5f4a46ee88cce
Sam Broughton <sambroughton@hotmail.co.uk>
2016-06-30 20:03:07 +0100

    Fix linting errors in shard package

--- src/client/pkg/shard/shard.go
+++ src/client/pkg/shard/shard.go
@@ -16,14 +16,17 @@ type Sharder interface {
 	AssignRoles(address string, cancel chan bool) error
 }
 
+// NewSharder creates a Sharder using a discovery client.
 func NewSharder(discoveryClient discovery.Client, numShards uint64, namespace string) Sharder {
 	return newSharder(discoveryClient, numShards, namespace)
 }
 
+// NewLocalSharder creates a Sharder user a list of addresses.
 func NewLocalSharder(addresses []string, numShards uint64) Sharder {
 	return newLocalSharder(addresses, numShards)
 }
 
+// A Server represents a server that has roles for shards.
 type Server interface {
 	// AddShard tells the server it now has a role for a shard.
 	AddShard(shard uint64) error
@@ -31,12 +34,14 @@ type Server interface {
 	DeleteShard(shard uint64) error
 }
 
+// A Frontend represents a frontend which receives new versions.
 type Frontend interface {
 	// Version tells the Frontend a new version exists.
 	// Version should block until the Frontend is done using the previous version.
 	Version(version int64) error
 }
 
+// Router represents a router from shard id and version to grpc connections.
 type Router interface {
 	GetShards(version int64) (map[uint64]bool, error)
 	GetClientConn(shard uint64, version int64) (*grpc.ClientConn, error)
@@ -44,6 +49,7 @@ type Router interface {
 	CloseClientConns() error // close all outstanding client connections
 }
 
+// NewRouter creates a Router.
 func NewRouter(
 	sharder Sharder,
 	dialer grpcutil.Dialer,
