7c5fc48d14e68e2b82e4c101aa85173d91e66b1e
Derek Chiang <derekchiang93@gmail.com>
2016-06-30 18:50:57 -0700

    Add --force to delete-repo

--- src/server/pfs/drive/drive.go
+++ src/server/pfs/drive/drive.go
@@ -16,7 +16,7 @@ type Driver interface {
 	CreateRepo(repo *pfs.Repo, created *google_protobuf.Timestamp, provenance []*pfs.Repo, shards map[uint64]bool) error
 	InspectRepo(repo *pfs.Repo, shards map[uint64]bool) (*pfs.RepoInfo, error)
 	ListRepo(provenance []*pfs.Repo, shards map[uint64]bool) ([]*pfs.RepoInfo, error)
-	DeleteRepo(repo *pfs.Repo, shards map[uint64]bool) error
+	DeleteRepo(repo *pfs.Repo, shards map[uint64]bool, force bool) error
 	StartCommit(repo *pfs.Repo, commitID string, parentID string, branch string, started *google_protobuf.Timestamp, provenance []*pfs.Commit, shards map[uint64]bool) error
 	FinishCommit(commit *pfs.Commit, finished *google_protobuf.Timestamp, cancel bool, shards map[uint64]bool) error
 	InspectCommit(commit *pfs.Commit, shards map[uint64]bool) (*pfs.CommitInfo, error)
