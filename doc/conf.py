6689df1397233dcef8d983d820db0d52be663a2e
Sean Jezewski <seanwjezewski@gmail.com>
2016-07-27 15:19:08 -0700

    Enable slightly better sidebar
    We'll need to populate the document structure a bit more to make it more helpful

--- doc/conf.py
+++ doc/conf.py
@@ -182,7 +182,7 @@ html_static_path = ['_static']
 
 # Custom sidebar templates, maps document names to template names.
 #
-# html_sidebars = {}
+html_sidebars = { '**': ['searchbox.html', 'globaltoc.html', 'relations.html', 'sourcelink.html'], }
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
