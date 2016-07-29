a280eea56f46e8ff7b39a1f749f4beb443d5a107
Derek Chiang <derekchiang93@gmail.com>
2016-07-29 14:45:15 -0700

    Bug fix and test update

--- src/server/pfs/server/server_refactor_test.go
+++ src/server/pfs/server/server_refactor_test.go
@@ -57,7 +57,7 @@ func TestNEWAPIPutFile(t *testing.T) {
 	require.NoError(t, err)
 	_, err = client.PutFile(repo, commit1.ID, "file", strings.NewReader("bar\n"))
 	require.NoError(t, err)
-	_, err = client.PutFile(repo, commit1.ID, "file", strings.NewReader("buzz\n"))
+	_, err = client.PutFile(repo, "master/0", "file", strings.NewReader("buzz\n"))
 	require.NoError(t, err)
 	require.NoError(t, client.FinishCommit(repo, "master/0"))
 
@@ -68,4 +68,33 @@ func TestNEWAPIPutFile(t *testing.T) {
 	buffer.Reset()
 	require.NoError(t, client.GetFile(repo, "master/0", "file", 0, 0, "", nil, buffer))
 	require.Equal(t, expected, buffer.String())
+
+	commit2, err := client.StartCommit(repo, "", "master")
+	require.NoError(t, err)
+	_, err = client.PutFile(repo, commit2.ID, "file", strings.NewReader("foo\n"))
+	require.NoError(t, err)
+	_, err = client.PutFile(repo, commit2.ID, "file", strings.NewReader("bar\n"))
+	require.NoError(t, err)
+	_, err = client.PutFile(repo, "master/1", "file", strings.NewReader("buzz\n"))
+	require.NoError(t, err)
+	require.NoError(t, client.FinishCommit(repo, "master/1"))
+
+	expected = "foo\nbar\nbuzz\nfoo\nbar\nbuzz\n"
+	buffer.Reset()
+	require.NoError(t, client.GetFile(repo, commit2.ID, "file", 0, 0, "master/0", nil, buffer))
+	require.Equal(t, expected, buffer.String())
+	buffer.Reset()
+	require.NoError(t, client.GetFile(repo, "master/1", "file", 0, 0, "master/0", nil, buffer))
+	require.Equal(t, expected, buffer.String())
+
+	_, err = client.StartCommit(repo, "master/1", "foo")
+	require.NoError(t, err)
+	_, err = client.PutFile(repo, "foo/0", "file", strings.NewReader("foo\nbar\nbuzz\n"))
+	require.NoError(t, client.FinishCommit(repo, "foo/0"))
+
+	expected = "foo\nbar\nbuzz\nfoo\nbar\nbuzz\nfoo\nbar\nbuzz\n"
+	buffer.Reset()
+	require.NoError(t, client.GetFile(repo, "foo/0", "file", 0, 0, "master/0", nil, buffer))
+	require.Equal(t, expected, buffer.String())
+
 }
