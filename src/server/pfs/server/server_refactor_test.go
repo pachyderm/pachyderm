9789a7ce38dec1bc590f49021ed6c6a733cc1c2c
Derek Chiang <derekchiang93@gmail.com>
2016-07-29 18:48:16 -0700

    Make PutFile create directories

--- src/server/pfs/server/server_refactor_test.go
+++ src/server/pfs/server/server_refactor_test.go
@@ -46,7 +46,7 @@ func TestNEWAPIStartCommitNewBranchRF(t *testing.T) {
 	require.Equal(t, "test", commitInfo.Commit.Repo.Name)
 }
 
-func TestNEWAPIPutFile(t *testing.T) {
+func TestNEWAPIPutFileRF(t *testing.T) {
 	t.Parallel()
 	client, _ := getClientAndServer(t)
 	repo := "test"
@@ -96,5 +96,45 @@ func TestNEWAPIPutFile(t *testing.T) {
 	buffer.Reset()
 	require.NoError(t, client.GetFile(repo, "foo/0", "file", 0, 0, "master/0", nil, buffer))
 	require.Equal(t, expected, buffer.String())
+}
 
+func TestNEWAPIDeleteFile(t *testing.T) {
+	t.Parallel()
+	client, _ := getClientAndServer(t)
+	repo := "test"
+	require.NoError(t, client.CreateRepo(repo))
+
+	_, err := client.StartCommit(repo, "", "master")
+	require.NoError(t, err)
+	_, err = client.PutFile(repo, "master/0", "file", strings.NewReader("foo\n"))
+	require.NoError(t, err)
+	require.NoError(t, client.FinishCommit(repo, "master/0"))
+
+	_, err = client.StartCommit(repo, "", "master")
+	require.NoError(t, err)
+	err = client.DeleteFile(repo, "master/1", "file", false, "")
+	require.NoError(t, err)
+	_, err = client.PutFile(repo, "master/1", "file", strings.NewReader("bar\n"))
+	require.NoError(t, err)
+	require.NoError(t, client.FinishCommit(repo, "master/1"))
+
+	expected := "bar\n"
+	var buffer bytes.Buffer
+	require.NoError(t, client.GetFile(repo, "master/1", "file", 0, 0, "master/0", nil, &buffer))
+	require.Equal(t, expected, buffer.String())
+
+	_, err = client.StartCommit(repo, "", "master")
+	require.NoError(t, err)
+	_, err = client.PutFile(repo, "master/2", "file", strings.NewReader("buzz\n"))
+	require.NoError(t, err)
+	err = client.DeleteFile(repo, "master/2", "file", false, "")
+	require.NoError(t, err)
+	_, err = client.PutFile(repo, "master/2", "file", strings.NewReader("foo\n"))
+	require.NoError(t, err)
+	require.NoError(t, client.FinishCommit(repo, "master/2"))
+
+	expected = "foo\n"
+	buffer.Reset()
+	require.NoError(t, client.GetFile(repo, "master/2", "file", 0, 0, "master/0", nil, &buffer))
+	require.Equal(t, expected, buffer.String())
 }
