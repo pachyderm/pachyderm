eafe0e9705f5c8fd62de160cfc368efa46cca8b1
Sean Jezewski <seanwjezewski@gmail.com>
2016-08-10 16:13:19 -0400

    In the case of an empty repo, tell fuse the repo directory is read only

--- src/server/pfs/fuse/filesystem.go
+++ src/server/pfs/fuse/filesystem.go
@@ -532,6 +532,12 @@ func (d *directory) lookUpRepo(ctx context.Context, name string) (fs.Node, error
 	result.RepoAlias = commitMount.Alias
 	result.Shard = commitMount.Shard
 
+	if commitMount.Commit.ID == "" {
+		// The Repo is empty
+		result.Write = false
+		result.Modified = repoInfo.Created
+		return result, nil
+	}
 	commitInfo, err := d.fs.apiClient.InspectCommit(
 		commitMount.Commit.Repo.Name,
 		commitMount.Commit.ID,
