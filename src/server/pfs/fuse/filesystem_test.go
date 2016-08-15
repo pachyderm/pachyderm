3df0769d960b4f675c3a5fcc8930621c8150c19b
Sean Jezewski <seanwjezewski@gmail.com>
2016-08-15 10:29:56 -0700

    Turn off fuse debug logging
    For travis' sake

--- src/server/pfs/fuse/filesystem_test.go
+++ src/server/pfs/fuse/filesystem_test.go
@@ -813,7 +813,7 @@ func testFuse(
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		require.NoError(t, mounter.MountAndCreate(mountpoint, nil, nil, ready, true))
+		require.NoError(t, mounter.MountAndCreate(mountpoint, nil, nil, ready, false))
 	}()
 
 	<-ready
