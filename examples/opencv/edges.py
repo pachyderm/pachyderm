8474070ea3c4f79c4c2ae03e02f884b25b1c29ce
Joe Doliner <jdoliner@gmail.com>
2016-08-18 18:01:30 -0700

    Extends README and adds more comments.

--- examples/opencv/edges.py
+++ examples/opencv/edges.py
@@ -3,11 +3,15 @@ import numpy as np
 from matplotlib import pyplot as plt
 import os
  
+# make_edges reads an image from /pfs/images and outputs the result of running
+# edge detection on that image to /pfs/out. Note that /pfs/images and
+# /pfs/out are special directories that Pachyderm injects into the container.
 def make_edges(image):
     img = cv2.imread('/pfs/images/' + image)
     edges = cv2.Canny(img,100,200)
     plt.imsave("/pfs/out/" + os.path.splitext(image)[0]+'.png', edges, cmap = 'gray')
 
+# walk /pfs/images and call make_edges on every file found
 for subdir, dirs, files in os.walk("/pfs/images"):
     for file in files:
         make_edges(file)
