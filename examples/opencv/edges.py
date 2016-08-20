ccc291f44bb451971e18d64fe430acc7401ef87a
Joe Doliner <jdoliner@gmail.com>
2016-08-19 17:55:32 -0700

    Update edges.py to work with nested files.

--- examples/opencv/edges.py
+++ examples/opencv/edges.py
@@ -7,11 +7,14 @@ import os
 # edge detection on that image to /pfs/out. Note that /pfs/images and
 # /pfs/out are special directories that Pachyderm injects into the container.
 def make_edges(image):
-    img = cv2.imread('/pfs/images/' + image)
+    print(image)
+    img = cv2.imread(image)
+    tail = os.path.split(image)[1]
     edges = cv2.Canny(img,100,200)
-    plt.imsave("/pfs/out/" + os.path.splitext(image)[0]+'.png', edges, cmap = 'gray')
+    plt.imsave(os.path.join("/pfs/out", os.path.splitext(tail)[0]+'.png'), edges, cmap = 'gray')
 
 # walk /pfs/images and call make_edges on every file found
-for subdir, dirs, files in os.walk("/pfs/images"):
+for dirpath, dirs, files in os.walk("/pfs/images"):
     for file in files:
-        make_edges(file)
+        print("image: ", os.path.join(dirpath, file))
+        make_edges(os.path.join(dirpath, file))
