f9d64bbd7fef7e7a170d131cb2f4bf5c525cc221
Joe Doliner <jdoliner@gmail.com>
2016-08-25 12:45:18 -0700

    More work on benchmarks.
    It now runs successfully and returns meaningful results.

--- src/server/pachyderm_bench_test.go
+++ src/server/pachyderm_bench_test.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"fmt"
-	"io/ioutil"
 	"math/rand"
 	"testing"
 	"time"
@@ -17,41 +16,64 @@ import (
 const (
 	nCommits = 10
 	nFiles   = 10
-	MB       = 1024 * 1024 * 1024
+	MB       = 1024 * 1024
 )
 
+type CountWriter struct {
+	count int64
+}
+
+func (w *CountWriter) Write(p []byte) (int, error) {
+	w.count += int64(len(p))
+	return len(p), nil
+}
+
 func BenchmarkPutGetFile(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		repo := uniqueString("BenchmarkPutGetFile")
-		c, err := client.NewInCluster()
-		require.NoError(b, err)
-		require.NoError(b, c.CreateRepo(repo))
-		var commits []*pfsclient.Commit
-		for j := 0; j < nCommits; j++ {
-			commit, err := c.StartCommit(repo, "", "master")
-			commits = append(commits, commit)
-			require.NoError(b, err)
+	repo := uniqueString("BenchmarkPutGetFile")
+	c, err := client.NewInCluster()
+	require.NoError(b, err)
+	require.NoError(b, c.CreateRepo(repo))
+	var commits []*pfsclient.Commit
+	if !b.Run("PutFile", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			for j := 0; j < nCommits; j++ {
+				commit, err := c.StartCommit(repo, "", "master")
+				commits = append(commits, commit)
+				require.NoError(b, err)
+				var eg errgroup.Group
+				for k := 0; k < nFiles; k++ {
+					k := k
+					eg.Go(func() error {
+						rand := rand.New(rand.NewSource(int64(time.Now().UnixNano())))
+						_, err := c.PutFile(repo, "master", fmt.Sprintf("file%d", k), workload.NewReader(rand, MB))
+						b.SetBytes(MB)
+						return err
+					})
+				}
+				require.NoError(b, eg.Wait())
+				require.NoError(b, c.FinishCommit(repo, "master"))
+			}
+		}
+	}) {
+		return
+	}
+	if !b.Run("GetFile", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
 			var eg errgroup.Group
-			for k := 0; k < nFiles; k++ {
-				k := k
-				eg.Go(func() error {
-					rand := rand.New(rand.NewSource(int64(time.Now().UnixNano())))
-					_, err := c.PutFile(repo, "master", fmt.Sprintf("file%d", k), workload.NewReader(rand, MB))
-					return err
-				})
+			for _, commit := range commits {
+				commit := commit
+				for k := 0; k < nFiles; k++ {
+					k := k
+					eg.Go(func() error {
+						w := &CountWriter{}
+						defer func() { b.SetBytes(w.count) }()
+						return c.GetFile(repo, commit.ID, fmt.Sprintf("file%d", k), 0, 0, "", false, nil, w)
+					})
+				}
 			}
 			require.NoError(b, eg.Wait())
 		}
-		var eg errgroup.Group
-		for _, commit := range commits {
-			commit := commit
-			for k := 0; k < nFiles; k++ {
-				k := k
-				eg.Go(func() error {
-					return c.GetFile(repo, commit.ID, fmt.Sprintf("file%d", k), 0, 0, "", false, nil, ioutil.Discard)
-				})
-			}
-		}
-		require.NoError(b, eg.Wait())
+	}) {
+		return
 	}
 }
