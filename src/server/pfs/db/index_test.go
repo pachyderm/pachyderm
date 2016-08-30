e8b89a86f7213f4e1787bb15258344600e85ad1f
Derek Chiang <derekchiang93@gmail.com>
2016-08-30 14:57:55 -0700

    Continue cleaning up index tests; add more tests

--- src/server/pfs/db/index_test.go
+++ src/server/pfs/db/index_test.go
@@ -12,46 +12,61 @@ import (
 )
 
 var (
-	port int32 = 40651
+	RethinkAddress = "localhost:28015"
+	port           = 40651
 )
 
-func TestDiffPrefixIndex(t *testing.T) {
-	testSetup(t, func(dbClient *gorethink.Session) {
-		cursor, err := gorethink.Expr(DiffPrefixIndex.CreateFunction(gorethink.Expr(&persist.Diff{
-			Repo: "repo",
-			Path: "/foo/bar/fizz/buzz",
-			Clock: &persist.Clock{
-				Branch: "branch",
-				Clock:  1,
-			},
-		}))).Run(dbClient)
-		require.NoError(t, err)
-		keys := []interface{}{}
-		expectedPrefixes := []interface{}{
-			"/",
-			"/foo",
-			"/foo/bar",
-			"/foo/bar/fizz",
-		}
-		require.NoError(t, cursor.All(&keys))
-		fmt.Printf("keys: %v", keys)
-		for i, key := range keys {
-			// This is kinda hacky, but we have to compare the string
-			// representations of these two values because DeepEqual doesn't
-			// work when the underlying types are different.  In this case,
-			// key is an interface{} and the expected value is an []interface{},
-			// even though their underlying values are equal.
-			require.Equal(t, fmt.Sprintf("%v", []interface{}{"repo", expectedPrefixes[i], []interface{}{"branch", 1}}), fmt.Sprintf("%v", key))
-		}
-	})
+func TestDiffPathIndex(t *testing.T) {
+	dbClient := getClient(t)
+	path := "/foo/bar/fizz/buzz"
+	cursor, err := gorethink.Expr(DiffPathIndex.CreateFunction(gorethink.Expr(&persist.Diff{
+		Repo: "repo",
+		Path: path,
+		Clock: &persist.Clock{
+			Branch: "branch",
+			Clock:  1,
+		},
+	}))).Run(dbClient)
+	require.NoError(t, err)
+	var key []interface{}
+	require.NoError(t, cursor.All(&key))
+	require.Equal(t, fmt.Sprintf("%v", []interface{}{"repo", path, []interface{}{"branch", 1}}), fmt.Sprintf("%v", key))
 }
 
-func testSetup(t *testing.T, testCode func(*gorethink.Session)) {
+func TestDiffPrefixIndex(t *testing.T) {
+	dbClient := getClient(t)
+	cursor, err := gorethink.Expr(DiffPrefixIndex.CreateFunction(gorethink.Expr(&persist.Diff{
+		Repo: "repo",
+		Path: "/foo/bar/fizz/buzz",
+		Clock: &persist.Clock{
+			Branch: "branch",
+			Clock:  1,
+		},
+	}))).Run(dbClient)
+	require.NoError(t, err)
+	keys := []interface{}{}
+	expectedPrefixes := []interface{}{
+		"/",
+		"/foo",
+		"/foo/bar",
+		"/foo/bar/fizz",
+	}
+	require.NoError(t, cursor.All(&keys))
+	for i, key := range keys {
+		// This is kinda hacky, but we have to compare the string
+		// representations of these two values because DeepEqual doesn't
+		// work when the underlying types are different.  In this case,
+		// key is an interface{} and the expected value is an []interface{},
+		// even though their underlying values are equal.
+		require.Equal(t, fmt.Sprintf("%v", []interface{}{"repo", expectedPrefixes[i], []interface{}{"branch", 1}}), fmt.Sprintf("%v", key))
+	}
+}
+
+func getClient(t *testing.T) *gorethink.Session {
 	dbClient, err := gorethink.Connect(gorethink.ConnectOpts{
 		Address: RethinkAddress,
 		Timeout: connectTimeoutSeconds * time.Second,
 	})
 	require.NoError(t, err)
-
-	testCode(dbClient)
+	return dbClient
 }
