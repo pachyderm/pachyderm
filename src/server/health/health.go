c0c7f5327bbfa502f8eabca77030c92c03e80c75
Joe Doliner <jdoliner@gmail.com>
2016-09-11 01:58:04 -0700

    Adds a healthcheck api.

---
+++ src/server/health/health.go
@@ -0,0 +1,17 @@
+package health
+
+import (
+	"github.com/pachyderm/pachyderm/src/client/health"
+	"go.pedge.io/pb/go/google/protobuf"
+	"golang.org/x/net/context"
+)
+
+func NewHealthServer() health.HealthServer {
+	return &healthServer{}
+}
+
+type healthServer struct{}
+
+func (*healthServer) Health(context.Context, *google_protobuf.Empty) (*google_protobuf.Empty, error) {
+	return google_protobuf.EmptyInstance, nil
+}
