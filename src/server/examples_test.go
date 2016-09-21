c59c7c57cce2a8563017926250e4a6445393338b
Sean Jezewski <seanwjezewski@gmail.com>
2016-09-21 09:12:30 -0700

    Use client instead of CLI for this part of the test

--- src/server/examples_test.go
+++ src/server/examples_test.go
@@ -5,7 +5,6 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"strings"
 	"testing"
 
 	"github.com/pachyderm/pachyderm/src/client"
@@ -18,47 +17,23 @@ func TestTensorFlow(t *testing.T) {
 		t.Skip("Skipping integration tests in short mode")
 	}
 
+	c := getPachClient(t)
+
 	cwd, err := os.Getwd()
 	require.NoError(t, err)
 	exampleDir := filepath.Join(cwd, "../../examples/tensor_flow")
 	cmd := exec.Command("make", "all")
 	cmd.Dir = exampleDir
-	raw, err := cmd.CombinedOutput()
+	_, err = cmd.CombinedOutput()
 	require.NoError(t, err)
 
-	cmd = exec.Command("pachctl", "list-commit", "GoT_scripts")
-	cmd.Dir = exampleDir
-	raw, err = cmd.CombinedOutput()
-	lines := strings.Split(string(raw), "\n")
-	require.Equal(t, 3, len(lines))
-
-	getSecondField := func(line string) string {
-		tokens := strings.Split(line, " ")
-		seenField := 0
-		for _, token := range tokens {
-			if token != "" {
-				seenField += 1
-				if seenField == 2 {
-					return token
-				}
-			}
-		}
-		return ""
-	}
-
-	// Make sure the second field is `ID`
-	// Example stdout we're parsing:
-	//
-	// BRANCH                             ID                                 PARENT              STARTED             FINISHED            SIZE
-	// c1001a97c0cc4bea825ee50cc613e039   5fc5a07edd094432acf474662ad02854   <none>              About an hour ago   About an hour ago   2.625 MiB
-
-	require.Equal(t, "ID", getSecondField(lines[0]))
-	inputCommitID := getSecondField(lines[1])
-	require.NotEqual(t, "", inputCommitID)
+	commitInfos, err := c.ListCommit([]string{"GoT_scripts"}, nil, client.CommitTypeRead, false, client.CommitStatusAll, nil)
+	require.NoError(t, err)
+	require.Equal(t, 1, len(commitInfos))
+	inputCommitID := commitInfos[0].Commit.ID
 
 	// Wait until the GoT_generate job has finished
-	c := getPachClient(t)
-	commitInfos, err := c.FlushCommit([]*pfsclient.Commit{client.NewCommit("GoT_scripts", inputCommitID)}, nil)
+	commitInfos, err = c.FlushCommit([]*pfsclient.Commit{client.NewCommit("GoT_scripts", inputCommitID)}, nil)
 	require.NoError(t, err)
 	require.Equal(t, 2, len(commitInfos))
 
