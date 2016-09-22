4cd383e74f705072779bd6a9ab9f43b088ec02bf
Sean Jezewski <seanwjezewski@gmail.com>
2016-09-22 15:10:29 -0700

    Finish out fruit stand test

--- src/server/examples/examples_test.go
+++ src/server/examples/examples_test.go
@@ -2,11 +2,13 @@ package examples
 
 import (
 	"bytes"
-	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strconv"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/pachyderm/pachyderm/src/client"
 	pfsclient "github.com/pachyderm/pachyderm/src/client/pfs"
@@ -45,7 +47,6 @@ func TestExampleTensorFlow(t *testing.T) {
 		false,
 	)
 	require.NoError(t, err)
-	fmt.Printf("commits: %v\n", commitInfos)
 	require.Equal(t, 1, len(commitInfos))
 	inputCommitID := commitInfos[0].Commit.ID
 
@@ -84,7 +85,7 @@ func TestFruitStand(t *testing.T) {
 	t.Parallel()
 
 	require.NoError(t, c.CreateRepo("data"))
-	repoInfos, err := c.ListRepo()
+	repoInfos, err := c.ListRepo(nil)
 	require.NoError(t, err)
 	var repoNames []interface{}
 	for _, repoInfo := range repoInfos {
@@ -92,6 +93,134 @@ func TestFruitStand(t *testing.T) {
 	}
 	require.OneOfEquals(t, "data", repoNames)
 
-	require.NoError(t, c.DeleteRepo("data"))
+	cmd := exec.Command(
+		"pachctl",
+		"put-file",
+		"data",
+		"master",
+		"sales",
+		"-c",
+		"-f",
+		"https://raw.githubusercontent.com/pachyderm/pachyderm/master/doc/examples/fruit_stand/set1.txt",
+	)
+	_, err = cmd.CombinedOutput()
+	require.NoError(t, err)
 
+	commitInfos, err := c.ListCommit(
+		[]*pfsclient.Commit{{
+			Repo: &pfsclient.Repo{"data"},
+		}},
+		nil,
+		client.CommitTypeRead,
+		client.CommitStatusNormal,
+		false,
+	)
+	require.Equal(t, 1, len(commitInfos))
+	commit := commitInfos[0].Commit
+
+	var buffer bytes.Buffer
+	require.NoError(t, c.GetFile("data", commit.ID, "sales", 0, 0, "", false, nil, &buffer))
+	lines := strings.Split(buffer.String(), "\n")
+	if len(lines) < 100 {
+		t.Fatalf("Sales file has too few lines (%v)\n", len(lines))
+	}
+
+	cmd = exec.Command(
+		"pachctl",
+		"create-pipeline",
+		"-f",
+		"https://raw.githubusercontent.com/pachyderm/pachyderm/master/doc/examples/fruit_stand/pipeline.json",
+	)
+	_, err = cmd.CombinedOutput()
+	require.NoError(t, err)
+
+	time.Sleep(5)
+	repoInfos, err = c.ListRepo(nil)
+	require.NoError(t, err)
+	repoNames = []interface{}{"sum", "filter", "data"}
+	for _, repoInfo := range repoInfos {
+		require.EqualOneOf(t, repoNames, repoInfo.Repo.Name)
+	}
+
+	commitInfos, err = c.FlushCommit([]*pfsclient.Commit{client.NewCommit("data", commit.ID)}, nil)
+	require.NoError(t, err)
+	require.Equal(t, 3, len(commitInfos))
+
+	commitInfos, err = c.ListCommit(
+		[]*pfsclient.Commit{{
+			Repo: &pfsclient.Repo{"sum"},
+		}},
+		nil,
+		client.CommitTypeRead,
+		client.CommitStatusNormal,
+		false,
+	)
+	require.NoError(t, err)
+	require.Equal(t, 1, len(commitInfos))
+
+	buffer.Reset()
+	require.NoError(t, c.GetFile("sum", commitInfos[0].Commit.ID, "apple", 0, 0, "", false, nil, &buffer))
+	require.NotNil(t, buffer)
+	firstCount, err := strconv.ParseInt(strings.TrimRight(buffer.String(), "\n"), 10, 0)
+	require.NoError(t, err)
+	if firstCount < 100 {
+		t.Fatalf("Wrong sum for apple (%i) ... too low\n", firstCount)
+	}
+
+	// Add more data to input
+
+	cmd = exec.Command(
+		"pachctl",
+		"put-file",
+		"data",
+		"master",
+		"sales",
+		"-c",
+		"-f",
+		"https://raw.githubusercontent.com/pachyderm/pachyderm/master/examples/fruit_stand/set2.txt",
+	)
+	_, err = cmd.Output()
+	require.NoError(t, err)
+
+	// Flush commit!
+	commitInfos, err = c.ListCommit(
+		[]*pfsclient.Commit{{
+			Repo: &pfsclient.Repo{"data"},
+		}},
+		nil,
+		client.CommitTypeRead,
+		client.CommitStatusAll,
+		false,
+	)
+	require.NoError(t, err)
+	require.Equal(t, 2, len(commitInfos))
+
+	commitInfos, err = c.FlushCommit([]*pfsclient.Commit{client.NewCommit("data", commitInfos[1].Commit.ID)}, nil)
+	require.NoError(t, err)
+	require.Equal(t, 3, len(commitInfos))
+
+	commitInfos, err = c.ListCommit(
+		[]*pfsclient.Commit{{
+			Repo: &pfsclient.Repo{"sum"},
+		}},
+		nil,
+		client.CommitTypeRead,
+		client.CommitStatusNormal,
+		false,
+	)
+	require.NoError(t, err)
+	require.Equal(t, 2, len(commitInfos))
+
+	buffer.Reset()
+	require.NoError(t, c.GetFile("sum", commitInfos[1].Commit.ID, "apple", 0, 0, "", false, nil, &buffer))
+	require.NotNil(t, buffer)
+	secondCount, err := strconv.ParseInt(strings.TrimRight(buffer.String(), "\n"), 10, 0)
+	require.NoError(t, err)
+	if firstCount > secondCount {
+		t.Fatalf("Second sum (%v) is smaller than first (%v)\n", secondCount, firstCount)
+	}
+
+	require.NoError(t, c.DeleteRepo("sum", false))
+	require.NoError(t, c.DeleteRepo("filter", false))
+	require.NoError(t, c.DeleteRepo("data", false))
 }
