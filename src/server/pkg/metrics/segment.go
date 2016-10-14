f9865c30c03be7daf3a5dc89ecc9b879027180b4
Sean Jezewski <seanwjezewski@gmail.com>
2016-10-13 17:49:02 -0700

    Report user actions as events and identify users when we first see their ID

--- src/server/pkg/metrics/segment.go
+++ src/server/pkg/metrics/segment.go
@@ -5,8 +5,9 @@ import (
 	"go.pedge.io/lion"
 )
 
-func reportSegment(metrics *Metrics) {
-	client := analytics.New("hhxbyr7x50w3jtgcwcZUyOFrTf4VNMrD")
+var client = analytics.New("hhxbyr7x50w3jtgcwcZUyOFrTf4VNMrD")
+
+func reportClusterMetricsToSegment(metrics *Metrics) {
 	err := client.Track(&analytics.Track{
 		Event:       "metrics",
 		AnonymousId: metrics.ID,
@@ -23,6 +24,28 @@ func reportSegment(metrics *Metrics) {
 		},
 	})
 	if err != nil {
-		lion.Errorf("error reporting to Segment: %s", err.Error())
+		lion.Errorf("error reporting cluster metrics to Segment: %s", err.Error())
+	}
+}
+
+func identifyNewUser(userID string) {
+	err := client.Identify(&analytics.Identify{
+		UserId: userID,
+	})
+	if err != nil {
+		lion.Errorf("error reporting user identity to Segment: %s", err.Error())
+	}
+}
+
+func reportUserMetricsToSegment(userActions *UserActions) {
+	for userID, actions := range userActions {
+		err := client.Track(&analytics.Track{
+			Event:      "Usage",
+			UserId:     userID,
+			Properties: actions,
+		})
+		if err != nil {
+			lion.Errorf("error reporting user action to Segment: %s", err.Error())
+		}
 	}
 }
