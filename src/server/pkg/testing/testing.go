d9f565c8c6f969e7726294c4468a2daaa87ced42
Joe Doliner <jdoliner@gmail.com>
2016-11-02 16:27:50 -0700

    Adds state matching for commits.

--- src/server/pkg/testing/testing.go
+++ src/server/pkg/testing/testing.go
@@ -1,6 +1,7 @@
 package testing
 
 import (
+	"fmt"
 	"sort"
 	"testing"
 
@@ -41,6 +42,13 @@ func MatchState(state *State, c *client.APIClient, t *testing.T) {
 		repoInfo, err := c.InspectRepo(repoState.Info.Repo.Name)
 		require.NoError(t, err)
 		matchRepoState(repoState, repoInfo, t)
+		commitInfos, err := c.ListCommit([]*pfs.Commit{client.NewCommit(repoState.Info.Repo.Name, "")},
+			nil, client.CommitTypeNone, client.CommitStatusAll, false)
+		require.NoError(t, err)
+		require.Equal(t, len(repoState.Commits), len(commitInfos))
+		for i, commitState := range repoState.Commits {
+			matchCommitState(commitState, commitInfos[i], t)
+		}
 	}
 }
 
@@ -66,6 +74,46 @@ func matchRepoProvenance(x []*pfs.Repo, y []*pfs.Repo, t *testing.T) {
 	}
 	for _, repo := range y {
 		ys = append(ys, repo.Name)
+	}
+	sort.Strings(xs)
+	sort.Strings(ys)
+	require.Equal(t, xs, ys)
+}
+
+func matchCommitState(commitState *CommitState, commitInfo *pfs.CommitInfo, t *testing.T) {
+	require.Equal(t, commitState.Info.Commit, commitInfo.Commit)
+	if commitState.Info.Branch != "" {
+		require.Equal(t, commitState.Info.Branch, commitInfo.Commit)
+	}
+	if commitState.Info.CommitType != client.CommitTypeNone {
+		require.Equal(t, commitState.Info.CommitType, commitInfo.CommitType)
+	}
+	if commitState.Info.ParentCommit != nil {
+		require.Equal(t, commitState.Info.ParentCommit, commitInfo.ParentCommit)
+	}
+	if commitState.Info.Started != nil {
+		require.Equal(t, commitState.Info.Started, commitInfo.Started)
+	}
+	if commitState.Info.Finished != nil {
+		require.Equal(t, commitState.Info.Finished, commitInfo.Finished)
+	}
+	if commitState.Info.SizeBytes != 0 {
+		require.Equal(t, commitState.Info.SizeBytes, commitInfo.SizeBytes)
+	}
+	if commitState.Info.Provenance != nil {
+		matchCommitProvenance(commitState.Info.Provenance, commitInfo.Provenance, t)
+	}
+}
+
+func matchCommitProvenance(x []*pfs.Commit, y []*pfs.Commit, t *testing.T) {
+	require.Equal(t, len(x), len(y))
+	var xs []string
+	var ys []string
+	for _, commit := range x {
+		xs = append(xs, fmt.Sprintf("%s/%s", commit.Repo.Name, commit.ID))
+	}
+	for _, commit := range y {
+		ys = append(ys, fmt.Sprintf("%s/%s", commit.Repo.Name, commit.ID))
 	}
 	sort.Strings(xs)
 	sort.Strings(ys)
