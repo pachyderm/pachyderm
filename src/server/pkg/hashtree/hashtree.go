ebc6aca9bacc8908902b606abce534a7294d9e5e
Derek Chiang <derekchiang93@gmail.com>
2017-01-04 16:47:37 -0800

    Implement GlobFile and add a test

--- src/server/pkg/hashtree/hashtree.go
+++ src/server/pkg/hashtree/hashtree.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"path"
 	"sort"
+	"strings"
 )
 
 // List all files that are direct children of the directory 'dir'
@@ -88,4 +89,27 @@ func (h *HashTree) PutFile(path string, hash []byte) error {
 
 	// Update the hash values of all directories in 'path'
 	updateHashes(path)
+}
+
+func (h *HashTree) GlobFile(pattern string) ([]*Node, error) {
+	// the pattern should always start with a slash because that's how our
+	// paths are.
+	if !strings.HasPrefix(pattern, "/") {
+		pattern = "/" + pattern
+	}
+
+	var res []*Node
+	for p, node := range h.Fs {
+		matched, err := path.Match(pattern, p)
+		if err != nil {
+			if err == path.ErrBadPattern {
+				return nil, MalformedGlobErr
+			}
+			return nil, err
+		}
+		if matched {
+			res = append(res, node)
+		}
+	}
+	return res, nil
 }
