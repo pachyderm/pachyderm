c9d7068c2459ea9bf54d316d448758f4b4e57472
Derek Chiang <derekchiang93@gmail.com>
2017-01-13 02:20:40 -0800

    make the List API nicer

--- src/server/pfs/drive/driver.go
+++ src/server/pfs/drive/driver.go
@@ -139,9 +139,20 @@ func (d *driver) ListRepo(ctx context.Context, provenance []*pfs.Repo) ([]*pfs.R
 	}
 
 	var result []*pfs.RepoInfo
-	var key string
-	repoInfo := &pfs.RepoInfo{}
-	if err := d.repos(ctx).List(&key, repoInfo, func() error {
+	iterate, err := d.repos(ctx).List()
+	if err != nil {
+		return nil, err
+	}
+nextRepo:
+	for {
+		repoName, repoInfo := "", pfs.RepoInfo{}
+		ok, err := iterate(&repoName, &repoInfo)
+		if err != nil {
+			return nil, err
+		}
+		if !ok {
+			break
+		}
 		for _, reqProv := range provenance {
 			var matched bool
 			for _, prov := range repoInfo.Provenance {
@@ -150,15 +161,10 @@ func (d *driver) ListRepo(ctx context.Context, provenance []*pfs.Repo) ([]*pfs.R
 				}
 			}
 			if !matched {
-				return nil
+				continue nextRepo
 			}
 		}
-		copy := &pfs.RepoInfo{}
-		*copy = *repoInfo
-		result = append(result, copy)
-		return nil
-	}); err != nil {
-		return nil, err
+		result = append(result, &repoInfo)
 	}
 	return result, nil
 }
@@ -172,18 +178,25 @@ func (d *driver) DeleteRepo(ctx context.Context, repo *pfs.Repo, force bool) err
 	defer repos.Unlock()
 
 	// Check if this repo is the provenance of some other repos
+	iterate, err := repos.List()
+	if err != nil {
+		return err
+	}
 	if !force {
-		var key string
-		repoInfo := &pfs.RepoInfo{}
-		if err := repos.List(&key, repoInfo, func() error {
+		for {
+			key, repoInfo := "", pfs.RepoInfo{}
+			ok, err := iterate(&key, &repoInfo)
+			if err != nil {
+				return err
+			}
+			if !ok {
+				break
+			}
 			for _, prov := range repoInfo.Provenance {
 				if prov.Name == repo.Name {
-					return fmt.Errorf("repo %s is the provenance of repo %s", repo.Name, prov.Name)
+					return fmt.Errorf("cannot delete repo %s because it's the provenance of repo %s", repo.Name, prov.Name)
 				}
 			}
-			return nil
-		}); err != nil {
-			return err
 		}
 	}
 
@@ -208,19 +221,23 @@ func (d *driver) StartCommit(ctx context.Context, parent *pfs.Commit, provenance
 		ID:   uuid.NewWithoutDashes(),
 	}
 	_, err := concurrency.NewSTMRepeatable(ctx, d.etcdClient, func(stm concurrency.STM) error {
+		repos := d.repos(ctx).stm(stm)
+		commits := d.commits(ctx)(parent.Repo.Name).stm(stm)
+		refs := d.refs(ctx)(parent.Repo.Name).stm(stm)
+
+		// Check if repo exists
 		repoInfo := &pfs.RepoInfo{}
-		if err := d.repos(ctx).stm(stm).Get(parent.Repo.Name, repoInfo); err != nil {
+		if err := repos.Get(parent.Repo.Name, repoInfo); err != nil {
 			return err
 		}
+
 		commitInfo := &pfs.CommitInfo{
 			Commit:     commit,
 			Started:    now(),
 			Provenance: provenance,
 		}
-		commits := d.commits(ctx)(parent.Repo.Name).stm(stm)
-		refs := d.refs(ctx)(parent.Repo.Name).stm(stm)
 		ref := &pfs.Ref{}
-		// See if the ref exists
+		// See if we are given a ref
 		if err := refs.Get(parent.ID, ref); err != nil {
 			if _, ok := err.(ErrNotFound); !ok {
 				return err
