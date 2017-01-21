9d6288a407b05e2538adea54c7377faa69170d99
Joe Doliner <jdoliner@gmail.com>
2017-01-20 18:28:58 -0800

    Adds a test for tags.

--- src/server/pfs/server/block_api_server_test.go
+++ src/server/pfs/server/block_api_server_test.go
@@ -16,6 +16,18 @@ func TestPutGet(t *testing.T) {
 	require.Equal(t, []byte("foo"), value)
 }
 
+func TestTags(t *testing.T) {
+	c := getPachClient(t)
+	_, err := c.PutObject([]byte("foo"), "bar", "buzz")
+	require.NoError(t, err)
+	value, err := c.GetTag("bar")
+	require.NoError(t, err)
+	require.Equal(t, []byte("foo"), value)
+	value, err = c.GetTag("buzz")
+	require.NoError(t, err)
+	require.Equal(t, []byte("foo"), value)
+}
+
 func getPachClient(t testing.TB) *client.APIClient {
 	client, err := client.NewFromAddress("0.0.0.0:30650")
 	require.NoError(t, err)
