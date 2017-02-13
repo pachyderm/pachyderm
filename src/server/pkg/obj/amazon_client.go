74d868b75f57e08c5063d32b83bf8164a6c8e88c
Joe Doliner <jdoliner@gmail.com>
2017-02-13 15:54:21 -0800

    aws-sdk-go import changed.

--- src/server/pkg/obj/amazon_client.go
+++ src/server/pkg/obj/amazon_client.go
@@ -9,7 +9,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3Manager"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/aws/aws-sdk-go/service/storagegateway"
 )
 
