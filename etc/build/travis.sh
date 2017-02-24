80c02af8e64332b64a73978852a2e9c846f11971
Sean Jezewski <seanwjezewski@gmail.com>
2017-02-24 13:05:11 -0800

    Add travis install steps for benchmarks

--- etc/build/travis.sh
+++ etc/build/travis.sh
@@ -3,7 +3,12 @@
 # Check the cron value to see if this is a daily job
 if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
 	echo "Running daily benchmarks"
-	make bench
+    # Use the secrets in the travis environment to setup the aws creds for the aws command:
+    echo "${AWS_ACCESS_KEY_ID}
+${AWS_SECRET_ACCESS_KEY}
+
+" | aws configure
+	sudo -E make bench
 else
 	echo "Running tests"
 	make test
