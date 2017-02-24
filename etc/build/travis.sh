ec6ee9161ebcbb31a86d318c8c033f99da1749c7
Sean Jezewski <seanwjezewski@gmail.com>
2017-02-24 13:45:40 -0800

    Run benchmarks on travis for now for testing

--- etc/build/travis.sh
+++ etc/build/travis.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # Check the cron value to see if this is a daily job
+
+# for now always run bench since thats what I'm trying to test
+TRAVIS_EVENT_TYPE=cron
+
 if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
 	echo "Running daily benchmarks"
     # Use the secrets in the travis environment to setup the aws creds for the aws command:
