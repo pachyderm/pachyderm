5b1ed6ad72690d92f1f949c8f7c2dead995f2f76
Derek Chiang <derekchiang93@gmail.com>
2017-02-23 16:42:32 -0800

    Make worker download data properly

--- src/server/pkg/worker/api_server.go
+++ src/server/pkg/worker/api_server.go
@@ -2,9 +2,11 @@ package worker
 
 import (
 	"fmt"
-	"path"
+	"path/filepath"
 	"sync"
+	"time"
 
+	"go.pedge.io/proto/rpclog"
 	"golang.org/x/net/context"
 
 	etcd "github.com/coreos/etcd/clientv3"
@@ -16,6 +18,7 @@ import (
 
 type APIServer struct {
 	sync.Mutex
+	protorpclog.Logger
 	pachClient   *client.APIClient
 	etcdClient   *etcd.Client
 	pipelineInfo *pps.PipelineInfo
@@ -23,6 +26,8 @@ type APIServer struct {
 
 func NewAPIServer(pachClient *client.APIClient, etcdClient *etcd.Client, pipelineInfo *pps.PipelineInfo) *APIServer {
 	return &APIServer{
+		Mutex:        sync.Mutex{},
+		Logger:       protorpclog.NewLogger(""),
 		pachClient:   pachClient,
 		etcdClient:   etcdClient,
 		pipelineInfo: pipelineInfo,
@@ -30,21 +35,24 @@ func NewAPIServer(pachClient *client.APIClient, etcdClient *etcd.Client, pipelin
 }
 
 func (a *APIServer) downloadData(ctx context.Context, data []*pfs.FileInfo) error {
+	fmt.Println("BP0")
 	for i, datum := range data {
 		input := a.pipelineInfo.Inputs[i]
-		if err := filesync.Pull(ctx, a.pachClient, path.Join(client.PPSInputPrefix, input.Name), datum, input.Lazy); err != nil {
+		fmt.Println("BP1")
+		if err := filesync.Pull(ctx, a.pachClient, filepath.Join(client.PPSInputPrefix, input.Name), datum, input.Lazy); err != nil {
 			return err
 		}
+		fmt.Println("BP2")
 	}
 	return nil
 }
 
-func (a *APIServer) Process(ctx context.Context, req *ProcessRequest) (*ProcessResponse, error) {
+func (a *APIServer) Process(ctx context.Context, req *ProcessRequest) (resp *ProcessResponse, retErr error) {
+	defer func(start time.Time) { a.Log(req, resp, retErr, time.Since(start)) }(time.Now())
 	a.Lock()
 	defer a.Unlock()
-	fmt.Printf("processing: %v\n", req)
 	if err := a.downloadData(ctx, req.Data); err != nil {
 		return nil, err
 	}
-	return nil, nil
+	return &ProcessResponse{}, nil
 }
