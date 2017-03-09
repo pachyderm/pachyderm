c43e4fc7a35378e3b6e2bd34af58a21850dc6736
Sean Jezewski <seanwjezewski@gmail.com>
2017-03-08 16:14:06 -0800

    Generate new ssh key for travis

--- etc/build/travis.sh
+++ etc/build/travis.sh
@@ -15,6 +15,19 @@ ${AWS_SECRET_ACCESS_KEY}
     make install
     echo "pachctl is installed here:"
     which pachctl
+    # Travis doesn't come w an ssh key
+    # kops needs one in place (because it enables ssh access to nodes w it)
+    # for now we'll just generate one on the fly
+    # travis supports adding a persistent one if we pay: https://docs.travis-ci.com/user/private-dependencies/#Generating-a-new-key
+
+    echo "
+
+
+" | ssh-keygen -t rsa -b 4096 -C "buildbot@pachyderm.io"
+    echo "generated ssh keys:"
+    ls ~/.ssh
+    cat ~/.ssh/id_rsa.pub
+
 	sudo -E make bench
 else
 	echo "Running tests"
