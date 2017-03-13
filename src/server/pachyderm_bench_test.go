c1443e1a2e1e50b4ec6a92060366f85f81f40c1e
Derek Chiang <derekchiang93@gmail.com>
2017-03-13 12:26:09 -0700

    Support generating different file sizes using a normal distribution; add a benchmark for putting large files

--- src/server/pachyderm_bench_test.go
+++ src/server/pachyderm_bench_test.go
@@ -1,11 +1,10 @@
 package server
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"math/rand"
 	"path"
+	"sync/atomic"
 	"testing"
 
 	"github.com/pachyderm/pachyderm/src/client"
@@ -13,100 +12,145 @@ import (
 	"github.com/pachyderm/pachyderm/src/client/pkg/require"
 	"github.com/pachyderm/pachyderm/src/client/pkg/uuid"
 	ppsclient "github.com/pachyderm/pachyderm/src/client/pps"
+	"github.com/pachyderm/pachyderm/src/server/pkg/workload"
 	"golang.org/x/sync/errgroup"
 )
 
 const (
 	KB               = 1024
-	MB               = 1024 * 1024
+	MB               = 1024 * KB
+	GB               = 1024 * MB
 	lettersAndSpaces = "abcdefghijklmnopqrstuvwxyz      "
 )
 
-type CountWriter struct {
+// countWriter increments a counter by the number of bytes given
+type countWriter struct {
 	count int64
 }
 
-func (w *CountWriter) Write(p []byte) (int, error) {
-	w.count += int64(len(p))
+func (w *countWriter) Write(p []byte) (int, error) {
+	atomic.AddInt64(&w.count, int64(len(p)))
 	return len(p), nil
 }
 
-func randBytes(size int) io.Reader {
-	buf := make([]byte, size)
-	rand.Read(buf)
-	return bytes.NewReader(buf)
+// Distribution returns numbers that conform to some statistical distribution
+type distribution interface {
+	Get() int64
 }
 
-func BenchmarkPachyderm(b *testing.B) {
+type uniformDistribution struct {
+	size int64
+}
+
+func (u *uniformDistribution) Get() int64 {
+	return u.size
+}
+
+type normalDistribution struct {
+	rand   *rand.Rand
+	stdDev float64
+	mean   float64
+}
+
+func (n *normalDistribution) Get() int64 {
+	return int64(rand.NormFloat64()*n.stdDev + n.mean)
+}
+
+func getRand(b *testing.B) *rand.Rand {
+	source := rand.Int63()
+	// Log the source so we can reproduce results
+	b.Logf("Using rand source: %d", source)
+	return rand.New(rand.NewSource(source))
+}
+
+func BenchmarkManySmallFiles(b *testing.B) {
+	r := getRand(b)
+	benchmarkFiles(b, 1000000, r, &normalDistribution{
+		rand:   r,
+		stdDev: 500,
+		mean:   1 * KB,
+	})
+}
+
+func BenchmarkSomeLargeFiles(b *testing.B) {
+	r := getRand(b)
+	benchmarkFiles(b, 1000, r, &normalDistribution{
+		rand:   r,
+		stdDev: 500 * MB,
+		mean:   1 * GB,
+	})
+}
+
+func benchmarkFiles(b *testing.B, fileNum int, r *rand.Rand, d distribution) {
 	repo := "BenchmarkPachyderm" + uuid.NewWithoutDashes()[0:12]
 	c, err := client.NewFromAddress("127.0.0.1:30650")
 	require.NoError(b, err)
 	require.NoError(b, c.CreateRepo(repo))
-	nFiles := 1000000
 
 	commit, err := c.StartCommit(repo, "")
 	require.NoError(b, err)
-	if !b.Run(fmt.Sprintf("Put%dFiles", nFiles), func(b *testing.B) {
+	if !b.Run(fmt.Sprintf("Put%dFiles", fileNum), func(b *testing.B) {
+		var totalBytes int64
 		var eg errgroup.Group
-		for k := 0; k < nFiles; k++ {
+		for k := 0; k < fileNum; k++ {
 			k := k
 			eg.Go(func() error {
-				_, err := c.PutFile(repo, commit.ID, fmt.Sprintf("file%d", k), randBytes(KB))
+				fileSize := d.Get()
+				atomic.AddInt64(&totalBytes, fileSize)
+				_, err := c.PutFile(repo, commit.ID, fmt.Sprintf("file%d", k), workload.NewReader(r, fileSize))
 				return err
 			})
 		}
-		b.SetBytes(int64(nFiles * KB))
 		require.NoError(b, eg.Wait())
+		b.SetBytes(totalBytes)
 	}) {
 		return
 	}
 	require.NoError(b, c.FinishCommit(repo, commit.ID))
 	require.NoError(b, c.SetBranch(repo, commit.ID, "master"))
 
-	if !b.Run(fmt.Sprintf("Get%dFiles", nFiles), func(b *testing.B) {
+	if !b.Run(fmt.Sprintf("Get%dFiles", fileNum), func(b *testing.B) {
+		w := &countWriter{}
 		var eg errgroup.Group
-		w := &CountWriter{}
-		defer func() { b.SetBytes(w.count) }()
-		for k := 0; k < nFiles; k++ {
+		for k := 0; k < fileNum; k++ {
 			k := k
 			eg.Go(func() error {
 				return c.GetFile(repo, commit.ID, fmt.Sprintf("file%d", k), 0, 0, w)
 			})
 		}
 		require.NoError(b, eg.Wait())
+		defer func() { b.SetBytes(w.count) }()
 	}) {
 		return
 	}
 
-	if !b.Run(fmt.Sprintf("PipelineCopy%dFiles", nFiles), func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			pipeline := "BenchmarkPachydermPipeline" + uuid.NewWithoutDashes()[0:12]
-			require.NoError(b, c.CreatePipeline(
-				pipeline,
-				"",
-				[]string{"bash"},
-				[]string{fmt.Sprintf("cp -R %s /pfs/out/", path.Join("/pfs", repo, "/*"))},
-				&ppsclient.ParallelismSpec{
-					Strategy: ppsclient.ParallelismSpec_CONSTANT,
-					Constant: 4,
-				},
-				[]*ppsclient.PipelineInput{{
-					Repo: client.NewRepo(repo),
-					Glob: "/*",
-				}},
-				"",
-				false,
-			))
-			_, err := c.FlushCommit([]*pfsclient.Commit{client.NewCommit(repo, commit.ID)}, nil)
-			require.NoError(b, err)
-			b.StopTimer()
-			repoInfo, err := c.InspectRepo(repo)
-			require.NoError(b, err)
-			b.SetBytes(int64(repoInfo.SizeBytes))
-			repoInfo, err = c.InspectRepo(pipeline)
-			require.NoError(b, err)
-			b.SetBytes(int64(repoInfo.SizeBytes))
-		}
+	if !b.Run(fmt.Sprintf("PipelineCopy%dFiles", fileNum), func(b *testing.B) {
+		pipeline := "BenchmarkPachydermPipeline" + uuid.NewWithoutDashes()[0:12]
+		require.NoError(b, c.CreatePipeline(
+			pipeline,
+			"",
+			[]string{"bash"},
+			[]string{fmt.Sprintf("cp -R %s /pfs/out/", path.Join("/pfs", repo, "/*"))},
+			&ppsclient.ParallelismSpec{
+				Strategy: ppsclient.ParallelismSpec_CONSTANT,
+				Constant: 4,
+			},
+			[]*ppsclient.PipelineInput{{
+				Repo: client.NewRepo(repo),
+				Glob: "/*",
+			}},
+			"",
+			false,
+		))
+		_, err := c.FlushCommit([]*pfsclient.Commit{client.NewCommit(repo, commit.ID)}, nil)
+		require.NoError(b, err)
+		b.StopTimer()
+		repoInfo, err := c.InspectRepo(repo)
+		require.NoError(b, err)
+		b.SetBytes(int64(repoInfo.SizeBytes))
+		repoInfo, err = c.InspectRepo(pipeline)
+		require.NoError(b, err)
+		b.SetBytes(int64(repoInfo.SizeBytes))
 	}) {
 		return
 	}
