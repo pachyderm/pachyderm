f0c4f4e17340bee2345035a1cf4595c32e39758f
Joe Doliner <jdoliner@gmail.com>
2017-03-16 18:45:34 -0700

    Make the linter happy.

--- src/client/pfs.go
+++ src/client/pfs.go
@@ -541,6 +541,11 @@ func (c APIClient) GetFile(repoName string, commitID string, path string, offset
 	return nil
 }
 
+// GetFileReader returns a reader for the contents of a file at a specific Commit.
+// offset specifies a number of bytes that should be skipped in the beginning of the file.
+// size limits the total amount of data returned, note you will get fewer bytes
+// than size if you pass a value larger than the size of the file.
+// If size is set to 0 then all of the data will be returned.
 func (c APIClient) GetFileReader(repoName string, commitID string, path string, offset int64, size int64) (io.Reader, error) {
 	apiGetFileClient, err := c.getFile(repoName, commitID, path, offset, size)
 	if err != nil {
