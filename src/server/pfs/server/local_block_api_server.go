04d54d4e5995be7a6690d48fc6d38087f49c33b4
Derek Chiang <derekchiang93@gmail.com>
2017-03-19 22:43:30 -0700

    Make benchmark automation work by pushing pachd/worker images

--- src/server/pfs/server/local_block_api_server.go
+++ src/server/pfs/server/local_block_api_server.go
@@ -246,7 +246,6 @@ func (r *putObjectReader) Read(p []byte) (int, error) {
 		if err != nil {
 			return 0, err
 		}
-		fmt.Printf("receiving %d bytes to object server\n", len(request.Value))
 		// buffer.Write cannot error
 		r.buffer.Write(request.Value)
 		r.tags = append(r.tags, request.Tags...)
