a202c4ca209aad5cfe02bfff5fff7dda2bf7e4bc
Joe Doliner <jdoliner@gmail.com>
2017-04-11 11:41:40 -0700

    Make the linter happy.

--- src/server/pkg/worker/worker.go
+++ src/server/pkg/worker/worker.go
@@ -4,6 +4,9 @@ import (
 	"github.com/pachyderm/pachyderm/src/client/pps"
 )
 
+// MatchDatum checks if a datum matches a filter.  To match each string in
+// filter must correspond match at least 1 datum's Path or Hash. Order of
+// filter and data is irrelevant.
 func MatchDatum(filter []string, data []*pps.Datum) bool {
 	// All paths in request.DataFilters must appear somewhere in the log
 	// line's inputs, or it's filtered
