7453f17af4c3ccb894cd1799bb4a35e3240ae507
Derek Chiang <derekchiang93@gmail.com>
2017-04-21 13:48:52 -0700

    Run CleanUp before cleanUpData so we don't end up opening pipes that have been deleted

--- src/server/pkg/worker/api_server.go
+++ src/server/pkg/worker/api_server.go
@@ -452,19 +452,25 @@ func (a *APIServer) Process(ctx context.Context, req *ProcessRequest) (resp *Pro
 	// Download input data
 	logger.Logf("input has not been processed, downloading data")
 	puller := filesync.NewPuller()
-	if err := a.downloadData(req.Data, puller); err != nil {
-		return nil, err
-	}
-	defer func() {
-		if err := puller.CleanUp(); err != nil && retErr == nil {
-			retErr = err
-		}
-	}()
+	err = a.downloadData(req.Data, puller)
+	// We run these cleanup functions no matter what, so that if
+	// downloadData partially succeeded, we still clean up the resources.
 	defer func() {
 		if err := a.cleanUpData(); retErr == nil && err != nil {
 			retErr = err
 		}
 	}()
+	// It's important that we run puller.CleanUp before a.cleanUpData,
+	// because otherwise puller.Cleanup might try tp open pipes that have
+	// been deleted.
+	defer func() {
+		if err := puller.CleanUp(); err != nil && retErr == nil {
+			retErr = err
+		}
+	}()
+	if err != nil {
+		return nil, err
+	}
 
 	environ := a.userCodeEnviron(req)
 
