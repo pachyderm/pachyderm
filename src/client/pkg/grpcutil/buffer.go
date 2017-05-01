af2787383ce6b1cb57cad8e22036c057c2d189ff
Derek Chiang <derekchiang93@gmail.com>
2017-05-01 13:18:41 -0700

    Move the buffer pool into the grpcutil package

---
+++ src/client/pkg/grpcutil/buffer.go
@@ -0,0 +1,26 @@
+package grpcutil
+
+import (
+	"sync"
+)
+
+var bufPool = sync.Pool{
+	New: func() interface{} {
+		// This buffer size is:
+		// 1. Reasonably smaller than the max gRPC size
+		// 2. Small enough that having hundreds of these buffers won't
+		// overwhelm the node
+		// 3. Large enough for message-sending to be efficient
+		return make([]byte, MaxMsgSize/10)
+	},
+}
+
+// Get returns a buffer.  The buffer may or may not be freshly allocated.
+func GetBuffer() []byte {
+	return bufPool.Get().([]byte)
+}
+
+// Put returns the buffer to the pool.
+func PutBuffer(buf []byte) {
+	bufPool.Put(buf)
+}
