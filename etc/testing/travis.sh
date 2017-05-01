2efa2503c8aa4ddb6b849826e609b1432577d2dc
Matthew Steffen <msteffen@pachyderm.io>
2017-05-01 13:37:51 -0700

    fix travis.sh

--- etc/testing/travis.sh
+++ etc/testing/travis.sh
@@ -2,7 +2,7 @@
 
 # Check the cron value to see if this is a daily job
 
-if [[ "$TRAVIS_EVENT_TYPE" != "cron" ]]; then
+if [[ "$TRAVIS_EVENT_TYPE" == "cron" ]]; then
 	echo "Running daily benchmarks"
   # Use the secrets in the travis environment to setup the aws creds for the aws command:
   echo -e "${AWS_ACCESS_KEY_ID}\n${AWS_SECRET_ACCESS_KEY}\n" \
