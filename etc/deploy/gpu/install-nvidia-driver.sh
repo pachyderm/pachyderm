9da07bc71459b32f4d4099ca4b19c9f6cb6c7be2
Sean Jezewski <seanwjezewski@gmail.com>
2017-05-09 19:26:53 -0700

    Put runner in rootfs so we can run it

--- etc/deploy/gpu/install-nvidia-driver.sh
+++ etc/deploy/gpu/install-nvidia-driver.sh
@@ -10,9 +10,11 @@ else
 	# If not ... install the drivers and restart
 	echo $1
 	NVIDIA_RUNNER=$1
+	cp $NVIDIA_RUNNER /rootfs
 	chroot /rootfs ./$NVIDIA_RUNNER --ui=none --no-questions --accept-license
 	# We want to overwrite, not append since the default
 	# value of this file w our current AMI is just 'exit 0'
+	echo "PACHNVIDIADRIVERINSTALL updating rc.local"
 	chroot /rootfs sudo cat >>/etc/rc.local  <<EOL
 nvidia-smi -pm 1 || true
 nvidia-smi -acp 0 || true
@@ -21,8 +23,9 @@ nvidia-smi --auto-boost-permission=0 || true
 nvidia-modprobe -u -c=0 -m || true
 EOL
 
+	echo "PACHNVIDIADRIVERINSTALL (not) going to restart"
 	# Don't think this will work ... but it might
 	# if not ... our deploy script / instructions will need to include doing a restart
 	# only AFTER the driver install has completed
-	chroot /rootfs shutdown -r	
+	#chroot /rootfs shutdown -r	
 fi
