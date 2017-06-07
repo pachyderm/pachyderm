6a4892adc4f1c30f7cb2ae416fae70ded9015285
Sean Jezewski <seanwjezewski@gmail.com>
2017-06-06 17:05:44 -0700

    Separate cloudfront deployment from script to secure cloudfront
    Because our users will need to wait on IT to generate CF keypairs

--- etc/deploy/aws.sh
+++ etc/deploy/aws.sh
@@ -90,6 +90,9 @@ create_s3_bucket() {
   fi
 
   if [ "$2" != "false" ]; then
+    mkdir -p tmp
+    sed 's/BUCKET_NAME/'$BUCKET'/' etc/deploy/cloudfront/bucket-policy.json.template > tmp/bucket-policy.json
+    aws s3api put-bucket-policy --bucket $BUCKET --policy file://tmp/bucket-policy.json --region=${AWS_REGION}
     create_cloudfront_distribution "${BUCKET}" || exit 1
   fi
 }
@@ -103,25 +106,11 @@ create_cloudfront_distribution() {
 
   someuuid=$(uuid | cut -f 1 -d-)
   mkdir -p tmp
-
-  sed 's/XXCallerReferenceXX/'$someuuid'/' etc/deploy/cloudfront/origin-access-identity.json.template > tmp/cloudfront-origin-access-identity.json
-  sed -i 's/XXBucketNameXX/'$BUCKET'/' tmp/cloudfront-origin-access-identity.json
-  aws cloudfront create-cloud-front-origin-access-identity --cloud-front-origin-access-identity-config file://tmp/cloudfront-origin-access-identity.json > tmp/cloudfront-origin-access-identity-info.json
-  CLOUDFRONT_OAI_CANONICAL=$(cat tmp/cloudfront-origin-access-identity-info.json | jq -r ".CloudFrontOriginAccessIdentity.S3CanonicalUserId")
-  echo "Got Cloudfront Origin Access Identity Canonical user id : ${CLOUDFRONT_OAI_CANONICAL}"
-  CLOUDFRONT_OAI_ID=$(cat tmp/cloudfront-origin-access-identity-info.json | jq -r ".CloudFrontOriginAccessIdentity.Id")
-  echo "Got Cloudfront Origin Access Identity ID : ${CLOUDFRONT_OAI_ID}"
-
   sed 's/XXCallerReferenceXX/'$someuuid'/' etc/deploy/cloudfront/distribution.json.template > tmp/cloudfront-distribution.json
   sed -i 's/XXBucketNameXX/'$BUCKET'/' tmp/cloudfront-distribution.json
-  sed -i 's/XXCLOUDFRONT_OAI_IDXX/'$CLOUDFRONT_OAI_ID'/' tmp/cloudfront-distribution.json
+
   aws cloudfront create-distribution --distribution-config file://tmp/cloudfront-distribution.json > tmp/cloudfront-distribution-info.json
   CLOUDFRONT_DOMAIN=$(cat tmp/cloudfront-distribution-info.json | jq -r ".Distribution.DomainName" | cut -f 1 -d .)
-  echo "Setup cloudfront distribution with domain ${CLOUDFRONT_DOMAIN}"
-
-  sed 's/XXBUCKET_NAMEXX/'$BUCKET'/' etc/deploy/cloudfront/bucket-policy.json.template > tmp/bucket-policy.json
-  sed -i 's/XXCLOUDFRONT_OAI_CANONICALXX/'$CLOUDFRONT_OAI_CANONICAL'/' tmp/bucket-policy.json
-  aws s3api put-bucket-policy --bucket $BUCKET --policy file://tmp/bucket-policy.json --region=${AWS_REGION}
 }
 
 deploy_k8s_on_aws() {
