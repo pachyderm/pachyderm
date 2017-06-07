1ab7d64635d72d070a2e885a66554cd723f7acc0
Sean Jezewski <seanwjezewski@gmail.com>
2017-06-06 22:42:51 -0700

    Update cloudfront distribution to require signed cookies

--- generate-cookie.sh
+++ generate-cookie.sh
@@ -12,14 +12,9 @@ signature=$(cat etc/deploy/cloudfront/cookie-policy.json | tr -d " \t\n\r" | ope
 echo "cookie policy [$cookie_policy]"
 echo "signature     [$signature]"
 
-curl -v \
+curl -v -L \
     -H "Cookie: CloudFront-Policy=$cookie_policy" \
     -H "Cookie: CloudFront-Signature=$signature" \
     -H "Cookie: CloudFront-Key-Pair-Id=$keypairid" \
-    http://d3q2p4tuagkx3p.cloudfront.net/pach/block/7249a7d412dc4a97bbb851b262afb752
+    http://d2dh50rmvikcjm.cloudfront.net/IMG_3577.PNG
 
-curl -v \
-    -H "Cookie: CloudFront-Policy=$cookie_policy" \
-    -H "Cookie: CloudFront-Signature=$signature" \
-    -H "Cookie: CloudFront-Key-Pair-Id=$keypairid" \
-    http://d3q2p4tuagkx3p.cloudfront.net/pach/block/a950a0ad3d8c41578c2115d871243492
