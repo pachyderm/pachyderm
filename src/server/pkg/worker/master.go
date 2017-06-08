09ae317b4e3cc612b73c621900d37b8934c877bc
Derek Chiang <derekchiang93@gmail.com>
2017-06-08 11:33:25 -0700

    Refactor PPS master; update pipeline state to RUNNING in master

--- src/server/pkg/worker/master.go
+++ src/server/pkg/worker/master.go
@@ -48,71 +48,143 @@ func (a *APIServer) master() {
 		defer masterLock.Unlock()
 		ctx = masterLock.Context()
 
-		jobInfos, err := a.pachClient.PpsAPIClient.ListJob(ctx, &pps.ListJobRequest{
-			Pipeline: a.pipelineInfo.Pipeline,
+		// Set pipeline state to running
+		_, err = col.NewSTM(ctx, a.etcdClient, func(stm col.STM) error {
+			pipelineName := a.pipelineInfo.Pipeline.Name
+			pipelines := a.pipelines.ReadWrite(stm)
+			pipelineInfo := new(pps.PipelineInfo)
+			if err := pipelines.Get(pipelineName, pipelineInfo); err != nil {
+				return err
+			}
+			pipelineInfo.State = pps.PipelineState_PIPELINE_RUNNING
+			pipelines.Put(pipelineName, pipelineInfo)
+			return nil
 		})
-		if err != nil {
-			return err
-		}
 
 		jobCh := make(chan *pps.JobInfo)
 		defer close(jobCh)
 		go a.datumFeeder(ctx, jobCh)
 
-		for _, jobInfo := range jobInfos.JobInfo {
-			switch jobInfo.State {
-			case pps.JobState_JOB_STARTING, pps.JobState_JOB_RUNNING:
-				jobCh <- jobInfo
-			}
+		return a.jobSpawner(ctx, jobCh)
+	}, backoff.NewInfiniteBackOff(), func(err error, d time.Duration) error {
+		protolion.Errorf("master: error running the master process: %v; retrying in %v", err, d)
+		return nil
+	})
+}
+
+// jobSpawner spawns jobs
+func (a *APIServer) jobSpawner(ctx context.Context, jobCh chan *pps.JobInfo) error {
+	jobInfos, err := a.pachClient.PpsAPIClient.ListJob(ctx, &pps.ListJobRequest{
+		Pipeline: a.pipelineInfo.Pipeline,
+	})
+	if err != nil {
+		return err
+	}
+
+	for _, jobInfo := range jobInfos.JobInfo {
+		switch jobInfo.State {
+		case pps.JobState_JOB_STARTING, pps.JobState_JOB_RUNNING:
+			jobCh <- jobInfo
+		}
+	}
+
+	bsf, err := newBranchSetFactory(ctx, a.pachClient.PfsAPIClient, a.pipelineInfo.Input)
+	if err != nil {
+		return err
+	}
+	defer bsf.Close()
+nextInput:
+	for {
+		bs := <-bsf.Chan()
+		if bs.Err != nil {
+			return err
 		}
 
-		bsf, err := newBranchSetFactory(ctx, a.pachClient.PfsAPIClient, a.pipelineInfo.Input)
+		// (create JobInput for new processing job)
+		jobInput := proto.Clone(a.pipelineInfo.Input).(*pps.Input)
+		var visitErr error
+		pps.VisitInput(jobInput, func(input *pps.Input) {
+			if input.Atom != nil {
+				for _, branch := range bs.Branches {
+					if input.Atom.Repo == branch.Head.Repo.Name && input.Atom.Branch == branch.Name {
+						input.Atom.Commit = branch.Head.ID
+					}
+				}
+				if input.Atom.Commit == "" {
+					visitErr = fmt.Errorf("didn't find input commit for %s/%s", input.Atom.Repo, input.Atom.Branch)
+				}
+				input.Atom.FromCommit = ""
+			}
+		})
+		if visitErr != nil {
+			return visitErr
+		}
+
+		jobsRO := a.jobs.ReadOnly(ctx)
+		// Check if this input set has already been processed
+		jobIter, err := jobsRO.GetByIndex(ppsdb.JobsInputIndex, jobInput)
 		if err != nil {
 			return err
 		}
-		defer bsf.Close()
-	nextInput:
+
+		// This is doing the same thing as the line above but for 1.4.5 style jobs
+		oldJobIter, err := jobsRO.GetByIndex(ppsdb.JobsInputsIndex, untranslateJobInputs(jobInput))
+		if err != nil {
+			return err
+		}
+
+		// Check if any of the jobs in jobIter have been run already. If so, skip
+		// this input.
 		for {
-			bs := <-bsf.Chan()
-			if bs.Err != nil {
+			var jobID string
+			var jobInfo pps.JobInfo
+			ok, err := jobIter.Next(&jobID, &jobInfo)
+			if err != nil {
 				return err
 			}
+			if !ok {
+				ok, err := oldJobIter.Next(&jobID, &jobInfo)
+				if err != nil {
+					return err
+				}
+				if !ok {
+					break
+				}
+			}
+			if jobInfo.PipelineID == a.pipelineInfo.ID && jobInfo.PipelineVersion == a.pipelineInfo.Version {
+				// TODO(derek): we should check if the output commit exists.  If the
+				// output commit has been deleted, we should re-run the job.
+				continue nextInput
+			}
+		}
 
-			// (create JobInput for new processing job)
-			jobInput := proto.Clone(a.pipelineInfo.Input).(*pps.Input)
-			var visitErr error
-			pps.VisitInput(jobInput, func(input *pps.Input) {
-				if input.Atom != nil {
-					for _, branch := range bs.Branches {
-						if input.Atom.Repo == branch.Head.Repo.Name && input.Atom.Branch == branch.Name {
-							input.Atom.Commit = branch.Head.ID
-						}
-					}
-					if input.Atom.Commit == "" {
-						visitErr = fmt.Errorf("didn't find input commit for %s/%s", input.Atom.Repo, input.Atom.Branch)
-					}
-					input.Atom.FromCommit = ""
+		// now we need to find the parentJob for this job. The parent job
+		// is defined as the job with the same input commits except for the
+		// newest input commit which triggered this job. In place of it we
+		// use that commit's parent.
+		newBranch := bs.Branches[bs.NewBranch]
+		newCommitInfo, err := a.pachClient.PfsAPIClient.InspectCommit(ctx, &pfs.InspectCommitRequest{
+			Commit: newBranch.Head,
+		})
+		if err != nil {
+			return err
+		}
+		var parentJob *pps.Job
+		if newCommitInfo.ParentCommit != nil {
+			// recreate our parent's inputs so we can look it up in etcd
+			parentJobInput := proto.Clone(jobInput).(*pps.Input)
+			pps.VisitInput(parentJobInput, func(input *pps.Input) {
+				if input.Atom != nil && input.Atom.Repo == newBranch.Head.Repo.Name && input.Atom.Branch == newBranch.Name {
+					input.Atom.Commit = newCommitInfo.ParentCommit.ID
 				}
 			})
 			if visitErr != nil {
 				return visitErr
 			}
-
-			jobsRO := a.jobs.ReadOnly(ctx)
-			// Check if this input set has already been processed
-			jobIter, err := jobsRO.GetByIndex(ppsdb.JobsInputIndex, jobInput)
+			jobIter, err := jobsRO.GetByIndex(ppsdb.JobsInputIndex, parentJobInput)
 			if err != nil {
 				return err
 			}
-
-			// This is doing the same thing as the line above but for 1.4.5 style jobs
-			oldJobIter, err := jobsRO.GetByIndex(ppsdb.JobsInputsIndex, untranslateJobInputs(jobInput))
-			if err != nil {
-				return err
-			}
-
-			// Check if any of the jobs in jobIter have been run already. If so, skip
-			// this input.
 			for {
 				var jobID string
 				var jobInfo pps.JobInfo
@@ -121,93 +193,40 @@ func (a *APIServer) master() {
 					return err
 				}
 				if !ok {
-					ok, err := oldJobIter.Next(&jobID, &jobInfo)
-					if err != nil {
-						return err
-					}
-					if !ok {
-						break
-					}
+					break
 				}
 				if jobInfo.PipelineID == a.pipelineInfo.ID && jobInfo.PipelineVersion == a.pipelineInfo.Version {
-					// TODO(derek): we should check if the output commit exists.  If the
-					// output commit has been deleted, we should re-run the job.
-					continue nextInput
+					parentJob = jobInfo.Job
 				}
 			}
-
-			// now we need to find the parentJob for this job. The parent job
-			// is defined as the job with the same input commits except for the
-			// newest input commit which triggered this job. In place of it we
-			// use that commit's parent.
-			newBranch := bs.Branches[bs.NewBranch]
-			newCommitInfo, err := a.pachClient.PfsAPIClient.InspectCommit(ctx, &pfs.InspectCommitRequest{
-				Commit: newBranch.Head,
-			})
-			if err != nil {
-				return err
-			}
-			var parentJob *pps.Job
-			if newCommitInfo.ParentCommit != nil {
-				// recreate our parent's inputs so we can look it up in etcd
-				parentJobInput := proto.Clone(jobInput).(*pps.Input)
-				pps.VisitInput(parentJobInput, func(input *pps.Input) {
-					if input.Atom != nil && input.Atom.Repo == newBranch.Head.Repo.Name && input.Atom.Branch == newBranch.Name {
-						input.Atom.Commit = newCommitInfo.ParentCommit.ID
-					}
-				})
-				if visitErr != nil {
-					return visitErr
-				}
-				jobIter, err := jobsRO.GetByIndex(ppsdb.JobsInputIndex, parentJobInput)
-				if err != nil {
-					return err
-				}
-				for {
-					var jobID string
-					var jobInfo pps.JobInfo
-					ok, err := jobIter.Next(&jobID, &jobInfo)
-					if err != nil {
-						return err
-					}
-					if !ok {
-						break
-					}
-					if jobInfo.PipelineID == a.pipelineInfo.ID && jobInfo.PipelineVersion == a.pipelineInfo.Version {
-						parentJob = jobInfo.Job
-					}
-				}
-			}
-
-			job, err := a.pachClient.PpsAPIClient.CreateJob(ctx, &pps.CreateJobRequest{
-				Pipeline: a.pipelineInfo.Pipeline,
-				Input:    jobInput,
-				// TODO(derek): Note that once the pipeline restarts, the `job`
-				// variable is lost and we don't know who is our parent job.
-				ParentJob: parentJob,
-				NewBranch: newBranch,
-			})
-			if err != nil {
-				return err
-			}
-
-			jobInfo, err := a.pachClient.PpsAPIClient.InspectJob(ctx, &pps.InspectJobRequest{
-				Job: job,
-			})
-			if err != nil {
-				return err
-			}
-
-			jobCh <- jobInfo
 		}
 
-		return nil
-	}, backoff.NewInfiniteBackOff(), func(err error, d time.Duration) error {
-		protolion.Errorf("master: error running the master process: %v; retrying in %v", err, d)
-		return nil
-	})
+		job, err := a.pachClient.PpsAPIClient.CreateJob(ctx, &pps.CreateJobRequest{
+			Pipeline: a.pipelineInfo.Pipeline,
+			Input:    jobInput,
+			// TODO(derek): Note that once the pipeline restarts, the `job`
+			// variable is lost and we don't know who is our parent job.
+			ParentJob: parentJob,
+			NewBranch: newBranch,
+		})
+		if err != nil {
+			return err
+		}
+
+		jobInfo, err := a.pachClient.PpsAPIClient.InspectJob(ctx, &pps.InspectJobRequest{
+			Job: job,
+		})
+		if err != nil {
+			return err
+		}
+
+		jobCh <- jobInfo
+	}
+
+	return nil
 }
 
+// datumFeeder feeds datums to jobs
 func (a *APIServer) datumFeeder(ctx context.Context, jobCh chan *pps.JobInfo) {
 	ppsClient := a.pachClient.PpsAPIClient
 	pfsClient := a.pachClient.PfsAPIClient
