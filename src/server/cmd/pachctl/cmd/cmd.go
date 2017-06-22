4185e31ee86e107ba1f96420791feebf208b6007
Sean Jezewski <seanwjezewski@gmail.com>
2017-06-22 11:00:23 -0700

    Update metrics call within pachctl version to not block

--- src/server/cmd/pachctl/cmd/cmd.go
+++ src/server/cmd/pachctl/cmd/cmd.go
@@ -75,8 +75,27 @@ Environment variables:
 		Long:  "Return version information.",
 		Run: cmdutil.RunFixedArgs(0, func(args []string) (retErr error) {
 			if !noMetrics {
-				metricsFn := metrics.ReportAndFlushUserAction("Version")
-				defer func(start time.Time) { metricsFn(start, retErr) }(time.Now())
+				metricsDone := make(chan struct{})
+				go func() {
+					metrics.ReportAndFlushUserAction("VersionStarted", time.Now())
+					close(metricsDone)
+				}()
+
+				defer func() {
+					select {
+					case <-metricsDone:
+					case time.After(time.Second * 5):
+
+					}
+				}()
+
+				defer func(start time.Time) {
+					if retErr != nil {
+						metrics.ReportAndFlushUserAction("VersionErrored", retErr)
+					} else {
+						metrics.ReportAndFlushUserAction("VersionFinished", time.Since(start).Seconds())
+					}
+				}(time.Now())
 			}
 			writer := tabwriter.NewWriter(os.Stdout, 20, 1, 3, ' ', 0)
 			printVersionHeader(writer)
