886d502a66e304420767c354cc3cf4bf40945b65
Derek Chiang <derekchiang93@gmail.com>
2017-06-26 12:56:10 -0700

    Bump version to 1.5.0

--- src/client/version/client.go
+++ src/client/version/client.go
@@ -10,9 +10,9 @@ const (
 	// MajorVersion is the current major version for pachyderm.
 	MajorVersion = 1
 	// MinorVersion is the current minor version for pachyderm.
-	MinorVersion = 4
+	MinorVersion = 5
 	// MicroVersion is the patch number for pachyderm.
-	MicroVersion = 8
+	MicroVersion = 0
 )
 
 var (
