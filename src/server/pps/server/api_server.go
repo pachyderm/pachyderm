c7b0e768e9029f772db7c4aa08c053da64c5f60f
Derek Chiang <derekchiang93@gmail.com>
2017-06-27 15:17:02 -0700

    Make it so that a pipeline's memory request is at least as big as its cache size

--- src/server/pps/server/api_server.go
+++ src/server/pps/server/api_server.go
@@ -1314,15 +1314,26 @@ func jobStateToStopped(state pps.JobState) bool {
 	}
 }
 
-func parseResourceList(resources *pps.ResourceSpec) (*api.ResourceList, error) {
+func parseResourceList(resources *pps.ResourceSpec, cacheSize string) (*api.ResourceList, error) {
 	cpuQuantity, err := resource.ParseQuantity(fmt.Sprintf("%f", resources.Cpu))
 	if err != nil {
 		return nil, fmt.Errorf("could not parse cpu quantity: %s", err)
 	}
+
 	memQuantity, err := resource.ParseQuantity(resources.Memory)
 	if err != nil {
 		return nil, fmt.Errorf("could not parse memory quantity: %s", err)
 	}
+	cacheQuantity, err := resource.ParseQuantity(cacheSize)
+	if err != nil {
+		return nil, fmt.Errorf("could not parse cache quantity: %s", err)
+	}
+	// Here we are sanity checking.  A pipeline should request at least
+	// as much memory as it needs for caching.
+	if cacheQuantity.Cmp(memQuantity) > 0 {
+		memQuantity = cacheQuantity
+	}
+
 	gpuQuantity, err := resource.ParseQuantity(fmt.Sprintf("%d", resources.Gpu))
 	if err != nil {
 		return nil, fmt.Errorf("could not parse gpu quantity: %s", err)
