b05d7d6d92b5b4f5ad2f50a2dd737db2bac83318
Joe Doliner <jdoliner@gmail.com>
2017-07-12 14:14:40 -0700

    Adds datum migration to worker.

--- src/server/pkg/worker/api_server.go
+++ src/server/pkg/worker/api_server.go
@@ -12,6 +12,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"sync"
 	"syscall"
@@ -494,6 +495,37 @@ func HashDatum(pipelineInfo *pps.PipelineInfo, data []*Input) (string, error) {
 	return client.HashPipelineName(pipelineInfo.Pipeline.Name) + hex.EncodeToString(hash.Sum(nil)), nil
 }
 
+// HashDatum15 computes and returns the hash of datum + pipeline for version <= 1.5.0, with a
+// pipeline-specific prefix.
+func HashDatum15(pipelineInfo *pps.PipelineInfo, data []*Input) (string, error) {
+	hash := sha256.New()
+	for _, datum := range data {
+		hash.Write([]byte(datum.Name))
+		hash.Write([]byte(datum.FileInfo.File.Path))
+		hash.Write(datum.FileInfo.Hash)
+	}
+
+	// We set env to nil because if env contains more than one elements,
+	// since it's a map, the output of Marshal() can be non-deterministic.
+	env := pipelineInfo.Transform.Env
+	pipelineInfo.Transform.Env = nil
+	defer func() {
+		pipelineInfo.Transform.Env = env
+	}()
+	bytes, err := pipelineInfo.Transform.Marshal()
+	if err != nil {
+		return "", err
+	}
+	hash.Write(bytes)
+	hash.Write([]byte(pipelineInfo.Pipeline.Name))
+	hash.Write([]byte(pipelineInfo.ID))
+	hash.Write([]byte(strconv.Itoa(int(pipelineInfo.Version))))
+
+	// Note in 1.5.0 this function was called HashPipelineID, it's now called
+	// HashPipelineName but it has the same implementation.
+	return client.HashPipelineName(pipelineInfo.ID) + hex.EncodeToString(hash.Sum(nil)), nil
+}
+
 // Process processes a datum.
 func (a *APIServer) Process(ctx context.Context, req *ProcessRequest) (resp *ProcessResponse, retErr error) {
 	logger := a.getTaggedLogger(req)
@@ -539,7 +571,38 @@ func (a *APIServer) Process(ctx context.Context, req *ProcessRequest) (resp *Pro
 	if err != nil {
 		return nil, err
 	}
-	if _, err := a.pachClient.InspectTag(ctx, &pfs.Tag{tag}); err == nil {
+	tag15, err := HashDatum15(a.pipelineInfo, req.Data)
+	if err != nil {
+		return nil, err
+	}
+	foundTag := false
+	foundTag15 := false
+	var object *pfs.Object
+	var eg errgroup.Group
+	eg.Go(func() error {
+		if _, err := a.pachClient.InspectTag(ctx, &pfs.Tag{tag}); err == nil {
+			foundTag = true
+		}
+		return nil
+	})
+	eg.Go(func() error {
+		if objectInfo, err := a.pachClient.InspectTag(ctx, &pfs.Tag{tag15}); err == nil {
+			foundTag15 = true
+			object = objectInfo.Object
+		}
+		return nil
+	})
+
+	logger.Logf("foundTag: %t, foundTag15: %t", foundTag, foundTag15)
+	if foundTag15 && !foundTag {
+		if _, err := a.pachClient.ObjectAPIClient.TagObject(ctx, &pfs.TagObjectRequest{
+			Object: object,
+			Tags:   []*pfs.Tag{&pfs.Tag{tag}},
+		}); err != nil {
+			return nil, err
+		}
+	}
+	if foundTag15 || foundTag {
 		// We've already computed the output for these inputs. Return immediately
 		logger.Logf("skipping input, as it's already been processed")
 		return &ProcessResponse{
