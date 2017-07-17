362b4b165eb77d2f15a30561557bb56e680ad0cc
Derek Chiang <derekchiang93@gmail.com>
2017-07-17 00:25:09 -0700

    Install uuid

--- etc/testing/travis.sh
+++ etc/testing/travis.sh
@@ -6,7 +6,7 @@ make test
 echo "Running aws tests"
 
 sudo pip install awscli
-sudo apt-get install realpath
+sudo apt-get install realpath uuid jq
 wget https://github.com/kubernetes/kops/releases/download/1.6.2/kops-linux-amd64
 chmod +x kops-linux-amd64
 sudo mv kops-linux-amd64 /usr/local/bin/kops
