a98a2944b627e3d14954f10ddf28024541f94fc7
Derek Chiang <derekchiang93@gmail.com>
2017-07-16 23:54:54 -0700

    Install jq

--- etc/testing/travis.sh
+++ etc/testing/travis.sh
@@ -6,7 +6,7 @@ make test
 echo "Running aws tests"
 
 pip install awscli
-sudo apt-get install realpath
+sudo apt-get install realpath jq
 wget https://github.com/kubernetes/kops/releases/download/1.6.2/kops-linux-amd64
 chmod +x kops-linux-amd64
 sudo mv kops-linux-amd64 /usr/local/bin/kops
