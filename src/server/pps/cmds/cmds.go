9314e6279feaffdb92a8ba31c4bce82e719de00e
Daniel Whitenack <whitenack.daniel@gmail.com>
2017-07-19 09:35:15 -0400

    pps docs formatting

--- src/server/pps/cmds/cmds.go
+++ src/server/pps/cmds/cmds.go
@@ -96,7 +96,7 @@ The increase the throughput of a job increase the Shard paremeter.
 
 Examples:
 
-	` + codestart + `# return all jobs
+` + codestart + `# return all jobs
 $ pachctl list-job
 
 # return all jobs in pipeline foo
@@ -218,7 +218,7 @@ $ pachctl list-job -p foo bar/YYY
 
 Examples:
 
-	` + codestart + `# return logs emitted by recent jobs in the "filter" pipeline
+` + codestart + `# return logs emitted by recent jobs in the "filter" pipeline
 $ pachctl get-logs --pipeline=filter
 
 # return logs emitted by the job aedfa12aedf
