08b4d189b6a1ab2b7806c5c2c2db38039fe11946
Matthew Steffen <msteffen@pachyderm.io>
2017-08-04 07:26:23 -0700

    Shorten TestUseMultipleWorkers

--- src/server/pkg/backoff/backoff_test.go
+++ src/server/pkg/backoff/backoff_test.go
@@ -1,6 +1,7 @@
 package backoff
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
@@ -23,5 +24,36 @@ func TestConstantBackOff(t *testing.T) {
 	backoff := NewConstantBackOff(time.Second)
 	if backoff.NextBackOff() != time.Second {
 		t.Error("invalid interval")
+	}
+}
+
+func abstime(t time.Duration) time.Duration {
+	if t < 0 {
+		return -t
+	}
+	return t
+}
+
+func TestConstantBackOffCompare(t *testing.T) {
+	var callTimes [10]time.Time
+	idx := 0
+	start := time.Now()
+	Retry(func() error {
+		callTimes[idx] = time.Now()
+		idx++
+		return fmt.Errorf("expected error")
+	}, RetryEvery(time.Second).For(9*time.Second))
+
+	epsilon := 500 * time.Millisecond
+	if idx < 8 {
+		t.Fatalf("expected 9 retries, but only saw %d", idx)
+	}
+	nextT := start
+	for i := 0; i < idx; i++ {
+		if abstime(callTimes[i].Sub(nextT)) > epsilon {
+			t.Fatalf("expected retry %d to occur at %s but actually occurred at %s",
+				i, nextT.String(), callTimes[i].String())
+		}
+		nextT = nextT.Add(1 * time.Second)
 	}
 }
