845b5280105251c94cae078758cf449506f48b93
Joe Doliner <jdoliner@gmail.com>
2017-08-11 15:19:02 -0700

    Expands TestCronPipeline, making it harder.

--- src/server/pachyderm_test.go
+++ src/server/pachyderm_test.go
@@ -3951,9 +3951,9 @@ func TestCronPipeline(t *testing.T) {
 	}
 	t.Parallel()
 	c := getPachClient(t)
-	pipeline := uniqueString("pipeline")
+	pipeline1 := uniqueString("pipeline1")
 	require.NoError(t, c.CreatePipeline(
-		pipeline,
+		pipeline1,
 		"",
 		[]string{"cp", "/pfs/time/time", "/pfs/out/time"},
 		nil,
@@ -3963,7 +3963,7 @@ func TestCronPipeline(t *testing.T) {
 		false,
 	))
 
-	repo := fmt.Sprintf("%s_%s", pipeline, "time")
+	repo := fmt.Sprintf("%s_%s", pipeline1, "time")
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
 	defer cancel() //cleanup resources
 	iter, err := c.WithCtx(ctx).SubscribeCommit(repo, "master", "")
@@ -3974,6 +3974,43 @@ func TestCronPipeline(t *testing.T) {
 	commitIter, err := c.FlushCommit([]*pfs.Commit{commitInfo.Commit}, nil)
 	require.NoError(t, err)
 	commitInfos := collectCommitInfos(t, commitIter)
+	require.Equal(t, 1, len(commitInfos))
+
+	dataRepo := uniqueString("TestCronPipeline_data")
+	require.NoError(t, c.CreateRepo(dataRepo))
+	pipeline2 := uniqueString("pipeline2")
+	require.NoError(t, c.CreatePipeline(
+		pipeline2,
+		"",
+		[]string{"bash"},
+		[]string{
+			"cp /pfs/time/time /pfs/out/time",
+			fmt.Sprintf("cp /pfs/%s/file /pfs/out/file", dataRepo),
+		},
+		nil,
+		client.NewCrossInput(
+			client.NewCronInput("time", "@every 20s"),
+			client.NewAtomInput(dataRepo, "/"),
+		),
+		"",
+		false,
+	))
+	dataCommit, err := c.StartCommit(dataRepo, "master")
+	require.NoError(t, err)
+	_, err = c.PutFile(dataRepo, "master", "file", strings.NewReader("file"))
+	require.NoError(t, c.FinishCommit(dataRepo, "master"))
+
+	repo = fmt.Sprintf("%s_%s", pipeline2, "time")
+	ctx, cancel = context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel() //cleanup resources
+	iter, err = c.WithCtx(ctx).SubscribeCommit(repo, "master", "")
+	require.NoError(t, err)
+	commitInfo, err = iter.Next()
+	require.NoError(t, err)
+
+	commitIter, err = c.FlushCommit([]*pfs.Commit{dataCommit, commitInfo.Commit}, nil)
+	require.NoError(t, err)
+	commitInfos = collectCommitInfos(t, commitIter)
 	require.Equal(t, 1, len(commitInfos))
 }
 
