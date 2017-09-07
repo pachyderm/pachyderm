d54f8ccccb995b6f74bea9e2dc5aa1557666718f
Joe Doliner <jdoliner@gmail.com>
2017-09-07 13:25:00 -0700

    Adds makes queue size configurable by pipeline.

--- src/server/pps/server/api_server.go
+++ src/server/pps/server/api_server.go
@@ -1182,6 +1182,7 @@ func (a *apiServer) CreatePipeline(ctx context.Context, request *pps.CreatePipel
 		EnableStats:        request.EnableStats,
 		Salt:               uuid.NewWithoutDashes(),
 		Batch:              request.Batch,
+		MaxQueueSize:       request.MaxQueueSize,
 	}
 	setPipelineDefaults(pipelineInfo)
 	var visitErr error
@@ -1391,6 +1392,9 @@ func setPipelineDefaults(pipelineInfo *pps.PipelineInfo) {
 		pipelineInfo.ResourceSpec = &pps.ResourceSpec{
 			Memory: pipelineInfo.CacheSize,
 		}
+	}
+	if pipelineInfo.MaxQueueSize == 0 {
+		pipelineInfo.MaxQueueSize = 10
 	}
 }
 
