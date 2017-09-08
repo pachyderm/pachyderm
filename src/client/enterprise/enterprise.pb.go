e0080525c2994744bf635e8ffabf82b67f8b2dc5
Matthew Steffen <msteffen@pachyderm.io>
2017-09-08 16:16:16 -0700

    Return TokenInfo from Activate and GetState in Enterprise API

--- src/client/enterprise/enterprise.pb.go
+++ src/client/enterprise/enterprise.pb.go
@@ -9,6 +9,7 @@
 
 	It has these top-level messages:
 		EnterpriseRecord
+		TokenInfo
 		ActivateRequest
 		ActivateResponse
 		GetStateRequest
@@ -90,21 +91,40 @@ func (m *EnterpriseRecord) GetExpires() *google_protobuf.Timestamp {
 	return nil
 }
 
+// TokenInfo contains information about the currently active enterprise token
+type TokenInfo struct {
+	// expires indicates when the current token expires (unset if there is no
+	// current token)
+	Expires *google_protobuf.Timestamp `protobuf:"bytes,1,opt,name=expires" json:"expires,omitempty"`
+}
+
+func (m *TokenInfo) Reset()                    { *m = TokenInfo{} }
+func (m *TokenInfo) String() string            { return proto.CompactTextString(m) }
+func (*TokenInfo) ProtoMessage()               {}
+func (*TokenInfo) Descriptor() ([]byte, []int) { return fileDescriptorEnterprise, []int{1} }
+
+func (m *TokenInfo) GetExpires() *google_protobuf.Timestamp {
+	if m != nil {
+		return m.Expires
+	}
+	return nil
+}
+
 type ActivateRequest struct {
 	// activation_code is a Pachyderm enterprise activation code. New users can
 	// obtain trial activation codes
 	ActivationCode string `protobuf:"bytes,1,opt,name=activation_code,json=activationCode,proto3" json:"activation_code,omitempty"`
 	// expires is a timestamp indicating when this activation code will expire.
-	// Note that this should not generally be set (it's primarily used for
-	// testing), and is only applied if it's earlier than the signed expiration
-	// time in 'activation_code'.
+	// This should not generally be set (it's primarily used for testing), and is
+	// only applied if it's earlier than the signed expiration time in
+	// 'activation_code'.
 	Expires *google_protobuf.Timestamp `protobuf:"bytes,2,opt,name=expires" json:"expires,omitempty"`
 }
 
 func (m *ActivateRequest) Reset()                    { *m = ActivateRequest{} }
 func (m *ActivateRequest) String() string            { return proto.CompactTextString(m) }
 func (*ActivateRequest) ProtoMessage()               {}
-func (*ActivateRequest) Descriptor() ([]byte, []int) { return fileDescriptorEnterprise, []int{1} }
+func (*ActivateRequest) Descriptor() ([]byte, []int) { return fileDescriptorEnterprise, []int{2} }
 
 func (m *ActivateRequest) GetActivationCode() string {
 	if m != nil {
@@ -121,12 +141,20 @@ func (m *ActivateRequest) GetExpires() *google_protobuf.Timestamp {
 }
 
 type ActivateResponse struct {
+	Info *TokenInfo `protobuf:"bytes,1,opt,name=info" json:"info,omitempty"`
 }
 
 func (m *ActivateResponse) Reset()                    { *m = ActivateResponse{} }
 func (m *ActivateResponse) String() string            { return proto.CompactTextString(m) }
 func (*ActivateResponse) ProtoMessage()               {}
-func (*ActivateResponse) Descriptor() ([]byte, []int) { return fileDescriptorEnterprise, []int{2} }
+func (*ActivateResponse) Descriptor() ([]byte, []int) { return fileDescriptorEnterprise, []int{3} }
+
+func (m *ActivateResponse) GetInfo() *TokenInfo {
+	if m != nil {
+		return m.Info
+	}
+	return nil
+}
 
 type GetStateRequest struct {
 }
@@ -134,16 +162,17 @@ type GetStateRequest struct {
 func (m *GetStateRequest) Reset()                    { *m = GetStateRequest{} }
 func (m *GetStateRequest) String() string            { return proto.CompactTextString(m) }
 func (*GetStateRequest) ProtoMessage()               {}
-func (*GetStateRequest) Descriptor() ([]byte, []int) { return fileDescriptorEnterprise, []int{3} }
+func (*GetStateRequest) Descriptor() ([]byte, []int) { return fileDescriptorEnterprise, []int{4} }
 
 type GetStateResponse struct {
-	State State `protobuf:"varint,1,opt,name=state,proto3,enum=enterprise.State" json:"state,omitempty"`
+	State State      `protobuf:"varint,1,opt,name=state,proto3,enum=enterprise.State" json:"state,omitempty"`
+	Info  *TokenInfo `protobuf:"bytes,2,opt,name=info" json:"info,omitempty"`
 }
 
 func (m *GetStateResponse) Reset()                    { *m = GetStateResponse{} }
 func (m *GetStateResponse) String() string            { return proto.CompactTextString(m) }
 func (*GetStateResponse) ProtoMessage()               {}
-func (*GetStateResponse) Descriptor() ([]byte, []int) { return fileDescriptorEnterprise, []int{4} }
+func (*GetStateResponse) Descriptor() ([]byte, []int) { return fileDescriptorEnterprise, []int{5} }
 
 func (m *GetStateResponse) GetState() State {
 	if m != nil {
@@ -152,8 +181,16 @@ func (m *GetStateResponse) GetState() State {
 	return State_NONE
 }
 
+func (m *GetStateResponse) GetInfo() *TokenInfo {
+	if m != nil {
+		return m.Info
+	}
+	return nil
+}
+
 func init() {
 	proto.RegisterType((*EnterpriseRecord)(nil), "enterprise.EnterpriseRecord")
+	proto.RegisterType((*TokenInfo)(nil), "enterprise.TokenInfo")
 	proto.RegisterType((*ActivateRequest)(nil), "enterprise.ActivateRequest")
 	proto.RegisterType((*ActivateResponse)(nil), "enterprise.ActivateResponse")
 	proto.RegisterType((*GetStateRequest)(nil), "enterprise.GetStateRequest")
@@ -304,6 +341,34 @@ func (m *EnterpriseRecord) MarshalTo(dAtA []byte) (int, error) {
 	return i, nil
 }
 
+func (m *TokenInfo) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalTo(dAtA)
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *TokenInfo) MarshalTo(dAtA []byte) (int, error) {
+	var i int
+	_ = i
+	var l int
+	_ = l
+	if m.Expires != nil {
+		dAtA[i] = 0xa
+		i++
+		i = encodeVarintEnterprise(dAtA, i, uint64(m.Expires.Size()))
+		n2, err := m.Expires.MarshalTo(dAtA[i:])
+		if err != nil {
+			return 0, err
+		}
+		i += n2
+	}
+	return i, nil
+}
+
 func (m *ActivateRequest) Marshal() (dAtA []byte, err error) {
 	size := m.Size()
 	dAtA = make([]byte, size)
@@ -329,11 +394,11 @@ func (m *ActivateRequest) MarshalTo(dAtA []byte) (int, error) {
 		dAtA[i] = 0x12
 		i++
 		i = encodeVarintEnterprise(dAtA, i, uint64(m.Expires.Size()))
-		n2, err := m.Expires.MarshalTo(dAtA[i:])
+		n3, err := m.Expires.MarshalTo(dAtA[i:])
 		if err != nil {
 			return 0, err
 		}
-		i += n2
+		i += n3
 	}
 	return i, nil
 }
@@ -353,6 +418,16 @@ func (m *ActivateResponse) MarshalTo(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
+	if m.Info != nil {
+		dAtA[i] = 0xa
+		i++
+		i = encodeVarintEnterprise(dAtA, i, uint64(m.Info.Size()))
+		n4, err := m.Info.MarshalTo(dAtA[i:])
+		if err != nil {
+			return 0, err
+		}
+		i += n4
+	}
 	return i, nil
 }
 
@@ -393,6 +468,16 @@ func (m *GetStateResponse) MarshalTo(dAtA []byte) (int, error) {
 		dAtA[i] = 0x8
 		i++
 		i = encodeVarintEnterprise(dAtA, i, uint64(m.State))
+	}
+	if m.Info != nil {
+		dAtA[i] = 0x12
+		i++
+		i = encodeVarintEnterprise(dAtA, i, uint64(m.Info.Size()))
+		n5, err := m.Info.MarshalTo(dAtA[i:])
+		if err != nil {
+			return 0, err
+		}
+		i += n5
 	}
 	return i, nil
 }
@@ -438,6 +523,16 @@ func (m *EnterpriseRecord) Size() (n int) {
 	return n
 }
 
+func (m *TokenInfo) Size() (n int) {
+	var l int
+	_ = l
+	if m.Expires != nil {
+		l = m.Expires.Size()
+		n += 1 + l + sovEnterprise(uint64(l))
+	}
+	return n
+}
+
 func (m *ActivateRequest) Size() (n int) {
 	var l int
 	_ = l
@@ -455,6 +550,10 @@ func (m *ActivateRequest) Size() (n int) {
 func (m *ActivateResponse) Size() (n int) {
 	var l int
 	_ = l
+	if m.Info != nil {
+		l = m.Info.Size()
+		n += 1 + l + sovEnterprise(uint64(l))
+	}
 	return n
 }
 
@@ -469,6 +568,10 @@ func (m *GetStateResponse) Size() (n int) {
 	_ = l
 	if m.State != 0 {
 		n += 1 + sovEnterprise(uint64(m.State))
+	}
+	if m.Info != nil {
+		l = m.Info.Size()
+		n += 1 + l + sovEnterprise(uint64(l))
 	}
 	return n
 }
@@ -545,6 +648,89 @@ func (m *EnterpriseRecord) Unmarshal(dAtA []byte) error {
 			m.ActivationCode = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
 		case 2:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Expires", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowEnterprise
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= (int(b) & 0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthEnterprise
+			}
+			postIndex := iNdEx + msglen
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			if m.Expires == nil {
+				m.Expires = &google_protobuf.Timestamp{}
+			}
+			if err := m.Expires.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipEnterprise(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if skippy < 0 {
+				return ErrInvalidLengthEnterprise
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *TokenInfo) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowEnterprise
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= (uint64(b) & 0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: TokenInfo: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: TokenInfo: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field Expires", wireType)
 			}
@@ -739,6 +925,39 @@ func (m *ActivateResponse) Unmarshal(dAtA []byte) error {
 			return fmt.Errorf("proto: ActivateResponse: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Info", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowEnterprise
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= (int(b) & 0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthEnterprise
+			}
+			postIndex := iNdEx + msglen
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			if m.Info == nil {
+				m.Info = &TokenInfo{}
+			}
+			if err := m.Info.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
 			skippy, err := skipEnterprise(dAtA[iNdEx:])
@@ -858,6 +1077,39 @@ func (m *GetStateResponse) Unmarshal(dAtA []byte) error {
 					break
 				}
 			}
+		case 2:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Info", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowEnterprise
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= (int(b) & 0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthEnterprise
+			}
+			postIndex := iNdEx + msglen
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			if m.Info == nil {
+				m.Info = &TokenInfo{}
+			}
+			if err := m.Info.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
 			skippy, err := skipEnterprise(dAtA[iNdEx:])
@@ -987,26 +1239,29 @@ var (
 func init() { proto.RegisterFile("client/enterprise/enterprise.proto", fileDescriptorEnterprise) }
 
 var fileDescriptorEnterprise = []byte{
-	// 326 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xb4, 0x50, 0xcb, 0x4e, 0xc2, 0x40,
-	0x14, 0xed, 0xa0, 0x3c, 0xbc, 0x24, 0x30, 0xcc, 0x8a, 0xa0, 0xa9, 0xa4, 0x1b, 0x08, 0x8b, 0x92,
-	0xa0, 0x3b, 0x57, 0x88, 0x0d, 0x61, 0x83, 0xa4, 0x12, 0xe3, 0xce, 0x40, 0xb9, 0x92, 0x49, 0xa0,
-	0x33, 0x74, 0x06, 0xe3, 0x9f, 0xe8, 0x27, 0xb9, 0xf4, 0x13, 0x0c, 0xfe, 0x88, 0xa1, 0x63, 0x2d,
-	0x10, 0xb7, 0xee, 0x66, 0xce, 0x3d, 0x39, 0x2f, 0x70, 0x82, 0x05, 0xc7, 0x50, 0xb7, 0x31, 0xd4,
-	0x18, 0xc9, 0x88, 0x2b, 0xdc, 0x79, 0xba, 0x32, 0x12, 0x5a, 0x30, 0x48, 0x91, 0xda, 0xf9, 0x5c,
-	0x88, 0xf9, 0x02, 0xdb, 0xf1, 0x65, 0xba, 0x7e, 0x6a, 0x6b, 0xbe, 0x44, 0xa5, 0x27, 0x4b, 0x69,
-	0xc8, 0xce, 0x0a, 0xa8, 0xf7, 0x4b, 0xf7, 0x31, 0x10, 0xd1, 0x8c, 0x35, 0xa0, 0x3c, 0x09, 0x34,
-	0x7f, 0x9e, 0x68, 0x2e, 0xc2, 0xc7, 0x40, 0xcc, 0xb0, 0x4a, 0xea, 0xa4, 0x79, 0xe2, 0x97, 0x52,
-	0xb8, 0x27, 0x66, 0xc8, 0x2e, 0x21, 0x8f, 0x2f, 0x92, 0x47, 0xa8, 0xaa, 0x99, 0x3a, 0x69, 0x16,
-	0x3b, 0x35, 0xd7, 0xf8, 0xb9, 0x89, 0x9f, 0x3b, 0x4e, 0xfc, 0xfc, 0x84, 0xea, 0x48, 0x28, 0x77,
-	0x8d, 0x0e, 0xfa, 0xb8, 0x5a, 0xa3, 0xd2, 0xff, 0xed, 0xc8, 0x80, 0xa6, 0x8e, 0x4a, 0x8a, 0x50,
-	0xa1, 0x53, 0x81, 0x72, 0x1f, 0xf5, 0x9d, 0x4e, 0x53, 0x38, 0x57, 0x40, 0x53, 0xc8, 0xd0, 0x58,
-	0x03, 0xb2, 0x6a, 0x0b, 0xc4, 0x79, 0x4a, 0x9d, 0x8a, 0xbb, 0x33, 0xb7, 0x61, 0x9a, 0x7b, 0xab,
-	0x05, 0xd9, 0xf8, 0xcf, 0x0a, 0x70, 0x3c, 0xbc, 0x1d, 0x7a, 0xd4, 0x62, 0x00, 0xb9, 0x6e, 0x6f,
-	0x3c, 0xb8, 0xf7, 0x28, 0x61, 0x45, 0xc8, 0x7b, 0x0f, 0xa3, 0x81, 0xef, 0xdd, 0xd0, 0x4c, 0xe7,
-	0x95, 0xc0, 0x51, 0x77, 0x34, 0x60, 0x7d, 0x28, 0x24, 0xb9, 0xd8, 0xe9, 0xae, 0xf2, 0xc1, 0x3e,
-	0xb5, 0xb3, 0xbf, 0x8f, 0x3f, 0x55, 0xac, 0xad, 0x50, 0x92, 0x7c, 0x5f, 0xe8, 0xa0, 0xe2, 0xbe,
-	0xd0, 0x61, 0x59, 0xc7, 0xba, 0xa6, 0xef, 0x1b, 0x9b, 0x7c, 0x6c, 0x6c, 0xf2, 0xb9, 0xb1, 0xc9,
-	0xdb, 0x97, 0x6d, 0x4d, 0x73, 0xf1, 0xb0, 0x17, 0xdf, 0x01, 0x00, 0x00, 0xff, 0xff, 0xf0, 0x90,
-	0x42, 0x61, 0x7a, 0x02, 0x00, 0x00,
+	// 369 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xb4, 0x91, 0xcf, 0x4e, 0xea, 0x40,
+	0x14, 0xc6, 0x3b, 0x5c, 0xfe, 0x1e, 0x12, 0x28, 0x93, 0xdc, 0x84, 0x70, 0x6f, 0x2a, 0xe9, 0x06,
+	0x64, 0x51, 0x12, 0x74, 0xeb, 0xa2, 0x62, 0x43, 0xba, 0x41, 0x52, 0x89, 0x71, 0x67, 0xa0, 0x9c,
+	0x92, 0x46, 0xe8, 0x94, 0xce, 0x60, 0x7c, 0x13, 0x7d, 0x24, 0x97, 0x3e, 0x82, 0xc1, 0x17, 0x31,
+	0xb4, 0x94, 0x16, 0x62, 0xa2, 0x1b, 0x77, 0xcd, 0x39, 0x5f, 0x7f, 0xdf, 0xf7, 0x9d, 0x01, 0xd5,
+	0x5e, 0xb8, 0xe8, 0x89, 0x2e, 0x7a, 0x02, 0x03, 0x3f, 0x70, 0x39, 0xa6, 0x3e, 0x35, 0x3f, 0x60,
+	0x82, 0x51, 0x48, 0x26, 0x8d, 0x93, 0x39, 0x63, 0xf3, 0x05, 0x76, 0xc3, 0xcd, 0x74, 0xed, 0x74,
+	0x85, 0xbb, 0x44, 0x2e, 0x26, 0x4b, 0x3f, 0x12, 0xab, 0x2b, 0x90, 0x8d, 0xbd, 0xdc, 0x42, 0x9b,
+	0x05, 0x33, 0xda, 0x82, 0xea, 0xc4, 0x16, 0xee, 0xe3, 0x44, 0xb8, 0xcc, 0xbb, 0xb7, 0xd9, 0x0c,
+	0xeb, 0xa4, 0x49, 0xda, 0x25, 0xab, 0x92, 0x8c, 0xfb, 0x6c, 0x86, 0xf4, 0x1c, 0x0a, 0xf8, 0xe4,
+	0xbb, 0x01, 0xf2, 0x7a, 0xa6, 0x49, 0xda, 0xe5, 0x5e, 0x43, 0x8b, 0xfc, 0xb4, 0xd8, 0x4f, 0x1b,
+	0xc7, 0x7e, 0x56, 0x2c, 0x55, 0x75, 0x28, 0x8d, 0xd9, 0x03, 0x7a, 0xa6, 0xe7, 0xb0, 0x34, 0x82,
+	0xfc, 0x1c, 0xe1, 0x43, 0x55, 0x8f, 0xa2, 0xa0, 0x85, 0xab, 0x35, 0x72, 0xf1, 0xdb, 0xa1, 0x2f,
+	0x40, 0x4e, 0x1c, 0xb9, 0xcf, 0x3c, 0x8e, 0xf4, 0x14, 0xb2, 0xae, 0xe7, 0xb0, 0x5d, 0xf0, 0xbf,
+	0x5a, 0xea, 0x25, 0xf6, 0x05, 0xad, 0x50, 0xa2, 0xd6, 0xa0, 0x3a, 0x40, 0x71, 0x23, 0x92, 0xc0,
+	0xaa, 0x03, 0x72, 0x32, 0xda, 0x11, 0x5b, 0x90, 0xe3, 0xdb, 0x41, 0x88, 0xac, 0xf4, 0x6a, 0x69,
+	0x64, 0xa4, 0x8c, 0xf6, 0x7b, 0xeb, 0xcc, 0xb7, 0xd6, 0x9d, 0x0e, 0xe4, 0xc2, 0x5f, 0x69, 0x11,
+	0xb2, 0xc3, 0xeb, 0xa1, 0x21, 0x4b, 0x14, 0x20, 0xaf, 0xf7, 0xc7, 0xe6, 0xad, 0x21, 0x13, 0x5a,
+	0x86, 0x82, 0x71, 0x37, 0x32, 0x2d, 0xe3, 0x4a, 0xce, 0xf4, 0x9e, 0x09, 0xfc, 0xd1, 0x47, 0x26,
+	0x1d, 0x40, 0x31, 0x6e, 0x4b, 0xff, 0xa5, 0xe1, 0x47, 0x57, 0x6f, 0xfc, 0xff, 0x7a, 0x19, 0xd5,
+	0x51, 0xa5, 0x2d, 0x28, 0x2e, 0x79, 0x08, 0x3a, 0xba, 0xc6, 0x21, 0xe8, 0xf8, 0x2e, 0xaa, 0x74,
+	0x29, 0xbf, 0x6e, 0x14, 0xf2, 0xb6, 0x51, 0xc8, 0xfb, 0x46, 0x21, 0x2f, 0x1f, 0x8a, 0x34, 0xcd,
+	0x87, 0xcf, 0x75, 0xf6, 0x19, 0x00, 0x00, 0xff, 0xff, 0x70, 0x83, 0x0d, 0xb1, 0x13, 0x03, 0x00,
+	0x00,
 }
