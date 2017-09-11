1e9ee58305385b8c902b82a4105ea935fbe8fb12
Derek Chiang <derekchiang93@gmail.com>
2017-09-11 13:43:03 -0700

    Implement SyncFile

--- src/server/pfs/server/hash.go
+++ src/server/pfs/server/hash.go
@@ -3,13 +3,18 @@ package server
 import (
 	"crypto/sha512"
 	"encoding/base64"
+	"encoding/hex"
 	"hash"
 
 	"github.com/pachyderm/pachyderm/src/client/pfs"
 )
 
-func newHash() hash.Hash {
+func NewHash() hash.Hash {
 	return sha512.New()
+}
+
+func EncodeHash(bytes []byte) string {
+	return hex.EncodeToString(bytes)
 }
 
 func getBlock(hash hash.Hash) *pfs.Block {
