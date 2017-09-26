5fb8162c36e7dfe42096997d29399113f15f63d4
Derek Chiang <derekchiang93@gmail.com>
2017-09-26 13:48:45 -0700

    Add a failing test for a pipeline that processes large files

--- src/server/pachyderm_test.go
+++ src/server/pachyderm_test.go
@@ -28,6 +28,7 @@ import (
 	"github.com/pachyderm/pachyderm/src/client/pps"
 	pfspretty "github.com/pachyderm/pachyderm/src/server/pfs/pretty"
 	"github.com/pachyderm/pachyderm/src/server/pkg/backoff"
+	"github.com/pachyderm/pachyderm/src/server/pkg/workload"
 	ppsserver "github.com/pachyderm/pachyderm/src/server/pps"
 	ppspretty "github.com/pachyderm/pachyderm/src/server/pps/pretty"
 
@@ -89,6 +90,64 @@ func TestPipelineWithParallelism(t *testing.T) {
 		var buf bytes.Buffer
 		require.NoError(t, c.GetFile(commitInfos[0].Commit.Repo.Name, commitInfos[0].Commit.ID, fmt.Sprintf("file-%d", i), 0, 0, &buf))
 		require.Equal(t, fmt.Sprintf("%d", i), buf.String())
+	}
+}
+
+func TestPipelineWithLargeFiles(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping integration tests in short mode")
+	}
+
+	c := getPachClient(t)
+	defer require.NoError(t, c.DeleteAll())
+
+	dataRepo := uniqueString("TestPipelineInputDataModification_data")
+	require.NoError(t, c.CreateRepo(dataRepo))
+
+	seed := int64(99)
+	r := rand.New(rand.NewSource(seed))
+	randContent := func(r *rand.Rand, i int) string {
+		return workload.RandString(r, int(pfs.ChunkSize)+i*1024*1024)
+	}
+
+	numFiles := 10
+	commit1, err := c.StartCommit(dataRepo, "master")
+	require.NoError(t, err)
+	for i := 0; i < numFiles; i++ {
+		_, err = c.PutFile(dataRepo, commit1.ID, fmt.Sprintf("file-%d", i),
+			strings.NewReader(randContent(r, i)))
+	}
+	require.NoError(t, c.FinishCommit(dataRepo, commit1.ID))
+
+	pipeline := uniqueString("pipeline")
+	require.NoError(t, c.CreatePipeline(
+		pipeline,
+		"",
+		[]string{"bash"},
+		[]string{
+			fmt.Sprintf("cp /pfs/%s/* /pfs/out/", dataRepo),
+		},
+		nil,
+		client.NewAtomInput(dataRepo, "/*"),
+		"",
+		false,
+	))
+
+	commitIter, err := c.FlushCommit([]*pfs.Commit{commit1}, nil)
+	require.NoError(t, err)
+	commitInfos := collectCommitInfos(t, commitIter)
+	require.Equal(t, 1, len(commitInfos))
+
+	// Use the same rand generator to get the same content
+	r = rand.New(rand.NewSource(seed))
+	for i := 0; i < numFiles; i++ {
+		var buf bytes.Buffer
+		require.NoError(t, c.GetFile(commitInfos[0].Commit.Repo.Name, commitInfos[0].Commit.ID, fmt.Sprintf("file-%d", i), 0, 0, &buf))
+		// we don't wanna use the `require` package here since it prints
+		// the strings, which would clutter the output.
+		if randContent(r, i) != buf.String() {
+			t.Fatalf("file content does not match")
+		}
 	}
 }
 
