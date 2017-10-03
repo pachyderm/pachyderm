24ec53363e0436b7b46b4ba840fd201283f88e93
Gabriel Grant <g@briel.ca>
2017-10-03 08:30:17 -0700

    bump dash version to 0.5.10

--- src/server/pkg/deploy/cmds/cmds.go
+++ src/server/pkg/deploy/cmds/cmds.go
@@ -23,7 +23,7 @@ import (
 	"go.pedge.io/pkg/cobra"
 )
 
-var defaultDashImage = "pachyderm/dash:0.5.9"
+var defaultDashImage = "pachyderm/dash:0.5.10"
 
 func maybeKcCreate(dryRun bool, manifest *bytes.Buffer, opts *assets.AssetOpts, metrics bool) error {
 	if dryRun {
