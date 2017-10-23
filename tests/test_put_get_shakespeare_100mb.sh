6c709650a3b8df20deb233c5ed2ba8dabd5e4a27
Matthew Steffen <msteffen@pachyderm.io>
2017-10-23 09:20:49 -0700

    fix test_put_get_shakespeare_100mb.sh

--- tests/test_put_get_shakespeare_100mb.sh
+++ tests/test_put_get_shakespeare_100mb.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+RANGE=gcs
 if [[ "${1}" == "--all" ]]; then
   ALL=true
   RANGE=gcs gsutil-stream cat-stream local-path
@@ -9,20 +10,28 @@ set -x
 
 THIS_DIR="$(dirname $0)"
 
+############
 # Delete all repos from previous experiments
+############
 [[ -d ./shakespeare_test ]] && rm -r ./shakespeare_test
 for r in gcs gsutil-stream cat-stream local-path; do
   pachctl delete-repo shakespeare-${r}
+done
+for r in ${RANGE}; do
   pachctl create-repo shakespeare-${r}
   mkdir -p ./shakespeare_test/${r}/{output,comparison}
 done
 
+############
 # Get data into pachyderm via GCS bucket path
+############
 COMMIT=$(pachctl start-commit shakespeare-gcs master | tee /dev/stderr)
 pachctl put-file shakespeare-gcs "${COMMIT}" / -r -f gs://shakespeare_plays
 pachctl finish-commit shakespeare-gcs "${COMMIT}"
 
+############
 # Get data into/out of pachyderm via all other methods (local path, stdin, etc)
+############
 if [[ "${ALL}" == "true" ]]; then
   COMMIT=$(pachctl start-commit shakespeare-cat-stream master | tee /dev/stderr)
   for f in 1mb 100mb; do
@@ -43,7 +52,9 @@ if [[ "${ALL}" == "true" ]]; then
   pachctl finish-commit shakespeare-local-path "${COMMIT}"
 fi
 
+############
 # Get data back out of pachyderm
+############
 for r in ${RANGE}; do
   for f in 1mb 100mb; do
     pachctl get-file shakespeare-${r} master /shakespeare_plays_${f}.txt -o shakespeare_test/${r}/output/shakespeare_plays_${f}.txt
@@ -59,4 +70,3 @@ find testdata/shakespeare_plays/ -name *.txt | while read f; do md5sum $f; done
 echo -e "\n############## experimental data ##############"
 ls -lR shakespeare_test/*/output/
 find shakespeare_test/*/output/ -name *.txt | while read f; do md5sum $f; done
-
