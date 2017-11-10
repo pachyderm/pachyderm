02853ff4f575b9eb93bcdc53d736c2eb55c424eb
Sean Jezewski <seanwjezewski@gmail.com>
2017-11-10 12:24:05 -0800

    Revert k8s launch script

--- etc/kube/internal.sh
+++ etc/kube/internal.sh
@@ -1,19 +1,14 @@
 set -Ee
 
-#cat /etc/kubernetes/manifests/master.json | jq '.spec.containers[1].command |= . + [ "--storage-backend=etcd2" ]' >/etc/kubernetes/manifests/master2.json
-#mv /etc/kubernetes/manifests/master2.json /etc/kubernetes/manifests/master.json
-echo 'kubeconfig:'
-cat $1
-
-#    --pod-manifest-path=/etc/kubernetes/manifests \
-
+cat /etc/kubernetes/manifests/master.json | jq '.spec.containers[1].command |= . + [ "--storage-backend=etcd2" ]' >/etc/kubernetes/manifests/master2.json
+mv /etc/kubernetes/manifests/master2.json /etc/kubernetes/manifests/master.json
 /hyperkube kubelet \
     --containerized \
-    --kubeconfig=$1 \
     --hostname-override="127.0.0.1" \
     --address="0.0.0.0" \
+    --api-servers=http://localhost:8080 \
     --cluster_dns=10.0.0.10 \
     --cluster_domain=cluster.local \
+    --pod-manifest-path=/etc/kubernetes/manifests \
     --allow-privileged=true \
-    --feature-gates="Accelerators=true" \
-    --fail-swap-on=false
+    --feature-gates="Accelerators=true"
