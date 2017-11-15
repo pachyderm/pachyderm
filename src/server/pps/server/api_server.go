01b0e52f52f24079639ed8e3943af332f44baf8e
Sean Jezewski <seanwjezewski@gmail.com>
2017-11-15 12:24:37 -0800

    Omit setting default mem/cpu

--- src/server/pps/server/api_server.go
+++ src/server/pps/server/api_server.go
@@ -1619,9 +1619,7 @@ func setPipelineDefaults(pipelineInfo *pps.PipelineInfo) {
 	}
 	if pipelineInfo.ResourceLimits == nil {
 		pipelineInfo.ResourceLimits = &pps.ResourceSpec{
-			Memory: "64G",
-			Cpu:    16,
-			Gpu:    0,
+			Gpu: 0,
 		}
 
 	}
