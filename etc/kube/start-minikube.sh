0ba4492af409e03ec945561545b412e282ab940f
Matthew Steffen <msteffen@pachyderm.io>
2017-11-17 12:41:32 -0800

    Make pachyderm run on k8s 1.7 or 1.8, and add a small test for k8s 1.7 (our main test suite already uses 1.8)

--- etc/kube/start-minikube.sh
+++ etc/kube/start-minikube.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 
-set -Ee
+set -Eex
 
-sudo CHANGE_MINIKUBE_NONE_USER=true minikube start --vm-driver=none --kubernetes-version=v1.8.0
+# Parse flags
+VERSION=v1.8.0
+while getopts ":v:" opt; do
+  case "${opt}" in
+    v)
+      VERSION="v${OPTARG}"
+      ;;
+    \?)
+      echo "Invalid argument: ${opt}"
+      exit 1
+      ;;
+  esac
+done
+
+sudo CHANGE_MINIKUBE_NONE_USER=true minikube start --vm-driver=none --kubernetes-version="${VERSION}"
 until kubectl version 2>/dev/null >/dev/null; do sleep 5; done
