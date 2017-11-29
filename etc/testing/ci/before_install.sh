4b7a240ad182cfab41d542f7a1471a079af6ae54
Omer Katz <omer.drow@gmail.com>
2017-11-29 08:06:37 +0200

    Don't run spellchecker in CI yet.

--- etc/testing/ci/before_install.sh
+++ etc/testing/ci/before_install.sh
@@ -20,7 +20,3 @@ curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.8.
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.23.0/minikube-linux-amd64 && \
     chmod +x ./minikube && \
     sudo mv ./minikube /usr/local/bin/minikube
-
-curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo npm install -g markdown-spellcheck
