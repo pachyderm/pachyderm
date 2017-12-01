b8dd637b4b7ae0e6c65af53e7b2d58b458e84902
Sean Jezewski <seanwjezewski@gmail.com>
2017-12-01 15:19:35 -0800

    Try a different IP range

--- etc/kube/start-minikube.sh
+++ etc/kube/start-minikube.sh
@@ -16,5 +16,5 @@ while getopts ":v:" opt; do
   esac
 done
 
-sudo CHANGE_MINIKUBE_NONE_USER=true minikube start --vm-driver=none --kubernetes-version="${VERSION}" --extra-config=kubelet.CertDir=/var/lib/kubelet/pki
+sudo CHANGE_MINIKUBE_NONE_USER=true minikube start --vm-driver=none --kubernetes-version="${VERSION}" --extra-config=kubelet.CertDir=/var/lib/kubelet/pki --extra-config=apiserver.ServiceClusterIpRange=10.0.0.1/12
 until kubectl version 2>/dev/null >/dev/null; do sleep 5; done
