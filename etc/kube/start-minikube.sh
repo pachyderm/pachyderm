7eb22ada2380d7448527d1765f8575f216f333a9
Sean Jezewski <seanwjezewski@gmail.com>
2017-12-01 16:00:06 -0800

    Try hacking at the DNS layer

--- etc/kube/start-minikube.sh
+++ etc/kube/start-minikube.sh
@@ -2,6 +2,15 @@
 
 set -Eex
 
+echo 'pre resolv conf:'
+cat /etc/resolve.conf
+sudo echo 'nameserver 8.8.8.8' > /etc/resolv.conf
+echo 'post resolv conf:'
+cat /etc/resolve.conf
+
+sudo systemctl stop systemd-resolved
+sudo systemctl disable systemd-resolved
+
 # Parse flags
 VERSION=v1.8.0
 while getopts ":v:" opt; do
@@ -16,5 +25,6 @@ while getopts ":v:" opt; do
   esac
 done
 
-sudo CHANGE_MINIKUBE_NONE_USER=true minikube start --vm-driver=none --kubernetes-version="${VERSION}" --extra-config=kubelet.CertDir=/var/lib/kubelet/pki --extra-config=apiserver.ServiceClusterIpRange=10.0.0.1/12
+#sudo CHANGE_MINIKUBE_NONE_USER=true minikube start --vm-driver=none --kubernetes-version="${VERSION}" --extra-config=kubelet.CertDir=/var/lib/kubelet/pki --extra-config=apiserver.ServiceClusterIpRange=10.0.0.1/12
+sudo CHANGE_MINIKUBE_NONE_USER=true minikube start --vm-driver=none --kubernetes-version="${VERSION}" --extra-config=kubelet.CertDir=/var/lib/kubelet/pki
 until kubectl version 2>/dev/null >/dev/null; do sleep 5; done
