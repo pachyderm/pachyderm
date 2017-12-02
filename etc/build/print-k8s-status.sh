117bfb5d5e90716e18e3f07d8aa781e00aefb8de
Sean Jezewski <seanwjezewski@gmail.com>
2017-12-01 16:37:04 -0800

    Try some debug output too

---
+++ etc/build/print-k8s-status.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function dump() {
+	date
+	
+	kubectl get all
+
+    kubectl --namespace=kube-system get pod
+    ls /etc/kubernetes/pki/
+    kubectl --namespace=kube-system get pod | grep apiserver | cut -f 1 -d " " | while read pod; do kubectl --namespace=kube-system exec $pod -- ps -a; done
+    
+	
+	# Get node status
+	kubectl get node | tail -n 1 | cut -f 1 -d " " | while read node; do kubectl get node/$node -o yaml; done
+	
+    # too verbose for now
+	#kubectl describe pod -l app=pachd
+	#kubectl logs -l app=pachd
+
+    minikube logs
+}
+
+while true; do
+	dump
+	sleep 10
+done
