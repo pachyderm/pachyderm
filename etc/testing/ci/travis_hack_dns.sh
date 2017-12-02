ed2b6396f6e5cf61084e5dba770a0109f2631827
Sean Jezewski <seanwjezewski@gmail.com>
2017-12-01 16:20:19 -0800

    Try looking for resolv conf in other places

--- etc/testing/ci/travis_hack_dns.sh
+++ etc/testing/ci/travis_hack_dns.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+set -Eex
+
+echo 'looking for resolv.conf files'
+echo 'run/systemd/resolve/resolv.conf:'
+cat /run/systemd/resolve/resolv.conf || true
+echo '/etc/systemd/resolved.conf:'
+cat /etc/systemd/resolved.conf || true
 
 touch /etc/resolve.conf
 echo 'pre resolv conf:'
@@ -7,5 +14,5 @@ cat /etc/resolve.conf || true
 echo 'nameserver 8.8.8.8' > /etc/resolv.conf
 echo 'post resolv conf:'
 cat /etc/resolve.conf || true
-sudo systemctl stop systemd-resolved
-sudo systemctl disable systemd-resolved
+sudo systemctl stop systemd-resolved || true
+sudo systemctl disable systemd-resolved || true
