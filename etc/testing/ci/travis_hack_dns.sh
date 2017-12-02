779f83058d00febe22a9a70581af372661509327
Sean Jezewski <seanwjezewski@gmail.com>
2017-12-01 16:12:41 -0800

    once more with feeling

--- etc/testing/ci/travis_hack_dns.sh
+++ etc/testing/ci/travis_hack_dns.sh
@@ -7,5 +7,5 @@ cat /etc/resolve.conf || true
 echo 'nameserver 8.8.8.8' > /etc/resolv.conf
 echo 'post resolv conf:'
 cat /etc/resolve.conf || true
-systemctl stop systemd-resolved
-systemctl disable systemd-resolved
+sudo systemctl stop systemd-resolved
+sudo systemctl disable systemd-resolved
