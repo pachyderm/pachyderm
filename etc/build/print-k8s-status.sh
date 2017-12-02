50a2955e1dd330ed66b243156d0c225eb5b6bfd2
Sean Jezewski <seanwjezewski@gmail.com>
2017-12-01 16:56:37 -0800

    Print more logs

--- etc/build/print-k8s-status.sh
+++ etc/build/print-k8s-status.sh
@@ -6,16 +6,19 @@ function dump() {
 	kubectl get all
 
     kubectl --namespace=kube-system get pod
-    ls /etc/kubernetes/pki/
+    ls /etc/kubernetes/pki/ || true
     kubectl --namespace=kube-system get pod | grep apiserver | cut -f 1 -d " " | while read pod; do kubectl --namespace=kube-system exec $pod -- ps -a; done
     
+    echo 'dns logs'
+    kubectl --namespace=kube-system get pod | grep dns | cut -f 1 -d " " | while read pod; do kubectl --namespace=kube-system logs po/$pod kubedns; done
 	
 	# Get node status
-	kubectl get node | tail -n 1 | cut -f 1 -d " " | while read node; do kubectl get node/$node -o yaml; done
+	# kubectl get node | tail -n 1 | cut -f 1 -d " " | while read node; do kubectl get node/$node -o yaml; done
 	
     # too verbose for now
-	#kubectl describe pod -l app=pachd
-	#kubectl logs -l app=pachd
+	kubectl describe pod -l app=pachd
+    echo 'pachd logs:'
+	kubectl logs -l app=pachd
 
     minikube logs
 }
