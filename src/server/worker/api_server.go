5bd268a93d61a2417c2559989dc0bb2668f1bccc
Joe Doliner <jdoliner@gmail.com>
2017-12-08 11:56:16 -0800

    Don't block on chunks for completed jobs.

--- src/server/worker/api_server.go
+++ src/server/worker/api_server.go
@@ -906,6 +906,11 @@ func (a *APIServer) worker() {
 			if err := e.Unmarshal(&jobID, jobInfo); err != nil {
 				return fmt.Errorf("error unmarshalling: %v", err)
 			}
+			// Don't bother with already completed jobs.
+			switch jobInfo.State {
+			case pps.JobState_JOB_FAILURE, pps.JobState_JOB_SUCCESS, pps.JobState_JOB_KILLED:
+				continue
+			}
 			df, err := NewDatumFactory(ctx, a.pachClient.PfsAPIClient, jobInfo.Input)
 			if err != nil {
 				return fmt.Errorf("error from NewDatumFactory: %v", err)
