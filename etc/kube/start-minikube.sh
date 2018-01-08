49a5b81ee407985081a2ef15551e489b97800896
Joe Doliner <jdoliner@gmail.com>
2018-01-08 13:33:13 -0800

    Fix kube-dns (RBAC breaks it).

--- etc/kube/start-minikube.sh
+++ etc/kube/start-minikube.sh
@@ -18,3 +18,7 @@ done
 
 sudo CHANGE_MINIKUBE_NONE_USER=true minikube start --vm-driver=none --kubernetes-version="${VERSION}" --extra-config=apiserver.Authorization.Mode=RBAC
 until kubectl version 2>/dev/null >/dev/null; do sleep 5; done
+
+# Apply some manual changes to fix DNS.
+kubectl -n kube-system create sa kube-dns
+until kubectl -n kube-system patch deploy/kube-dns -p '{"spec": {"template": {"spec": {"serviceAccountName": "kube-dns"}}}}' 2>/dev/null >/dev/null; do sleep 5; done
