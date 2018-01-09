565e1b1e4ab15cce65d8cf2ef31243c440d24ee7
Joe Doliner <jdoliner@gmail.com>
2018-01-08 16:12:46 -0800

    Make test-kube-17 pass by hiding rbac behind flags

--- etc/kube/start-minikube.sh
+++ etc/kube/start-minikube.sh
@@ -4,10 +4,14 @@ set -Eex
 
 # Parse flags
 VERSION=v1.8.0
-while getopts ":v:" opt; do
+RBAC=""
+while getopts ":v:r" opt; do
   case "${opt}" in
     v)
       VERSION="v${OPTARG}"
+      ;;
+    r)
+      RBAC="--extra-config=apiserver.Authorization.Mode=RBAC"
       ;;
     \?)
       echo "Invalid argument: ${opt}"
@@ -16,9 +20,9 @@ while getopts ":v:" opt; do
   esac
 done
 
-sudo CHANGE_MINIKUBE_NONE_USER=true minikube start --vm-driver=none --kubernetes-version="${VERSION}" --extra-config=apiserver.Authorization.Mode=RBAC
+sudo CHANGE_MINIKUBE_NONE_USER=true minikube start --vm-driver=none --kubernetes-version="${VERSION}" "${RBAC}"
 until kubectl version 2>/dev/null >/dev/null; do sleep 5; done
 
 # Apply some manual changes to fix DNS.
-## kubectl -n kube-system create sa kube-dns
-## until kubectl -n kube-system patch deploy/kube-dns -p '{"spec": {"template": {"spec": {"serviceAccountName": "kube-dns"}}}}' 2>/dev/null >/dev/null; do sleep 5; done
+kubectl -n kube-system create sa kube-dns
+until kubectl -n kube-system patch deploy/kube-dns -p '{"spec": {"template": {"spec": {"serviceAccountName": "kube-dns"}}}}' 2>/dev/null >/dev/null; do sleep 5; done
