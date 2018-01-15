2e4c0d37029795f92541a2c5b074021c0c8b576c
Sean Jezewski <seanwjezewski@gmail.com>
2018-01-15 11:13:25 -0800

    Fix vet error

--- src/server/pkg/collection/collection.go
+++ src/server/pkg/collection/collection.go
@@ -80,7 +80,7 @@ func (c *collection) indexDir(index Index, indexVal string) string {
 	indexDir := c.prefix
 	// remove trailing slash
 	indexDir = strings.TrimRight(indexDir, "/")
-	return fmt.Sprintf("%s__index_%s/%s", indexDir, index, indexVal)
+	return fmt.Sprintf("%s__index_%v/%s", indexDir, index, indexVal)
 }
 
 // See the documentation for `Index` for details.
