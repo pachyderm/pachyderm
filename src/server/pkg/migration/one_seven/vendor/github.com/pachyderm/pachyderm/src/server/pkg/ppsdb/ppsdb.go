e5e2c013c8cfb5461e84e4354da4d5b4f4648e1c
Joe Doliner <jdoliner@gmail.com>
2018-01-23 14:10:27 -0800

    Adds pfs/pps-dbs for to migration/one_seven.

---
+++ src/server/pkg/migration/one_seven/vendor/github.com/pachyderm/pachyderm/src/server/pkg/ppsdb/ppsdb.go
@@ -0,0 +1,50 @@
+// Package ppsdb contains the database schema that PPS uses.
+package ppsdb
+
+import (
+	"path"
+
+	etcd "github.com/coreos/etcd/clientv3"
+
+	"github.com/pachyderm/pachyderm/src/client/pps"
+	col "github.com/pachyderm/pachyderm/src/server/pkg/collection"
+)
+
+const (
+	pipelinesPrefix = "/pipelines"
+	jobsPrefix      = "/jobs"
+)
+
+var (
+	// JobsPipelineIndex maps pipeline to jobs started by the pipeline
+	JobsPipelineIndex = col.Index{"Pipeline", false}
+
+	// JobsInputIndex maps job inputs (repos + pipeline version) to output
+	// commit. This is how we know if we need to start a job.
+	JobsInputIndex = col.Index{"Input", false}
+
+	// JobsOutputIndex maps job outputs to the job that create them.
+	JobsOutputIndex = col.Index{"OutputCommit", false}
+)
+
+// Pipelines returns a Collection of pipelines
+func Pipelines(etcdClient *etcd.Client, etcdPrefix string) col.Collection {
+	return col.NewCollection(
+		etcdClient,
+		path.Join(etcdPrefix, pipelinesPrefix),
+		[]col.Index{},
+		&pps.EtcdPipelineInfo{},
+		nil,
+	)
+}
+
+// Jobs returns a Collection of jobs
+func Jobs(etcdClient *etcd.Client, etcdPrefix string) col.Collection {
+	return col.NewCollection(
+		etcdClient,
+		path.Join(etcdPrefix, jobsPrefix),
+		[]col.Index{JobsPipelineIndex, JobsOutputIndex},
+		&pps.EtcdJobInfo{},
+		nil,
+	)
+}
