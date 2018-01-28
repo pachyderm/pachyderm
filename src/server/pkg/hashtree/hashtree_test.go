a5def799e5259d2cb74eb6e1dd1861e225e8af62
Matthew Steffen <msteffen@pachyderm.io>
2018-01-27 16:21:57 -0800

    Update hashtree_test.go, after fixing require.go

--- src/server/pkg/hashtree/hashtree_test.go
+++ src/server/pkg/hashtree/hashtree_test.go
@@ -132,7 +132,7 @@ func TestPutFileBasic(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, 2, len(nodes))
 	for _, node := range nodes {
-		require.EqualOneOf(t, i("foo", "dir"), node.Name)
+		require.EqualOneOf(t, i("foo9", "dir"), node.Name)
 	}
 
 	nodes, err = h1.List("/dir")
