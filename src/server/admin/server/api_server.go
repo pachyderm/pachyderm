1a799cd1fd4e46a00f61027e202fc22123ed765b
Joe Doliner <jdoliner@gmail.com>
2018-02-06 13:44:38 -0800

    Implement object filtering in extract.

--- src/server/admin/server/api_server.go
+++ src/server/admin/server/api_server.go
@@ -37,108 +37,114 @@ func (a *apiServer) Extract(request *admin.ExtractRequest, extractServer admin.A
 	}); err != nil {
 		return err
 	}
-	if err := pachClient.ListObject(func(object *pfs.Object) error {
-		bytes, err := pachClient.ReadObject(object.Hash)
-		if err != nil {
-			return err
-		}
-		return extractServer.Send(&admin.Op{Object: &pfs.PutObjectRequest{Value: bytes}})
-	}); err != nil {
-		return err
-	}
-	if err := pachClient.ListTag(func(resp *pfs.ListTagsResponse) error {
-		return extractServer.Send(&admin.Op{
-			Tag: &pfs.TagObjectRequest{
-				Object: resp.Object,
-				Tags:   []*pfs.Tag{resp.Tag},
-			},
-		})
-	}); err != nil {
-		return err
-	}
-	ris, err := pachClient.ListRepo(nil)
-	if err != nil {
-		return err
-	}
-	for _, ri := range ris {
-		if len(ri.Provenance) > 0 {
-			continue
-		}
-		if err := extractServer.Send(&admin.Op{
-			Repo: &pfs.CreateRepoRequest{
-				Repo:        ri.Repo,
-				Provenance:  ri.Provenance,
-				Description: ri.Description,
-			},
+	if !request.NoObjects {
+		if err := pachClient.ListObject(func(object *pfs.Object) error {
+			bytes, err := pachClient.ReadObject(object.Hash)
+			if err != nil {
+				return err
+			}
+			return extractServer.Send(&admin.Op{Object: &pfs.PutObjectRequest{Value: bytes}})
 		}); err != nil {
 			return err
 		}
-		cis, err := pachClient.ListCommit(ri.Repo.Name, "", "", 0)
+		if err := pachClient.ListTag(func(resp *pfs.ListTagsResponse) error {
+			return extractServer.Send(&admin.Op{
+				Tag: &pfs.TagObjectRequest{
+					Object: resp.Object,
+					Tags:   []*pfs.Tag{resp.Tag},
+				},
+			})
+		}); err != nil {
+			return err
+		}
+	}
+	if !request.NoRepos {
+		ris, err := pachClient.ListRepo(nil)
 		if err != nil {
 			return err
 		}
-		for _, ci := range sortCommitInfos(cis) {
-			// Even without a parent, ParentCommit is used to indicate which
-			// repo to make the commit in.
-			if ci.ParentCommit == nil {
-				ci.ParentCommit = client.NewCommit(ri.Repo.Name, "")
+		for _, ri := range ris {
+			if len(ri.Provenance) > 0 {
+				continue
 			}
 			if err := extractServer.Send(&admin.Op{
-				Commit: &pfs.BuildCommitRequest{
-					Parent: ci.ParentCommit,
-					Tree:   ci.Tree,
-					ID:     ci.Commit.ID,
+				Repo: &pfs.CreateRepoRequest{
+					Repo:        ri.Repo,
+					Provenance:  ri.Provenance,
+					Description: ri.Description,
 				},
 			}); err != nil {
 				return err
 			}
-		}
-		bis, err := pachClient.ListBranch(ri.Repo.Name)
-		if err != nil {
-			return err
-		}
-		for _, bi := range bis {
-			if err := extractServer.Send(&admin.Op{
-				Branch: &pfs.SetBranchRequest{
-					Commit: bi.Head,
-					Branch: bi.Name,
-				},
-			},
-			); err != nil {
+			cis, err := pachClient.ListCommit(ri.Repo.Name, "", "", 0)
+			if err != nil {
 				return err
+			}
+			for _, ci := range sortCommitInfos(cis) {
+				// Even without a parent, ParentCommit is used to indicate which
+				// repo to make the commit in.
+				if ci.ParentCommit == nil {
+					ci.ParentCommit = client.NewCommit(ri.Repo.Name, "")
+				}
+				if err := extractServer.Send(&admin.Op{
+					Commit: &pfs.BuildCommitRequest{
+						Parent: ci.ParentCommit,
+						Tree:   ci.Tree,
+						ID:     ci.Commit.ID,
+					},
+				}); err != nil {
+					return err
+				}
+			}
+			bis, err := pachClient.ListBranch(ri.Repo.Name)
+			if err != nil {
+				return err
+			}
+			for _, bi := range bis {
+				if err := extractServer.Send(&admin.Op{
+					Branch: &pfs.SetBranchRequest{
+						Commit: bi.Head,
+						Branch: bi.Name,
+					},
+				},
+				); err != nil {
+					return err
+				}
 			}
 		}
 	}
-	pis, err := pachClient.ListPipeline()
-	if err != nil {
-		return err
-	}
-	for _, pi := range pis {
-		if err := extractServer.Send(&admin.Op{
-			Pipeline: &pps.CreatePipelineRequest{
-				Pipeline:           pi.Pipeline,
-				Transform:          pi.Transform,
-				ParallelismSpec:    pi.ParallelismSpec,
-				Egress:             pi.Egress,
-				OutputBranch:       pi.OutputBranch,
-				ScaleDownThreshold: pi.ScaleDownThreshold,
-				ResourceRequests:   pi.ResourceRequests,
-				ResourceLimits:     pi.ResourceLimits,
-				Input:              pi.Input,
-				Description:        pi.Description,
-				Incremental:        pi.Incremental,
-				CacheSize:          pi.CacheSize,
-				EnableStats:        pi.EnableStats,
-				Batch:              pi.Batch,
-				MaxQueueSize:       pi.MaxQueueSize,
-				Service:            pi.Service,
-				ChunkSpec:          pi.ChunkSpec,
-				DatumTimeout:       pi.DatumTimeout,
-				JobTimeout:         pi.JobTimeout,
-				Salt:               pi.Salt,
-			},
-		}); err != nil {
+	if !request.NoPipelines {
+		pis, err := pachClient.ListPipeline()
+		if err != nil {
 			return err
+		}
+		for _, pi := range pis {
+			if err := extractServer.Send(&admin.Op{
+				Pipeline: &pps.CreatePipelineRequest{
+					Pipeline:           pi.Pipeline,
+					Transform:          pi.Transform,
+					ParallelismSpec:    pi.ParallelismSpec,
+					Egress:             pi.Egress,
+					OutputBranch:       pi.OutputBranch,
+					ScaleDownThreshold: pi.ScaleDownThreshold,
+					ResourceRequests:   pi.ResourceRequests,
+					ResourceLimits:     pi.ResourceLimits,
+					Input:              pi.Input,
+					Description:        pi.Description,
+					Incremental:        pi.Incremental,
+					CacheSize:          pi.CacheSize,
+					EnableStats:        pi.EnableStats,
+					Batch:              pi.Batch,
+					MaxQueueSize:       pi.MaxQueueSize,
+					Service:            pi.Service,
+					ChunkSpec:          pi.ChunkSpec,
+					DatumTimeout:       pi.DatumTimeout,
+					JobTimeout:         pi.JobTimeout,
+					Salt:               pi.Salt,
+				},
+			}); err != nil {
+				return err
+			}
 		}
 	}
 	return nil
