cfd2c38c7f26952c09d9c4e4f7779b62e096b619
Joe Doliner <jdoliner@gmail.com>
2018-02-09 12:53:34 -0800

    Fix lint issues.

--- src/client/health.go
+++ src/client/health.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gogo/protobuf/types"
 )
 
+// Health health checks pachd, it returns an error if pachd isn't healthy.
 func (c APIClient) Health() error {
 	_, err := c.healthClient.Health(c.Ctx(), &types.Empty{})
 	return grpcutil.ScrubGRPC(err)
