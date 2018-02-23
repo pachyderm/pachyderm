18bd0f4f182bfde513aee28b1cbcf946529b5ef3
Sean Jezewski <seanwjezewski@gmail.com>
2018-02-22 17:23:13 -0800

    Update vault config to accept pachd address

--- src/plugin/vault/pachyderm/backend.go
+++ src/plugin/vault/pachyderm/backend.go
@@ -3,20 +3,14 @@ package pachyderm
 import (
 	"github.com/hashicorp/vault/logical"
 	"github.com/hashicorp/vault/logical/framework"
-	pclient "github.com/pachyderm/pachyderm/src/client"
 )
 
 type backend struct {
 	*framework.Backend
-	PachydermClient *pclient.APIClient
 }
 
 func Backend(c *logical.BackendConfig) (*backend, error) {
 	var b backend
-	client, err := pclient.NewFromAddress("127.0.0.1:30650")
-	if err != nil {
-		return nil, err
-	}
 	b = backend{
 		Backend: &framework.Backend{
 			BackendType: logical.TypeLogical,
@@ -30,7 +24,6 @@ func Backend(c *logical.BackendConfig) (*backend, error) {
 				b.revokePath(),
 			},
 		},
-		PachydermClient: client,
 	}
 
 	return &b, nil
