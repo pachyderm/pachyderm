7ccf3c396537fd366572023f33179a78f61b5712
Matthew Steffen <msteffen@pachyderm.io>
2018-03-16 15:20:55 -0700

    Fix PFS tests

--- src/server/auth/testing/auth.go
+++ src/server/auth/testing/auth.go
@@ -14,70 +14,70 @@ type InactiveAPIServer struct{}
 
 // Activate implements the Activate RPC, but just returns NotActivatedError
 func (a *InactiveAPIServer) Activate(ctx context.Context, req *auth.ActivateRequest) (resp *auth.ActivateResponse, retErr error) {
-	return nil, auth.NotActivatedError{}
+	return nil, auth.ErrNotActivated
 }
 
 // Deactivate implements the Deactivate RPC, but just returns NotActivatedError
 func (a *InactiveAPIServer) Deactivate(ctx context.Context, req *auth.DeactivateRequest) (resp *auth.DeactivateResponse, retErr error) {
-	return nil, auth.NotActivatedError{}
+	return nil, auth.ErrNotActivated
 }
 
 // GetAdmins implements the GetAdmins RPC, but just returns NotActivatedError
 func (a *InactiveAPIServer) GetAdmins(ctx context.Context, req *auth.GetAdminsRequest) (resp *auth.GetAdminsResponse, retErr error) {
-	return nil, auth.NotActivatedError{}
+	return nil, auth.ErrNotActivated
 }
 
 // ModifyAdmins implements the ModifyAdmins RPC, but just returns NotActivatedError
 func (a *InactiveAPIServer) ModifyAdmins(ctx context.Context, req *auth.ModifyAdminsRequest) (resp *auth.ModifyAdminsResponse, retErr error) {
-	return nil, auth.NotActivatedError{}
+	return nil, auth.ErrNotActivated
 }
 
 // Authenticate implements the Authenticate RPC, but just returns NotActivatedError
 func (a *InactiveAPIServer) Authenticate(ctx context.Context, req *auth.AuthenticateRequest) (resp *auth.AuthenticateResponse, retErr error) {
-	return nil, auth.NotActivatedError{}
+	return nil, auth.ErrNotActivated
 }
 
 // Authorize implements the Authorize RPC, but just returns NotActivatedError
 func (a *InactiveAPIServer) Authorize(ctx context.Context, req *auth.AuthorizeRequest) (resp *auth.AuthorizeResponse, retErr error) {
-	return nil, auth.NotActivatedError{}
+	return nil, auth.ErrNotActivated
 }
 
 // WhoAmI implements the WhoAmI RPC, but just returns NotActivatedError
 func (a *InactiveAPIServer) WhoAmI(ctx context.Context, req *auth.WhoAmIRequest) (resp *auth.WhoAmIResponse, retErr error) {
-	return nil, auth.NotActivatedError{}
+	return nil, auth.ErrNotActivated
 }
 
 // SetScope implements the SetScope RPC, but just returns NotActivatedError
 func (a *InactiveAPIServer) SetScope(ctx context.Context, req *auth.SetScopeRequest) (resp *auth.SetScopeResponse, retErr error) {
-	return nil, auth.NotActivatedError{}
+	return nil, auth.ErrNotActivated
 }
 
 // GetScope implements the GetScope RPC, but just returns NotActivatedError
 func (a *InactiveAPIServer) GetScope(ctx context.Context, req *auth.GetScopeRequest) (resp *auth.GetScopeResponse, retErr error) {
-	return nil, auth.NotActivatedError{}
+	return nil, auth.ErrNotActivated
 }
 
 // GetACL implements the GetACL RPC, but just returns NotActivatedError
 func (a *InactiveAPIServer) GetACL(ctx context.Context, req *auth.GetACLRequest) (resp *auth.GetACLResponse, retErr error) {
-	return nil, auth.NotActivatedError{}
+	return nil, auth.ErrNotActivated
 }
 
 // SetACL implements the SetACL RPC, but just returns NotActivatedError
 func (a *InactiveAPIServer) SetACL(ctx context.Context, req *auth.SetACLRequest) (resp *auth.SetACLResponse, retErr error) {
-	return nil, auth.NotActivatedError{}
+	return nil, auth.ErrNotActivated
 }
 
 // GetAuthToken implements the GetAuthToken RPC, but just returns NotActivatedError
 func (a *InactiveAPIServer) GetAuthToken(ctx context.Context, req *auth.GetAuthTokenRequest) (resp *auth.GetAuthTokenResponse, retErr error) {
-	return nil, auth.NotActivatedError{}
+	return nil, auth.ErrNotActivated
 }
 
 // ExtendAuthToken implements the ExtendAuthToken RPC, but just returns NotActivatedError
 func (a *InactiveAPIServer) ExtendAuthToken(ctx context.Context, req *auth.ExtendAuthTokenRequest) (resp *auth.ExtendAuthTokenResponse, retErr error) {
-	return nil, auth.NotActivatedError{}
+	return nil, auth.ErrNotActivated
 }
 
 // RevokeAuthToken implements the RevokeAuthToken RPC, but just returns NotActivatedError
 func (a *InactiveAPIServer) RevokeAuthToken(ctx context.Context, req *auth.RevokeAuthTokenRequest) (resp *auth.RevokeAuthTokenResponse, retErr error) {
-	return nil, auth.NotActivatedError{}
+	return nil, auth.ErrNotActivated
 }
