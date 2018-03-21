49aea1ba1ee95553834f44771e17de56332a344d
Joe Doliner <jdoliner@gmail.com>
2018-03-21 13:09:10 -0700

    Put the right branch in the branches field.

--- src/server/pfs/server/server_test.go
+++ src/server/pfs/server/server_test.go
@@ -138,8 +138,8 @@ func TestCreateRepoDeleteRepoRace(t *testing.T) {
 		// both succeed, leaving us with a repo bar that has a nonexistent
 		// provenance foo
 		require.True(t, err1 != nil || err2 != nil)
-		require.NoError(t, client.DeleteRepo("bar", false))
-		require.NoError(t, client.DeleteRepo("foo", false))
+		client.DeleteRepo("bar", true)
+		client.DeleteRepo("foo", true)
 	}
 }
 
