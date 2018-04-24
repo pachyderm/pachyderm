77db1297d7744b56c4041c243dabf126fba1f1f3
Sean Jezewski <seanwjezewski@gmail.com>
2018-04-24 15:43:12 -0700

    Start adding API call prometheus stats to PFS

--- src/server/pkg/log/log.go
+++ src/server/pkg/log/log.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/fatih/camelcase"
 	"github.com/sirupsen/logrus"
 )
 
@@ -32,11 +33,70 @@ func NewLogger(service string) Logger {
 // Helper function used to log requests and responses from our GRPC method
 // implementations
 func (l *logger) Log(request interface{}, response interface{}, err error, duration time.Duration) {
+	state := "started"
 	if err != nil {
 		l.LogAtLevelFromDepth(request, response, err, duration, logrus.ErrorLevel, 4)
+		state = "errored"
 	} else {
 		l.LogAtLevelFromDepth(request, response, err, duration, logrus.InfoLevel, 4)
 	}
+	if duration.Seconds() > 0 {
+		state = "finished"
+	}
+	l.ReportMetric(state, duration)
+}
+
+func (l *logger) ReportMetric(state string, duration time.Duration) {
+	depth := 4
+	pc := make([]uintptr, depth)
+	runtime.Callers(depth, pc)
+	split := strings.Split(runtime.FuncForPC(pc[0]).Name(), ".")
+	method := split[len(split)-1]
+	fmt.Printf("GOING TO REPORT STATS FOR METHOD: %v\n", method)
+	fmt.Printf("state: %v, duration: %v\n", state, duration)
+
+	var tokens []string
+	for _, token := range camelcase.Split(method) {
+		tokens = append(tokens, strings.ToLower(token))
+	}
+	rootStatName := string.Join(tokens, "_")
+
+	bucketFactor = 2.0
+	bucketCount = 20 // Which makes the max bucket 2^20 seconds or ~12 days in size
+	runTime = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Namespace: "pachyderm",
+			Subsystem: "pachd",
+			Name:      fmt.Sprintf("%v_time", rootStatName),
+			Help:      fmt.Sprintf("Run time of %v", method),
+			Buckets:   prometheus.ExponentialBuckets(1.0, bucketFactor, bucketCount),
+		},
+		[]string{
+			"state", // Since both finished and errored datums can have proc times
+		},
+	)
+	if err := prometheus.Register(runTime); err != nil {
+		fmt.Printf("error registering prometheus metric: %v\n", err)
+	}
+	if hist, err := runTime.GetMetricWithLabelValues(state); err != nil {
+		logger.Logf("failed to get histogram w labels: state (%v) with error %v", state, err)
+	} else {
+		hist.Observe(duration.Seconds())
+	}
+
+	secondsCount = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Namespace: "pachyderm",
+			Subsystem: "pachd",
+			Name:      fmt.Sprintf("%v_seconds_count", rootStatName),
+			Help:      fmt.Sprintf("cumulative number of seconds spent in %v", method),
+		},
+	)
+	if err := prometheus.Register(secondsCount); err != nil {
+		fmt.Printf("error registering prometheus metric: %v\n", err)
+	}
+	secondsCount.Add(duration.Seconds())
+
 }
 
 func (l *logger) LogAtLevelFromDepth(request interface{}, response interface{}, err error, duration time.Duration, level logrus.Level, depth int) {
