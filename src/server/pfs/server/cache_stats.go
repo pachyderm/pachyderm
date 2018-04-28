a4d62a2dcaf59ffa73a8bc53624a607fd7b6af90
Sean Jezewski <seanwjezewski@gmail.com>
2018-04-27 17:03:24 -0700

    Refactor cache stats to implement prometheus.Collector

--- src/server/pfs/server/cache_stats.go
+++ src/server/pfs/server/cache_stats.go
@@ -23,47 +23,57 @@ var groupCacheStatFields = []string{
 	"ServerRequests",
 }
 
-var cacheStats map[string]prometheus.Gauge
+type cacheStats struct {
+	cacheName    string
+	descriptions map[string]*prometheus.Desc
+	*groupcache.Stats
+}
 
-func statName(cacheName string, fieldName string) string {
+func NewCacheStats(cacheName string, groupCacheStats *groupcache.Stats) *cacheStats {
+	c := &cacheStats{cacheName, make(map[string]*prometheus.Desc), groupCacheStats}
+	if err := prometheus.Register(c); err != nil {
+		logrus.Infof("error registering prometheus metric: %v", err)
+	}
+	return c
+}
+
+func (c *cacheStats) Describe(ch chan<- *prometheus.Desc) {
+	for _, statFieldName := range groupCacheStatFields {
+		statName := c.statName(statFieldName)
+		desc := prometheus.NewDesc(
+			statName,
+			fmt.Sprintf("groupcache %v", statFieldName),
+			[]string{},
+			nil,
+		)
+		c.descriptions[statName] = desc
+		ch <- desc
+	}
+}
+
+func (c *cacheStats) Collect(ch chan<- prometheus.Metric) {
+	for _, statFieldName := range groupCacheStatFields {
+		r := reflect.ValueOf(c) // or do I need to ref c.Stats ??
+		value := reflect.Indirect(r).FieldByName(statFieldName)
+		metric, err := prometheus.NewConstMetric(
+			c.descriptions[c.statName(statFieldName)],
+			prometheus.GaugeValue,
+			float64(value.Int()),
+		)
+		if err != nil {
+			logrus.Infof("error reporting cache prometheus metric: %v", err)
+		} else {
+			ch <- metric
+		}
+	}
+}
+
+func (c *cacheStats) statName(fieldName string) string {
 	var tokens []string
 	for _, token := range camelcase.Split(fieldName) {
 		tokens = append(tokens, strings.ToLower(token))
 	}
 	groupCacheStatName := strings.Join(tokens, "_")
 
-	return fmt.Sprintf("cache_%v_%v_gauge", cacheName, groupCacheStatName)
-}
-
-func initCacheStats(cacheNames []string) {
-	cacheStats = make(map[string]prometheus.Gauge)
-	for _, cacheName := range cacheNames {
-		for _, statFieldName := range groupCacheStatFields {
-			name := statName(cacheName, statFieldName)
-			gauge, ok := cacheStats[name]
-			if !ok {
-				gauge = prometheus.NewGauge(
-					prometheus.GaugeOpts{
-						Namespace: "pachyderm",
-						Subsystem: "pachd",
-						Name:      name,
-						Help:      fmt.Sprintf("gauge for groupcache %v field", statFieldName),
-					},
-				)
-				if err := prometheus.Register(gauge); err != nil {
-					logrus.Infof("error registering prometheus metric: %v", err)
-				} else {
-					cacheStats[name] = gauge
-				}
-			}
-		}
-	}
-}
-
-func reportCacheStats(cacheName string, stats *groupcache.Stats) {
-	for _, statFieldName := range groupCacheStatFields {
-		r := reflect.ValueOf(stats)
-		value := reflect.Indirect(r).FieldByName(statFieldName)
-		cacheStats[statName(cacheName, statFieldName)].Set(value.Float())
-	}
+	return fmt.Sprintf("cache_%v_%v_gauge", c.cacheName, groupCacheStatName)
 }
