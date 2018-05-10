4dddc2a675e07a2547be3db261c214b741457e44
Bryce McAnally <bryce@pachyderm.io>
2018-05-10 11:15:38 -0700

    Change iteration to a callback interface

--- src/server/pkg/collection/iterator.go
+++ src/server/pkg/collection/iterator.go
@@ -7,6 +7,7 @@ import (
 
 	etcd "github.com/coreos/etcd/clientv3"
 	"github.com/coreos/etcd/mvcc/mvccpb"
+	"github.com/pachyderm/pachyderm/src/server/pkg/errutil"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -22,84 +23,71 @@ type Options struct {
 // DefaultOptions are the default sort options when iterating through etcd key/values.
 var DefaultOptions = &Options{etcd.SortByCreateRevision, etcd.SortDescend, false}
 
-type etcdIterator interface {
-	next() ([]*mvccpb.KeyValue, error)
-	done() bool
-}
-
-type iterator struct {
-	c           *readonlyCollection
-	prefix      string
-	limitPtr    *int64
-	opts        *Options
-	compareFunc func(*mvccpb.KeyValue, *mvccpb.KeyValue) int
-	last        bool
-}
-
-func newEtcdIterator(c *readonlyCollection, prefix string, limitPtr *int64, opts *Options) etcdIterator {
+func iterate(c *readonlyCollection, prefix string, limitPtr *int64, opts *Options, f func(*mvccpb.KeyValue) error) error {
 	if opts.SelfSort {
-		iter := &selfSortRevisionIterator{iterator: iterator{c: c, prefix: prefix, limitPtr: limitPtr, opts: opts}}
-		_, iter.compareFunc = iteratorFuncs(opts)
-		return iter
+		return iterateSelfSortRevision(c, prefix, limitPtr, opts, f)
 	}
-	iter := &revisionIterator{iterator: iterator{c: c, prefix: prefix, limitPtr: limitPtr, opts: opts}}
-	iter.fromFunc, iter.compareFunc = iteratorFuncs(opts)
-	return iter
+
+	return iterateRevision(c, prefix, limitPtr, opts, f)
 }
 
-func iteratorFuncs(opts *Options) (func(*mvccpb.KeyValue) etcd.OpOption, func(kv1 *mvccpb.KeyValue, kv2 *mvccpb.KeyValue) int) {
-	var fromFunc func(*mvccpb.KeyValue) etcd.OpOption
-	var compareFunc func(kv1 *mvccpb.KeyValue, kv2 *mvccpb.KeyValue) int
+func iterateFuncs(opts *Options) (func(*mvccpb.KeyValue) etcd.OpOption, func(kv1 *mvccpb.KeyValue, kv2 *mvccpb.KeyValue) int) {
+	var from func(*mvccpb.KeyValue) etcd.OpOption
+	var compare func(kv1 *mvccpb.KeyValue, kv2 *mvccpb.KeyValue) int
 	switch opts.Target {
 	case etcd.SortByCreateRevision:
 		switch opts.Order {
 		case etcd.SortAscend:
-			fromFunc = func(fromKey *mvccpb.KeyValue) etcd.OpOption { return etcd.WithMinCreateRev(fromKey.CreateRevision) }
+			from = func(fromKey *mvccpb.KeyValue) etcd.OpOption { return etcd.WithMinCreateRev(fromKey.CreateRevision) }
 		case etcd.SortDescend:
-			fromFunc = func(fromKey *mvccpb.KeyValue) etcd.OpOption { return etcd.WithMaxCreateRev(fromKey.CreateRevision) }
+			from = func(fromKey *mvccpb.KeyValue) etcd.OpOption { return etcd.WithMaxCreateRev(fromKey.CreateRevision) }
 		}
-		compareFunc = func(kv1 *mvccpb.KeyValue, kv2 *mvccpb.KeyValue) int {
+		compare = func(kv1 *mvccpb.KeyValue, kv2 *mvccpb.KeyValue) int {
 			return int(kv1.CreateRevision - kv2.CreateRevision)
 		}
 	case etcd.SortByModRevision:
 		switch opts.Order {
 		case etcd.SortAscend:
-			fromFunc = func(fromKey *mvccpb.KeyValue) etcd.OpOption { return etcd.WithMinModRev(fromKey.ModRevision) }
+			from = func(fromKey *mvccpb.KeyValue) etcd.OpOption { return etcd.WithMinModRev(fromKey.ModRevision) }
 		case etcd.SortDescend:
-			fromFunc = func(fromKey *mvccpb.KeyValue) etcd.OpOption { return etcd.WithMaxModRev(fromKey.ModRevision) }
+			from = func(fromKey *mvccpb.KeyValue) etcd.OpOption { return etcd.WithMaxModRev(fromKey.ModRevision) }
 		}
-		compareFunc = func(kv1 *mvccpb.KeyValue, kv2 *mvccpb.KeyValue) int {
+		compare = func(kv1 *mvccpb.KeyValue, kv2 *mvccpb.KeyValue) int {
 			return int(kv1.ModRevision - kv2.ModRevision)
 		}
 	}
-	return fromFunc, compareFunc
+	return from, compare
 }
 
-type revisionIterator struct {
-	iterator
-	fromKey  *mvccpb.KeyValue
-	fromFunc func(*mvccpb.KeyValue) etcd.OpOption
-}
-
-func (iter *revisionIterator) next() ([]*mvccpb.KeyValue, error) {
-	opts := []etcd.OpOption{etcd.WithPrefix(), etcd.WithSort(iter.opts.Target, iter.opts.Order)}
-	if iter.fromKey != nil {
-		opts = append(opts, iter.fromFunc(iter.fromKey))
+func iterateRevision(c *readonlyCollection, prefix string, limitPtr *int64, opts *Options, f func(*mvccpb.KeyValue) error) error {
+	etcdOpts := []etcd.OpOption{etcd.WithPrefix(), etcd.WithSort(opts.Target, opts.Order)}
+	var fromKey *mvccpb.KeyValue
+	from, compare := iterateFuncs(opts)
+	for {
+		if fromKey != nil {
+			etcdOpts = append(etcdOpts, from(fromKey))
+		}
+		resp, done, err := getWithLimit(c, prefix, limitPtr, etcdOpts)
+		if err != nil {
+			return err
+		}
+		kvs := getNewKeys(resp.Kvs, fromKey)
+		for _, kv := range kvs {
+			if err := f(kv); err != nil {
+				if err == errutil.ErrBreak {
+					return nil
+				}
+				return err
+			}
+		}
+		if done {
+			return nil
+		}
+		if compare(resp.Kvs[0], resp.Kvs[len(resp.Kvs)-1]) == 0 {
+			return fmt.Errorf("revision contains too many objects to fit in one batch (this is likely a bug)")
+		}
+		fromKey = kvs[len(kvs)-1]
 	}
-	resp, done, err := getWithLimit(iter.c, iter.prefix, iter.limitPtr, opts)
-	if err != nil {
-		return nil, err
-	}
-	kvs := getNewKeys(resp.Kvs, iter.fromKey)
-	if done {
-		iter.last = true
-		return kvs, nil
-	}
-	if iter.compareFunc(resp.Kvs[0], resp.Kvs[len(resp.Kvs)-1]) == 0 {
-		return nil, fmt.Errorf("revision contains too many objects to fit in one batch (this is likely a bug)")
-	}
-	iter.fromKey = kvs[len(kvs)-1]
-	return kvs, nil
 }
 
 func getNewKeys(respKvs []*mvccpb.KeyValue, fromKey *mvccpb.KeyValue) []*mvccpb.KeyValue {
@@ -114,41 +102,47 @@ func getNewKeys(respKvs []*mvccpb.KeyValue, fromKey *mvccpb.KeyValue) []*mvccpb.
 	return nil
 }
 
-func (iter *revisionIterator) done() bool {
-	return iter.last
+type kvSort struct {
+	kvs     []*mvccpb.KeyValue
+	compare func(kv1 *mvccpb.KeyValue, kv2 *mvccpb.KeyValue) int
 }
 
-type selfSortRevisionIterator struct {
-	iterator
-	kvs []*mvccpb.KeyValue
-}
-
-func (iter *selfSortRevisionIterator) next() ([]*mvccpb.KeyValue, error) {
-	opts := []etcd.OpOption{etcd.WithFromKey(), etcd.WithRange(endKeyFromPrefix(iter.prefix))}
-	fromKey := iter.prefix
-	iter.kvs = []*mvccpb.KeyValue{}
+func iterateSelfSortRevision(c *readonlyCollection, prefix string, limitPtr *int64, opts *Options, f func(*mvccpb.KeyValue) error) error {
+	etcdOpts := []etcd.OpOption{etcd.WithFromKey(), etcd.WithRange(endKeyFromPrefix(prefix))}
+	fromKey := prefix
+	kvs := []*mvccpb.KeyValue{}
 	for {
-		resp, done, err := getWithLimit(iter.c, fromKey, iter.limitPtr, opts)
+		resp, done, err := getWithLimit(c, fromKey, limitPtr, etcdOpts)
 		if err != nil {
-			return nil, err
+			return err
 		}
-		if fromKey == iter.prefix {
-			iter.kvs = append(iter.kvs, resp.Kvs...)
+		if fromKey == prefix {
+			kvs = append(kvs, resp.Kvs...)
 		} else {
-			iter.kvs = append(iter.kvs, resp.Kvs[1:]...)
+			kvs = append(kvs, resp.Kvs[1:]...)
 		}
 		if done {
-			iter.last = true
 			break
 		}
-		fromKey = string(iter.kvs[len(iter.kvs)-1].Key)
+		fromKey = string(kvs[len(kvs)-1].Key)
 	}
-	if iter.opts.Order == etcd.SortAscend {
-		sort.Sort(iter)
-	} else {
-		sort.Sort(sort.Reverse(iter))
+	_, compare := iterateFuncs(opts)
+	sorter := &kvSort{kvs, compare}
+	switch opts.Order {
+	case etcd.SortAscend:
+		sort.Sort(sorter)
+	case etcd.SortDescend:
+		sort.Sort(sort.Reverse(sorter))
 	}
-	return iter.kvs, nil
+	for _, kv := range kvs {
+		if err := f(kv); err != nil {
+			if err == errutil.ErrBreak {
+				return nil
+			}
+			return err
+		}
+	}
+	return nil
 }
 
 func endKeyFromPrefix(prefix string) string {
@@ -156,22 +150,18 @@ func endKeyFromPrefix(prefix string) string {
 	return prefix[0:len(prefix)-1] + string(byte(prefix[len(prefix)-1])+1)
 }
 
-func (iter *selfSortRevisionIterator) done() bool {
-	return iter.last
+func (s *kvSort) Len() int {
+	return len(s.kvs)
 }
 
-func (iter *selfSortRevisionIterator) Len() int {
-	return len(iter.kvs)
+func (s *kvSort) Less(i, j int) bool {
+	return s.compare(s.kvs[i], s.kvs[j]) < 0
 }
 
-func (iter *selfSortRevisionIterator) Less(i, j int) bool {
-	return iter.compareFunc(iter.kvs[i], iter.kvs[j]) < 0
-}
-
-func (iter *selfSortRevisionIterator) Swap(i, j int) {
-	t := iter.kvs[i]
-	iter.kvs[i] = iter.kvs[j]
-	iter.kvs[j] = t
+func (s *kvSort) Swap(i, j int) {
+	t := s.kvs[i]
+	s.kvs[i] = s.kvs[j]
+	s.kvs[j] = t
 }
 
 func getWithLimit(c *readonlyCollection, key string, limitPtr *int64, opts []etcd.OpOption) (*etcd.GetResponse, bool, error) {
