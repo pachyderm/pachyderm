cc331e949f3eae6b73fbad1105589798b3154ba2
Joe Doliner <jdoliner@gmail.com>
2018-09-04 12:52:41 -0700

    Implment file based fuse gets.

--- src/server/pfs/fuse/file.go
+++ src/server/pfs/fuse/file.go
@@ -1,44 +1,67 @@
 package fuse
 
 import (
-	"bytes"
 	"context"
 	"io"
+	"io/ioutil"
+	"os"
 	"sync"
 	"syscall"
 	"time"
 
 	"github.com/hanwen/go-fuse/fuse"
 	"github.com/hanwen/go-fuse/fuse/nodefs"
-	"github.com/pachyderm/pachyderm/src/client"
 	"github.com/pachyderm/pachyderm/src/client/pfs"
 )
 
 type file struct {
-	fs     *filesystem
-	name   string
-	c      *client.APIClient
-	cancel func()
-	// r is a reader for the file that we keep open, this allows us to optimize
-	// the common case of reading the entire file from beginning to end.
-	// Without r we would have to open a new connection every time someone
-	// wanted to read a chunk of the file.
-	r      io.Reader
-	offset int64
-	m      map[int64]chan struct{}
-	mu     sync.Mutex
+	name    string
+	attr    *fuse.Attr
+	cancel  func()
+	pfsFile *pfs.File
+	file    *os.File
+	counter *counter
 }
 
-func newFile(fs *filesystem, name string) *file {
+func newFile(fs *filesystem, name string) (*file, fuse.Status) {
+	attr, status := fs.getAttr(name)
+	if status != fuse.OK {
+		return nil, status
+	}
 	ctx, cancel := context.WithCancel(fs.c.Ctx())
 	c := fs.c.WithCtx(ctx)
-	return &file{
-		fs:     fs,
-		name:   name,
-		c:      c,
-		cancel: cancel,
-		m:      make(map[int64]chan struct{}),
+	f, err := ioutil.TempFile("", "pfs-fuse")
+	if err != nil {
+		return nil, fuse.ToStatus(err)
 	}
+	if err := os.Remove(f.Name()); err != nil {
+		return nil, fuse.ToStatus(err)
+	}
+	_, pfsFile, err := fs.parsePath(name)
+	if err != nil {
+		return nil, toStatus(err)
+	}
+	if pfsFile == nil {
+		return nil, fuse.Status(syscall.EISDIR)
+	}
+	counter := newCounter()
+	// Argument order is important here because it means that writes to w must
+	// complete writing to f before being written to counter. Thus counter can
+	// tell us conclusively at least (but not at most) a certain number of
+	// bytes has been written to f.
+	w := io.MultiWriter(f, counter)
+	go func() {
+		if err := c.GetFile(pfsFile.Commit.Repo.Name, pfsFile.Commit.ID, pfsFile.Path, 0, 0, w); err != nil {
+			// Do something with this error
+		}
+	}()
+	return &file{
+		attr:    attr,
+		cancel:  cancel,
+		pfsFile: pfsFile,
+		file:    f,
+		counter: counter,
+	}, fuse.OK
 }
 
 func (f *file) Write(data []byte, off int64) (written uint32, code fuse.Status) {
@@ -56,18 +79,15 @@ func (f *file) InnerFile() nodefs.File {
 }
 
 func (f *file) Read(dest []byte, offset int64) (fuse.ReadResult, fuse.Status) {
-	repo, file, err := f.fs.parsePath(f.name)
-	if err != nil {
+	waitn := offset + int64(len(dest))
+	if waitn > int64(f.attr.Size) {
+		waitn = int64(f.attr.Size)
+	}
+	f.counter.wait(waitn)
+	if err := f.file.Sync(); err != nil {
 		return nil, toStatus(err)
 	}
-	switch {
-	case repo != nil:
-		return nil, fuse.Status(syscall.EISDIR)
-	case file != nil:
-		return f.newReadResult(file, offset, len(dest)), fuse.OK
-	default:
-		return nil, fuse.Status(syscall.EISDIR)
-	}
+	return fuse.ReadResultFd(f.file.Fd(), offset, len(dest)), fuse.OK
 }
 
 func (f *file) Flock(flags int) fuse.Status {
@@ -90,9 +110,8 @@ func (f *file) Truncate(size uint64) fuse.Status {
 }
 
 func (f *file) GetAttr(out *fuse.Attr) fuse.Status {
-	attr, status := f.fs.getAttr(f.name)
-	*out = *attr
-	return status
+	*out = *f.attr
+	return fuse.OK
 }
 
 func (f *file) Chown(uid uint32, gid uint32) fuse.Status {
@@ -111,98 +130,31 @@ func (f *file) Allocate(off uint64, size uint64, mode uint32) fuse.Status {
 	return fuse.EROFS
 }
 
-type readResult struct {
-	f      *file
-	file   *pfs.File
-	offset int64
-	size   int
+type counter struct {
+	n    int64
+	mu   sync.Mutex
+	cond *sync.Cond
 }
 
-func (f *file) newReadResult(file *pfs.File, offset int64, size int) *readResult {
-	return &readResult{
-		f:      f,
-		file:   file,
-		offset: offset,
-		size:   size,
-	}
+func newCounter() *counter {
+	result := &counter{}
+	result.cond = sync.NewCond(&result.mu)
+	return result
 }
 
-// readFull is like read but always fills up the buffer
-func readFull(r io.Reader, p []byte) error {
-	n := 0
-	for n < len(p) {
-		_n, err := r.Read(p[n:])
-		if err != nil {
-			return err
-		}
-		n += _n
-	}
-	return nil
+func (c *counter) Write(p []byte) (int, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.n += int64(len(p))
+	c.cond.Broadcast()
+	return len(p), nil
 }
 
-func (r *readResult) Bytes(buf []byte) ([]byte, fuse.Status) {
-	size := r.size
-	if len(buf) < size {
-		size = len(buf)
+// wait until more than n bytes have been written
+func (c *counter) wait(n int64) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	for c.n < n {
+		c.cond.Wait()
 	}
-	if size < len(buf) {
-		buf = buf[0:size]
-	}
-
-	res, err := func() ([]byte, error) {
-		r.f.mu.Lock()
-		defer r.f.mu.Unlock()
-		if r.f.r == nil {
-			reader, err := r.f.c.GetFileReader(r.file.Commit.Repo.Name, r.file.Commit.ID, r.file.Path, r.offset, 0)
-			if err != nil {
-				return nil, err
-			}
-			r.f.r = reader
-			r.f.offset = r.offset
-		}
-		if r.offset > r.f.offset {
-			ch := make(chan struct{})
-			r.f.m[r.offset] = ch
-			r.f.mu.Unlock()
-			select {
-			case <-ch:
-			case <-time.After(time.Second):
-			}
-			r.f.mu.Lock()
-			delete(r.f.m, r.offset)
-		}
-		if r.f.offset == r.offset {
-			if err := readFull(r.f.r, buf); err != nil {
-				r.f.r = nil
-				if err == io.EOF {
-					return nil, nil
-				}
-				return nil, err
-			}
-			r.f.offset += int64(size)
-			if ch, ok := r.f.m[r.f.offset]; ok {
-				close(ch)
-			}
-			return buf, nil
-		}
-		return nil, nil
-	}()
-	if err != nil {
-		return nil, fuse.ToStatus(err)
-	}
-	if res != nil {
-		return res, fuse.OK
-	}
-
-	buffer := bytes.NewBuffer(buf[:0])
-	if err := r.f.c.GetFile(r.file.Commit.Repo.Name, r.file.Commit.ID, r.file.Path, r.offset, int64(size), buffer); err != nil {
-		return nil, fuse.ToStatus(err)
-	}
-	return buffer.Bytes(), fuse.OK
 }
-
-func (r *readResult) Size() int {
-	return r.size
-}
-
-func (r *readResult) Done() {}
