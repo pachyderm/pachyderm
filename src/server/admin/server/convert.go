2440f8810511402a8505e3d13e118855b230e923
Matthew Steffen <msteffen@pachyderm.io>
2018-12-20 12:25:23 -0800

    Fix 1.7 conversion functions

--- src/server/admin/server/convert.go
+++ src/server/admin/server/convert.go
@@ -8,12 +8,18 @@ import (
 )
 
 func convert1_7Repo(r *pfs_1_7.Repo) *pfs.Repo {
+	if r == nil {
+		return nil
+	}
 	return &pfs.Repo{
 		Name: r.Name,
 	}
 }
 
 func convert1_7Commit(c *pfs_1_7.Commit) *pfs.Commit {
+	if c == nil {
+		return nil
+	}
 	return &pfs.Commit{
 		Repo: convert1_7Repo(c.Repo),
 		ID:   c.ID,
@@ -21,24 +27,27 @@ func convert1_7Commit(c *pfs_1_7.Commit) *pfs.Commit {
 }
 
 func convert1_7Commits(commits []*pfs_1_7.Commit) []*pfs.Commit {
+	if commits == nil {
+		return nil
+	}
 	result := make([]*pfs.Commit, 0, len(commits))
 	for _, c := range commits {
 		result = append(result, convert1_7Commit(c))
 	}
-}
-
-func convert1_7Branch(b *pfs_1_7.Branch) *pfs.Branch {
-	return &pfs.Branch{
-		Repo: convert1_7Repo(b.Repo),
-		Name: b.Name,
-	}
+	return result
 }
 
 func convert1_7Object(o *pfs_1_7.Object) *pfs.Object {
+	if o == nil {
+		return nil
+	}
 	return &pfs.Object{Hash: o.Hash}
 }
 
 func convert1_7Objects(objects []*pfs_1_7.Object) []*pfs.Object {
+	if objects == nil {
+		return nil
+	}
 	result := make([]*pfs.Object, 0, len(objects))
 	for _, o := range objects {
 		result = append(result, convert1_7Object(o))
@@ -47,10 +56,16 @@ func convert1_7Objects(objects []*pfs_1_7.Object) []*pfs.Object {
 }
 
 func convert1_7Tag(tag *pfs_1_7.Tag) *pfs.Tag {
+	if tag == nil {
+		return nil
+	}
 	return &pfs.Tag{Name: tag.Name}
 }
 
 func convert1_7Tags(tags []*pfs_1_7.Tag) []*pfs.Tag {
+	if tags == nil {
+		return nil
+	}
 	result := make([]*pfs.Tag, 0, len(tags))
 	for _, t := range tags {
 		result = append(result, convert1_7Tag(t))
@@ -59,32 +74,67 @@ func convert1_7Tags(tags []*pfs_1_7.Tag) []*pfs.Tag {
 }
 
 func convert1_7Branch(b *pfs_1_7.Branch) *pfs.Branch {
+	if b == nil {
+		return nil
+	}
 	return &pfs.Branch{
-		Repo: &pfs.Repo{Name: b.Repo.Name},
+		Repo: convert1_7Repo(b.Repo),
 		Name: b.Name,
 	}
 }
 
 func convert1_7Branches(branches []*pfs_1_7.Branch) []*pfs.Branch {
+	if branches == nil {
+		return nil
+	}
 	result := make([]*pfs.Branch, 0, len(branches))
 	for _, b := range branches {
-		result := append(result, convert1_7Branch(b))
+		result = append(result, convert1_7Branch(b))
 	}
 	return result
 }
 
 func convert1_7Pipeline(p *pps_1_7.Pipeline) *pps.Pipeline {
+	if p == nil {
+		return nil
+	}
 	return &pps.Pipeline{
 		Name: p.Name,
 	}
 }
 
+func convert1_7Secret(s *pps_1_7.Secret) *pps.Secret {
+	if s == nil {
+		return nil
+	}
+	return &pps.Secret{
+		Name:      s.Name,
+		Key:       s.Key,
+		MountPath: s.MountPath,
+		EnvVar:    s.EnvVar,
+	}
+}
+
+func convert1_7Secrets(secrets []*pps_1_7.Secret) []*pps.Secret {
+	if secrets == nil {
+		return nil
+	}
+	result := make([]*pps.Secret, 0, len(secrets))
+	for _, s := range secrets {
+		result = append(result, convert1_7Secret(s))
+	}
+	return result
+}
+
 func convert1_7Transform(t *pps_1_7.Transform) *pps.Transform {
+	if t == nil {
+		return nil
+	}
 	return &pps.Transform{
 		Image:            t.Image,
 		Cmd:              t.Cmd,
 		Env:              t.Env,
-		Secrets:          t.Secrets,
+		Secrets:          convert1_7Secrets(t.Secrets),
 		ImagePullSecrets: t.ImagePullSecrets,
 		Stdin:            t.Stdin,
 		AcceptReturnCode: t.AcceptReturnCode,
@@ -95,6 +145,9 @@ func convert1_7Transform(t *pps_1_7.Transform) *pps.Transform {
 }
 
 func convert1_7ParallelismSpec(s *pps_1_7.ParallelismSpec) *pps.ParallelismSpec {
+	if s == nil {
+		return nil
+	}
 	return &pps.ParallelismSpec{
 		Constant:    s.Constant,
 		Coefficient: s.Coefficient,
@@ -102,18 +155,27 @@ func convert1_7ParallelismSpec(s *pps_1_7.ParallelismSpec) *pps.ParallelismSpec 
 }
 
 func convert1_7HashtreeSpec(h *pps_1_7.HashtreeSpec) *pps.HashtreeSpec {
+	if h == nil {
+		return nil
+	}
 	return &pps.HashtreeSpec{
 		Constant: h.Constant,
 	}
 }
 
 func convert1_7Egress(e *pps_1_7.Egress) *pps.Egress {
+	if e == nil {
+		return nil
+	}
 	return &pps.Egress{
 		URL: e.URL,
 	}
 }
 
 func convert1_7ResourceSpec(r *pps_1_7.ResourceSpec) *pps.ResourceSpec {
+	if r == nil {
+		return nil
+	}
 	return &pps.ResourceSpec{
 		Cpu:    r.Cpu,
 		Memory: r.Memory,
@@ -123,18 +185,13 @@ func convert1_7ResourceSpec(r *pps_1_7.ResourceSpec) *pps.ResourceSpec {
 }
 
 func convert1_7Input(i *pps_1_7.Input) *pps.Input {
-	convert1_7AtomInput := func(i *pps_1_7.AtomInput) *pps.AtomInput {
-		return &pps.PFSInput{
-			Name:       i.Name,
-			Repo:       i.Repo,
-			Branch:     i.Branch,
-			Commit:     i.Commit,
-			Glob:       i.Glob,
-			Lazy:       i.Lazy,
-			EmptyFiles: i.EmptyFiles,
-		}
+	if i == nil {
+		return nil
 	}
-	convert1_7PFSInput := func(i *pps_1_7.PFSInput) *pps.PFSInput {
+	convert1_7AtomInput := func(i *pps_1_7.AtomInput) *pps.PFSInput {
+		if i == nil {
+			return nil
+		}
 		return &pps.PFSInput{
 			Name:       i.Name,
 			Repo:       i.Repo,
@@ -146,6 +203,9 @@ func convert1_7Input(i *pps_1_7.Input) *pps.Input {
 		}
 	}
 	convert1_7CronInput := func(i *pps_1_7.CronInput) *pps.CronInput {
+		if i == nil {
+			return nil
+		}
 		return &pps.CronInput{
 			Name:   i.Name,
 			Repo:   i.Repo,
@@ -155,6 +215,9 @@ func convert1_7Input(i *pps_1_7.Input) *pps.Input {
 		}
 	}
 	convert1_7GitInput := func(i *pps_1_7.GitInput) *pps.GitInput {
+		if i == nil {
+			return nil
+		}
 		return &pps.GitInput{
 			Name:   i.Name,
 			URL:    i.URL,
@@ -164,25 +227,29 @@ func convert1_7Input(i *pps_1_7.Input) *pps.Input {
 	}
 	return &pps.Input{
 		// Note: this is deprecated and replaced by `PfsInput`
-		Atom:  convert1_7AtomInput(i.Atom),
-		Pfs:   convert1_7PFSInput(i.Pfs),
+		Pfs:   convert1_7AtomInput(i.Atom),
 		Cross: convert1_7Inputs(i.Cross),
 		Union: convert1_7Inputs(i.Union),
 		Cron:  convert1_7CronInput(i.Cron),
 		Git:   convert1_7GitInput(i.Git),
 	}
-
 }
 
 func convert1_7Inputs(inputs []*pps_1_7.Input) []*pps.Input {
-	result := make([]*pfs.Input, 0, len(inputs))
-	for _, i := range branches {
-		result := append(result, convert1_7Input(i))
+	if inputs == nil {
+		return nil
+	}
+	result := make([]*pps.Input, 0, len(inputs))
+	for _, i := range inputs {
+		result = append(result, convert1_7Input(i))
 	}
 	return result
 }
 
 func convert1_7Service(s *pps_1_7.Service) *pps.Service {
+	if s == nil {
+		return nil
+	}
 	return &pps.Service{
 		InternalPort: s.InternalPort,
 		ExternalPort: s.ExternalPort,
@@ -191,6 +258,9 @@ func convert1_7Service(s *pps_1_7.Service) *pps.Service {
 }
 
 func convert1_7ChunkSpec(c *pps_1_7.ChunkSpec) *pps.ChunkSpec {
+	if c == nil {
+		return nil
+	}
 	return &pps.ChunkSpec{
 		Number:    c.Number,
 		SizeBytes: c.SizeBytes,
@@ -198,6 +268,9 @@ func convert1_7ChunkSpec(c *pps_1_7.ChunkSpec) *pps.ChunkSpec {
 }
 
 func convert1_7SchedulingSpec(s *pps_1_7.SchedulingSpec) *pps.SchedulingSpec {
+	if s == nil {
+		return nil
+	}
 	return &pps.SchedulingSpec{
 		NodeSelector:      s.NodeSelector,
 		PriorityClassName: s.PriorityClassName,
