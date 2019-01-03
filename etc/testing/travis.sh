c8fb0b8c0d87e1f065def872531ee5d87c3ee782
Yusuf Simonson <simonson@gmail.com>
2019-01-02 20:08:47 -0500

    Fixed tabbing

--- etc/testing/travis.sh
+++ etc/testing/travis.sh
@@ -18,16 +18,16 @@ until {
   minikube status 2>&1 >/dev/null
   kubectl version 2>&1 >/dev/null
 }; do
-  if ((max_t-- <= 0)); then
-    echo "Could not connect to minikube"
-    echo "minikube status --alsologtostderr --loglevel=0 -v9:"
-    echo "==================================================="
-    minikube status --alsologtostderr --loglevel=0 -v9
-    exit 1
-  fi
-	echo -en "\e[G$${WHEEL:0:1}";
-	WHEEL="$${WHEEL:1}$${WHEEL:0:1}";
-	sleep 1;
+    if ((max_t-- <= 0)); then
+        echo "Could not connect to minikube"
+        echo "minikube status --alsologtostderr --loglevel=0 -v9:"
+        echo "==================================================="
+        minikube status --alsologtostderr --loglevel=0 -v9
+        exit 1
+    fi
+    echo -en "\e[G$${WHEEL:0:1}";
+    WHEEL="$${WHEEL:1}$${WHEEL:0:1}";
+    sleep 1;
 done
 minikube status
 kubectl version
@@ -39,17 +39,16 @@ PPS_SUITE=`echo $BUCKET | grep PPS > /dev/null; echo $?`
 make install
 make docker-build
 for i in $(seq 3); do
-  make clean-launch-dev || true # may be nothing to delete
-  make launch-dev && break
-  (( i < 3 )) # false if this is the last loop (causes exit)
-  sleep 10
+    make clean-launch-dev || true # may be nothing to delete
+    make launch-dev && break
+    (( i < 3 )) # false if this is the last loop (causes exit)
+    sleep 10
 done
 
 go install ./src/testing/match
 
 if [[ "$BUCKET" == "MISC" ]]; then
-	echo "Running misc test suite"
-
+    echo "Running misc test suite"
 
     if [[ "$TRAVIS_PULL_REQUEST_SLUG" == "pachyderm/pachyderm" ]]; then
         make lint enterprise-code-checkin-test docker-build test-pfs-server test-pfs-cmds test-deploy-cmds test-libs test-vault test-auth test-enterprise test-worker test-admin
@@ -59,39 +58,39 @@ if [[ "$BUCKET" == "MISC" ]]; then
         make lint enterprise-code-checkin-test docker-build test-pfs-server test-pfs-cmds test-deploy-cmds test-libs test-auth test-enterprise test-worker test-admin
     fi
 
-	make test-misc
+    make test-misc
 elif [[ $PPS_SUITE -eq 0 ]]; then
-	PART=`echo $BUCKET | grep -Po '\d+'`
-	NUM_BUCKETS=`cat etc/build/PPS_BUILD_BUCKET_COUNT`
-	echo "Running pps test suite, part $PART of $NUM_BUCKETS"
-	LIST=`go test -v  ./src/server/ -list ".*" | grep -v ok | grep -v Benchmark`
-	COUNT=`echo $LIST | tr " " "\n" | wc -l`
-	BUCKET_SIZE=$(( $COUNT / $NUM_BUCKETS ))
-	MIN=$(( $BUCKET_SIZE * $(( $PART - 1 )) ))
-	#The last bucket may have a few extra tests, to accommodate rounding errors from bucketing:
-	MAX=$COUNT
-	if [[ $PART -ne $NUM_BUCKETS ]]; then
-		MAX=$(( $MIN + $BUCKET_SIZE ))
+    PART=`echo $BUCKET | grep -Po '\d+'`
+    NUM_BUCKETS=`cat etc/build/PPS_BUILD_BUCKET_COUNT`
+    echo "Running pps test suite, part $PART of $NUM_BUCKETS"
+    LIST=`go test -v  ./src/server/ -list ".*" | grep -v ok | grep -v Benchmark`
+    COUNT=`echo $LIST | tr " " "\n" | wc -l`
+    BUCKET_SIZE=$(( $COUNT / $NUM_BUCKETS ))
+    MIN=$(( $BUCKET_SIZE * $(( $PART - 1 )) ))
+    #The last bucket may have a few extra tests, to accommodate rounding errors from bucketing:
+    MAX=$COUNT
+    if [[ $PART -ne $NUM_BUCKETS ]]; then
+        MAX=$(( $MIN + $BUCKET_SIZE ))
     fi
 
-	RUN=""
-	INDEX=0
+    RUN=""
+    INDEX=0
 
-	for test in $LIST; do
-		if [[ $INDEX -ge $MIN ]] && [[ $INDEX -lt $MAX ]] ; then
-			if [[ "$RUN" == "" ]]; then
-				RUN=$test
-			else
-				RUN="$RUN|$test"
-			fi
-		fi
-		INDEX=$(( $INDEX + 1 ))
-	done
-	echo "Running $( echo $RUN | tr '|' '\n' | wc -l ) tests of $COUNT total tests"
-	make RUN=-run=\"$RUN\" test-pps-helper
+    for test in $LIST; do
+        if [[ $INDEX -ge $MIN ]] && [[ $INDEX -lt $MAX ]] ; then
+            if [[ "$RUN" == "" ]]; then
+                RUN=$test
+            else
+                RUN="$RUN|$test"
+            fi
+        fi
+        INDEX=$(( $INDEX + 1 ))
+    done
+    echo "Running $( echo $RUN | tr '|' '\n' | wc -l ) tests of $COUNT total tests"
+    make RUN=-run=\"$RUN\" test-pps-helper
 else
-	echo "Unknown bucket"
-	exit 1
+    echo "Unknown bucket"
+    exit 1
 fi
 
 # Disable aws CI for now, see:
