2f8cfb8a15471811e238322ff12f7a9716d6fecc
Yusuf Simonson <simonson@gmail.com>
2019-01-30 16:18:34 -0500

    Support for multiple concurrent s3 servers

--- src/server/pfs/s3/s3_test.go
+++ src/server/pfs/s3/s3_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
+	"net/http"
 	"strings"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -11,31 +14,35 @@ import (
 	"github.com/pachyderm/pachyderm/src/client/pkg/require"
 )
 
-func TestSimple(t *testing.T) {
+var nextPort uint32 = 40000
+
+func serve(t *testing.T) (*http.Server, uint16) {
 	pc := server.GetPachClient(t)
-	repo := "repo"
-	require.NoError(t, pc.CreateRepo(repo))
-	file := "file"
-	content := "content"
-	_, err := pc.PutFile(repo, "master", file, strings.NewReader(content))
-	require.NoError(t, err)
-	go func() { Serve(pc, 30655) }()
-	c, err := minio.New("127.0.0.1:30655", "id", "secret", false)
+	require.NoError(t, pc.CreateRepo("repo"))
+	_, err := pc.PutFile("repo", "master", "file", strings.NewReader("content"))
 	require.NoError(t, err)
 
-	// Try to fetch the contents a few times, in case the s3 proxy is still
-	// booting up
-	var fetchedContent string
-	for i := 0; i < 10; i++ {
-		fetchedContent, err = getObject(c, repo, file)
-		if err == nil {
-			break
+	port := uint16(atomic.AddUint32(&nextPort, 1))
+	srv := Server(pc, port)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+			t.Fatalf("http server returned an error: %v", err)
 		}
-		time.Sleep(1 * time.Second)
+	}()
+
+	// Wait for the server to start
+	c := &http.Client{}
+	for i := 0; i < 100; i++ {
+		res, err := c.Get(fmt.Sprintf("http://127.0.0.1:%d/repo/", port))
+		if err == nil && res.StatusCode == 200 {
+			return srv, port
+		}
+		time.Sleep(100 * time.Millisecond)
 	}
 
-	require.NoError(t, err)
-	require.Equal(t, content, fetchedContent)
+	t.Fatalf("server failed to start after a few seconds")
+	return nil, 0
 }
 
 func getObject(c *minio.Client, repo, file string) (string, error) {
@@ -51,4 +58,17 @@ func getObject(c *minio.Client, repo, file string) (string, error) {
 		return "", err
 	}
 	return string(bytes), nil
+}
+
+
+func TestGetFile(t *testing.T) {
+	srv, port := serve(t)
+	c, err := minio.New(fmt.Sprintf("127.0.0.1:%d", port), "id", "secret", false)
+	require.NoError(t, err)
+
+	fetchedContent, err := getObject(c, "repo", "file")
+	require.NoError(t, err)
+	require.Equal(t, "content", fetchedContent)
+
+	require.NoError(t, srv.Close())
 }
--- src/server/pfs/s3/server.go
+++ src/server/pfs/s3/server.go
@@ -10,10 +10,62 @@ import (
 	"github.com/pachyderm/pachyderm/src/client"
 )
 
+// const shutdownTimeoutSecs = 10
 const locationResponse = `<?xml version="1.0" encoding="UTF-8"?>
 <LocationConstraint xmlns="http://s3.amazonaws.com/doc/2006-03-01/">PACHYDERM</LocationConstraint>`
 
-func serveRoot(pc *client.APIClient, w http.ResponseWriter, r *http.Request, repo string) {
+// type S3Server struct {
+// 	server *http.Server
+// }
+
+// func NewS3Server() *S3Server {
+// 	return *S3Server{
+// 		server: *http.Server{
+// 			Addr: ,
+// 			Handler: 
+// 		}
+// 	}
+// }
+
+// func (s *S3Server) Listen() error {
+// 	if err := s.server.ListenAndServe(); err == http.ErrServerClosed {
+// 		return nil
+// 	} else {
+// 		return err
+// 	}
+// }
+
+// func (s *S3Server) Close() error {
+// 	ctx := context.WithTimeout(context.Background(), shutdownTimeoutSecs * time.Second)
+// 	return s.server.Shutdown(ctx)
+// }
+
+type handler struct {
+	pc *client.APIClient
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	parts := strings.SplitN(r.URL.Path, "/", 3)
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, fmt.Sprintf("%v", err), 400)
+		return
+	}
+
+	if len(parts) < 3 {
+		http.Error(w, "Invalid path", 404)
+		return
+	}
+
+	repo, file := parts[1], parts[2]
+
+	if file == "" {
+		h.serveRoot(w, r, repo)
+	} else {
+		h.serveFile(w, r, repo, file)
+	}
+}
+
+func (h handler) serveRoot(w http.ResponseWriter, r *http.Request, repo string) {
 	if _, ok := r.Form["location"]; ok {
 		w.Header().Set("Content-Type", "application/xml")
 		w.Write([]byte(locationResponse))
@@ -22,8 +74,8 @@ func serveRoot(pc *client.APIClient, w http.ResponseWriter, r *http.Request, rep
 	}
 }
 
-func serveFile(pc *client.APIClient, w http.ResponseWriter, r *http.Request, repo, name string) {
-	fileInfo, err := pc.InspectFile(repo, "master", name)
+func (h handler) serveFile(w http.ResponseWriter, r *http.Request, repo, name string) {
+	fileInfo, err := h.pc.InspectFile(repo, "master", name)
 	if err != nil {
 		code := 500
 
@@ -41,7 +93,7 @@ func serveFile(pc *client.APIClient, w http.ResponseWriter, r *http.Request, rep
 		return
 	}
 
-	reader, err := pc.GetFileReadSeeker(repo, "master", name)
+	reader, err := h.pc.GetFileReadSeeker(repo, "master", name)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("%v", err), 500)
 		return
@@ -50,28 +102,10 @@ func serveFile(pc *client.APIClient, w http.ResponseWriter, r *http.Request, rep
 	http.ServeContent(w, r, "", timestamp, reader)
 }
 
-// Serve runs an HTTP server with an S3-like API for PFS
-func Serve(pc *client.APIClient, port uint16) {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		parts := strings.SplitN(r.URL.Path, "/", 3)
-		if err := r.ParseForm(); err != nil {
-			http.Error(w, fmt.Sprintf("%v", err), 400)
-			return
-		}
-
-		if len(parts) < 3 {
-			http.Error(w, "Invalid path", 404)
-			return
-		}
-
-		repo, file := parts[1], parts[2]
-
-		if file == "" {
-			serveRoot(pc, w, r, repo)
-		} else {
-			serveFile(pc, w, r, repo, file)
-		}
-	})
-
-	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+// Server runs an HTTP server with an S3-like API for PFS
+func Server(pc *client.APIClient, port uint16) *http.Server {
+	return &http.Server {
+		Addr: fmt.Sprintf(":%d", port),
+		Handler: handler{ pc: pc },
+	}
 }
