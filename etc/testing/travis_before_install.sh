8ccccb24eb22ef31dde755158b98cf408701c86a
Yusuf Simonson <simonson@gmail.com>
2019-02-05 14:15:06 -0500

    Standardized tabbing

--- etc/testing/travis_before_install.sh
+++ etc/testing/travis_before_install.sh
@@ -15,37 +15,37 @@ chown root:$USER /etc/fuse.conf
 # To get the latest kubectl version:
 # curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt
 if [ ! -f ~/cached-deps/kubectl ] ; then
-	KUBECTL_VERSION=v1.13.0
-	curl -L -o kubectl https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl && \
-	    chmod +x ./kubectl && \
-	    sudo mv ./kubectl ~/cached-deps/kubectl
-	kubectl version --client
+    KUBECTL_VERSION=v1.13.0
+    curl -L -o kubectl https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl && \
+        chmod +x ./kubectl && \
+        sudo mv ./kubectl ~/cached-deps/kubectl
+    kubectl version --client
 fi
 
 # Install minikube
 # To get the latest minikube version:
 # curl https://api.github.com/repos/kubernetes/minikube/releases | jq -r .[].tag_name | sort | tail -n1
 if [ ! -f ~/cached-deps/minikube ] ; then
-	MINIKUBE_VERSION=v0.31.0
-	curl -L -o minikube https://storage.googleapis.com/minikube/releases/${MINIKUBE_VERSION}/minikube-linux-amd64 && \
-	    chmod +x ./minikube && \
-	    sudo mv ./minikube ~/cached-deps/minikube
+    MINIKUBE_VERSION=v0.31.0
+    curl -L -o minikube https://storage.googleapis.com/minikube/releases/${MINIKUBE_VERSION}/minikube-linux-amd64 && \
+        chmod +x ./minikube && \
+        sudo mv ./minikube ~/cached-deps/minikube
 fi
 
 # Install vault
 if [ ! -f ~/cached-deps/vault ] ; then
-	curl -Lo vault.zip https://releases.hashicorp.com/vault/0.9.5/vault_0.9.5_linux_amd64.zip && \
-	    unzip vault.zip && \
-	    sudo mv ./vault ~/cached-deps/vault
+    curl -Lo vault.zip https://releases.hashicorp.com/vault/0.9.5/vault_0.9.5_linux_amd64.zip && \
+        unzip vault.zip && \
+        sudo mv ./vault ~/cached-deps/vault
 fi
 
 # Install etcdctl
 # To get the latest etcd version:
 # curl -s https://api.github.com/repos/coreos/etcd/releases | jq -r .[].tag_name | sort | tail -n1
 if [ ! -f ~/cached-deps/etcdctl ] ; then
-	ETCD_VERSION=v3.3.6
-	curl -L https://storage.googleapis.com/etcd/${ETCD_VERSION}/etcd-${ETCD_VERSION}-linux-amd64.tar.gz | \
-	  tar xzf - --strip-components=1 && \
-	  sudo mv ./etcdctl ~/cached-deps/etcdctl
-	etcdctl --version
+    ETCD_VERSION=v3.3.6
+    curl -L https://storage.googleapis.com/etcd/${ETCD_VERSION}/etcd-${ETCD_VERSION}-linux-amd64.tar.gz | \
+        tar xzf - --strip-components=1 && \
+        sudo mv ./etcdctl ~/cached-deps/etcdctl
+    etcdctl --version
 fi
