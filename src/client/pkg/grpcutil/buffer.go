4dc115cf6e5d0a55af13e2436bdd1b49d35fd5bd
Joe Doliner <jdoliner@gmail.com>
2019-02-04 16:54:30 -0800

    Pour one out for the linter.

--- src/client/pkg/grpcutil/buffer.go
+++ src/client/pkg/grpcutil/buffer.go
@@ -4,10 +4,13 @@ import (
 	"sync"
 )
 
+// BufPool is a wrapper around sync.Pool that makes it a little nicer to use
+// for []byte by doing the casting for you and defining the `New` function.
 type BufPool struct {
 	sync.Pool
 }
 
+// NewBufPool creates a new BufPool that returns buffers of the given size.
 func NewBufPool(size int) *BufPool {
 	return &BufPool{sync.Pool{
 		New: func() interface{} { return make([]byte, size) },
@@ -25,7 +28,7 @@ func (b *BufPool) PutBuffer(buf []byte) {
 	bufPool.Put(buf)
 }
 
-// bufPool is a pool that's size for grpc connections
+// bufPool is a pool of buffers that are sized for grpc connections
 // This buffer size is:
 // 1. Reasonably smaller than the max gRPC size
 // 2. Small enough that having hundreds of these buffers won't
