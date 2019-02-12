d046e4e4c75fde7790f117a9b74fe20d1b2c2995
Matthew Steffen <msteffen@pachyderm.io>
2019-02-12 10:12:54 -0800

    Tweak comments

--- src/testing/loadtest/split/build/docker-build.sh
+++ src/testing/loadtest/split/build/docker-build.sh
@@ -3,7 +3,7 @@
 # (i.e. in a scratch containiner, with no libc), so that their docker images are
 # as small as possible.
 #
-# **Compiled binaries will be stored in ./out**
+# **Compiled binaries will be stored in ./_out**
 
 set -ex
 
@@ -11,8 +11,8 @@ set -ex
 rm -rf ./_out || true
 mkdir _out
 
-# Setup build command. The linker flags tell the go compiler to build a fully
-# static binary
+# Setup build command. The linker flags, along with CGO_ENABLED=0 (set below)
+# tell the go compiler to build a fully static binary (see comment at top)
 LD_FLAGS="-extldflags -static"
 BUILD_PATH=github.com/pachyderm/pachyderm/src/testing/loadtest/split
 BUILD_CMD="
@@ -31,20 +31,21 @@ mv ../bin/* /out/"
 #   faster:
 #     /root/.cache/go-build <- $HOME/.cache/go-build
 #
-# - This makes pachyderm (including the pachyderm client and this benchmark)
-#   available to be built. Mounting all of it in (rather than e.g. copying it)
-#   means that parallel changes can be made to the benchmark, server, and
-#   client simultaneously:
+# - Mouting the whole pachyderm repo (including the pachyderm client and this
+#   benchmark) makes this benchmark available to be built. Mounting all of it in
+#   together (rather than e.g. mounting just this dir and having the pachdyerm
+#   repo in ./vendor) means that parallel changes can be made to the benchmark,
+#   server, and client simultaneously:
 #     /go/src/.../pachyderm <- $GOPATH/src/.../pachyerm/
 #
-# - This is necessary to build the pachyderm client (which the 'split'
-#   supervisor depends on, and which has dependencies but has no vendor
-#   directory). It accomplishes a similar goal to the mainline pachyderm's
-#   'make docker-build' directive, which symlinks src/server/vendor into the
-#   client:
+# - Mounting src/server/vendor into src/client/vendor is necessary to build the
+#   pachyderm client (which the 'split' supervisor depends on, and which has
+#   dependencies but has no vendor directory). It accomplishes a similar goal to
+#   the mainline pachyderm's 'make docker-build' directive, which symlinks
+#   src/server/vendor into the client:
 #     /go/src/../pachyderm/src/client/vendor <- $GOPATH/.../pachyderm/src/server/vendor
 #
-# - This avoids a stupid bug where
+# - Mounting ./_out into src/client/vendor/...pachyderm avoids a stupid bug where
 #   src/client/vendor/github.com/pachyderm/pachyderm/src/client can fail to
 #   build because it (a vendored dependency) doesn't have the right
 #   dependencies to build. We prevent the go compiler from trying to build the
