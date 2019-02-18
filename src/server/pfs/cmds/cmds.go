c58f7b5e3b0f5c4a7b5a74078c7b76508f51fc68
Yusuf Simonson <simonson@gmail.com>
2019-02-18 14:14:13 -0500

    Added command for running s3 gateway

--- src/server/pfs/cmds/cmds.go
+++ src/server/pfs/cmds/cmds.go
@@ -23,6 +23,7 @@ import (
 	pfsclient "github.com/pachyderm/pachyderm/src/client/pfs"
 	"github.com/pachyderm/pachyderm/src/client/pkg/grpcutil"
 	"github.com/pachyderm/pachyderm/src/server/pfs/fuse"
+	"github.com/pachyderm/pachyderm/src/server/pfs/s3"
 	"github.com/pachyderm/pachyderm/src/server/pfs/pretty"
 	"github.com/pachyderm/pachyderm/src/server/pkg/cmdutil"
 	"github.com/pachyderm/pachyderm/src/server/pkg/sync"
@@ -1144,6 +1145,26 @@ $ pachctl diff-file foo master path1 bar master path2
 	}
 	unmount.Flags().BoolVarP(&all, "all", "a", false, "unmount all pfs mounts")
 
+	var port uint16
+	s3gateway := &cobra.Command{
+		Use:   "s3gateway",
+		Short: "Expose pfs via an S3-like API. This command blocks.",
+		Long:  "Expose pfs via an S3-like API. This command blocks.",
+		Run: cmdutil.RunFixedArgs(0, func(args []string) error {
+			client, err := client.NewOnUserMachine(!*noMetrics, !*noPortForwarding, "user")
+			if err != nil {
+				return err
+			}
+			defer client.Close()
+			server := s3.Server(client, port)
+			if err := server.ListenAndServe(); err != http.ErrServerClosed {
+				return err
+			}
+			return nil
+		}),
+	}
+	s3gateway.Flags().Uint16VarP(&port, "port", "p", 30600, "The local port to bind the S3 gateway to.")
+
 	var result []*cobra.Command
 	result = append(result, repo)
 	result = append(result, createRepo)
@@ -1176,6 +1197,7 @@ $ pachctl diff-file foo master path1 bar master path2
 	result = append(result, getTag)
 	result = append(result, mount)
 	result = append(result, unmount)
+	result = append(result, s3gateway)
 	return result
 }
 
