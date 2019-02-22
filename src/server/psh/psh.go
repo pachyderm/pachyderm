fad0688010962da6b191173d32b64738c2897beb
Joe Doliner <jdoliner@gmail.com>
2019-02-22 15:05:26 -0800

    Actually exec lines.

--- src/server/psh/psh.go
+++ src/server/psh/psh.go
@@ -3,9 +3,16 @@ package psh
 import (
 	"fmt"
 	"io"
+	"math/rand"
 	"os"
+	"os/exec"
+	"strings"
 
 	"golang.org/x/crypto/ssh/terminal"
+)
+
+var (
+	prompts = []string{"🐘 ", "🐎 ", "🐴 ", "🐮 ", "🐂 "}
 )
 
 func Shell() error {
@@ -21,18 +28,43 @@ func Shell() error {
 		io.Reader
 		io.Writer
 	}{os.Stdin, os.Stdout}
-	term := terminal.NewTerminal(screen, "🐘 ")
+	term := terminal.NewTerminal(screen, prompts[rand.Intn(len(prompts))])
 	for {
 		line, err := term.ReadLine()
 		if err == io.EOF {
+			fmt.Fprintln(os.Stdout, "exit")
 			return nil
 		}
 		if err != nil {
 			return err
 		}
-		if line == "" {
-			continue
+		parts := strings.Split(line, " ")
+		if len(parts) == 0 {
+			return nil
 		}
-		fmt.Fprintln(term, line)
+		cmd := exec.Command(parts[0], parts[1:]...)
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		terminal.Restore(0, oldState)
+		if err := cmd.Run(); err != nil {
+			return err
+		}
+		oldState, err = terminal.MakeRaw(0)
+		if err != nil {
+			return err
+		}
 	}
+}
+
+func Run(line string) error {
+	parts := strings.Split(line, " ")
+	if len(parts) == 0 {
+		return nil
+	}
+	cmd := exec.Command(parts[0], parts[1:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
 }
