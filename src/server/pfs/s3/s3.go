8f8b500b403731e7019a0c2b07ae327956cb89ed
Yusuf Simonson <simonson@gmail.com>
2019-03-06 13:11:41 -0500

    Added note

--- src/server/pfs/s3/s3.go
+++ src/server/pfs/s3/s3.go
@@ -71,6 +71,8 @@ func Server(pc *client.APIClient, port uint16, errLogWriter io.Writer, multipart
 	objectRouter.Methods("DELETE").HandlerFunc(objectHandler.del)
 
 	router.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// TODO: this will trigger for paths that are not valid utf-8 strings, giving the incorrect error message. See:
+		// ./etc/testing/s3gateway/conformance.py --nose-args 's3tests.functional.test_s3:test_object_create_unreadable' --no-persist
 		newInvalidBucketNameError(r).write(w)
 	})
 
