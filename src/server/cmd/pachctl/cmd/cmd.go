bfa31242f06cf68589088fa7445709e0a1240654
Matthew Steffen <msteffen@pachyderm.io>
2019-03-06 11:42:02 +0100

    Fix error in pachctl documentation

--- src/server/cmd/pachctl/cmd/cmd.go
+++ src/server/cmd/pachctl/cmd/cmd.go
@@ -153,9 +153,9 @@ func PachctlCmd() (*cobra.Command, error) {
 Environment variables:
   PACHD_ADDRESS=<host>:<port>, the pachd server to connect to (e.g. 127.0.0.1:30650).
   PACH_CONFIG=<path>, the path where pachctl will attempt to load your pach config.
-  JAEGER_ENDPOINT=<host>:<port>, the jaeger server to connect to, if --trace is set
-	PACH_ENABLE_TRACING={true,false}, If true, and JAEGER_ENDPOINT is set, attach a
-	  Jaeger trace to all outgoing RPCs
+  JAEGER_ENDPOINT=<host>:<port>, the Jaeger server to connect to, if PACH_ENABLE_TRACING is set
+  PACH_ENABLE_TRACING={true,false}, If true, and JAEGER_ENDPOINT is set, attach a
+    Jaeger trace to all outgoing RPCs
 `,
 		PersistentPreRun: func(cmd *cobra.Command, args []string) {
 			log.SetFormatter(new(prefixed.TextFormatter))
