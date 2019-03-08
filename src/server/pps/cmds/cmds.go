708542c8db72801df7bb679f60390fa277ce34ce
Tryneus <marc.hesse@gmail.com>
2019-03-07 16:49:24 -0800

    Fix some typos in docs and strings (#3554)

--- src/server/pps/cmds/cmds.go
+++ src/server/pps/cmds/cmds.go
@@ -55,12 +55,12 @@ func Cmds(noMetrics *bool, noPortForwarding *bool) ([]*cobra.Command, error) {
 
 Jobs run a containerized workload over a set of finished input commits.
 Creating a job will also create a new repo and a commit in that repo which
-contains the output of the job. Unless the job is created with another job as a
-parent. If the job is created with a parent it will use the same repo as its
+contains the output of the job, unless the job is created with another job as a
+parent. If the job is created with a parent, it will use the same repo as its
 parent job and the commit it creates will use the parent job's commit as a
 parent.
-If the job fails the commit it creates will not be finished.
-The increase the throughput of a job increase the Shard paremeter.
+If the job fails, the commit it creates will not be finished.
+To increase the throughput of a job, increase the 'shard' parameter.
 `,
 		Run: cmdutil.RunFixedArgs(0, func(args []string) error {
 			return nil
