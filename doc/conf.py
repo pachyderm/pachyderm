1b8b8078f33c1dcaaf9406e87ac8fa4f13358001
John Karabaic <36678903+pappasilenus@users.noreply.github.com>
2019-03-25 13:44:26 -0700

    changed copyright year
    should probably add a year range at some point.

--- doc/conf.py
+++ doc/conf.py
@@ -57,7 +57,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Pachyderm'
-copyright = '2018, Pachyderm Inc.'
+copyright = '2019, Pachyderm Inc.'
 author = 'Pachyderm'
 
 # The version info for the project you're documenting, acts as replacement for
