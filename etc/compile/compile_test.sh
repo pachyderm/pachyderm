8aee8f1eb7da2c82a157109a32956e706191e1ac
Matthew Steffen <msteffen@pachyderm.io>
2019-04-09 00:41:25 -0700

    Make in-cluster (dockerized) test more complete

--- etc/compile/compile_test.sh
+++ etc/compile/compile_test.sh
@@ -17,8 +17,33 @@ fi
 rm -rf ./_tmp/*
 echo "Building test"
 go test \
-  -c -o /go/src/github.com/pachyderm/pachyderm/_tmp/test \
+  -c -o /go/src/github.com/pachyderm/pachyderm/_tmp/pachyderm_test \
   ./src/server
+go test \
+  -c -o /go/src/github.com/pachyderm/pachyderm/_tmp/pfs_server_test \
+  ./src/server/pfs/server
+go test \
+  -c -o /go/src/github.com/pachyderm/pachyderm/_tmp/pfs_cmds_test \
+  ./src/server/pfs/cmds
+go test \
+  -c -o /go/src/github.com/pachyderm/pachyderm/_tmp/pps_cmds_test \
+  ./src/server/pps/cmds
+go test \
+  -c -o /go/src/github.com/pachyderm/pachyderm/_tmp/auth_server_test \
+  ./src/server/auth/server
+go test \
+  -c -o /go/src/github.com/pachyderm/pachyderm/_tmp/auth_cmds_test \
+  ./src/server/auth/cmds
+go test \
+  -c -o /go/src/github.com/pachyderm/pachyderm/_tmp/enterprise_server_test \
+  ./src/server/enterprise/server
+go test \
+  -c -o /go/src/github.com/pachyderm/pachyderm/_tmp/worker_test \
+  ./src/server/worker
+go test -c -o /go/src/github.com/pachyderm/pachyderm/_tmp/collection_test ./src/server/pkg/collection
+go test -c -o /go/src/github.com/pachyderm/pachyderm/_tmp/hashtree_test ./src/server/pkg/hashtree
+go test -c -o /go/src/github.com/pachyderm/pachyderm/_tmp/cert_test ./src/server/pkg/cert
+go test -c -o /go/src/github.com/pachyderm/pachyderm/_tmp/vault_test ./src/plugin/vault
 echo "Test built..."
 pwd
 ls /go/src/github.com/pachyderm/pachyderm/_tmp
@@ -26,5 +51,5 @@ ls /go/src/github.com/pachyderm/pachyderm/_tmp
 cp Dockerfile.test _tmp/Dockerfile
 cp etc/testing/artifacts/giphy.gif _tmp/
 docker build -t pachyderm_test _tmp
-docker tag pachyderm_test pachyderm/test:latest
-docker tag pachyderm_test pachyderm/test:local
+docker tag pachyderm_test:latest pachyderm/test:latest
+docker tag pachyderm_test:latest pachyderm/test:local
