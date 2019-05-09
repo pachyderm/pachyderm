f161a5cdf51a81f8a90acf7d4ac094c4cae2c0a4
Joe Doliner <jdoliner@gmail.com>
2019-05-08 17:11:04 -0700

    Adds the start of a gpt-2 example.

---
+++ examples/ml/gpt-2/tweets.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python3
+from twitter_scraper import get_tweets
+import os
+
+for name in os.listdir("/pfs/users/users"):
+    with open(os.path.join("/pfs/users/users", name)) as f:
+        for user in f:
+            user = user.strip()
+            with open(os.path.join("/pfs/out", user), "w+") as out:
+                for tweet in get_tweets(user, pages=1):
+                    out.write("<|begintweet|> ")
+                    out.write(tweet['text'])
+                    out.write(" <|endtweet|> ")
