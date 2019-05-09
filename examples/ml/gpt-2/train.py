8350d2e8938097754106531ff03d12df18c65bfc
Joe Doliner <jdoliner@gmail.com>
2019-05-09 15:24:07 -0700

    Adds start of train code.

--- examples/ml/gpt-2/train.py
+++ examples/ml/gpt-2/train.py
@@ -1,12 +1,16 @@
+#!/usr/local/bin/python3
 import gpt_2_simple as gpt2
+import os
+
+tweets = [f for f in os.listdir("/pfs/tweets")]
 
 model_name = "117M"
-gpt2.download_gpt2(model_name=model_name)   # model is saved into current directory under /models/117M/
+gpt2.download_gpt2(model_name=model_name)
 
 sess = gpt2.start_tf_sess()
 gpt2.finetune(sess,
-              'got.txt',
+              os.path.join("/pfs/tweets", tweets[0]),
               model_name=model_name,
-              steps=1000)   # steps is max number of training steps
+              steps=10)   # steps is max number of training steps
 
 gpt2.generate(sess)
