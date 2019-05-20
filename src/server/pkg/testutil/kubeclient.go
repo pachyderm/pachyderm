6e21ae56359e51d42ff59a6285ee228902293ab9
Matthew Steffen <msteffen@pachyderm.io>
2019-05-20 11:29:34 -0700

    Shorten tu.GetKubeClient

--- src/server/pkg/testutil/kubeclient.go
+++ src/server/pkg/testutil/kubeclient.go
@@ -1,15 +1,19 @@
 package testutil
 
 import (
-	"bytes"
-	"github.com/pachyderm/pachyderm/src/client/pkg/require"
+	"fmt"
 	"os"
-	"os/exec"
-	"strings"
 	"testing"
+	"time"
 
+	"github.com/pachyderm/pachyderm/src/client/pkg/require"
+	"github.com/pachyderm/pachyderm/src/server/pkg/backoff"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kube "k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 // GetKubeClient connects to the Kubernetes API server either from inside the
@@ -17,78 +21,17 @@ import (
 // connect to the same cluster as kubectl)
 func GetKubeClient(t testing.TB) *kube.Clientset {
 	var config *rest.Config
+	var err error
 	host := os.Getenv("KUBERNETES_SERVICE_HOST")
 	if host != "" {
-		var err error
 		config, err = rest.InClusterConfig()
-		require.NoError(t, err)
 	} else {
-		// Use kubectl binary to parse .kube/config and get address of current
-		// cluster. Hopefully, once we upgrade to k8s.io/client-go, we will be able
-		// to do this in-process with a library
-		// First, figure out if we're talking to minikube or localhost
-		cmd := exec.Command("kubectl", "config", "current-context")
-		cmd.Stderr = os.Stderr
-		if context, err := cmd.Output(); err == nil {
-			context = bytes.TrimSpace(context)
-			// kubectl has a context -- not talking to localhost
-			// Get cluster and user name from kubectl
-			buf := &bytes.Buffer{}
-			cmd := BashCmd(strings.Join([]string{
-				`kubectl config get-contexts "{{.context}}" | tail -n+2 | awk '{print $3}'`,
-				`kubectl config get-contexts "{{.context}}" | tail -n+2 | awk '{print $4}'`,
-			}, "\n"),
-				"context", string(context))
-			cmd.Stdout = buf
-			require.NoError(t, cmd.Run(), "couldn't get kubernetes context info")
-			lines := strings.Split(buf.String(), "\n")
-			clustername, username := lines[0], lines[1]
-
-			// Get user info
-			buf.Reset()
-			cmd = BashCmd(strings.Join([]string{
-				`cluster="$(kubectl config view -o json | jq -r '.users[] | select(.name == "{{.user}}") | .user' )"`,
-				`echo "${cluster}" | jq -r '.["client-certificate"]'`,
-				`echo "${cluster}" | jq -r '.["client-key"]'`,
-			}, "\n"),
-				"user", username)
-			cmd.Stdout = buf
-			require.NoError(t, cmd.Run(), "couldn't get kubernetes user info")
-			lines = strings.Split(buf.String(), "\n")
-			clientCert, clientKey := lines[0], lines[1]
-
-			// Get cluster info
-			buf.Reset()
-			cmd = BashCmd(strings.Join([]string{
-				`cluster="$(kubectl config view -o json | jq -r '.clusters[] | select(.name == "{{.cluster}}") | .cluster')"`,
-				`echo "${cluster}" | jq -r .server`,
-				`echo "${cluster}" | jq -r '.["certificate-authority"]'`,
-			}, "\n"),
-				"cluster", clustername)
-			cmd.Stdout = buf
-			require.NoError(t, cmd.Run(), "couldn't get kubernetes cluster info: %s", buf.String())
-			lines = strings.Split(buf.String(), "\n")
-			address, CAKey := lines[0], lines[1]
-
-			// Generate config
-			config = &rest.Config{
-				Host: address,
-				TLSClientConfig: rest.TLSClientConfig{
-					CertFile: clientCert,
-					KeyFile:  clientKey,
-					CAFile:   CAKey,
-				},
-			}
-		} else {
-			// no context -- talking to localhost
-			config = &rest.Config{
-				Host: "http://0.0.0.0:8080",
-				TLSClientConfig: rest.TLSClientConfig{
-					Insecure: false,
-				},
-			}
-		}
+		rules := clientcmd.NewDefaultClientConfigLoadingRules()
+		kubeConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(rules,
+			&clientcmd.ConfigOverrides{})
+		config, err = kubeConfig.ClientConfig()
 	}
+	require.NoError(t, err)
 	k, err := kube.NewForConfig(config)
 	require.NoError(t, err)
 	return k
