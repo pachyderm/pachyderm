86b83a3a0a3f41521e4fc4ef22c072f57cf72697
Kyle White <kylewhite@gmail.com>
2019-05-26 23:47:32 -0400

    Try running misc tests

--- etc/testing/travis.sh
+++ etc/testing/travis.sh
@@ -15,3 +15,8 @@ kind load docker-image pachyderm/pachd:local
 #make docker-build-kafka
 
 make launch-dev
+
+#Misc tests
+make lint test-pfs-server test-pfs-cmds test-pfs-storage test-deploy-cmds \
+    test-libs test-vault test-auth test-enterprise test-worker test-admin \
+    test-s3gateway-integration test-proto-static test-transaction
