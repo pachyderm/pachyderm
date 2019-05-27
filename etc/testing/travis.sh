4a752f10a7ec2818512cdba423f9649bca4b8655
Kyle White <kylewhite@gmail.com>
2019-05-26 23:34:59 -0400

    Load local images into kind

--- etc/testing/travis.sh
+++ etc/testing/travis.sh
@@ -8,12 +8,9 @@ PPS_SUITE=`echo $BUCKET | grep PPS > /dev/null; echo $?`
 
 make install
 make docker-build
-#COMPILE_IMAGE = "pachyderm/compile:$(shell cat etc/compile/GO_VERSION)"
-#make docker-build-worker 
 
-#docker-build-pachd 
-#docker-wait-worker 
-#docker-wait-pachd
+kind load docker-image pachyderm/worker:local
+kind load docker-image pachyderm/pachd:local
 
 #make docker-build-kafka
 
