5d6c69b300e072f209964fa13736bb08dcbdb63a
Yusuf Simonson <simonson@gmail.com>
2019-06-06 09:51:50 -0400

    Removed ability to get active context config

--- src/server/config/cmds.go
+++ src/server/config/cmds.go
@@ -95,36 +95,28 @@ func Cmds() []*cobra.Command {
 	}
 	commands = append(commands, cmdutil.CreateAlias(useContext, "config use context"))
 
-	var active bool
 	getContext := &cobra.Command{
 		Short: "Gets a context.",
-		Long:  "Gets the config of a context by its name, or the current active one.",
+		Long:  "Gets the config of a context by its name.",
 		Run: cmdutil.RunBoundedArgs(0, 1, func(args []string) (retErr error) {
-			if active && len(args) == 1 {
-				return errors.New("cannot get both the active context, and a specific one by its name")
-			}
-
 			cfg, err := config.Read()
 			if err != nil {
 				return err
 			}
 
-			var name string
-			if active {
-				name = cfg.V2.ActiveContext
-			} else {
-				name = args[0]
-			}
-
-			context, ok := cfg.V2.Contexts[name]
+			context, ok := cfg.V2.Contexts[args[0]]
 			if !ok {
-				return fmt.Errorf("context does not exist: %s", name)
+				return fmt.Errorf("context does not exist: %s", args[0])
 			}
 
-			return marshaller.Marshal(os.Stdout, context)
+			if err = marshaller.Marshal(os.Stdout, context); err != nil {
+				return err
+			}
+
+			fmt.Println()
+			return nil
 		}),
 	}
-	getContext.Flags().BoolVar(&active, "active", false, "Get the active context.")
 	commands = append(commands, cmdutil.CreateAlias(getContext, "config get context"))
 
 	setContext := &cobra.Command{
