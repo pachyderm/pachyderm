b9346177702bfdcc323219e568daa8e7156de4a0
Bryce McAnally <bryce@pachyderm.io>
2019-06-06 12:04:55 -0700

    Add rangeSize for Write utility in test

--- src/server/pkg/storage/chunk/chunk_test.go
+++ src/server/pkg/storage/chunk/chunk_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/pachyderm/pachyderm/src/client/pkg/require"
 )
 
-func Write(t *testing.T, chunks *Storage, n int) ([]*DataRef, []byte) {
+func Write(t *testing.T, chunks *Storage, n, rangeSize int) ([]*DataRef, []byte) {
 	var finalDataRefs []*DataRef
 	var seq []byte
 	t.Run("Write", func(t *testing.T) {
@@ -19,9 +19,9 @@ func Write(t *testing.T, chunks *Storage, n int) ([]*DataRef, []byte) {
 			return nil
 		}
 		seq = RandSeq(n * MB)
-		for i := 0; i < n; i++ {
+		for i := 0; i < n/rangeSize; i++ {
 			w.StartRange(cb)
-			_, err := w.Write(seq[i*MB : (i+1)*MB])
+			_, err := w.Write(seq[i*MB*rangeSize : (i+1)*MB*rangeSize])
 			require.NoError(t, err)
 		}
 		require.NoError(t, w.Close())
@@ -32,7 +32,7 @@ func Write(t *testing.T, chunks *Storage, n int) ([]*DataRef, []byte) {
 func TestWriteThenRead(t *testing.T) {
 	objC, chunks := LocalStorage(t)
 	defer Cleanup(objC, chunks)
-	finalDataRefs, seq := Write(t, chunks, 100)
+	finalDataRefs, seq := Write(t, chunks, 100, 1)
 	mid := len(finalDataRefs) / 2
 	initialRefs := finalDataRefs[:mid]
 	streamRefs := finalDataRefs[mid:]
@@ -77,8 +77,8 @@ func TestCopyN(t *testing.T) {
 	objC, chunks := LocalStorage(t)
 	defer Cleanup(objC, chunks)
 	// Write the initial data and count the chunks.
-	dataRefs1, seq1 := Write(t, chunks, 50)
-	dataRefs2, seq2 := Write(t, chunks, 50)
+	dataRefs1, seq1 := Write(t, chunks, 60, 20)
+	dataRefs2, seq2 := Write(t, chunks, 60, 20)
 	var initialChunkCount int64
 	require.NoError(t, chunks.List(context.Background(), func(_ string) error {
 		initialChunkCount++
