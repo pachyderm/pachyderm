73772ae34d80457d35eee71481197563788f0d25
Matthew Steffen <msteffen@pachyderm.io>
2019-06-19 15:02:28 -0700

    Fix race in build script

--- etc/compile/compile.sh
+++ etc/compile/compile.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-set -Ee
+set -Eex
 
 DIR="$(cd "$(dirname "${0}")/../.." && pwd)"
 cd "${DIR}"
+pwd
 
 BINARY="${1}"
 LD_FLAGS="${2}"
 PROFILE="${3}"
 
-mkdir -p _tmp
+TMP="$(mktemp -d -p . build.XXXXXXXXXX)"
 CGO_ENABLED=0 GOOS=linux go build \
   -installsuffix netgo \
   -tags netgo \
-  -o _tmp/${BINARY} \
+  -o ${TMP}/${BINARY} \
   -ldflags "${LD_FLAGS}" \
   -gcflags "all=-trimpath=$GOPATH" \
   src/server/cmd/${BINARY}/main.go
@@ -23,15 +24,16 @@ echo "LD_FLAGS=$LD_FLAGS"
 # When creating profile binaries, we dont want to detach or do docker ops
 if [ -z ${PROFILE} ]
 then
-    cp Dockerfile.${BINARY} _tmp/Dockerfile
+    cp Dockerfile.${BINARY} ${TMP}/Dockerfile
     if [ ${BINARY} = "worker" ]; then
-        cp ./etc/worker/* _tmp/
+        cp ./etc/worker/* ${TMP}/
     fi
-    cp /etc/ssl/certs/ca-certificates.crt _tmp/ca-certificates.crt
-    docker build ${DOCKER_BUILD_FLAGS} -t pachyderm_${BINARY} _tmp
+    cp /etc/ssl/certs/ca-certificates.crt ${TMP}/ca-certificates.crt
+    docker build ${DOCKER_BUILD_FLAGS} -t pachyderm_${BINARY} ${TMP}
     docker tag pachyderm_${BINARY}:latest pachyderm/${BINARY}:latest
     docker tag pachyderm_${BINARY}:latest pachyderm/${BINARY}:local
 else
-    cd _tmp
+    cd ${TMP}
     tar cf - ${BINARY}
 fi
+rm -rf "${TMP}"
