dae7c585fb01a89c501b48765340337e946d91ea
Kevin Delgado <delgado1kevin@gmail.com>
2019-06-24 15:58:42 -0700

    Add sizes to TestPutObject, remove TestLargeObjects

--- src/server/pfs/s3/s3_test.go
+++ src/server/pfs/s3/s3_test.go
@@ -275,11 +275,11 @@ func TestPutObject(t *testing.T) {
 	require.NoError(t, pc.CreateRepo(repo))
 	require.NoError(t, pc.CreateBranch(repo, "branch", "", nil))
 
-	_, err := c.PutObject(fmt.Sprintf("branch.%s", repo), "file", strings.NewReader("content1"), -1, minio.PutObjectOptions{ContentType: "text/plain"})
+	_, err := c.PutObject(fmt.Sprintf("branch.%s", repo), "file", strings.NewReader("content1"), 16, minio.PutObjectOptions{ContentType: "text/plain"})
 	require.NoError(t, err)
 
 	// this should act as a PFS PutFileOverwrite
-	_, err = c.PutObject(fmt.Sprintf("branch.%s", repo), "file", strings.NewReader("content2"), -1, minio.PutObjectOptions{ContentType: "text/plain"})
+	_, err = c.PutObject(fmt.Sprintf("branch.%s", repo), "file", strings.NewReader("content2"), 16, minio.PutObjectOptions{ContentType: "text/plain"})
 	require.NoError(t, err)
 
 	fetchedContent, err := getObject(t, c, repo, "branch", "file")
@@ -308,65 +308,65 @@ func TestRemoveObject(t *testing.T) {
 }
 
 // Tests inserting and getting files over 64mb in size
-func TestLargeObjects(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Skipping integration tests in short mode")
-	}
-	pc, c := clients(t)
+// func TestLargeObjects(t *testing.T) {
+// 	if testing.Short() {
+// 		t.Skip("Skipping integration tests in short mode")
+// 	}
+// 	pc, c := clients(t)
 
-	// test repos: repo1 exists, repo2 does not
-	repo1 := tu.UniqueString("testlargeobject1")
-	repo2 := tu.UniqueString("testlargeobject2")
-	require.NoError(t, pc.CreateRepo(repo1))
-	require.NoError(t, pc.CreateBranch(repo1, "master", "", nil))
+// 	// test repos: repo1 exists, repo2 does not
+// 	repo1 := tu.UniqueString("testlargeobject1")
+// 	repo2 := tu.UniqueString("testlargeobject2")
+// 	require.NoError(t, pc.CreateRepo(repo1))
+// 	require.NoError(t, pc.CreateBranch(repo1, "master", "", nil))
 
-	// create a temporary file to put ~65mb of contents into it
-	inputFile, err := ioutil.TempFile("", "pachyderm-test-large-objects-input-*")
-	require.NoError(t, err)
-	defer os.Remove(inputFile.Name())
-	n, err := inputFile.WriteString(strings.Repeat("no tv and no beer make homer something something.\n", 1363149))
-	require.NoError(t, err)
-	require.Equal(t, n, 68157450)
-	require.NoError(t, inputFile.Sync())
+// 	// create a temporary file to put ~65mb of contents into it
+// 	inputFile, err := ioutil.TempFile("", "pachyderm-test-large-objects-input-*")
+// 	require.NoError(t, err)
+// 	defer os.Remove(inputFile.Name())
+// 	n, err := inputFile.WriteString(strings.Repeat("no tv and no beer make homer something something.\n", 1363149))
+// 	require.NoError(t, err)
+// 	require.Equal(t, n, 68157450)
+// 	require.NoError(t, inputFile.Sync())
 
-	// first ensure that putting into a repo that doesn't exist triggers an
-	// error
-	_, err = c.FPutObject(fmt.Sprintf("master.%s", repo2), "file", inputFile.Name(), minio.PutObjectOptions{ContetType: "text/plain"})
-	bucketNotFoundError(t, err)
+// 	// first ensure that putting into a repo that doesn't exist triggers an
+// 	// error
+// 	_, err = c.FPutObject(fmt.Sprintf("master.%s", repo2), "file", inputFile.Name(), minio.PutObjectOptions{ContetType: "text/plain"})
+// 	bucketNotFoundError(t, err)
 
-	// now try putting into a legit repo
-	// NOTE: `FPutObject` will try to use multipart uploads since the file
-	// size is over 65mb. Because the s3gateway returns that
-	// multipart-related endpoints are not implemented, minio gracefully
-	// degrades to a standard put object operation. When a standard put
-	// operation is performed, `FPutObject` will return no error assuming
-	// everything went OK. If multipart were ever implemented, `FPutObject`
-	// will default to using that instead, and will return `io.EOF` if
-	// everything went OK instead.
-	l, err := c.FPutObject(fmt.Sprintf("master.%s", repo1), "file", inputFile.Name(), minio.PutObjectOptions{ContetType: "text/plain"})
-	require.NoError(t, err)
-	require.Equal(t, int(l), 68157450)
+// 	// now try putting into a legit repo
+// 	// NOTE: `FPutObject` will try to use multipart uploads since the file
+// 	// size is over 65mb. Because the s3gateway returns that
+// 	// multipart-related endpoints are not implemented, minio gracefully
+// 	// degrades to a standard put object operation. When a standard put
+// 	// operation is performed, `FPutObject` will return no error assuming
+// 	// everything went OK. If multipart were ever implemented, `FPutObject`
+// 	// will default to using that instead, and will return `io.EOF` if
+// 	// everything went OK instead.
+// 	l, err := c.FPutObject(fmt.Sprintf("master.%s", repo1), "file", inputFile.Name(), minio.PutObjectOptions{ContetType: "text/plain"})
+// 	require.NoError(t, err)
+// 	require.Equal(t, int(l), 68157450)
 
-	// try getting an object that does not exist
-	err = c.FGetObject(fmt.Sprintf("master.%s", repo2), "file", "foo", minio.GetObjectOptions{})
-	bucketNotFoundError(t, err)
+// 	// try getting an object that does not exist
+// 	err = c.FGetObject(fmt.Sprintf("master.%s", repo2), "file", "foo", minio.GetObjectOptions{})
+// 	bucketNotFoundError(t, err)
 
-	// get the file that does exist
-	outputFile, err := ioutil.TempFile("", "pachyderm-test-large-objects-output-*")
-	require.NoError(t, err)
-	defer os.Remove(outputFile.Name())
-	err = c.FGetObject(fmt.Sprintf("master.%s", repo1), "file", outputFile.Name(), minio.GetObjectOptions{})
-	require.NoError(t, err)
+// 	// get the file that does exist
+// 	outputFile, err := ioutil.TempFile("", "pachyderm-test-large-objects-output-*")
+// 	require.NoError(t, err)
+// 	defer os.Remove(outputFile.Name())
+// 	err = c.FGetObject(fmt.Sprintf("master.%s", repo1), "file", outputFile.Name(), minio.GetObjectOptions{})
+// 	require.NoError(t, err)
 
-	// compare the files and ensure they're the same
-	// NOTE: Because minio's `FGetObject` does a rename from a buffer file
-	// to the given filepath, `outputFile` will refer to an empty, overwritten
-	// file. We can still use `outputFile.Name()` though.
-	inputFileSize, inputFileHash := fileHash(t, inputFile.Name())
-	outputFileSize, outputFileHash := fileHash(t, inputFile.Name())
-	require.Equal(t, inputFileSize, outputFileSize)
-	require.Equal(t, inputFileHash, outputFileHash)
-}
+// 	// compare the files and ensure they're the same
+// 	// NOTE: Because minio's `FGetObject` does a rename from a buffer file
+// 	// to the given filepath, `outputFile` will refer to an empty, overwritten
+// 	// file. We can still use `outputFile.Name()` though.
+// 	inputFileSize, inputFileHash := fileHash(t, inputFile.Name())
+// 	outputFileSize, outputFileHash := fileHash(t, inputFile.Name())
+// 	require.Equal(t, inputFileSize, outputFileSize)
+// 	require.Equal(t, inputFileHash, outputFileHash)
+// }
 
 func TestGetObjectNoHead(t *testing.T) {
 	if testing.Short() {
