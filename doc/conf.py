ed9dc8d15f10bb06223b32a3749018f42dd95553
Svetlana Karslioglu <svetlana.karslioglu@gmail.com>
2019-06-25 10:20:18 -0700

    Add the robots.txt file

--- doc/conf.py
+++ doc/conf.py
@@ -186,7 +186,7 @@ html_static_path = ['_static']
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
 #
-# html_extra_path = []
+html_extra_path = ['_static']
 
 # If not None, a 'Last updated on:' timestamp is inserted at every page
 # bottom, using the given strftime format.
