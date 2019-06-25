f22e56dc957ada24c7296ad52917ec30d4420785
Yusuf Simonson <simonson@gmail.com>
2019-06-25 13:54:13 -0400

    gofmt

--- src/client/pkg/config/kube_config.go
+++ src/client/pkg/config/kube_config.go
@@ -1,24 +1,24 @@
 package config
 
 import (
-    "k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 // KubeConfig gets the kubernetes config
 func KubeConfig() clientcmd.ClientConfig {
-    rules := clientcmd.NewDefaultClientConfigLoadingRules()
-    overrides := &clientcmd.ConfigOverrides{}
-    return clientcmd.NewNonInteractiveDeferredLoadingClientConfig(rules, overrides)
+	rules := clientcmd.NewDefaultClientConfigLoadingRules()
+	overrides := &clientcmd.ConfigOverrides{}
+	return clientcmd.NewNonInteractiveDeferredLoadingClientConfig(rules, overrides)
 }
 
 // KubeNamespace gets the default namespace for the active context
 func KubeNamespace(kubeConfig clientcmd.ClientConfig) (string, error) {
-    namespace, _, err := kubeConfig.Namespace()
-    if err != nil {
-        return "", err
-    }
-    if namespace == "" {
-        return "default", nil
-    }
-    return namespace, nil
+	namespace, _, err := kubeConfig.Namespace()
+	if err != nil {
+		return "", err
+	}
+	if namespace == "" {
+		return "default", nil
+	}
+	return namespace, nil
 }
