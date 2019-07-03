91fbdc333d35bbb45123d823e3cdded3a0593e0b
Yusuf Simonson <simonson@gmail.com>
2019-07-03 12:23:22 -0400

    Extracted out s3server package

--- src/server/pkg/s3server/conformance/s3-tests/s3tests/functional/AnonymousAuth.py
+++
@@ -1,5 +0,0 @@
-from boto.auth_handler import AuthHandler
-
-class AnonymousAuthHandler(AuthHandler):
-    def add_auth(self, http_request, **kwargs):
-        return # Nothing to do for anonymous access!
--- src/server/pkg/s3server/user.go
+++
@@ -1,7 +0,0 @@
-package s3server
-
-// User is an XML-encodable representation of an S3 user
-type User struct {
-	ID          string `xml:"ID"`
-	DisplayName string `xml:"DisplayName"`
-}
