40a18ea63c7ab6fcee9b9c1f23bf66b8022909ea
papposilenus <github@exnext.com>
2019-07-31 16:49:34 -0700

    example complete.

--- examples/s3gateway/tf_job_s3_gateway.py
+++ examples/s3gateway/tf_job_s3_gateway.py
@@ -7,27 +7,28 @@ import sys
 import tensorflow as tf
 from tensorflow.python.lib.io import file_io
 
-
-# this is a directory with four files in it,
-# mf, sis, boom, and bah,
+# this is a directory with two directories in it
+# containing the first chapters of Moby Dick and A Tale of Two Cities
 # that's built into the image
 input_path  = os.getenv('INPUT_PATH', "/testdata")
 s3_bucket = os.getenv('S3_BUCKET', 'master.testrepo')
 
 
 def main(_):
-    s3_path = 's3://' + args.bucket + '/'
-    print("walking {}".format(args.inputpath))
+    s3_path = 's3://' + args.bucket
+    print("walking {} for copying to {}".format(args.inputpath, s3_path))
     for dirpath, dirs, files in os.walk(args.inputpath, topdown=True):   
       for file in files:
         newpath = s3_path + dirpath + "/" +  file
         print("copying {} to {}".format(dirpath + "/" + file, newpath))
         file_io.copy(dirpath + "/" + file, newpath, True)
 
-    for dirpath, dirs, files in file_io.walk(s3_path +  args.inputpath, True):
+    targetpath = s3_path + args.inputpath
+    print("walking {} for reading files copied".format(targetpath))
+    for dirpath, dirs, files in file_io.walk(targetpath, True):
         for file in files:
-            newpath = dirpath + "/" + file
-            print("printing {} in {}  as string: >>{}<<".format(file, dirpath, file_io.read_file_to_string(newpath, False)))
+            newpath = dirpath + "/" +  file
+            print("printing {} in {} as string: >>{}<<".format(file, dirpath, file_io.read_file_to_string(newpath, False)))
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='put some files into an s3 bucket.')
@@ -42,3 +43,7 @@ if __name__ == '__main__':
   args, unparsed = parser.parse_known_args()
   tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
 
+# todo
+# create image and place in pachyderm repo
+# write README.md
+# create PR
