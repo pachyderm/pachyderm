a6336677382d6fc5f678e2212ebbc2c910e1f464
Kevin Delgado <delgado1kevin@gmail.com>
2019-08-14 14:12:33 -0700

    fix proto

--- src/client/auth/auth.pb.go
+++ src/client/auth/auth.pb.go
@@ -6,16 +6,15 @@ package auth
 import (
 	context "context"
 	fmt "fmt"
-	io "io"
-	math "math"
-	math_bits "math/bits"
-
 	_ "github.com/gogo/protobuf/gogoproto"
 	types "github.com/gogo/protobuf/types"
 	proto "github.com/golang/protobuf/proto"
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
+	io "io"
+	math "math"
+	math_bits "math/bits"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
