8a546823c1cc811a89c26ea90fe62248e4c7c793
Joe Doliner <jdoliner@gmail.com>
2019-08-23 12:45:07 -0700

    Adds fix command for extracts.

--- src/server/admin/cmds/cmds.go
+++ src/server/admin/cmds/cmds.go
@@ -2,9 +2,12 @@ package cmds
 
 import (
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/pachyderm/pachyderm/src/client"
+	"github.com/pachyderm/pachyderm/src/client/admin"
+	"github.com/pachyderm/pachyderm/src/client/pkg/pbutil"
 	"github.com/pachyderm/pachyderm/src/server/pkg/cmdutil"
 
 	"github.com/golang/snappy"
@@ -78,6 +81,65 @@ $ {{alias}} -u s3://bucket/backup`,
 	}
 	restore.Flags().StringVarP(&url, "url", "u", "", "An object storage url (i.e. s3://...) to restore from.")
 	commands = append(commands, cmdutil.CreateAlias(restore, "restore"))
+
+	view := &cobra.Command{
+		Short:   "View an extract.",
+		Long:    "View an extract.",
+		Example: "",
+		Run: cmdutil.RunFixedArgs(0, func(args []string) error {
+			reader := pbutil.NewReader(snappy.NewReader(os.Stdin))
+			op := &admin.Op{}
+			for {
+				if err := reader.Read(op); err != nil {
+					if err == io.EOF {
+						break
+					}
+					return err
+				}
+				fmt.Println(op)
+			}
+			return nil
+		}),
+	}
+	commands = append(commands, cmdutil.CreateAlias(view, "view"))
+
+	fix := &cobra.Command{
+		Short:   "Fix an extract.",
+		Long:    "Fix an extract.",
+		Example: "",
+		Run: cmdutil.RunFixedArgs(0, func(args []string) error {
+			reader := pbutil.NewReader(snappy.NewReader(os.Stdin))
+			out := pbutil.NewWriter(snappy.NewWriter(os.Stdout))
+			op := &admin.Op{}
+			pipelines := make(map[string]bool)
+			for {
+				if err := reader.Read(op); err != nil {
+					if err == io.EOF {
+						break
+					}
+					return err
+				}
+				if op.Op1_8.Pipeline != nil {
+					pipelines[op.Op1_8.Pipeline.Pipeline.Name] = true
+				}
+				if op.Op1_8.Commit != nil {
+					if pipelines[op.Op1_8.Commit.Parent.Repo.Name] {
+						continue
+					}
+				}
+				if op.Op1_8.Branch != nil {
+					if pipelines[op.Op1_8.Branch.Branch.Repo.Name] {
+						continue
+					}
+				}
+				if _, err := out.Write(op); err != nil {
+					return err
+				}
+			}
+			return nil
+		}),
+	}
+	commands = append(commands, cmdutil.CreateAlias(fix, "fix"))
 
 	inspectCluster := &cobra.Command{
 		Short: "Returns info about the pachyderm cluster",
