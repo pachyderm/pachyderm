1f1eeba4667e31212a68031762d04e56aa60f1d1
Yusuf Simonson <simonson@gmail.com>
2019-09-09 11:39:13 -0400

    Rebuilt protos

--- src/server/pkg/storage/chunk/chunk.pb.go
+++ src/server/pkg/storage/chunk/chunk.pb.go
@@ -20,7 +20,7 @@ var _ = math.Inf
 // is compatible with the proto package it is being compiled against.
 // A compilation error at this line likely means your copy of the
 // proto package needs to be updated.
-const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
+const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 
 type Chunk struct {
 	Hash                 string   `protobuf:"bytes,1,opt,name=hash,proto3" json:"hash,omitempty"`
