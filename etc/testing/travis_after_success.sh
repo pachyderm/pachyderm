085c45017a26caf4bd88fa3ebc6ee2d9b2605f4a
Yusuf Simonson <simonson@gmail.com>
2019-10-03 14:56:40 -0400

    Push tags after a successful test run

---
+++ etc/testing/travis_after_success.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ex
+
+if [[ "$TRAVIS_PULL_REQUEST" = "true" && "$TRAVIS_SECURE_ENV_VARS" == "true" ]]; then
+    if [[ -z "${DOCKER_USERNAME}" || -z "${DOCKER_PASSWORD}" ]]; then
+        echo 'the docker login-related environment variables ($DOCKER_USERNAME and $DOCKER_PASSWORD) were not found; skipping push.'
+        exit 0
+    fi
+
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+    ./etc/build/release_version
+    export VERSION="$(shell cat VERSION)"
+    docker tag pachyderm/pachd:latest pachyderm/pachd:$VERSION
+    docker push pachyderm/pachd:$VERSION
+    docker tag pachyderm/worker:latest pachyderm/worker:$VERSION
+    docker push pachyderm/worker:$VERSION
+fi
