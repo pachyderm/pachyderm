3b3367f5f06ea258896c854bb014caa4535e6f5b
Yusuf Simonson <simonson@gmail.com>
2019-10-04 16:58:22 -0400

    Removed docs about deprecated env var

--- src/server/cmd/pachctl/cmd/cmd.go
+++ src/server/cmd/pachctl/cmd/cmd.go
@@ -318,7 +318,6 @@ func PachctlCmd() *cobra.Command {
 		Long: `Access the Pachyderm API.
 
 Environment variables:
-  PACHD_ADDRESS=<host>:<port>, the pachd server to connect to (e.g. 127.0.0.1:30650).
   PACH_CONFIG=<path>, the path where pachctl will attempt to load your pach config.
   JAEGER_ENDPOINT=<host>:<port>, the Jaeger server to connect to, if PACH_TRACE is set
   PACH_TRACE={true,false}, If true, and JAEGER_ENDPOINT is set, attach a
