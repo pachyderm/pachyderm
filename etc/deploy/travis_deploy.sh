a9c8cd5fc13fa4cf4f26f33811bc956151351c5b
Yusuf Simonson <simonson@gmail.com>
2019-10-04 10:57:25 -0400

    Try using deployments instead

---
+++ etc/deploy/travis_deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ex
+
+if [[ -z "${DOCKER_USERNAME}" || -z "${DOCKER_PASSWORD}" ]]; then
+    echo 'the docker login-related environment variables ($DOCKER_USERNAME and $DOCKER_PASSWORD) were not found; skipping push.'
+    exit 0
+fi
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+./etc/build/release_version
+export VERSION="$(shell cat VERSION)"
+docker tag pachyderm/pachd:latest pachyderm/pachd:$VERSION
+docker push pachyderm/pachd:$VERSION
+docker tag pachyderm/worker:latest pachyderm/worker:$VERSION
+docker push pachyderm/worker:$VERSION
