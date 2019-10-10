be07f7ae19a19afd046fade6c43dccf8ce8a4224
Yusuf Simonson <simonson@gmail.com>
2019-10-10 09:58:15 -0400

    Killed old test

--- src/server/cmd/pachctl/cmd/cmd_test.go
+++ src/server/cmd/pachctl/cmd/cmd_test.go
@@ -30,20 +30,6 @@ func TestPortForwardError(t *testing.T) {
 	require.Matches(t, "port-forward", errMsg.String())
 }
 
-func TestNoPort(t *testing.T) {
-	cfgFile := testConfig(t, "localhost")
-	defer os.Remove(cfgFile.Name())
-	os.Setenv("PACH_CONFIG", cfgFile.Name())
-
-	c := tu.Cmd("pachctl", "version", "--timeout=1ns")
-	var errMsg bytes.Buffer
-	c.Stdout = ioutil.Discard
-	c.Stderr = &errMsg
-	err := c.Run()
-	require.YesError(t, err) // 1ns should prevent even local connections
-	require.Matches(t, "30650", errMsg.String())
-}
-
 func TestWeirdPortError(t *testing.T) {
 	cfgFile := testConfig(t, "localhost:30560")
 	defer os.Remove(cfgFile.Name())
