86635a8dfca0fc29992a07e5a3da5eecfc52bb50
Bryce McAnally <bryce@pachyderm.io>
2019-10-11 06:52:17 -0700

    Re-introduce pfs prefix in storage layer

--- src/server/pkg/storage/fileset/storage.go
+++ src/server/pkg/storage/fileset/storage.go
@@ -2,6 +2,7 @@ package fileset
 
 import (
 	"context"
+	"path"
 
 	"github.com/pachyderm/pachyderm/src/server/pkg/obj"
 	"github.com/pachyderm/pachyderm/src/server/pkg/storage/chunk"
@@ -10,6 +11,7 @@ import (
 
 const (
 	headerTag = ""
+	prefix    = "pfs"
 	// FullMergeSuffix is appended to a prefix to create a path to be used for referencing
 	// the full merge of that prefix. The full merge of a prefix means that every
 	// path with the prefix has been merged into the referenced object.
@@ -34,17 +36,20 @@ func NewStorage(objC obj.Client, chunks *chunk.Storage) *Storage {
 }
 
 // New creates a new in-memory fileset.
-func (s *Storage) New(ctx context.Context, name string, opts ...Option) *FileSet {
-	return newFileSet(ctx, s, name, opts...)
+func (s *Storage) New(ctx context.Context, fileSet string, opts ...Option) *FileSet {
+	fileSet = applyPrefix(fileSet)
+	return newFileSet(ctx, s, fileSet, opts...)
 }
 
 // NewWriter creates a new Writer.
 func (s *Storage) NewWriter(ctx context.Context, fileSet string) *Writer {
+	fileSet = applyPrefix(fileSet)
 	return newWriter(ctx, s.objC, s.chunks, fileSet)
 }
 
 // NewReader creates a new Reader for a full file set.
 func (s *Storage) NewReader(ctx context.Context, fileSet string, opts ...index.Option) *Reader {
+	fileSet = applyPrefix(fileSet)
 	return newReader(ctx, s.objC, s.chunks, fileSet, opts...)
 }
 
@@ -52,11 +57,14 @@ func (s *Storage) NewReader(ctx context.Context, fileSet string, opts ...index.O
 // (bryce) this should be extended to be more configurable (different criteria
 // for creating shards).
 func (s *Storage) Shard(ctx context.Context, fileSets []string, shardFunc ShardFunc) error {
+	fileSets = applyPrefixes(fileSets)
 	return s.merge(ctx, fileSets, shardMergeFunc(shardFunc))
 }
 
 // Merge merges the file sets with the passed in prefix.
 func (s *Storage) Merge(ctx context.Context, outputFileSet string, inputFileSets []string, opts ...index.Option) error {
+	outputFileSet = applyPrefix(outputFileSet)
+	inputFileSets = applyPrefixes(inputFileSets)
 	w := s.NewWriter(ctx, outputFileSet)
 	if err := s.merge(ctx, inputFileSets, contentMergeFunc(w), opts...); err != nil {
 		return err
@@ -79,4 +87,15 @@ func (s *Storage) merge(ctx context.Context, fileSets []string, f mergeFunc, opt
 		fileStreams = append(fileStreams, &fileStream{r: r})
 	}
 	return merge(fileStreams, f)
+}
+
+func applyPrefix(fileSet string) string {
+	return path.Join(prefix, fileSet)
+}
+
+func applyPrefixes(fileSets []string) []string {
+	for i := 0; i < len(fileSets); i++ {
+		fileSets[i] = path.Join(prefix, fileSets[i])
+	}
+	return fileSets
 }
