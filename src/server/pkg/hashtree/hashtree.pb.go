2da49adeea6804af2b95d28f5647c82e96ce4816
Bryce McAnally <bryce@pachyderm.io>
2019-10-14 09:53:09 -0700

    Build protos with no cache (test-proto-static)

--- src/server/pkg/hashtree/hashtree.pb.go
+++ src/server/pkg/hashtree/hashtree.pb.go
@@ -2149,6 +2149,7 @@ func (m *Index) Unmarshal(dAtA []byte) error {
 func skipHashtree(dAtA []byte) (n int, err error) {
 	l := len(dAtA)
 	iNdEx := 0
+	depth := 0
 	for iNdEx < l {
 		var wire uint64
 		for shift := uint(0); ; shift += 7 {
@@ -2180,10 +2181,8 @@ func skipHashtree(dAtA []byte) (n int, err error) {
 					break
 				}
 			}
-			return iNdEx, nil
 		case 1:
 			iNdEx += 8
-			return iNdEx, nil
 		case 2:
 			var length int
 			for shift := uint(0); ; shift += 7 {
@@ -2204,55 +2203,30 @@ func skipHashtree(dAtA []byte) (n int, err error) {
 				return 0, ErrInvalidLengthHashtree
 			}
 			iNdEx += length
-			if iNdEx < 0 {
-				return 0, ErrInvalidLengthHashtree
-			}
-			return iNdEx, nil
 		case 3:
-			for {
-				var innerWire uint64
-				var start int = iNdEx
-				for shift := uint(0); ; shift += 7 {
-					if shift >= 64 {
-						return 0, ErrIntOverflowHashtree
-					}
-					if iNdEx >= l {
-						return 0, io.ErrUnexpectedEOF
-					}
-					b := dAtA[iNdEx]
-					iNdEx++
-					innerWire |= (uint64(b) & 0x7F) << shift
-					if b < 0x80 {
-						break
-					}
-				}
-				innerWireType := int(innerWire & 0x7)
-				if innerWireType == 4 {
-					break
-				}
-				next, err := skipHashtree(dAtA[start:])
-				if err != nil {
-					return 0, err
-				}
-				iNdEx = start + next
-				if iNdEx < 0 {
-					return 0, ErrInvalidLengthHashtree
-				}
-			}
-			return iNdEx, nil
+			depth++
 		case 4:
-			return iNdEx, nil
+			if depth == 0 {
+				return 0, ErrUnexpectedEndOfGroupHashtree
+			}
+			depth--
 		case 5:
 			iNdEx += 4
-			return iNdEx, nil
 		default:
 			return 0, fmt.Errorf("proto: illegal wireType %d", wireType)
 		}
+		if iNdEx < 0 {
+			return 0, ErrInvalidLengthHashtree
+		}
+		if depth == 0 {
+			return iNdEx, nil
+		}
 	}
-	panic("unreachable")
+	return 0, io.ErrUnexpectedEOF
 }
 
 var (
-	ErrInvalidLengthHashtree = fmt.Errorf("proto: negative length found during unmarshaling")
-	ErrIntOverflowHashtree   = fmt.Errorf("proto: integer overflow")
+	ErrInvalidLengthHashtree        = fmt.Errorf("proto: negative length found during unmarshaling")
+	ErrIntOverflowHashtree          = fmt.Errorf("proto: integer overflow")
+	ErrUnexpectedEndOfGroupHashtree = fmt.Errorf("proto: unexpected end of group")
 )
