547581e10cca973c92973e1a76ae7a1b3cb270dc
Matthew Steffen <msteffen@pachyderm.io>
2019-10-23 09:40:00 -0700

    Make failures easier to debug in test_tls.sh

--- etc/testing/test_tls.sh
+++ etc/testing/test_tls.sh
@@ -2,6 +2,11 @@
 
 set -euo pipefail
 
+which match || {
+  here="$(dirname "${0}")"
+  go install -v "${here}/../../src/testing/match"
+}
+
 address=$(pachctl config get context `pachctl config get active-context` | jq -r .pachd_address)
 if [[ "${address}" = "null" ]]; then
   echo "pachd_address must be set on the active context"
@@ -9,14 +14,15 @@ if [[ "${address}" = "null" ]]; then
 fi
 hostport=$(echo $address | sed -e 's/grpcs:\/\///g' -e 's/grpc:\/\///g')
 
+set -x
+
 # Generate self-signed cert and private key
 etc/deploy/gen_pachd_tls.sh $hostport ""
 
 # Restart pachyderm with the given certs
 etc/deploy/restart_with_tls.sh $hostport ${PWD}/pachd.pem ${PWD}/pachd.key
 
-set +x
-# Don't log our activation code when running this script in Travis
+set +x # Do not log our activation code when running this script in Travis
 pachctl enterprise activate "$(aws s3 cp s3://pachyderm-engineering/test_enterprise_activation_code.txt -)" && echo
 set -x
 
@@ -28,7 +34,7 @@ go test -v ./src/server -run TestPipelineWithParallelism
 echo admin | pachctl auth activate
 otp="$(pachctl auth get-otp admin)"
 echo "${otp}" | pachctl auth login --one-time-password
-pachctl auth whoami | grep -q admin # will fail if pachctl can't connect
+pachctl auth whoami | match admin # will fail if pachctl can't connect
 echo yes | pachctl auth deactivate
 
 # Undeploy TLS
