871451aac9c660776a49dcc76ff03b2875b08be0
Matthew Steffen <msteffen@pachyderm.io>
2019-10-24 09:30:00 -0700

    Add -count=1 to all go tests to avoid cache

--- etc/testing/pfs_server.sh
+++ etc/testing/pfs_server.sh
@@ -18,4 +18,4 @@ fi
 
 # grep out PFS server logs, as otherwise the test output is too verbose to
 # follow and breaks travis
-go test -v ./src/server/pfs/server -timeout $TIMEOUT | grep -v "$(date +^%FT)"
+go test -v -count=1 ./src/server/pfs/server -timeout $TIMEOUT | grep -v "$(date +^%FT)"
