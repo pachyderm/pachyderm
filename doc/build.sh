18289dbba6a1472c8727be1c6f13e6c2e097b1b3
Matthew Steffen <msteffen@pachyderm.io>
2019-11-06 15:59:13 -0800

    Docs build script only builds updated versions

--- doc/build.sh
+++ doc/build.sh
@@ -1,34 +1,53 @@
 #!/bin/bash
-# Delete site/
-rm -rf site/
-# Build each version of the docs
-mkdocs build --config-file mkdocs.yml --site-dir site/latest/
-mkdocs build --config-file mkdocs-1.9.x.yml --site-dir site/1.9.x/
+
+set -ex
+
+# Rebuild all docs versions
+for d in $(ls docs); do
+    # don't rebuild archive dir
+    if [[ "${d}" == "archive" ]]; then
+        continue
+    fi
+    in_dir="./docs/${d}"
+    out_dir="./site/${d}"
+
+    # Check for mkdocs file
+    mkdocs_file="mkdocs-${d}.yml"
+    if [[ ! -f "${mkdocs_file}" ]]; then
+        echo "expected mkdocs config file \"${mkdocs_file}\" for docs version \"${d}\", but the config file wasn't found"
+        exit 1
+    fi
+
+    # Delete old built site/
+    if [[ -d  "${out_dir}" ]]; then
+      rm -r "${out_dir}"
+    fi
+
+    # rebuild site
+    mkdocs build --config-file "${mkdocs_file}" --site-dir "${out_dir}"
+done
 
 # Add each version of the docs to the dropdown defined by
 # "material/partials/versions.html"
 cat <<EOF >material/partials/versions.html
 <div class="mdl-selectfield">
-<select class="mdl-selectfield__select" id="selector" onchange="let pathParts = window.location.pathname.split('/'); pathParts[1] = this.value; window.location.pathname = pathParts.join('/')">
-              <option value="latest">latest</option>
+<select class="mdl-selectfield__select" id="version-selector" onchange="let pathParts = window.location.pathname.split('/'); pathParts[1] = this.value; window.location.pathname = pathParts.join('/')">
 EOF
 for d in $(ls docs); do
+    # don't rebuild archive dir
     if [[ "${d}" == "archive" ]]; then
         continue
     fi
-    if [[ "${d}" == "latest" ]]; then
-         continue
-    fi
     cat <<EOF >>material/partials/versions.html
-              <option value="${d}">${d}</option>
+        <option value="${d}">${d}</option>
 EOF
 done
 cat <<EOF >>material/partials/versions.html
-     </select>
-     <!-- set initial value of 'select' to the version of the docs being browsed -->
-     <script type="text/javascript">
-       var pathParts = window.location.pathname.split('/');
-       document.getElementById('selector').value = pathParts[1]
-     </script>
+    </select>
+    <!-- set initial value of 'select' to the version of the docs being browsed -->
+    <script type="text/javascript">
+      var pathParts = window.location.pathname.split('/');
+      document.getElementById('version-selector').value = pathParts[1]
+    </script>
    </div>
 EOF
