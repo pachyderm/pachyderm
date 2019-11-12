30b54b1a423b959fc5e6552fa962e36c4ed89eb9
Yusuf Simonson <simonson@gmail.com>
2019-11-12 11:12:32 -0500

    Debug

--- etc/compile/wait.sh
+++ etc/compile/wait.sh
@@ -4,9 +4,5 @@ CONTAINER="${1}"
 
 docker wait "$CONTAINER" 2>/dev/null 1>/dev/null
 RET=$?
-
-if [ "$RET" -ne 0 ]
-then
-	docker logs "$CONTAINER"
-	exit "$RET"
-fi
+docker logs "$CONTAINER"
+exit "$RET"
