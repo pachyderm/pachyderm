790c031959a1667cac6d5d06865043b0e315eac5
Yusuf Simonson <simonson@gmail.com>
2019-11-12 11:35:31 -0500

    Revert "Debug"
    This reverts commit 30b54b1a423b959fc5e6552fa962e36c4ed89eb9.

--- etc/compile/wait.sh
+++ etc/compile/wait.sh
@@ -4,5 +4,9 @@ CONTAINER="${1}"
 
 docker wait "$CONTAINER" 2>/dev/null 1>/dev/null
 RET=$?
-docker logs "$CONTAINER"
-exit "$RET"
+
+if [ "$RET" -ne 0 ]
+then
+	docker logs "$CONTAINER"
+	exit "$RET"
+fi
