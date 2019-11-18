4dffef2fdba01bca9364321ceb2cc2bfb608769e
Matthew Steffen <msteffen@pachyderm.io>
2019-11-18 10:38:36 -0800

    use serde in auth/cmds/cmds.go

--- src/server/auth/cmds/configure.go
+++ src/server/auth/cmds/configure.go
@@ -2,19 +2,18 @@ package cmds
 
 import (
 	"bytes"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 
 	"github.com/pachyderm/pachyderm/src/client"
 	"github.com/pachyderm/pachyderm/src/client/auth"
 	"github.com/pachyderm/pachyderm/src/client/pkg/grpcutil"
 	"github.com/pachyderm/pachyderm/src/server/pkg/cmdutil"
-	"gopkg.in/pachyderm/yaml.v3"
+	"github.com/pachyderm/pachyderm/src/server/pkg/serde"
 
-	"github.com/gogo/protobuf/jsonpb"
 	"github.com/spf13/cobra"
 )
 
@@ -39,22 +38,21 @@ func GetConfigCmd() *cobra.Command {
 				fmt.Println("no auth config set")
 				return nil
 			}
-			var output []byte
-			switch format {
-			case "json":
-				output, err = json.MarshalIndent(resp.Configuration, "", "  ")
-				if err != nil {
-					return fmt.Errorf("could not marshal response:\n%v\ndue to: %v", resp.Configuration, err)
-				}
-			case "yaml":
-				output, err = yaml.Marshal(resp.Configuration)
-				if err != nil {
-					return fmt.Errorf("could not marshal response:\n%v\ndue to: %v", resp.Configuration, err)
-				}
-			default:
-				return fmt.Errorf("invalid output format: %v", format)
+
+			var buf bytes.Buffer
+			if format == "" {
+				format = "json"
+			} else {
+				format = strings.ToLower(format)
 			}
-			fmt.Println(string(output))
+			e, err := serde.GetEncoder(format, &buf)
+			if err != nil {
+				return err
+			}
+			if err := e.EncodeProto(resp.Configuration); err != nil {
+				return err
+			}
+			fmt.Println(buf.String())
 			return nil
 		}),
 	}
@@ -93,26 +91,10 @@ func SetConfigCmd() *cobra.Command {
 				return errors.New("must set input file (use \"-\" to read from stdin)")
 			}
 
-			// Parse config as YAML into an unstructured document (JSON is a subset of
-			// YAML, so this will allow us to convert both JSON configs and YAML
-			// configs to JSON), and then re-parse the JSON using jsonpb, which has
-			// special support for reading timestamps and such into proto-generated
-			// structs
+			// parse config
 			var config auth.AuthConfig
-			holder := map[string]interface{}{}
-			// deserialize yaml/json into 'holder'
-			if err := yaml.Unmarshal(rawConfigBytes, &holder); err != nil {
+			if err := serde.DecodeYAML(rawConfigBytes, &config); err != nil {
 				return fmt.Errorf("could not parse config: %v", err)
-			}
-			// serialize 'holder' to json
-			jsonConfigBytes, err := json.Marshal(holder)
-			if err != nil {
-				return fmt.Errorf("serialization error while canonicalizing auth config: %v", err)
-			}
-			// parse again into an AuthConfig, with special parser
-			decoder := json.NewDecoder(bytes.NewReader(jsonConfigBytes))
-			if err := jsonpb.UnmarshalNext(decoder, &config); err != nil {
-				return fmt.Errorf("parse error while canonicalizing auth config: %v", err)
 			}
 			// TODO(msteffen): try to handle empty config?
 			_, err = c.SetConfiguration(c.Ctx(), &auth.SetConfigurationRequest{
