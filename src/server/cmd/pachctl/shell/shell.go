74a02b58023e6641a4b9571b3d421937fe5779c7
Joe Doliner <jdoliner@gmail.com>
2019-12-04 17:12:10 -0800

    Slightly smarter completion.

--- src/server/cmd/pachctl/shell/shell.go
+++ src/server/cmd/pachctl/shell/shell.go
@@ -1,14 +1,24 @@
 package shell
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 	"strings"
 
 	prompt "github.com/c-bata/go-prompt"
+	"github.com/spf13/cobra"
 )
 
-func executor(in string) {
+type shell struct {
+	rootCmd *cobra.Command
+}
+
+func newShell(rootCmd *cobra.Command) *shell {
+	return &shell{rootCmd: rootCmd}
+}
+
+func (s *shell) executor(in string) {
 	cmd := exec.Command("bash")
 	cmd.Stdin = strings.NewReader("pachctl " + in)
 	cmd.Stdout = os.Stdout
@@ -17,15 +27,39 @@ func executor(in string) {
 	return
 }
 
-func suggestor(in prompt.Document) []prompt.Suggest {
-	return []prompt.Suggest{}
+func (s *shell) suggestor(in prompt.Document) []prompt.Suggest {
+	args := strings.Fields(in.Text)
+	cmd := s.rootCmd
+	text := ""
+	if len(args) > 0 {
+		var err error
+		cmd, _, err = cmd.Traverse(args[:len(args)-1])
+		if err != nil {
+			return []prompt.Suggest{{
+				Text: fmt.Sprintf("Error: %v", err.Error()),
+			}}
+		}
+		text = args[len(args)-1]
+	}
+	suggestions := cmd.SuggestionsFor(text)
+	var result []prompt.Suggest
+	for _, suggestion := range suggestions {
+		result = append(result, prompt.Suggest{
+			Text: suggestion,
+		})
+	}
+	return result
+}
+
+func (s *shell) run() {
+	prompt.New(
+		s.executor,
+		s.suggestor,
+		prompt.OptionPrefix(">>> "),
+	).Run()
 }
 
 // Run runs a prompt, it does not return.
-func Run() {
-	prompt.New(
-		executor,
-		suggestor,
-		prompt.OptionPrefix(">>> "),
-	).Run()
+func Run(rootCmd *cobra.Command) {
+	newShell(rootCmd).run()
 }
