90229007a19e9c066ed2da34d0c9ae3da3afefbc
Matthew Steffen <msteffen@pachyderm.io>
2019-12-11 12:37:04 -0800

    Make docker-build no longer builds as root

--- etc/compile/compile.sh
+++ etc/compile/compile.sh
@@ -1,47 +1,23 @@
 #!/bin/sh
+#
+# This is the first step in building Pachyderm binaries ('pachd' and 'worker')
+# and is called by the 'make docker-build-pachd' and 'make docker-build-worker'
+# make targets. It doesn't actually run the go compiler (that's in
+# run_go_cmds.sh, called at the bottom) but it sets up an unprivileged user
+# with the host caller's user ID, under which the go compiler will run
 
 set -Eex
 
-DIR="$(cd "$(dirname "${0}")/../.." && pwd)"
-cd "${DIR}"
-pwd
+useradd --uid="${CALLING_USER_ID}" caller
+groupadd  --gid="${DOCKER_GROUP_ID}" docker
+# Hack: add caller (which we want to have all privileges inside the container,
+# but also have the calling user's ID) to the 'root' group, giving it access to
+# /root/.cache so that 'go build' works
+usermod --append --groups=docker,root caller
+usermod --home=/root caller
+chmod g+rwx /root
 
-BINARY="${1}"
-LD_FLAGS="${2}"
-PROFILE="${3}"
-
-mkdir -p _tmp
-# Note that github.com/pachyderm/pachyderm is mounted into the
-# {pachd,worker}_compile docker container that this script is running in, so
-# 'mkdir' below actually creates a dir on the host machine. The dir name
-# includes ${BINARY} so that it doesn't collide with any concurrently-running
-# pachyderm builds (e.g. when we build pachd and worker concurrently in 'make
-# docker-build'). See https://github.com/pachyderm/pachyderm/issues/3845
-TMP=docker_build_${BINARY}.tmpdir
-mkdir -p "${TMP}"
-CGO_ENABLED=0 GOOS=linux go build \
-  -installsuffix netgo \
-  -tags netgo \
-  -o ${TMP}/${BINARY} \
-  -ldflags "${LD_FLAGS}" \
-  -gcflags "all=-trimpath=$GOPATH" \
-  src/server/cmd/${BINARY}/main.go
-
-echo "LD_FLAGS=$LD_FLAGS"
-
-# When creating profile binaries, we dont want to detach or do docker ops
-if [ -z ${PROFILE} ]
-then
-    cp Dockerfile.${BINARY} ${TMP}/Dockerfile
-    if [ ${BINARY} = "worker" ]; then
-        cp ./etc/worker/* ${TMP}/
-    fi
-    cp /etc/ssl/certs/ca-certificates.crt ${TMP}/ca-certificates.crt
-    docker build ${DOCKER_BUILD_FLAGS} -t pachyderm_${BINARY} ${TMP}
-    docker tag pachyderm_${BINARY}:latest pachyderm/${BINARY}:latest
-    docker tag pachyderm_${BINARY}:latest pachyderm/${BINARY}:local
-else
-    cd ${TMP}
-    tar cf - ${BINARY}
-fi
-rm -rf "${TMP}"
+# Run "go build" as "caller", to avoid littering host machine's $GOPATH with
+# root-owned files
+export ROOT_PATH="${PATH}"
+su caller -- "$(dirname "${0}")/run_go_cmds.sh" "${@}"
---
+++ etc/compile/run_go_cmds.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+#
+# This file actually builds Pachyderm binaries ('pachd' and 'worker') and is
+# called by etc/compile/compile.sh inside the Pachyderm build container, but it
+# run as an unprivileged user with the host caller's user ID, instead of as
+# root. This prevents 'go build' from littering the pachyderm git directory
+# with root-owned files
+
+set -Eex
+
+export PATH="${ROOT_PATH}"
+env
+whoami
+id -u ${USER}
+echo $HOME
+
+DIR="$(cd "$(dirname "${0}")/../.." && pwd)"
+cd "${DIR}"
+pwd
+
+BINARY="${1}"
+LD_FLAGS="${2}"
+PROFILE="${3}"
+
+mkdir -p _tmp
+# Note that github.com/pachyderm/pachyderm is mounted into the
+# {pachd,worker}_compile docker container that this script is running in, so
+# 'mkdir' below actually creates a dir on the host machine. The dir name
+# includes ${BINARY} so that it doesn't collide with any concurrently-running
+# pachyderm builds (e.g. when we build pachd and worker concurrently in 'make
+# docker-build'). See https://github.com/pachyderm/pachyderm/issues/3845
+TMP=docker_build_${BINARY}.tmpdir
+mkdir -p "${TMP}"
+CGO_ENABLED=0 GOOS=linux go build \
+  -installsuffix netgo \
+  -tags netgo \
+  -o ${TMP}/${BINARY} \
+  -ldflags "${LD_FLAGS}" \
+  -gcflags "all=-trimpath=$GOPATH" \
+  src/server/cmd/${BINARY}/main.go
+
+echo "LD_FLAGS=$LD_FLAGS"
+
+# When creating profile binaries, we dont want to detach or do docker ops
+if [ -z ${PROFILE} ]
+then
+    cp Dockerfile.${BINARY} ${TMP}/Dockerfile
+    if [ ${BINARY} = "worker" ]; then
+        cp ./etc/worker/* ${TMP}/
+    fi
+    cp /etc/ssl/certs/ca-certificates.crt ${TMP}/ca-certificates.crt
+    docker build ${DOCKER_BUILD_FLAGS} -t pachyderm_${BINARY} ${TMP}
+    docker tag pachyderm_${BINARY}:latest pachyderm/${BINARY}:latest
+    docker tag pachyderm_${BINARY}:latest pachyderm/${BINARY}:local
+else
+    cd ${TMP}
+    tar cf - ${BINARY}
+fi
+rm -rf "${TMP}"
