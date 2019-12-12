1840b1664ca2c6b7bf93d5b76d59144d18cda522
grey <grey@pachyderm.io>
2019-12-12 12:38:15 -0800

    update invocation of pfs server tests

--- etc/testing/pfs_server.sh
+++ etc/testing/pfs_server.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
 
-export ETCD_IMAGE=$1
-export TIMEOUT=$2
-
-# If etcd is not available, start it in a docker container
-if ! ETCDCTL_API=3 etcdctl --endpoints=127.0.0.1:32379 get "testkey"; then
-    docker run \
-        --rm \
-        --publish 32379:2379 \
-        --ulimit nofile=2048 \
-        $ETCD_IMAGE \
-        etcd \
-        --listen-client-urls=http://0.0.0.0:2379 \
-        --advertise-client-urls=http://0.0.0.0:2379 &
-fi
+export TIMEOUT=$1
 
 # grep out PFS server logs, as otherwise the test output is too verbose to
 # follow and breaks travis
-go test -v -count=1 ./src/server/pfs/server -timeout $TIMEOUT | grep -v "$(date +^%FT)"
+go test -v -count=1 ./src/server/pfs/server/testing -timeout $TIMEOUT | grep -v "$(date +^%FT)"
