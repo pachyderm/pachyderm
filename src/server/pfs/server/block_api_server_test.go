c523b3a9eeba800cc67565be2efca34dcf892cb3
grey <grey@pachyderm.io>
2019-12-12 12:25:19 -0800

    updating block server tests

--- src/server/pfs/server/block_api_server_test.go
+++ src/server/pfs/server/block_api_server_test.go
@@ -9,13 +9,12 @@ import (
 
 	"github.com/pachyderm/pachyderm/src/client/pkg/require"
 	"github.com/pachyderm/pachyderm/src/server/pkg/workload"
+	tu "github.com/pachyderm/pachyderm/src/server/pkg/testutil"
 )
 
 func TestPutGet(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Skipping integration tests in short mode")
-	}
-	c := GetPachClient(t, GetBasicConfig())
+	t.Parallel()
+	err := tu.WithRealEnv(func(env *tu.RealEnv) error {
 	object, _, err := c.PutObject(strings.NewReader("foo"))
 	require.NoError(t, err)
 	value, err := c.ReadObject(object.Hash)
@@ -24,13 +23,15 @@ func TestPutGet(t *testing.T) {
 	objectInfo, err := c.InspectObject(object.Hash)
 	require.NoError(t, err)
 	require.Equal(t, uint64(3), objectInfo.BlockRef.Range.Upper-objectInfo.BlockRef.Range.Lower)
+
+		return nil
+	})
+	require.NoError(t, err)
 }
 
 func TestTags(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Skipping integration tests in short mode")
-	}
-	c := GetPachClient(t, GetBasicConfig())
+	t.Parallel()
+	err := tu.WithRealEnv(func(env *tu.RealEnv) error {
 	object, _, err := c.PutObject(strings.NewReader("foo"), "bar", "fizz")
 	require.NoError(t, err)
 	require.NoError(t, c.TagObject(object.Hash, "buzz"))
@@ -48,13 +49,15 @@ func TestTags(t *testing.T) {
 	value, err = c.ReadTag("quux")
 	require.NoError(t, err)
 	require.Equal(t, []byte("foo"), value)
+
+		return nil
+	})
+	require.NoError(t, err)
 }
 
 func TestShortTag(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Skipping integration tests in short mode")
-	}
-	c := GetPachClient(t, GetBasicConfig())
+	t.Parallel()
+	err := tu.WithRealEnv(func(env *tu.RealEnv) error {
 	object, _, err := c.PutObject(strings.NewReader("content"), "t")
 	require.NoError(t, err)
 
@@ -64,13 +67,15 @@ func TestShortTag(t *testing.T) {
 	value, err = c.ReadTag("t")
 	require.NoError(t, err)
 	require.Equal(t, []byte("content"), value)
+
+		return nil
+	})
+	require.NoError(t, err)
 }
 
 func TestManyObjects(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Skipping integration tests in short mode")
-	}
-	c := GetPachClient(t, GetBasicConfig())
+	t.Parallel()
+	err := tu.WithRealEnv(func(env *tu.RealEnv) error {
 	var objects []string
 	for i := 0; i < 25; i++ {
 		object, _, err := c.PutObject(strings.NewReader(string(i)), fmt.Sprint(i))
@@ -85,13 +90,15 @@ func TestManyObjects(t *testing.T) {
 		require.NoError(t, err)
 		require.Equal(t, []byte(string(i)), value)
 	}
+
+		return nil
+	})
+	require.NoError(t, err)
 }
 
 func TestBigObject(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Skipping integration tests in short mode")
-	}
-	c := GetPachClient(t, GetBasicConfig())
+	t.Parallel()
+	err := tu.WithRealEnv(func(env *tu.RealEnv) error {
 	r := workload.NewReader(rand.New(rand.NewSource(time.Now().UnixNano())), 50*1024*1024)
 	object, _, err := c.PutObject(r)
 	require.NoError(t, err)
@@ -101,13 +108,15 @@ func TestBigObject(t *testing.T) {
 	value, err = c.ReadObject(object.Hash)
 	require.NoError(t, err)
 	require.Equal(t, 50*1024*1024, len(value))
+
+		return nil
+	})
+	require.NoError(t, err)
 }
 
 func TestReadObjects(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Skipping integration tests in short mode")
-	}
-	c := GetPachClient(t, GetBasicConfig())
+	t.Parallel()
+	err := tu.WithRealEnv(func(env *tu.RealEnv) error {
 	fooObject, _, err := c.PutObject(strings.NewReader("foo"))
 	require.NoError(t, err)
 	barObject, _, err := c.PutObject(strings.NewReader("bar"))
@@ -127,4 +136,8 @@ func TestReadObjects(t *testing.T) {
 	value, err = c.ReadObjects([]string{fooObject.Hash, barObject.Hash}, 4, 0)
 	require.NoError(t, err)
 	require.Equal(t, []byte("ar"), value)
+
+		return nil
+	})
+	require.NoError(t, err)
 }
