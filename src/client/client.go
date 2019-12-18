c813ae47a35d94fbc0294195ab07ce14405f201d
Yusuf Simonson <simonson@gmail.com>
2019-12-18 16:49:36 -0500

    Fixed cluster ID verification for the case where we're communicating with an older pachd instance

--- src/client/client.go
+++ src/client/client.go
@@ -460,16 +460,18 @@ func NewOnUserMachine(prefix string, options ...Option) (*APIClient, error) {
 
 	// Verify cluster ID
 	clusterID, err := client.ClusterID()
-	if err != nil {
+	if err != nil && (context.ClusterID != "" || !strings.Contains(err.Error(), "unknown method")) {
 		return nil, fmt.Errorf("could not get cluster ID: %v", err)
 	}
-	if context.ClusterID == "" {
-		context.ClusterID = clusterID
-		if err = cfg.Write(); err != nil {
-			return nil, fmt.Errorf("could not write config to save cluster ID: %v", err)
+	if context.ClusterID != clusterID {
+		if context.ClusterID == "" {
+			context.ClusterID = clusterID
+			if err = cfg.Write(); err != nil {
+				return nil, fmt.Errorf("could not write config to save cluster ID: %v", err)
+			}
+		} else {
+			return nil, fmt.Errorf("connected to the wrong cluster (context cluster ID = %q vs reported cluster ID = %q)", context.ClusterID, clusterID)
 		}
-	} else if context.ClusterID != clusterID {
-		return nil, fmt.Errorf("connected to the wrong cluster (context cluster ID = %q vs reported cluster ID = %q)", context.ClusterID, clusterID)
 	}
 
 	// Add port forwarding. This will set it to nil if port forwarding is
