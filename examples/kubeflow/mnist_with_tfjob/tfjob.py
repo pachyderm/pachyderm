1903de47fc440d02c842a7ac5c16db52e18db4ed
Yusuf Simonson <simonson@gmail.com>
2020-01-09 14:32:49 -0500

    Fixed mnist script

--- examples/kubeflow/mnist_with_tfjob/tfjob.py
+++ examples/kubeflow/mnist_with_tfjob/tfjob.py
@@ -7,74 +7,70 @@ from tensorflow import keras
 import time
 import numpy as np
 import zipfile
+import tempfile
 import python_pachyderm
 from tensorflow.python.lib.io import file_io
 from tensorflow.python.keras.utils.data_utils import get_file
 
-# this is the Pachyderm repo & branch we'll copy files from
-input_bucket = os.getenv('INPUT_BUCKET', 'master.inputrepo')
-# this is the Pachyderm repo & branch  we'll copy the files to
-output_bucket = os.getenv('OUTPUT_BUCKET', "master.outputrepo")
-# this is local directory we'll copy the files to
-data_dir = os.getenv('DATA_DIR', "/tmp/data")
-# this is the training data file in the input repo
-training_data = os.getenv('TRAINING_DATA', "mninst.npz")
-# this is the name of model file in the output repo
-model_file = os.getenv('MODEL_FILE', "my_model.h5")
-
-client = python_pachyderm.PfsClient(host=os.getenv('PACHD_SERVICE_HOST'), port=os.getenv('PACHD_SERVICE_PORT'))
-
 def main(_):
-  input_url = "s3://{}/".format(input_bucket)
-  output_url = "s3://{}/".format(output_bucket)
-  
-  os.makedirs(data_dir)
+  # this is the Pachyderm repo we'll copy files from
+  input_repo = os.getenv('INPUT_REPO', "inputrepo")
+  # this is the Pachyderm branch we'll copy files from
+  input_branch = os.getenv('INPUT_BRANCH', "master")
+  # this is the Pachyderm repo we'll copy the files to
+  output_repo = os.getenv('OUTPUT_REPO', "outputrepo")
+  # this is the Pachyderm branch we'll copy the files to
+  output_branch = os.getenv('OUTPUT_BRANCH', "master")
 
-  # first, we copy files from pachyderm into a convenient
-  # local directory for processing.
-  input_uri = os.path.join(input_url, training_data)
-  training_data_path = os.path.join(data_dir, training_data)
-  print("copying {} to {}".format(input_uri, training_data_path))
-  file_io.copy(input_uri, training_data_path, True)
-  
-  (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data(path=training_data_path)
-  train_labels = train_labels[:1000]
-  test_labels = test_labels[:1000]
+  client = python_pachyderm.Client.new_in_cluster()
 
-  train_images = train_images[:1000].reshape(-1, 28 * 28) / 255.0
-  test_images = test_images[:1000].reshape(-1, 28 * 28) / 255.0
+  input_url = "s3://{}.{}/".format(input_branch, input_repo)
+  output_url = "s3://{}.{}/".format(output_branch, output_repo)
 
-  # Returns a short sequential model
-  def create_model():
-    model = tf.keras.models.Sequential([
-      keras.layers.Dense(512, activation=tf.keras.activations.relu, input_shape=(784,)),
-      keras.layers.Dropout(0.2),
-      keras.layers.Dense(10, activation=tf.keras.activations.softmax)
-    ])
+  with tempfile.TemporaryDirectory(suffix="pachyderm-mnist-with-tfjob") as data_dir:
+    # first, we copy files from pachyderm into a convenient
+    # local directory for processing.
+    input_uri = os.path.join(input_url, "mnist.npz")
+    training_data_path = os.path.join(data_dir, "mnist.npz")
+    print("copying {} to {}".format(input_uri, training_data_path))
+    file_io.copy(input_uri, training_data_path, True)
+    
+    (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data(path=training_data_path)
+    train_labels = train_labels[:1000]
+    test_labels = test_labels[:1000]
 
-    model.compile(
-      optimizer=tf.keras.optimizers.Adam(),
-      loss=tf.keras.losses.sparse_categorical_crossentropy,
-      metrics=['accuracy']
-    )
+    train_images = train_images[:1000].reshape(-1, 28 * 28) / 255.0
+    test_images = test_images[:1000].reshape(-1, 28 * 28) / 255.0
 
-    return model
+    # Returns a short sequential model
+    def create_model():
+      model = tf.keras.models.Sequential([
+        keras.layers.Dense(512, activation=tf.keras.activations.relu, input_shape=(784,)),
+        keras.layers.Dropout(0.2),
+        keras.layers.Dense(10, activation=tf.keras.activations.softmax)
+      ])
 
-  # Create a basic model instance
-  model = create_model()
-  model.summary()
+      model.compile(
+        optimizer=tf.keras.optimizers.Adam(),
+        loss=tf.keras.losses.sparse_categorical_crossentropy,
+        metrics=['accuracy']
+      )
 
-  model.fit(train_images, train_labels, batch_size=32, epochs=5, validation_data=(test_images, test_labels))
+      return model
 
-  # Save entire model to a HDF5 file
-  model_file = os.path.join(data_dir, model_file)
-  model.save(model_file)
-  # Copy file over to Pachyderm
-  output_uri = os.path.join(output_url, model_file)
-  print("copying {} to {}".format(model_file, output_uri))
-  file_io.copy(model_file, output_uri, True)
+    # Create a basic model instance
+    model = create_model()
+    model.summary()
 
-  client.finish_commit(('output', 'master'))
+    model.fit(train_images, train_labels, batch_size=32, epochs=5, validation_data=(test_images, test_labels))
+
+    # Save entire model to a HDF5 file
+    model_path = os.path.join(data_dir, "my_model.h5")
+    model.save(model_path)
+    # Copy file over to Pachyderm
+    output_uri = os.path.join(output_url, model_path)
+    print("copying {} to {}".format(model_path, output_uri))
+    file_io.copy(model_path, output_uri, True)
 
 if __name__ == '__main__':
   tf.compat.v1.app.run(main=main, argv=[sys.argv[0]])
