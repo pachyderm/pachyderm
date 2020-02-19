3f6425f0432401511d6b600b86123d5aec2ac73b
grey <grey@pachyderm.io>
2020-02-18 21:23:41 -0800

    checkpoint

--- src/server/worker/pipeline/transform/chain/chain.go
+++ src/server/worker/pipeline/transform/chain/chain.go
@@ -1,165 +1,312 @@
+package chain
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"sync"
+
+	"github.com/pachyderm/pachyderm/src/server/worker/common"
+	"github.com/pachyderm/pachyderm/src/server/worker/datum"
+)
 
 // Interface - put job into black box
 // only for jobs in the running state
 // black box returns datum.Iterator of datums to be processed as they are safe to be processed
 // Notify black box when a job succeeds or fails so it can propagate datums to downstream jobs
 
+type DatumHasher interface {
+	Hash([]*common.Input) string
+}
+
 type JobData interface {
 	Iterator() (datum.Iterator, error)
 }
 
 type JobDatumIterator interface {
-	func Next() bool
-	func Datum() []*common.Input
-	func NumAvailable() int
+	Next(context.Context) (bool, error)
+	Datum() []*common.Input
+	NumAvailable() int
+	AdditiveOnly() bool
+	DatumSet() DatumSet
 }
 
-type RunningJobs interface {
-	func Start(JobData) (JobDatumIterator, error)
-	func Succeed(JobData, recoveredDatums DatumSet) error
-	func Fail(JobData) error
+type JobChain interface {
+	Initialized() bool
+	Initialize(baseDatums DatumSet) error
+
+	Start(jd JobData) (JobDatumIterator, error)
+	Succeed(jd JobData, recoveredDatums DatumSet) error
+	Fail(jd JobData) error
 }
 
-type DatumSet map[string]struct{}{}
+type DatumSet map[string]struct{}
 
 type jobDatumIterator struct {
 	data JobData
-	rj *runningJobs
+	jc   *jobChain
 
-	unyielded DatumSet // Datums that are waiting on an ancestor job
-	yielding DatumSet // Datums that may be yielded as the iterator progresses
-	yielded DatumSet // Datums that have been yielded
+	// TODO: lower memory consumption - all these datumsets might result in a
+	// really large memory footprint. See if we can do a streaming interface to
+	// replace these - will likely require the new storage layer, as additive-only
+	// jobs need this stuff the most.
+	unyielded       DatumSet // Datums that are waiting on an ancestor job
+	yielding        DatumSet // Datums that may be yielded as the iterator progresses
+	yielded         DatumSet // Datums that have been yielded
+	allDatums       DatumSet // All datum hashes from the datum iterator
+	recoveredDatums DatumSet // Recovered datums from a completed job
 
 	ancestors []*jobDatumIterator
-	dit datum.Iterator
+	dit       datum.Iterator
 
-	parent *jobDatumIterator // If set, this job is additive-only and must wait on its parent to finish before iteration can complete
-
+	finished     bool
+	additiveOnly bool
+	// TODO: have a 'doneProcessing' (for additive-subtractive descendents) and 'doneMerging' (for additive-only decendents)
 	done chan struct{}
 }
 
-type runningJobs struct {
-	mutex sync.Mutex
-	jobs []*jobDatumIterator
-	datumsBase DatumSet
+type jobChain struct {
+	mutex      sync.Mutex
+	hasher     DatumHasher
+	jobs       []*jobDatumIterator
+	baseDatums DatumSet
 }
 
-func NewRunningJobs(driver driver.Driver) (RunningJobs, error) {
-	datumsBase := make(
-	// TODO: init datums base
-
-	return &runningJobs{
-		jobs: []*jobDatumIterator{},	
-		datumsBase: datumsBase,
+func NewJobChain(hasher DatumHasher) (JobChain, error) {
+	return &jobChain{
+		hasher:     hasher,
+		jobs:       []*jobDatumIterator{},
+		baseDatums: nil,
 	}, nil
 }
 
+func (jc *jobChain) Initialized() bool {
+	return jc.baseDatums != nil
+}
 
-func (rj *runningJobs) Start(jd JobData) (JobDatumIterator, error) {
+func (jc *jobChain) Initialize(baseDatums DatumSet) error {
+	if jc.Initialized() {
+		return fmt.Errorf("cannot reinitialize JobChain")
+	}
+	jc.baseDatums = baseDatums
+	return nil
+}
+
+func (jc *jobChain) Start(jd JobData) (JobDatumIterator, error) {
+	if !jc.Initialized() {
+		return nil, fmt.Errorf("JobChain is not initialized")
+	}
+
 	dit, err := jd.Iterator()
 	if err != nil {
 		return nil, err
 	}
 
-	jdi := &jobDatumIterator {
-		data: jd,
-		rj: rj,
+	jdi := &jobDatumIterator{
+		data:      jd,
+		jc:        jc,
 		unyielded: make(DatumSet),
-		yielding: make(DatumSet),
-		yielded: make(DatumSet),
-		dit: dit,
+		yielding:  make(DatumSet),
+		yielded:   make(DatumSet),
+		allDatums: make(DatumSet),
+		ancestors: []*jobDatumIterator{},
+		dit:       dit,
+		done:      make(chan struct{}),
 	}
 
-	rj.mutex.Lock()
-	defer rj.mutex.Unlock()
+	jc.mutex.Lock()
+	defer jc.mutex.Unlock()
 
 	dit.Reset()
+	ancestors := map[*jobDatumIterator]struct{}{}
 	for i := 0; i < dit.Len(); i++ {
 		inputs := jdi.dit.DatumN(i)
-		datumHash := rj.hash(inputs)
+		hash := jc.hasher.Hash(inputs)
+		jdi.allDatums[hash] = struct{}{}
+
+		safeToProcess := true
+		// ancestors should be _all_ previous jobs which have _any_ datum overlap with this job
+		for _, ancestor := range jc.jobs {
+			if !ancestor.finished {
+				if _, ok := ancestor.allDatums[hash]; ok {
+					ancestors[ancestor] = struct{}{}
+					safeToProcess = false
+				}
+			}
+		}
+
+		if safeToProcess {
+			jdi.yielding[hash] = struct{}{}
+		} else {
+			jdi.unyielded[hash] = struct{}{}
+		}
 	}
 
-	return jdi, nil
-}
-
-func (rj *runningJobs) hash(inputs []*common.Input) string {
-	return common.HashDatum(rj.driver.PipelineInfo().Pipeline.Name, rj.driver.PipelineInfo().Salt, inputs)
-}
-
-func (rj *runningJobs) Fail(jd JobData) error {
-	rj.mutex.Lock()
-	defer rj.mutex.Unlock()
-
-	index, err := rj.indexOf(jd)
-	if err != nil {
-		return err
+	// If this job is additive-only from the parent job, we should mark it now - loop over parent datums to see if they are all present
+	parentDatums := jc.baseDatums
+	if len(jc.jobs) > 0 {
+		parentDatums = jc.jobs[len(jc.jobs)-1].allDatums
 	}
-
-
-}
-
-func (rj *runningJobs) Succeed(jd JobData, recoveredDatums DatumSet) (DatumSet, error) {
-	rj.mutex.Lock()
-	defer rj.mutex.Unlock()
-
-	index, err := rj.indexOf(jd)
-	if err != nil {
-		return err
-	}
-
-	jdi := rj.jobs[index]
-
-	if len(jdi.yielding) != 0 || len(jdi.unyielded) != 0 {
-		return fmt.Errorf(
-			"cannot advance a job with remaining datums: %d + %d of %d",
-			len(unyielded), len(yielding), len(unyielded) + len(yielding) + len(yielded),
-		)
-	}
-
-	newBaseDatums := make(DatumSet)
-	// new base DatumSet will be yielded - recovered (if not additive-only), or base + yielded - recovered (if additive-only)
-	for hash := range jdi.yielded {
-		if _, ok := recoveredDatums[hash]; !ok {
-			newBaseDatums[hash] = struct{}{}
+	jdi.additiveOnly = true
+	for hash := range parentDatums {
+		if _, ok := jdi.allDatums[hash]; !ok {
+			jdi.additiveOnly = false
+			break
 		}
 	}
 
 	if jdi.additiveOnly {
-		// Need to merge with ancestor's base datum set
+		// If this is additive-only, we only need to enqueue new datums (since the parent job)
+		for hash := range jdi.allDatums {
+			if _, ok := parentDatums[hash]; ok {
+				delete(jdi.yielding, hash)
+				delete(jdi.unyielded, hash)
+			}
+		}
 	}
 
-	return newBaseDatums, nil
+	for ancestor := range ancestors {
+		jdi.ancestors = append(jdi.ancestors, ancestor)
+	}
+
+	jc.jobs = append(jc.jobs, jdi)
+	return jdi, nil
 }
 
-func (jdi *jobDatumIterator) Next() bool {
+func (jc *jobChain) indexOf(jd JobData) (int, error) {
+	for i, x := range jc.jobs {
+		if x.data == jd {
+			return i, nil
+		}
+	}
+	return 0, fmt.Errorf("job not found in job chain")
+}
+
+func (jc *jobChain) cleanFinishedJobs() {
+	var newBaseDatums DatumSet
+	index := -1
+	for i, job := range jc.jobs {
+		if !job.finished {
+			break
+		}
+		index = i
+		if job.allDatums != nil {
+			newBaseDatums = job.allDatums
+		}
+	}
+
+	jc.jobs = jc.jobs[index+1:]
+	jc.baseDatums = newBaseDatums
+}
+
+func (jc *jobChain) Fail(jd JobData) error {
+	jc.mutex.Lock()
+	defer jc.mutex.Unlock()
+
+	index, err := jc.indexOf(jd)
+	if err != nil {
+		return err
+	}
+
+	jdi := jc.jobs[index]
+	jdi.allDatums = nil
+	jdi.finished = true
+
+	jc.cleanFinishedJobs()
+
+	close(jdi.done)
+
+	return nil
+}
+
+func (jc *jobChain) Succeed(jd JobData, recoveredDatums DatumSet) error {
+	jc.mutex.Lock()
+	defer jc.mutex.Unlock()
+
+	index, err := jc.indexOf(jd)
+	if err != nil {
+		return err
+	}
+
+	jdi := jc.jobs[index]
+
+	if len(jdi.yielding) != 0 || len(jdi.unyielded) != 0 {
+		return fmt.Errorf(
+			"cannot succeed a job with remaining datums: %d + %d of %d",
+			len(jdi.unyielded), len(jdi.yielding), len(jdi.unyielded)+len(jdi.yielding)+len(jdi.yielded),
+		)
+	}
+
+	for hash := range recoveredDatums {
+		delete(jdi.allDatums, hash)
+	}
+
+	jdi.recoveredDatums = recoveredDatums
+	jdi.finished = true
+
+	if index == 0 {
+		jc.jobs = jc.jobs[1:]
+		jc.baseDatums = jdi.allDatums
+	}
+
+	jc.cleanFinishedJobs()
+
+	close(jdi.done)
+
+	return nil
+}
+
+func safeToProcess(hash string, ancestors []*jobDatumIterator) bool {
+	for _, ancestor := range ancestors {
+		if _, ok := ancestor.allDatums[hash]; ok {
+			return false
+		}
+	}
+	return true
+}
+
+func (jdi *jobDatumIterator) Next(ctx context.Context) (bool, error) {
 	for {
 		for len(jdi.yielding) == 0 {
 			if len(jdi.unyielded) == 0 || len(jdi.ancestors) == 0 {
-				return false
+				return false, nil
 			}
 
 			// Wait on an ancestor job
-			cases := make([]reflect.SelectCase, len(jdi.ancestors))
+			cases := make([]reflect.SelectCase, 0, len(jdi.ancestors)+1)
 			for _, x := range jdi.ancestors {
 				cases = append(cases, reflect.SelectCase{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(x.done)})
 			}
+			cases = append(cases, reflect.SelectCase{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(ctx.Done())})
 
+			// Wait for an ancestor job to finish, then remove it from our dependencies
 			index, _, _ := reflect.Select(cases)
-			finishedAncestor := jdi.ancestors[index]
+			if index == len(cases)-1 {
+				return false, ctx.Err()
+			}
+			// ancestor = jdi.ancestors[index]
 			jdi.ancestors = append(jdi.ancestors[:index], jdi.ancestors[index+1:]...)
 
+			// TODO: special handling if jdi is additive-only
+
 			// TODO: update 'yielding' from 'unyielded'
+			for hash := range jdi.unyielded {
+				if safeToProcess(hash, jdi.ancestors) {
+					delete(jdi.unyielded, hash)
+					jdi.yielding[hash] = struct{}{}
+				}
+			}
 
 			jdi.dit.Reset()
 		}
 
 		for jdi.dit.Next() {
 			inputs := jdi.dit.Datum()
-			datumHash := jdi.rj.hash(inputs)
-			if _, ok := jdi.yielding[datumHash]; ok {
-				delete(jdi.yielding[datumHash])
-				jdi.yielded[datumHash] = struct{}{}
-				return true
+			hash := jdi.jc.hasher.Hash(inputs)
+			if _, ok := jdi.yielding[hash]; ok {
+				delete(jdi.yielding, hash)
+				jdi.yielded[hash] = struct{}{}
+				return true, nil
 			}
 		}
 		// TODO: assert that len(jdi.yielding) == 0
@@ -172,4 +319,12 @@ func (jdi *jobDatumIterator) NumAvailable() int {
 
 func (jdi *jobDatumIterator) Datum() []*common.Input {
 	return jdi.dit.Datum()
+}
+
+func (jdi *jobDatumIterator) DatumSet() DatumSet {
+	return jdi.allDatums
+}
+
+func (jdi *jobDatumIterator) AdditiveOnly() bool {
+	return false // TODO: implement
 }
---
+++ src/server/worker/pipeline/transform/chain/chain_test.go
@@ -0,0 +1,301 @@
+package chain
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+
+	"github.com/pachyderm/pachyderm/src/client/pfs"
+	"github.com/pachyderm/pachyderm/src/client/pkg/require"
+	"github.com/pachyderm/pachyderm/src/server/worker/common"
+	"github.com/pachyderm/pachyderm/src/server/worker/datum"
+)
+
+type testHasher struct{}
+
+func (th *testHasher) Hash(inputs []*common.Input) string {
+	return common.HashDatum("", "", inputs)
+}
+
+type testIterator struct {
+	index  int
+	inputs [][]*common.Input
+}
+
+func (ti *testIterator) Reset() {
+	ti.index = -1
+}
+
+func (ti *testIterator) Len() int {
+	return len(ti.inputs)
+}
+
+func (ti *testIterator) Next() bool {
+	if ti.index < len(ti.inputs)-1 {
+		ti.index++
+	}
+
+	return ti.index < len(ti.inputs)
+}
+
+func (ti *testIterator) Datum() []*common.Input {
+	return ti.inputs[ti.index]
+}
+
+func (ti *testIterator) DatumN(n int) []*common.Input {
+	return ti.inputs[n]
+}
+
+// Convert a test-friendly string to a real fake inputs array
+func datumToInputs(name string) []*common.Input {
+	return []*common.Input{
+		&common.Input{
+			Name: "inputRepo",
+			FileInfo: &pfs.FileInfo{
+				File: &pfs.File{Path: name},
+				Hash: []byte(name),
+			},
+		},
+	}
+}
+
+func inputsToDatum(inputs []*common.Input) (string, error) {
+	if len(inputs) != 1 {
+		return "", fmt.Errorf("should only have 1 input for test datums")
+	}
+	return inputs[0].FileInfo.File.Path, nil
+}
+
+func newTestChain(t *testing.T, datums []string) JobChain {
+	hasher := &testHasher{}
+	chain, err := NewJobChain(hasher)
+	require.NoError(t, err)
+	require.False(t, chain.Initialized())
+
+	baseDatums := make(DatumSet)
+	for _, datum := range datums {
+		baseDatums[hasher.Hash(datumToInputs(datum))] = struct{}{}
+	}
+
+	require.NoError(t, chain.Initialize(baseDatums))
+	require.True(t, chain.Initialized())
+
+	return chain
+}
+
+func datumsToInputs(datums []string) [][]*common.Input {
+	inputs := [][]*common.Input{}
+	for _, datum := range datums {
+		inputs = append(inputs, datumToInputs(datum))
+	}
+	return inputs
+}
+
+func newTestIterator(datums []string) datum.Iterator {
+	return &testIterator{inputs: datumsToInputs(datums)}
+}
+
+type testJob struct {
+	dit datum.Iterator
+}
+
+func newTestJob(datums []string) JobData {
+	return &testJob{dit: newTestIterator(datums)}
+}
+
+func (tj *testJob) Iterator() (datum.Iterator, error) {
+	return tj.dit, nil
+}
+
+func requireIteratorContents(t *testing.T, jdi JobDatumIterator, expected []string) {
+	found := []string{}
+	for range expected {
+		hasNext, err := jdi.Next(context.Background())
+		require.NoError(t, err)
+		require.True(t, hasNext)
+		datum, err := inputsToDatum(jdi.Datum())
+		require.NoError(t, err)
+		found = append(found, datum)
+	}
+	fmt.Printf("expected: %v\nfound: %v\n", expected, found)
+	require.ElementsEqual(t, expected, found)
+	requireIteratorDone(t, jdi)
+}
+
+func requireIteratorDone(t *testing.T, jdi JobDatumIterator) {
+	hasNext, err := jdi.Next(context.Background())
+	require.NoError(t, err)
+	require.False(t, hasNext)
+}
+
+func requireIteratorContentsNonBlocking(t *testing.T, jdi JobDatumIterator, expected []string) {
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	found := []string{}
+
+	require.Equal(t, len(expected), jdi.NumAvailable())
+	for range expected {
+		hasNext, err := jdi.Next(ctx)
+		require.NoError(t, err)
+		require.True(t, hasNext)
+		datum, err := inputsToDatum(jdi.Datum())
+		require.NoError(t, err)
+		found = append(found, datum)
+	}
+
+	require.ElementsEqual(t, expected, found)
+}
+
+func TestEmptyBase(t *testing.T) {
+	jobDatums := []string{"a", "b"}
+	chain := newTestChain(t, []string{})
+	job := newTestJob(jobDatums)
+	jdi, err := chain.Start(job)
+	require.NoError(t, err)
+	requireIteratorContents(t, jdi, jobDatums)
+}
+
+func TestAdditiveOnBase(t *testing.T) {
+	chain := newTestChain(t, []string{"a"})
+	job := newTestJob([]string{"a", "b", "c"})
+	jdi, err := chain.Start(job)
+	require.NoError(t, err)
+	requireIteratorContents(t, jdi, []string{"b", "c"})
+}
+
+func TestSubtractiveOnBase(t *testing.T) {
+	jobDatums := []string{"a", "c"}
+	chain := newTestChain(t, []string{"a", "b", "c"})
+	job := newTestJob(jobDatums)
+	jdi, err := chain.Start(job)
+	require.NoError(t, err)
+	requireIteratorContents(t, jdi, jobDatums)
+}
+
+func TestAdditiveSubtractiveOnBase(t *testing.T) {
+	jobDatums := []string{"b", "c", "d", "e"}
+	chain := newTestChain(t, []string{"a", "b", "c"})
+	job := newTestJob(jobDatums)
+	jdi, err := chain.Start(job)
+	require.NoError(t, err)
+	requireIteratorContents(t, jdi, jobDatums)
+}
+
+// Read from a channel until we have the expected datums, then verify they
+// are correct, then make sure the channel doesn't have anything else.
+func requireDatums(t *testing.T, datumChan <-chan string, expected []string) {
+	// Recvs should be near-instant, but set a decently long timeout to avoid flakiness
+	actual := []string{}
+loop:
+	for range expected {
+		select {
+		case x := <-datumChan:
+			actual = append(actual, x)
+		case <-time.After(time.Second):
+			break loop
+		}
+	}
+	require.ElementsEqual(t, expected, actual)
+
+	select {
+	case x := <-datumChan:
+		require.NoError(t, fmt.Errorf("datum channel contains extra datum: %s", x))
+	default:
+	}
+}
+
+func superviseTestJob(ctx context.Context, eg *errgroup.Group, jdi JobDatumIterator) <-chan string {
+	canceledCtx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	datumsChan := make(chan string)
+	eg.Go(func() error {
+		for ok, err := jdi.Next(ctx); ok; ok, err = jdi.Next(ctx) {
+			if err != nil {
+				fmt.Printf("next1 error: %s\n", err)
+				return err
+			}
+			for {
+				datum, err := inputsToDatum(jdi.Datum())
+				if err != nil {
+					fmt.Printf("inputsToDatum error: %s\n", err)
+					return err
+				}
+
+				fmt.Printf("got datum: %s\n", datum)
+				datumsChan <- datum
+
+				if jdi.NumAvailable() == 0 {
+					break
+				}
+				// Because we have more available, this should never block, so use the
+				// canceledCtx to make sure
+				ok, err := jdi.Next(canceledCtx)
+				if err != nil {
+					fmt.Printf("next2 error: %s\n", err)
+					return err
+				}
+				if !ok {
+					fmt.Printf("incorrect count error\n")
+					return fmt.Errorf("iterator should have had more available")
+				}
+			}
+		}
+		fmt.Printf("iterator done\n")
+		return nil
+	})
+
+	return datumsChan
+}
+
+// Job 1: AB
+// Job 2:  BC
+// Job 3:  BCD
+func TestFailureSplit(t *testing.T) {
+	chain := newTestChain(t, []string{})
+	job1 := newTestJob([]string{"a", "b"})
+	job2 := newTestJob([]string{"b", "c"})
+	job3 := newTestJob([]string{"b", "c", "d"})
+
+	eg, ctx := errgroup.WithContext(context.Background())
+
+	jdi1, err := chain.Start(job1)
+	require.NoError(t, err)
+	datums1 := superviseTestJob(ctx, eg, jdi1)
+
+	jdi2, err := chain.Start(job2)
+	require.NoError(t, err)
+	datums2 := superviseTestJob(ctx, eg, jdi2)
+
+	jdi3, err := chain.Start(job3)
+	require.NoError(t, err)
+	datums3 := superviseTestJob(ctx, eg, jdi3)
+
+	requireDatums(t, datums1, []string{"a", "b"})
+	requireDatums(t, datums2, []string{"c"})
+	requireDatums(t, datums3, []string{"d"})
+
+	require.NoError(t, chain.Fail(job2))
+
+	requireDatums(t, datums1, []string{})
+	requireDatums(t, datums2, []string{})
+	requireDatums(t, datums3, []string{"c"})
+
+	require.NoError(t, chain.Succeed(job1, make(DatumSet)))
+
+	requireDatums(t, datums1, []string{})
+	requireDatums(t, datums2, []string{})
+	requireDatums(t, datums3, []string{"b"})
+
+	require.NoError(t, chain.Succeed(job3, make(DatumSet)))
+
+	requireDatums(t, datums1, []string{})
+	requireDatums(t, datums2, []string{})
+	requireDatums(t, datums3, []string{})
+
+	require.NoError(t, eg.Wait())
+}
