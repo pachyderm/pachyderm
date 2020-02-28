86e5df48cf559378193c6fdb77eb55c53c86a358
Bryce McAnally <bryce@pachyderm.io>
2020-02-28 11:46:31 -0800

    Refactor task queue testing and add testing for canceled tasks and subtask failures

--- src/server/pkg/work/testing/work_test.go
+++ src/server/pkg/work/testing/work_test.go
@@ -2,6 +2,7 @@ package testing
 
 import (
 	"context"
+	"fmt"
 	"math/rand"
 	"strconv"
 	"sync"
@@ -14,7 +15,14 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
-// generateRandomString is a helper function for getPachClient
+var (
+	subtaskFailure = fmt.Errorf("subtask failure")
+)
+
+func seedStr(seed int64) string {
+	return fmt.Sprint("seed: ", strconv.FormatInt(seed, 10))
+}
+
 func generateRandomString(n int) string {
 	rand.Seed(time.Now().UnixNano())
 	b := make([]byte, n)
@@ -24,7 +32,10 @@ func generateRandomString(n int) string {
 	return string(b)
 }
 
-func TestTaskQueue(t *testing.T) {
+func test(t *testing.T, workerFailProb, taskCancelProb, subtaskFailProb float64) {
+	seed := time.Now().UTC().UnixNano()
+	rand.Seed(seed)
+	msg := seedStr(seed)
 	require.NoError(t, testutil.WithEtcdEnv(func(env *testutil.EtcdEnv) error {
 		etcdPrefix := generateRandomString(32)
 		numTasks := 10
@@ -43,22 +54,27 @@ func TestTaskQueue(t *testing.T) {
 		var processMu sync.Mutex
 		subtasksCollected := taskMapsFunc()
 		// Setup workers.
-		failProb := 0.1
 		for i := 0; i < numWorkers; i++ {
 			go func() {
 				for {
 					ctx, cancel := context.WithCancel(context.Background())
 					w := work.NewWorker(context.Background(), env.EtcdClient, etcdPrefix, "")
 					if err := w.Run(ctx, func(_ context.Context, task, subtask *work.Task) error {
-						if rand.Float64() < failProb {
+						if rand.Float64() < workerFailProb {
 							cancel()
 						} else {
 							i, err := strconv.Atoi(task.ID)
-							require.NoError(t, err)
+							require.NoError(t, err, msg)
 							processMu.Lock()
-							require.False(t, subtasksProcessed[i][subtask.ID])
-							subtasksProcessed[i][subtask.ID] = true
-							processMu.Unlock()
+							defer processMu.Unlock()
+							if subtasksProcessed[i] != nil {
+								require.False(t, subtasksProcessed[i][subtask.ID], msg)
+								if rand.Float64() < subtaskFailProb {
+									delete(subtasksCreated[i], subtask.ID)
+									return subtaskFailure
+								}
+								subtasksProcessed[i][subtask.ID] = true
+							}
 						}
 						return nil
 					}); err != nil {
@@ -75,26 +91,61 @@ func TestTaskQueue(t *testing.T) {
 		for i := 0; i < numTasks; i++ {
 			i := i
 			taskID := strconv.Itoa(i)
-			m, err := tq.CreateTask(context.Background(), &work.Task{ID: taskID})
-			require.NoError(t, err)
+			ctx, cancel := context.WithCancel(context.Background())
+			m, err := tq.CreateTask(ctx, &work.Task{ID: taskID})
+			require.NoError(t, err, msg)
 			eg.Go(func() error {
 				m.Collect(func(_ context.Context, subtaskInfo *work.TaskInfo) error {
+					if subtaskInfo.State == work.State_FAILURE {
+						require.Equal(t, subtaskFailure.Error(), subtaskInfo.Reason, msg)
+						return nil
+					}
 					subtasksCollected[i][subtaskInfo.Task.ID] = true
 					return nil
 				})
 				// Create subtasks.
 				for j := 0; j < numSubtasks; j++ {
 					subtaskID := strconv.Itoa(j)
-					require.NoError(t, m.CreateSubtask(&work.Task{ID: subtaskID}))
+					require.NoError(t, m.CreateSubtask(&work.Task{ID: subtaskID}), msg)
 					subtasksCreated[i][subtaskID] = true
 				}
-				require.NoError(t, m.Wait())
-				require.NoError(t, tq.DeleteTask(context.Background(), taskID))
+				if rand.Float64() < taskCancelProb {
+					cancel()
+					require.Equal(t, ctx.Err(), m.Wait(), msg)
+					subtasksCreated[i] = nil
+					processMu.Lock()
+					subtasksProcessed[i] = nil
+					processMu.Unlock()
+					subtasksCollected[i] = nil
+				} else {
+					require.NoError(t, m.Wait(), msg)
+				}
+				require.NoError(t, tq.DeleteTask(context.Background(), taskID), msg)
 				return nil
 			})
 		}
-		require.NoError(t, eg.Wait())
-		require.Equal(t, subtasksCreated, subtasksProcessed, subtasksCollected)
+		require.NoError(t, eg.Wait(), msg)
+		require.Equal(t, subtasksCreated, subtasksProcessed, subtasksCollected, msg)
 		return nil
-	}))
+	}), msg)
+}
+
+func TestBasic(t *testing.T) {
+	test(t, 0, 0, 0)
+}
+
+func TestWorkerCrashes(t *testing.T) {
+	test(t, 0.1, 0, 0)
+}
+
+func TestCancelTasks(t *testing.T) {
+	test(t, 0, 0.2, 0)
+}
+
+func TestSubtaskFailures(t *testing.T) {
+	test(t, 0, 0, 0.1)
+}
+
+func TestEverything(t *testing.T) {
+	test(t, 0.1, 0.2, 0.1)
 }
