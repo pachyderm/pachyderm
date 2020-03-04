017215a4143c5aaca39ddccdae51efebc22ac666
Yusuf Simonson <simonson@gmail.com>
2020-03-04 10:31:48 -0500

    Selectively cache images

---
+++ etc/testing/travis_before_cache.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir -p $HOME/docker
+rm $HOME/docker/*
+tag=pachyderm_build:latest
+id=$(docker images pachyderm_build --format '{{.ID}}')
+test -e $HOME/docker/${id}.tar.gz || docker save ${tag} | gzip -2 > $HOME/docker/${id}.tar.gz
