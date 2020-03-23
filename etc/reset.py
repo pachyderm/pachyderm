db67fd59e3eb554fe028874e48b71654da57c7df
Yusuf Simonson <simonson@gmail.com>
2020-03-23 18:32:50 -0400

    Added ability to optionally deploy dash/jupyterhub with reset script

--- etc/reset.py
+++ etc/reset.py
@@ -4,26 +4,24 @@ import os
 import re
 import json
 import time
+import argparse
 import threading
 import subprocess
 
 ETCD_IMAGE = "quay.io/coreos/etcd:v3.3.5"
 
-DELETABLE_RESOURCES = [
-    "replicasets",
-    "services",
-    "deployments",
-    "pods",
-    "rc",
-    "serviceaccounts",
-    "secrets",
-    "clusterrole",
-    "clusterrolebinding",
-]
-
 NEWLINE_SEPARATE_OBJECTS_PATTERN = re.compile(r"\}\n+\{", re.MULTILINE)
 
 GCP_KUBE_CONTEXT_NAME_PATTERN = re.compile(r"gke_([^_]+)_(.+)")
+
+# Resources to delete on reset, that aren't deleted by `pachctl undeploy`
+DELETABLE_RESOURCES = [
+    "roles.rbac.authorization.k8s.io",
+    "rolebindings.rbac.authorization.k8s.io",
+]
+
+class RedactedString(str):
+    pass
 
 class ExcThread(threading.Thread):
     def __init__(self, target):
@@ -52,42 +50,39 @@ def join(*targets):
 
 class BaseDriver:
     def clear(self):
-        # ignore errors on `pachctl delete all` because there's a variety of
-        # ways it could fail that we can recover from:
-        # 1) pachyderm isn't installed on the cluster yet
-        # 2) pachctl isn't installed
-        # 3) pachd is unresponsive
-        # ... etc.
-        try:
-            run("pachctl", "delete", "all", stdin="yes\n", timeout=5)
-        except:
-            pass
-
-        run("kubectl", "delete", ",".join(DELETABLE_RESOURCES), "-l", "suite=pachyderm")
+        join(
+            lambda: run("pachctl", "undeploy", "--all", stdin="y\n"),
+            lambda: run("kubectl", "delete", ",".join(DELETABLE_RESOURCES), "-l", "suite=pachyderm"),
+        )
 
     def start(self):
         pass
 
-    def create_manifest(self):
-        return capture("pachctl", "deploy", "local", "-d", "--dry-run", "--create-context", "--no-guaranteed")
+    def create_manifest(self, include_dash):
+        args = ["pachctl", "deploy", "local", "-d", "--dry-run", "--create-context", "--no-guaranteed"]
+        if not include_dash:
+            args.append("--no-dashboard")
+        return capture(*args)
 
     def sync_images(self, deployments):
-        dash_image = find_in_json(deployments, lambda j: \
-            isinstance(j, dict) and j.get("name") == "dash" and j.get("image") is not None)["image"]
-        grpc_proxy_image = find_in_json(deployments, lambda j: \
-            isinstance(j, dict) and j.get("name") == "grpc-proxy")["image"]
-        
-        run("docker", "pull", dash_image)
-        run("docker", "pull", grpc_proxy_image)
+        dash_spec = find_in_json(deployments, lambda j: \
+            isinstance(j, dict) and j.get("name") == "dash" and j.get("image") is not None)
+        grpc_proxy_spec = find_in_json(deployments, lambda j: \
+            isinstance(j, dict) and j.get("name") == "grpc-proxy")
+
+        if dash_spec is not None:
+            run("docker", "pull", dash_spec["image"])
+        if grpc_proxy_spec is not None:
+            run("docker", "pull", grpc_proxy_spec["image"])
         run("docker", "pull", ETCD_IMAGE)
 
-        return (
-            dash_image,
-            grpc_proxy_image,
-            ETCD_IMAGE,
-            "pachyderm/pachd:local",
-            "pachyderm/worker:local",
-        )
+        push_images = [ETCD_IMAGE, "pachyderm/pachd:local", "pachyderm/worker:local"]
+        if dash_spec is not None:
+            push_images.append(dash_spec["image"])
+        if grpc_proxy_spec is not None:
+            push_images.append(grpc_proxy_spec["image"])
+
+        return push_images
 
     def update_config(self):
         pass
@@ -122,15 +117,20 @@ class GCPDriver(BaseDriver):
         super().clear()
         run("kubectl", "delete", "secret", "regcred", raise_on_error=False)
 
-    def create_manifest(self):
-        registry_url = f"gcr.io/{self.project_id}"
-        return capture("pachctl", "deploy", "local", "-d", "--dry-run", "--create-context", "--no-guaranteed",
-                       "--image-pull-secret", "regcred", "--registry", registry_url)
+    def create_manifest(self, include_dash):
+        args = [
+            "pachctl", "deploy", "local", "-d", "--dry-run", "--create-context", "--no-guaranteed",
+            "--image-pull-secret", "regcred", "--registry", f"gcr.io/{self.project_id}"
+        ]
+        if not include_dash:
+            args.append("--no-dashboard")
+        return capture(*args)
 
     def sync_images(self, deployments):
         docker_config_path = os.path.expanduser("~/.docker/config.json")
         run("kubectl", "create", "secret", "generic", "regcred",
-            f"--from-file=.dockerconfigjson={docker_config_path}", "--type=kubernetes.io/dockerconfigjson")
+            f"--from-file=.dockerconfigjson={docker_config_path}",
+            "--type=kubernetes.io/dockerconfigjson")
 
         for image in super().sync_images(deployments):
             if image.startswith("quay.io/"):
@@ -161,15 +161,21 @@ def print_status(status):
     print(f"===> {status}")
 
 def run(cmd, *args, raise_on_error=True, stdin=None, capture_output=False, timeout=None):
-    all_args = [cmd, *args]
-    print_status("running: `{}`".format(" ".join(all_args)))
-    return subprocess.run(all_args, check=raise_on_error, capture_output=capture_output, input=stdin,
+    print_args = [cmd, *[a if not isinstance(a, RedactedString) else "[redacted]" for a in args]]
+    print_status("running: `{}`".format(" ".join(print_args)))
+
+    return subprocess.run([cmd, *args], check=raise_on_error, capture_output=capture_output, input=stdin,
                           encoding="utf8", timeout=timeout)
 
 def capture(cmd, *args):
     return run(cmd, *args, capture_output=True).stdout
 
 def main():
+    parser = argparse.ArgumentParser(description="Resets a pachyderm cluster.")
+    parser.add_argument("--dash", action="store_true", help="Deploy dash")
+    parser.add_argument("--jupyterhub", action="store_true", help="Deploy jupyterhub")
+    args = parser.parse_args()
+
     if "GOPATH" not in os.environ:
         raise Exception("Must set GOPATH")
     if "PACH_CA_CERTS" in os.environ:
@@ -208,7 +214,7 @@ def main():
     version = capture("pachctl", "version", "--client-only")
     print_status(f"deploy pachyderm version v{version}")
 
-    deployments_str = driver.create_manifest()
+    deployments_str = driver.create_manifest(args.dash)
     deployments_json = json.loads("[{}]".format(NEWLINE_SEPARATE_OBJECTS_PATTERN.sub("},{", deployments_str)))
     driver.sync_images(deployments_json)
     run("kubectl", "create", "-f", "-", stdin=deployments_str)
@@ -217,6 +223,13 @@ def main():
     while run("pachctl", "version", raise_on_error=False).returncode:
         print_status("waiting for pachyderm to come up...")
         time.sleep(1)
+
+    if args.jupyterhub:
+        enterprise_token = capture("aws", "s3", "cp",
+                                   "s3://pachyderm-engineering/test_enterprise_activation_code.txt", "-")
+        run("pachctl", "enterprise", "activate", RedactedString(enterprise_token))
+        run("pachctl", "auth", "activate", stdin="admin\n")
+        run("pachctl", "deploy-jupyterhub")
 
 if __name__ == "__main__":
     main()
