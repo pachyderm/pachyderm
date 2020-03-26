19ee8072a93bcdc778fe92b37af49b53031e90da
Adele Lopez <adele@pachyderm.io>
2020-03-26 15:50:15 -0700

    fix spout input bug

--- src/server/pps/server/api_server.go
+++ src/server/pps/server/api_server.go
@@ -1665,6 +1665,9 @@ func (a *apiServer) validatePipeline(pachClient *client.APIClient, pipelineInfo 
 				return errors.Errorf("the spout marker name must be a valid filename: %v", pipelineInfo.Spout.Marker)
 			}
 		}
+		if pipelineInfo.Input != nil {
+			return errors.Errorf("spout pipelines must not have an input")
+		}
 	}
 	return nil
 }
