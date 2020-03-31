63ea4db134f1a061380669ab0669b0805070b96d
Joe Doliner <jdoliner@gmail.com>
2020-03-31 15:06:09 -0700

    Enables writing to fuse mounts.

--- src/server/pfs/fuse2/fuse.go
+++ src/server/pfs/fuse2/fuse.go
@@ -16,6 +16,8 @@ import (
 	"github.com/pachyderm/pachyderm/src/client"
 	"github.com/pachyderm/pachyderm/src/client/pfs"
 	"github.com/pachyderm/pachyderm/src/server/pkg/errutil"
+	pfssync "github.com/pachyderm/pachyderm/src/server/pkg/sync"
+	"github.com/pachyderm/pachyderm/src/server/pkg/uuid"
 )
 
 const (
@@ -58,13 +60,14 @@ func (m *mount) commit(repo string) (string, error) {
 }
 
 // Mount pfs to mountPoint, opts may be left nil.
-func Mount(c *client.APIClient, mountPoint string, opts *Options) error {
+func Mount(c *client.APIClient, mountPoint string, opts *Options) (retErr error) {
 	files := make(map[string]*file)
+	commits := opts.getCommits()
 	server, err := fs.Mount(mountPoint, &node{
 		file: client.NewFile("", "", ""),
 		m: &mount{
 			c:       c,
-			commits: opts.getCommits(),
+			commits: commits,
 			files:   files,
 		},
 	}, opts.getFuse())
@@ -81,8 +84,47 @@ func Mount(c *client.APIClient, mountPoint string, opts *Options) error {
 		server.Unmount()
 	}()
 	server.Serve()
+	pfcs := make(map[string]client.PutFileClient)
 	for _, file := range files {
-		fmt.Printf("%+v\n", file)
+		if file.dirty {
+			var pfc client.PutFileClient
+			if _pfc, ok := pfcs[file.pfs.Commit.Repo.Name]; ok {
+				pfc = _pfc
+			} else {
+				pfc, err = c.NewPutFileClient()
+				if err != nil {
+					return err
+				}
+				defer func() {
+					if err := pfc.Close(); err != nil && retErr == nil {
+						retErr = err
+					}
+				}()
+				pfcs[file.pfs.Commit.Repo.Name] = pfc
+			}
+			if err := func() error {
+				f, err := os.Open(file.path)
+				if err != nil {
+					return err
+				}
+				if err := os.Remove(file.path); err != nil {
+					return err
+				}
+				defer func() {
+					if err := f.Close(); err != nil && retErr == nil {
+						retErr = err
+					}
+				}()
+				branch := "master"
+				if commit, ok := commits[file.pfs.Commit.Repo.Name]; ok && !uuid.IsUUIDWithoutDashes(commit) {
+					branch = commit
+				}
+				file.pfs.Commit.ID = branch
+				return pfssync.PushFile(c, pfc, file.pfs, f)
+			}(); err != nil {
+				return err
+			}
+		}
 	}
 	return nil
 }
