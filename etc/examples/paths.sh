870f1c43be0e0e0b787c19f27aea2206e1cded94
John Karabaic <github@exnext.com>
2020-04-07 15:10:04 -0700

    moved testing targets into Makefile.testing, created new ones, and cl… (#4715)
    * moved testing targets into Makefile.testing, created new ones, and cleaned up Makefile targets.  Please read Makefile.testing for instructions.
    
    * fixed a typo in docs.
    
    * Update examples/Makefile.testing
    
    Co-Authored-By: Svetlana Karslioglu <svetlana@pachyderm.io>
    
    * Update examples/Makefile.testing
    
    Co-Authored-By: Svetlana Karslioglu <svetlana@pachyderm.io>
    
    * Apply suggestions from code review
    
    typos and style changes
    
    Co-Authored-By: Svetlana Karslioglu <svetlana@pachyderm.io>
    
    * addressed comments
    
    * removed .phony targets
    
    * created helper functions, simplified command variables
    
    * addressed comments on env vars, added SHELL env var.
    
    * fixed travis lint problems
    
    * fixed final build break issues
    
    * changed non-posix source to '.'.
    
    Co-authored-by: Svetlana Karslioglu <svetlana@pachyderm.io>

---
+++ etc/examples/paths.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# these can be set by folks running the examples/Makefile.testing to use a different binaries than in the path
+ECHO=${ECHO:-echo}
+KUBECTL=${KUBECTL:-kubectl}
+PACHCTL=${PACHCTL:-pachctl}
+
