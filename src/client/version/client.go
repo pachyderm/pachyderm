192f2574a2059116d1bc037e57554aa3ce2fb680
nitinjainsj <nitin@pachyderm.io>
2020-04-15 16:27:02 -0700

    Increment version for 1.10.2 point release

--- src/client/version/client.go
+++ src/client/version/client.go
@@ -12,7 +12,7 @@ const (
 	// MinorVersion is the current minor version for pachyderm.
 	MinorVersion = 10
 	// MicroVersion is the patch number for pachyderm.
-	MicroVersion = 1
+	MicroVersion = 2
 )
 
 var (
