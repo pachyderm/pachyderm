a36190a87b1f92c039c614c415b8671b0132135f
grey <marc.hesse@gmail.com>
2020-04-23 15:16:44 -0700

    different kube config concat for windows and unix

---
+++ src/server/pkg/deploy/cmds/cmds_windows.go
@@ -0,0 +1,12 @@
+// +build windows
+
+package cmds
+
+import (
+	"fmt"
+)
+
+// The delimiter for multiple paths in the the KUBECONFIG environment variable differs between unix and windows.
+func concatKubeConfig(a string, b string) string {
+	return fmt.Sprintf("%s;%s", a, b)
+}
