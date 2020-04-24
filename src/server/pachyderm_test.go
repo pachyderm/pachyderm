7b4c45eea2c316e4b0a29c894e519661284e66ca
Adele Lopez <adele@pachyderm.io>
2020-04-24 15:58:03 -0700

    delete all after each spout test

--- src/server/pachyderm_test.go
+++ src/server/pachyderm_test.go
@@ -9856,7 +9856,7 @@ func TestSpout(t *testing.T) {
 		t.Skip("Skipping integration tests in short mode")
 	}
 	c := tu.GetPachClient(t)
-	require.NoError(t, c.DeleteAll())
+
 	t.Run("SpoutBasic", func(t *testing.T) {
 		dataRepo := tu.UniqueString("TestSpoutBasic_data")
 		require.NoError(t, c.CreateRepo(dataRepo))
@@ -9921,6 +9921,15 @@ func TestSpout(t *testing.T) {
 		jobInfos, err := c.FlushJobAll([]*pfs.Commit{client.NewCommit(pipeline, "master")}, []string{downstreamPipeline})
 		require.NoError(t, err)
 		require.Equal(t, 1, len(jobInfos))
+
+		// finally, let's make sure that the provenance is in a consistent state after running the spout test
+		require.NoError(t, c.Fsck(false, func(resp *pfs.FsckResponse) error {
+			if resp.Error != "" {
+				return errors.New(resp.Error)
+			}
+			return nil
+		}))
+		require.NoError(t, c.DeleteAll())
 	})
 
 	t.Run("SpoutOverwrite", func(t *testing.T) {
@@ -9967,6 +9976,14 @@ func TestSpout(t *testing.T) {
 			}
 			prevLength = fileLength
 		}
+		// finally, let's make sure that the provenance is in a consistent state after running the spout test
+		require.NoError(t, c.Fsck(false, func(resp *pfs.FsckResponse) error {
+			if resp.Error != "" {
+				return errors.New(resp.Error)
+			}
+			return nil
+		}))
+		require.NoError(t, c.DeleteAll())
 	})
 	t.Run("SpoutProvenance", func(t *testing.T) {
 		dataRepo := tu.UniqueString("TestSpoutProvenance_data")
@@ -10058,6 +10075,14 @@ func TestSpout(t *testing.T) {
 				require.Equal(t, provenanceID, provenance.ID)
 			}
 		}
+		// finally, let's make sure that the provenance is in a consistent state after running the spout test
+		require.NoError(t, c.Fsck(false, func(resp *pfs.FsckResponse) error {
+			if resp.Error != "" {
+				return errors.New(resp.Error)
+			}
+			return nil
+		}))
+		require.NoError(t, c.DeleteAll())
 	})
 	t.Run("ServiceSpout", func(t *testing.T) {
 		dataRepo := tu.UniqueString("TestServiceSpout_data")
@@ -10148,6 +10173,15 @@ func TestSpout(t *testing.T) {
 		err = c.GetFile(pipeline, commitInfo.Commit.ID, files[0].File.Path, 0, 0, &buf)
 		require.NoError(t, err)
 		require.Equal(t, buf.String(), "foo")
+
+		// finally, let's make sure that the provenance is in a consistent state after running the spout test
+		require.NoError(t, c.Fsck(false, func(resp *pfs.FsckResponse) error {
+			if resp.Error != "" {
+				return errors.New(resp.Error)
+			}
+			return nil
+		}))
+		require.NoError(t, c.DeleteAll())
 	})
 
 	t.Run("SpoutMarker", func(t *testing.T) {
@@ -10323,6 +10357,14 @@ func TestSpout(t *testing.T) {
 				t.Errorf("line did not have the expected form %v, '%v'", len(line), line)
 			}
 		}
+		// finally, let's make sure that the provenance is in a consistent state after running the spout test
+		require.NoError(t, c.Fsck(false, func(resp *pfs.FsckResponse) error {
+			if resp.Error != "" {
+				return errors.New(resp.Error)
+			}
+			return nil
+		}))
+		require.NoError(t, c.DeleteAll())
 	})
 
 	t.Run("SpoutInputValidation", func(t *testing.T) {
@@ -10350,6 +10392,14 @@ func TestSpout(t *testing.T) {
 				},
 			})
 		require.YesError(t, err)
+		// finally, let's make sure that the provenance is in a consistent state after running the spout test
+		require.NoError(t, c.Fsck(false, func(resp *pfs.FsckResponse) error {
+			if resp.Error != "" {
+				return errors.New(resp.Error)
+			}
+			return nil
+		}))
+		require.NoError(t, c.DeleteAll())
 	})
 
 	t.Run("SpoutRapidOpenClose", func(t *testing.T) {
@@ -10387,16 +10437,16 @@ func TestSpout(t *testing.T) {
 			}
 		}
 
+		// finally, let's make sure that the provenance is in a consistent state after running the spout test
+		require.NoError(t, c.Fsck(false, func(resp *pfs.FsckResponse) error {
+			if resp.Error != "" {
+				return errors.New(resp.Error)
+			}
+			return nil
+		}))
 		require.NoError(t, c.DeleteAll())
 	})
 
-	// finally, let's make sure that the provenance is in a consistent state after running all of the spout tests
-	require.NoError(t, c.Fsck(false, func(resp *pfs.FsckResponse) error {
-		if resp.Error != "" {
-			return errors.New(resp.Error)
-		}
-		return nil
-	}))
 }
 
 func TestKafka(t *testing.T) {
