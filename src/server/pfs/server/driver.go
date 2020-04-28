43c3e82300d17d68c98b6ea2d53bf59d19593c54
Matthew Steffen <msteffen@pachyderm.io>
2020-04-28 14:39:31 -0700

    Support BuildCommit.Started/.Finished + add test

--- src/server/pfs/server/driver.go
+++ src/server/pfs/server/driver.go
@@ -20,6 +20,7 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 
 	"golang.org/x/sync/errgroup"
 	"golang.org/x/sync/semaphore"
@@ -1092,16 +1093,17 @@ func (d *driver) deleteRepo(txnCtx *txnenv.TransactionContext, repo *pfs.Repo, f
 // ID can be passed in for transactions, which need to ensure the ID doesn't
 // change after the commit ID has been reported to a client.
 func (d *driver) startCommit(txnCtx *txnenv.TransactionContext, ID string, parent *pfs.Commit, branch string, provenance []*pfs.CommitProvenance, description string) (*pfs.Commit, error) {
-	return d.makeCommit(txnCtx, ID, parent, branch, provenance, nil, nil, nil, nil, nil, description, 0)
+	return d.makeCommit(txnCtx, ID, parent, branch, provenance, nil, nil, nil, nil, nil, description, time.Time{}, time.Time{}, 0)
 }
 
 func (d *driver) buildCommit(ctx context.Context, ID string, parent *pfs.Commit,
 	branch string, provenance []*pfs.CommitProvenance,
-	tree *pfs.Object, trees []*pfs.Object, datums *pfs.Object, sizeBytes uint64) (*pfs.Commit, error) {
+	tree *pfs.Object, trees []*pfs.Object, datums *pfs.Object,
+	started, finished time.Time, sizeBytes uint64) (*pfs.Commit, error) {
 	commit := &pfs.Commit{}
 	err := d.txnEnv.WithWriteContext(ctx, func(txnCtx *txnenv.TransactionContext) error {
 		var err error
-		commit, err = d.makeCommit(txnCtx, ID, parent, branch, provenance, tree, trees, datums, nil, nil, "", sizeBytes)
+		commit, err = d.makeCommit(txnCtx, ID, parent, branch, provenance, tree, trees, datums, nil, nil, "", started, finished, sizeBytes)
 		return err
 	})
 	return commit, err
@@ -1131,6 +1133,8 @@ func (d *driver) makeCommit(
 	recordFiles []string,
 	records []*pfs.PutFileRecords,
 	description string,
+	started time.Time,
+	finished time.Time,
 	sizeBytes uint64,
 ) (*pfs.Commit, error) {
 	// Validate arguments:
@@ -1154,12 +1158,42 @@ func (d *driver) makeCommit(
 	newCommitInfo := &pfs.CommitInfo{
 		Commit:      newCommit,
 		Origin:      &pfs.CommitOrigin{Kind: pfs.OriginKind_USER},
-		Started:     types.TimestampNow(),
 		Description: description,
 	}
 	if branch != "" {
 		if err := ancestry.ValidateName(branch); err != nil {
 			return nil, err
+		}
+	}
+
+	// Set newCommitInfo.Started and possibly newCommitInfo.Finished. Enforce:
+	// 1) 'started' and 'newCommitInfo.Started' must be set
+	// 2) 'started' <= 'finished'
+	switch {
+	case started.IsZero() && finished.IsZero():
+		// set 'started' to Now() && leave 'finished' unset (open commit)
+		started = time.Now()
+	case started.IsZero() && !finished.IsZero():
+		// set 'started' to 'finished'
+		started = finished
+	case !started.IsZero() && finished.IsZero():
+		if now := time.Now(); now.Before(started) {
+			started = now // prevent finished < started (if user finishes commit soon)
+		}
+	case !started.IsZero() && !finished.IsZero():
+		if finished.Before(started) {
+			started = finished // prevent finished < started
+		}
+	}
+	var err error
+	newCommitInfo.Started, err = types.TimestampProto(started)
+	if err != nil {
+		return nil, fmt.Errorf("could not convert 'started' time: %v", err)
+	}
+	if !finished.IsZero() {
+		newCommitInfo.Finished, err = types.TimestampProto(finished)
+		if err != nil {
+			return nil, fmt.Errorf("could not convert 'finished' time: %v", err)
 		}
 	}
 
@@ -1242,7 +1276,7 @@ func (d *driver) makeCommit(
 	// 1. Write 'newCommit' to 'openCommits' collection OR
 	// 2. Finish 'newCommit' (if treeRef != nil or records != nil); see
 	//    "FinishCommit case" above)
-	if treeRef != nil || treesRefs != nil || records != nil {
+	if treeRef != nil || treesRefs != nil || records != nil || newCommitInfo.Finished != nil {
 		if records != nil {
 			parentTree, err := d.getTreeForCommit(txnCtx, parent)
 			if err != nil {
@@ -1268,12 +1302,14 @@ func (d *driver) makeCommit(
 			sizeBytes = uint64(tree.FSSize())
 		}
 
-		// now 'treeRef' is guaranteed to be set
+		// at this point, either 'treeRef' is set OR 'newCommit' is tree-less
 		newCommitInfo.Tree = treeRef
 		newCommitInfo.Trees = treesRefs
 		newCommitInfo.Datums = datumsRef
 		newCommitInfo.SizeBytes = sizeBytes
-		newCommitInfo.Finished = types.TimestampNow()
+		if newCommitInfo.Finished == nil {
+			newCommitInfo.Finished = types.TimestampNow()
+		}
 
 		// If we're updating the master branch, also update the repo size (see
 		// "Update repoInfo" below)
@@ -2845,7 +2881,7 @@ func (d *driver) putFiles(pachClient *client.APIClient, s *putFileServer) error 
 		// a commit with no ID, that ID will be filled in with the head of
 		// branch (if it exists).
 		return d.txnEnv.WithWriteContext(ctx, func(txnCtx *txnenv.TransactionContext) error {
-			_, err := d.makeCommit(txnCtx, "", client.NewCommit(repo, ""), branch, nil, nil, nil, nil, putFilePaths, putFileRecords, "", 0)
+			_, err := d.makeCommit(txnCtx, "", client.NewCommit(repo, ""), branch, nil, nil, nil, nil, putFilePaths, putFileRecords, "", time.Time{}, time.Time{}, 0)
 			return err
 		})
 	}
@@ -3274,7 +3310,7 @@ func (d *driver) copyFile(pachClient *client.APIClient, src *pfs.File, dst *pfs.
 	// dst is finished => all PutFileRecords are in 'records'--put in a new commit
 	if !dstIsOpenCommit {
 		return d.txnEnv.WithWriteContext(pachClient.Ctx(), func(txnCtx *txnenv.TransactionContext) error {
-			_, err = d.makeCommit(txnCtx, "", client.NewCommit(dst.Commit.Repo.Name, ""), branch, nil, nil, nil, nil, paths, records, "", 0)
+			_, err = d.makeCommit(txnCtx, "", client.NewCommit(dst.Commit.Repo.Name, ""), branch, nil, nil, nil, nil, paths, records, "", time.Time{}, time.Time{}, 0)
 			return err
 		})
 	}
@@ -4172,7 +4208,7 @@ func (d *driver) deleteFile(pachClient *client.APIClient, file *pfs.File) error 
 			return pfsserver.ErrCommitFinished{file.Commit}
 		}
 		return d.txnEnv.WithWriteContext(pachClient.Ctx(), func(txnCtx *txnenv.TransactionContext) error {
-			_, err := d.makeCommit(txnCtx, "", client.NewCommit(file.Commit.Repo.Name, ""), branch, nil, nil, nil, nil, []string{file.Path}, []*pfs.PutFileRecords{&pfs.PutFileRecords{Tombstone: true}}, "", 0)
+			_, err := d.makeCommit(txnCtx, "", client.NewCommit(file.Commit.Repo.Name, ""), branch, nil, nil, nil, nil, []string{file.Path}, []*pfs.PutFileRecords{&pfs.PutFileRecords{Tombstone: true}}, "", time.Time{}, time.Time{}, 0)
 			return err
 		})
 	}
