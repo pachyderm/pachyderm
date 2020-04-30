80415b1712e8d8b825b7f284a358aa3febf72e3b
grey <grey@pachyderm.io>
2020-04-30 12:02:25 -0700

    fix merge

--- src/server/pfs/server/driver_unix.go
+++ src/server/pfs/server/driver_unix.go
@@ -45,6 +45,14 @@ func (d *driver) downloadTree(pachClient *client.APIClient, object *pfs.Object, 
 	if err != nil {
 		return nil, err
 	}
+
+	// Ensure we close the file if we've errored
+	defer func() {
+		if retErr != nil {
+			f.Close()
+		}
+	}()
+
 	// Mark the file for removal (Linux won't remove it until we close the file)
 	if err := os.Remove(name); err != nil {
 		return nil, err
