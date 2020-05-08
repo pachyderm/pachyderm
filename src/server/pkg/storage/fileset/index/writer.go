89d678bd0c1073ac781686d25d63364e9a563bb6
Bryce McAnally <bryce@pachyderm.io>
2020-05-08 08:18:45 -0700

    Always close object storage writer (even after errors)

--- src/server/pkg/storage/fileset/index/writer.go
+++ src/server/pkg/storage/fileset/index/writer.go
@@ -152,8 +152,8 @@ func (w *Writer) Close() (retErr error) {
 		return err
 	}
 	defer func() {
-		if retErr == nil {
-			retErr = objW.Close()
+		if err := objW.Close(); retErr == nil {
+			retErr = err
 		}
 	}()
 	// Handles the empty file set case.
