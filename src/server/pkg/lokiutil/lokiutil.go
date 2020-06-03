2be99a77a02f9db00eb53c210cf8522b375b219a
Joe Doliner <jdoliner@gmail.com>
2020-06-03 13:43:30 -0700

    Remove commented out code.

--- src/server/pkg/lokiutil/lokiutil.go
+++ src/server/pkg/lokiutil/lokiutil.go
@@ -10,43 +10,6 @@ import (
 )
 
 func ForEachLine(resp *loghttp.QueryResponse, f func(line string) error) error {
-	// common := commonLabels(streams)
-
-	// // Remove the labels we want to show from common
-	// if len(q.ShowLabelsKey) > 0 {
-	// 	common = matchLabels(false, common, q.ShowLabelsKey)
-	// }
-
-	// if len(common) > 0 && !q.Quiet {
-	// 	log.Println("Common labels:", color.RedString(common.String()))
-	// }
-
-	// if len(q.IgnoreLabelsKey) > 0 && !q.Quiet {
-	// 	log.Println("Ignoring labels key:", color.RedString(strings.Join(q.IgnoreLabelsKey, ",")))
-	// }
-
-	// // Remove ignored and common labels from the cached labels and
-	// // calculate the max labels length
-	// maxLabelsLen := q.FixedLabelsLen
-	// for i, s := range streams {
-	// 	// Remove common labels
-	// 	ls := subtract(s.Labels, common)
-
-	// 	// Remove ignored labels
-	// 	if len(q.IgnoreLabelsKey) > 0 {
-	// 		ls = matchLabels(false, ls, q.IgnoreLabelsKey)
-	// 	}
-
-	// 	// Overwrite existing Labels
-	// 	streams[i].Labels = ls
-
-	// 	// Update max labels length
-	// 	len := len(ls.String())
-	// 	if maxLabelsLen < len {
-	// 		maxLabelsLen = len
-	// 	}
-	// }
-
 	// sort and display entries
 	streams, ok := resp.Data.Result.(loghttp.Streams)
 	if !ok {
