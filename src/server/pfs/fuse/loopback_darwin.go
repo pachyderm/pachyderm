5a00ca2cd0c402310a20667c7601f6e0fde734ef
Yusuf Simonson <simonson@gmail.com>
2020-06-04 21:54:05 -0400

    Separated out file from node logic

--- src/server/pfs/fuse/loopback_darwin.go
+++ src/server/pfs/fuse/loopback_darwin.go
@@ -9,107 +9,34 @@ package fuse
 import (
 	"context"
 	"syscall"
-	"time"
-	"unsafe"
+
+	"golang.org/x/sys/unix"
 
 	"github.com/hanwen/go-fuse/v2/fs"
-	"github.com/hanwen/go-fuse/v2/fuse"
-	"github.com/pachyderm/pachyderm/src/server/pfs/fuse/utimens"
 )
 
 func (n *loopbackNode) Getxattr(ctx context.Context, attr string, dest []byte) (uint32, syscall.Errno) {
-	return 0, syscall.ENOSYS
+	sz, err := unix.Getxattr(n.path(), attr, dest)
+	return uint32(sz), fs.ToErrno(err)
 }
 
 func (n *loopbackNode) Setxattr(ctx context.Context, attr string, data []byte, flags uint32) syscall.Errno {
-	return syscall.ENOSYS
+	err := unix.Setxattr(n.path(), attr, data, int(flags))
+	return fs.ToErrno(err)
 }
 
 func (n *loopbackNode) Removexattr(ctx context.Context, attr string) syscall.Errno {
-	return syscall.ENOSYS
+	err := unix.Removexattr(n.path(), attr)
+	return fs.ToErrno(err)
 }
 
 func (n *loopbackNode) Listxattr(ctx context.Context, dest []byte) (uint32, syscall.Errno) {
-	return 0, syscall.ENOSYS
+	sz, err := unix.Listxattr(n.path(), dest)
+	return uint32(sz), fs.ToErrno(err)
 }
 
 func (n *loopbackNode) renameExchange(name string, newparent *loopbackNode, newName string) syscall.Errno {
 	return syscall.ENOSYS
-}
-
-func (f *loopbackFile) Allocate(ctx context.Context, off uint64, sz uint64, mode uint32) syscall.Errno {
-	// TODO: Handle `mode` parameter.
-
-	// From `man fcntl` on OSX:
-	//     The F_PREALLOCATE command operates on the following structure:
-	//
-	//             typedef struct fstore {
-	//                 u_int32_t fst_flags;      /* IN: flags word */
-	//                 int       fst_posmode;    /* IN: indicates offset field */
-	//                 off_t     fst_offset;     /* IN: start of the region */
-	//                 off_t     fst_length;     /* IN: size of the region */
-	//                 off_t     fst_bytesalloc; /* OUT: number of bytes allocated */
-	//             } fstore_t;
-	//
-	//     The flags (fst_flags) for the F_PREALLOCATE command are as follows:
-	//
-	//           F_ALLOCATECONTIG   Allocate contiguous space.
-	//
-	//           F_ALLOCATEALL      Allocate all requested space or no space at all.
-	//
-	//     The position modes (fst_posmode) for the F_PREALLOCATE command indicate how to use the offset field.  The modes are as fol-
-	//     lows:
-	//
-	//           F_PEOFPOSMODE   Allocate from the physical end of file.
-	//
-	//           F_VOLPOSMODE    Allocate from the volume offset.
-
-	k := struct {
-		Flags      uint32 // u_int32_t
-		Posmode    int64  // int
-		Offset     int64  // off_t
-		Length     int64  // off_t
-		Bytesalloc int64  // off_t
-	}{
-		0,
-		0,
-		int64(off),
-		int64(sz),
-		0,
-	}
-
-	// Linux version for reference:
-	// err := syscall.Fallocate(int(f.File.Fd()), mode, int64(off), int64(sz))
-	_, _, errno := syscall.Syscall(syscall.SYS_FCNTL, uintptr(f.fd), uintptr(syscall.F_PREALLOCATE), uintptr(unsafe.Pointer(&k)))
-
-	return errno
-}
-
-// timeToTimeval - Convert time.Time to syscall.Timeval
-//
-// Note: This does not use syscall.NsecToTimespec because
-// that does not work properly for times before 1970,
-// see https://github.com/golang/go/issues/12777
-func timeToTimeval(t *time.Time) syscall.Timeval {
-	var tv syscall.Timeval
-	tv.Usec = int32(t.Nanosecond() / 1000)
-	tv.Sec = t.Unix()
-	return tv
-}
-
-// MacOS before High Sierra lacks utimensat() and UTIME_OMIT.
-// We emulate using utimes() and extra Getattr() calls.
-func (f *loopbackFile) utimens(a *time.Time, m *time.Time) syscall.Errno {
-	var attr fuse.AttrOut
-	if a == nil || m == nil {
-		errno := f.Getattr(context.Background(), &attr)
-		if errno != 0 {
-			return errno
-		}
-	}
-	tv := utimens.Fill(a, m, &attr.Attr)
-	err := syscall.Futimes(int(f.fd), tv)
-	return fs.ToErrno(err)
 }
 
 func (n *loopbackNode) CopyFileRange(ctx context.Context, fhIn fs.FileHandle,
