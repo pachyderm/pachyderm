9e820cdb3fb21400aaa53cbce648f35e5229ee93
Brendon Carroll <7072757+brendoncarroll@users.noreply.github.com>
2020-06-11 10:30:54 -0400

    Adds GlobFileV2 rpc

--- src/client/transaction.go
+++ src/client/transaction.go
@@ -398,6 +398,9 @@ func (c *pfsBuilderClient) GetTarConditionalV2(ctx context.Context, opts ...grpc
 func (c *pfsBuilderClient) ListFileV2(ctx context.Context, req *pfs.ListFileRequest, opts ...grpc.CallOption) (pfs.API_ListFileV2Client, error) {
 	return nil, unsupportedError("ListFileV2")
 }
+func (c *pfsBuilderClient) GlobFileV2(ctx context.Context, req *pfs.GlobFileRequest, opts ...grpc.CallOption) (pfs.API_GlobFileV2Client, error) {
+	return nil, unsupportedError("GlobFileV2")
+}
 
 func (c *objectBuilderClient) PutObject(ctx context.Context, opts ...grpc.CallOption) (pfs.ObjectAPI_PutObjectClient, error) {
 	return nil, unsupportedError("PutObject")
--- src/server/pfs/server/api_server.go
+++ src/server/pfs/server/api_server.go
@@ -635,6 +635,11 @@ func (a *apiServer) ListFileV2(req *pfs.ListFileRequest, server pfs.API_ListFile
 	return errors.Errorf("v2 method not implemented")
 }
 
+// GlobFileV2 not implemented
+func (a *apiServer) GlobFileV2(req *pfs.GlobFileRequest, server pfs.API_GlobFileV2Server) error {
+	return errors.Errorf("method not implemented")
+}
+
 func drainFileServer(putFileServer interface {
 	Recv() (*pfs.PutFileRequest, error)
 }) {
