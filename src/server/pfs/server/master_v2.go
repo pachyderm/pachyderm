21682baf36a2b43872c8a29c6cdb46218514e217
Brendon Carroll <7072757+brendoncarroll@users.noreply.github.com>
2020-06-23 14:32:04 -0400

    remove loop around retry in master

--- src/server/pfs/server/master_v2.go
+++ src/server/pfs/server/master_v2.go
@@ -21,24 +21,20 @@ const (
 func (d *driverV2) master(env *serviceenv.ServiceEnv, objClient obj.Client, db *gorm.DB) {
 	ctx := context.Background()
 	masterLock := dlock.NewDLock(d.etcdClient, path.Join(d.prefix, masterLockPath))
-	for {
-		err := backoff.RetryNotify(func() error {
-			masterCtx, err := masterLock.Lock(ctx)
-			if err != nil {
-				return err
-			}
-			defer masterLock.Unlock(masterCtx)
-			opts, err := gc.ServiceEnvToOptions(env)
-			if err != nil {
-				return err
-			}
-			return gc.Run(masterCtx, objClient, db, opts...)
-		}, backoff.NewInfiniteBackOff(), func(err error, _ time.Duration) error {
-			log.Printf("error in pfs master: %v", err)
-			return err
-		})
+	err := backoff.RetryNotify(func() error {
+		masterCtx, err := masterLock.Lock(ctx)
 		if err != nil {
-			panic(err)
+			return err
 		}
-	}
+		defer masterLock.Unlock(masterCtx)
+		opts, err := gc.ServiceEnvToOptions(env)
+		if err != nil {
+			return err
+		}
+		return gc.Run(masterCtx, objClient, db, opts...)
+	}, backoff.NewInfiniteBackOff(), func(err error, _ time.Duration) error {
+		log.Printf("error in pfs master: %v", err)
+		return err
+	})
+	panic(err)
 }
