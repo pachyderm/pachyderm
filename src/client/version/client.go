83f184e2875d495124875b247f605a086a0e0ea4
nitinjainsj <nitin@pachyderm.com>
2020-07-06 09:41:14 -0700

    Fix dash compatibility file read to be freom a branch not a tag

--- src/client/version/client.go
+++ src/client/version/client.go
@@ -67,6 +67,15 @@ func IsCustomRelease(version *pb.Version) bool {
 	return false
 }
 
+// Get branch from version string
+// patch release of .0 is always from the master. Others are from the M.m.x branch
+func BranchFromVersion(version *pb.Version) string {
+	if version.Micro == 0 {
+		return fmt.Sprintf("master")
+	}
+	return fmt.Sprintf("%d.%d.x", version.Major, version.Minor)
+}
+
 // PrettyVersion calls PrettyPrintVersion on Version and returns the result.
 func PrettyVersion() string {
 	return PrettyPrintVersion(Version)
--- src/server/pkg/deploy/cmds/cmds.go
+++ src/server/pkg/deploy/cmds/cmds.go
@@ -1307,23 +1307,31 @@ underlying volume will not be removed.`)
 }
 
 func getDefaultOrLatestDashImage(dashImage string, dryRun bool) string {
-	var err error
-	tagVersion := version.PrettyPrintVersion(version.Version)
-	defer func() {
-		if err != nil && !dryRun {
-			fmt.Printf("No updated dash image found for pachctl %v: %v Falling back to dash image %v\n", tagVersion, err, defaultDashImage)
-		}
-	}()
 	if dashImage != "" {
 		// It has been supplied explicitly by version on the command line
 		return dashImage
 	}
 	dashImage = defaultDashImage
-	verCustom := tagVersion
+
+	var err error
+	var relVersion string
+
+	// This is the branch where to look.
+	// If a new dash version need to be pushed we can just update the compatibility file
+	// in github repo. All version of pachd will pick it upon restart. To make this work
+	// we have to point to the right branch (not tag) in the repo
+	branch := version.BranchFromVersion(version.Version)
 	if version.IsCustomRelease(version.Version) {
-		verCustom = version.PrettyPrintVersionNoAdditional(version.Version)
+		relVersion = version.PrettyPrintVersionNoAdditional(version.Version)
+	} else {
+		relVersion = version.PrettyPrintVersion(version.Version)
 	}
-	compatibleDashVersionsURL := fmt.Sprintf("https://raw.githubusercontent.com/pachyderm/pachyderm/v%v/etc/compatibility/%v", tagVersion, verCustom)
+	defer func() {
+		if err != nil && !dryRun {
+			fmt.Printf("No updated dash image found for pachctl in branch %v in release %v: %v Falling back to dash image %v\n", branch, relVersion, err, defaultDashImage)
+		}
+	}()
+	compatibleDashVersionsURL := fmt.Sprintf("https://raw.githubusercontent.com/pachyderm/pachyderm/%v/etc/compatibility/%v", branch, relVersion)
 	resp, err := http.Get(compatibleDashVersionsURL)
 	if err != nil {
 		return dashImage
