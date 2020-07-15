a43261dd5b01507ce6226f526d5c73daeb2fe094
Joe Doliner <jdoliner@gmail.com>
2020-07-15 10:25:45 -0700

    Rework warning message.

--- src/server/pfs/cmds/mount_darwin.go
+++ src/server/pfs/cmds/mount_darwin.go
@@ -5,6 +5,5 @@ package cmds
 import "fmt"
 
 func printWarning() {
-	fmt.Println(`WARNING: FUSE is not officially supported on macOS.
-You may encounter issues depending on which version of macOS you're on.`)
+	fmt.Println(`WARNING: Mount is supported on macOS versions 1.10.5 and earlier. Mount is implemented using FUSE which isn't supported in macOS 1.11. FUSE on macOS differs from Linux FUSE which causes some issues known issues. For the best experience we recommend using mount on Linux.`)
 }
