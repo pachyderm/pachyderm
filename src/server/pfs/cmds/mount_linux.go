cb21737af5f25cdaab15d662e88c517781f0d7d4
Joe Doliner <jdoliner@gmail.com>
2020-07-14 17:18:48 -0700

    Don't print warning on linux.

--- src/server/pfs/cmds/mount_linux.go
+++ src/server/pfs/cmds/mount_linux.go
@@ -2,9 +2,4 @@
 
 package cmds
 
-import "fmt"
-
-func printWarning() {
-	fmt.Println(`WARNING: FUSE is not officially supported on macOS.
-You may encounter issues depending on which version of macOS you're on.`)
-}
+func printWarning() {}
