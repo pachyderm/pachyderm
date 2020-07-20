471f96aa83e3ea536f35d94340d2923fc2052353
Matthew Steffen <msteffen@pachyderm.io>
2020-07-20 08:11:53 -0700

    Increment version in master now that 1.11.0 is cut

--- src/client/version/client.go
+++ src/client/version/client.go
@@ -12,7 +12,7 @@ const (
 	// MajorVersion is the current major version for pachyderm.
 	MajorVersion = 1
 	// MinorVersion is the current minor version for pachyderm.
-	MinorVersion = 11
+	MinorVersion = 12
 	// MicroVersion is the patch number for pachyderm.
 	MicroVersion = 0
 )
