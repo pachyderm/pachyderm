4ab2d482401cc9a2336d2b9d55234576fa67d79f
Luke Marsden <me@lukemarsden.net>
2020-07-23 16:40:52 +0100

    Always put quotes around things in bash in case they are empty-string.

--- etc/testing/travis.sh
+++ etc/testing/travis.sh
@@ -26,7 +26,7 @@ if [[ "$TRAVIS_SECURE_ENV_VARS" == "true" ]]; then
 
     if [ ! "${TRAVIS_PULL_REQUEST}" == "false" ]; then
         echo "Detected PR build, checking for consistency..."
-        if [[ ! $version == *"-${TRAVIS_COMMIT}" ]]; then
+        if [[ ! "$version" == *"-${TRAVIS_COMMIT}" ]]; then
             echo "Detected that we have a version of the code checked out which"
             echo "is not the same as the version we are trying to test."
             echo "Bailing."
