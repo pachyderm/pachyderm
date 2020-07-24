51f38e039e3d928d87226b0afdaf484b6277d269
Luke Marsden <me@lukemarsden.net>
2020-07-24 15:50:03 +0100

    Begin implementing plan in https://www.dropbox.com/scl/fi/361jzngrcfhb6yic4es9o/Notes-on-CI-issues.paper?dl=0&rlkey=uf6bz9pxpgfgfsohrsc5vrjwb

---
+++ etc/testing/travis_build_check_and_stash.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -ex
+
+# Travis doesn't reliably give us the right version of the code in PR builds.
+#
+# Sometimes it's too old (the GitHub merge ref API can be out of date) and so
+# we get code from a previous commit in the pull request.
+#
+# Sometimes it's too new (a new commit was pushed to the PR before we ran, and
+# GitHub updated the merge ref API already).
+#
+# We know that we are trying to test code in a merge preview with
+# $TRAVIS_PULL_REQUEST_SHA on the RHS of the merge though, that information is
+# reliable at least.
+#
+# So, try a few times to fetch and identify a valid version of the code. If we
+# time out, give up. If we succeed, stash that code in a docker image which
+# later tests can use to get it back.
+
+pwd
+cd /home/travis/gopath/src/github.com/pachyderm/pachyderm
+
+#        if [[ ! "$version" == *"-${TRAVIS_PULL_REQUEST_SHA}" ]]; then
+
+if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
+    # These shenannigans not needed for release and branch builds, hopefully.
+    exit 0
+fi
+
+echo "Detected PR build, checking for consistency..."
+
+tries=0
+while true; do
+    # Check if HEAD is a merge commit with the commit we want to test as one of
+    # the parents (the other one would be current main branch HEAD)
+    parents=$(git rev-list --parents -n 1 HEAD)
+    if [[ "$TRAVIS_PULL_REQUEST_SHA" == *"$parents"* ]]; then
+        echo "Great, found the commit we're meant to be testing ($TRAVIS_PULL_REQUEST_SHA)"
+        echo "as one of the parents of the HEAD merge preview commit ($parents)"
+        break
+    else
+        ((tries++))
+        if [ "$tries" -gt 60 ]; then
+            echo "Gave up waiting for GitHub to give us the right commit";
+            exit 1
+        fi
+        # The commit we want to test wasn't found yet, maybe the GitHub API is out
+        # of date. Let's wait a second and try to update it, then check again.
+        sleep 1
+        echo "GitHub didn't give us the commit we're meant to be testing ($TRAVIS_PULL_REQUEST_SHA)"
+        echo "as one of the parents of the HEAD merge preview commit ($parents). Trying again..."
+        git fetch origin +refs/pull/${TRAVIS_PULL_REQUEST}/merge
+        git checkout -qf FETCH_HEAD
+    fi
+done
---
+++ etc/testing/travis_test_check_and_pull.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -ex
+
+# TODO: write this.
