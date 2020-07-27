b79c14d195c2b80991406b1e835500ff1bad65dd
Joe Doliner <jdoliner@gmail.com>
2020-07-27 10:04:26 -0700

    More work on the gpt-3 example.

---
+++ examples/ml/gpt-3/train.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -ex
+
+openai-ft \
+-t /pfs/data/train.jsonl \
+--val /pfs/data/test.jsonl \
+-e $ENGINE \
+-m ada \
+--batch-size 4 \
+--val-batch-size 4 \
+--num-epochs 10 \
+-k $OPENAI_API_KEY \
