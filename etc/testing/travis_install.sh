98ff4916a286d8ce7dd72c5268db52b889a269c9
Luke Marsden <me@lukemarsden.net>
2020-07-28 10:33:37 +0100

    Remove all references to cached-deps, just install the binaries each time (loading the cache was taking 3+ minutes on startup, probably not a win)

--- etc/testing/travis_install.sh
+++ etc/testing/travis_install.sh
@@ -36,46 +36,46 @@ pip3 install --upgrade --user awscli
 # Install kubectl
 # To get the latest kubectl version:
 # curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt
-if [ ! -f ~/cached-deps/kubectl ] ; then
+if [ ! -f /usr/local/bin/kubectl ] ; then
     KUBECTL_VERSION=v1.13.0
     curl -L -o kubectl https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl && \
         chmod +x ./kubectl && \
-        mv ./kubectl ~/cached-deps/kubectl
+        mv ./kubectl /usr/local/bin/kubectl
 fi
 
 # Install minikube
 # To get the latest minikube version:
 # curl https://api.github.com/repos/kubernetes/minikube/releases | jq -r .[].tag_name | sort | tail -n1
-if [ ! -f ~/cached-deps/minikube ] ; then
+if [ ! -f /usr/local/bin/minikube ] ; then
     MINIKUBE_VERSION=v0.31.0
     curl -L -o minikube https://storage.googleapis.com/minikube/releases/${MINIKUBE_VERSION}/minikube-linux-amd64 && \
         chmod +x ./minikube && \
-        mv ./minikube ~/cached-deps/minikube
+        mv ./minikube /usr/local/bin/minikube
 fi
 
 # Install vault
-if [ ! -f ~/cached-deps/vault ] ; then
+if [ ! -f /usr/local/bin/vault ] ; then
     curl -Lo vault.zip https://releases.hashicorp.com/vault/1.2.3/vault_1.2.3_linux_amd64.zip && \
         unzip vault.zip && \
-        mv ./vault ~/cached-deps/vault
+        mv ./vault /usr/local/bin/vault
 fi
 
 # Install etcdctl
 # To get the latest etcd version:
 # curl -Ls https://api.github.com/repos/etcd-io/etcd/releases | jq -r .[].tag_name
-if [ ! -f ~/cached-deps/etcdctl ] ; then
+if [ ! -f /usr/local/bin/etcdctl ] ; then
     ETCD_VERSION=v3.3.12
     curl -L https://storage.googleapis.com/etcd/${ETCD_VERSION}/etcd-${ETCD_VERSION}-linux-amd64.tar.gz \
         | tar xzf - --strip-components=1 && \
-        mv ./etcdctl ~/cached-deps/etcdctl
+        mv ./etcdctl /usr/local/bin/etcdctl
 fi
 
 # Install kubeval
-if [ ! -f ~/cached-deps/kubeval ]; then
+if [ ! -f /usr/local/bin/kubeval ]; then
   KUBEVAL_VERSION=0.14.0
   curl -L https://github.com/instrumenta/kubeval/releases/download/${KUBEVAL_VERSION}/kubeval-linux-amd64.tar.gz \
       | tar xzf - kubeval && \
-      mv ./kubeval ~/cached-deps/kubeval
+      mv ./kubeval /usr/local/bin/kubeval
 fi
 
 # Install helm
