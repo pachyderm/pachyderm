7d7ca4fe58a0326a060459445f218e4ce902119d
Bryce McAnally <bryce@pachyderm.io>
2020-07-29 14:29:17 -0500

    Fix validate bug

--- src/server/pfs/server/testing/server_v2_test.go
+++ src/server/pfs/server/testing/server_v2_test.go
@@ -410,7 +410,7 @@ func newRandomFileGenerator(opts ...randomFileGeneratorOption) fileGenerator {
 			}
 			state.sizeLeft -= size
 		})
-		file := tarutil.NewFile(name, chunk.RandSeq(size))
+		file := tarutil.NewFile("/"+name, chunk.RandSeq(size))
 		if err := tarutil.WriteFile(tw, file); err != nil {
 			return nil, err
 		}
@@ -608,7 +608,7 @@ func (v *validator) recordFileSet(files fileSetSpec) {
 	}
 }
 
-func (v *validator) validate(r io.Reader) error {
+func (v *validator) validate(r io.Reader) (retErr error) {
 	var namesSorted []string
 	for name := range v.files {
 		namesSorted = append(namesSorted, name)
@@ -617,9 +617,24 @@ func (v *validator) validate(r io.Reader) error {
 	if len(namesSorted) > 0 {
 		namesSorted = append([]string{"/"}, namesSorted...)
 	}
+	defer func() {
+		if retErr == nil {
+			if len(namesSorted) != 0 {
+				retErr = errors.Errorf("got back less files than expected")
+			}
+		}
+	}()
 	return tarutil.Iterate(r, func(file tarutil.File) error {
 		if len(namesSorted) == 0 {
 			return errors.Errorf("got back more files than expected")
+		}
+		hdr, err := file.Header()
+		if err != nil {
+			return err
+		}
+		if hdr.Name == "/" && namesSorted[0] == "/" {
+			namesSorted = namesSorted[1:]
+			return nil
 		}
 		ok, err := tarutil.Equal(v.files[namesSorted[0]], file)
 		if err != nil {
