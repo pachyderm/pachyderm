7ff2e9fbc6585508b3007b43481c5f6d51acd961
Adele Lopez <adele@pachyderm.io>
2020-08-12 16:06:29 -0700

    keep track of errors during sorts

--- src/server/pfs/server/driver.go
+++ src/server/pfs/server/driver.go
@@ -1428,9 +1428,10 @@ func (d *driver) makeCommit(
 		}
 
 		// we need to check the commit info of the 'j' provenance commit to get the parent
-		provCommitInfo, err := d.resolveCommit(txnCtx.Stm, newCommitInfo.Provenance[j].Commit)
+		var provCommitInfo *pfs.CommitInfo
+		provCommitInfo, err = d.resolveCommit(txnCtx.Stm, newCommitInfo.Provenance[j].Commit)
 		if err != nil {
-			// not really anything to do at this point
+			// not really anything to do at this point, error will be captured after
 			return true
 		}
 		// the parent commit of 'j' should precede it
@@ -1440,6 +1441,10 @@ func (d *driver) makeCommit(
 		}
 		return false
 	})
+	// capture any errors during sorting
+	if err != nil {
+		return nil, err
+	}
 
 	// Finally, create the commit
 	if err := commits.Create(newCommit.ID, newCommitInfo); err != nil {
@@ -1815,6 +1820,7 @@ nextSubvBI:
 
 		// this ensures that the job's output commit uses the latest commit on the branch, by ensuring it is the
 		// last commit to appear in the provenance slice
+		var err error
 		sort.SliceStable(newCommitInfo.Provenance, func(i, j int) bool {
 			// to make sure the parent relationship is respected during sort, we need to make sure that we organize
 			// the provenance by repo name and branch name
@@ -1825,9 +1831,10 @@ nextSubvBI:
 			}
 
 			// we need to check the commit info of the 'j' provenance commit to get the parent
-			provCommitInfo, err := d.resolveCommit(stm, newCommitInfo.Provenance[j].Commit)
+			var provCommitInfo *pfs.CommitInfo
+			provCommitInfo, err = d.resolveCommit(stm, newCommitInfo.Provenance[j].Commit)
 			if err != nil {
-				// not really anything to do at this point
+				// not really anything to do at this point, error will be captured after
 				return true
 			}
 			// the parent commit of 'j' should precede it
@@ -1837,6 +1844,10 @@ nextSubvBI:
 			}
 			return false
 		})
+		// capture any errors during sorting
+		if err != nil {
+			return err
+		}
 
 		// finally create open 'commit'
 		if err := stmCommits.Create(newCommit.ID, newCommitInfo); err != nil {
