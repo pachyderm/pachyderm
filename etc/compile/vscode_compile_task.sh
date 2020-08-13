edc4550cde040648e2ba0147199bbeec5032ccc6
grey <grey@pachyderm.io>
2020-08-12 17:22:18 -0700

    fix BREAK error when listing commits with a limit

--- etc/compile/vscode_compile_task.sh
+++ etc/compile/vscode_compile_task.sh
@@ -10,6 +10,7 @@ LD_FLAGS="-X github.com/pachyderm/pachyderm/src/client/version.AdditionalVersion
 DOCKER_OPTS=()
 DOCKER_OPTS+=("--build-arg=GO_VERSION=${GO_VERSION}")
 DOCKER_OPTS+=("--build-arg=LD_FLAGS=${LD_FLAGS}")
+DOCKER_OPTS+=("--build-arg=GC_FLAGS=${GC_FLAGS}")
 DOCKER_OPTS+=("--memory=3gb")
 
 if [[ "${OS}" == "Windows_NT" ]]; then
@@ -17,7 +18,7 @@ if [[ "${OS}" == "Windows_NT" ]]; then
   eval "$(minikube docker-env --shell bash)"
 fi
 
-DOCKER_BUILDKIT=1 docker build "${DOCKER_OPTS[@]}" --progress plain -t pachyderm_build "${REPO_DIR}"
+DOCKER_BUILDKIT=1 docker build "${DOCKER_OPTS[@]}" --progress plain -f Dockerfile.pachd -t pachyderm_build "${REPO_DIR}"
 docker build "${DOCKER_OPTS[@]}" -t pachyderm/pachd "${REPO_DIR}/etc/pachd"
 docker tag pachyderm/pachd pachyderm/pachd:local
 docker build "${DOCKER_OPTS[@]}" -t pachyderm/worker "${REPO_DIR}/etc/worker"
