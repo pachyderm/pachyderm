36b3afc17bae1488d4d9baf74b7ff09806bb1879
Kyle White <kylewhite@gmail.com>
2020-09-21 10:39:38 -0400

    Move rbac to it's own folder Set namespace to .Release.Namespace for rbac objects Add Rbac namespace tests

--- test/go.mod
+++ test/go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/gruntwork-io/terratest v0.30.0
 	k8s.io/api v0.18.3
+	k8s.io/client-go v0.18.3
 )
--- test/pachyderm_helm_test.go
+++ test/pachyderm_helm_test.go
@@ -4,8 +4,11 @@ import (
 	"testing"
 
 	appsv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
 
 	"github.com/gruntwork-io/terratest/modules/helm"
+	"github.com/gruntwork-io/terratest/modules/k8s"
 )
 
 func TestDashImageTag(t *testing.T) {
@@ -81,4 +84,92 @@ func TestPachdImageTag(t *testing.T) {
 	if deploymentContainers[0].Image != expectedContainerImage {
 		t.Fatalf("Rendered container image (%s) is not expected (%s)", deploymentContainers[0].Image, expectedContainerImage)
 	}
+}
+
+func TestSetNamespaceWorkerRoleBinding(t *testing.T) {
+
+	helmChartPath := "../pachyderm"
+
+	expectedNamespace := "kspace"
+
+	options := &helm.Options{
+		KubectlOptions: k8s.NewKubectlOptions("", "", expectedNamespace),
+	}
+
+	output := helm.RenderTemplate(t, options, helmChartPath, "rolebinding", []string{"templates/pachd/rbac/worker-rolebinding.yaml"})
+
+	var rolebinding rbacv1.RoleBinding
+	helm.UnmarshalK8SYaml(t, output, &rolebinding)
+
+	subjects := rolebinding.Subjects
+	if subjects[0].Namespace != expectedNamespace {
+		t.Fatalf("Rendered namespace (%s) is not expected (%s)", subjects[0].Namespace, expectedNamespace)
+	}
+
+}
+
+func TestSetNamespaceServiceAccount(t *testing.T) {
+
+	helmChartPath := "../pachyderm"
+
+	expectedNamespace := "kspace1234"
+
+	options := &helm.Options{
+		KubectlOptions: k8s.NewKubectlOptions("", "", expectedNamespace),
+	}
+
+	output := helm.RenderTemplate(t, options, helmChartPath, "serviceaccount", []string{"templates/pachd/rbac/serviceaccount.yaml"})
+
+	var serviceAccount v1.ServiceAccount
+	helm.UnmarshalK8SYaml(t, output, &serviceAccount)
+
+	metadata := serviceAccount.ObjectMeta
+
+	if metadata.Namespace != expectedNamespace {
+		t.Fatalf("Rendered namespace (%s) is not expected (%s)", metadata.Namespace, expectedNamespace)
+	}
+}
+
+func TestSetNamespaceWorkerServiceAccount(t *testing.T) {
+
+	helmChartPath := "../pachyderm"
+
+	expectedNamespace := "kspaceworker"
+
+	options := &helm.Options{
+		KubectlOptions: k8s.NewKubectlOptions("", "", expectedNamespace),
+	}
+
+	output := helm.RenderTemplate(t, options, helmChartPath, "workerserviceaccount", []string{"templates/pachd/rbac/worker-serviceaccount.yaml"})
+
+	var serviceAccount v1.ServiceAccount
+	helm.UnmarshalK8SYaml(t, output, &serviceAccount)
+
+	metadata := serviceAccount.ObjectMeta
+
+	if metadata.Namespace != expectedNamespace {
+		t.Fatalf("Rendered namespace (%s) is not expected (%s)", metadata.Namespace, expectedNamespace)
+	}
+}
+
+func TestSetNamespaceClusterRoleBinding(t *testing.T) {
+
+	helmChartPath := "../pachyderm"
+
+	expectedNamespace := "kspace123"
+
+	options := &helm.Options{
+		KubectlOptions: k8s.NewKubectlOptions("", "", expectedNamespace),
+	}
+
+	output := helm.RenderTemplate(t, options, helmChartPath, "clusterrolebinding", []string{"templates/pachd/rbac/clusterrolebinding.yaml"})
+
+	var rolebinding rbacv1.ClusterRoleBinding
+	helm.UnmarshalK8SYaml(t, output, &rolebinding)
+
+	subjects := rolebinding.Subjects
+	if subjects[0].Namespace != expectedNamespace {
+		t.Fatalf("Rendered namespace (%s) is not expected (%s)", subjects[0].Namespace, expectedNamespace)
+	}
+
 }
