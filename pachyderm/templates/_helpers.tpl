02358275a267195ba379713855efa4f65f380f10
Kyle White <kylewhite@gmail.com>
2020-09-25 18:19:18 -0400

    Checkpoint commit

--- pachyderm/templates/_helpers.tpl
+++ pachyderm/templates/_helpers.tpl
@@ -1,1 +1,6 @@
 {{/* vim: set filetype=mustache: */}}
+{{- define "imagePullSecret" }}
+{{- with .Values.imageCredentials }}
+{{- printf "{\"auths\":{\"%s\":{\"username\":\"%s\",\"password\":\"%s\",\"email\":\"%s\",\"auth\":\"%s\"}}}" .registry .username .password .email (printf "%s:%s" .username .password | b64enc) | b64enc }}
+{{- end }}
+{{- end }}
--- test/pachyderm_helm_test.go
+++ test/pachyderm_helm_test.go
@@ -1,6 +1,8 @@
 package helmtest
 
 import (
+	"errors"
+	"fmt"
 	"testing"
 
 	appsv1 "k8s.io/api/apps/v1"
@@ -68,6 +70,54 @@ func TestPachdImageTag(t *testing.T) {
 	if deploymentContainers[0].Image != expectedContainerImage {
 		t.Fatalf("Rendered container image (%s) is not expected (%s)", deploymentContainers[0].Image, expectedContainerImage)
 	}
+}
+
+func TestPachdImageTagDeploymentEnv(t *testing.T) {
+	helmChartPath := "../pachyderm"
+
+	expectedTag := "1234"
+	expectedContainerImage := "pachyderm/pachd:" + expectedTag
+	options := &helm.Options{
+		SetValues: map[string]string{"pachd.image.tag": expectedTag},
+	}
+
+	output := helm.RenderTemplate(t, options, helmChartPath, "deployment", []string{"templates/pachd/deployment.yaml"})
+
+	var deployment appsv1.Deployment
+	fmt.Printf("%+v\n", deployment)
+	helm.UnmarshalK8SYaml(t, output, &deployment)
+
+	deploymentContainers := deployment.Spec.Template.Spec.Containers
+
+	//fmt.Printf("%+v\n", deploymentContainers[0].Env)
+	pachdVersionEnvVar := "PACHD_VERSION"
+	err, version := GetEnvVarByName(deploymentContainers[0].Env, pachdVersionEnvVar)
+
+	if err != nil {
+		t.Fatalf("Could not find env var")
+	}
+	workerImageEnvVar := "WORKER_IMAGE"
+	err, workerImage := GetEnvVarByName(deploymentContainers[0].Env, workerImageEnvVar)
+	if err != nil {
+		t.Fatalf("Could not find env var")
+	}
+	workerSidecarImageEnvVar := "WORKER_SIDECAR_IMAGE"
+	err, workerSidecarImage := GetEnvVarByName(deploymentContainers[0].Env, workerSidecarImageEnvVar)
+	if err != nil {
+		t.Fatalf("Could not find env var")
+	}
+
+	if version != expectedTag {
+		t.Fatalf("Rendered EnvVar (%s) value (%s) is not expected (%s)", workerImageEnvVar, version, expectedTag)
+	}
+	if workerImage != expectedContainerImage {
+		t.Fatalf("Rendered EnvVar (%s) value (%s) is not expected (%s)", workerImageEnvVar, workerImage, expectedContainerImage)
+	}
+
+	if workerSidecarImage != expectedContainerImage {
+		t.Fatalf("Rendered EnvVar (%s) value (%s) is not expected (%s)", workerSidecarImageEnvVar, workerImage, expectedContainerImage)
+	}
+
 }
 
 func TestSetNamespaceWorkerRoleBinding(t *testing.T) {
@@ -157,3 +207,126 @@ func TestSetNamespaceClusterRoleBinding(t *testing.T) {
 	}
 
 }
+
+func TestAmazonStorageSecretsAmazonRegion(t *testing.T) {
+	/*
+		amazonRegion:
+		amazonBucket:
+		amazonID:
+		amazonSecret:
+		amazonToken:
+		amazonDistribution:
+		customEndpoint:
+		retries:
+		timeout:
+		uploadACL:
+		reverse:
+		partSize:
+		maxUploadParts:
+		disableSSL:
+		noVerifySSL:
+		#TODO Vault env vars
+		#TODO iamRole - Check all places IAM role rendered
+	*/
+	helmChartPath := "../pachyderm"
+	amazonRegion := "blah"
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"pachd.storage.backend":             "AMAZON",
+			"pachd.storage.amazon.amazonRegion": amazonRegion},
+	}
+
+	output := helm.RenderTemplate(t, options, helmChartPath, "secret", []string{"templates/pachd/storage-secret.yaml"})
+
+	var secret v1.Secret
+	helm.UnmarshalK8SYaml(t, output, &secret)
+
+	//TODO Check Result
+
+	//fmt.Printf("%+v\n", blah)
+	//fmt.Print(secret.Data["amazon-region"])
+}
+
+func TestMicrosoftStorageSecrets(t *testing.T) {
+
+	helmChartPath := "../pachyderm"
+	microsoftContainer := "blah"
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"pachd.storage.backend":                      "MICROSOFT",
+			"pachd.storage.microsoft.microsoftContainer": microsoftContainer},
+	}
+
+	output := helm.RenderTemplate(t, options, helmChartPath, "secret", []string{"templates/pachd/storage-secret.yaml"})
+
+	var secret v1.Secret
+	helm.UnmarshalK8SYaml(t, output, &secret)
+
+	//TODO Check Result
+
+	//fmt.Printf("%+v\n", blah)
+}
+
+func TestGoogleStorageSecrets(t *testing.T) {
+
+	helmChartPath := "../pachyderm"
+	googleBucket := "blah"
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"pachd.storage.backend":             "GOOGLE",
+			"pachd.storage.google.googleBucket": googleBucket},
+	}
+
+	output := helm.RenderTemplate(t, options, helmChartPath, "secret", []string{"templates/pachd/storage-secret.yaml"})
+
+	var secret v1.Secret
+	helm.UnmarshalK8SYaml(t, output, &secret)
+
+	//TODO Check result
+
+	//fmt.Printf("%+v\n", blah)
+}
+
+func TestMinioStorageSecrets(t *testing.T) {
+
+	helmChartPath := "../pachyderm"
+	minioBucket := "blah"
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"pachd.storage.backend":           "MINIO",
+			"pachd.storage.minio.minioBucket": minioBucket},
+	}
+
+	output := helm.RenderTemplate(t, options, helmChartPath, "secret", []string{"templates/pachd/storage-secret.yaml"})
+
+	var secret v1.Secret
+	helm.UnmarshalK8SYaml(t, output, &secret)
+
+	// TODO Check result
+
+	//fmt.Printf("%+v\n", blah)
+}
+
+func GetEnvVarByName(envVars []v1.EnvVar, name string) (error, string) {
+	for _, v := range envVars {
+		if v.Name == name {
+			return nil, v.Value
+		}
+	}
+	return errors.New("Not found"), ""
+}
+
+//Tests todo
+/*
+Overall
+- Check existence of values / non existence of values / overrides / defaults
+
+- Namespace correctly set
+- Service correctly wired through
+- Secrets correctly wired through
+
+TODO
+- Storage Class Tests
+
+
+*/
