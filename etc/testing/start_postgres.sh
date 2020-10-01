b19ac023c20432d3ad60c3b708df9481d636b919
Brendon Carroll <7072757+brendoncarroll@users.noreply.github.com>
2020-10-01 08:39:08 -0400

    Setup Postgres in CI for Storage V2 Tests (#5283)
    - Adds script for starting postgres docker container
    - Sets postgres version to 13.0

---
+++ etc/testing/start_postgres.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if ! docker ps | grep -q postgres
+then
+    echo "starting postgres..."
+    docker run -d \
+    -e POSTGRES_USER=pachyderm \
+    -e POSTGRES_DB=pgc \
+    -e POSTGRES_HOST_AUTH_METHOD=trust \
+    -p 32228:5432 \
+    postgres:13.0-alpine
+else
+    echo "postgres already started"
+fi
