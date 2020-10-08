01a2f08aafec517b432bf5faf5e7a4783461a0b4
Luke Marsden <me@lukemarsden.net>
2020-10-08 17:50:08 +0100

    comment out unused import

--- src/server/pps/server/s3g_sidecar.go
+++ src/server/pps/server/s3g_sidecar.go
@@ -25,7 +25,7 @@ import (
 
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	//"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 const (
