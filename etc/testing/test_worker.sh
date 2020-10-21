99a9d9eef384d2a4d8353e423ddc90aea6ea1373
Matthew Steffen <msteffen@pachyderm.io>
2020-10-21 13:05:53 -0700

    Strip log output from datum tests

---
+++ etc/testing/test_worker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euxo pipefail
+
+export TIMEOUT="${1}"
+export PROM_PORT="$(
+  kubectl --namespace=monitoring get svc/prometheus -o json \
+    | jq -r .spec.ports[0].nodePort
+)"
+
+# grep out PFS server logs, as otherwise the test output is too verbose to
+# follow and breaks travis
+go test -v -count=1 ./src/server/worker/... -timeout "${TIMEOUT}" \
+  | grep -v "$(date +^%FT)"
