ac18602ab6f773109b945d8cf081f0633ad1d663
Matthew Steffen <msteffen@pachyderm.io>
2020-10-20 23:39:25 -0700

    Fix hashtree_test & mv relevant tests to path_test

--- src/server/pkg/path/path_test.go
+++ src/server/pkg/path/path_test.go
@@ -8,17 +8,17 @@ import (
 
 func TestClean(t *testing.T) {
 	for input, expected := range map[string]string{
-		"/":          "",
-		".":          "",
-		"/.":         "",
-		"foo/":       "/foo",
-		"foo/.":      "/foo",
-		"/foo/":      "/foo",
-		"/foo/.":     "/foo",
-		"/foo/..":    "",
-		"/foo/../..": "",
-		"/(*)":       "/(*)",
-		"/(*)/":      "/(*)",
+		`/`:          ``,
+		`.`:          ``,
+		`/.`:         ``,
+		`foo/`:       `/foo`,
+		`foo/.`:      `/foo`,
+		`/foo/`:      `/foo`,
+		`/foo/.`:     `/foo`,
+		`/foo/..`:    ``,
+		`/foo/../..`: ``,
+		`/(*)`:       `/(*)`,
+		`/(*)/`:      `/(*)`,
 	} {
 		require.Equal(t, expected, Clean(input), "for %q", input)
 	}
@@ -26,17 +26,17 @@ func TestClean(t *testing.T) {
 
 func TestBase(t *testing.T) {
 	for input, expected := range map[string]string{
-		"/":          "",
-		".":          "",
-		"foo/":       "foo",
-		"foo/.":      "foo", // pre-refactor, was ""
-		"/foo/":      "foo",
-		"/foo/.":     "foo",
-		"/foo/..":    "",
-		"/foo/../..": "",
-		"/(*)":       "(*)",
-		"/foo/bar":   "bar",
-		"/foo/*":     "*",
+		`/`:          ``,
+		`.`:          ``,
+		`foo/`:       `foo`,
+		`foo/.`:      `foo`, // pre-refactor, was ""
+		`/foo/`:      `foo`,
+		`/foo/.`:     `foo`,
+		`/foo/..`:    ``,
+		`/foo/../..`: ``,
+		`/(*)`:       `(*)`,
+		`/foo/bar`:   `bar`,
+		`/foo/*`:     `*`,
 	} {
 		require.Equal(t, expected, Base(input), "for %q", input)
 	}
@@ -44,17 +44,17 @@ func TestBase(t *testing.T) {
 
 func TestDir(t *testing.T) {
 	for input, expected := range map[string]string{
-		"/":          "",
-		".":          "",
-		"foo/":       "", // pre-refactor, was "/foo"
-		"foo/.":      "",
-		"/foo/":      "",
-		"/foo/.":     "",
-		"/foo/..":    "",
-		"/foo/../..": "",
-		"/(*)":       "",
-		"/foo/bar":   "/foo",
-		"/foo/*":     "/foo",
+		`/`:          ``,
+		`.`:          ``,
+		`foo/`:       ``, // pre-refactor, was "/foo"
+		`foo/.`:      ``,
+		`/foo/`:      ``,
+		`/foo/.`:     ``,
+		`/foo/..`:    ``,
+		`/foo/../..`: ``,
+		`/(*)`:       ``,
+		`/foo/bar`:   `/foo`,
+		`/foo/*`:     `/foo`,
 	} {
 		require.Equal(t, expected, Dir(input), "for %q", input)
 	}
@@ -62,17 +62,17 @@ func TestDir(t *testing.T) {
 
 func TestSplit(t *testing.T) {
 	for input, expected := range map[string]struct{ base, dir string }{
-		"/":          {"", ""},
-		".":          {"", ""},
-		"foo/":       {"", "foo"}, // pre-refactor, was {"foo", "foo"}
-		"foo/.":      {"", "foo"}, // pre-refactor, was {"/foo", ""}
-		"/foo/":      {"", "foo"},
-		"/foo/.":     {"", "foo"},
-		"/foo/..":    {"", ""},
-		"/foo/../..": {"", ""},
-		"/(*)":       {"", "(*)"},
-		"/foo/bar":   {"/foo", "bar"},
-		"/foo/*":     {"/foo", "*"},
+		`/`:          {``, ``},
+		`.`:          {``, ``},
+		`foo/`:       {``, `foo`}, // pre-refactor, was {"foo", "foo"}
+		`foo/.`:      {``, `foo`}, // pre-refactor, was {"/foo", ""}
+		`/foo/`:      {``, `foo`},
+		`/foo/.`:     {``, `foo`},
+		`/foo/..`:    {``, ``},
+		`/foo/../..`: {``, ``},
+		`/(*)`:       {``, `(*)`},
+		`/foo/bar`:   {`/foo`, `bar`},
+		`/foo/*`:     {`/foo`, `*`},
 	} {
 		b, d := Split(input)
 		require.Equal(t, expected, struct{ base, dir string }{b, d}, "for %q", input)
@@ -81,18 +81,31 @@ func TestSplit(t *testing.T) {
 
 func TestIsGlob(t *testing.T) {
 	for input, expected := range map[string]bool{
-		"/":           false,
-		".":           false,
-		"/foo/":       false,
-		"/foo/.":      false,
-		"/foo/..":     false,
-		"/foo/../..":  false,
-		"(*)/":        true,
-		"/(*)":        true,
-		"/foo/bar":    false,
-		"/foo/*":      true,
-		"/foo-?/":     true,
-		"/foo-(a|b)/": true,
+		`/`:           false,
+		`.`:           false,
+		`/foo/`:       false,
+		`/foo/.`:      false,
+		`/foo/..`:     false,
+		`/foo/../..`:  false,
+		`(*)/`:        true,
+		`/(*)`:        true,
+		`/foo/bar`:    false,
+		`/foo/*`:      true,
+		`/foo-?/`:     true,
+		`/foo-(a|b)/`: true,
+
+		// These are carried over from hashtree/hashtree_test.go
+		`*`:                       true,
+		`path/to*/file`:           true,
+		`path/**/file`:            true,
+		`path/to/f?le`:            true,
+		`pa!h/to/file`:            true,
+		`pa[th]/to/file`:          true,
+		`pa{th}/to/file`:          true,
+		`*/*`:                     true,
+		`path`:                    false,
+		`path/to/file1.txt`:       false,
+		`path/to_test-a/file.txt`: false,
 	} {
 		require.Equal(t, expected, IsGlob(input), "for %q", input)
 	}
@@ -100,19 +113,37 @@ func TestIsGlob(t *testing.T) {
 
 func TestGlobLiteralPrefix(t *testing.T) {
 	for input, expected := range map[string]string{
-		"/":           "",
-		".":           "",
-		"/foo/":       "/foo",
-		"/foo/.":      "/foo",
-		"/foo/..":     "",
-		"/foo/../..":  "",
-		"/(*)":        "/",
-		"(*)/":        "/",
-		"/foo/bar":    "/foo/bar",
-		"/foo/*":      "/foo/",
-		"/foo-?/":     "/foo-",
-		"/foo-(a|b)/": "/foo-",
+		`/`:           ``,
+		`.`:           ``,
+		`/foo/`:       `/foo`,
+		`/foo/.`:      `/foo`,
+		`/foo/..`:     ``,
+		`/foo/../..`:  ``,
+		`/(*)`:        `/`,
+		`(*)/`:        `/`,
+		`/foo/bar`:    `/foo/bar`,
+		`/foo/*`:      `/foo/`,
+		`/foo-?/`:     `/foo-`,
+		`/foo-(a|b)/`: `/foo-`,
+
+		// These are carried over from hashtree/hashtree_test.go
+		`*`:             `/`,
+		`**`:            `/`,
+		`dir/*`:         `/dir/`,
+		`dir/**`:        `/dir/`,
+		`dir/(*)`:       `/dir/`,
+		`di?/(*)`:       `/di`,
+		`di?[rg]`:       `/di`,
+		`dir/@(a)`:      `/dir/`,
+		`dir/+(a)`:      `/dir/`,
+		`dir/{foo,bar}`: `/dir/`,
+		`dir/(a|b)`:     `/dir/`,
+		`dir/^[a-z]`:    `/dir/`,
+		`dir/[!abc]`:    `/dir/`,
 	} {
 		require.Equal(t, expected, GlobLiteralPrefix(input), "for %q", input)
 	}
+}
+
+func TestGlobPrefix(t *testing.T) {
 }
