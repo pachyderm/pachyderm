78c97cec96ddca988ebee6a397a193dce1a8b5f8
Matthew Steffen <msteffen@pachyderm.io>
2020-10-22 07:44:02 -0700

    Fix lint error

--- etc/testing/test_worker.sh
+++ etc/testing/test_worker.sh
@@ -2,10 +2,11 @@
 set -euxo pipefail
 
 export TIMEOUT="${1}"
-export PROM_PORT="$(
+PROM_PORT="$(
   kubectl --namespace=monitoring get svc/prometheus -o json \
     | jq -r .spec.ports[0].nodePort
 )"
+export PROM_PORT
 
 # grep out PFS server logs, as otherwise the test output is too verbose to
 # follow and breaks travis
