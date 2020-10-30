38cdea9508c750ed7b2786e0d6e168966aa27332
nitinjainsj <nitin@pachyderm.com>
2020-10-29 21:01:43 -0700

    Add goreleaser for doing point/custom/release-candidate builds. Also support build and push of docker images using goreleaser

---
+++ etc/build/make_release.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+make VERSION_ADDITIONAL=$VERSION_ADDITIONAL install-clean
+version="$("$GOPATH/bin/pachctl" version --client-only)"
+
+echo "--- Releasing Version: $version"
+make VERSION=$version VERSION_ADDITIONAL=$VERSION_ADDTIONAL release
