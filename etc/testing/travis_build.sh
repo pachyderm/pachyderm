5e4f4256d2e1f5b7900c9c0c6527d983e3c91f22
nitinjainsj <nitin@pachyderm.com>
2020-11-03 23:02:05 -0800

    Correct git tag

--- etc/testing/travis_build.sh
+++ etc/testing/travis_build.sh
@@ -7,7 +7,7 @@ if [[ "$TRAVIS_SECURE_ENV_VARS" == "true" ]]; then
     docker login -u pachydermbuildbot -p "${DOCKER_PWD}"
     make install
     version=$(pachctl version --client-only)
-    git tag -f -am "Travis test v$(version)" v$(version)
+    git tag -f -am "Travis test v${version}" v${version}
     make docker-build
     docker tag "pachyderm/pachd:local" "pachyderm/pachd:${version}"
     docker push "pachyderm/pachd:${version}"
