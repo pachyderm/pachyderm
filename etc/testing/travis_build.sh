2cca3e61e5d05cbdf56559a78db64ac738d54981
nitinjainsj <nitin@pachyderm.com>
2020-11-03 22:53:56 -0800

    Set git tag version in test -- needed to goreleaser

--- etc/testing/travis_build.sh
+++ etc/testing/travis_build.sh
@@ -5,8 +5,10 @@ set -ex
 # We can't run the build step if there's no access to the secret env vars
 if [[ "$TRAVIS_SECURE_ENV_VARS" == "true" ]]; then
     docker login -u pachydermbuildbot -p "${DOCKER_PWD}"
-    make install docker-build
+    make install
     version=$(pachctl version --client-only)
+    git tag -f -am "Travis test v$(version)" v$(version)
+    make docker-build
     docker tag "pachyderm/pachd:local" "pachyderm/pachd:${version}"
     docker push "pachyderm/pachd:${version}"
     docker tag "pachyderm/worker:local" "pachyderm/worker:${version}"
