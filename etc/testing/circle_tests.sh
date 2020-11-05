fd30692a342c7344bd02757925248f64186b988d
Luke Marsden <me@lukemarsden.net>
2020-11-05 22:20:50 +0000

    Some tidy-up.

--- etc/testing/circle_tests.sh
+++ etc/testing/circle_tests.sh
@@ -2,9 +2,6 @@
 
 set -euo pipefail
 
-# In case we're retrying on a new cluster
-rm -f "${HOME}"/.pachyderm/config.json
-
 # Get a kubernetes cluster
 # Specify the slot so that future builds on this branch+suite id automatically
 # clean up previous VMs
@@ -20,18 +17,11 @@ echo "===================="
 KUBECONFIG="$(pwd)/kubeconfig"
 export KUBECONFIG
 
-# [x]: get docker image over there (well, we did already)
-# we assume 'make docker-build' has been done by a previous build step. see .circleci/config.yml
-
-# [ ]: send files across TODO: make this use rsync --delete
 echo "Copying context to runner."
 time ./etc/testing/testctl-rsync.sh . /root/project
 echo "Finished copying context."
 
-# [x]: pass environment variables through, at least ENT_ACT_CODE, BUCKET
-# [x]: pass arguments over
-
-# workaround https://serverfault.com/questions/482907/setting-a-variable-for-a-given-ssh-host
+# NB: https://serverfault.com/questions/482907/setting-a-variable-for-a-given-ssh-host
 
 echo "Starting test $BUCKET."
 time ./etc/testing/testctl-ssh.sh \
