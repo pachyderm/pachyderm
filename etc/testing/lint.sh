8d0d08a77d62ce022885b26e42d18c6ec4a4f345
Matthew Steffen <msteffen@pachyderm.io>
2020-11-12 00:51:09 -0800

    Fix lint so it runs in CI

--- etc/testing/lint.sh
+++ etc/testing/lint.sh
@@ -3,18 +3,30 @@
 set -e
 
 GIT_REPO_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" 2>&1 > /dev/null && git rev-parse --show-toplevel)
+cd "${GIT_REPO_DIR}"
+
+# Collect untracked args into '\( -o -path abc -o -path def -o ... \) -prune'
+# argument to be passed to 'find'
+skip_paths=()
+for file in $(git status --porcelain | grep '^??' | sed 's/^?? //'); do
+  # Convert to relative path, as required by -path
+  file="./$(realpath --relative-to="." "${file}")"
+  # 1. Always prefix with -o (i.e. don't fencepost; find always has an argument
+  #    before skip_paths, so including the initial -o works whether skip_paths
+  #    is empty or not
+  # 2. strip trailing slash as required by -path
+  skip_paths+=( -o -path "${file%/}" )
+done
 
 go get -u golang.org/x/lint/golint
-for file in $(find "${GIT_REPO_DIR}/src" -name '*.go' | grep -v '\.pb\.go'); do
-    if [[ $file == *fileset/tar* ]]; then
-        continue
-    fi
+find "./src" \
+  \( -path "*.pb.go" -o -path "*fileset/tar*" "${skip_paths[@]}" \) -prune -o -name '*.go' -print \
+| while read -r file; do
     golint -set_exit_status "$file";
 done;
 
 files=$(gofmt -l "${GIT_REPO_DIR}/src" || true)
-
-if [[ $files ]]; then
+if [[ -n "${files}" ]]; then
     echo Files not passing gofmt:
     tr ' ' '\n'  <<< "$files"
     exit 1
@@ -22,5 +34,10 @@ fi
 
 go get honnef.co/go/tools/cmd/staticcheck
 staticcheck "${GIT_REPO_DIR}/..."
+
 # shellcheck disable=SC2046
-shellcheck -e SC2010 -e SC2181 -e SC2004 -e SC2219 $(find . -path ./etc/plugin -prune -o -name "*.sh" -print)
+find . \
+  \( -path ./etc/plugin "${skip_paths[@]}" \) -prune -o -name "*.sh" -print \
+| while read -r file; do
+    shellcheck -e SC1091 -e SC2010 -e SC2181 -e SC2004 -e SC2219 "${file}"
+done
