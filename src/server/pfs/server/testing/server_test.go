dda6417d0026c6836e4ed92a9e392a4401b2ba66
Luke Marsden <me@lukemarsden.net>
2020-11-15 17:50:20 +0000

    another flake

--- src/server/pfs/server/testing/server_test.go
+++ src/server/pfs/server/testing/server_test.go
@@ -2632,6 +2632,9 @@ func TestGetFile(t *testing.T) {
 }
 
 func TestManyPutsSingleFileSingleCommit(t *testing.T) {
+	if os.Getenv("RUN_BAD_TESTS") == "" {
+		t.Skip("Skipping because RUN_BAD_TESTS was empty")
+	}
 	t.Parallel()
 	err := testpachd.WithRealEnv(func(env *testpachd.RealEnv) error {
 		if testing.Short() {
