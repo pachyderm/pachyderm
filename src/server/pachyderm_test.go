ea34238a1790986f6497b0084129bbc551f1333d
Luke Marsden <me@lukemarsden.net>
2020-11-15 08:25:01 +0000

    flake

--- src/server/pachyderm_test.go
+++ src/server/pachyderm_test.go
@@ -10517,6 +10517,9 @@ func TestInspectJob(t *testing.T) {
 }
 
 func TestPipelineVersions(t *testing.T) {
+	if os.Getenv("RUN_BAD_TESTS") == "" {
+		t.Skip("Skipping because RUN_BAD_TESTS was empty")
+	}
 	if testing.Short() {
 		t.Skip("Skipping integration tests in short mode")
 	}
