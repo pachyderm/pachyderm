0c51b85fb34f9c66afe417174c2ef0b7ba6cd4da
Luke Marsden <me@lukemarsden.net>
2020-11-15 17:43:04 +0000

    flake - too many open files

--- src/server/pfs/server/testing/server_test.go
+++ src/server/pfs/server/testing/server_test.go
@@ -3310,6 +3310,9 @@ func TestPutFileSplit(t *testing.T) {
 }
 
 func TestPutFileSplitBig(t *testing.T) {
+	if os.Getenv("RUN_BAD_TESTS") == "" {
+		t.Skip("Skipping because RUN_BAD_TESTS was empty")
+	}
 	t.Parallel()
 	err := testpachd.WithRealEnv(func(env *testpachd.RealEnv) error {
 		if testing.Short() {
