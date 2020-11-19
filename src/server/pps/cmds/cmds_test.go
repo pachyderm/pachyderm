49079caf3cadc760465437e348fe5d60f139da24
Luke Marsden <me@lukemarsden.net>
2020-11-19 11:35:45 +0000

    Another flake

--- src/server/pps/cmds/cmds_test.go
+++ src/server/pps/cmds/cmds_test.go
@@ -333,6 +333,10 @@ func TestRunPipeline(t *testing.T) {
 
 // TestYAMLPipelineSpec tests creating a pipeline with a YAML pipeline spec
 func TestYAMLPipelineSpec(t *testing.T) {
+	if os.Getenv("RUN_BAD_TESTS") == "" {
+		t.Skip("Skipping because RUN_BAD_TESTS was empty")
+	}
+
 	if testing.Short() {
 		t.Skip("Skipping integration tests in short mode")
 	}
