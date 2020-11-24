253faa6fb37625de44c1cae8d85594bb9df96d59
A Gardner <3100188+actgardner@users.noreply.github.com>
2020-11-24 10:20:35 -0500

    Explicitly set blocking factor for tar on stdout

--- etc/proto/build.sh
+++ etc/proto/build.sh
@@ -2,6 +2,6 @@
 # This script build protos using the pachyderm_proto image.
 
 find src -regex ".*\.proto" -print0 \
-| xargs -0 tar cf - \
+| xargs -0 tar cf - -b20 \
 | docker run -i pachyderm_proto \
-| tar xf -
+| tar xf - -b20
