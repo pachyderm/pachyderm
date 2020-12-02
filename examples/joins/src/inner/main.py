2abf521374e2901bf913fbfbec5a33928fc6db25
Nadege PEPIN <nadege.pepin@pachyderm.io>
2020-12-02 10:50:51 -0500

    added outer join example source code

--- examples/joins/src/inner/main.py
+++ examples/joins/src/inner/main.py
@@ -1,21 +1,28 @@
 #!/usr/local/bin/python3
 
-import glob, os, json, shutil
+import glob, json, os, shutil, sys
 
 
-store_file  = glob.glob(os.path.join("/pfs/stores", "*.txt"))[0]
-purchase_file = glob.glob(os.path.join("/pfs/purchases", "*.txt"))[0]
+store_path  = glob.glob(os.path.join("/pfs/stores", "*.txt"))[0]
+purchase_path = glob.glob(os.path.join("/pfs/purchases", "*.txt"))[0]
 
-print("Opening store_file...: " + store_file)
+print("Opening store_file...: " + store_path)
 
-with open(store_file, 'r') as store_json:    
+with open(store_path, 'r') as store_json:    
     store = json.load(store_json)
-    zipcode = store['address']['postal']
+    zipcode = store['address']['zipcode']
     print("zipcode: " + zipcode)
-    print("Copying : " + purchase_file + " to: " + "/pfs/out/"+zipcode+"/")
-    #os.makedirs("/pfs/out/"+zipcode+"/", exist_ok=True)
-    #shutil.copy2(purchase_file, "/pfs/out/"+zipcode+"/") 
-
-    shutil.copy(purchase_file, "/pfs/out/"+zipcode+".txt") 
+    
+    with open("/pfs/out/"+zipcode+".txt", 'w') as location_file:  
+        # Add a text separator to identify in what store the purchase was made
+        separator_line = "\nPurchase at store: "+ str(store["storeid"]) +" - "+ store["name"]+" \n"    
+        location_file.write(separator_line)
   
+        print("Appending : " + purchase_path + " to:" + "/pfs/out/"+zipcode+".txt")
+        # Copy the content of the purchase file into the corresponding zipcode file
+        with  open(purchase_path, 'r') as purchase_file:
+            location_file.write(purchase_file.read()) 
+
+
+
 
