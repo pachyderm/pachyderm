3d29024bffa629616e0906ef857aad526a088186
Matthew Steffen <msteffen@pachyderm.io>
2020-12-02 23:03:31 -0800

    remove 'set -e'; this only runs in err case

--- etc/testing/kube_debug.sh
+++ etc/testing/kube_debug.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 echo "=== TEST FAILED OR TIMED OUT, DUMPING DEBUG INFO ==="
 
 # TODO: Extend this to show kubectl describe output for failed pods, this will
@@ -14,9 +12,9 @@ cmds=(
   'kubectl describe pod -l suite=pachyderm,app=etcd'
   # Set --tail b/c by default 'kubectl logs' only outputs 10 lines if -l is set
   'kubectl logs --tail=500 -l suite=pachyderm,app=pachd'
-  'kubectl logs --tail=1000 -l suite=pachyderm,app=pachd --previous || true # if pachd restarted'
+  'kubectl logs --tail=1000 -l suite=pachyderm,app=pachd --previous # if pachd restarted'
   'sudo dmesg | tail -n 40'
-  '{ minikube logs | tail -n 100; } || true'
+  'minikube logs | tail -n 100'
   'top -b -n 1 | head -n 40'
   'df -h'
 )
