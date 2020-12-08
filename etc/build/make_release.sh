ac0ab1e75db1c48cb3926e5e926432e23fda286e
nitinjainsj <nitin@pachyderm.com>
2020-12-07 19:10:06 -0800

    Fix VERSION_ADDITIONAL in release

--- etc/build/make_release.sh
+++ etc/build/make_release.sh
@@ -6,4 +6,4 @@ make VERSION_ADDITIONAL="$VERSION_ADDITIONAL" install-clean
 version="$("$GOPATH/bin/pachctl" version --client-only)"
 
 echo "--- Releasing Version: $version"
-make VERSION="$version" VERSION_ADDITIONAL="$VERSION_ADDTIONAL" release
+make VERSION="$version" VERSION_ADDITIONAL="$VERSION_ADDITIONAL" release
