797ddb463f0fc4ff4c39a4234842d8a95f5eed90
Matthew Steffen <msteffen@pachyderm.io>
2021-01-05 22:57:27 -0800

    Use exponential backoff around k8s watch

--- src/server/pps/server/poller.go
+++ src/server/pps/server/poller.go
@@ -218,7 +218,7 @@ func (m *ppsMaster) pollPipelinePods(pollClient *client.APIClient) {
 			}
 		}
 		return backoff.ErrContinue // keep polling until cancelled (RetryUntilCancel)
-	}, &backoff.ZeroBackOff{}, backoff.NotifyContinue("pollPipelinePods"),
+	}, backoff.NewExponentialBackOff(), backoff.NotifyContinue("pollPipelinePods"),
 	); err != nil && ctx.Err() == nil {
 		panic("pollPipelinePods is exiting prematurely which should not happen; restarting pod...")
 	}
