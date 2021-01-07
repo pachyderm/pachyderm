6a4a028b11639947a1fdba5b15b499686fa97aa5
Nadege PEPIN <nadegepepin@users.noreply.github.com>
2021-01-07 16:08:00 -0500

    An intro-level example of spout 2.0 (#5556)
    * first version
    
    * Readme creation
    
    * typo in readme
    
    * Update README.md
    
    Minor typo/omissions
    
    * Update examples/spouts/spout101/README.md
    
    Co-authored-by: Joey Zwicker <joey@pachyderm.io>
    
    * Update examples/spouts/spout101/README.md
    
    Co-authored-by: Joey Zwicker <joey@pachyderm.io>
    
    * Update examples/spouts/spout101/README.md
    
    Co-authored-by: Joey Zwicker <joey@pachyderm.io>
    
    * Update examples/spouts/spout101/README.md
    
    Co-authored-by: Joey Zwicker <joey@pachyderm.io>
    
    * update according to feed back
    
    * typo
    
    * docker image version changed to 1 and python 'print' clean up
    
    * added comment for connection
    
    * updated example with call to upgraded python-pachyderm Client.new_from_config() function
    
    Co-authored-by: Joey Zwicker <joey@pachyderm.io>

---
+++ examples/spouts/spout101/src/consumer/main.py
@@ -0,0 +1,41 @@
+#!/usr/local/bin/python3
+from python_pachyderm import Client
+import os
+import json
+import hashlib
+from random import randint
+from time import sleep
+
+# Emulates the reception of messages from a third party messaging system or queue (such as AWS SQS, Kafka, Google Pub/Sub etc...)
+
+
+def receive_message():
+    # Emulates a network response time to poll new messages
+    sleep(randint(10, 30))
+    # Creates a random string of 1KB
+    random1 = os.urandom(1024)
+    random2 = os.urandom(2048)
+    return (random1, random2)
+
+# Polls data from a third party messaging system or queue and push them to a Pachyderm repo in a transaction.
+
+
+def polling_consumer():
+
+    while True:
+        # Polls queue
+        msgs = receive_message()
+        if msgs:
+            print("connecting to pachd")
+            client = Client.new_from_config()
+            print("connected")
+            
+            with client.commit('spout', 'master') as c:
+                for msg in msgs:
+                    # hash the file to assign unique name
+                    filename = hashlib.sha256(msg).hexdigest() + ".txt"
+                    client.put_file_bytes(c, filename, msg)
+
+
+if __name__ == '__main__':
+    polling_consumer()
