260fb784cdb1620b5e5d4b1fc73d3d17a2e1301b
Steve Manuel <nilslice@gmail.com>
2021-01-07 13:42:32 -0700

    etc: install protolock and invoke before building protos

--- etc/proto/build.sh
+++ etc/proto/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-# This script build protos using the pachyderm_proto image.
+# This script builds protos using the pachyderm_proto image.
+# Before generating any code, it verifies that no backward-incompatible changes have been introduced 
+# to the .proto definition files.
+
+set -e
+
+protolock status
 
 find src -regex ".*\.proto" -print0 \
 | xargs -0 tar cf - \
