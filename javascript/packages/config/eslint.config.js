7870fc13802b915c5575cff098a687a1e9c56fb2
Cam <cpepin@fastmail.com>
2021-01-14 17:49:16 -0500

    feat(2309): Adding DAG resolver to the Dash API (#2315)

--- javascript/packages/config/eslint.config.js
+++ javascript/packages/config/eslint.config.js
@@ -75,7 +75,7 @@ module.exports = {
   settings: {
     react: {
       pragma: 'React',
-      version: 'detect',
+      version: 'latest',
     },
     'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
     'import/parsers': {
