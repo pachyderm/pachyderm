09cf6e67d0606c71bf29996aa00fa7b3e0e0d2e7
Nadege PEPIN <nadege.pepin@pachyderm.io>
2021-02-05 18:20:53 -0500

    new docker build

--- examples/word_count/build_tag_deploy.sh
+++ examples/word_count/build_tag_deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 export DOCKER_BUILDKIT=1
 export COMPOSE_DOCKER_CLI_BUILD=1
-export v=1
+export v=3
 # Build your image
-docker image build -t word-count:${v} .
+docker image build --platform=linux/amd64 -t word-count:${v} .
 # Tag your image -> repl
 docker tag word-count:${v} npepin/word-count:${v}
 # Push your image on Docker hub
