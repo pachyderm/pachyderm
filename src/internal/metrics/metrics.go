9d1c9284a1749bf426a65c6fa6cbe78e82d0fa30
nitinjainsj <nitin@pachyderm.com>
2021-02-09 13:52:55 -0800

    Add pipeline metrics
    Fix lint issues
    
    Add couple of metrics and logs
    
    Add additional stats

--- src/internal/metrics/metrics.go
+++ src/internal/metrics/metrics.go
@@ -161,21 +161,164 @@ func externalMetrics(kubeClient *kube.Clientset, metrics *Metrics) error {
 	return nil
 }
 
-func internalMetrics(pachClient *client.APIClient, metrics *Metrics) error {
+func internalMetrics(pachClient *client.APIClient, metrics *Metrics) {
+
+	// We should not return due to an error
+
+	// Activation code
 	enterpriseState, err := pachClient.Enterprise.GetState(pachClient.Ctx(), &enterprise.GetStateRequest{})
-	if err != nil {
-		return err
+	if err == nil {
+		metrics.ActivationCode = enterpriseState.ActivationCode
 	}
-	metrics.ActivationCode = enterpriseState.ActivationCode
+
+	// Pipeline info
 	resp, err := pachClient.PpsAPIClient.ListPipeline(pachClient.Ctx(), &pps.ListPipelineRequest{AllowIncomplete: true})
-	if err != nil {
-		return err
+	if err == nil {
+		metrics.Pipelines = int64(len(resp.PipelineInfo)) // Number of pipelines
+		for _, pi := range resp.PipelineInfo {
+			if pi.ParallelismSpec != nil {
+				if metrics.MaxParallelism < pi.ParallelismSpec.Constant {
+					metrics.MaxParallelism = pi.ParallelismSpec.Constant
+				}
+				if metrics.MinParallelism > pi.ParallelismSpec.Constant {
+					metrics.MinParallelism = pi.ParallelismSpec.Constant
+				}
+				metrics.NumParallelism += 1
+			}
+			if pi.Egress != nil {
+				metrics.CfgEgress += 1
+			}
+			if pi.JobCounts != nil {
+				var cnt int64 = 0
+				for _, c := range pi.JobCounts {
+					cnt += int64(c)
+				}
+				if metrics.Jobs < cnt {
+					metrics.Jobs = cnt
+				}
+			}
+			if pi.ResourceRequests != nil {
+				if pi.ResourceRequests.Cpu != 0 {
+					metrics.ResourceCpuReq += pi.ResourceRequests.Cpu
+					if metrics.ResourceCpuReqMax < pi.ResourceRequests.Cpu {
+						metrics.ResourceCpuReqMax = pi.ResourceRequests.Cpu
+					}
+				}
+				if pi.ResourceRequests.Memory != "" {
+					metrics.ResourceMemReq += (pi.ResourceRequests.Memory + " ")
+				}
+				if pi.ResourceRequests.Gpu != nil {
+					metrics.ResourceGpuReq += pi.ResourceRequests.Gpu.Number
+					if metrics.ResourceGpuReqMax < pi.ResourceRequests.Gpu.Number {
+						metrics.ResourceGpuReqMax = pi.ResourceRequests.Gpu.Number
+					}
+				}
+				if pi.ResourceRequests.Disk != "" {
+					metrics.ResourceDiskReq += (pi.ResourceRequests.Disk + " ")
+				}
+			}
+			if pi.ResourceLimits != nil {
+				if pi.ResourceLimits.Cpu != 0 {
+					metrics.ResourceCpuLimit += pi.ResourceLimits.Cpu
+					if metrics.ResourceCpuLimitMax < pi.ResourceLimits.Cpu {
+						metrics.ResourceCpuLimitMax = pi.ResourceLimits.Cpu
+					}
+				}
+				if pi.ResourceLimits.Memory != "" {
+					metrics.ResourceMemLimit += (pi.ResourceLimits.Memory + " ")
+				}
+				if pi.ResourceLimits.Gpu != nil {
+					metrics.ResourceGpuLimit += pi.ResourceLimits.Gpu.Number
+					if metrics.ResourceGpuLimitMax < pi.ResourceLimits.Gpu.Number {
+						metrics.ResourceGpuLimitMax = pi.ResourceLimits.Gpu.Number
+					}
+				}
+				if pi.ResourceLimits.Disk != "" {
+					metrics.ResourceDiskLimit += (pi.ResourceLimits.Disk + " ")
+				}
+			}
+			if pi.Input != nil {
+				if pi.Input.Pfs.OuterJoin {
+					metrics.InputOuterJoin += 1
+				}
+				if pi.Input.Pfs.Lazy {
+					metrics.InputLazy += 1
+				}
+				if pi.Input.Pfs.EmptyFiles {
+					metrics.InputEmptyFiles += 1
+				}
+				if pi.Input.Pfs.S3 {
+					metrics.InputS3 += 1
+				}
+				if pi.Input.Pfs.Trigger != nil {
+					metrics.InputTrigger += 1
+				}
+				if pi.Input.Join != nil {
+					metrics.InputJoin += 1
+				}
+				if pi.Input.Group != nil {
+					metrics.InputGroup += 1
+				}
+				if pi.Input.Cross != nil {
+					metrics.InputCross += 1
+				}
+				if pi.Input.Union != nil {
+					metrics.InputUnion += 1
+				}
+				if pi.Input.Cron != nil {
+					metrics.InputCron += 1
+				}
+				if pi.Input.Git != nil {
+					metrics.InputGit += 1
+				}
+			}
+			if pi.EnableStats {
+				metrics.CfgStats += 1
+			}
+			if pi.Service != nil {
+				metrics.CfgServices += 1
+			}
+			if pi.Spout != nil {
+				metrics.PpsSpout += 1
+				if pi.Spout.Service != nil {
+					metrics.PpsSpoutService += 1
+				}
+			}
+			if pi.Standby {
+				metrics.CfgStandby += 1
+			}
+			if pi.S3Out {
+				metrics.CfgS3Gateway += 1
+			}
+			if pi.Transform != nil {
+				if pi.Transform.ErrCmd != nil {
+					metrics.CfgErrcmd += 1
+				}
+				if pi.Transform.Build != nil {
+					metrics.PpsBuild += 1
+				}
+			}
+			if pi.TFJob != nil {
+				metrics.CfgTfjob += 1
+			}
+		}
 	}
-	metrics.Pipelines = int64(len(resp.PipelineInfo))
+
 	ris, err := pachClient.ListRepo()
-	if err != nil {
-		return err
+	if err == nil {
+		var sz, mbranch uint64 = 0, 0
+		for _, ri := range ris {
+			if (sz + ri.SizeBytes) < sz {
+				sz = 0xFFFFFFFFFFFFFFFF
+			} else {
+				sz += ri.SizeBytes
+			}
+			if mbranch < uint64(len(ri.Branches)) {
+				mbranch = uint64(len(ri.Branches))
+			}
+		}
+		metrics.Repos = int64(len(ris))
+		metrics.Bytes = sz
+		metrics.MaxBranches = mbranch
 	}
-	metrics.Repos = int64(len(ris))
-	return nil
 }
