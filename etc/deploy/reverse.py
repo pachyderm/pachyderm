22cd5a3142d1a407deb5a65cc15c6dc13f8ede52
grey <grey@pachyderm.io>
2021-02-10 12:14:31 -0800

    update reverse script

--- etc/deploy/reverse.py
+++ etc/deploy/reverse.py
@@ -1,75 +1,70 @@
 #!/usr/bin/env python
 
-import boto3, queue, os, re, subprocess, sys, threading, time
+import boto3, queue, os, subprocess, sys, threading, time, traceback
 
-top_level_dirs = ['chunk', 'pach', 'artifacts']
+processed = 0
+client_mutex = threading.Lock()
 
-forward_regexes = [re.compile(f'^{x}/.*$') for x in top_level_dirs]
-reverse_regexes = [re.compile(f'^.*/{x[::-1]}$') for x in top_level_dirs]
-
-num_workers = 10
-def do_worker(done, work_queue, from_bucket, to_bucket):
+def make_client():
+    global client_mutex
+    client_mutex.acquire()
     client = boto3.client('s3')
+    client_mutex.release()
+    return client
+
+num_workers = 40
+def do_worker(done, work_queue, from_bucket, to_bucket):
+    global processed
+    client = make_client()
     while True:
         try:
             path = work_queue.get_nowait()
-
-            print(f'Reversing {path} -> {path[::-1]}')
             client.copy({'Bucket': from_bucket, 'Key': path}, to_bucket, path[::-1])
+            processed += 1
         except queue.Empty:
             if done.is_set():
                 return
             time.sleep(1)
 
-def do_reverse(unreverse, work_queue):
-    if unreverse:
-        print('This will change all reversed pachyderm object paths to normal pachyderm object paths')
-    else:
-        print('This will change all normal pachyderm object paths to reversed pachyderm object paths')
-    if input('Are you sure? (y/N) ').lower() not in ['y', 'yes']:
-        print('Aborted')
-        sys.exit(1)
+def do_reverse(work_queue, from_bucket):
+    client = make_client()
+    kwargs = {'Bucket': from_bucket}
+    while True:
+        res = client.list_objects_v2(**kwargs)
+        kwargs['ContinuationToken'] = res['NextContinuationToken']
 
-    i = 0
-    with open('objects.txt') as f:
-        for line in f:
-            (date, time, size, path) = line.strip().split()
-            if any([x.match(path) for x in forward_regexes]):
-                if not unreverse:
-                    work_queue.put(path)
-                    i += 1
-            elif any([x.match(path) for x in reverse_regexes]):
-                if unreverse:
-                    work_queue.put(path)
-                    i += 1
-            else:
-                print(f'Unknown object: {path}')
+        for item in res['Contents']:
+            work_queue.put(item['Key'])
 
-            if i == 100:
-                return
+def do_progress(done):
+    while not done.is_set():
+        buf = f'reversed: {processed}'
+        sys.stdout.write(buf)
+        sys.stdout.flush()
+        sys.stdout.write('\b' * len(buf))
 
-unreverse_values = {'unreverse': True, 'reverse': False}
-
-usage = f'{sys.argv[0]} (unreverse | reverse) <from-bucket> <to-bucket>'
+usage = f'{sys.argv[0]} <from-bucket> <to-bucket>'
 if __name__ == '__main__':
-    if len(sys.argv) != 4 or sys.argv[1] not in unreverse_values.keys():
+    if len(sys.argv) != 3:
         print(usage)
         sys.exit(1)
 
-    unreverse = unreverse_values[sys.argv[1]]
-    from_bucket = sys.argv[2]
-    to_bucket = sys.argv[3]
+    from_bucket = sys.argv[1]
+    to_bucket = sys.argv[2]
 
     done = threading.Event()
     work_queue = queue.Queue(maxsize=num_workers * 2)
 
     workers = [threading.Thread(target=lambda: do_worker(done, work_queue, from_bucket, to_bucket)) for i in range(num_workers)]
+    workers.append(threading.Thread(target=lambda: do_progress(done)))
     [x.start() for x in workers]
 
     try:
-        do_reverse(unreverse, work_queue)
+        do_reverse(work_queue, from_bucket)
     except KeyboardInterrupt:
         pass
+    except Exception as ex:
+        traceback.print_exc()
 
     try:
         done.set()
