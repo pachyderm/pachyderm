e6fff9291cbdd7c9db11019dc00be50faa923547
grey <grey@pachyderm.io>
2021-02-12 17:08:18 -0800

    replaced use of mp.Queue.qsize with mp.Value, some reorganizing of the print_progress subprocess

--- etc/deploy/reverse.py
+++ etc/deploy/reverse.py
@@ -22,17 +22,18 @@ from datetime import datetime, timedelta
 import multiprocessing as mp
 
 num_workers = 100
-def do_worker(done, work_queue, finished_queue, from_bucket, to_bucket):
+def do_worker(id, done, work_queue, finished, from_bucket, to_bucket):
     client = boto3.client('s3')
     try:
         while True:
             try:
                 path = work_queue.get_nowait()
                 client.copy({'Bucket': from_bucket, 'Key': path}, to_bucket, path[::-1])
-                finished_queue.put(path)
+                with finished.get_lock():
+                    finished.value += 1
             except queue.Empty:
                 if done.is_set():
-                    return
+                    break
                 time.sleep(1)
     except KeyboardInterrupt:
         pass
@@ -64,30 +65,16 @@ def end_migration(client, to_bucket, start_time):
     # Write an empty object to indicate the migration was completed for the given start time
     res = client.put_object(Bucket=to_bucket, Key=migration_state_path, Metadata={'migration_start_time': start_time.isoformat()})
 
-def print_progress(done, scanned, enqueued, finished_queue):
-    finished = 0
+def print_progress(done, scanned, enqueued, finished):
     while not done.is_set():
-        while True:
-            try:
-                _ = finished_queue.get_nowait()
-                finished += 1
-            except queue.Empty:
-                break
-
-        buf = f'scanned: {scanned.value} enqueued: {enqueued.value} finished: {finished}...'
+        buf = f'scanned: {scanned.value} enqueued: {enqueued.value} finished: {finished.value}...'
         sys.stdout.write(buf)
         sys.stdout.flush()
         sys.stdout.write('\b' * len(buf))
         time.sleep(0.1)
 
-def all_objects(client, from_bucket, from_time, finished_queue):
+def all_objects(client, from_bucket, from_time, scanned, enqueued):
     kwargs = {'Bucket': from_bucket}
-    scanned = mp.Value('l', 0)
-    enqueued = mp.Value('l', 0)
-    done = mp.Event()
-
-    progress = mp.Process(target=print_progress, args=(done, scanned, enqueued, finished_queue))
-    progress.start()
 
     while True:
         res = client.list_objects_v2(**kwargs)
@@ -102,17 +89,19 @@ def all_objects(client, from_bucket, from_time, finished_queue):
         else:
             break
 
-    done.set()
-    progress.join()
-    print(f'\nSkipped objects: {scanned.value - enqueued.value}\nProcessed objects: {enqueued.value}')
-
 def do_reverse(from_bucket, to_bucket):
     complete = False
-    done = mp.Event()
     work_queue = mp.Queue(maxsize=1001) # Slightly more than one batch of results from list_object
-    finished_queue = mp.Queue(maxsize=1001)
 
-    workers = [mp.Process(target=do_worker, args=(done, work_queue, finished_queue, from_bucket, to_bucket)) for i in range(num_workers)]
+    progress_done = mp.Event()
+    scanned = mp.Value('l', 0)
+    enqueued = mp.Value('l', 0)
+    finished = mp.Value('l', 0)
+    progress_worker = mp.Process(target=print_progress, args=(progress_done, scanned, enqueued, finished))
+    progress_worker.start()
+
+    done = mp.Event()
+    workers = [mp.Process(target=do_worker, args=(i, done, work_queue, finished, from_bucket, to_bucket)) for i in range(num_workers)]
     [x.start() for x in workers]
 
     try:
@@ -121,7 +110,7 @@ def do_reverse(from_bucket, to_bucket):
         print(f'Previous start time: {from_time}')
         print(f'Current start time: {start_time}')
 
-        for path, modified in all_objects(client, from_bucket, from_time, finished_queue):
+        for path, modified in all_objects(client, from_bucket, from_time, scanned, enqueued):
             work_queue.put(path)
         complete = True
     except KeyboardInterrupt:
@@ -133,11 +122,23 @@ def do_reverse(from_bucket, to_bucket):
         done.set()
         [x.join() for x in workers]
     except KeyboardInterrupt:
+        print('Aborted')
         os._exit(1)
+
+    try:
+        progress_done.set()
+        progress_worker.join()
+    except KeyboardInterrupt:
+        print('Aborted')
+        os._exit(1)
+
+    print(f'\nSkipped objects: {scanned.value - enqueued.value}\nProcessed objects: {finished.value}')
 
     if complete:
         print('Writing out successful migration timestamp')
         end_migration(client, to_bucket, start_time)
+    else:
+        print('Incomplete migration, not saving migration timestamp')
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
