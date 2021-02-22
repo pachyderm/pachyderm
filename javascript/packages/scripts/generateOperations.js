1ef8f39c7a98ee780a4201bc215af923c80a8675
Cam <cpepin@fastmail.com>
2021-02-22 11:55:02 -0500

    Extracting operation generation script into shared library (#2534)

---
+++ javascript/packages/scripts/generateOperations.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+const {gqlPluckFromCodeStringSync} = require('@graphql-tools/graphql-tag-pluck');
+const {sync: globSync} = require('glob');
+
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.error('You must provide an output file!');
+}
+
+const outputFile = args[0];
+
+const operations = globSync(
+  path.join(process.cwd(), '/src/**/+(queries|mutations|fragments)/*.ts'),
+).reduce((acc, filePath) => {
+  acc +=
+    '\n' +
+    gqlPluckFromCodeStringSync(filePath, fs.readFileSync(filePath, 'utf8'));
+
+  return acc;
+}, '');
+
+fs.writeFileSync(
+  outputFile,
+  operations,
+);
