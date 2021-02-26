372850b30d84479aa0262d63f23729d10d413708
A Gardner <3100188+actgardner@users.noreply.github.com>
2021-02-26 11:58:57 -0500

    Add utility function to check cluster role bindings

--- src/server/auth/server/util.go
+++ src/server/auth/server/util.go
@@ -8,7 +8,26 @@ import (
 	txnenv "github.com/pachyderm/pachyderm/v2/src/internal/transactionenv"
 )
 
-// CheckIsAuthorizedInTransaction is identicalto CheckIsAuthorized except that
+// CheckClusterIsAuthorizedInTransaction returns an error if the current user doesn't have
+// the permissions in `p` on the cluster
+func CheckClusterIsAuthorizedInTransaction(txnCtx *txnenv.TransactionContext, p ...auth.Permission) error {
+	me, err := txnCtx.Client.WhoAmI(txnCtx.ClientContext, &auth.WhoAmIRequest{})
+	if auth.IsErrNotActivated(err) {
+		return nil
+	}
+
+	req := &auth.AuthorizeRequest{Resource: &auth.Resource{Type: auth.ResourceType_CLUSTER}, Permissions: p}
+	resp, err := txnCtx.Auth().AuthorizeInTransaction(txnCtx, req)
+	if err != nil {
+		return errors.Wrapf(grpcutil.ScrubGRPC(err), "error during authorization check for operation on cluster")
+	}
+	if !resp.Authorized {
+		return &auth.ErrNotAuthorized{Subject: me.Username, Resource: auth.Resource{Type: auth.ResourceType_CLUSTER}, Required: p}
+	}
+	return nil
+}
+
+// CheckRepoIsAuthorizedInTransaction is identical to CheckIsAuthorized except that
 // it performs reads consistent with the latest state of the STM transaction.
 func CheckRepoIsAuthorizedInTransaction(txnCtx *txnenv.TransactionContext, r string, p ...auth.Permission) error {
 	me, err := txnCtx.Client.WhoAmI(txnCtx.ClientContext, &auth.WhoAmIRequest{})
@@ -27,8 +46,8 @@ func CheckRepoIsAuthorizedInTransaction(txnCtx *txnenv.TransactionContext, r str
 	return nil
 }
 
-// CheckRepoIsAuthorized returns an error if the current user (in 'pachClient') has
-// authorization scope 's' for repo 'r'
+// CheckRepoIsAuthorizedInTransaction returns an error if the current user doesn't have
+// the permissions in `p` on the repo `r`
 func CheckRepoIsAuthorized(pachClient *client.APIClient, r string, p ...auth.Permission) error {
 	ctx := pachClient.Ctx()
 	me, err := pachClient.WhoAmI(ctx, &auth.WhoAmIRequest{})
