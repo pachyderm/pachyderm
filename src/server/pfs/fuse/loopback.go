a4b644c515d062394c62574ca1af7a5e11a87812
Brendon Carroll <7072757+brendoncarroll@users.noreply.github.com>
2021-03-01 16:50:21 -0500

    Let fuse inodes be generated automatically

--- src/server/pfs/fuse/loopback.go
+++ src/server/pfs/fuse/loopback.go
@@ -233,21 +233,10 @@ func (n *loopbackNode) Rename(ctx context.Context, name string, newParent fs.Ino
 }
 
 func (r *loopbackRoot) idFromStat(st *syscall.Stat_t) fs.StableAttr {
-	// We compose an inode number by the underlying inode, and
-	// mixing in the device number. In traditional filesystems,
-	// the inode numbers are small. The device numbers are also
-	// small (typically 16 bit). Finally, we mask out the root
-	// device number of the root, so a loopback FS that does not
-	// encompass multiple mounts will reflect the inode numbers of
-	// the underlying filesystem
-	swapped := (uint64(st.Dev) << 32) | (uint64(st.Dev) >> 32)
-	swappedRootDev := (r.rootDev << 32) | (r.rootDev >> 32)
 	return fs.StableAttr{
 		Mode: uint32(st.Mode),
 		Gen:  1,
-		// This should work well for traditional backing FSes,
-		// not so much for other go-fuse FS-es
-		Ino: (swapped ^ swappedRootDev) ^ st.Ino,
+		Ino:  0, // let fuse generate this automatically
 	}
 }
 
