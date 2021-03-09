c47b885ebb067dba854c959fbc6b239471476d13
Alysha Gardner <3100188+actgardner@users.noreply.github.com>
2021-03-09 15:22:55 -0500

    Split up the admin role into permissions (#5784)

--- src/internal/auth/handler.go
+++ src/internal/auth/handler.go
@@ -30,27 +30,25 @@ func authenticated(pachClient *client.APIClient, fullMethod string) error {
 	return err
 }
 
-// admin permits an RPC if auth is fully enabled and the user is a cluster admin
-func admin(pachClient *client.APIClient, fullMethod string) error {
-	resp, err := pachClient.Authorize(pachClient.Ctx(), &auth.AuthorizeRequest{
-		Resource: &auth.Resource{Type: auth.ResourceType_CLUSTER},
-		Permissions: []auth.Permission{
-			auth.Permission_CLUSTER_ADMIN,
-		},
-	})
-	if err != nil {
-		return err
-	}
+// clusterPermissions permits an RPC if the user is authorized with the given permissions on the cluster
+func clusterPermissions(permissions ...auth.Permission) authHandler {
+	return func(pachClient *client.APIClient, fullMethod string) error {
+		resp, err := pachClient.Authorize(pachClient.Ctx(), &auth.AuthorizeRequest{
+			Resource:    &auth.Resource{Type: auth.ResourceType_CLUSTER},
+			Permissions: permissions,
+		})
+		if err != nil {
+			return err
+		}
 
-	if resp.Authorized {
-		return nil
-	}
+		if resp.Authorized {
+			return nil
+		}
 
-	return &auth.ErrNotAuthorized{
-		Subject:  resp.Principal,
-		Resource: auth.Resource{Type: auth.ResourceType_CLUSTER},
-		Required: []auth.Permission{
-			auth.Permission_CLUSTER_ADMIN,
-		},
+		return &auth.ErrNotAuthorized{
+			Subject:  resp.Principal,
+			Resource: auth.Resource{Type: auth.ResourceType_CLUSTER},
+			Required: permissions,
+		}
 	}
 }
