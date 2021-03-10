68874a2663d37fb789671adcf2b663b2523f77bf
Brendon Carroll <7072757+brendoncarroll@users.noreply.github.com>
2021-03-10 14:58:13 -0500

    Create, Get, Delete changes

--- src/internal/storage/chunk/util.go
+++ src/internal/storage/chunk/util.go
@@ -1,13 +1,11 @@
 package chunk
 
 import (
-	"context"
 	"math/rand"
 	"testing"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/pachyderm/pachyderm/v2/src/internal/obj"
-	"github.com/pachyderm/pachyderm/v2/src/internal/require"
 	"github.com/pachyderm/pachyderm/v2/src/internal/storage/kv"
 	"github.com/pachyderm/pachyderm/v2/src/internal/storage/track"
 )
@@ -15,19 +13,8 @@ import (
 // NewTestStorage creates a local storage instance for testing during the lifetime of
 // the callback.
 func NewTestStorage(t testing.TB, db *sqlx.DB, tr track.Tracker, opts ...StorageOption) (obj.Client, *Storage) {
-	mdstore := NewTestStore(t, db)
 	objC, _ := obj.NewTestClient(t)
-	return objC, NewStorage(objC, kv.NewMemCache(10), mdstore, tr, opts...)
-}
-
-// NewTestStore creates a store for testing.
-func NewTestStore(t testing.TB, db *sqlx.DB) MetadataStore {
-	ctx := context.Background()
-	tx := db.MustBegin()
-	tx.MustExec(`CREATE SCHEMA IF NOT EXISTS STORAGE`)
-	require.NoError(t, SetupPostgresStoreV0(ctx, "storage.chunks", tx))
-	require.NoError(t, tx.Commit())
-	return NewPostgresStore(db)
+	return objC, NewStorage(objC, kv.NewMemCache(10), db, tr, opts...)
 }
 
 var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
