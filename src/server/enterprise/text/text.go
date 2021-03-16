e69b26120ca5375ea179f9bf2d7e714a39ff0f6e
Joe Doliner <jdoliner@gmail.com>
2021-03-16 12:34:43 -0700

    Adds enterprise limits and error text.

---
+++ src/server/enterprise/text/text.go
@@ -0,0 +1,12 @@
+package text
+
+const (
+	OpenSourceProduct = "Pachyderm Community Edition"
+	EnterpriseProduct = "Pachyderm Enterprise"
+	ActivateCTA       = "Use the command `pachctl enterprise activate` to enter your key."
+	RegisterCTA       = `Pachyderm offers readily available activation keys for proofs-of-concept, startups, academic, nonprofit, or open-source projects. Tell us about your project to get one.
+
+Learn about Pachyderm Enterprise here:
+
+<URL>`
+)
