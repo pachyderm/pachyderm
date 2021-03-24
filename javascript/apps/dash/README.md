8e43617e7572d2ba3027d22015a66ef1973ed193
Cam <cpepin@fastmail.com>
2021-03-24 13:09:31 -0400

    Adding setup script for 2.0 auth (#27)

--- javascript/apps/dash/README.md
+++ javascript/apps/dash/README.md
@@ -1,5 +1,10 @@
 # Dash
 
+## Install Dependencies
+```
+make install
+```
+
 ## Development
 
 ### Against the mock gRPC
@@ -14,40 +19,9 @@ This feature is still under development, and will likely change.
 
 1. Ensure that your cluster is deployed with `2.0.0-alpha.9` or later. Users of brew can install with `brew install pachyderm/tap/pachctl@2.0`.
 1. [Deploy a pachyderm cluster locally](https://docs.pachyderm.com/latest/getting_started/local_installation/), or create a workspace using Hub.
-1. Create a `.env.test.local` in this directory. In that file, add the variable `REACT_APP_PACHD_ADDRESS`.
-1. Fill `REACT_APP_PACHD_ADDRESS` with the address of the gRPC API server in your pach cluster. For local clusters, this is `localhost:30650`. (2.0 clusters are not yet available to be deployed on Hub)
 1. [Create a Github OAuth app](https://docs.github.com/en/developers/apps/creating-an-oauth-app). For local clusters, set your callback url to `http://localhost:30658/callback`. Make sure to save the secret key, you'll need it for the next step.
-1. Generate an enterprise key for the next step: https://enterprise-token-gen.pachyderm.io/dev. For Max OS users, `echo '<your-enterprise-token-here>' | pachctl enterprise activate`.
-1. Run `pachctl auth activate`.
-1. (Optional) It's helpful to add your Github account as a cluster admin using: `pachctl auth set cluster clusterAdmin user:<your github email>`. This will make your Github IDP user a cluster admin, allowing you to finish the remaining steps which require admin privileges. This will also give your user access to all created cluster resources. If you choose not to do this, you'll need to hold off on using `pachctl auth login` until finishing the remaining steps (remaining logged in as the root user). At that point, you'll need to login with your Github user before creating any cluster resources. Otherwise, your Github user won't have all of the necessary privileges to interact with create repos/pipelines/etc.
-1. Set the active enterprise context by running: ```pachctl config set active-enterprise-context `pachctl config get active-context` ```
-1. Configure the issuer for by running: `pachctl idp set-config --issuer 'http://localhost:30658/'`
-1. Configure the GitHub connector by running:
-    ```
-    pachctl idp create-connector --id github --name github --type github --config - <<EOF
-    {
-      "clientID": "<Github client id>",
-      "clientSecret": "<Github client secret>",
-      "redirectURI": "http://localhost:30658/callback",
-      "org": "pachyderm"
-    }
-    EOF
-    ```
-1. Configure the OIDC provider by running:
-    ```
-    pachctl auth set-config  <<EOF
-    {
-      "issuer": "http://localhost:30658/",
-      "localhost_issuer": true,
-      "client_id": "pachd",
-      "client_secret": "notsecret",
-      "redirect_uri": "http://localhost:30657/authorization-code/callback"
-    }
-    EOF
-    ```
-1. Create the pachyderm client using: `pachctl idp create-client --id dash --name dash --redirectUris http://localhost:4000/oauth/callback/\?inline\=true`. Save the client-secret!
-1. Add the variable `OAUTH_CLIENT_SECRET` to the `.env.test.local` you created earlier with the value set to the dash client secret.
-1. Update the `pachd` to allow dash to issue tokens on it's behalf using `pachctl idp update-client pachd --trustedPeers dash`
+1. Generate an enterprise key for the next step: https://enterprise-token-gen.pachyderm.io/dev. For Mac OS users, `echo '<your-enterprise-token-here>' | pachctl enterprise activate`.
+1. Run `make setup-auth`. This will walk you through the setup for your local cluster.
 1. Run `pachctl port-forward`
 1. (Optional) Use `pachctl auth login` to login via Github. If you opt not to do this, you will continue as the root user when creating resources.
 1. Run `npm run start:dev` from the /backend directory, and `npm run start` from the /frontend directory.
---
+++ javascript/apps/dash/setup.js
@@ -0,0 +1,220 @@
+const {exec} = require('child_process');
+const {writeFile, readFileSync} = require('fs');
+const {exit} = require('process');
+const {createInterface} = require('readline');
+const {parse, stringify} = require('envfile');
+
+const LOCAL_PACHD_ADDRESS = 'localhost:30650';
+
+// -------- utilities ----------
+const executePachCommand = (command) => {
+  return new Promise((res, rej) => {
+    exec(`pachctl ${command}`, (err, stdout) => {
+      if (err) {
+        rej(err);
+      } else {
+        res(stdout);
+      }
+    });
+  });
+};
+
+const writeToEnv = (variable, value) => {
+  const environment = readFileSync('.env.test.local', 'utf8');
+  const envObject = parse(environment) || {};
+
+  envObject[variable] = value;
+  return new Promise((res, rej) => {
+    writeFile('.env.test.local', stringify(envObject), (err) => {
+      if (err) {
+        rej(err);
+      } else {
+        res();
+      }
+    })
+  })
+};
+
+const askQuestion = (question) => {
+  const readline = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise((res) => {
+    readline.question(question + ' ', (answer) => {
+      res(answer);
+      readline.close();
+    });
+  });
+};
+// ------------------------------
+
+const checkPachVersion = async () => {
+  console.log('Checking pachd version...');
+  try {
+    const output = await executePachCommand('version');
+    const pachdVersion = output
+      .split('\n')
+      .find(line => line.includes('pachd'))
+      .replace('pachd', '')
+      .trim();
+
+    if (pachdVersion.startsWith('2')) {
+      console.log(`${pachdVersion} ✅`);
+    } else {
+      console.error('You must be using a 2.0 cluster!');
+      exit(1);
+    }
+  } catch (e) {
+    console.error('Problem checking pachd version:', e);
+    exit(1);
+  }
+}
+
+const writePachdAddress = async () => {
+  console.log('Writing pachd address to .env.test.local...');
+  try {
+    await writeToEnv('REACT_APP_PACHD_ADDRESS', LOCAL_PACHD_ADDRESS);
+    console.log(`${LOCAL_PACHD_ADDRESS} ✅`);
+  } catch (e) {
+    console.error('Problem writing pachd address to .env.test.local:', e);
+    exit(1);
+  }
+}
+
+const activateAuth = async () => {
+  console.log('Activating auth...');
+  try {
+    await executePachCommand('auth activate');
+    console.log('Auth activated ✅');
+  } catch (e) {
+    if (String(e).includes('OIDC client with id "pachd" already exists')) {
+      console.log('Auth was previously activated ⚠️');
+    } else {
+      console.error('Problem activating auth:', e);
+      exit(1);
+    }
+  }
+}
+
+const setupClusterAdmin = async () => {
+  const userEmail = await askQuestion('What is the primary email associated with your Github account?');
+
+  console.log('Setting up cluster admin...');
+  try {
+    await executePachCommand(`auth set cluster clusterAdmin user:${userEmail}`);
+    console.log(`${userEmail} added as a cluster admin ✅`);
+  } catch (e) {
+    console.error('Problem creating cluster admin:', e);
+    exit(1);
+  }
+}
+
+const setEnterpriseContext = async () => {
+  console.log('Setting enterprise context...');
+
+  try {
+    await executePachCommand('config set active-enterprise-context `pachctl config get active-context`');
+    console.log('Enterprise context set ✅');
+  } catch (e) {
+    console.error('Problem setting enterprise context:', e);
+    exit(1);
+  }
+}
+
+const configureIssuer = async () => {
+  console.log('Configuring issuer...');
+  try {
+    await executePachCommand(`idp set-config --issuer 'http://localhost:30658/'`);
+    console.log('Issuer configured ✅');
+  } catch (e) {
+    console.error('Problem configuring issuer:', e);
+    exit(1);
+  }
+}
+
+const configureGithubConnector = async () => {
+  const clientId = await askQuestion('Enter Github Client ID:');
+  const clientSecret = await askQuestion('Enter Github Client Secret:');
+
+  console.log('Configuring Github connector...');
+  try {
+    await executePachCommand(`idp create-connector --id github --name github --type github --config - <<EOF
+      {
+        "clientID": "${clientId}",
+        "clientSecret": "${clientSecret}",
+        "redirectURI": "http://localhost:30658/callback",
+        "org": "pachyderm"
+      }`);
+    console.log(`Connector configured with client ID ${clientId} ✅`);
+  } catch (e) {
+    if (String(e).includes('ID already exists')) {
+      console.log('Connector previously configured ⚠️');
+    } else {
+      console.log('Problem configuring connector:', e);
+      exit(1);
+    }
+  }
+}
+
+const configureOIDCProvider = async () => {
+  console.log('Configuring OIDC provider...');
+  try {
+    await executePachCommand(`auth set-config  <<EOF
+      {
+        "issuer": "http://localhost:30658/",
+        "localhost_issuer": true,
+        "client_id": "pachd",
+        "client_secret": "notsecret",
+        "redirect_uri": "http://localhost:30657/authorization-code/callback"
+      }`);
+    console.log('OIDC Provider configured ✅');
+  } catch (e) {
+    console.log('Problem configuring connector:', e);
+    exit(1);
+  }
+}
+
+const configureDashClient = async () => {
+  console.log('Configuring Dash client...');
+  try {
+    const output = await executePachCommand(`idp create-client --id dash --name dash --redirectUris http://localhost:4000/oauth/callback/\?inline\=true`);
+    const secret = output.match(/"(.*?)"/)[1];
+    await writeToEnv('OAUTH_CLIENT_SECRET', secret);
+    console.log('Dash client configured ✅');
+  } catch (e) {
+    if (String(e).includes(`"dash" already exists`)) {
+      console.log('Dash client was previously configured ⚠️');
+    } else {
+      console.log('Problem configuring dash client:', e);
+      exit(1);
+    }
+  }
+}
+
+const setupTrustedPeers = async () => {
+  console.log('Setting up trusted peers...');
+  try {
+    await executePachCommand('idp update-client pachd --trustedPeers dash');
+    console.log('Trusted peers added ✅');
+  } catch (e) {
+    console.log('Problem setting up trusted peers:', e);
+    exit(1);
+  }
+}
+
+const setup = async () => {
+  await checkPachVersion();
+  await writePachdAddress();
+  await activateAuth();
+  await setupClusterAdmin();
+  await setEnterpriseContext();
+  await configureIssuer();
+  await configureGithubConnector();
+  await configureOIDCProvider();
+  await configureDashClient();
+  await setupTrustedPeers();
+}
+
+setup();
