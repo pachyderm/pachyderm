ec8daf0cb22dd06f9a710a2217eae078b5bc75bb
Brendon Carroll <7072757+brendoncarroll@users.noreply.github.com>
2021-03-24 15:06:55 -0400

    Keeps Object Storage and Postgres in Sync (#5818)
    These changes let us create and delete chunks without ever ending up with a chunk without a corresponding object in object storage. It is possible to wind up with multiple objects per chunk, and objects without a Postgres entry, but the existence of these anomalies will not impact correctness, and there are background services to eliminate them eventually.

--- src/internal/storage/chunk/client.go
+++ src/internal/storage/chunk/client.go
@@ -2,6 +2,8 @@ package chunk
 
 import (
 	"context"
+	"database/sql"
+	fmt "fmt"
 	"path"
 	"strings"
 	"time"
@@ -11,14 +13,20 @@ import (
 	"github.com/pachyderm/pachyderm/v2/src/internal/errors"
 	"github.com/pachyderm/pachyderm/v2/src/internal/storage/kv"
 	"github.com/pachyderm/pachyderm/v2/src/internal/storage/track"
-	"github.com/sirupsen/logrus"
 )
 
-// Client allows manipulation of individual chunks, by maintaining consistency between
+// Client mediates access to a content-addressed store
+type Client interface {
+	Create(ctx context.Context, md Metadata, chunkData []byte) (ID, error)
+	Get(ctx context.Context, chunkID ID, cb kv.ValueCallback) error
+	Close() error
+}
+
+// trackedClient allows manipulation of individual chunks, by maintaining consistency between
 // a tracker and an kv.Store
-type Client struct {
+type trackedClient struct {
 	store   kv.Store
-	mdstore MetadataStore
+	db      *sqlx.DB
 	tracker track.Tracker
 	renewer *track.Renewer
 	ttl     time.Duration
@@ -26,15 +34,15 @@ type Client struct {
 
 // NewClient returns a client which will write to objc, mdstore, and tracker.  Name is used
 // for the set of temporary objects
-func NewClient(store kv.Store, mdstore MetadataStore, tr track.Tracker, name string) *Client {
+func NewClient(store kv.Store, db *sqlx.DB, tr track.Tracker, name string) Client {
 	var renewer *track.Renewer
 	if name != "" {
 		renewer = track.NewRenewer(tr, name, defaultChunkTTL)
 	}
-	c := &Client{
+	c := &trackedClient{
 		store:   store,
+		db:      db,
 		tracker: tr,
-		mdstore: mdstore,
 		renewer: renewer,
 		ttl:     defaultChunkTTL,
 	}
@@ -43,73 +51,107 @@ func NewClient(store kv.Store, mdstore MetadataStore, tr track.Tracker, name str
 
 // Create creates a new chunk from metadata and chunkData.
 // It returns the ID for the chunk
-func (c *Client) Create(ctx context.Context, md Metadata, chunkData []byte) (_ ID, retErr error) {
+func (c *trackedClient) Create(ctx context.Context, md Metadata, chunkData []byte) (_ ID, retErr error) {
+	if c.renewer == nil {
+		panic("client must be named to create chunks")
+	}
 	chunkID := Hash(chunkData)
 	var pointsTo []string
 	for _, cid := range md.PointsTo {
-		pointsTo = append(pointsTo, ObjectID(cid))
+		pointsTo = append(pointsTo, cid.TrackerID())
 	}
-	chunkOID := ObjectID(chunkID)
-	if err := dbutil.WithTx(ctx, c.mdstore.DB(), func(tx *sqlx.Tx) error {
-		if err := c.mdstore.SetTx(tx, chunkID, md); err != nil && err != ErrMetadataExists {
+	chunkTID := chunkID.TrackerID()
+	var needUpload bool
+	var gen uint64
+	if err := dbutil.WithTx(ctx, c.db, func(tx *sqlx.Tx) error {
+		if err := c.tracker.CreateTx(tx, chunkTID, pointsTo, c.ttl); err != nil {
 			return err
 		}
-		return c.tracker.CreateTx(tx, chunkOID, pointsTo, c.ttl)
+		var ents []Entry
+		if err := tx.Select(&ents, `
+		SELECT chunk_id, gen
+		FROM storage.chunk_objects
+		WHERE uploaded = TRUE AND tombstone = FALSE AND chunk_id = $1`, chunkID); err != nil {
+			return err
+		}
+		if len(ents) > 0 {
+			needUpload = false
+			return nil
+		}
+		if err := tx.Get(&gen, `
+		INSERT INTO storage.chunk_objects (chunk_id, size)
+		VALUES ($1, $2)
+		RETURNING gen
+		`, chunkID, md.Size); err != nil {
+			return err
+		}
+		needUpload = true
+		return nil
 	}); err != nil {
 		return nil, err
 	}
-	if err := c.renewer.Add(ctx, chunkOID); err != nil {
+	if err := c.renewer.Add(ctx, chunkTID); err != nil {
 		return nil, err
 	}
-	// TODO: need to check for existence of specific objects.
-	key := chunkKey(chunkID)
-	if exists, err := c.store.Exists(ctx, key); err != nil {
-		return nil, err
-	} else if exists {
+	if !needUpload {
 		return chunkID, nil
 	}
+	key := chunkKey(chunkID, gen)
 	if err := c.store.Put(ctx, key, chunkData); err != nil {
+		return nil, err
+	}
+	_, err := c.db.Exec(`
+	UPDATE storage.chunk_objects
+	SET uploaded = TRUE
+	WHERE chunk_id = $1 AND gen = $2
+	`, chunkID, gen)
+	if err != nil {
 		return nil, err
 	}
 	return chunkID, nil
 }
 
 // Get writes data for a chunk with ID chunkID to w.
-func (c *Client) Get(ctx context.Context, chunkID ID, cb kv.ValueCallback) (retErr error) {
-	key := chunkKey(chunkID)
+func (c *trackedClient) Get(ctx context.Context, chunkID ID, cb kv.ValueCallback) (retErr error) {
+	var gen uint64
+	err := c.db.Get(&gen, `
+	SELECT gen
+	FROM storage.chunk_objects
+	WHERE uploaded = TRUE AND tombstone = FALSE AND chunk_id = $1
+	LIMIT 1
+	`, chunkID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			err = errors.Errorf("no objects for chunk %v", chunkID)
+		}
+		return err
+	}
+	key := chunkKey(chunkID, gen)
 	return c.store.Get(ctx, key, cb)
 }
 
 // Close closes the client, stopping the background renewal of created objects
-func (c *Client) Close() error {
+func (c *trackedClient) Close() error {
 	if c.renewer != nil {
 		return c.renewer.Close()
 	}
 	return nil
 }
 
-func chunkPath(chunkID ID) string {
+func chunkPath(chunkID ID, gen uint64) string {
 	if len(chunkID) == 0 {
 		panic("chunkID cannot be empty")
 	}
-	return path.Join(prefix, chunkID.HexString())
+	return path.Join(prefix, fmt.Sprintf("%s.%016x", chunkID.HexString(), gen))
 }
 
-func chunkKey(chunkID ID) []byte {
-	return []byte(chunkPath(chunkID))
-}
-
-// ObjectID returns an object ID for use with a tracker
-func ObjectID(chunkID ID) string {
-	return prefix + "/" + chunkID.HexString()
+func chunkKey(chunkID ID, gen uint64) []byte {
+	return []byte(chunkPath(chunkID, gen))
 }
 
 var _ track.Deleter = &deleter{}
 
-type deleter struct {
-	mdstore MetadataStore
-	store   kv.Store
-}
+type deleter struct{}
 
 func (d *deleter) DeleteTx(tx *sqlx.Tx, id string) error {
 	if !strings.HasPrefix(id, prefix+"/") {
@@ -119,15 +161,10 @@ func (d *deleter) DeleteTx(tx *sqlx.Tx, id string) error {
 	if err != nil {
 		return err
 	}
-	if err := d.mdstore.DeleteTx(tx, chunkID); err != nil {
-		return err
-	}
-	// TODO: this is wrong, but at least it's obviously wrong.
-	go func() {
-		ctx := context.Background()
-		if err := d.store.Delete(ctx, chunkKey(chunkID)); err != nil {
-			logrus.Error(err)
-		}
-	}()
-	return nil
+	_, err = tx.Exec(`
+		UPDATE storage.chunk_objects
+		SET tombstone = TRUE
+		WHERE chunk_id = $1
+	`, chunkID)
+	return err
 }
