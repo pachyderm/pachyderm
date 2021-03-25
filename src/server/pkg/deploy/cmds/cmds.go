62b945a5934a101cd324b47bf0a5e801e68b9162
Robert A. Uhl <robert.uhl@pachyderm.io>
2021-03-25 11:56:29 -0400

    Send deprecation warnings to STDERR

--- src/server/pkg/deploy/cmds/cmds.go
+++ src/server/pkg/deploy/cmds/cmds.go
@@ -282,11 +282,9 @@ func containsEmpty(vals []string) bool {
 	return false
 }
 
-// deprecationWarning prints a deprecation warning to os.Stdout.
-//
-// TODO: consider printing deprecation warnings to os.Stderr.
+// deprecationWarning prints a deprecation warning to os.Stderr.
 func deprecationWarning(msg string) {
-	fmt.Printf("DEPRECATED: %s\n\n", msg)
+	fmt.Fprintf(os.Stderr, "DEPRECATED: %s\n\n", msg)
 }
 
 func standardDeployCmds() []*cobra.Command {
