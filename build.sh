bb13dc5aac1f318661b6efd2af969ef0278418cf
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2021-03-30 16:33:51 -0400

    more ignores

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -e
 VERSION=$(date +%s)
 go get github.com/go-bindata/go-bindata/...
 go-bindata -o src/server/cmd/worker/assets/assets.go -pkg assets /etc/ssl/certs/...
+CGO_ENABLED=0 go build -ldflags "${LD_FLAGS}" -o pachctl "src/server/cmd/pachctl/main.go"
 CGO_ENABLED=0 go build -ldflags "${LD_FLAGS}" -o pachd "src/server/cmd/pachd/main.go"
 CGO_ENABLED=0 go build -ldflags "${LD_FLAGS}" -o worker "src/server/cmd/worker/main.go"
 docker build -f Dockerfile.pachd . -t localhost:5000/pachd:1.13-$VERSION
