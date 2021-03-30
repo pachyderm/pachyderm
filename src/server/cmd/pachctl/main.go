6f1e26a291302795df7b1c9552979e652259416e
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2021-03-30 15:06:25 -0400

    assets: generated ssl certs (do not merge)

--- src/server/cmd/pachctl/main.go
+++ src/server/cmd/pachctl/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
-	"runtime/pprof"
 	"strings"
+	"time"
 
+	"github.com/dustin/go-humanize"
+	"github.com/pachyderm/pachyderm/src/client"
 	"github.com/pachyderm/pachyderm/src/client/pkg/tracing"
 	"github.com/pachyderm/pachyderm/src/server/cmd/pachctl/cmd"
 	"github.com/spf13/pflag"
@@ -21,28 +23,33 @@ func main() {
 		fmt.Println("server exited")
 	}()
 
-	fmt.Println("writing cpu profile to prof")
-	fd, err := os.OpenFile("prof", os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0666)
-	if err != nil {
-		panic(fmt.Sprintf("create prof: %v", err))
-	}
-	if err := pprof.StartCPUProfile(fd); err != nil {
-		panic(fmt.Sprintf("start profile: %v", err))
-	}
+	// fmt.Println("writing cpu profile to prof")
+	// fd, err := os.OpenFile("prof", os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0666)
+	// if err != nil {
+	// 	panic(fmt.Sprintf("create prof: %v", err))
+	// }
+	// if err := pprof.StartCPUProfile(fd); err != nil {
+	// 	panic(fmt.Sprintf("start profile: %v", err))
+	// }
 
 	// Remove kubernetes client flags from the spf13 flag set
 	// (we link the kubernetes client, so otherwise they're in 'pachctl --help')
 	pflag.CommandLine = pflag.NewFlagSet(os.Args[0], pflag.ExitOnError)
 	tracing.InstallJaegerTracerFromEnv()
-	err = func() error {
+	var duration time.Duration
+	err := func() error {
+		start := time.Now()
 		defer tracing.CloseAndReportTraces()
+		defer func() { duration = time.Since(start) }()
 		return cmd.PachctlCmd().Execute()
 	}()
-	pprof.StopCPUProfile()
-	if err := fd.Close(); err != nil {
-		panic(fmt.Sprintf("close prof: %v", err))
-	}
-	fmt.Println("wrote prof")
+	fmt.Printf("copy: %v bytes in %v (%v/s)\n", humanize.Bytes(uint64(client.BytesSent)), time.Duration(client.TimeTaken).String(), humanize.Bytes(uint64(float64(client.BytesSent)/time.Duration(client.TimeTaken).Seconds())))
+	fmt.Printf("wall: %v bytes in %v (%v/s)\n", humanize.Bytes(uint64(client.BytesSent)), duration.String(), humanize.Bytes(uint64(float64(client.BytesSent)/duration.Seconds())))
+	// pprof.StopCPUProfile()
+	// if err := fd.Close(); err != nil {
+	// 	panic(fmt.Sprintf("close prof: %v", err))
+	// }
+	// fmt.Println("wrote prof")
 	if err != nil {
 		if errString := strings.TrimSpace(err.Error()); errString != "" {
 			fmt.Fprintf(os.Stderr, "%s\n", errString)
