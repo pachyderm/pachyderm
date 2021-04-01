ef23ff50cc6de80278ce1000dc71c238acaff86e
A Gardner <3100188+actgardner@users.noreply.github.com>
2021-04-01 14:57:00 -0400

    Typo

--- src/identity/identity.go
+++ src/identity/identity.go
@@ -11,7 +11,7 @@ var (
 	// ErrInvalidID is returned if the client or connector ID does not exist
 	ErrInvalidID = status.Error(codes.Internal, "ID does not exist")
 
-	// ErrAlreadyExists is returned if the clientor connector ID already exists
+	// ErrAlreadyExists is returned if the client or connector ID already exists
 	ErrAlreadyExists = status.Error(codes.Internal, "ID already exists")
 )
 
