b8f369bdd836a0ce6fbcc6bf6c77b5228624e311
grey <grey@pachyderm.io>
2021-04-12 01:31:28 -0700

    checkpoint

--- src/internal/obj/util.go
+++ src/internal/obj/util.go
@@ -3,28 +3,7 @@ package obj
 import (
 	"context"
 	"io"
-	"io/ioutil"
-	"os"
-	"path"
-	"strings"
-	"testing"
-
-	"github.com/pachyderm/pachyderm/v2/src/internal/require"
 )
-
-// NewTestClient creates a Client which is cleaned up after the test exists
-func NewTestClient(t testing.TB) (Client, string) {
-	dirBase := path.Join(os.TempDir(), "pachyderm_test")
-	require.NoError(t, os.MkdirAll(dirBase, 0700))
-	dir, err := ioutil.TempDir(dirBase, "")
-	require.NoError(t, err)
-	t.Cleanup(func() {
-		require.NoError(t, os.RemoveAll(dir))
-	})
-	objC, err := NewLocalClient(dir)
-	require.NoError(t, err)
-	return objC, strings.ReplaceAll(strings.Trim(dir, "/"), "/", ".")
-}
 
 // Copy copys an object from src at srcPath to dst at dstPath
 func Copy(ctx context.Context, src, dst Client, srcPath, dstPath string) (retErr error) {
