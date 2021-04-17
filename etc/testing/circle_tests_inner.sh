50a273ef20422fdd1888b313581c31be00196e6e
A Gardner <3100188+actgardner@users.noreply.github.com>
2021-04-17 14:30:03 -0400

    Fix jq formatting

--- etc/testing/circle_tests_inner.sh
+++ etc/testing/circle_tests_inner.sh
@@ -15,7 +15,7 @@ GOPATH=/root/go
 export GOPATH
 PATH="${GOPATH}/bin:${PATH}"
 export PATH
-TESTFLAGS="-json | tee -a results.json | jq -r '.Output'"
+TESTFLAGS="-json | tee -a results.json | jq -j 'select( .Output != null ) | .Output'"
 export TESTFLAGS
 
 # Some tests (e.g. TestMigrateFrom1_7) expect
