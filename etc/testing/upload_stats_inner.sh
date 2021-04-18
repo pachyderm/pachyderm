91e9e3e9cbad6c42a844c669f419e82409b23c04
A Gardner <3100188+actgardner@users.noreply.github.com>
2021-04-17 21:53:59 -0400

    Debug why SA isn't showing up

--- etc/testing/upload_stats_inner.sh
+++ etc/testing/upload_stats_inner.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-set -xeuo pipefail
+set -euo pipefail
+
+echo $GOOGLE_TEST_UPLOAD_CREDS > /tmp/google-creds.json
+gcloud auth activate-service-account --key-file=/tmp/google-creds.json
+
+set -x
 
 GOPATH=/root/go
 export GOPATH
 PATH="${GOPATH}/bin:${PATH}"
 export PATH
-
-echo $GOOGLE_TEST_UPLOAD_CREDS > /tmp/google-creds.json
-gcloud auth activate-service-account --key-file=/tmp/google-creds.json
 
 go install github.com/actgardner/test-stat
 test-stat /tmp/results.json
