a34c9e396cda94624a7eb7538b0085ef92390141
A Gardner <3100188+actgardner@users.noreply.github.com>
2021-04-17 23:01:53 -0400

    Set ADC

--- etc/testing/upload_stats_inner.sh
+++ etc/testing/upload_stats_inner.sh
@@ -3,14 +3,16 @@
 set -euo pipefail
 
 echo $GOOGLE_TEST_UPLOAD_CREDS > /tmp/google-creds.json
-gcloud auth activate-service-account --key-file=/tmp/google-creds.json
+export GOOGLE_APPLICATION_CREDENTIALS=/tmp/google-creds.json
 
 set -x
 
-GOPATH=/root/go
+GOPATH=/root/go/workspace
 export GOPATH
-PATH="${GOPATH}/bin:${PATH}"
+PATH="${GOPATH}/bin:/root/go/bin:${PATH}"
 export PATH
 
-go install github.com/actgardner/test-stat
-test-stat /tmp/results.json
+if [ -f /tmp/results.json ]; then
+  go get github.com/actgardner/test-stat
+  test-stat /tmp/results.json
+fi
