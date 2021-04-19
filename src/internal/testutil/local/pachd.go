2739ca0938cacb82dcfe66431a2eb56b3dd3599d
Matthew Steffen <msteffen@pachyderm.io>
2021-04-19 16:58:00 -0700

    Re-enable http API in local pachd (per doFullMode)

--- src/internal/testutil/local/pachd.go
+++ src/internal/testutil/local/pachd.go
@@ -460,33 +460,32 @@ func RunLocal() (retErr error) {
 	go waitForError("Internal Pachd GRPC Server", errChan, true, func() error {
 		return internalServer.Wait()
 	})
-	// TODO: Make http server work with V2.
-	//go waitForError("HTTP Server", errChan, requireNoncriticalServers, func() error {
-	//	httpServer, err := pach_http.NewHTTPServer(address)
-	//	if err != nil {
-	//		return err
-	//	}
-	//	server := http.Server{
-	//		Addr:    fmt.Sprintf(":%v", env.HTTPPort),
-	//		Handler: httpServer,
-	//	}
+	go waitForError("HTTP Server", errChan, requireNoncriticalServers, func() error {
+		httpServer, err := pach_http.NewHTTPServer(address)
+		if err != nil {
+			return err
+		}
+		server := http.Server{
+			Addr:    fmt.Sprintf(":%v", env.Config().HTTPPort),
+			Handler: httpServer,
+		}
 
-	//	certPath, keyPath, err := tls.GetCertPaths()
-	//	if err != nil {
-	//		log.Warnf("pfs-over-HTTP - TLS disabled: %v", err)
-	//		return server.ListenAndServe()
-	//	}
+		certPath, keyPath, err := tls.GetCertPaths()
+		if err != nil {
+			log.Warnf("pfs-over-HTTP - TLS disabled: %v", err)
+			return server.ListenAndServe()
+		}
 
-	//	cLoader := tls.NewCertLoader(certPath, keyPath, tls.CertCheckFrequency)
-	//	err = cLoader.LoadAndStart()
-	//	if err != nil {
-	//		return errors.Wrapf(err, "couldn't load TLS cert for pfs-over-http: %v", err)
-	//	}
+		cLoader := tls.NewCertLoader(certPath, keyPath, tls.CertCheckFrequency)
+		err = cLoader.LoadAndStart()
+		if err != nil {
+			return errors.Wrapf(err, "couldn't load TLS cert for pfs-over-http: %v", err)
+		}
 
-	//	server.TLSConfig = &gotls.Config{GetCertificate: cLoader.GetCertificate}
+		server.TLSConfig = &gotls.Config{GetCertificate: cLoader.GetCertificate}
 
-	//	return server.ListenAndServeTLS(certPath, keyPath)
-	//})
+		return server.ListenAndServeTLS(certPath, keyPath)
+	})
 	go waitForError("Githook Server", errChan, requireNoncriticalServers, func() error {
 		return githook.RunGitHookServer(address, etcdAddress, path.Join(env.Config().EtcdPrefix, env.Config().PPSEtcdPrefix))
 	})
