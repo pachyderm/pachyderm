7520600c6b3c8748e9dc964126f70b69f17a62de
Alysha Gardner <3100188+actgardner@users.noreply.github.com>
2021-04-20 11:54:21 -0400

    BigQuery test data warehouse (take 2) (#5996)
    * Revert "Revert "Upload go test output to BigQuery for test dashboard (#5992)" (#5993)"
    
    This reverts commit 3c0da16075703b1651a11478dbf4904cd3cad417.
    
    * Convert test output to json later
    
    * Try and reduce buffering in piped output
    
    * Add -t flag for elapsed field to be emitted
    
    * Add comments to make linter pass

--- etc/testing/circle_tests_inner.sh
+++ etc/testing/circle_tests_inner.sh
@@ -15,6 +15,8 @@ GOPATH=/root/go
 export GOPATH
 PATH="${GOPATH}/bin:${PATH}"
 export PATH
+TESTFLAGS="-v | stdbuf -i0 tee -a /tmp/results"
+export TESTFLAGS
 
 # Some tests (e.g. TestMigrateFrom1_7) expect
 # $GOPATH/src/github.com/pachyderm/pachyderm to point to .
