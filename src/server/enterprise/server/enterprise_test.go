610926be7285ec5f8179ee19da29e46b3b6ddcb3
Matthew Steffen <msteffen@pachyderm.io>
2021-04-22 23:22:32 -0700

    Add test for new enterprise pipeline validation

--- src/server/enterprise/server/enterprise_test.go
+++ src/server/enterprise/server/enterprise_test.go
@@ -1,15 +1,22 @@
 package server
 
 import (
+	"bytes"
+	"fmt"
+	"strconv"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/gogo/protobuf/types"
 	"golang.org/x/net/context"
 
+	"github.com/pachyderm/pachyderm/src/client"
 	"github.com/pachyderm/pachyderm/src/client/enterprise"
+	"github.com/pachyderm/pachyderm/src/client/pfs"
 	"github.com/pachyderm/pachyderm/src/client/pkg/errors"
 	"github.com/pachyderm/pachyderm/src/client/pkg/require"
+	"github.com/pachyderm/pachyderm/src/client/pps"
 	"github.com/pachyderm/pachyderm/src/server/pkg/backoff"
 	tu "github.com/pachyderm/pachyderm/src/server/pkg/testutil"
 )
@@ -248,4 +255,95 @@ func TestDoubleDeactivate(t *testing.T) {
 		&enterprise.GetStateRequest{})
 	require.NoError(t, err)
 	require.Equal(t, enterprise.State_NONE, resp.State)
+}
+
+func TestParallelism(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping integration tests in short mode")
+	}
+	tu.DeleteAll(t)
+	c := tu.GetPachClient(t)
+
+	dataRepo := tu.UniqueString(t.Name() + "_data")
+	require.NoError(t, c.CreateRepo(dataRepo))
+
+	commit1, err := c.StartCommit(dataRepo, "master")
+	require.NoError(t, err)
+	for i := 0; i < 10; i++ {
+		_, err = c.PutFile(dataRepo, "master", strconv.Itoa(i), strings.NewReader("foo"))
+		require.NoError(t, err)
+	}
+	require.NoError(t, c.FinishCommit(dataRepo, commit1.ID))
+
+	pipeline := tu.UniqueString(t.Name())
+	err = c.CreatePipeline(
+		pipeline,
+		"",
+		[]string{"bash"},
+		[]string{
+			fmt.Sprintf("cp /pfs/%s/* /pfs/out/", dataRepo),
+		},
+		&pps.ParallelismSpec{
+			Constant: 10, // disallowed by enterprise check
+		},
+		client.NewPFSInput(dataRepo, "/*"),
+		"",
+		false,
+	)
+	require.YesError(t, err)
+	require.Matches(t, "parallelism", err.Error())
+
+	// Activate Pachyderm Enterprise and make sure the state is ACTIVE
+	_, err = c.Enterprise.Activate(context.Background(),
+		&enterprise.ActivateRequest{ActivationCode: tu.GetTestEnterpriseCode(t)})
+	require.NoError(t, err)
+	require.NoError(t, backoff.Retry(func() error {
+		resp, err := c.Enterprise.GetState(context.Background(),
+			&enterprise.GetStateRequest{})
+		if err != nil {
+			return err
+		}
+		if resp.State != enterprise.State_ACTIVE {
+			return errors.Errorf("expected enterprise state to be ACTIVE but was %v", resp.State)
+		}
+		expires, err := types.TimestampFromProto(resp.Info.Expires)
+		if err != nil {
+			return err
+		}
+		if time.Until(expires) <= year {
+			return errors.Errorf("expected test token to expire >1yr in the future, but expires at %v (congratulations on making it to 2026!)", expires)
+		}
+		activationCode, err := unmarshalActivationCode(resp.ActivationCode)
+		if err != nil {
+			return err
+		}
+		if activationCode.Signature != "" {
+			return errors.Errorf("incorrect activation code signature, expected empty string")
+		}
+		return nil
+	}, backoff.NewTestingBackOff()))
+
+	require.NoError(t, c.CreatePipeline(
+		pipeline,
+		"",
+		[]string{"bash"},
+		[]string{
+			fmt.Sprintf("cp /pfs/%s/* /pfs/out/", dataRepo),
+		},
+		&pps.ParallelismSpec{
+			Constant: 10, // disallowed by enterprise check
+		},
+		client.NewPFSInput(dataRepo, "/*"),
+		"",
+		false,
+	))
+	_, err = c.FlushCommitAll([]*pfs.Commit{client.NewCommit(dataRepo, "master")}, nil)
+	require.NoError(t, err)
+	var buf bytes.Buffer
+	for i := 0; i < 10; i++ {
+		buf.Reset()
+		require.NoError(t, c.GetFile(pipeline, "master", strconv.Itoa(i), 0, 0, &buf))
+		require.NoError(t, err)
+		require.Equal(t, "foo", buf.String())
+	}
 }
