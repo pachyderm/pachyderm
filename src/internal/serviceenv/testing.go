6042334e65267a6aadac1a723b95ebdd0da53b03
A Gardner <3100188+actgardner@users.noreply.github.com>
2021-04-27 11:28:44 -0400

    Serviceenv handles creating dex storage provider

--- src/internal/serviceenv/testing.go
+++ src/internal/serviceenv/testing.go
@@ -6,6 +6,7 @@ import (
 	"github.com/pachyderm/pachyderm/v2/src/client"
 
 	etcd "github.com/coreos/etcd/clientv3"
+	dex_storage "github.com/dexidp/dex/storage"
 	loki "github.com/grafana/loki/pkg/logcli/client"
 	"github.com/jmoiron/sqlx"
 	log "github.com/sirupsen/logrus"
@@ -22,6 +23,7 @@ type TestServiceEnv struct {
 	KubeClient    *kube.Clientset
 	LokiClient    *loki.Client
 	DBClient      *sqlx.DB
+	DexDB         dex_storage.Storage
 	Log           *log.Logger
 	Ctx           context.Context
 }
@@ -56,6 +58,10 @@ func (s *TestServiceEnv) ClusterID() string {
 
 func (s *TestServiceEnv) Logger() *log.Logger {
 	return s.Log
+}
+
+func (s *TestServiceEnv) GetDexDB() dex_storage.Storage {
+	return s.DexDB
 }
 
 func (s *TestServiceEnv) Close() error {
