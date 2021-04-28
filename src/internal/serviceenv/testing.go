8f55ac8b3b889f8766efaf2bc088a6bd1da31608
Alysha Gardner <3100188+actgardner@users.noreply.github.com>
2021-04-28 09:29:51 -0400

    Add Ready channel for test serviceenv (#6029)

--- src/internal/serviceenv/testing.go
+++ src/internal/serviceenv/testing.go
@@ -24,6 +24,11 @@ type TestServiceEnv struct {
 	DBClient      *sqlx.DB
 	Log           *log.Logger
 	Ctx           context.Context
+
+	// Ready is a channel that blocks `GetPachClient` until it's closed.
+	// This avoids a race when we need to instantiate the server before
+	// getting a client pointing at the same server.
+	Ready chan interface{}
 }
 
 func (s *TestServiceEnv) Config() *Configuration {
@@ -31,7 +36,8 @@ func (s *TestServiceEnv) Config() *Configuration {
 }
 
 func (s *TestServiceEnv) GetPachClient(ctx context.Context) *client.APIClient {
-	return s.PachClient
+	<-s.Ready
+	return s.PachClient.WithCtx(ctx)
 }
 func (s *TestServiceEnv) GetEtcdClient() *etcd.Client {
 	return s.EtcdClient
