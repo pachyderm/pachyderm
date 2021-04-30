52e6ffdd3a43df32394b122e4b8bea05b44b679f
A Gardner <3100188+actgardner@users.noreply.github.com>
2021-04-30 16:30:13 -0400

    Try building outside of containers

--- etc/testing/circle/build.sh
+++ etc/testing/circle/build.sh
@@ -2,10 +2,29 @@
 
 set -ex
 
-export GOPATH=/home/circleci/.go_workspace
-export PATH=$(pwd):$(pwd)/cached-deps:$GOPATH/bin:$PATH
+GOPATH=/home/circleci/.go_workspace
+export GOPATH
+
+PATH=$(pwd):$(pwd)/cached-deps:$GOPATH/bin:$PATH
+export PATH
+
+go get github.com/go-bindata/go-bindata/...
+
+go-bindata -o src/server/cmd/worker/assets/assets.go -pkg assets /etc/ssl/certs/...
+
+CGO_ENABLED=0 go build -ldflags "${LD_FLAGS}" -o init "./etc/worker/init.go"
+CGO_ENABLED=0 go build -o pachctl_nocgo -ldflags "${LD_FLAGS}" -gcflags "${GC_FLAGS}" ./src/server/cmd/pachctl
+CGO_ENABLED=0 go build -ldflags "${LD_FLAGS}" -o pachd "src/server/cmd/pachd/main.go"
+CGO_ENABLED=0 go build -ldflags "${LD_FLAGS}" -o worker "src/server/cmd/worker/main.go"
+
+go build -o pachctl -ldflags "${LD_FLAGS}" -gcflags "${GC_FLAGS}" ./src/server/cmd/pachctl
+
+cp -R /etc/ssl/certs .
+cp -R /usr/share/ca-certificates .
 
 eval $(minikube docker-env)
+docker build -t pachyderm/pachd:local . -f etc/testing/circle/Dockerfile.pachd
+docker build -t pachyderm/pachctl:local . -f etc/testing/circle/Dockerfile.pachctl
+docker build -t pachyderm/worker:local . -f etc/testing/circle/Dockerfile.worker
 make install
-make docker-build
 make docker-build-pipeline-build
