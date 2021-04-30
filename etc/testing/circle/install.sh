723dbaf53f8c1886282d90b27813cf7a498572bd
A Gardner <3100188+actgardner@users.noreply.github.com>
2021-04-30 10:57:09 -0400

    Skip apt update

--- etc/testing/circle/install.sh
+++ etc/testing/circle/install.sh
@@ -7,7 +7,6 @@ mkdir -p cached-deps
 # Install deps
 sudo snap install --classic goreleaser
 sudo snap install jq
-sudo apt-get update -y
 sudo apt-get install -y -qq \
   silversearcher-ag \
   python3 \
