da3ba84e6940dac2cb3e70061edfb898dcdea293
A Gardner <3100188+actgardner@users.noreply.github.com>
2021-04-30 10:51:50 -0400

    Fix cache paths

--- etc/testing/circle/install.sh
+++ etc/testing/circle/install.sh
@@ -31,7 +31,7 @@ pip3 install --upgrade --user awscli s3transfer==0.3.4
 # Install kubectl
 # To get the latest kubectl version:
 # curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt
-if [ ! -f ~/cached-deps/kubectl ] ; then
+if [ ! -f cached-deps/kubectl ] ; then
     KUBECTL_VERSION=v1.19.2
     curl -L -o kubectl https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl && \
         chmod +x ./kubectl
@@ -41,7 +41,7 @@ fi
 # Install minikube
 # To get the latest minikube version:
 # curl https://api.github.com/repos/kubernetes/minikube/releases | jq -r .[].tag_name | sort -V | tail -n1
-if [ ! -f ~/cached-deps/minikube ] ; then
+if [ ! -f cached-deps/minikube ] ; then
     MINIKUBE_VERSION=v1.13.1 # If changed, also do etc/kube/start-minikube.sh
     curl -L -o minikube https://storage.googleapis.com/minikube/releases/${MINIKUBE_VERSION}/minikube-linux-amd64 && \
         chmod +x ./minikube
@@ -51,7 +51,7 @@ fi
 # Install etcdctl
 # To get the latest etcd version:
 # curl -Ls https://api.github.com/repos/etcd-io/etcd/releases | jq -r .[].tag_name
-if [ ! -f ~/cached-deps/etcdctl ] ; then
+if [ ! -f cached-deps/etcdctl ] ; then
     ETCD_VERSION=v3.3.12
     curl -L https://storage.googleapis.com/etcd/${ETCD_VERSION}/etcd-${ETCD_VERSION}-linux-amd64.tar.gz \
         | tar xzf - --strip-components=1
@@ -59,7 +59,7 @@ if [ ! -f ~/cached-deps/etcdctl ] ; then
 fi
 
 # Install kubeval
-if [ ! -f ~/cached-deps/kubeval ]; then
+if [ ! -f cached-deps/kubeval ]; then
   KUBEVAL_VERSION=0.15.0
   curl -L https://github.com/instrumenta/kubeval/releases/download/${KUBEVAL_VERSION}/kubeval-linux-amd64.tar.gz \
       | tar xzf - kubeval
@@ -67,7 +67,7 @@ if [ ! -f ~/cached-deps/kubeval ]; then
 fi
 
 # Install helm
-if [ ! -f ~/cached-deps/helm ]; then
+if [ ! -f cached-deps/helm ]; then
   HELM_VERSION=3.5.4
   curl -L https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz \
       | tar xzf - linux-amd64/helm
