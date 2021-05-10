d6c2dee68ac525a93a50114bf16e7dce4f0ee292
Matthew Steffen <msteffen@pachyderm.io>
2021-05-09 22:22:49 -0700

    Remove spurious return in pollPipelinePods

--- src/server/pps/server/poller.go
+++ src/server/pps/server/poller.go
@@ -224,7 +224,6 @@ func (m *ppsMaster) pollPipelinePods(pollClient *client.APIClient) {
 					}
 				}
 			}
-			return backoff.ErrContinue // keep polling until cancelled (RetryUntilCancel)
 		}
 	}, backoff.NewInfiniteBackOff(), backoff.NotifyContinue("pollPipelinePods"),
 	); err != nil && ctx.Err() == nil {
