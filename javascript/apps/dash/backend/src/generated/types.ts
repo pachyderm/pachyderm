bab9a6fe39d729c65f066c27745d68a0f93d7735
Connor Wallace <25064607+Connor-Wallace@users.noreply.github.com>
2021-05-11 10:04:35 -0600

    combine codegen files (#139)

--- javascript/apps/dash/backend/src/generated/types.ts
+++ javascript/apps/dash/backend/src/generated/types.ts
@@ -8,6 +8,10 @@ import {GraphQLResolveInfo} from 'graphql';
 import {Context} from '@dash-backend/lib/types';
 export type Maybe<T> = T | null;
 export type Exact<T extends {[key: string]: unknown}> = {[K in keyof T]: T[K]};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> &
+  {[SubKey in K]?: Maybe<T[SubKey]>};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> &
+  {[SubKey in K]: Maybe<T[SubKey]>};
 export type RequireFields<T, K extends keyof T> = {
   [X in Exclude<keyof T, K>]?: T[X];
 } &
