db54776699136500fb65f1cfa92b26ed7d2e76d1
Kyle White <kylewhite@gmail.com>
2021-05-21 13:11:18 -0400

    Service account additional annotations (#89)
    * Add service account additional annotations

---
+++ test/rbac_test.go
@@ -0,0 +1,90 @@
+package helmtest
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/helm"
+	v1 "k8s.io/api/core/v1"
+)
+
+func TestServiceAccount(t *testing.T) {
+	helmChartPath := "../pachyderm"
+
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"pachd.storage.backend": "LOCAL",
+		},
+	}
+
+	output := helm.RenderTemplate(t, options, helmChartPath, "blah", []string{"templates/pachd/rbac/serviceaccount.yaml"})
+
+	var serviceAccount v1.ServiceAccount
+
+	helm.UnmarshalK8SYaml(t, output, &serviceAccount)
+
+	if len(serviceAccount.Annotations) != 0 {
+		t.Fatalf("Expected no service account annotations, found %s", serviceAccount.Annotations)
+	}
+}
+
+func TestWorkerServiceAccount(t *testing.T) {
+	helmChartPath := "../pachyderm"
+
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"pachd.storage.backend": "LOCAL",
+		},
+	}
+
+	output := helm.RenderTemplate(t, options, helmChartPath, "blah", []string{"templates/pachd/rbac/worker-serviceaccount.yaml"})
+
+	var serviceAccount v1.ServiceAccount
+
+	helm.UnmarshalK8SYaml(t, output, &serviceAccount)
+
+	if len(serviceAccount.Annotations) != 0 {
+		t.Fatalf("Expected no service account annotations, found %s", serviceAccount.Annotations)
+	}
+}
+
+func TestServiceAccountAdditionalAnnotations(t *testing.T) {
+	helmChartPath := "../pachyderm"
+
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"pachd.storage.backend": "LOCAL",
+		},
+		ValuesFiles: []string{"exampleAnnotations.yaml"},
+	}
+
+	output := helm.RenderTemplate(t, options, helmChartPath, "blah", []string{"templates/pachd/rbac/serviceaccount.yaml"})
+
+	var serviceAccount v1.ServiceAccount
+
+	helm.UnmarshalK8SYaml(t, output, &serviceAccount)
+
+	if serviceAccount.Annotations["eks.amazonaws.com/role-arn"] != "blah123" {
+		t.Fatalf("Service Account annotations not expected %s", serviceAccount.Annotations)
+	}
+}
+
+func TestWorkerServiceAccountAdditionalAnnotations(t *testing.T) {
+	helmChartPath := "../pachyderm"
+
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"pachd.storage.backend": "LOCAL",
+		},
+		ValuesFiles: []string{"exampleWorkerAnnotations.yaml"},
+	}
+
+	output := helm.RenderTemplate(t, options, helmChartPath, "blah", []string{"templates/pachd/rbac/worker-serviceaccount.yaml"})
+
+	var serviceAccount v1.ServiceAccount
+
+	helm.UnmarshalK8SYaml(t, output, &serviceAccount)
+
+	if serviceAccount.Annotations["eks.amazonaws.com/role-arn"] != "blah123" {
+		t.Fatalf("Service Account annotations not expected %s", serviceAccount.Annotations)
+	}
+}
