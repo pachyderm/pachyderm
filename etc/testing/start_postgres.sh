0082ffddd1597437f261f899610279a764f70841
Brendon Carroll <7072757+brendoncarroll@users.noreply.github.com>
2021-05-25 17:27:58 -0400

    wip

--- etc/testing/start_postgres.sh
+++ etc/testing/start_postgres.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
 
-if [ "$CI" = "true" ]
-then 
-    echo "Skipping launching postgres, using PG launched in kube"
-else 
-    if ! docker ps | grep -q postgres
-    then
-        echo "starting postgres..."
-        docker run -d \
-        -e POSTGRES_DB=pgc \
-        -e POSTGRES_HOST_AUTH_METHOD=trust \
-        -p 32228:5432 \
-        postgres:13.0-alpine
-    else
-        echo "postgres already started"
-    fi
+set -ve
+
+if ! docker ps | grep -q postgres
+then
+    echo "starting postgres..."
+    postgres_id=$(docker run -d \
+    -e POSTGRES_DB=pgc \
+    -e POSTGRES_HOST_AUTH_METHOD=trust \
+    -p 30228:5432 \
+    postgres:13.0-alpine)
+
+    postgres_ip=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' $postgres_id)
+
+    docker run -d \
+    -e DB_USER=postgres \
+    -e AUTH_TYPE=trust \
+    -e DB_HOST=$postgres_ip \
+    -e DB_PORT=30228 \
+    -p 30229:5432 \
+    edoburu/pgbouncer:1.15.0
+else
+    echo "postgres already started"
 fi
+
