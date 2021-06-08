c28f8b6dc12ab46ef5099e01af2ef85275b11d2d
A Gardner <3100188+actgardner@users.noreply.github.com>
2021-06-08 16:19:01 -0400

    Try mounting caching dirs

--- etc/testing/circle/env.sh
+++ etc/testing/circle/env.sh
@@ -6,6 +6,9 @@ export GOCACHE
 GOPATH=/home/circleci/.go_workspace
 export GOPATH
 
+MODCACHE=/home/circleci/.go_workspace/pkg/mod
+export MODCACHE
+
 PATH=$(pwd):$(pwd)/cached-deps:$GOPATH/bin:$PATH
 export PATH
 
