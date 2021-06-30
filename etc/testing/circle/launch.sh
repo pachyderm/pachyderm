ed525f266be49cdf0d5775a68282f8749ac20e66
Kyle White <kylewhite@gmail.com>
2021-06-30 22:19:05 +0000

    fix up path

--- etc/testing/circle/launch.sh
+++ etc/testing/circle/launch.sh
@@ -10,7 +10,7 @@ source "$(dirname "$0")/env.sh"
 # so we explicitly create the host path on the host machine and chmod it so we can write to it. 
 minikube ssh 'mkdir -p /tmp/pachyderm/pachd && chmod -R 777 /tmp/pachyderm'
 
-helm install pachyderm ../../helm/pachyderm -f helm-values.yaml
+helm install pachyderm etc/helm/pachyderm -f helm-values.yaml
 
 kubectl wait --for=condition=ready pod -l app=pachd --timeout=5m
 
