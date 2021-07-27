33449c1f564b4337bdd5461b039c16bfb3e6259c
Jeff Rifwald <jmcriffey@users.noreply.github.com>
2021-07-27 14:32:13 -0400

    feat(repo): add polling to repo tab (#355)

--- frontend/src/hooks/useRepo.ts
+++ frontend/src/hooks/useRepo.ts
@@ -1,9 +1,11 @@
+import {REPO_POLL_INTERVAL_MS} from '@dash-frontend/constants/pollIntervals';
 import {useRepoQuery} from '@dash-frontend/generated/hooks';
 import {RepoQueryArgs} from '@graphqlTypes';
 
 const useRepo = (args: RepoQueryArgs) => {
   const {data, error, loading} = useRepoQuery({
     variables: {args},
+    pollInterval: REPO_POLL_INTERVAL_MS,
   });
 
   return {
