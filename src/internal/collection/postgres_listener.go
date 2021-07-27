6653b816194b5e0e3ad7db6a0d3a109f6bd17300
Brendon Carroll <7072757+brendoncarroll@users.noreply.github.com>
2021-07-26 20:07:45 -0400

    fix collection tests on pg_bouncer

--- src/internal/collection/postgres_listener.go
+++ src/internal/collection/postgres_listener.go
@@ -326,6 +326,9 @@ type postgresListener struct {
 }
 
 func NewPostgresListener(dsn string) PostgresListener {
+	// Apparently this was very important for lib/pq to work
+	dsn = strings.Replace(dsn, "statement_cache_mode=describe", "", -1)
+	dsn += " binary_parameters=yes"
 	eg, _ := errgroup.WithContext(context.Background())
 
 	l := &postgresListener{
