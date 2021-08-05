2ad39ca87ab0215f3849684c0c98f329fe010a12
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2021-08-04 21:24:48 -0400

    welp, let's just add everything i think i need to do and debug from there

--- etc/testing/circle/run_hub_tests.sh
+++ etc/testing/circle/run_hub_tests.sh
@@ -2,7 +2,26 @@
 
 set -euxo pipefail
 
+# install hubcli
 wget https://github.com/pachyderm/hubcli/releases/download/v0.0.1-beta.1/hubcli
 chmod a+x hubcli
-./hubcli --endpoint https://hub.pachyderm.com/api/graphql --apikey $HUB_API_KEY --op create-workspace-and-wait --orgid 2193 --loglevel trace --infofile workspace.json
+
+# install pachctl and friends
+make install
+VERSION=$(pachctl version --client-only)
+git config user.email "donotreply@pachyderm.com"
+git config user.name "anonymous"
+git tag -f -am "Circle CI test v$VERSION" v"$VERSION"
+
+# push a docker image
+make docker-build
+
+# create a workspace
+./hubcli --endpoint https://hub.pachyderm.com/api/graphql --apikey $HUB_API_KEY --op create-workspace-and-wait --orgid 2193 --loglevel trace --infofile workspace.json --version $VERSION
+
+# run tests against hub
+pachctl run pfs-load-test
+pachctl run pps-load-test
+
+# delete the workspace
 ./hubcli --endpoint https://hub.pachyderm.com/api/graphql --apikey $HUB_API_KEY --op delete-workspace --loglevel trace --infofile workspace.json
