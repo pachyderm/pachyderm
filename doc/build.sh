be97f199eb8b759a6e52ece6dad30ecfb1342570
Nadege PEPIN <nadege.pepin@pachyderm.io>
2021-08-05 11:19:20 -0400

    more renaming Beta in beta because distracted...

--- doc/build.sh
+++ doc/build.sh
@@ -61,8 +61,8 @@ for d in $all_versions; do
     # rebuild site
     mkdocs build --config-file "${mkdocs_file}" --site-dir "${out_dir}"
 done
-## Temp preview of 2.0.x-Beta out of master
-mkdocs build --config-file "mkdocs-master.yml" --site-dir "site/2.0.x-Beta"
+## Temp preview of 2.0.x-beta out of master
+mkdocs build --config-file "mkdocs-master.yml" --site-dir "site/2.0.x-beta"
 
 # Finally, copy latest version of the docs into 'latest'
 if [[ -z "${latest_version}" ]]; then
