9affe8a637743bd255cca4fc7b219bef492a695b
pierrelamarche <pierrelamarche5@gmail.com>
2021-09-07 13:45:31 -0300

    Fix scrollable table on file browser & capitalization (#403)
    * fix: scrollable table on file browser
    
    * fix: no icon view text capitalize
    
    * fix: newline

--- frontend/src/views/FileBrowser/components/ListViewTable/ListViewTable.tsx
+++ frontend/src/views/FileBrowser/components/ListViewTable/ListViewTable.tsx
@@ -22,54 +22,56 @@ const ListViewTable: React.FC<ListViewTableProps> = ({files}) => {
   } = useListViewTable(files);
 
   return (
-    <Table className={styles.base} data-testid="ListViewTable__view">
-      <Table.Head sticky>
-        <Table.Row>
-          <Table.HeaderCell
-            onClick={nameClick}
-            sortable={true}
-            sortLabel="name"
-            sortSelected={comparatorName === 'Name'}
-            sortReversed={!reversed}
-          >
-            Name
-          </Table.HeaderCell>
-          <Table.HeaderCell
-            onClick={sizeClick}
-            sortable={true}
-            sortLabel="size"
-            sortSelected={comparatorName === 'Size'}
-            sortReversed={!reversed}
-          >
-            Size
-          </Table.HeaderCell>
-          <Table.HeaderCell
-            onClick={dateClick}
-            sortable={true}
-            sortLabel="date"
-            sortSelected={comparatorName === 'Date'}
-            sortReversed={!reversed}
-          >
-            Date Uploaded
-          </Table.HeaderCell>
-          <Table.HeaderCell
-            onClick={typeClick}
-            sortable={true}
-            sortLabel="type"
-            sortSelected={comparatorName === 'Type'}
-            sortReversed={!reversed}
-          >
-            Type
-          </Table.HeaderCell>
-          <Table.HeaderCell>Actions</Table.HeaderCell>
-        </Table.Row>
-      </Table.Head>
-      <Table.Body>
-        {tableData.map((file) => (
-          <FileTableRow file={file} key={file.path} />
-        ))}
-      </Table.Body>
-    </Table>
+    <div className={styles.scrolling}>
+      <Table className={styles.base} data-testid="ListViewTable__view">
+        <Table.Head sticky relativeShadow>
+          <Table.Row>
+            <Table.HeaderCell
+              onClick={nameClick}
+              sortable={true}
+              sortLabel="name"
+              sortSelected={comparatorName === 'Name'}
+              sortReversed={!reversed}
+            >
+              Name
+            </Table.HeaderCell>
+            <Table.HeaderCell
+              onClick={sizeClick}
+              sortable={true}
+              sortLabel="size"
+              sortSelected={comparatorName === 'Size'}
+              sortReversed={!reversed}
+            >
+              Size
+            </Table.HeaderCell>
+            <Table.HeaderCell
+              onClick={dateClick}
+              sortable={true}
+              sortLabel="date"
+              sortSelected={comparatorName === 'Date'}
+              sortReversed={!reversed}
+            >
+              Date Uploaded
+            </Table.HeaderCell>
+            <Table.HeaderCell
+              onClick={typeClick}
+              sortable={true}
+              sortLabel="type"
+              sortSelected={comparatorName === 'Type'}
+              sortReversed={!reversed}
+            >
+              Type
+            </Table.HeaderCell>
+            <Table.HeaderCell>Actions</Table.HeaderCell>
+          </Table.Row>
+        </Table.Head>
+        <Table.Body>
+          {tableData.map((file) => (
+            <FileTableRow file={file} key={file.path} />
+          ))}
+        </Table.Body>
+      </Table>
+    </div>
   );
 };
 
