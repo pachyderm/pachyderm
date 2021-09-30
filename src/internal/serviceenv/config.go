67f7e2548f19bae693a75472cb0c0fd203923780
Bryce McAnally <bryce@pachyderm.io>
2021-09-30 13:39:10 -0500

    Distributed compaction performance improvements (#6955)
    * Improve distributed compaction performance, add file set renewal in compaction, and additional logging for commit finishing / compaction
    
    * Address feedback
    
    * Check commit finished before using details

--- src/internal/serviceenv/config.go
+++ src/internal/serviceenv/config.go
@@ -34,8 +34,8 @@ type GlobalConfiguration struct {
 	PostgresPassword               string `env:"POSTGRES_PASSWORD"`
 	PostgresMaxOpenConns           int    `env:"POSTGRES_MAX_OPEN_CONNS,default=10"`
 	PostgresMaxIdleConns           int    `env:"POSTGRES_MAX_IDLE_CONNS,default=2"`
-	PGBouncerMaxOpenConns          int    `env:"PG_BOUNCER_MAX_OPEN_CONNS,default=100"`
-	PGBouncerMaxIdleConns          int    `env:"PG_BOUNCER_MAX_IDLE_CONNS,default=20"`
+	PGBouncerMaxOpenConns          int    `env:"PG_BOUNCER_MAX_OPEN_CONNS,default=10"`
+	PGBouncerMaxIdleConns          int    `env:"PG_BOUNCER_MAX_IDLE_CONNS,default=2"`
 	PostgresConnMaxLifetimeSeconds int    `env:"POSTGRES_CONN_MAX_LIFETIME_SECONDS,default=0"`
 	PostgresConnMaxIdleSeconds     int    `env:"POSTGRES_CONN_MAX_IDLE_SECONDS,default=0"`
 	PachdServiceHost               string `env:"PACHD_SERVICE_HOST"`
