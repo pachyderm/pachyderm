69aa5747afacca87c2f21cf2bd01fe51e7aa473c
Bryce McAnally <bryce@pachyderm.io>
2021-11-03 14:15:51 -0500

    Use a real context in the chunk renewer (#7051)

--- src/internal/storage/chunk/renewer.go
+++ src/internal/storage/chunk/renewer.go
@@ -12,14 +12,14 @@ type Renewer struct {
 	ss *renew.StringSet
 }
 
-func NewRenewer(tr track.Tracker, name string, ttl time.Duration) *Renewer {
+func NewRenewer(ctx context.Context, tr track.Tracker, name string, ttl time.Duration) *Renewer {
 	renewFunc := func(ctx context.Context, x string, ttl time.Duration) error {
 		_, err := tr.SetTTL(ctx, x, ttl)
 		return err
 	}
 	composeFunc := renew.NewTmpComposer(tr, name)
 	return &Renewer{
-		ss: renew.NewStringSet(context.TODO(), ttl, renewFunc, composeFunc),
+		ss: renew.NewStringSet(ctx, ttl, renewFunc, composeFunc),
 	}
 }
 
