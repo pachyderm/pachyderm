89c97e870695769dac9147c14cd40dee092615a1
rwootton <58671608+rwootton@users.noreply.github.com>
2021-11-08 08:53:59 -0700

    [FRON-129] code preview component (#116)
    * [FRON-129] code preview component
    
    * minor version rev
    
    * package lock
    
    * Updated config.yml
    
    * Updated config.yml

---
+++ packages/components/src/CodePreview/CodePreview.tsx
@@ -0,0 +1,30 @@
+import hljs from 'highlight.js/lib/core';
+import 'highlight.js/styles/googlecode.css';
+import json from 'highlight.js/lib/languages/json';
+import yaml from 'highlight.js/lib/languages/yaml';
+import React, {useRef, useEffect} from 'react';
+
+import styles from './CodePreview.module.css';
+
+hljs.registerLanguage('json', json);
+hljs.registerLanguage('yaml', yaml);
+
+const CodePreview = ({
+  children,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => {
+  const element = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (element.current) {
+      hljs.highlightElement(element.current);
+    }
+  }, [element]);
+  return (
+    <div className={styles.code} ref={element} {...props}>
+      {children}
+    </div>
+  );
+};
+
+export default CodePreview;
