004d93410d40c9a1d01eea44a875d4f4595192a6
Peter Fedak <pzfedak@gmail.com>
2021-11-08 17:45:56 -0700

    Add special error message for inspect job/commit.
    It seems reasonable to expect "inspect job <ID>" to do what "list job
    <ID>" does, so if we detect that usage we should nudge users towards
    the intended command.

--- src/server/pfs/cmds/cmds.go
+++ src/server/pfs/cmds/cmds.go
@@ -366,7 +366,9 @@ $ {{alias}} test@fork -p XXX`,
 		Long:  "Return info about a commit.",
 		Run: cmdutil.RunFixedArgs(1, func(args []string) error {
 			commit, err := cmdutil.ParseCommit(args[0])
-			if err != nil {
+			if err != nil && uuid.IsUUIDWithoutDashes(args[0]) {
+				return errors.New(`Use "list commit <id>" to see commits with a given ID across different repos `)
+			} else {
 				return err
 			}
 			c, err := client.NewOnUserMachine("user")
