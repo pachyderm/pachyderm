fc98a4b08b9960ac0c702d24883965f551473b2e
Albert <albert.cui@pachyderm.io>
2021-11-11 10:42:51 -0500

    Improve CI (#8)
    - add pytest to CI
    - move config from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -52,33 +52,7 @@ setup_args = dict(
     description=pkg_json["description"],
     license=pkg_json["license"],
     license_file="LICENSE",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
-    install_requires=[
-        "jupyter_server>=1.6,<2",
-        "jupyter_packaging~=0.10.0,<2",
-        'jupyterlab>=3.0.14'
-    ],
-    zip_safe=False,
-    include_package_data=True,
-    python_requires=">=3.6",
-    platforms="Linux, Mac OS X, Windows",
-    keywords=["Jupyter", "JupyterLab", "JupyterLab3"],
-    classifiers=[
-        "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Framework :: Jupyter",
-        "Framework :: Jupyter :: JupyterLab",
-        "Framework :: Jupyter :: JupyterLab :: 3",
-        "Framework :: Jupyter :: JupyterLab :: Extensions",
-        "Framework :: Jupyter :: JupyterLab :: Extensions :: Prebuilt",
-    ],
+    keywords=pkg_json["keywords"],
 )
 
 try:
