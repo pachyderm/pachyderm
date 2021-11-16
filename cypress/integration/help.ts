ab1facccde550d973fafe32bf0650e07114fc33a
work <peterjfranchina@gmail.com>
2021-11-16 16:57:20 -0500

    adding more tests

--- cypress/integration/help.ts
+++ cypress/integration/help.ts
@@ -3,12 +3,23 @@ describe('help', () => {
     cy.visit('/lab');
   });
 
-  // afterEach(() => {
-  //   cy.logout();
-  // });
-
   it('Should open the pachyderm docs from the help menu.', () => {
     cy.findAllByText('Help').first().click();
     cy.findByText('Pachyderm Docs');
+  });
+
+  it.only('Should open contact support modal from the help menu.', () => {
+    cy.findAllByText('Help').first().click();
+    cy.findAllByText('Contact Pachyderm Support').click();
+    cy.findByText('Chat with us on');
+    cy.findByText('Slack')
+      .should('have.prop', 'href')
+      .and('include', 'https://slack.pachyderm.io');
+    cy.findByText('Email us at');
+    cy.findByText('support@pachyderm.com').should(
+      'have.prop',
+      'href',
+      'mailto:support@pachyderm.com'
+    );
   });
 });
