8c362ae2a715849d332ef92bdd092838d2e9820b
rwootton <58671608+rwootton@users.noreply.github.com>
2021-11-18 11:25:21 -0700

    export config module (#124)

--- packages/components/src/TutorialModal/index.ts
+++ packages/components/src/TutorialModal/index.ts
@@ -2,5 +2,12 @@ export {default as TutorialModal} from './TutorialModal';
 export type {Section, TaskComponentProps, Step} from './lib/types';
 export {default as TaskCard} from './components/TaskCard';
 export {default as Mark} from './components/Mark';
-export {MultiSelectModule, useMultiSelectModule} from './components/Modules';
-export type {MultiSelectConfig} from './components/Modules';
+export {
+  MultiSelectModule,
+  useMultiSelectModule,
+  ConfigurationUploadModule,
+} from './components/Modules';
+export type {
+  MultiSelectConfig,
+  ConfigurationUploadConfig,
+} from './components/Modules';
