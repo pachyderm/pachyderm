671f0761bac8f9ecf55dd232b7c6a1522c49779b
anonymous <donotreply@pachyderm.com>
2021-12-01 16:22:55 -0600

    re-remove rootless

--- etc/testing/circle/upgrade_test.sh
+++ etc/testing/circle/upgrade_test.sh
@@ -3,12 +3,12 @@
 set -ve
 
 
-git checkout $1 &&
+git checkout "$1" &&
 
 ./etc/testing/circle/build.sh &&
 ./etc/testing/circle/launch.sh &&
 
-git checkout $2 &&
+git checkout "$2" &&
 go test -v ./src/testing/upgrade -run TestPreUpgrade &&
 
 # build and upgrade to HEAD
