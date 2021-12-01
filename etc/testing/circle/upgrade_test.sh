80b3386d59dd257397a2d42af7820e658f500c2c
Alon Cohen <alonco124@gmail.com>
2021-12-01 16:51:21 -0600

    Create a basic upgrade test in CI (#7118)
    * Create a basic upgrade test in CI

---
+++ etc/testing/circle/upgrade_test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -ve
+
+
+git checkout "$1" &&
+
+./etc/testing/circle/build.sh &&
+./etc/testing/circle/launch.sh &&
+
+git checkout "$2" &&
+go test -v ./src/testing/upgrade -run TestPreUpgrade &&
+
+# build and upgrade to HEAD
+./etc/testing/circle/build.sh &&
+./etc/testing/circle/helm-upgrade.sh &&
+go test -v ./src/testing/upgrade -run TestPostUpgrade
