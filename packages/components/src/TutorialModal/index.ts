c913b5c69beb18dbbf13f827f91005dfb2fadbf5
Connor Wallace <25064607+Connor-Wallace@users.noreply.github.com>
2021-12-01 11:00:37 -0700

    export terminal component (#133)

--- packages/components/src/TutorialModal/index.ts
+++ packages/components/src/TutorialModal/index.ts
@@ -2,6 +2,7 @@ export {default as TutorialModal} from './TutorialModal';
 export type {Section, TaskComponentProps, Story} from './lib/types';
 export {default as TaskCard} from './components/TaskCard';
 export {default as Mark} from './components/Mark';
+export {default as Terminal} from './components/Terminal';
 export {
   MultiSelectModule,
   useMultiSelectModule,
