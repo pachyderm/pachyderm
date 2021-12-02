157689422147aa96ba9bb2d8b521a10e0144e9a8
Cam <cpepin@fastmail.com>
2021-12-02 15:57:34 -0500

    Updating protos to 2.0.3-1 (#470)

--- scripts/checkVersion.js
+++ scripts/checkVersion.js
@@ -1,9 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 // The last part of the npm version is only for npm usage
-const packageVersion = require('../backend/node_modules/@pachyderm/node-pachyderm/package.json').dependencies[
-  '@pachyderm/proto'
-];
+const packageVersion = require('../backend/node_modules/@pachyderm/node-pachyderm/version.json').pachyderm;
 const versionVersion = require('../version.json').pachyderm;
 
 if (packageVersion !== versionVersion) {
