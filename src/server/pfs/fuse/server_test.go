7d0103b48e0a9725c54a2eb6ee6fd165d03b6873
Luke Marsden <me@lukemarsden.net>
2021-12-06 17:35:20 +0000

    introduce a testing strategy

---
+++ src/server/pfs/fuse/server_test.go
@@ -0,0 +1,79 @@
+package fuse
+
+import (
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/pachyderm/pachyderm/v2/src/client"
+	"github.com/pachyderm/pachyderm/v2/src/internal/dockertestenv"
+	"github.com/pachyderm/pachyderm/v2/src/internal/require"
+	"github.com/pachyderm/pachyderm/v2/src/internal/testpachd"
+)
+
+func TestBasicServer(t *testing.T) {
+	env := testpachd.NewRealEnv(t, dockertestenv.NewTestDBConfig(t))
+	require.NoError(t, env.PachClient.CreateRepo("repo"))
+	commit := client.NewCommit("repo", "master", "")
+	err := env.PachClient.PutFile(commit, "dir/file1", strings.NewReader("foo"))
+	require.NoError(t, err)
+	err = env.PachClient.PutFile(commit, "dir/file2", strings.NewReader("foo"))
+	require.NoError(t, err)
+	withServerMount(t, env.PachClient, nil, func(mountPoint string) {
+		repos, err := ioutil.ReadDir(mountPoint)
+		require.NoError(t, err)
+		require.Equal(t, 1, len(repos))
+		require.Equal(t, "repo", filepath.Base(repos[0].Name()))
+
+		files, err := ioutil.ReadDir(filepath.Join(mountPoint, "repo"))
+		require.NoError(t, err)
+		require.Equal(t, 1, len(files))
+		require.Equal(t, "dir", filepath.Base(files[0].Name()))
+
+		files, err = ioutil.ReadDir(filepath.Join(mountPoint, "repo", "dir"))
+		require.NoError(t, err)
+		require.Equal(t, 2, len(files))
+		require.Equal(t, "file1", filepath.Base(files[0].Name()))
+		require.Equal(t, "file2", filepath.Base(files[1].Name()))
+
+		data, err := ioutil.ReadFile(filepath.Join(mountPoint, "repo", "dir", "file1"))
+		require.NoError(t, err)
+		require.Equal(t, "foo", string(data))
+	})
+}
+
+// TODO: pass reference to the MountManager object to the test func, so that the
+// test can call MountBranch, UnmountBranch etc directly for convenience
+func withServerMount(tb testing.TB, c *client.APIClient, opts *Options, f func(mountPoint string)) {
+	dir := tb.TempDir()
+	if opts == nil {
+		opts = &Options{}
+	}
+	if opts.Unmount == nil {
+		opts.Unmount = make(chan struct{})
+	}
+	unmounted := make(chan struct{})
+	var mountErr error
+	defer func() {
+		close(opts.Unmount)
+		<-unmounted
+		require.NoError(tb, mountErr)
+	}()
+	defer func() {
+		// recover because panics leave the mount in a weird state that makes
+		// it hard to rerun the tests, mostly relevent when you're iterating on
+		// these tests, or the code they test.
+		if r := recover(); r != nil {
+			tb.Fatal(r)
+		}
+	}()
+	go func() {
+		mountErr = Mount(c, dir, opts)
+		close(unmounted)
+	}()
+	// Gotta give the fuse mount time to come up.
+	time.Sleep(2 * time.Second)
+	f(dir)
+}
