b097175adba5ee98f71a48fd7a7734a617325b8b
Kyle White <kylewhite@gmail.com>
2021-12-09 20:31:25 +0000

    Add kube, Try previewing stack

--- etc/pulumi/main.go
+++ etc/pulumi/main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"github.com/pulumi/pulumi-gcp/sdk/v5/go/gcp/container"
+	"github.com/pulumi/pulumi-gcp/sdk/v5/go/gcp/projects"
 	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/storage"
+	"github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes"
+	appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/apps/v1"
+	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/core/v1"
+	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/meta/v1"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
@@ -17,6 +23,146 @@ func main() {
 
 		// Export the DNS name of the bucket
 		ctx.Export("bucketName", bucket.Url)
+
+		containerService, err := projects.NewService(ctx, "project", &projects.ServiceArgs{
+			Service: pulumi.String("container.googleapis.com"),
+		})
+		if err != nil {
+			return err
+		}
+
+		engineVersions, err := container.GetEngineVersions(ctx, &container.GetEngineVersionsArgs{})
+		if err != nil {
+			return err
+		}
+		masterVersion := engineVersions.LatestMasterVersion
+
+		cluster, err := container.NewCluster(ctx, "demo-cluster", &container.ClusterArgs{
+			InitialNodeCount: pulumi.Int(2),
+			MinMasterVersion: pulumi.String(masterVersion),
+			NodeVersion:      pulumi.String(masterVersion),
+			NodeConfig: &container.ClusterNodeConfigArgs{
+				MachineType: pulumi.String("n1-standard-1"),
+				OauthScopes: pulumi.StringArray{
+					pulumi.String("https://www.googleapis.com/auth/compute"),
+					pulumi.String("https://www.googleapis.com/auth/devstorage.read_only"),
+					pulumi.String("https://www.googleapis.com/auth/logging.write"),
+					pulumi.String("https://www.googleapis.com/auth/monitoring"),
+				},
+			},
+		}, pulumi.DependsOn([]pulumi.Resource{containerService}))
+		if err != nil {
+			return err
+		}
+
+		ctx.Export("kubeconfig", generateKubeconfig(cluster.Endpoint, cluster.Name, cluster.MasterAuth))
+
+		k8sProvider, err := kubernetes.NewProvider(ctx, "k8sprovider", &kubernetes.ProviderArgs{
+			Kubeconfig: generateKubeconfig(cluster.Endpoint, cluster.Name, cluster.MasterAuth),
+		}, pulumi.DependsOn([]pulumi.Resource{cluster}))
+		if err != nil {
+			return err
+		}
+
+		namespace, err := corev1.NewNamespace(ctx, "app-ns", &corev1.NamespaceArgs{
+			Metadata: &metav1.ObjectMetaArgs{
+				Name: pulumi.String("demo-ns"),
+			},
+		}, pulumi.Provider(k8sProvider))
+		if err != nil {
+			return err
+		}
+
+		appLabels := pulumi.StringMap{
+			"app": pulumi.String("demo-app"),
+		}
+		_, err = appsv1.NewDeployment(ctx, "app-dep", &appsv1.DeploymentArgs{
+			Metadata: &metav1.ObjectMetaArgs{
+				Namespace: namespace.Metadata.Elem().Name(),
+			},
+			Spec: appsv1.DeploymentSpecArgs{
+				Selector: &metav1.LabelSelectorArgs{
+					MatchLabels: appLabels,
+				},
+				Replicas: pulumi.Int(3),
+				Template: &corev1.PodTemplateSpecArgs{
+					Metadata: &metav1.ObjectMetaArgs{
+						Labels: appLabels,
+					},
+					Spec: &corev1.PodSpecArgs{
+						Containers: corev1.ContainerArray{
+							corev1.ContainerArgs{
+								Name:  pulumi.String("demo-app"),
+								Image: pulumi.String("jocatalin/kubernetes-bootcamp:v2"),
+							}},
+					},
+				},
+			},
+		}, pulumi.Provider(k8sProvider))
+		if err != nil {
+			return err
+		}
+
+		service, err := corev1.NewService(ctx, "app-service", &corev1.ServiceArgs{
+			Metadata: &metav1.ObjectMetaArgs{
+				Namespace: namespace.Metadata.Elem().Name(),
+				Labels:    appLabels,
+			},
+			Spec: &corev1.ServiceSpecArgs{
+				Ports: corev1.ServicePortArray{
+					corev1.ServicePortArgs{
+						Port:       pulumi.Int(80),
+						TargetPort: pulumi.Int(8080),
+					},
+				},
+				Selector: appLabels,
+				Type:     pulumi.String("LoadBalancer"),
+			},
+		}, pulumi.Provider(k8sProvider))
+		if err != nil {
+			return err
+		}
+
+		ctx.Export("url", service.Status.ApplyT(func(status *corev1.ServiceStatus) *string {
+			ingress := status.LoadBalancer.Ingress[0]
+			if ingress.Hostname != nil {
+				return ingress.Hostname
+			}
+			return ingress.Ip
+		}))
+
 		return nil
 	})
+}
+
+func generateKubeconfig(clusterEndpoint pulumi.StringOutput, clusterName pulumi.StringOutput,
+	clusterMasterAuth container.ClusterMasterAuthOutput) pulumi.StringOutput {
+	context := pulumi.Sprintf("demo_%s", clusterName)
+
+	return pulumi.Sprintf(`apiVersion: v1
+clusters:
+- cluster:
+    certificate-authority-data: %s
+    server: https://%s
+  name: %s
+contexts:
+- context:
+    cluster: %s
+    user: %s
+  name: %s
+current-context: %s
+kind: Config
+preferences: {}
+users:
+- name: %s
+  user:
+    auth-provider:
+      config:
+        cmd-args: config config-helper --format=json
+        cmd-path: gcloud
+        expiry-key: '{.credential.token_expiry}'
+        token-key: '{.credential.access_token}'
+      name: gcp`,
+		clusterMasterAuth.ClusterCaCertificate().Elem(),
+		clusterEndpoint, context, context, context, context, context, context)
 }
