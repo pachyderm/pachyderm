7b9358a8dcdda3001875cd9e30c00b06f38efad0
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2021-12-09 17:40:22 -0500

    make nstreams easily configurable

--- src/oneoff/test-hanging-streams/main.go
+++ src/oneoff/test-hanging-streams/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
 	"sync/atomic"
 	"time"
@@ -11,25 +12,33 @@ import (
 	"google.golang.org/grpc/metadata"
 )
 
+var (
+	nstreams = flag.Int("n", 10, "number of streams to hold open")
+)
+
 func main() {
+	flag.Parse()
+
 	cc, err := grpc.Dial("localhost:80", grpc.WithInsecure())
 	if err != nil {
 		log.Fatal(err)
 	}
 	doneCh := make(chan int)
 	ppsC := pps.NewAPIClient(cc)
-	nstreams := 10
-	var open, gotMsg int64
-	for i := 0; i < nstreams; i++ {
+	var open, errs, gotMsg int64
+	for i := 0; i < *nstreams; i++ {
 		go func(i int) {
 			defer func() {
 				doneCh <- i
 			}()
+			// ctx, c := context.WithTimeout(context.Background(), 5*time.Second)
+			// defer c()
 			ctx := metadata.AppendToOutgoingContext(context.Background(), "authn-token", "a37bcba91de049aaa5ee1643472bf490")
 			stream, err := ppsC.ListJobSet(ctx, &pps.ListJobSetRequest{
 				Details: true,
 			})
 			if err != nil {
+				atomic.AddInt64(&errs, 1)
 				log.Printf("%d: start stream: %v", i, err)
 				return
 			}
@@ -39,6 +48,7 @@ func main() {
 			log.Printf("%d: stream open", i)
 
 			if _, err := stream.Recv(); err != nil {
+				atomic.AddInt64(&errs, 1)
 				log.Printf("%d: recv: %v", i, err)
 				return
 			}
@@ -50,10 +60,10 @@ func main() {
 	go func() {
 		for {
 			time.Sleep(time.Second)
-			log.Printf("%v open, %v got message", atomic.LoadInt64(&open), atomic.LoadInt64(&gotMsg))
+			log.Printf("%v open, %v got message, %v errored", atomic.LoadInt64(&open), atomic.LoadInt64(&gotMsg), atomic.LoadInt64(&errs))
 		}
 	}()
-	for i := 1; i < nstreams; i++ {
+	for i := 1; i < *nstreams; i++ {
 		streamID := <-doneCh
 		log.Printf("stream %d done", streamID)
 	}
