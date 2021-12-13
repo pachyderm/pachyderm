5a2756659e338de7d6ff29d37b31c805326058c9
acohen4 <alonco124@gmail.com>
2021-12-13 11:36:06 -0500

    Updating to v1/Ingress in hub's helm-test

--- etc/helm/test/hub_test.go
+++ etc/helm/test/hub_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/gruntwork-io/terratest/modules/helm"
 	appsV1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
+	netV1 "k8s.io/api/networking/v1"
 )
 
 func TestHub(t *testing.T) {
@@ -42,7 +43,7 @@ func TestHub(t *testing.T) {
 	}
 	for _, object := range objects {
 		switch object := object.(type) {
-		case *appsV1.Ingress:
+		case *netV1.Ingress:
 			for _, rule := range object.Spec.Rules {
 				if rule.Host == "console.test" {
 					checks["ingress"] = true
