8cfc0eb0934870dc04af45f3c28df16f2a3af505
rwootton <58671608+rwootton@users.noreply.github.com>
2021-12-15 10:27:19 -0700

    fix form modal sizing & add small and success props (#138)
    * fix form modal sizing & add small and success props
    
    * next package version
    
    * add disabled to form modal too

--- packages/components/src/Modal/FormModal/FormModal.tsx
+++ packages/components/src/Modal/FormModal/FormModal.tsx
@@ -18,10 +18,14 @@ export interface FormModalProps<T> {
   onHide?: () => void;
   error?: string;
   loading?: boolean;
+  updating?: boolean;
   formContext: UseFormReturn<T>;
   onSubmit: SubmitHandler<T>;
   confirmText?: string;
   headerText?: string;
+  success?: boolean;
+  small?: boolean;
+  disabled?: boolean;
 }
 
 const FormModal = <T,>({
@@ -30,31 +34,47 @@ const FormModal = <T,>({
   onHide = noop,
   error = '',
   loading,
+  updating,
   formContext,
   onSubmit,
   confirmText = 'Submit',
   headerText,
+  success,
+  small,
+  disabled,
 }: FormModalProps<T>) => {
+  const modalStatus =
+    (updating && 'updating') ||
+    (error && 'error') ||
+    (success && 'success') ||
+    null;
+
+  const modalStatusMessage = error || (success && "You're all set!") || '';
+
   return (
-    <Modal show={isOpen} onHide={onHide} actionable>
-      {error && <ModalStatus status="error">{error}</ModalStatus>}
+    <Form formContext={formContext} onSubmit={onSubmit}>
+      <Modal show={isOpen} onHide={onHide} small={small} actionable>
+        {modalStatus ? (
+          <ModalStatus status={modalStatus}>{modalStatusMessage}</ModalStatus>
+        ) : null}
 
-      <ModalHeader onHide={onHide} actionable>
-        {headerText}
-      </ModalHeader>
+        <ModalHeader onHide={onHide} actionable>
+          {headerText}
+        </ModalHeader>
 
-      <Form formContext={formContext} onSubmit={onSubmit}>
         <ModalBody>{loading ? <LoadingDots /> : children}</ModalBody>
 
         <ModalFooter
           buttonType="submit"
           confirmText={confirmText}
-          disabled={loading}
-          onConfirm={noop}
+          disabled={disabled}
+          onConfirm={() => {
+            formContext.handleSubmit(onSubmit);
+          }}
           onHide={onHide}
         />
-      </Form>
-    </Modal>
+      </Modal>
+    </Form>
   );
 };
 
