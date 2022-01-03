c2d19a110d716d0a6c8fc93844cb77467802b93c
Connor Wallace <25064607+Connor-Wallace@users.noreply.github.com>
2022-01-03 16:58:17 -0500

    add e2e test for commit browser (#485)

--- cypress/integration/Project.ts
+++ cypress/integration/Project.ts
@@ -1,6 +1,8 @@
 describe('Project', () => {
   beforeEach(() => {
-    cy.login();
+    cy.login().then(() => {
+      cy.findAllByText('View Project').eq(0).click();
+    });
   });
 
   afterEach(() => {
@@ -8,10 +10,21 @@ describe('Project', () => {
   });
 
   it('should navigate to the project page', () => {
-    cy.findAllByText('View Project').eq(0).click();
     cy.findByText('Show Jobs');
     cy.findByText('Reset Canvas');
     cy.findByText('images');
     cy.findAllByText('edges').should('have.length', 2);
   });
+
+  it('should show the correct number of commits', () => {
+
+    cy.findByText('images').click();
+    cy.findAllByText('Committed', {exact: false}).should('have.length', 2);
+    cy.findAllByText('(57.27 KB)', {exact: false}).should('have.length', 2);
+    cy.findByRole('button', {name: 'Close'}).click();
+    cy.findAllByText('edges').eq(0).click();
+    cy.findAllByText('Committed', {exact: false}).should('have.length', 1);
+    cy.findByText('(22.22 KB)', {exact: false});
+
+  })
 });
