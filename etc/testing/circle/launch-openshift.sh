d80e702ef4d76b9fb20d9ac59c6ea10a20a696fc
acohen4 <alonco124@gmail.com>
2022-01-04 17:20:20 -1000

    fox

--- etc/testing/circle/launch-openshift.sh
+++ etc/testing/circle/launch-openshift.sh
@@ -14,9 +14,7 @@ PACHD_REPO="${REGISTRY}/${PROJECT}/pachd"
 WORKER_REPO="${REGISTRY}/${PROJECT}/worker"
 
 helm install pachyderm etc/helm/pachyderm -f etc/testing/circle/helm-values.yaml \
---set etcd.securityContext.enabled=false --set pachd.securityContext.enabled=false \
---set pachd.image.repository=${PACHD_REPO} --set pachd.worker.image.repository=${WORKER_REPO} \
---set postgresql.enabled=true postgresql.securityContext.enabled=false \
---set postgresql.persistence.enabled=false --set postgresql.containerSecurityContext.enabled=false;
+-f etc/testing/circle/helm-values-openshift.yaml \
+--set pachd.image.repository=${PACHD_REPO} --set pachd.worker.image.repository=${WORKER_REPO};
 
 kubectl wait --for=condition=ready pod -l app=pachd --timeout=5m
