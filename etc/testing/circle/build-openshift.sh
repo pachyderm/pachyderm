3eb789e956652038ee5e8da94298f235c02b8a5f
acohen4 <alonco124@gmail.com>
2022-01-05 06:44:20 -1000

    move checkout step

--- etc/testing/circle/build-openshift.sh
+++ etc/testing/circle/build-openshift.sh
@@ -14,12 +14,11 @@ git config user.name "anonymous"
 git tag -f -am "Circle CI test v$VERSION" v"$VERSION"
 make docker-build
 
-# REGISTRY="default-route-openshift-image-registry.apps-crc.testing"
-REGISTRY=$(oc get route -n openshift-image-registry | awk 'FNR == 2 {print $2}') &&
-docker login -u unused -p $(oc whoami -t) ${REGISTRY} &&
+REGISTRY="$(oc get route -n openshift-image-registry | awk 'FNR == 2 {print $2}')" &&
+docker login -u unused -p "$(oc whoami -t)" "${REGISTRY}";
 
-docker tag pachyderm/pachd:local ${REGISTRY}/pach-test/pachd:local &&
-docker tag pachyderm/worker:local ${REGISTRY}/pach-test/worker:local &&
+docker tag pachyderm/pachd:local "${REGISTRY}"/pach-test/pachd:local &&
+docker tag pachyderm/worker:local "${REGISTRY}"/pach-test/worker:local &&
 
-docker push ${REGISTRY}/pach-test/pachd:local &&
-docker push ${REGISTRY}/pach-test/worker:local 
+docker push "${REGISTRY}"/pach-test/pachd:local &&
+docker push "${REGISTRY}"/pach-test/worker:local 
