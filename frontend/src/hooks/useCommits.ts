3c060186b611e010a10399ced9de8226a0e42703
rwootton <58671608+rwootton@users.noreply.github.com>
2022-01-21 09:38:07 -0700

    [FRON-278] poll commits query (#498)

--- frontend/src/hooks/useCommits.ts
+++ frontend/src/hooks/useCommits.ts
@@ -1,5 +1,6 @@
 import {CommitsQueryArgs} from '@graphqlTypes';
 
+import {COMMITS_POLL_INTERVAL_MS} from '@dash-frontend/constants/pollIntervals';
 import {useGetCommitsQuery} from '@dash-frontend/generated/hooks';
 
 export const COMMIT_LIMIT = 100;
@@ -11,6 +12,7 @@ type UseCommitArgs = {
 
 const useCommits = ({args, skip = false}: UseCommitArgs) => {
   const {data, error, loading} = useGetCommitsQuery({
+    pollInterval: COMMITS_POLL_INTERVAL_MS,
     variables: {args},
     skip,
   });
