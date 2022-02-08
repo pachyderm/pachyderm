e8cf6057c33c4d98db50d86fcc033bebd8fa670a
Cam <cpepin@fastmail.com>
2022-02-08 15:56:42 -0500

    FRON-327: Adding warnings/cleaning up tutorials if user leaves lineage or the window (#522)

--- frontend/src/views/Project/tutorials/ProjectTutorial.tsx
+++ frontend/src/views/Project/tutorials/ProjectTutorial.tsx
@@ -1,4 +1,5 @@
-import React, {useEffect} from 'react';
+import React, {useCallback, useEffect} from 'react';
+import {Prompt} from 'react-router';
 
 import useLocalProjectSettings from '@dash-frontend/hooks/useLocalProjectSettings';
 import useUrlQueryState from '@dash-frontend/hooks/useUrlQueryState';
@@ -18,6 +19,9 @@ const TUTORIALS: TutorialMap = {
   'image-processing': ImageProcessing,
 };
 
+const WARNING_MESSAGE =
+  'Are you sure you want to leave? All tutorial progress will be lost.';
+
 const ProjectTutorial: React.FC = () => {
   const {projectId} = useUrlState();
   const {viewState, updateViewState} = useUrlQueryState();
@@ -27,21 +31,55 @@ const ProjectTutorial: React.FC = () => {
     key: 'active_tutorial',
   });
 
+  const onClose = useCallback(() => {
+    updateViewState({tutorialId: undefined});
+    setActiveTutorial(null);
+  }, [updateViewState, setActiveTutorial]);
+
   useEffect(() => {
     if (viewState.tutorialId && viewState.tutorialId !== id) {
       setActiveTutorial(viewState.tutorialId);
     }
   }, [viewState.tutorialId, setActiveTutorial, id]);
 
-  const onClose = () => {
-    updateViewState({tutorialId: undefined});
-    setActiveTutorial(null);
-  };
+  useEffect(() => {
+    if (id) {
+      const beforeUnloadListener = (event: BeforeUnloadEvent) => {
+        event.preventDefault();
+
+        // Firefox will automatically trigger the unload prompt by
+        // having a truthy event listener. Chrome requires you to have a
+        // non-undefined returned value.
+        event.returnValue = '';
+      };
+
+      window.addEventListener('beforeunload', beforeUnloadListener, {
+        capture: true,
+      });
+
+      window.addEventListener('unload', onClose);
+
+      return () => {
+        window.removeEventListener('beforeunload', beforeUnloadListener, {
+          capture: true,
+        });
+
+        window.removeEventListener('unload', onClose);
+
+        onClose();
+      };
+    }
+  }, [id, onClose]);
 
   if (id) {
     const Tutorial = TUTORIALS[id];
     if (Tutorial) {
-      return <Tutorial onClose={onClose} />;
+      return (
+        <>
+          <Prompt message={WARNING_MESSAGE} />
+          <Tutorial onClose={onClose} />
+        </>
+      );
     }
   }
   return null;
