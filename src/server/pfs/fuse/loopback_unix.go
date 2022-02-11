219c48a4c012d7c24a39736813c52dff0da81749
Sahith <sahith.malyala@pachyderm.io>
2022-02-10 19:25:26 -0500

    Change to symlink unix methods as this is what go-fuse does

--- src/server/pfs/fuse/loopback_unix.go
+++ src/server/pfs/fuse/loopback_unix.go
@@ -12,21 +12,21 @@ import (
 )
 
 func (n *loopbackNode) Getxattr(ctx context.Context, attr string, dest []byte) (uint32, unix.Errno) {
-	sz, err := unix.Getxattr(n.path(), attr, dest)
+	sz, err := unix.Lgetxattr(n.path(), attr, dest)
 	return uint32(sz), fs.ToErrno(err)
 }
 
 func (n *loopbackNode) Setxattr(ctx context.Context, attr string, data []byte, flags uint32) unix.Errno {
-	err := unix.Setxattr(n.path(), attr, data, int(flags))
+	err := unix.Lsetxattr(n.path(), attr, data, int(flags))
 	return fs.ToErrno(err)
 }
 
 func (n *loopbackNode) Removexattr(ctx context.Context, attr string) unix.Errno {
-	err := unix.Removexattr(n.path(), attr)
+	err := unix.Lremovexattr(n.path(), attr)
 	return fs.ToErrno(err)
 }
 
 func (n *loopbackNode) Listxattr(ctx context.Context, dest []byte) (uint32, unix.Errno) {
-	sz, err := unix.Listxattr(n.path(), dest)
+	sz, err := unix.Llistxattr(n.path(), dest)
 	return uint32(sz), fs.ToErrno(err)
 }
