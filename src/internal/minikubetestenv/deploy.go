1ece5a876b05c282a5410b52a61279a1f642a5e6
Alon Cohen <alonco124@gmail.com>
2022-02-24 04:13:29 -0800

    Testing enterprise deployments (#7299)
    * Testing enterprise deployments
    
    * Login with mock IDP in deploy enterprise tests

--- src/internal/minikubetestenv/deploy.go
+++ src/internal/minikubetestenv/deploy.go
@@ -2,6 +2,7 @@ package minikubetestenv
 
 import (
 	"context"
+	"fmt"
 	"strings"
 	"testing"
 	"time"
@@ -10,7 +11,9 @@ import (
 	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/pachyderm/pachyderm/v2/src/client"
 	"github.com/pachyderm/pachyderm/v2/src/internal/backoff"
+	"github.com/pachyderm/pachyderm/v2/src/internal/config"
 	"github.com/pachyderm/pachyderm/v2/src/internal/errors"
+	"github.com/pachyderm/pachyderm/v2/src/internal/grpcutil"
 	"github.com/pachyderm/pachyderm/v2/src/internal/require"
 	"github.com/pachyderm/pachyderm/v2/src/internal/testutil"
 	v1 "k8s.io/api/core/v1"
@@ -24,7 +27,21 @@ const (
 	helmRelease            = "test-release"
 	ns                     = "default"
 	localImage             = "local"
+	licenseKeySecretName   = "enterprise-license-key-secret"
 )
+
+func getPachAddress(t testing.TB) *grpcutil.PachdAddress {
+	cfg, err := config.Read(false, false)
+	require.NoError(t, err)
+
+	_, context, err := cfg.ActiveContext(true)
+	require.NoError(t, err)
+
+	pa, err := grpcutil.ParsePachdAddress(context.PachdAddress)
+	require.NoError(t, err)
+
+	return pa
+}
 
 func localDeploymentWithMinioOptions(namespace, image string) *helm.Options {
 	return &helm.Options{
@@ -32,8 +49,10 @@ func localDeploymentWithMinioOptions(namespace, image string) *helm.Options {
 		SetValues: map[string]string{
 			"deployTarget": "custom",
 
-			"pachd.service.type":           "NodePort",
-			"pachd.image.tag":              image,
+			"pachd.service.type":        "NodePort",
+			"pachd.image.tag":           image,
+			"pachd.clusterDeploymentID": "dev",
+
 			"pachd.storage.backend":        "MINIO",
 			"pachd.storage.minio.bucket":   "pachyderm-test",
 			"pachd.storage.minio.endpoint": "minio.default.svc.cluster.local:9000",
@@ -50,7 +69,41 @@ func localDeploymentWithMinioOptions(namespace, image string) *helm.Options {
 	}
 }
 
-func waitForPachd(t *testing.T, ctx context.Context, kubeClient *kube.Clientset, namespace, version string) {
+func withEnterprise(t testing.TB, namespace string) *helm.Options {
+	addr := getPachAddress(t)
+	return &helm.Options{
+		KubectlOptions: &k8s.KubectlOptions{Namespace: namespace},
+		SetValues: map[string]string{
+			"pachd.enterpriseLicenseKeySecretName": licenseKeySecretName,
+			"pachd.rootToken":                      testutil.RootToken,
+			"pachd.oauthClientSecret":              "oidc-client-secret",
+			"pachd.enterpriseSecret":               "enterprise-secret",
+			"oidc.userAccessibleOauthIssuerHost":   fmt.Sprintf("%s:30658", addr.Host),
+			"ingress.host":                         fmt.Sprintf("%s:30657", addr.Host),
+		},
+	}
+}
+
+func union(a, b *helm.Options) *helm.Options {
+	c := &helm.Options{
+		KubectlOptions: &k8s.KubectlOptions{Namespace: b.KubectlOptions.Namespace},
+		SetValues:      make(map[string]string),
+		SetStrValues:   make(map[string]string),
+	}
+	copy := func(src, dst *helm.Options) {
+		for k, v := range src.SetValues {
+			dst.SetValues[k] = v
+		}
+		for k, v := range src.SetStrValues {
+			dst.SetStrValues[k] = v
+		}
+	}
+	copy(a, c)
+	copy(b, c)
+	return c
+}
+
+func waitForPachd(t testing.TB, ctx context.Context, kubeClient *kube.Clientset, namespace, version string) {
 	require.NoError(t, backoff.Retry(func() error {
 		pachds, err := kubeClient.CoreV1().Pods(namespace).List(ctx, metav1.ListOptions{LabelSelector: "app=pachd"})
 		if err != nil {
@@ -66,19 +119,37 @@ func waitForPachd(t *testing.T, ctx context.Context, kubeClient *kube.Clientset,
 }
 
 // Deploy pachyderm using a `helm install ...`, then run a test with an API Client corresponding to the deployment
-func InstallPublishedRelease(t *testing.T, ctx context.Context, kubeClient *kube.Clientset, version, user string) *client.APIClient {
+func InstallPublishedRelease(t testing.TB, ctx context.Context, kubeClient *kube.Clientset, version, user string, cleanup bool) *client.APIClient {
+	maybeCleanup(t, cleanup, kubeClient)
 	require.NoError(t, helm.InstallE(t, localDeploymentWithMinioOptions(ns, version), helmChartPublishedPath, helmRelease))
 	waitForPachd(t, ctx, kubeClient, ns, version)
 	return testutil.AuthenticatedPachClient(t, testutil.NewPachClient(t), user)
 }
 
-func UpgradeRelease(t *testing.T, ctx context.Context, kubeClient *kube.Clientset, user string) *client.APIClient {
+func UpgradeRelease(t testing.TB, ctx context.Context, kubeClient *kube.Clientset, user string, cleanup bool) *client.APIClient {
+	maybeCleanup(t, cleanup, kubeClient)
 	require.NoError(t, helm.UpgradeE(t, localDeploymentWithMinioOptions(ns, localImage), helmChartLocalPath, helmRelease))
 	waitForPachd(t, ctx, kubeClient, ns, localImage)
 	return testutil.AuthenticatedPachClient(t, testutil.NewPachClient(t), user)
 }
 
-func DeleteRelease(t *testing.T, ctx context.Context, kubeClient *kube.Clientset) {
+func InstallReleaseEnterprise(t testing.TB, ctx context.Context, kubeClient *kube.Clientset, user string, cleanup bool) *client.APIClient {
+	maybeCleanup(t, cleanup, kubeClient)
+	createSecretEnterpriseKeySecret(t, ctx, kubeClient, ns)
+	opts := union(localDeploymentWithMinioOptions(ns, localImage), withEnterprise(t, ns))
+	require.NoError(t, helm.InstallE(t, opts, helmChartLocalPath, helmRelease))
+	waitForPachd(t, ctx, kubeClient, ns, localImage)
+	return testutil.AuthenticatedPachClientPostActivate(t, testutil.NewPachClient(t), user)
+}
+func maybeCleanup(t testing.TB, cleanup bool, kubeClient *kube.Clientset) {
+	if cleanup {
+		t.Cleanup(func() {
+			deleteRelease(t, context.Background(), kubeClient)
+		})
+	}
+}
+
+func deleteRelease(t testing.TB, ctx context.Context, kubeClient *kube.Clientset) {
 	options := &helm.Options{
 		KubectlOptions: &k8s.KubectlOptions{Namespace: ns},
 	}
@@ -95,4 +166,14 @@ func DeleteRelease(t *testing.T, ctx context.Context, kubeClient *kube.Clientset
 		}
 		return errors.Errorf("pvcs have yet to be deleted")
 	}, backoff.RetryEvery(5*time.Second).For(2*time.Minute)))
+}
+
+func createSecretEnterpriseKeySecret(t testing.TB, ctx context.Context, kubeClient *kube.Clientset, ns string) {
+	_, err := kubeClient.CoreV1().Secrets(ns).Create(ctx, &v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{Name: licenseKeySecretName},
+		StringData: map[string]string{
+			"enterprise-license-key": testutil.GetTestEnterpriseCode(t),
+		},
+	}, metav1.CreateOptions{})
+	require.True(t, err == nil || strings.Contains(err.Error(), "already exists"))
 }
