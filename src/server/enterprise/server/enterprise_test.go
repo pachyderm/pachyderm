ad99eecb68d6d29b54130fde5dede185aa1bfe53
Robert A. Uhl <robert.uhl@pachyderm.io>
2022-03-03 12:45:41 -0500

    Correct sense of test

--- src/server/enterprise/server/enterprise_test.go
+++ src/server/enterprise/server/enterprise_test.go
@@ -338,7 +338,7 @@ func TestPauseUnpause(t *testing.T) {
 
 	// ListRepo should return an error since the cluster is paused now
 	_, err = client.ListRepo()
-	require.NoError(t, err)
+	require.YesError(t, err)
 
 	_, err = client.Enterprise.Unpause(client.Ctx(), &enterprise.UnpauseRequest{})
 	require.NoError(t, err)
