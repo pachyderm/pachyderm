ba03b251389e4ba7f11cda8be6733e78b96d1383
Tyler Britten <1933680+tybritten@users.noreply.github.com>
2022-03-04 13:37:21 -0500

    fix example

--- etc/testing/examples.sh
+++ etc/testing/examples.sh
@@ -134,9 +134,6 @@ pushd examples/word_count/
         pachctl create pipeline -f reduce.json
     popd
 
-    pachctl list file attributes@master
-    pachctl create pipeline -f julia_infer.json
-
     pachctl wait commit "reduce@master"
 
     # just make sure we outputted some files that were right
