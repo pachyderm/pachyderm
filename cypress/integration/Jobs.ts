c1818468f706cd87d2d04cffb84a4c9da5ac1aa6
pierrelamarche <pierrelamarche5@gmail.com>
2022-03-14 11:12:20 -0300

    chore: add E2E test for jobset details (#551)

---
+++ cypress/integration/Jobs.ts
@@ -0,0 +1,33 @@
+describe('Jobs', () => {
+    before(() => {
+      cy.setupProject().visit('/');
+    })
+
+    beforeEach(() => {
+      cy.findAllByText('View Project').eq(0).click();
+    });
+
+    afterEach(() => {
+      cy.visit('/')
+    })
+  
+    after(() => {
+      cy.deleteReposAndPipelines().logout();
+    })
+  
+    it('should show jobset details', () => {
+      cy.findByText('Jobs', {timeout: 12000}).click();
+      cy.findByText('See Details').click();
+      cy.findByTestId('InfoPanel__commitLink', {timeout: 12000}).should('contain', /^[a-zA-Z0-9_-]{32}/);
+      cy.findByTestId('InfoPanel__pipeline').should('have.text', 'edges');
+      // wait for job to finish
+      cy.findByTestId('InfoPanel__state', {timeout: 12000}).should('have.text', 'Success');
+      cy.findByTestId('InfoPanel__started').should('include.text', 'ago');
+      cy.findByTestId('InfoPanel__duration').should('include.text', 'seconds');
+      cy.findByTestId('InfoPanel__processed').should('have.text', '1');
+      cy.findByTestId('InfoPanel__skipped').should('have.text', '0');
+      cy.findByTestId('InfoPanel__failed').should('have.text', '0');
+      cy.findByTestId('InfoPanel__recovered').should('have.text', '0');
+      cy.findByTestId('InfoPanel__total').should('have.text', '1');
+    });
+  });
