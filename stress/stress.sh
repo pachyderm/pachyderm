6e4e32b0c0e8b8d4e6783a533f4b25778e942f01
Luke Marsden <me@lukemarsden.net>
2022-03-21 18:02:19 +0000

    fast

--- stress/stress.sh
+++ stress/stress.sh
@@ -10,22 +10,13 @@ sleep 10
 while true; do
 
     pachctl create repo data
-    sleep 1
     echo "version 1" | pachctl put file data@master:/myfile.txt
-    sleep 1
     curl -XPUT 'localhost:9002/repos/data/master/_mount?name=data&mode=ro'
-    sleep 1
     cat /pfs/data/myfile.txt
-    sleep 1
     curl -XPUT 'localhost:9002/repos/data/master/_unmount?name=data'
-    sleep 1
     pachctl create branch data@v1 --head master
-    sleep 1
     curl -XPUT 'localhost:9002/repos/data/master/_mount?name=data&mode=ro'
-    sleep 1
     cat /pfs/data/myfile.txt
-    sleep 1
     pachctl delete repo data
-    sleep 1
 
 done
