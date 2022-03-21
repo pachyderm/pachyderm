bf38b5ae9b272ab36c1870847ae4dd61ca57b406
Luke Marsden <me@lukemarsden.net>
2022-03-21 17:55:20 +0000

    make rerunnable

--- stress/stress.sh
+++ stress/stress.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -xeuo pipefail
 
+pachctl delete repo data || true
+umount /pfs || true
+
+# waiting for the browser to prompt the python backend to prompt the mount server to exist again
+sleep 10
+
 while true; do
 
     pachctl create repo data
