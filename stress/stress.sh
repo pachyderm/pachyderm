5bdc98a73b360526f40dc7306f1228c4318260f4
Luke Marsden <me@lukemarsden.net>
2022-03-23 15:51:21 +0000

    log cases

--- stress/stress.sh
+++ stress/stress.sh
@@ -12,11 +12,13 @@ while true; do
     pachctl create repo data
     echo "version 1" | pachctl put file data@master:/myfile.txt
     curl -XPUT 'localhost:9002/repos/data/master/_mount?name=data&mode=ro'
+    echo case 1
     ls /pfs/data |grep myfile
     cat /pfs/data/myfile.txt
     curl -XPUT 'localhost:9002/repos/data/master/_unmount?name=data'
     pachctl create branch data@v1 --head master
     curl -XPUT 'localhost:9002/repos/data/master/_mount?name=data&mode=ro'
+    echo case 2
     ls /pfs/data |grep myfile
     cat /pfs/data/myfile.txt
     pachctl delete repo data
