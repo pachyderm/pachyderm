b52f22f34cc5f6417bb07cfd258bdbe7594d798e
Luke Marsden <me@lukemarsden.net>
2022-03-23 15:40:41 +0000

    Add directory listing

--- stress/stress.sh
+++ stress/stress.sh
@@ -12,10 +12,12 @@ while true; do
     pachctl create repo data
     echo "version 1" | pachctl put file data@master:/myfile.txt
     curl -XPUT 'localhost:9002/repos/data/master/_mount?name=data&mode=ro'
+    ls /pfs/data |grep myfile
     cat /pfs/data/myfile.txt
     curl -XPUT 'localhost:9002/repos/data/master/_unmount?name=data'
     pachctl create branch data@v1 --head master
     curl -XPUT 'localhost:9002/repos/data/master/_mount?name=data&mode=ro'
+    ls /pfs/data |grep myfile
     cat /pfs/data/myfile.txt
     pachctl delete repo data
 
