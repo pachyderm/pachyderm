dec974eca636247de474f77f9aaee20dda5d7c22
Jose Molina-Melendez <molinamelendezj@gmail.com>
2022-03-23 18:09:04 -0400

    add the --wait all for minikube start (#7427)
    * add the --wait all for minikube start
    
    * adding a timeout for docker daemon to be ready

--- etc/testing/circle/start-minikube.sh
+++ etc/testing/circle/start-minikube.sh
@@ -9,6 +9,7 @@ VERSION=v1.19.0
 minikube_args=(
   "--vm-driver=docker"
   "--kubernetes-version=${VERSION}"
+  "--wait=all"
 )
 while getopts ":v" opt; do
   case "${opt}" in
@@ -20,6 +21,17 @@ while getopts ":v" opt; do
       exit 1
       ;;
   esac
+done
+
+# wait for docker or timeout
+timeout=120
+while ! docker version >/dev/null 2>&1; do
+  timeout=$((timeout - 1))
+  if [ $timeout -eq 0 ]; then
+    echo "Timed out waiting for docker daemon"
+    exit 1
+  fi
+  sleep 1
 done
 
 minikube start "${minikube_args[@]}"
