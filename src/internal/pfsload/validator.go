1fdf52332aa0849d6bb03efdc17ba21fff223c47
Bryce McAnally <bryce@pachyderm.io>
2022-03-24 11:01:54 -0500

    Distribute load testing (#7388)
    * Distribute load testing
    
    * Make distributed load testing work with auth
    
    * Filter auth errors when auth is disabled
    
    * Address feedback

--- src/internal/pfsload/validator.go
+++ src/internal/pfsload/validator.go
@@ -3,6 +3,7 @@ package pfsload
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"io"
 	"math/rand"
 	"strings"
@@ -10,196 +11,147 @@ import (
 	"github.com/pachyderm/pachyderm/v2/src/client"
 	"github.com/pachyderm/pachyderm/v2/src/internal/errors"
 	"github.com/pachyderm/pachyderm/v2/src/internal/storage/fileset"
-	"github.com/pachyderm/pachyderm/v2/src/internal/tarutil"
 	"github.com/pachyderm/pachyderm/v2/src/pfs"
-	pfsserver "github.com/pachyderm/pachyderm/v2/src/server/pfs"
 )
-
-type FrequencySpec struct {
-	Count int `yaml:"count,omitempty"`
-	Prob  int `yaml:"prob,omitempty"`
-	count int
-}
-
-type ValidatorSpec struct {
-	FrequencySpec *FrequencySpec `yaml:"frequency,omitempty"`
-}
 
 type Validator struct {
 	spec   *ValidatorSpec
-	buffer *fileset.Buffer
 	random *rand.Rand
-	files  []string
+	// TODO: Potentially make this pachhash.Output.
+	hash  []byte
+	count int64
 }
 
-func NewValidator(client Client, spec *ValidatorSpec, random *rand.Rand) (Client, *Validator, error) {
-	if spec.FrequencySpec != nil {
-		if err := validateProb(spec.FrequencySpec.Prob); err != nil {
-			return nil, nil, err
+func NewValidator(spec *ValidatorSpec, random *rand.Rand) (*Validator, error) {
+	if spec.Frequency != nil {
+		if err := validateProb(int(spec.Frequency.Prob)); err != nil {
+			return nil, err
 		}
 	}
-	v := &Validator{
+	return &Validator{
 		spec:   spec,
-		buffer: fileset.NewBuffer(),
 		random: random,
-	}
-	return &validatorClient{
-		Client:    client,
-		validator: v,
-	}, v, nil
+		hash:   pfs.NewHash().Sum(nil),
+	}, nil
 }
 
-// TODO: The performance is better than it was, but long sequences of alternating add / delete can still be bad.
-func (v *Validator) RandomFile() (string, error) {
-	if v.files == nil {
-		v.files = []string{}
-		if err := v.buffer.WalkAdditive(func(p, _ string, r io.Reader) error {
-			v.files = append(v.files, p)
-			return nil
-		}); err != nil {
-			return "", err
-		}
-		v.random.Shuffle(len(v.files), func(i, j int) {
-			v.files[i], v.files[j] = v.files[j], v.files[i]
-		})
-	}
-	if len(v.files) > 0 {
-		file := v.files[0]
-		v.files = v.files[1:]
-		return file, nil
-	}
-	return "no-op-delete", nil
+func (v *Validator) AddHash(hash []byte) {
+	v.hash = xor(v.hash, hash)
 }
 
-type file struct {
-	name string
-	hash []byte
+func xor(b1, b2 []byte) []byte {
+	result := make([]byte, len(b1))
+	for i, b := range b1 {
+		result[i] = b ^ b2[i]
+	}
+	return result
 }
 
+// TODO: Distribute validation and revert back to being based on file path + content rather than
+// file path + size.
 func (v *Validator) Validate(client Client, commit *pfs.Commit) (retErr error) {
-	if v.spec.FrequencySpec != nil {
-		freq := v.spec.FrequencySpec
+	if v.spec.Frequency != nil {
+		freq := v.spec.Frequency
 		if freq.Count > 0 {
-			freq.count++
-			if freq.Count > freq.count {
+			v.count++
+			if freq.Count > v.count {
 				return nil
 			}
-			freq.count = 0
-		} else if !shouldExecute(v.random, freq.Prob) {
+			v.count = 0
+		} else if !shouldExecute(v.random, int(freq.Prob)) {
 			return nil
 		}
-	}
-	var files []*file
-	if err := v.buffer.WalkAdditive(func(p, tag string, r io.Reader) error {
-		buf := &bytes.Buffer{}
-		if _, err := io.Copy(buf, r); err != nil {
-			return errors.EnsureStack(err)
-		}
-		files = append(files, &file{
-			name: p,
-			hash: buf.Bytes(),
-		})
-		return nil
-	}); err != nil {
-		return err
 	}
 	err := client.WaitCommitSet(commit.ID, func(ci *pfs.CommitInfo) error {
 		if ci.Commit.Branch.Repo.Type != pfs.UserRepoType {
 			return nil
 		}
-		return validate(client, ci.Commit, files)
+		return validate(client, ci.Commit, v.hash)
 	})
 	return errors.EnsureStack(err)
 }
 
-func validate(client Client, commit *pfs.Commit, files []*file) (retErr error) {
-	defer func() {
-		if retErr == nil {
-			if len(files) != 0 {
-				retErr = errors.Errorf("got back less files than expected")
-			}
-		}
-	}()
-	r, err := client.GetFileTAR(client.Ctx(), commit, "**")
-	if err != nil {
-		return errors.EnsureStack(err)
-	}
-	err = tarutil.Iterate(r, func(file tarutil.File) error {
-		if len(files) == 0 {
-			return errors.Errorf("got back more files than expected")
-		}
-		hdr, err := file.Header()
-		if err != nil {
-			return errors.EnsureStack(err)
-		}
-		if strings.HasSuffix(hdr.Name, "/") {
+func validate(client Client, commit *pfs.Commit, expectedHash []byte) error {
+	hash := pfs.NewHash().Sum(nil)
+	if err := client.GlobFile(client.Ctx(), commit, "**", func(fi *pfs.FileInfo) error {
+		if strings.HasSuffix(fi.File.Path, "/") {
 			return nil
 		}
 		h := pfs.NewHash()
-		if err := file.Content(h); err != nil {
-			return errors.EnsureStack(err)
-		}
-		if !bytes.Equal(files[0].hash, h.Sum(nil)) {
-			return errors.Errorf("file %v's content is incorrect (actual file path: %v)", files[0].name, hdr.Name)
-		}
-		files = files[1:]
+		h.Write([]byte(fi.File.Path))
+		h.Write([]byte(fmt.Sprintf("%016d", fi.SizeBytes)))
+		hash = xor(hash, h.Sum(nil))
 		return nil
-	}, true)
-	if pfsserver.IsFileNotFoundErr(err) && len(files) == 0 {
-		return nil
+	}); err != nil {
+		return errors.EnsureStack(err)
 	}
-	return err
+	if !bytes.Equal(hash, expectedHash) {
+		return errors.Errorf("hash of files at commit %v different from validator hash", commit)
+	}
+	return nil
 }
 
 type validatorClient struct {
 	Client
-	validator *Validator
+	hash []byte
 }
 
-func (vc *validatorClient) WithModifyFileClient(ctx context.Context, commit *pfs.Commit, cb func(client.ModifyFile) error) error {
-	err := vc.Client.WithModifyFileClient(ctx, commit, func(mf client.ModifyFile) (retErr error) {
-		vmfc := &validatorModifyFileClient{
+func NewValidatorClient(client Client) *validatorClient {
+	return &validatorClient{
+		Client: client,
+	}
+}
+
+func (vc *validatorClient) WithCreateFileSetClient(ctx context.Context, cb func(client.ModifyFile) error) (*pfs.CreateFileSetResponse, error) {
+	resp, err := vc.Client.WithCreateFileSetClient(ctx, func(mf client.ModifyFile) error {
+		vcfsc := &validatorCreateFileSetClient{
 			ModifyFile: mf,
-			buffer:     fileset.NewBuffer(),
 		}
-		if err := cb(vmfc); err != nil {
+		if err := cb(vcfsc); err != nil {
 			return err
 		}
-		for _, p := range vmfc.deletes {
-			vc.validator.buffer.Delete(p, fileset.DefaultFileDatum)
+		if vcfsc.hash == nil {
+			return nil
 		}
-		return vmfc.buffer.WalkAdditive(func(p, tag string, r io.Reader) error {
-			vc.validator.files = nil
-			w := vc.validator.buffer.Add(p, tag)
-			_, err := io.Copy(w, r)
-			return errors.EnsureStack(err)
-		})
+		if vc.hash == nil {
+			vc.hash = vcfsc.hash
+			return nil
+		}
+		vc.hash = xor(vc.hash, vcfsc.hash)
+		return nil
 	})
-	return errors.EnsureStack(err)
+	return resp, errors.EnsureStack(err)
 }
 
-type validatorModifyFileClient struct {
+type validatorCreateFileSetClient struct {
 	client.ModifyFile
-	buffer  *fileset.Buffer
-	deletes []string
+	hash []byte
 }
 
-func (vmfc *validatorModifyFileClient) PutFile(path string, r io.Reader, opts ...client.PutFileOption) error {
-	h := pfs.NewHash()
-	if err := vmfc.ModifyFile.PutFile(path, io.TeeReader(r, h), opts...); err != nil {
+func (vcfsc *validatorCreateFileSetClient) PutFile(path string, r io.Reader, opts ...client.PutFileOption) error {
+	path = fileset.Clean(path, false)
+	sr := &sizeReader{Reader: r}
+	if err := vcfsc.ModifyFile.PutFile(path, sr, opts...); err != nil {
 		return errors.EnsureStack(err)
 	}
-	vmfc.buffer.Delete(path, fileset.DefaultFileDatum)
-	w := vmfc.buffer.Add(path, fileset.DefaultFileDatum)
-	_, err := io.Copy(w, bytes.NewReader(h.Sum(nil)))
-	return errors.EnsureStack(err)
-}
-
-func (vmfc *validatorModifyFileClient) DeleteFile(path string, opts ...client.DeleteFileOption) error {
-	if err := vmfc.ModifyFile.DeleteFile(path, opts...); err != nil {
-		return errors.EnsureStack(err)
+	hash := pfs.NewHash()
+	hash.Write([]byte(path))
+	hash.Write([]byte(fmt.Sprintf("%016d", sr.size)))
+	if vcfsc.hash == nil {
+		vcfsc.hash = hash.Sum(nil)
+		return nil
 	}
-	vmfc.deletes = append(vmfc.deletes, path)
-	vmfc.buffer.Delete(path, fileset.DefaultFileDatum)
+	vcfsc.hash = xor(vcfsc.hash, hash.Sum(nil))
 	return nil
+}
+
+type sizeReader struct {
+	io.Reader
+	size int64
+}
+
+func (sr *sizeReader) Read(data []byte) (int, error) {
+	n, err := sr.Reader.Read(data)
+	sr.size += int64(n)
+	return n, errors.EnsureStack(err)
 }
