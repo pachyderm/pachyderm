de70780753c33142039d93f5b7d7390af0188067
Bryce McAnally <bryce@pachyderm.io>
2022-03-31 13:20:40 -0500

    Add more seeds to the stable hash test

--- src/internal/storage/fileset/fileset_test.go
+++ src/internal/storage/fileset/fileset_test.go
@@ -203,16 +203,58 @@ func countChunks(t *testing.T, s *Storage) (count int64) {
 
 // This test ensures that future changes do not affect the stable hashes of files since that would be a breaking change.
 func TestStableHash(t *testing.T) {
-	seed := int64(1648577872380609229)
-	msg := fmt.Sprint("seed: ", strconv.FormatInt(seed, 10))
-	output, err := pachhash.ParseHex([]byte("27e12145099615b6bf0364a4472452dfe0e8105e6d58d7fbc5d0c038c7a50736"))
-	require.NoError(t, err)
-	random := rand.New(rand.NewSource(seed))
-	testStableHash(t, randutil.Bytes(random, 100*units.KB), output[:], msg)
-	output, err = pachhash.ParseHex([]byte("5672e6f3e1841f3f1e284c2d4b7c12dc213ffc88878c9d3e2302be8acd0198ef"))
-	require.NoError(t, err)
-	random = rand.New(rand.NewSource(seed))
-	testStableHash(t, randutil.Bytes(random, 100*units.MB), output[:], msg)
+	type testData struct {
+		seed     int64
+		expected []string
+	}
+	tds := []*testData{
+		&testData{
+			seed: 1648577872380609229,
+			expected: []string{
+				"27e12145099615b6bf0364a4472452dfe0e8105e6d58d7fbc5d0c038c7a50736",
+				"5672e6f3e1841f3f1e284c2d4b7c12dc213ffc88878c9d3e2302be8acd0198ef",
+			},
+		},
+		&testData{
+			seed: 1648742949150704545,
+			expected: []string{
+				"020e7fda5b3d81b000918ac4fa808a6569fc01000d15322247206aeeea1761a2",
+				"d1842da7cfdc60c4a63c8ab58bd8eee4fa765a270743f3aa36ece6cbef786423",
+			},
+		},
+		&testData{
+			seed: 1648742961991348032,
+			expected: []string{
+				"8a801a53ba2933cccea69fdb61eb3aa04b2019eea541aa848c6b42ef4c7262d5",
+				"d578bcae30c790722048f7d698a6279049012a42b339e802da07bca269831b30",
+			},
+		},
+		&testData{
+			seed: 1648742974827637769,
+			expected: []string{
+				"70e7271fc45251fcf788175df481ab6fd090f37860fcdc63b9f25c26834adf52",
+				"b480bf9cffce51b52615a59dc254647145c6376551cf864d2eb87c63bd11344d",
+			},
+		},
+		&testData{
+			seed: 1648742988445537518,
+			expected: []string{
+				"ca46ed3b9bc6a9b090d94a71d4d308ca05580ab467681cdb236b097206feba5a",
+				"79d4bc9c45b07aecaf19b0fe3cea273b059d70c23dabbe3f9d575c1051dddd44",
+			},
+		},
+	}
+	for _, td := range tds {
+		msg := fmt.Sprint("seed: ", strconv.FormatInt(td.seed, 10))
+		output, err := pachhash.ParseHex([]byte(td.expected[0]))
+		require.NoError(t, err)
+		random := rand.New(rand.NewSource(td.seed))
+		testStableHash(t, randutil.Bytes(random, 100*units.KB), output[:], msg)
+		output, err = pachhash.ParseHex([]byte(td.expected[1]))
+		require.NoError(t, err)
+		random = rand.New(rand.NewSource(td.seed))
+		testStableHash(t, randutil.Bytes(random, 100*units.MB), output[:], msg)
+	}
 }
 
 func TestStableHashFuzz(t *testing.T) {
