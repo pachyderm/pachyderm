6138c6fae8f2781073a57c368d3326f57f399b68
Jose Molina-Melendez <molinamelendezj@gmail.com>
2022-04-03 00:25:22 -0400

    debug pachd testing pod

--- etc/testing/circle/launch.sh
+++ etc/testing/circle/launch.sh
@@ -10,7 +10,11 @@ kubectl apply -f etc/testing/minio.yaml
 
 helm install pachyderm etc/helm/pachyderm -f etc/testing/circle/helm-values.yaml
 
+kubectl logs -l app=pachd
+
 kubectl wait --for=condition=ready pod -l app=pachd --timeout=5m
+
+kubectl logs -l app=pachd
 
 # Wait for loki to be deployed
 kubectl wait --for=condition=ready pod -l app=loki --timeout=5m
