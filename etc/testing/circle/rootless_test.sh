2e149464ddb2963f612ffb1ce209f4d0830a311a
molinamelendezj <molinamelendezj@gmail.com>
2022-04-04 17:39:20 -0400

    wait for docker ready on rootless tests

--- etc/testing/circle/rootless_test.sh
+++ etc/testing/circle/rootless_test.sh
@@ -6,14 +6,26 @@ export PATH="${PWD}:${PWD}/cached-deps:${GOPATH}/bin:${PATH}"
 
 VERSION=v1.19.0
 
+# wait for docker or timeout
+timeout=120
+while ! docker version >/dev/null 2>&1; do
+  timeout=$((timeout - 1))
+  if [ $timeout -eq 0 ]; then
+    echo "Timed out waiting for docker daemon"
+    exit 1
+  fi
+  sleep 1
+done
+
 # start minikube with pod security admission plugin
 minikube start \
     --vm-driver=docker \
     --kubernetes-version=${VERSION} \
+    --cpus=7 \
+    --memory=12Gi \
+    --wait=all \
     --extra-config=apiserver.enable-admission-plugins=PodSecurityPolicy \
     --addons=pod-security-policy
-
-minikube addons enable default-storageclass
 
 # add a PodSecurityPolicy which disables root
 kubectl delete psp restricted privileged || true
