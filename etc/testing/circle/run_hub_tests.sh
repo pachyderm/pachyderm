8bed95ed7412c297debc24e1beb7f057b5e7776c
molinamelendezj <molinamelendezj@gmail.com>
2022-04-21 14:14:39 -0400

    avoid set flag error for default case

--- etc/testing/circle/run_hub_tests.sh
+++ etc/testing/circle/run_hub_tests.sh
@@ -24,19 +24,23 @@ if [ "${?}" -ne 0 ]; then
 	exit 1
 fi
 
+set +u
 DURATION=$(echo "$PFS_RESPONSE_SPEC" | jq '.duration')
 DURATION=${DURATION: 1:-2}
 bq insert --ignore_unknown_values insights.load-tests << EOF
   {"gitBranch": "$CIRCLE_BRANCH","specName": "$BUCKET","duration": ${DURATION:='default_load'},"type": "PFS", "commit": "$CIRCLE_SHA1", "timeStamp": "$(date +%Y-%m-%dT%H:%M:%S)"}
 EOF
+set -u
 
 PPS_RESPONSE_SPEC=$(pachctl run pps-load-test "${@}")
 
+set +u
 DURATION=$(echo "$PPS_RESPONSE_SPEC" | jq '.duration')
 DURATION=${DURATION: 1:-2}
 bq insert --ignore_unknown_values insights.load-tests << EOF
   {"gitBranch": "$CIRCLE_BRANCH","specName": "$BUCKET","duration": ${DURATION:='default_load'},"type": "PPS", "commit": "$CIRCLE_SHA1", "timeStamp": "$(date +%Y-%m-%dT%H:%M:%S)"} 
 EOF
+set -u
 
 if [ "${?}" -ne 0 ]; then
 	pachctl debug dump /tmp/debug-dump
