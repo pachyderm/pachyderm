b1d2cfd8d89057ad8d1b8d3c05de8e8c8f41b6d4
Jose Molina-Melendez <molinamelendezj@gmail.com>
2022-04-22 11:51:48 -0400

    migrate ci load test runs off of hub (#7530)
    * capture run pfs-load-test results
    
    * make bucket optional for run on demand
    
    * temp default to bucket 7 for load test
    
    * remove spec print
    
    * bq upload step
    
    * use $BUCKET var as spec name
    
    * escape \<<
    
    * use local job env variables instead
    
    * remove local job env var
    
    * adjust zone and region of gcp proj
    
    * clean up
    
    * move bq insert into local scope
    
    * add additional data elements to load test metric
    
    * trying load test on minikube xlarge circle machine
    
    * test run LOAD3
    
    * try run load test manually
    
    * remove manual load test run
    
    * switch hub live test to minikube
    
    * switch ci load test to xlarge for required 7cpus
    
    * address linter
    
    * avoid set flag error for default case
    
    * final test run
    
    * remove testing default
    
    * save bq results only for nightly runs
    
    * remove extra =
    
    * remove default test value
    
    * remove hub refs from file names

---
+++ etc/testing/circle/run_load_tests.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+mkdir -p "${HOME}/go/bin"
+export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
+export GOPATH="${HOME}/go"
+
+pachctl version --client-only
+
+# Set version for docker builds.
+VERSION="$(pachctl version --client-only)"
+export VERSION
+
+# Print client and server versions, for debugging.
+pachctl version
+
+# Run load tests.
+set +e
+PFS_RESPONSE_SPEC=$(pachctl run pfs-load-test "${@}")
+
+if [ "${?}" -ne 0 ]; then
+	pachctl debug dump /tmp/debug-dump
+	exit 1
+fi
+
+if [ "$CIRCLE_JOB" = "nightly_load" ]; then
+
+DURATION=$(echo "$PFS_RESPONSE_SPEC" | jq '.duration')
+DURATION=${DURATION: 1:-2}
+bq insert --ignore_unknown_values insights.load-tests << EOF
+  {"gitBranch": "$CIRCLE_BRANCH","specName": "$BUCKET","duration": $DURATION,"type": "PFS", "commit": "$CIRCLE_SHA1", "timeStamp": "$(date +%Y-%m-%dT%H:%M:%S)"}
+EOF
+
+fi
+
+PPS_RESPONSE_SPEC=$(pachctl run pps-load-test "${@}")
+
+if [ "$CIRCLE_JOB" = "nightly_load" ]; then
+
+DURATION=$(echo "$PPS_RESPONSE_SPEC" | jq '.duration')
+DURATION=${DURATION: 1:-2}
+bq insert --ignore_unknown_values insights.load-tests << EOF
+  {"gitBranch": "$CIRCLE_BRANCH","specName": "$BUCKET","duration": $DURATION,"type": "PPS", "commit": "$CIRCLE_SHA1", "timeStamp": "$(date +%Y-%m-%dT%H:%M:%S)"} 
+EOF
+
+fi
+
+if [ "${?}" -ne 0 ]; then
+	pachctl debug dump /tmp/debug-dump
+	exit 1
+fi
+set -e
+pachctl debug dump /tmp/debug-dump
