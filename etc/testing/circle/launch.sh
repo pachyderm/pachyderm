dc1163a5155a1504ec51ea6436f42112f4aed19e
acohen4 <alonco124@gmail.com>
2022-05-04 18:44:50 -0400

    Try to remove Loki from default namespace in CI

--- etc/testing/circle/launch.sh
+++ etc/testing/circle/launch.sh
@@ -8,8 +8,4 @@ source "$(dirname "$0")/env.sh"
 # deploy object storage
 kubectl apply -f etc/testing/minio.yaml
 
-helm install pachyderm etc/helm/pachyderm -f etc/testing/circle/helm-values.yaml
-
-kubectl wait --for=condition=ready pod -l app=pachd --timeout=5m
-
 pachctl config update context "$(pachctl config get active-context)" --pachd-address="$(minikube ip):30650"
