5157c4d91435b3735000db479d86ab2eaf44d619
Robert A. Uhl <robert.uhl@pachyderm.io>
2022-05-05 09:26:45 -0400

    Delint

--- src/server/pps/cmds/cmds_test.go
+++ src/server/pps/cmds/cmds_test.go
@@ -833,6 +833,7 @@ func TestJobDatumCount(t *testing.T) {
 	`, "pipeline", tu.UniqueString("p-")).Run())
 	// need some time for the pod to spin up
 	require.NoErrorWithinTRetry(t, 2*time.Minute, func() error {
+		//nolint:wrapcheck
 		return tu.BashCmd(`
 pachctl list job -x | match ' / 1'
 `).Run()
