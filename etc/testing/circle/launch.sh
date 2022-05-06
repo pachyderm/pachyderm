5fc30518190d3712739d8a7d5a39bbafe7619c42
Alon Cohen <alonco124@gmail.com>
2022-05-06 15:58:41 -0400

    Deploy Loki  in test clusters (#7591)
    * Try to remove Loki from default namespace in CI

--- etc/testing/circle/launch.sh
+++ etc/testing/circle/launch.sh
@@ -12,8 +12,4 @@ helm install pachyderm etc/helm/pachyderm -f etc/testing/circle/helm-values.yaml
 
 kubectl wait --for=condition=ready pod -l app=pachd --timeout=5m
 
-# Wait for loki to be deployed
-kubectl wait --for=condition=ready pod -l app=loki --timeout=5m
-kubectl wait --for=condition=ready pod -l app=promtail --timeout=5m
-
 pachctl config update context "$(pachctl config get active-context)" --pachd-address="$(minikube ip):30650"
