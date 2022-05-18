c017fecf6c4e8440f10447cee02e230b7699f4b8
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2022-05-17 21:17:57 -0400

    Add a proxy to expose all external services on one port (#7525)
    * add envoy deployment and service
    
    add envoy.yaml
    
    add the envoy config
    
    attempt to test thru envoy
    
    build the envoy config file with jsonnet; add ports that directly route to particular services
    
    use our own headless services to find console and pachd, so that the user's settings for port numbers doesn't invalidate the envoy config
    
    let the user configure the port numbers for the direct service routes
    
    adjust config to test pachd through the multiplexed port, and everything else thru direct ports
    
    envoy.yaml is now envoy.json, and is autogenerated
    
    * route minikubetestenv traffic through the proxy
    
    * minikubetestenv: check that pachd is ready to receive requests
    
    envoy: make grpc timeout 3600s, for sloooow tests (and a good idea in general actually)
    
    generate-envoy-yaml: rename, since we actually generate JSON now
    
    helpers: ensure that issuerURI ends with /dex when the proxy is enabled
    
    envoy: completely disable stream_idle_timeout
    
    format
    
    test jsonnetfmttedness and envoy config validity
    
    enable the envoy test
    
    envoy tests: fixes
    
    envoy tests: sudo make me a sandwich
    
    envoy tests: fixes
    
    envoy: continue to try to make the tests work
    
    envoy: run tests in their container, since the debian packages are SUPER OUTDATED
    
    envoy test: fixes
    
    envoy test: fixes
    
    envoy test: fixes
    
    envoy test: fixes
    
    envoy test: fixes
    
    revert pachd health check timing
    
    proxy: disable by default, but start it with the tests and adjust tests to use /dex instead of / for auth things
    
    deploy: handle portOffset = 0 specially
    
    make OIDCTokenForTrustedApp use /dex
    
    tests: more /dex addition
    
    one more "dex"
    
    envoy.jsonnet: let httpListener take a list of routes
    
    envoy: route S3v2 traffic
    
    tweak health checks
    
    envoy: correctly route to envoy
    
    * envoy: run as user 101 instead of root
    
    * CI: bring back ClusterIP by default (lost during rebase)
    
    * envoy: route Ingress thru envoy when enabled
    
    * serve services internally on the same ports as externally; avoid the proxy for != 2.3 releases
    
    * use the proxy when version is unset
    
    * also serve the grpc port
    
    * ignore health checks for now
    
    * retry cluster DNS discovery more quickly
    
    * wait for endpoint readiness before load tests and examples
    
    * avoid some shellcheck warnings
    
    * avoid timing out file uploads after 300s
    
    * envoy.json: explain why the multiplexed route has services listed in a particular order; autogenerate all direct- routes, but sorted by internal port
    
    * helm: make the envoy image configurable with repository/tag/pullPolicy
    
    * helm: remove spurious envoyImage field

--- etc/testing/circle/run_load_tests.sh
+++ etc/testing/circle/run_load_tests.sh
@@ -13,7 +13,20 @@ VERSION="$(pachctl version --client-only)"
 export VERSION
 
 # Print client and server versions, for debugging.
-pachctl version
+helm install pachyderm etc/helm/pachyderm -f etc/testing/circle/helm-values.yaml
+
+kubectl wait --for=condition=ready pod -l app=pachd --timeout=5m
+
+# Print client and server versions, for debugging.  (Also waits for proxy to discover pachd, etc.)
+for i in $(seq 1 20); do
+    if pachctl version; then
+        echo "pachd ready after $i attempts"
+        break
+    else
+        sleep 5
+        continue
+    fi
+done
 
 # Run load tests.
 set +e
@@ -41,7 +54,7 @@ if [[ "$CIRCLE_JOB" == *"nightly_load"* ]]; then
 DURATION=$(echo "$PPS_RESPONSE_SPEC" | jq '.duration')
 DURATION=${DURATION: 1:-2}
 bq insert --ignore_unknown_values insights.load-tests << EOF
-  {"gitBranch": "$CIRCLE_BRANCH","specName": "$BUCKET","duration": $DURATION,"type": "PPS", "commit": "$CIRCLE_SHA1", "timeStamp": "$(date +%Y-%m-%dT%H:%M:%S)"} 
+  {"gitBranch": "$CIRCLE_BRANCH","specName": "$BUCKET","duration": $DURATION,"type": "PPS", "commit": "$CIRCLE_SHA1", "timeStamp": "$(date +%Y-%m-%dT%H:%M:%S)"}
 EOF
 
 fi
