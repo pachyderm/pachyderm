e408d97f23e1eb40b485d57a2c4647ba254f4fb2
Peter Fedak <pzfedak@gmail.com>
2022-05-20 16:21:17 -0600

    Delay gRPC status code determination.
    Our error interceptor discarded the outermost layers of an error until
    it found a status code, which in turn required that we maintain a status
    code at the top level of all errors we returned. Our logging library
    also relied on this top-level status to make decisions about log level.
    
    Instead of worrying about this at all error return points, we can fix
    the interceptor behavior to always preserve the full error. We can also
    extract status codes from arbitrary errors via unwrapping as needed.

--- src/internal/middleware/errors/interceptor.go
+++ src/internal/middleware/errors/interceptor.go
@@ -5,28 +5,55 @@ import (
 	"errors"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 // UnaryServerInterceptor translates errors for unary RPCs
 func UnaryServerInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 	res, err := handler(ctx, req)
-	return res, unwrapGRPC(err)
+	return res, errorForGRPC(err)
 }
 
 // StreamServerInterceptor translates errors for streaming RPCs
 func StreamServerInterceptor(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
 	err := handler(srv, stream)
-	return unwrapGRPC(err)
+	return errorForGRPC(err)
 }
 
-func unwrapGRPC(err error) error {
+func errorForGRPC(err error) error {
+	if _, ok := status.FromError(err); ok {
+		// handles nil as well
+		return err //nolint:wrapcheck
+	}
+	code := codes.Unknown
 	cursor := err
 	for cursor != nil {
-		cursor = errors.Unwrap(cursor)
-		if res, ok := status.FromError(cursor); ok && res != nil {
-			return cursor //nolint:wrapcheck
+		if res, ok := status.FromError(cursor); ok {
+			code = res.Code()
+			break
 		}
+		cursor = errors.Unwrap(cursor)
 	}
-	return err
+	return &gRPCStatusError{
+		code: code,
+		err:  err,
+	} //nolint:wrapcheck
+}
+
+type gRPCStatusError struct {
+	err  error
+	code codes.Code
+}
+
+func (e *gRPCStatusError) GRPCStatus() *status.Status {
+	return status.New(e.code, e.Error())
+}
+
+func (e *gRPCStatusError) Error() string {
+	return e.err.Error()
+}
+
+func (e *gRPCStatusError) Unwrap() error {
+	return e.err
 }
