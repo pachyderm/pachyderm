f04888b027cb74343b6a955aa7b13cec96a26a3c
acohen4 <alonco124@gmail.com>
2022-05-25 15:36:22 -0600

    fixup

--- src/internal/serviceenv/config.go
+++ src/internal/serviceenv/config.go
@@ -5,7 +5,7 @@ type Configuration struct {
 	*GlobalConfiguration
 	*PachdSpecificConfiguration
 	*WorkerSpecificConfiguration
-	*EnterpriseSpecifcConfiguration
+	*EnterpriseSpecificConfiguration
 }
 
 // GlobalConfiguration contains the global configuration.
@@ -79,7 +79,7 @@ type GlobalConfiguration struct {
 type PachdFullConfiguration struct {
 	GlobalConfiguration
 	PachdSpecificConfiguration
-	EnterpriseSpecifcConfiguration
+	EnterpriseSpecificConfiguration
 }
 
 // PachdSpecificConfiguration contains the pachd specific configuration.
@@ -106,24 +106,13 @@ type PachdSpecificConfiguration struct {
 // EnterpriseServerConfiguration contains the full configuration for an enterprise server
 type EnterpriseServerConfiguration struct {
 	GlobalConfiguration
-	EnterpriseSpecifcConfiguration
+	EnterpriseSpecificConfiguration
 }
 
-// EnterpriseSpecifcConfiguration contains the configuration required for enterprise features
-type EnterpriseSpecifcConfiguration struct {
+// EnterpriseSpecificConfiguration contains the configuration required for enterprise features
+type EnterpriseSpecificConfiguration struct {
 	AuthRootToken string `env:"AUTH_ROOT_TOKEN,default="`
 	LicenseKey    string `env:"LICENSE_KEY,default="`
-}
-
-// EnterpriseServerConfiguration contains the full configuration for an enterprise server
-type EnterpriseServerConfiguration struct {
-	GlobalConfiguration
-	EnterpriseSpecifcConfiguration
-}
-
-// EnterpriseSpecifcConfiguration contains the configuration required for enterprise features
-type EnterpriseSpecifcConfiguration struct {
-	AuthRootToken string `env:"AUTH_ROOT_TOKEN,default="`
 }
 
 // StorageConfiguration contains the storage configuration.
@@ -177,7 +166,7 @@ func NewConfiguration(config interface{}) *Configuration {
 	case *PachdFullConfiguration:
 		configuration.GlobalConfiguration = &v.GlobalConfiguration
 		configuration.PachdSpecificConfiguration = &v.PachdSpecificConfiguration
-		configuration.EnterpriseSpecifcConfiguration = &v.EnterpriseSpecifcConfiguration
+		configuration.EnterpriseSpecificConfiguration = &v.EnterpriseSpecificConfiguration
 		return configuration
 	case *WorkerFullConfiguration:
 		configuration.GlobalConfiguration = &v.GlobalConfiguration
@@ -185,7 +174,7 @@ func NewConfiguration(config interface{}) *Configuration {
 		return configuration
 	case *EnterpriseServerConfiguration:
 		configuration.GlobalConfiguration = &v.GlobalConfiguration
-		configuration.EnterpriseSpecifcConfiguration = &v.EnterpriseSpecifcConfiguration
+		configuration.EnterpriseSpecificConfiguration = &v.EnterpriseSpecificConfiguration
 		return configuration
 	default:
 		return nil
