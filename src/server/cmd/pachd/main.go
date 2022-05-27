dcc3aba99e3ec31599b3cdb84cda69df14e0d328
Armaan Varadaraj <armaan.varadaraj@pachyderm.io>
2022-05-27 16:19:13 -0600

    [CORE-467] add new mux for oidc and metrics (#7686)

--- src/server/cmd/pachd/main.go
+++ src/server/cmd/pachd/main.go
@@ -970,8 +970,9 @@ func doFullMode(config interface{}) (retErr error) {
 		return errors.EnsureStack(server.ListenAndServeTLS(certPath, keyPath))
 	})
 	go waitForError("Prometheus Server", errChan, requireNoncriticalServers, func() error {
-		http.Handle("/metrics", promhttp.Handler())
-		return errors.EnsureStack(http.ListenAndServe(fmt.Sprintf(":%v", env.Config().PrometheusPort), nil))
+		mux := http.NewServeMux()
+		mux.Handle("/metrics", promhttp.Handler())
+		return errors.EnsureStack(http.ListenAndServe(fmt.Sprintf(":%v", env.Config().PrometheusPort), mux))
 	})
 	go func(c chan os.Signal) {
 		<-c
@@ -1304,8 +1305,9 @@ func doPausedMode(config interface{}) (retErr error) {
 		return errors.EnsureStack(server.ListenAndServeTLS(certPath, keyPath))
 	})
 	go waitForError("Prometheus Server", errChan, requireNoncriticalServers, func() error {
-		http.Handle("/metrics", promhttp.Handler())
-		return errors.EnsureStack(http.ListenAndServe(fmt.Sprintf(":%v", env.Config().PrometheusPort), nil))
+		mux := http.NewServeMux()
+		mux.Handle("/metrics", promhttp.Handler())
+		return errors.EnsureStack(http.ListenAndServe(fmt.Sprintf(":%v", env.Config().PrometheusPort), mux))
 	})
 	go func(c chan os.Signal) {
 		<-c
