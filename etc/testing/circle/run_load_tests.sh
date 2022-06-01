ead3d3f35483c2c63f294ab125494e0b9635ec34
molinamelendezj <molinamelendezj@gmail.com>
2022-06-01 16:29:30 -0400

    give load test there own helm values

--- etc/testing/circle/run_load_tests.sh
+++ etc/testing/circle/run_load_tests.sh
@@ -32,7 +32,7 @@ JOB=$(echo "$CIRCLE_JOB" | tr '[:upper:]' '[:lower:]')
 
 # # provision a pulumi load test env
 curl -X POST -H "Authorization: Bearer ${HELIUM_API_TOKEN}" \
- -F name=commit-"${CIRCLE_SHA1:0:7}-${JOB}" -F pachdVersion="${VERSION}" -F valuesYaml=@etc/testing/circle/helm-values.yaml \
+ -F name=commit-"${CIRCLE_SHA1:0:7}-${JOB}" -F pachdVersion="${VERSION}" -F valuesYaml=@etc/testing/circle/helm-load-env-values.yaml \
   https://helium.pachyderm.io/v1/api/workspace
 
 # wait for helium to kick off to pulumi before pinging it.
