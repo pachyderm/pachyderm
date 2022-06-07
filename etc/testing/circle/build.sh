511cc3f24359296a493c9d7c750f89a1d4de0a5f
Kyle White <kylewhite@gmail.com>
2022-06-07 16:09:52 +0000

    Remove minikube stuff from build script, comment out other circle jobs

--- etc/testing/circle/build.sh
+++ etc/testing/circle/build.sh
@@ -12,21 +12,6 @@ VERSION=$(pachctl version --client-only)
 
 ## local docker build
 
-# first, wait for minikube's docker to be ready (minikube is starting up async in a background job)
-for _ in $(seq 36); do
-    if minikube docker-env &>/dev/null; then
-        echo 'minikube docker ready' | ts
-        eval "$(minikube docker-env)"
-        break
-    fi
-    echo 'sleeping' | ts
-    sleep 5
-done
-
-if [[ -z "${MINIKUBE_ACTIVE_DOCKERD}" ]]; then
-    echo 'never setup docker, aborting' | ts
-    exit 1
-fi
 
 # then actually do the build
 git config user.email "donotreply@pachyderm.com"
