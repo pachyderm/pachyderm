ec5c2d1cf2dade67b676d06e1bd58d7d0425cabf
Armaan Varadaraj <armaan.varadaraj@pachyderm.io>
2022-06-11 17:49:10 -0700

    remove redundant build tags

--- etc/testing/pfs_server.sh
+++ etc/testing/pfs_server.sh
@@ -3,4 +3,4 @@ set -euxo pipefail
 
 export TIMEOUT=$1
 
-go test -v -count=1 -tags=k8s ./src/server/pfs/server ./src/server/pfs/server/testing -timeout "$TIMEOUT" -tags=k8s
+go test -v -count=1 -tags=k8s ./src/server/pfs/server ./src/server/pfs/server/testing -timeout "$TIMEOUT"
