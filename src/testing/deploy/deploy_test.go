b726345be59dae4b32257d32c26ac82dcf494225
molinamelendezj <molinamelendezj@gmail.com>
2022-06-15 12:21:31 -0400

    put tests back

--- src/testing/deploy/deploy_test.go
+++ src/testing/deploy/deploy_test.go
@@ -1,80 +1,123 @@
 package main
 
-import "testing"
+import (
+	"context"
+	"net/http"
+	"net/url"
+	"strings"
+	"testing"
 
-func TestFailOnPurpose(t *testing.T) {
-	t.Logf("Testing fail")
-	t.Fail()
+	"github.com/pachyderm/pachyderm/v2/src/auth"
+	"github.com/pachyderm/pachyderm/v2/src/client"
+	"github.com/pachyderm/pachyderm/v2/src/internal/minikubetestenv"
+	"github.com/pachyderm/pachyderm/v2/src/internal/require"
+	"github.com/pachyderm/pachyderm/v2/src/internal/testutil"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestDeployEnterprise(t *testing.T) {
+	k := testutil.GetKubeClient(t)
+	c := minikubetestenv.InstallRelease(t,
+		context.Background(),
+		"default",
+		k,
+		&minikubetestenv.DeployOpts{
+			AuthUser:     auth.RootUser,
+			Enterprise:   true,
+			CleanupAfter: true,
+		})
+	whoami, err := c.AuthAPIClient.WhoAmI(c.Ctx(), &auth.WhoAmIRequest{})
+	require.NoError(t, err)
+	require.Equal(t, auth.RootUser, whoami.Username)
+	c.SetAuthToken("")
+	mockIDPLogin(t, c)
 }
 
-// func TestDeployEnterprise(t *testing.T) {
-// 	k := testutil.GetKubeClient(t)
-// 	c := minikubetestenv.InstallRelease(t,
-// 		context.Background(),
-// 		"default",
-// 		k,
-// 		&minikubetestenv.DeployOpts{
-// 			AuthUser:     auth.RootUser,
-// 			Enterprise:   true,
-// 			CleanupAfter: true,
-// 		})
-// 	whoami, err := c.AuthAPIClient.WhoAmI(c.Ctx(), &auth.WhoAmIRequest{})
-// 	require.NoError(t, err)
-// 	require.Equal(t, auth.RootUser, whoami.Username)
-// 	c.SetAuthToken("")
-// 	mockIDPLogin(t, c)
-// }
+func TestUpgradeEnterpriseWithEnv(t *testing.T) {
+	k := testutil.GetKubeClient(t)
+	opts := &minikubetestenv.DeployOpts{
+		AuthUser:     auth.RootUser,
+		Enterprise:   true,
+		CleanupAfter: true,
+	}
+	c := minikubetestenv.InstallRelease(t, context.Background(), "default", k, opts)
+	whoami, err := c.AuthAPIClient.WhoAmI(c.Ctx(), &auth.WhoAmIRequest{})
+	require.NoError(t, err)
+	require.Equal(t, auth.RootUser, whoami.Username)
+	// set new root token via env
+	opts.AuthUser = ""
+	token := "new-root-token"
+	opts.ValueOverrides = map[string]string{"pachd.rootToken": token}
+	c = minikubetestenv.UpgradeRelease(t, context.Background(), "default", k, opts)
+	c.SetAuthToken(token)
+	whoami, err = c.AuthAPIClient.WhoAmI(c.Ctx(), &auth.WhoAmIRequest{})
+	require.NoError(t, err)
+	require.Equal(t, auth.RootUser, whoami.Username)
+	// old token should no longer work
+	c.SetAuthToken(testutil.RootToken)
+	_, err = c.AuthAPIClient.WhoAmI(c.Ctx(), &auth.WhoAmIRequest{})
+	require.YesError(t, err)
+}
 
-// func mockIDPLogin(t testing.TB, c *client.APIClient) {
-// 	// login using mock IDP admin
-// 	hc := &http.Client{}
-// 	c.SetAuthToken("")
-// 	loginInfo, err := c.GetOIDCLogin(c.Ctx(), &auth.GetOIDCLoginRequest{})
-// 	require.NoError(t, err)
-// 	state := loginInfo.State
+func TestEnterpriseServerMember(t *testing.T) {
+	k := testutil.GetKubeClient(t)
+	_, err := k.CoreV1().Namespaces().Create(context.Background(),
+		&v1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "enterprise",
+			},
+		},
+		metav1.CreateOptions{})
+	require.True(t, err == nil || strings.Contains(err.Error(), "already exists"), "Error '%v' does not contain 'already exists'", err)
+	ec := minikubetestenv.InstallRelease(t, context.Background(), "enterprise", k, &minikubetestenv.DeployOpts{
+		AuthUser:         auth.RootUser,
+		EnterpriseServer: true,
+		CleanupAfter:     true,
+	})
+	whoami, err := ec.AuthAPIClient.WhoAmI(ec.Ctx(), &auth.WhoAmIRequest{})
+	require.NoError(t, err)
+	require.Equal(t, auth.RootUser, whoami.Username)
+	mockIDPLogin(t, ec)
+	c := minikubetestenv.InstallRelease(t, context.Background(), "default", k, &minikubetestenv.DeployOpts{
+		AuthUser:         auth.RootUser,
+		EnterpriseMember: true,
+		Enterprise:       true,
+		CleanupAfter:     true,
+	})
+	whoami, err = c.AuthAPIClient.WhoAmI(c.Ctx(), &auth.WhoAmIRequest{})
+	require.NoError(t, err)
+	require.Equal(t, auth.RootUser, whoami.Username)
+	c.SetAuthToken("")
+	loginInfo, err := c.GetOIDCLogin(c.Ctx(), &auth.GetOIDCLoginRequest{})
+	require.NoError(t, err)
+	require.True(t, strings.Contains(loginInfo.LoginURL, ":31658"))
+	mockIDPLogin(t, c)
+}
 
-// 	// Get the initial URL from the grpc, which should point to the dex login page
-// 	resp, err := hc.Get(loginInfo.LoginURL)
-// 	require.NoError(t, err)
+func mockIDPLogin(t testing.TB, c *client.APIClient) {
+	// login using mock IDP admin
+	hc := &http.Client{}
+	c.SetAuthToken("")
+	loginInfo, err := c.GetOIDCLogin(c.Ctx(), &auth.GetOIDCLoginRequest{})
+	require.NoError(t, err)
+	state := loginInfo.State
 
-// 	vals := make(url.Values)
-// 	vals.Add("login", "admin")
-// 	vals.Add("password", "password")
+	// Get the initial URL from the grpc, which should point to the dex login page
+	resp, err := hc.Get(loginInfo.LoginURL)
+	require.NoError(t, err)
 
-// 	_, err = hc.PostForm(resp.Request.URL.String(), vals)
-// 	require.NoError(t, err)
+	vals := make(url.Values)
+	vals.Add("login", "admin")
+	vals.Add("password", "password")
 
-// 	authResp, err := c.AuthAPIClient.Authenticate(c.Ctx(), &auth.AuthenticateRequest{OIDCState: state})
-// 	require.NoError(t, err)
-// 	c.SetAuthToken(authResp.PachToken)
-// 	whoami, err := c.AuthAPIClient.WhoAmI(c.Ctx(), &auth.WhoAmIRequest{})
-// 	require.NoError(t, err)
-// 	require.Equal(t, "user:"+testutil.DexMockConnectorEmail, whoami.Username)
-// }
+	_, err = hc.PostForm(resp.Request.URL.String(), vals)
+	require.NoError(t, err)
 
-// func TestParallelDeployments(t *testing.T) {
-// 	eg, _ := errgroup.WithContext(context.Background())
-// 	var c1 *client.APIClient
-// 	var c2 *client.APIClient
-// 	eg.Go(func() error {
-// 		c1, _ = minikubetestenv.AcquireCluster(t)
-// 		_, err := c1.PfsAPIClient.CreateRepo(context.Background(), &pfs.CreateRepoRequest{Repo: client.NewRepo("c1")})
-// 		return errors.Wrap(err, "CreateRepo error")
-// 	})
-// 	eg.Go(func() error {
-// 		c2, _ = minikubetestenv.AcquireCluster(t)
-// 		_, err := c2.PfsAPIClient.CreateRepo(context.Background(), &pfs.CreateRepoRequest{Repo: client.NewRepo("c2")})
-// 		return errors.Wrap(err, "CreateRepo error")
-// 	})
-// 	require.NoError(t, eg.Wait())
-
-// 	c1List, err := c1.ListRepo()
-// 	require.NoError(t, err)
-// 	require.Equal(t, 1, len(c1List))
-// 	require.Equal(t, c1List[0].Repo.Name, "c1")
-
-// 	c2List, err := c2.ListRepo()
-// 	require.NoError(t, err)
-// 	require.Equal(t, 1, len(c2List))
-// 	require.Equal(t, c2List[0].Repo.Name, "c2")
-// }
+	authResp, err := c.AuthAPIClient.Authenticate(c.Ctx(), &auth.AuthenticateRequest{OIDCState: state})
+	require.NoError(t, err)
+	c.SetAuthToken(authResp.PachToken)
+	whoami, err := c.AuthAPIClient.WhoAmI(c.Ctx(), &auth.WhoAmIRequest{})
+	require.NoError(t, err)
+	require.Equal(t, "user:"+testutil.DexMockConnectorEmail, whoami.Username)
+}
--- src/testing/deploy/upgrade_test.go
+++ src/testing/deploy/upgrade_test.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"strings"
+	"testing"
+
+	proto "github.com/gogo/protobuf/proto"
+	"github.com/pachyderm/pachyderm/v2/src/client"
+	"github.com/pachyderm/pachyderm/v2/src/enterprise"
+	"github.com/pachyderm/pachyderm/v2/src/internal/errors"
+	"github.com/pachyderm/pachyderm/v2/src/internal/minikubetestenv"
+	"github.com/pachyderm/pachyderm/v2/src/internal/require"
+	"github.com/pachyderm/pachyderm/v2/src/internal/testutil"
+	"github.com/pachyderm/pachyderm/v2/src/pps"
+)
+
 const (
 	inputRepo      string = "input"
 	outputRepo     string = "output"
@@ -14,95 +31,95 @@ var (
 	}
 )
 
-// // runs the upgrade test from all versions specified in "fromVersions" against the local image
-// func upgradeTest(suite *testing.T, ctx context.Context, preUpgrade func(*testing.T, *client.APIClient), postUpgrade func(*testing.T, *client.APIClient)) {
-// 	k := testutil.GetKubeClient(suite)
-// 	for _, from := range fromVersions {
-// 		suite.Run(fmt.Sprintf("UpgradeFrom_%s", from), func(t *testing.T) {
-// 			preUpgrade(t, minikubetestenv.InstallRelease(t,
-// 				context.Background(),
-// 				"default",
-// 				k,
-// 				&minikubetestenv.DeployOpts{
-// 					Version: from,
-// 				}))
-// 			postUpgrade(t, minikubetestenv.UpgradeRelease(t,
-// 				context.Background(),
-// 				"default",
-// 				k,
-// 				&minikubetestenv.DeployOpts{
-// 					WaitSeconds:  10,
-// 					CleanupAfter: true,
-// 				}))
-// 		})
-// 	}
-// }
+// runs the upgrade test from all versions specified in "fromVersions" against the local image
+func upgradeTest(suite *testing.T, ctx context.Context, preUpgrade func(*testing.T, *client.APIClient), postUpgrade func(*testing.T, *client.APIClient)) {
+	k := testutil.GetKubeClient(suite)
+	for _, from := range fromVersions {
+		suite.Run(fmt.Sprintf("UpgradeFrom_%s", from), func(t *testing.T) {
+			preUpgrade(t, minikubetestenv.InstallRelease(t,
+				context.Background(),
+				"default",
+				k,
+				&minikubetestenv.DeployOpts{
+					Version: from,
+				}))
+			postUpgrade(t, minikubetestenv.UpgradeRelease(t,
+				context.Background(),
+				"default",
+				k,
+				&minikubetestenv.DeployOpts{
+					WaitSeconds:  10,
+					CleanupAfter: true,
+				}))
+		})
+	}
+}
 
-// // pre-upgrade:
-// // - create a pipeline "output" that copies contents from repo "input"
-// // - create file input@master:/foo
-// // post-upgrade:
-// // - create file input@master:/bar
-// // - verify output@master:/bar and output@master:/foo still exists
-// func TestUpgradeSimple(t *testing.T) {
-// 	upgradeTest(t, context.Background(),
-// 		func(t *testing.T, c *client.APIClient) {
-// 			c = testutil.AuthenticatedPachClient(t, c, upgradeSubject)
-// 			require.NoError(t, c.CreateRepo(inputRepo))
-// 			require.NoError(t,
-// 				c.CreatePipeline(outputRepo,
-// 					"busybox",
-// 					[]string{"sh"},
-// 					[]string{"cp /pfs/input/* /pfs/out/;"},
-// 					nil,
-// 					&pps.Input{Pfs: &pps.PFSInput{Glob: "/", Repo: inputRepo}},
-// 					"master",
-// 					false,
-// 				))
-// 			require.NoError(t, c.WithModifyFileClient(client.NewCommit(inputRepo, "master", ""), func(mf client.ModifyFile) error {
-// 				return errors.EnsureStack(mf.PutFile("foo", strings.NewReader("foo")))
-// 			}))
+// pre-upgrade:
+// - create a pipeline "output" that copies contents from repo "input"
+// - create file input@master:/foo
+// post-upgrade:
+// - create file input@master:/bar
+// - verify output@master:/bar and output@master:/foo still exists
+func TestUpgradeSimple(t *testing.T) {
+	upgradeTest(t, context.Background(),
+		func(t *testing.T, c *client.APIClient) {
+			c = testutil.AuthenticatedPachClient(t, c, upgradeSubject)
+			require.NoError(t, c.CreateRepo(inputRepo))
+			require.NoError(t,
+				c.CreatePipeline(outputRepo,
+					"busybox",
+					[]string{"sh"},
+					[]string{"cp /pfs/input/* /pfs/out/;"},
+					nil,
+					&pps.Input{Pfs: &pps.PFSInput{Glob: "/", Repo: inputRepo}},
+					"master",
+					false,
+				))
+			require.NoError(t, c.WithModifyFileClient(client.NewCommit(inputRepo, "master", ""), func(mf client.ModifyFile) error {
+				return errors.EnsureStack(mf.PutFile("foo", strings.NewReader("foo")))
+			}))
 
-// 			commitInfo, err := c.InspectCommit(outputRepo, "master", "")
-// 			require.NoError(t, err)
-// 			commitInfos, err := c.WaitCommitSetAll(commitInfo.Commit.ID)
-// 			require.NoError(t, err)
+			commitInfo, err := c.InspectCommit(outputRepo, "master", "")
+			require.NoError(t, err)
+			commitInfos, err := c.WaitCommitSetAll(commitInfo.Commit.ID)
+			require.NoError(t, err)
 
-// 			var buf bytes.Buffer
-// 			for _, info := range commitInfos {
-// 				if proto.Equal(info.Commit.Branch.Repo, client.NewRepo(outputRepo)) {
-// 					require.NoError(t, c.GetFile(info.Commit, "foo", &buf))
-// 					require.Equal(t, "foo", buf.String())
-// 				}
-// 			}
-// 		},
+			var buf bytes.Buffer
+			for _, info := range commitInfos {
+				if proto.Equal(info.Commit.Branch.Repo, client.NewRepo(outputRepo)) {
+					require.NoError(t, c.GetFile(info.Commit, "foo", &buf))
+					require.Equal(t, "foo", buf.String())
+				}
+			}
+		},
 
-// 		func(t *testing.T, c *client.APIClient) {
-// 			c = testutil.AuthenticateClient(t, c, upgradeSubject)
-// 			state, err := c.Enterprise.GetState(c.Ctx(), &enterprise.GetStateRequest{})
-// 			require.NoError(t, err)
-// 			require.Equal(t, enterprise.State_ACTIVE, state.State)
-// 			require.NoError(t, c.WithModifyFileClient(client.NewCommit(inputRepo, "master", ""), func(mf client.ModifyFile) error {
-// 				return errors.EnsureStack(mf.PutFile("bar", strings.NewReader("bar")))
-// 			}))
+		func(t *testing.T, c *client.APIClient) {
+			c = testutil.AuthenticateClient(t, c, upgradeSubject)
+			state, err := c.Enterprise.GetState(c.Ctx(), &enterprise.GetStateRequest{})
+			require.NoError(t, err)
+			require.Equal(t, enterprise.State_ACTIVE, state.State)
+			require.NoError(t, c.WithModifyFileClient(client.NewCommit(inputRepo, "master", ""), func(mf client.ModifyFile) error {
+				return errors.EnsureStack(mf.PutFile("bar", strings.NewReader("bar")))
+			}))
 
-// 			commitInfo, err := c.InspectCommit(outputRepo, "master", "")
-// 			require.NoError(t, err)
-// 			commitInfos, err := c.WaitCommitSetAll(commitInfo.Commit.ID)
-// 			require.NoError(t, err)
+			commitInfo, err := c.InspectCommit(outputRepo, "master", "")
+			require.NoError(t, err)
+			commitInfos, err := c.WaitCommitSetAll(commitInfo.Commit.ID)
+			require.NoError(t, err)
 
-// 			var buf bytes.Buffer
-// 			for _, info := range commitInfos {
-// 				if proto.Equal(info.Commit.Branch.Repo, client.NewRepo(outputRepo)) {
-// 					require.NoError(t, c.GetFile(info.Commit, "foo", &buf))
-// 					require.Equal(t, "foo", buf.String())
+			var buf bytes.Buffer
+			for _, info := range commitInfos {
+				if proto.Equal(info.Commit.Branch.Repo, client.NewRepo(outputRepo)) {
+					require.NoError(t, c.GetFile(info.Commit, "foo", &buf))
+					require.Equal(t, "foo", buf.String())
 
-// 					buf.Reset()
+					buf.Reset()
 
-// 					require.NoError(t, c.GetFile(info.Commit, "bar", &buf))
-// 					require.Equal(t, "bar", buf.String())
-// 				}
-// 			}
-// 		},
-// 	)
-// }
+					require.NoError(t, c.GetFile(info.Commit, "bar", &buf))
+					require.Equal(t, "bar", buf.String())
+				}
+			}
+		},
+	)
+}
