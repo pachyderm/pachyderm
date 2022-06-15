f8af207354b1d61c1a61ec39e7eeafab2ee89c50
Sahith Malyala <sahith.malyala@pachyderm.io>
2022-06-15 16:18:46 -0400

    Fix boolean logic for env vars (#139)

--- jupyterlab_pachyderm/env.py
+++ jupyterlab_pachyderm/env.py
@@ -1,12 +1,12 @@
 import os
 
-MOCK_PACHYDERM_SERVICE = os.environ.get("MOCK_PACHYDERM_SERVICE", False)
-MOUNT_SERVER_ENABLED = os.environ.get("MOUNT_SERVER_ENABLED", True)
+MOCK_PACHYDERM_SERVICE = os.environ.get("MOCK_PACHYDERM_SERVICE", 'False').lower() in ('true', '1', 't')
+MOUNT_SERVER_ENABLED = os.environ.get("MOUNT_SERVER_ENABLED", 'True').lower() in ('true', '1', 't')
 PFS_MOUNT_DIR = os.environ.get("PFS_MOUNT_DIR", "/pfs")
-ENSURE_MOUNT_SERVER = bool(os.environ.get("ENSURE_MOUNT_SERVER", True))
-JUPYTERLAB_CI_TESTS = bool(os.environ.get("JUPYTERLAB_CI_TESTS", False))
+ENSURE_MOUNT_SERVER = os.environ.get("ENSURE_MOUNT_SERVER", 'True').lower() in ('true', '1', 't')
+JUPYTERLAB_CI_TESTS = os.environ.get("JUPYTERLAB_CI_TESTS", 'False').lower() in ('true', '1', 't')
 
-PACHYDERM_EXT_DEBUG = bool(os.environ.get("PACHYDERM_EXT_DEBUG", False))
+PACHYDERM_EXT_DEBUG = os.environ.get("PACHYDERM_EXT_DEBUG", 'False').lower() in ('true', '1', 't')
 if PACHYDERM_EXT_DEBUG:
     from jupyterlab_pachyderm.log import get_logger
 
