805b0e64a49c771dd70625b0a13dc2f670170220
Alon Cohen <alonco124@gmail.com>
2022-06-20 15:40:48 -0400

    [2.2.x Backport] Enterprise Members shouldn't run the Identity/Dex Service (#7701) (#7785)
    * Enterprise Members shouldn't run the Identity/Dex Service (#7701)
    
    * Enterprise Members shouldn't run the Identity/Dex Service
    
    * Include EnterpriseMember in tests
    
    * only configure identity server for embedded-enterprise or enterprise server
    
    * Remove stuff from 2.3
    
    * fix

--- src/internal/serviceenv/config.go
+++ src/internal/serviceenv/config.go
@@ -5,6 +5,7 @@ type Configuration struct {
 	*GlobalConfiguration
 	*PachdSpecificConfiguration
 	*WorkerSpecificConfiguration
+	*EnterpriseSpecificConfiguration
 }
 
 // GlobalConfiguration contains the global configuration.
@@ -79,6 +80,7 @@ type GlobalConfiguration struct {
 type PachdFullConfiguration struct {
 	GlobalConfiguration
 	PachdSpecificConfiguration
+	EnterpriseSpecificConfiguration
 }
 
 // PachdSpecificConfiguration contains the pachd specific configuration.
@@ -100,6 +102,17 @@ type PachdSpecificConfiguration struct {
 	PachdPodName                 string `env:"PACHD_POD_NAME,required"`
 	EnableWorkerSecurityContexts bool   `env:"ENABLE_WORKER_SECURITY_CONTEXTS,default=true"`
 	TLSCertSecretName            string `env:"TLS_CERT_SECRET_NAME,default="`
+}
+
+// EnterpriseServerConfiguration contains the full configuration for an enterprise server
+type EnterpriseServerConfiguration struct {
+	GlobalConfiguration
+	EnterpriseSpecificConfiguration
+}
+
+// EnterpriseSpecificConfiguration contains the configuration required for enterprise features
+type EnterpriseSpecificConfiguration struct {
+	EnterpriseMember bool `env:"ENTERPRISE_MEMBER,default=false"`
 }
 
 // StorageConfiguration contains the storage configuration.
@@ -153,10 +166,15 @@ func NewConfiguration(config interface{}) *Configuration {
 	case *PachdFullConfiguration:
 		configuration.GlobalConfiguration = &v.GlobalConfiguration
 		configuration.PachdSpecificConfiguration = &v.PachdSpecificConfiguration
+		configuration.EnterpriseSpecificConfiguration = &v.EnterpriseSpecificConfiguration
 		return configuration
 	case *WorkerFullConfiguration:
 		configuration.GlobalConfiguration = &v.GlobalConfiguration
 		configuration.WorkerSpecificConfiguration = &v.WorkerSpecificConfiguration
+		return configuration
+	case *EnterpriseServerConfiguration:
+		configuration.GlobalConfiguration = &v.GlobalConfiguration
+		configuration.EnterpriseSpecificConfiguration = &v.EnterpriseSpecificConfiguration
 		return configuration
 	default:
 		return nil
