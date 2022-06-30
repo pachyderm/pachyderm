e4ba29357646c8c62a0e0c8a32d74f54cfea7801
molinamelendezj <molinamelendezj@gmail.com>
2022-06-30 11:08:31 -0400

    change start up logic

--- etc/testing/circle/run_release_tests.sh
+++ etc/testing/circle/run_release_tests.sh
@@ -13,8 +13,9 @@ VERSION="$(pachctl version --client-only)"
 export VERSION
 
 # provision a pulumi test env
+#TODO; switch to CIRCLE_TAG:1 once done with testing
 curl -X POST -H "Authorization: Bearer ${HELIUM_API_TOKEN}" \
- -F name=release-"${CIRCLE_SHA1:0:7}" -F pachdVersion="${CIRCLE_SHA1}" \
+ -F name=release-"${CIRCLE_SHA1:0:7}" -F pachdVersion="${CIRCLE_SHA1}" -F helmVersion="${CIRCLE_TAG}-${CIRCLE_SHA1}" \
   https://helium.pachyderm.io/v1/api/workspace
 
 # wait for helium to kick off to pulumi before pinging it.
@@ -25,12 +26,17 @@ for _ in $(seq 108); do
   if [[ ${STATUS} == "ready" ]]
   then
     echo "success"
-    READY=true
     break
   fi
   echo 'sleeping'
   sleep 10
 done
+
+if [[ ${STATUS} != "ready" ]]
+then
+  echo "failed to provision pulumi workspace"
+  exit 1
+fi
 
 pachdIp=$(curl -s -H "Authorization: Bearer ${HELIUM_API_TOKEN}" "https://helium.pachyderm.io/v1/api/workspace/release-${CIRCLE_SHA1:0:7}"  | jq .Workspace.PachdIp)
 
@@ -39,12 +45,19 @@ echo "{\"pachd_address\": ${pachdIp}, \"source\": 2}" | tr -d \\ | pachctl confi
 echo "${HELIUM_PACHCTL_AUTH_TOKEN}" | pachctl auth use-auth-token
 
 # Print client and server versions, for debugging.  (Also waits for proxy to discover pachd, etc.)
+READY=false
 for i in $(seq 1 20); do
     if pachctl version; then
         echo "pachd ready after $i attempts"
+        READY=true
         break
     else
         sleep 5
         continue
     fi
 done
+
+if [ "$READY" = false ] ; then
+    echo 'pachd failed to start'
+    exit 1
+fi
