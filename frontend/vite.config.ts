a8eed6f0f7438609bfa32e8a844846aa1733b0fc
Peter <60992304+pleeko@users.noreply.github.com>
2022-07-01 20:16:59 -0400

    add sourcemap to build (#670)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -45,6 +45,7 @@ export default defineConfig({
   // comment this out if that isn't relevant for your project
   build: {
     outDir: 'build',
+    sourcemap: true,
   },
   plugins: [
     react(),
