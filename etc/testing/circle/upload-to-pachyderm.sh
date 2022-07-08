bac9f99cf74f74545152cb607be9ddcc014f1f1f
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2022-07-08 05:48:04 -0400

    fix tmpdir

--- etc/testing/circle/upload-to-pachyderm.sh
+++ etc/testing/circle/upload-to-pachyderm.sh
@@ -6,6 +6,8 @@ pachctl config set context pachops --overwrite <<EOF
 {"pachd_address":"grpcs://pachyderm.pachops.com:443", "session_token":"$PACHOPS_PACHYDERM_ROBOT_TOKEN"}
 EOF
 
+pachctl config set active-context pachops
+
 pachctl version
 
 pachctl put file -r build-results@master:/pachyderm/$CIRCLE_BRANCH/$CIRCLE_BUILD_NUM/$CIRCLE_JOB/$CIRCLE_WORKFLOW_JOB_ID/ -f /tmp/test-results
