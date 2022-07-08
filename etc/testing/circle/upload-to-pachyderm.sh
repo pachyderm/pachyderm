c2a59481a9ef44d3a6818ca3d62c69a0c59ed301
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2022-07-08 05:27:27 -0400

    circle: try to upload build results to pachyderm

---
+++ etc/testing/circle/upload-to-pachyderm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+pachctl config set context pachops --overwrite <<EOF
+{"pachd_address":"grpcs://pachyderm.pachops.com:443", "session_token":"$PACHOPS_PACHYDERM_ROBOT_TOKEN"}
+EOF
+
+pachctl version
+
+pachctl put file build-results@master:/pachyderm/$CIRCLE_BRANCH/$CIRCLE_BUILD_NUM/$CIRCLE_JOB/$CIRCLE_WORKFLOW_JOB_ID/report.xml -f /tmp/raw-test-output/gotestsum-report.xml
