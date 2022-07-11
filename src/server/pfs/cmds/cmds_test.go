4690f9479a85216ce293f93404a9ee2a4214efea
Joe Doliner <jdoliner@gmail.com>
2022-07-11 13:13:24 -0400

    Fixes compilation of test.

--- src/server/pfs/cmds/cmds_test.go
+++ src/server/pfs/cmds/cmds_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/pachyderm/pachyderm/v2/src/client"
 	"github.com/pachyderm/pachyderm/v2/src/internal/minikubetestenv"
 	"github.com/pachyderm/pachyderm/v2/src/internal/require"
 	tu "github.com/pachyderm/pachyderm/v2/src/internal/testutil"
@@ -105,27 +106,22 @@ func TestMountParsing(t *testing.T) {
 	}
 	expected := map[string]*fuse.RepoOptions{
 		"repo1": {
-			Name:   "repo1", // name of mount, i.e. where to mount it
-			Repo:   "repo1",
-			Branch: "branch",
-			Write:  true,
+			Name:  "repo1", // name of mount, i.e. where to mount it
+			File:  client.NewFile("repo1", "branch", "", ""),
+			Write: true,
 		},
 		"repo2": {
-			Name:   "repo2",
-			Repo:   "repo2",
-			Branch: "master",
-			Write:  true,
+			Name:  "repo2",
+			File:  client.NewFile("repo2", "master", "", ""),
+			Write: true,
 		},
 		"repo3": {
-			Name:   "repo3",
-			Repo:   "repo3",
-			Branch: "master",
+			Name: "repo3",
+			File: client.NewFile("repo3", "master", "", ""),
 		},
 		"repo4": {
-			Name:   "repo4",
-			Repo:   "repo4",
-			Branch: "master",
-			Commit: "dee0c3904d6f44beb4fa10fc0db12d02",
+			Name: "repo4",
+			File: client.NewFile("repo4", "master", "dee0c3904d6f44beb4fa10fc0db12d02", ""),
 		},
 	}
 	opts, err := parseRepoOpts([]string{"repo1@branch+w", "repo2+w", "repo3", "repo4@master=dee0c3904d6f44beb4fa10fc0db12d02"})
