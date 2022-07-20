22f3ac4e34bd20195113f4a778818153646e1edf
Robert Uhl <robert.uhl@pachyderm.io>
2022-07-20 13:21:32 -0400

    [CORE-873] Update insecure dependencies (#7932)
    * Update to latest pachyderm/dex
    
    * Delint
    
    * Merge indirect require blocks
    
    * Update etcd version
    
    * Update containerd to v1.5.13
    
    Addresses:
    
    - CVE-2021-32760
    - CVE-2021-41103
    - GHSA-5j5w-g665-5m35
    - CVE-2021-43816
    - CVE-2022-23648
    - CVE-2022-31030
    
    * Update docker/distribution to v2.8.0
    
    Addresses GHSA-qq97-vm5h-rrhg
    
    * Tidy Helm go.mod
    
    * Update to testify v1.7.1
    
    * Merge require blocks

--- src/client/client.go
+++ src/client/client.go
@@ -16,6 +16,7 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/health/grpc_health_v1"
 
 	// Import registers the grpc GZIP encoder
@@ -785,7 +786,7 @@ func DefaultDialOptions() []grpc.DialOption {
 func (c *APIClient) connect(timeout time.Duration, unaryInterceptors []grpc.UnaryClientInterceptor, streamInterceptors []grpc.StreamClientInterceptor) error {
 	dialOptions := DefaultDialOptions()
 	if c.caCerts == nil {
-		dialOptions = append(dialOptions, grpc.WithInsecure())
+		dialOptions = append(dialOptions, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	} else {
 		tlsCreds := credentials.NewClientTLSFromCert(c.caCerts, "")
 		dialOptions = append(dialOptions, grpc.WithTransportCredentials(tlsCreds))
