0915d0ec2e1be2a761a9cb955b1421a05aa8e6c6
Kyle White <kylewhite@gmail.com>
2022-07-21 19:19:53 +0000

    Move comment, sync kubectl version with kubernetes version

--- etc/testing/circle/install.sh
+++ etc/testing/circle/install.sh
@@ -25,7 +25,7 @@ sudo chown root:root /etc/fuse.conf
 # To get the latest kubectl version:
 # curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt
 if [ ! -f cached-deps/kubectl ] ; then
-    KUBECTL_VERSION=v1.23.5
+    KUBECTL_VERSION=v1.25.2 # If changed, also do etc/kube/start-minikube.sh
     curl -L -o kubectl https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl && \
         chmod +x ./kubectl
         mv ./kubectl cached-deps/kubectl
@@ -35,7 +35,7 @@ fi
 # To get the latest minikube version:
 # curl https://api.github.com/repos/kubernetes/minikube/releases | jq -r .[].tag_name | sort -V | tail -n1
 if [ ! -f cached-deps/minikube ] ; then
-    MINIKUBE_VERSION=v1.26.0 # If changed, also do etc/kube/start-minikube.sh
+    MINIKUBE_VERSION=v1.26.0 
     curl -L -o minikube https://storage.googleapis.com/minikube/releases/${MINIKUBE_VERSION}/minikube-linux-amd64 && \
         chmod +x ./minikube
         mv ./minikube cached-deps/minikube
