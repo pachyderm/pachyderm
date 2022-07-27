415278ffa0befa2274530fea173f1f9bc39994f9
Albert Cui <albert.cui@pachyderm.io>
2022-07-27 14:14:58 -0400

    fail on partial errors for auth deactivate

--- src/server/auth/server/api_server.go
+++ src/server/auth/server/api_server.go
@@ -506,11 +506,21 @@ func (a *apiServer) rotateRootTokenInTransaction(txCtx *txncontext.TransactionCo
 // Deactivate implements the protobuf auth.Deactivate RPC
 func (a *apiServer) Deactivate(ctx context.Context, req *auth.DeactivateRequest) (resp *auth.DeactivateResponse, retErr error) {
 	if err := dbutil.WithTx(ctx, a.env.DB, func(sqlTx *pachsql.Tx) error {
-		a.roleBindings.ReadWrite(sqlTx).DeleteAll() //nolint:errcheck
-		a.deleteAllAuthTokens(ctx, sqlTx)           //nolint:errcheck
-		a.members.ReadWrite(sqlTx).DeleteAll()      //nolint:errcheck
-		a.groups.ReadWrite(sqlTx).DeleteAll()       //nolint:errcheck
-		a.authConfig.ReadWrite(sqlTx).DeleteAll()   //nolint:errcheck
+		if err := a.roleBindings.ReadWrite(sqlTx).DeleteAll(); err != nil {
+			return errors.EnsureStack(err)
+		}
+		if err := a.deleteAllAuthTokens(ctx, sqlTx); err != nil {
+			return errors.EnsureStack(err)
+		}
+		if err := a.members.ReadWrite(sqlTx).DeleteAll(); err != nil {
+			return errors.EnsureStack(err)
+		}
+		if err := a.groups.ReadWrite(sqlTx).DeleteAll(); err != nil {
+			return errors.EnsureStack(err)
+		}
+		if err := a.authConfig.ReadWrite(sqlTx).DeleteAll(); err != nil {
+			return errors.EnsureStack(err)
+		}
 		return nil
 	}); err != nil {
 		return nil, err
