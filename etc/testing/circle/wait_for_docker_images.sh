a0caff3db6c74c3c5b7253ef00fbfe525580504d
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2022-08-04 23:35:43 -0400

    wait_for_docker_images: fix env var

--- etc/testing/circle/wait_for_docker_images.sh
+++ etc/testing/circle/wait_for_docker_images.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -euxo pipefail
+
 for _ in $(seq 1 120); do
-    if docker manifest inspect "pachyderm/pachd:$CIRCLE_SHA"; then
+    if docker manifest inspect "pachyderm/pachd:$CIRCLE_SHA1"; then
         echo "manifest exists"
         exit 0
     fi
