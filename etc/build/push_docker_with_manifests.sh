48e4ba63739776c77748d8ab7bda76ea194806b7
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2022-08-04 23:18:29 -0400

    push_docker: fix lint

--- etc/build/push_docker_with_manifests.sh
+++ etc/build/push_docker_with_manifests.sh
@@ -6,19 +6,16 @@ REPO=pachyderm
 
 for product in pachd worker pachctl mount-server; do
     echo "push $product $VERSION..."
-    echo ""
-    docker push $REPO/$product-amd64:$VERSION
-    docker push $REPO/$product-arm64:$VERSION
+    docker push "$REPO/$product-amd64:$VERSION"
+    docker push "$REPO/$product-arm64:$VERSION"
 
-    docker manifest create $REPO/$product:$VERSION \
-           $REPO/$product-amd64:$VERSION \
-           $REPO/$product-arm64:$VERSION
+    docker manifest create "$REPO/$product:$VERSION" \
+           "$REPO/$product-amd64:$VERSION" \
+           "$REPO/$product-arm64:$VERSION"
 
-    docker manifest annotate $REPO/$product:$VERSION $REPO/$product-amd64:$VERSION --arch amd64
-    docker manifest annotate $REPO/$product:$VERSION $REPO/$product-arm64:$VERSION --arch arm64
+    docker manifest annotate "$REPO/$product:$VERSION" "$REPO/$product-amd64:$VERSION" --arch amd64
+    docker manifest annotate "$REPO/$product:$VERSION" "$REPO/$product-arm64:$VERSION" --arch arm64
 
-    docker manifest inspect $REPO/$product:$VERSION
-    docker manifest push $REPO/$product:$VERSION
-    echo "ok"
-    echo ""
+    docker manifest inspect "$REPO/$product:$VERSION"
+    docker manifest push "$REPO/$product:$VERSION"
 done
