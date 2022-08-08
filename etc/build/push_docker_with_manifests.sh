2b24c7574620715509aa264a25e81f26dca97a59
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2022-08-08 13:34:24 -0400

    release: integrate manifests into the release process

--- etc/build/push_docker_with_manifests.sh
+++ etc/build/push_docker_with_manifests.sh
@@ -2,19 +2,27 @@
 
 set -euxo pipefail
 
+# $VERSION is the tag to identify the manifest.  $UNDERLYING is the tag applied to the underlying
+# containers.  For example, if you build images that are tagged pachyderm/pachd:SOMESHA1 and want
+# that to become pachyderm/pachd:12.34.56, $VERSION should be 12.34.56 and $UNDERLYING should be
+# SOMESHA1.
+
 REPO=pachyderm
+UNDERLYING=${UNDERLYING:-$VERSION}
 
 for product in pachd worker pachctl mount-server; do
-    echo "push $product $VERSION..."
-    docker push "$REPO/$product-amd64:$VERSION"
-    docker push "$REPO/$product-arm64:$VERSION"
+    echo ""
+    echo "push $product tag=$VERSION (based on tag=$UNDERLYING)..."
+    echo ""
+    docker push "$REPO/$product-amd64:$UNDERLYING" || docker pull "$REPO/$product-amd64:$UNDERLYING"
+    docker push "$REPO/$product-arm64:$UNDERLYING" || docker pull "$REPO/$product-arm64:$UNDERLYING"
 
     docker manifest create "$REPO/$product:$VERSION" \
-           "$REPO/$product-amd64:$VERSION" \
-           "$REPO/$product-arm64:$VERSION"
+           "$REPO/$product-amd64:$UNDERLYING" \
+           "$REPO/$product-arm64:$UNDERLYING"
 
-    docker manifest annotate "$REPO/$product:$VERSION" "$REPO/$product-amd64:$VERSION" --arch amd64
-    docker manifest annotate "$REPO/$product:$VERSION" "$REPO/$product-arm64:$VERSION" --arch arm64
+    docker manifest annotate "$REPO/$product:$VERSION" "$REPO/$product-amd64:$UNDERLYING" --arch amd64
+    docker manifest annotate "$REPO/$product:$VERSION" "$REPO/$product-arm64:$UNDERLYING" --arch arm64
 
     docker manifest inspect "$REPO/$product:$VERSION"
     docker manifest push "$REPO/$product:$VERSION"
