0eb5dbffa69626da0a4e0f72a83cadf3d302a253
Sean Slattery <sean.slattery203@gmail.com>
2022-08-22 15:38:58 -0500

    Revert "set test back to original values"
    This reverts commit 56614acb1001a91cea27e6ae2b7b76a3c081b260.

--- src/server/pachyderm_test.go
+++ src/server/pachyderm_test.go
@@ -4551,7 +4551,7 @@ func TestDatumStatusRestart(t *testing.T) {
 		"",
 		[]string{"bash"},
 		[]string{
-			"sleep 20",
+			"sleep 2",
 		},
 		nil,
 		client.NewPFSInput(dataRepo, "/*"),
@@ -4574,7 +4574,7 @@ func TestDatumStatusRestart(t *testing.T) {
 	// it's called, the datum being processes was started at a new and later time
 	// (than the last time checkStatus was called)
 	checkStatus := func() {
-		require.NoErrorWithinTRetry(t, time.Minute, func() error {
+		require.NoErrorWithinTRetry(t, time.Minute*2, func() error {
 			jobInfo, err := c.InspectJob(pipeline, commit1.ID, true)
 			require.NoError(t, err)
 			if len(jobInfo.Details.WorkerStatus) == 0 {
