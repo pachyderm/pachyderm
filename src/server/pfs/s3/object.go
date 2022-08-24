b6fe3e7465ab74687446a6c1a4a181d556ef4dc6
Luke Marsden <me@lukemarsden.net>
2022-08-24 17:46:40 +0100

    Remove commented-out code.

--- src/server/pfs/s3/object.go
+++ src/server/pfs/s3/object.go
@@ -72,7 +72,6 @@ func (c *controller) GetObject(r *http.Request, bucketName, file, version string
 	modTime, err := types.TimestampFromProto(fileInfo.Committed)
 	if err != nil {
 		c.logger.Debugf("Warning: using nil timestamp (file probably in open commit)")
-		// return nil, err
 	}
 
 	content, err := pc.GetFileReadSeeker(bucket.Commit, file)
