2aa9fbdccf98d7c320f13b191aea4229c8c13633
Robert A. Uhl <robert.uhl@pachyderm.io>
2022-09-21 09:45:36 -0400

    Use *pfs.Repo

--- src/internal/ppsload/pipeline.go
+++ src/internal/ppsload/pipeline.go
@@ -37,15 +37,15 @@ func createPipelines(pachClient *client.APIClient, spec string, parallelism int6
 			continue
 		}
 		split := strings.Split(pipelineStr, ":")
-		repo := strings.TrimSpace(split[0]) + namespace
+		repoName := strings.TrimSpace(split[0]) + namespace
 		// Create source repos.
 		if strings.TrimSpace(split[1]) == "" {
-			if err := pachClient.CreateProjectRepo(pfs.DefaultProjectName, repo); err != nil {
+			if err := pachClient.CreateProjectRepo(pfs.DefaultProjectName, repoName); err != nil {
 				return nil, err
 			}
 			// First source repo will be the target of the PFS load test.
 			if retBranch == nil {
-				retBranch = client.NewProjectBranch(pfs.DefaultProjectName, repo, "master")
+				retBranch = client.NewProjectBranch(pfs.DefaultProjectName, repoName, "master")
 			}
 			continue
 		}
@@ -55,14 +55,15 @@ func createPipelines(pachClient *client.APIClient, spec string, parallelism int6
 			inputs[i] = strings.TrimSpace(inputs[i]) + namespace
 		}
 		// TODO(CORE-1063): plumb through project somehow
-		if err := createPipeline(pachClient, pfs.DefaultProjectName, repo, inputs, parallelism, podPatch); err != nil {
+		repo := &pfs.Repo{Project: &pfs.Project{Name: pfs.DefaultProjectName}, Name: repoName}
+		if err := createPipeline(pachClient, repo, inputs, parallelism, podPatch); err != nil {
 			return nil, err
 		}
 	}
 	return retBranch, nil
 }
 
-func createPipeline(pachClient *client.APIClient, project, repo string, inputRepos []string, parallelism int64, podPatch string) error {
+func createPipeline(pachClient *client.APIClient, repo *pfs.Repo, inputRepos []string, parallelism int64, podPatch string) error {
 	var inputs []*pps.Input
 	for i, inputRepo := range inputRepos {
 		inputs = append(inputs, &pps.Input{
@@ -79,7 +80,7 @@ func createPipeline(pachClient *client.APIClient, project, repo string, inputRep
 	_, err := pachClient.PpsAPIClient.CreatePipeline(
 		pachClient.Ctx(),
 		&pps.CreatePipelineRequest{
-			Pipeline: client.NewProjectPipeline(project, repo),
+			Pipeline: &pps.Pipeline{Project: repo.Project, Name: repo.Name},
 			Transform: &pps.Transform{
 				Cmd:   []string{"bash"},
 				Stdin: []string{"cp -r /pfs/input-*/* /pfs/out/"},
