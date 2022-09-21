f9ce8a6ea2b0590b1e33dc6a73f9dc2c88f121c6
Robert A. Uhl <robert.uhl@pachyderm.io>
2022-09-21 09:23:24 -0400

    pfsload.Commit: take a *pfs.Branch

--- src/internal/pfsload/commit.go
+++ src/internal/pfsload/commit.go
@@ -3,15 +3,18 @@ package pfsload
 import (
 	"github.com/pachyderm/pachyderm/v2/src/client"
 	"github.com/pachyderm/pachyderm/v2/src/internal/task"
+	"github.com/pachyderm/pachyderm/v2/src/pfs"
 )
 
-func Commit(pachClient *client.APIClient, taskService task.Service, project, repo, branch string, spec *CommitSpec, seed int64) error {
+func Commit(pachClient *client.APIClient, taskService task.Service, branch *pfs.Branch, spec *CommitSpec, seed int64) error {
 	env, err := NewEnv(pachClient, taskService, spec, seed)
 	if err != nil {
 		return err
 	}
+	project := branch.Repo.Project.GetName()
+	repo := branch.Repo.Name
 	for i := 0; i < int(spec.Count); i++ {
-		commit, err := pachClient.StartProjectCommit(project, repo, branch)
+		commit, err := pachClient.StartProjectCommit(project, repo, branch.Name)
 		if err != nil {
 			return err
 		}
@@ -20,7 +23,7 @@ func Commit(pachClient *client.APIClient, taskService task.Service, project, rep
 				return err
 			}
 		}
-		if err := pachClient.FinishProjectCommit(project, repo, branch, commit.ID); err != nil {
+		if err := pachClient.FinishProjectCommit(project, repo, branch.Name, commit.ID); err != nil {
 			return err
 		}
 		validator := env.Validator()
