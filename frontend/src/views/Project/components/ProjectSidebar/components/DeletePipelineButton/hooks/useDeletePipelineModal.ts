5b0d84aa3253b35169403ebb28fe0856254e8b05
pierrelamarche <pierrelamarche5@gmail.com>
2022-09-26 16:15:52 -0300

    chore: add explicit onComplete redirects for resource deletions (#739)

--- frontend/src/views/Project/components/ProjectSidebar/components/DeletePipelineButton/hooks/useDeletePipelineModal.ts
+++ frontend/src/views/Project/components/ProjectSidebar/components/DeletePipelineButton/hooks/useDeletePipelineModal.ts
@@ -1,15 +1,32 @@
 import {useCallback} from 'react';
+import {useHistory, useRouteMatch} from 'react-router';
 
 import {useDeletePipeline} from '@dash-frontend/hooks/useDeletePipeline';
 import useUrlState from '@dash-frontend/hooks/useUrlState';
+import {PROJECT_PATH} from '@dash-frontend/views/Project/constants/projectPaths';
+import {
+  lineageRoute,
+  projectPipelinesRoute,
+} from '@dash-frontend/views/Project/utils/routes';
 
 const useDeletePipelineButton = () => {
   const {projectId, pipelineId} = useUrlState();
+  const browserHistory = useHistory();
+  const projectMatch = !!useRouteMatch(PROJECT_PATH);
+
+  const onCompleted = () => {
+    if (projectMatch) {
+      browserHistory.push(projectPipelinesRoute({projectId}));
+    } else {
+      browserHistory.push(lineageRoute({projectId}));
+    }
+  };
+
   const {
     deletePipeline,
     loading: updating,
     error,
-  } = useDeletePipeline(pipelineId);
+  } = useDeletePipeline(pipelineId, onCompleted);
 
   const onDelete = useCallback(() => {
     if (pipelineId) {
--- frontend/src/views/Project/components/ProjectSidebar/components/DeleteRepoButton/hooks/useDeleteRepoModal.ts
+++ frontend/src/views/Project/components/ProjectSidebar/components/DeleteRepoButton/hooks/useDeleteRepoModal.ts
@@ -1,9 +1,31 @@
+import {useHistory, useRouteMatch} from 'react-router';
+
 import {useDeleteRepo} from '@dash-frontend/hooks/useDeleteRepo';
 import useUrlState from '@dash-frontend/hooks/useUrlState';
+import {PROJECT_PATH} from '@dash-frontend/views/Project/constants/projectPaths';
+import {
+  lineageRoute,
+  projectReposRoute,
+} from '@dash-frontend/views/Project/utils/routes';
 
 const useDeleteRepoButton = () => {
   const {projectId, repoId} = useUrlState();
-  const {deleteRepo, loading: updating, error} = useDeleteRepo(repoId);
+  const browserHistory = useHistory();
+  const projectMatch = !!useRouteMatch(PROJECT_PATH);
+
+  const onCompleted = () => {
+    if (projectMatch) {
+      browserHistory.push(projectReposRoute({projectId}));
+    } else {
+      browserHistory.push(lineageRoute({projectId}));
+    }
+  };
+
+  const {
+    deleteRepo,
+    loading: updating,
+    error,
+  } = useDeleteRepo(repoId, onCompleted);
 
   const onDelete = () => {
     if (repoId) {
