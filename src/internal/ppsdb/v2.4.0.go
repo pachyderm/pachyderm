7a7fc7260df30ad02f10e0907c17a599fe499697
Robert A. Uhl <robert.uhl@pachyderm.io>
2022-10-06 23:16:12 -0400

    Migrate PPS values as well as keys

--- src/internal/ppsdb/v2.4.0.go
+++ src/internal/ppsdb/v2.4.0.go
@@ -3,16 +3,50 @@ package ppsdb
 import (
 	"context"
 
+	"github.com/gogo/protobuf/proto"
+
+	"github.com/pachyderm/pachyderm/v2/src/pfs"
+	"github.com/pachyderm/pachyderm/v2/src/pps"
+
+	col "github.com/pachyderm/pachyderm/v2/src/internal/collection"
 	"github.com/pachyderm/pachyderm/v2/src/internal/errors"
 	"github.com/pachyderm/pachyderm/v2/src/internal/pachsql"
 )
 
 func MigrateV2_4_0(ctx context.Context, tx *pachsql.Tx) error {
-	if _, err := tx.ExecContext(ctx, `UPDATE collections.pipelines SET key = 'default/' || key, idx_name = 'default/' || idx_name, idx_version = 'default/' || idx_version`); err != nil {
-		return errors.Wrap(err, "could not update pipelines")
+	var oldJob = new(pps.JobInfo)
+	if err := col.MigratePostgreSQLCollection(ctx, tx, "jobs", jobsIndexes, oldJob, func(oldKey string) (newKey string, newVal proto.Message, err error) {
+		if oldJob.Job.Pipeline.Project.GetName() == "" {
+			oldJob.Job.Pipeline.Project = &pfs.Project{Name: "default"}
+		}
+		return JobKey(oldJob.Job), oldJob, nil
+
+	}); err != nil {
+		return errors.Wrap(err, "could not migrate jobs")
 	}
-	if _, err := tx.ExecContext(ctx, `UPDATE collections.jobs SET key = 'default/' || key, idx_pipeline = 'default/' || idx_pipeline, idx_job_state = 'default/' || idx_job_state, idx_jobset = 'default/' || idx_jobset`); err != nil {
-		return errors.Wrap(err, "could not update jobs")
+	var oldPipeline = new(pps.PipelineInfo)
+	if err := col.MigratePostgreSQLCollection(ctx, tx, "pipelines", pipelinesIndexes, oldPipeline, func(oldKey string) (newKey string, newVal proto.Message, err error) {
+		if oldPipeline.Pipeline.Project.GetName() == "" {
+			oldPipeline.Pipeline.Project = &pfs.Project{Name: "default"}
+		}
+		if newKey, err = pipelineCommitKey(oldPipeline.SpecCommit); err != nil {
+			return
+		}
+		return newKey, oldPipeline, nil
+
+	},
+		col.WithKeyGen(func(key interface{}) (string, error) {
+			if commit, ok := key.(*pfs.Commit); ok {
+				return pipelineCommitKey(commit)
+			}
+			return "", errors.New("must provide a spec commit")
+		}),
+		col.WithKeyCheck(func(key string) error {
+			_, _, _, err := ParsePipelineKey(key)
+			return err
+		}),
+	); err != nil {
+		return errors.Wrap(err, "could not migrate jobs")
 	}
 	return nil
 }
