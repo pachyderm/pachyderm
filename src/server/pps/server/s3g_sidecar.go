a863d24289ccfccf3530af6805e5744069b5a91f
Luke Marsden <me@lukemarsden.net>
2022-10-17 20:31:54 +0100

    why are you  getting stuck

--- src/server/pps/server/s3g_sidecar.go
+++ src/server/pps/server/s3g_sidecar.go
@@ -212,7 +212,7 @@ func (s *s3InstanceCreatingJobHandler) OnCreate(ctx context.Context, jobInfo *pp
 	if s.s.pipelineInfo.Details.S3Out {
 		outputBucket = &s3.Bucket{
 			Commit: jobInfo.OutputCommit,
-			Name:   "pachyderm-test", // maybe the name s2 is routing too needs to match the one the backend bucket is expecting too
+			Name:   "out", // maybe the name s2 is routing too needs to match the one the backend bucket is expecting too
 		}
 	}
 	var proxyToRealBackend bool
