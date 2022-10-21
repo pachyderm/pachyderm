2517c2341faff96407ed5c16f2beba7667db71eb
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2022-10-21 17:49:50 +0000

    randutil: use a much faster algorithm than rand.Intn to generate random data; test that generated data is random enough

--- src/internal/randutil/rand_test.go
+++ src/internal/randutil/rand_test.go
@@ -11,9 +11,9 @@ import (
 	"github.com/pachyderm/pachyderm/v2/src/internal/require"
 )
 
+var random = rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
+
 func BenchmarkReader(b *testing.B) {
-	seed := time.Now().UTC().UnixNano()
-	random := rand.New(rand.NewSource(seed))
 	b.SetBytes(100 * units.MB)
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
@@ -22,4 +22,28 @@ func BenchmarkReader(b *testing.B) {
 		_, err := io.Copy(buf, r)
 		require.NoError(b, err)
 	}
+}
+
+func testLetters(t *testing.T, buf []byte) {
+	t.Helper()
+	got := map[byte]int{}
+	for _, b := range buf {
+		got[byte(b)]++
+	}
+	for _, letter := range letters {
+		if n := got[letter]; n < 1 {
+			t.Errorf("letter %s never appears", string(letter))
+		}
+	}
+}
+
+func TestBytes(t *testing.T) {
+	testLetters(t, Bytes(random, 1000))
+}
+
+func TestBytesReader(t *testing.T) {
+	r := NewBytesReader(random, 1000)
+	buf, err := io.ReadAll(r)
+	require.NoError(t, err, "should have read bytes from BytesReader")
+	testLetters(t, buf)
 }
