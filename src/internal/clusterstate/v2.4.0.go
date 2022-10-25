45651f2e4b0612cb750f9bdd370a63e8e445e78d
Robert A. Uhl <robert.uhl@pachyderm.io>
2022-10-25 10:16:52 -0400

    Move v2.4.0 migrations into clusterstate
    This includes a minimal copy of PostgreSQL functionality from package
    collections.  It is copied in order to protect it from future code
    changes.

--- src/internal/clusterstate/v2.4.0.go
+++ src/internal/clusterstate/v2.4.0.go
@@ -1,44 +1,12 @@
 package clusterstate
 
 import (
-	"context"
-
-	"github.com/pachyderm/pachyderm/v2/src/pfs"
-	auth "github.com/pachyderm/pachyderm/v2/src/server/auth/server"
-
-	col "github.com/pachyderm/pachyderm/v2/src/internal/collection"
-	"github.com/pachyderm/pachyderm/v2/src/internal/errors"
 	"github.com/pachyderm/pachyderm/v2/src/internal/migrations"
-	"github.com/pachyderm/pachyderm/v2/src/internal/pfsdb"
-	"github.com/pachyderm/pachyderm/v2/src/internal/ppsdb"
+
+	v2_4_0 "github.com/pachyderm/pachyderm/v2/src/internal/clusterstate/v2.4.0"
 )
 
-var state_2_4_0 migrations.State = state_2_3_0.
-	Apply("Add projects collection", func(ctx context.Context, env migrations.Env) error {
-		return col.SetupPostgresCollections(ctx, env.Tx, pfsdb.CollectionsV2_4_0()...)
-	}).
-	Apply("Add default project", func(ctx context.Context, env migrations.Env) error {
-		var defaultProject = &pfs.ProjectInfo{
-			Project: &pfs.Project{
-				Name: "default", // hardcoded so that pfs.DefaultProjectName may change in the future
-			},
-		}
-		if err := pfsdb.Projects(nil, nil).ReadWrite(env.Tx).Create("default", defaultProject); err != nil {
-			return errors.Wrap(err, "could not create default project")
-		}
-		return nil
-	}).
-	Apply("Rename default project to “default”", func(ctx context.Context, env migrations.Env) error {
-		if err := pfsdb.MigrateV2_4_0(ctx, env.Tx); err != nil {
-			return err
-		}
-		if err := ppsdb.MigrateV2_4_0(ctx, env.Tx); err != nil {
-			return err
-		}
-		if err := auth.MigrateV2_4_0(ctx, env.Tx); err != nil {
-			return err
-		}
-		return nil
-	})
-	// DO NOT MODIFY THIS STATE
-	// IT HAS ALREADY SHIPPED IN A RELEASE
+var state_2_4_0 migrations.State = v2_4_0.Migrate(state_2_3_0)
+
+// DO NOT MODIFY THIS STATE
+// IT HAS ALREADY SHIPPED IN A RELEASE
--- src/internal/clusterstate/v2.4.0/auth.go
+++ src/internal/clusterstate/v2.4.0/auth.go
@@ -1,4 +1,4 @@
-package server
+package v2_4_0
 
 import (
 	"context"
@@ -7,10 +7,10 @@ import (
 	"github.com/pachyderm/pachyderm/v2/src/internal/pachsql"
 )
 
-// MigrateV2_4_0 migrates auth to be fully project-aware with a default project.
+// migrateAuth migrates auth to be fully project-aware with a default project.
 // It uses some internal knowledge about how cols.PostgresCollection works to do
 // so.
-func MigrateV2_4_0(ctx context.Context, tx *pachsql.Tx) error {
+func migrateAuth(ctx context.Context, tx *pachsql.Tx) error {
 	if _, err := tx.ExecContext(ctx, `UPDATE collections.role_bindings SET key = regexp_replace(key, '^REPO:([-a-zA-Z0-9_]+)$', 'REPO:default/\1') where key ~ '^REPO:([-a-zA-Z0-9_]+)'`); err != nil {
 		return errors.Wrap(err, "could not update role bindings")
 	}
