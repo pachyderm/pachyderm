9c8edce1bbf1a10ddaaf69f869172164080903c5
djanicekpach <daniel.janicek@pachyderm.io>
2022-11-08 09:36:57 -0800

    testutils and password tests

--- src/internal/testutil/auth.go
+++ src/internal/testutil/auth.go
@@ -144,16 +144,16 @@ func CommitCnt(t *testing.T, c *client.APIClient, repo *pfs.Repo) int {
 }
 
 // PipelineNames returns the names of all pipelines that 'c' gets from
-// ListPipeline.
-//
-// TODO(CORE-1100): replace with version which knows about projects.
-func PipelineNames(t *testing.T, c *client.APIClient) []string {
+// ListPipeline in the specified project. "" project name means get all pipelines.
+func PipelineNames(t *testing.T, c *client.APIClient, project string) []string {
 	t.Helper()
 	ps, err := c.ListPipeline(false)
 	require.NoError(t, err)
 	result := make([]string, len(ps))
 	for i, p := range ps {
-		result[i] = p.Pipeline.Name
+		if project == "" || project == p.Pipeline.Project.Name {
+			result[i] = p.Pipeline.Name
+		}
 	}
 	return result
 }
