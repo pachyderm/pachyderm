3086f936ace26cb59ded93fb0dd795ee2267b27a
Armaan Varadaraj <armaan.varadaraj@pachyderm.io>
2022-11-08 19:31:26 -0500

    tests

--- src/internal/clientsdk/pps.go
+++ src/internal/clientsdk/pps.go
@@ -90,3 +90,33 @@ func ListDatum(client pps.API_ListDatumClient) ([]*pps.DatumInfo, error) {
 	}
 	return results, nil
 }
+
+func ForEachJobSetInfo(client pps.API_ListJobSetClient, cb func(*pps.JobSetInfo) error) error {
+	for {
+		x, err := client.Recv()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return errors.EnsureStack(err)
+		}
+		if err := cb(x); err != nil {
+			if errors.Is(err, pacherr.ErrBreak) {
+				err = nil
+			}
+			return err
+		}
+	}
+	return nil
+}
+
+func ListJobSet(client pps.API_ListJobSetClient) ([]*pps.JobSetInfo, error) {
+	var results []*pps.JobSetInfo
+	if err := ForEachJobSetInfo(client, func(x *pps.JobSetInfo) error {
+		results = append(results, x)
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+	return results, nil
+}
