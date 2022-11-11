fbd4da7633e91d9ed3fe67805aa82196f3137481
Armaan Varadaraj <armaan.varadaraj@pachyderm.io>
2022-11-11 14:18:40 -0500

    [CORE-1231] pass pachctl args to server for audit logging (#8356)
    * send args in metadata
    
    * log user commands

--- src/client/client.go
+++ src/client/client.go
@@ -844,6 +844,9 @@ func (c *APIClient) AddMetadata(ctx context.Context) context.Context {
 		clientData["userid"] = c.metricsUserID
 		clientData["prefix"] = c.metricsPrefix
 	}
+	if len(os.Args) > 1 && os.Args[0] != "/pachd" {
+		clientData["command"] = strings.Join(os.Args, " ")
+	}
 
 	// Rescue any metadata pairs already in 'ctx' (otherwise
 	// metadata.NewOutgoingContext() would drop them). Note that this is similar
