124defbc2f00251ab5153099010b6bfdb3de770a
Robert A. Uhl <robert.uhl@pachyderm.io>
2022-11-21 14:45:13 -0500

    Use t.Helper in unauth debug test

--- src/server/pachyderm_test.go
+++ src/server/pachyderm_test.go
@@ -9967,6 +9967,7 @@ func TestListDatumFilter(t *testing.T) {
 }
 
 func testDebug(t *testing.T, c *client.APIClient, projectName, repoName string) {
+	t.Helper()
 	require.NoError(t, c.CreateProjectRepo(projectName, repoName))
 
 	expectedFiles, pipelines := tu.DebugFiles(t, projectName, repoName)
