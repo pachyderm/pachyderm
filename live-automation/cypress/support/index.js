5d63dda03a1da051b05f83c89f12a9653b3e8a37
Peter <60992304+pleeko@users.noreply.github.com>
2022-12-01 15:08:35 -0500

    delete live automation (#830)

--- live-automation/cypress/support/index.js
+++
@@ -1,8 +0,0 @@
-import './commands';
-import './save-network-requests';
-
-Cypress.on('uncaught:exception', (err, runnable) => {
-  // returning false here prevents Cypress from
-  // failing the test
-  return false
-});
