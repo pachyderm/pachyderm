d0d56dafe50029eedd153048382298a4a1fa270f
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2022-12-01 23:15:28 +0000

    benchmark-uploads: add support for uploading thru console

--- etc/benchmark-uploads/main.go
+++ etc/benchmark-uploads/main.go
@@ -1,27 +1,35 @@
+//nolint:wrapcheck
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
 	"log"
+	"math"
+	"mime/multipart"
 	"net/http"
 	"net/http/httputil"
 	"os"
 	"os/signal"
+	"strconv"
 	"time"
 
+	"github.com/docker/go-units"
 	"github.com/dustin/go-humanize"
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
 	"github.com/pachyderm/pachyderm/v2/src/client"
+	"github.com/pkg/errors"
 	uuid "github.com/satori/go.uuid"
 )
 
 var (
 	size = flag.String("size", "50MB", "size of the file to upload")
-	kind = flag.String("kind", "grpc", "type of benchmark to perform; grpc, http (requires experimental pachd), s3, s3-multipart")
+	kind = flag.String("kind", "grpc", "type of benchmark to perform; grpc, http (requires experimental pachd), s3, s3-multipart, console")
 	addr = flag.String("addr", "localhost", "for http and s3, the address to connect to (always requires a valid pach context, though)")
 )
 
@@ -33,6 +41,11 @@ type R struct {
 
 func (r *R) Read(p []byte) (int, error) {
 	var n int
+	if r.Len == 0 {
+		fmt.Printf("0\n")
+		r.EOFAt = time.Now()
+		return 0, io.EOF
+	}
 	for i := range p {
 		if r.Len > 0 {
 			p[i] = r.b
@@ -120,11 +133,126 @@ func main() {
 			}
 			defer res.Body.Close()
 
-			b, err := httputil.DumpResponse(res, true)
+			d, err := httputil.DumpResponse(res, true)
 			if err != nil {
 				return err
 			}
-			log.Printf("%s", b)
+			log.Printf("%s", d)
+			return nil
+		})
+	case "console":
+		benchErr = bench(func(name string, r io.Reader, length uint64) error {
+			// Start
+			start, err := json.Marshal(map[string]any{
+				"branch": "master",
+				"path":   "/",
+				"repo":   "benchmark-upload",
+			})
+			if err != nil {
+				return err
+			}
+			req, err := http.NewRequestWithContext(ctx, "POST", "http://"+*addr+"/upload/start", bytes.NewReader(start))
+			if err != nil {
+				return err
+			}
+			req.Header.Add("content-type", "application/json")
+			req.AddCookie(&http.Cookie{
+				Name:  "dashAuthToken",
+				Value: c.AuthToken(),
+			})
+			res, err := http.DefaultClient.Do(req)
+			if err != nil {
+				return errors.Wrap(err, "start")
+			}
+			body, err := io.ReadAll(res.Body)
+			res.Body.Close()
+			if err != nil {
+				return err
+			}
+			if got, want := res.StatusCode, http.StatusOK; got != want {
+				return fmt.Errorf("start: unexpected status code: got %d want %d %v", got, want, res.Status)
+			}
+			var idInfo struct {
+				UploadId string `json:"uploadId"`
+			}
+			if err := json.Unmarshal(body, &idInfo); err != nil {
+				return err
+			}
+
+			// Upload
+			ckSize := 50 * units.MB
+			nCk := int(math.Ceil(float64(length) / float64(ckSize)))
+			buf := make([]byte, units.MB)
+			for i := 1; i <= nCk; i++ { // chunks start at 1, not 0
+				uploadR, uploadW := io.Pipe()
+				w := multipart.NewWriter(uploadW)
+				go func() {
+					defer uploadW.Close()
+					defer w.Close()
+					_ = w.WriteField("uploadId", idInfo.UploadId)
+					_ = w.WriteField("fileName", name)
+					_ = w.WriteField("chunkTotal", strconv.FormatInt(int64(nCk), 10))
+					_ = w.WriteField("currentChunk", strconv.FormatInt(int64(i), 10))
+					fw, _ := w.CreateFormFile("file", name)
+					if _, err := io.CopyBuffer(fw, io.LimitReader(r, int64(ckSize)), buf); err != nil {
+						uploadW.CloseWithError(fmt.Errorf("copy: %w", err))
+					}
+				}()
+
+				req, err := http.NewRequestWithContext(ctx, "POST", "http://"+*addr+"/upload", uploadR)
+				if err != nil {
+					return err
+				}
+				req.Header.Add("content-type", w.FormDataContentType())
+				req.AddCookie(&http.Cookie{
+					Name:  "dashAuthToken",
+					Value: c.AuthToken(),
+				})
+
+				res, err := http.DefaultClient.Do(req)
+				if err != nil {
+					return errors.Wrapf(err, "chunk %d/%d", i, nCk)
+				}
+				res.Body.Close()
+				if got, want := res.StatusCode, http.StatusOK; got != want {
+					return fmt.Errorf("chunk %d/%d: unexpected status code: got %d want %d %v", i, nCk, got, want, res.Status)
+				}
+				fmt.Printf(",")
+			}
+			n, err := r.Read(buf)
+			if n != 0 {
+				return fmt.Errorf("%d bytes remain unexpectedly", n)
+			}
+			if err == nil || err != io.EOF {
+				return fmt.Errorf("reader finished with unexpected error: %w", err)
+			}
+			fmt.Printf("\n")
+
+			// Finish
+			finish, err := json.Marshal(idInfo) // could reuse "body" from start section
+			if err != nil {
+				return err
+			}
+			req, err = http.NewRequestWithContext(ctx, "POST", "http://"+*addr+"/upload/finish", bytes.NewReader(finish))
+			if err != nil {
+				return err
+			}
+			req.Header.Add("content-type", "application/json")
+			req.AddCookie(&http.Cookie{
+				Name:  "dashAuthToken",
+				Value: c.AuthToken(),
+			})
+			res, err = http.DefaultClient.Do(req)
+			if err != nil {
+				return errors.Wrap(err, "finish")
+			}
+			res.Body.Close()
+			if err != nil {
+				return err
+			}
+			if got, want := res.StatusCode, http.StatusOK; got != want {
+				return fmt.Errorf("finish: unexpected status code: got %d want %d %v", got, want, res.Status)
+			}
 			return nil
 		})
 	case "s3":
