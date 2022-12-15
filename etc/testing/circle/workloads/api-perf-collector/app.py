eb97377766597b4e84ae2d29b2e03030c0b8abe3
djanicekpach <daniel.janicek@pachyderm.io>
2022-12-15 14:53:01 -0800

    format columns and insert

--- etc/testing/circle/workloads/api-perf-collector/app.py
+++ etc/testing/circle/workloads/api-perf-collector/app.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import re
 import time
 from google.cloud import bigquery
 import os
@@ -15,18 +16,17 @@ def main():
         "Version": os.getenv('PACHD_VERSION'),
         "Upload_Timestamp": time.time()
     }
-    test_string = get_file_rows("api-perf_stats.csv", common_columns)
-    print(f'My rows: {test_string}')
+
     for key, value in common_columns.items():
         if not value:
             raise Exception(
                 f'Missing a required value to export api performance data. Need {key}.')
 
-    # table = client.get_table("{}.{}.{}".format("build-release-001", "insights", "perf-tests"))
+    rows_to_insert = get_file_rows("api-perf_stats.csv", common_columns)
+    insert_to_bigquery(client, rows_to_insert, 'api-perf-stats')
+
 
 # Log in to big wuery and return the authenticated client
-
-
 def get_bigquery_client() -> bigquery.Client:
     bigquery_auth_json = json.loads(os.getenv('BIGQUERY_AUTH_JSON'))
     bigquery_credentials = \
@@ -34,9 +34,8 @@ def get_bigquery_client() -> bigquery.Client:
         .from_service_account_info(bigquery_auth_json)
     return bigquery.Client(credentials=bigquery_credentials)
 
+
 # Collects the rows to insert from a csv and prepares them for insertion into big query. returns the rows to insert
-
-
 def get_file_rows(file_name: str, common_columns: dict[str, any]) -> list[dict[str, any]]:
     rows_to_insert = []
     with open(file_name, mode='r') as c:
@@ -51,8 +50,25 @@ def get_file_rows(file_name: str, common_columns: dict[str, any]) -> list[dict[s
     return rows_to_insert
 
 
+# format column names to be compatible with bigquery's rules
 def format_column_names(raw: list[str]) -> list[str]:
-    return raw
+    formatted = []
+    for name in raw:
+        # strip non-alphamnumeric column titles
+        tmp = re.sub('[^a-zA-Z\d:]', '_', name)
+        # append _ instead of starting with a number
+        if re.match('\d', tmp[0]) is not None:
+            tmp = '_' + tmp
+        formatted.append(tmp)
+    return formatted
+
+
+def insert_to_bigquery(client: bigquery.Client, rows_to_insert: list[dict[str, any]], table_name: str):
+    table = client.get_table("{}.{}.{}".format(
+        "build-release-001", "insights", table_name))
+    errors = client.insert_rows_json(table, rows_to_insert)
+    if errors == []:
+        print(f'successful insert to {table_name}')
 
 
 if __name__ == "__main__":
