2f0bfcf161df0e4aa3178795b426787e0711eab3
Robert A. Uhl <robert.uhl@pachyderm.io>
2022-12-16 12:05:51 -0500

    Don’t create default project in tests

--- src/server/auth/server/testing/auth_integration_test.go
+++ src/server/auth/server/testing/auth_integration_test.go
@@ -198,7 +198,9 @@ func testDebug(t *testing.T, c *client.APIClient, projectName, repoName string) 
 	// change and recreates all the pods, which used to race with collecting logs.
 	alice := tu.Robot(tu.UniqueString("alice"))
 	aliceClient, adminClient := tu.AuthenticateClient(t, c, alice), tu.AuthenticateClient(t, c, auth.RootUser)
-	require.NoError(t, aliceClient.CreateProject(projectName))
+	if projectName != "default" {
+		require.NoError(t, aliceClient.CreateProject(projectName))
+	}
 
 	require.NoError(t, aliceClient.CreateProjectRepo(projectName, repoName))
 
