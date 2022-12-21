04f990b981451ccc2a5e2a831f288c831f21c24f
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2022-12-21 00:41:39 +0000

    delete pctx/http; log/http does what it was going to do

--- src/internal/pctx/http.go
+++
@@ -1,9 +0,0 @@
-package pctx
-
-// // ConnContextFn returns a function suitable for use as an (http.Server).ConnContext function.  It
-// // creates a new context containing pctx information and a logging span for each incoming request.
-// func ConnContextFn(ctx context.Context) func(l net.Listener) context.Context {
-// 	return func(l net.Listener) context.Context {
-
-// 	}
-// }
