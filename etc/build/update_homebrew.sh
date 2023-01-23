292a61e9b8d1b27c8495198cb1eeb1341424ff0f
Alon Cohen <alonco124@gmail.com>
2023-01-23 20:06:01 +0200

    Deduplicate Homebrew script flags in CI (#8531)

--- etc/build/update_homebrew.sh
+++ etc/build/update_homebrew.sh
@@ -3,14 +3,14 @@
 set -e
 
 version=$1
-stable=$3
+stable=$2
 
 echo "--- Updating homebrew formula to use binaries at version $version"
 BRANCH=master
-if [ -n "$2" ]
+if [[ $stable == 0 ]];
 then
     BRANCH=$version
-fi
+fi;
 
 MAJOR_MINOR=$(echo "$version" | cut -f -2 -d ".")
 
@@ -21,7 +21,7 @@ pushd homebrew-tap
     git checkout -b "$BRANCH" || git checkout "$BRANCH"
     VERSION=$version ./update-formula.sh
     git add "pachctl@$MAJOR_MINOR.rb"
-    if [[ $stable ]]; then
+    if [[ $stable == 1 ]]; then
         cp "pachctl@$MAJOR_MINOR.rb" pachctl.rb
         sed -i -E 's/class PachctlAT\([0-9]*\)/class Pachctl/g' pachctl.rb
         git add pachctl.rb
