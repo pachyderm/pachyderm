d7b06fb0b5e4c0ecd4f7470c8079856f3e83be9e
acohen4 <alonco124@gmail.com>
2023-01-24 14:53:48 +0200

    fix lint

--- etc/build/update_homebrew.sh
+++ etc/build/update_homebrew.sh
@@ -40,7 +40,7 @@ pushd homebrew-tap
     VERSION=$version ./update-formula.sh
     git add "pachctl@$MAJOR_MINOR.rb"
     if [[ $stable == 1 ]]; then
-	if [[ "${major}.${minor}" == $MAJOR_MINOR ]]; then 
+	if [[ "${major}.${minor}" == "$MAJOR_MINOR" ]]; then 
             cp "pachctl@$MAJOR_MINOR.rb" pachctl.rb
             sed -i -E 's/class PachctlAT\([0-9]*\)/class Pachctl/g' pachctl.rb
             git add pachctl.rb
