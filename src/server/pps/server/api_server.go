e11eccd20d58b2a71801afae8938e5f3c0a88b22
Robert A. Uhl <robert.uhl@pachyderm.io>
2023-02-01 13:09:20 -0500

    Update permitted length
    The total allowed is 63 characters; no existing pipeline with a name
    longer than 51 characters has ever been runnable (`pipeline-$NAME-v?`
    only allows 51 characters of pipeline name and one character of
    version).  Replacing `pipeline` with `$PROJECT` means that the sum of
    project and pipeline name lengths needs to be less than or equal to 59
    characters.

--- src/server/pps/server/api_server.go
+++ src/server/pps/server/api_server.go
@@ -1771,8 +1771,8 @@ func (a *apiServer) validatePipelineRequest(request *pps.CreatePipelineRequest) 
 		return errors.Errorf("pipeline name is %d characters long, but must have at most 63: %q",
 			len(request.Pipeline.Name), request.Pipeline.Name)
 	}
-	if len(request.Pipeline.Name)+len(request.Pipeline.Project.GetName()) > 62 {
-		return errors.Errorf("names of pipeline and project together may not exceed 62 characters")
+	if len(request.Pipeline.Name)+len(request.Pipeline.Project.GetName()) > 59 {
+		return errors.Errorf("names of pipeline and project together may not exceed 59 characters")
 	}
 	// TODO(msteffen) eventually TFJob and Transform will be alternatives, but
 	// currently TFJob isn't supported
