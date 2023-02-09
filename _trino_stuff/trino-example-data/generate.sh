73f362a084e990bb1e21f2c61b1c479dd7938b2b
Matthew Steffen <msteffen@pachyderm.io>
2023-02-09 02:31:05 -0500

    Improve demoability of generate.sh

--- _trino_stuff/trino-example-data/generate.sh
+++ _trino_stuff/trino-example-data/generate.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-aws s3 cp s3://presto-example/v2/example-metadata.json example-metadata.json
-
 # Remove all schemas except "tpch"
-jq '{ tpch: .tpch }' example-metadata.json | sponge example-metadata-filtered.json
+aws s3 cp s3://presto-example/v2/example-metadata.json - \
+  | jq '{ tpch: .tpch }' >metadata.json
 
-for t in $( jq -r '.tpch[].sources[]' example-metadata-filtered.json ); do
+for t in $( jq -r '.tpch[].sources[]' metadata.json ); do
   aws s3 cp "s3://presto-example/v2/$t" "$t"
 done
 
