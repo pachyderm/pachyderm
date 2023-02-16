efd7efebe01f8367bdff8e4192426bcba89fcf40
Peter Franchina <60992304+pleeko@users.noreply.github.com>
2023-02-16 09:47:43 -0500

    Update the datums dag to create failed datums along with adding some text to make logs more interesting (#986)

--- test-dags/lots-of-datums/index.js
+++ test-dags/lots-of-datums/index.js
@@ -1,12 +1,30 @@
-const fs = require("fs");
+const fs = require('fs');
+const LoremIpsum = require('lorem-ipsum').LoremIpsum;
 
-fs.readdirSync("/pfs/data").forEach(async (file) => {
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 6,
+    min: 3,
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4,
+  },
+});
+
+fs.readdirSync('/pfs/data').forEach(async (file) => {
+  if (parseFloat(file) < 0.5) {
+    throw new Error(lorem.generateSentences(1));
+  }
+
   // Can be tuned to test diffent load times
   await new Promise((resolve) => setTimeout(resolve, 500));
 
   console.log(file);
+  console.log(lorem.generateParagraphs(3));
+
   fs.writeFileSync(
-    "/pfs/out/" + Math.random().toString(),
-    Math.random().toString()
+    '/pfs/out/' + Math.random().toString(),
+    Math.random().toString(),
   );
 });
