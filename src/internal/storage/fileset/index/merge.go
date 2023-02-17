dd7b1f444dcc0d9fb96da6e8479304907c6d0f35
Brendon Carroll <7072757+brendoncarroll@users.noreply.github.com>
2023-02-17 13:15:18 -0500

    revert storage changes, they will be back and in greater numbers

--- src/internal/storage/fileset/index/merge.go
+++ src/internal/storage/fileset/index/merge.go
@@ -2,27 +2,30 @@ package index
 
 import (
 	"context"
+	"strings"
 
+	"github.com/pachyderm/pachyderm/v2/src/internal/miscutil"
 	"github.com/pachyderm/pachyderm/v2/src/internal/storage/chunk"
 	"github.com/pachyderm/pachyderm/v2/src/internal/stream"
 )
 
 func Merge(ctx context.Context, storage *chunk.Storage, indexes []*Index, cb func(*Index) error) error {
-	var its []stream.Peekable[*Index]
+	var ss []stream.Stream
 	for _, index := range indexes {
 		ir := NewReader(storage, nil, index)
-		it := stream.NewFromForEach(ctx, copyIndex, func(cb func(*Index) error) error {
+		iterateFunc := func(cb func(interface{}) error) error {
 			return ir.Iterate(ctx, func(index *Index) error {
 				return cb(index)
 			})
+		}
+		ss = append(ss, &indexStream{
+			iterator: miscutil.NewIterator(ctx, iterateFunc),
 		})
-		peekIt := stream.NewPeekable(it, copyIndex)
-		its = append(its, peekIt)
 	}
-	m := stream.NewMerger(its, LessThan)
-	return stream.ForEach[stream.Merged[*Index]](ctx, m, func(x stream.Merged[*Index]) error {
-		for _, idx := range x.Values {
-			if err := cb(idx); err != nil {
+	pq := stream.NewPriorityQueue(ss, compare)
+	return pq.Iterate(func(ss []stream.Stream) error {
+		for _, s := range ss {
+			if err := cb(s.(*indexStream).index); err != nil {
 				return err
 			}
 		}
@@ -30,13 +33,25 @@ func Merge(ctx context.Context, storage *chunk.Storage, indexes []*Index, cb fun
 	})
 }
 
-func LessThan(a, b *Index) bool {
-	if a.Path != b.Path {
-		return a.Path < b.Path
-	}
-	return a.File.Datum < b.File.Datum
+type indexStream struct {
+	iterator *miscutil.Iterator
+	index    *Index
 }
 
-func copyIndex(dst, src **Index) {
-	*dst = *src
+func (is *indexStream) Next() error {
+	data, err := is.iterator.Next()
+	if err != nil {
+		return err
+	}
+	is.index = data.(*Index)
+	return nil
+}
+
+func compare(s1, s2 stream.Stream) int {
+	idx1 := s1.(*indexStream).index
+	idx2 := s2.(*indexStream).index
+	if idx1.Path == idx2.Path {
+		return strings.Compare(idx1.File.Datum, idx2.File.Datum)
+	}
+	return strings.Compare(idx1.Path, idx2.Path)
 }
