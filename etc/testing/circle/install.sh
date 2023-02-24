63c70c71a7d9f46cbef8f5b8a6f359c682a5287c
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2023-02-24 13:31:21 -0500

    circle/install: update deps to their newest versions

--- etc/testing/circle/install.sh
+++ etc/testing/circle/install.sh
@@ -28,7 +28,7 @@ sudo chown root:root /etc/fuse.conf
 # To get the latest kubectl version:
 # curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt
 if [ ! -f cached-deps/kubectl ] ; then
-    KUBECTL_VERSION=v1.23.5
+    KUBECTL_VERSION=v1.26.1
     curl -L -o kubectl https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/${ARCH}/kubectl && \
         chmod +x ./kubectl
         mv ./kubectl cached-deps/kubectl
@@ -38,7 +38,7 @@ fi
 # To get the latest minikube version:
 # curl https://api.github.com/repos/kubernetes/minikube/releases | jq -r .[].tag_name | sort -V | tail -n1
 if [ ! -f cached-deps/minikube ] ; then
-    MINIKUBE_VERSION=v1.26.0
+    MINIKUBE_VERSION=v1.29.0
     curl -L -o minikube https://storage.googleapis.com/minikube/releases/${MINIKUBE_VERSION}/minikube-linux-${ARCH} && \
         chmod +x ./minikube
         mv ./minikube cached-deps/minikube
@@ -46,7 +46,7 @@ fi
 
 # Install kubeval
 if [ ! -f cached-deps/kubeval ] && [ "$ARCH" = "amd64" ]; then
-  KUBEVAL_VERSION=0.15.0
+  KUBEVAL_VERSION=0.16.1
   curl -L https://github.com/instrumenta/kubeval/releases/download/${KUBEVAL_VERSION}/kubeval-linux-${ARCH}.tar.gz \
       | tar xzf - kubeval
       mv ./kubeval cached-deps/kubeval
@@ -54,7 +54,7 @@ fi
 
 # Install helm
 if [ ! -f cached-deps/helm ]; then
-  HELM_VERSION=3.5.4
+  HELM_VERSION=3.10.2
   curl -L https://get.helm.sh/helm-v${HELM_VERSION}-linux-${ARCH}.tar.gz \
       | tar xzf - linux-${ARCH}/helm
       mv ./linux-${ARCH}/helm cached-deps/helm
@@ -62,7 +62,7 @@ fi
 
 # Install goreleaser
 if [ ! -f cached-deps/goreleaser ]; then
-  GORELEASER_VERSION=0.169.0
+  GORELEASER_VERSION=1.15.2
   curl -L https://github.com/goreleaser/goreleaser/releases/download/v${GORELEASER_VERSION}/goreleaser_Linux_x86_64.tar.gz \
       | tar xzf - -C cached-deps goreleaser
 fi
