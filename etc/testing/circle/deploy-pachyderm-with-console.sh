8c79d52756c145c9586316c6c2793be6b23bae25
Peter Franchina <60992304+pleeko@users.noreply.github.com>
2023-03-07 15:36:40 -0800

    Add a step to verify docker image (#1041)
    * add a step to verify docker image
    
    * update script premissions
    
    * add a step to verify docker image
    
    * update script premissions
    
    * reduce installs during step

---
+++ etc/testing/circle/deploy-pachyderm-with-console.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -ex
+
+export PATH=$(pwd):$(pwd)/cached-deps:$GOPATH/bin:$PATH
+export PACHYDERM_VERSION="$(jq -r .pachyderm version.json)"
+export PACHD_VERSION="$(jq -r .pachReleaseCommit version.json)"
+
+helm repo add pachyderm https://pachyderm.github.io/helmchart
+helm repo update
+helm install \
+    --wait --timeout 10m pachd pachyderm/pachyderm \
+    --set deployTarget=LOCAL \
+    --set console.image.tag=${CIRCLE_SHA1} \
+    --set console.config.disableTelemetry=true \
+    --version=${PACHYDERM_VERSION} \
+    --set pachd.image.tag=${PACHD_VERSION}
+
+# There is a bug with wait for json path https://github.com/kubernetes/kubectl/issues/1236
+# This bug has been fixed but the fix is only available in version 1.26
+# However, using --wait in helm above seems to get us to the same solution.
+# Therefore, the following code may not be needed.
+kubectl wait --for=condition=available deployment -l app=pachd --timeout=5m
+kubectl wait statefulset.apps/pachd-loki '--for=jsonpath={.status.readyReplicas}=1' --timeout=5m
+pachctl version
