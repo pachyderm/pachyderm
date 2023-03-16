7ff02c9374f9992e12938f0d8e403b5f1248f809
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2023-03-16 09:55:13 -0700

    pachctl: add "misc" section to pachctl for random tools; add some network tools (#8605) (#8619)
    * pachctl: add some misc network debugging utilities
    
    * pachctl: add a "misc" section with miscellaneous utilities that might be helpful to people debugging
    
    * misc: cmds: avoid 1.20 features for now
    
    * clean up error messages around dns lookups
    
    * lint cleanups
    
    * misc: http-head: print cert chain

---
+++ src/server/misc/cmds/cmds.go
@@ -0,0 +1,135 @@
+package cmds
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"os"
+
+	"github.com/pachyderm/pachyderm/v2/src/internal/cmdutil"
+	"github.com/pachyderm/pachyderm/v2/src/internal/errors"
+	"github.com/pachyderm/pachyderm/v2/src/internal/log"
+	"github.com/pachyderm/pachyderm/v2/src/internal/pctx"
+	"github.com/pachyderm/pachyderm/v2/src/internal/promutil"
+	"github.com/spf13/cobra"
+	"go.uber.org/multierr"
+	"go.uber.org/zap"
+)
+
+func Cmds() []*cobra.Command {
+	var commands []*cobra.Command
+
+	var d net.Dialer
+	setupControl(&d)
+
+	r := new(net.Resolver)
+	d.Resolver = r
+	r.Dial = func(ctx context.Context, network, address string) (_ net.Conn, retErr error) {
+		defer log.Span(ctx, "DialDNS", zap.String("network", network), zap.String("address", address))(log.Errorp(&retErr))
+		return d.DialContext(ctx, network, address) //nolint:wrapcheck
+	}
+
+	t := http.DefaultTransport.(*http.Transport).Clone()
+	t.DialContext = d.DialContext
+	t.TLSClientConfig.VerifyConnection = func(cs tls.ConnectionState) error {
+		for _, cert := range cs.PeerCertificates {
+			fmt.Printf("tls: cert: %v\n", cert.Subject.String())
+		}
+		fmt.Printf("tls: server name: %v\n", cs.ServerName)
+		fmt.Printf("tls: negotiated protocol: %v\n", cs.NegotiatedProtocol)
+		fmt.Println()
+		return nil
+	}
+	t.TLSClientConfig.InsecureSkipVerify = true
+	hc := &http.Client{
+		Transport: promutil.InstrumentRoundTripper("pachctl", t),
+	}
+
+	dnsLookup := &cobra.Command{
+		Use:   "{{alias}} <hostname>",
+		Short: "Do a DNS lookup on a hostname.",
+		Long:  "Do a DNS lookup on a hostname.",
+		Run: cmdutil.RunFixedArgs(1, func(args []string) error {
+			ctx := pctx.Background("")
+			var errs error
+			if result, err := r.LookupHost(ctx, args[0]); err != nil {
+				multierr.AppendInto(&errs, errors.Wrap(err, "lookup host"))
+			} else {
+				fmt.Printf("A: %v\n", result)
+			}
+			if result, err := r.LookupCNAME(ctx, args[0]); err != nil {
+				multierr.AppendInto(&errs, errors.Wrap(err, "lookup cname"))
+			} else {
+				fmt.Printf("CNAME: %v\n", result)
+			}
+			if result, err := r.LookupAddr(ctx, args[0]); err != nil {
+				multierr.AppendInto(&errs, errors.Wrap(err, "lookup reverse address"))
+			} else {
+				fmt.Printf("IP: %v\n", result)
+			}
+			if errs != nil {
+				fmt.Fprintf(os.Stderr, "some lookups not successful, this is normally fine:\n")
+				for _, err := range multierr.Errors(errs) {
+					fmt.Fprintf(os.Stderr, "    %v\n", err)
+				}
+			}
+			return nil
+		}),
+	}
+	commands = append(commands, cmdutil.CreateAlias(dnsLookup, "misc dns-lookup"))
+
+	httpHead := &cobra.Command{
+		Use:   "{{alias}} <url>",
+		Short: "Make an HTTP HEAD request.",
+		Long:  "Make an HTTP HEAD request.",
+		Run: cmdutil.RunFixedArgs(1, func(args []string) error {
+			ctx := pctx.Background("")
+			req, err := http.NewRequestWithContext(ctx, "HEAD", args[0], nil)
+			if err != nil {
+				return errors.Wrap(err, "NewRequest")
+			}
+			if content, err := httputil.DumpRequestOut(req, false); err == nil {
+				fmt.Printf("%s", content)
+			}
+			res, err := hc.Do(req)
+			if res != nil {
+				defer res.Body.Close()
+				if content, err := httputil.DumpResponse(res, true); err == nil {
+					fmt.Printf("%s", content)
+				}
+			}
+			if err != nil {
+				return errors.Wrap(err, "Do")
+			}
+			return nil
+		}),
+	}
+	commands = append(commands, cmdutil.CreateAlias(httpHead, "misc http-head"))
+
+	dial := &cobra.Command{
+		Use:   "{{alias}} <network(tcp|udp)> <address>",
+		Short: "Dials a network server and then disconnects.",
+		Long:  "Dials a network server and then disconnects.",
+		Run: cmdutil.RunFixedArgs(2, func(args []string) error {
+			ctx := pctx.Background("")
+			conn, err := d.DialContext(ctx, args[0], args[1])
+			if err != nil {
+				return errors.Wrap(err, "Dial")
+			}
+			fmt.Printf("OK: %s -> %s\n", conn.LocalAddr(), conn.RemoteAddr())
+			return errors.Wrap(conn.Close(), "Close")
+		}),
+	}
+	commands = append(commands, cmdutil.CreateAlias(dial, "misc dial"))
+
+	misc := &cobra.Command{
+		Short:  "Miscellaneous utilities unrelated to Pachyderm itself.",
+		Long:   "Miscellaneous utilities unrelated to Pachyderm itself.  These utilities can be removed or changed in minor releases; do not rely on them.",
+		Hidden: true,
+	}
+	commands = append(commands, cmdutil.CreateAlias(misc, "misc"))
+	return commands
+}
