b96a3c0cfd0247931e4cd490a5df30c3292eee49
Armaan Varadaraj <armaan.varadaraj@pachyderm.io>
2023-03-17 14:37:09 -0400

    error handling for retrieveCallInfo

--- src/internal/storage/metrics/metrics.go
+++ src/internal/storage/metrics/metrics.go
@@ -38,7 +38,10 @@ const (
 // This function also labels the request as successful or not, and records
 // the time spent in a separate metric.
 func ReportRequest(f func() error, skip ...int) (retErr error) {
-	ci := retrieveCallInfo(skip...)
+	ci, err := retrieveCallInfo(skip...)
+	if err != nil {
+		return err
+	}
 	ms, err := maybeRegisterSubsystem(ci.packageName)
 	if err != nil {
 		return err
@@ -59,7 +62,10 @@ func ReportRequest(f func() error, skip ...int) (retErr error) {
 // ReportRequestWithThroughput functions the same as ReportRequest, but also
 // reports the throughput in a separate metric.
 func ReportRequestWithThroughput(f func() (int64, error)) error {
-	ci := retrieveCallInfo()
+	ci, err := retrieveCallInfo()
+	if err != nil {
+		return err
+	}
 	ms, err := maybeRegisterSubsystem(ci.packageName)
 	if err != nil {
 		return err
@@ -81,14 +87,21 @@ type callInfo struct {
 	line        int
 }
 
-func retrieveCallInfo(skip ...int) *callInfo {
+func retrieveCallInfo(skip ...int) (*callInfo, error) {
 	skipFrames := 2
 	if len(skip) > 0 {
 		skipFrames += skip[0]
 	}
-	pc, file, line, _ := runtime.Caller(skipFrames)
+	pc, file, line, ok := runtime.Caller(skipFrames)
+	if !ok {
+		return nil, errors.Errorf("could not retrieve caller info")
+	}
 	_, fileName := path.Split(file)
-	parts := strings.Split(runtime.FuncForPC(pc).Name(), ".")
+	fn := runtime.FuncForPC(pc)
+	if fn == nil {
+		return nil, errors.Errorf("could not retrieve function for program counter")
+	}
+	parts := strings.Split(fn.Name(), ".")
 	pl := len(parts)
 	packageName := ""
 	funcName := parts[pl-1]
@@ -105,7 +118,7 @@ func retrieveCallInfo(skip ...int) *callInfo {
 		fileName:    fileName,
 		funcName:    funcName,
 		line:        line,
-	}
+	}, nil
 }
 
 func maybeRegisterSubsystem(packageName string) (*metrics, error) {
