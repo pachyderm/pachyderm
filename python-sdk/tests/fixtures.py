855fa4bcdd91e4c04bc30743c18e5c8c647cc145
Kyle White <kylewhite@gmail.com>
2023-03-23 15:30:12 -0400

    Try running tests on enterprise and auth activated cluster, remove ent and auth setup from python. Remove create/update from tests to prevent cluster pollution

--- python-sdk/tests/fixtures.py
+++ python-sdk/tests/fixtures.py
@@ -30,27 +30,16 @@ def client(request) -> "TestClient":
 def auth_client(client: "TestClient") -> "TestClient":
     from pachyderm_sdk.api.identity import IdentityServerConfig
 
-    activation_code = os.environ.get(ENTERPRISE_CODE_ENV)
-    auth_token = "i-am-root"
-    secret = "secret"
-    license_id = "localhost"
-    license_server = "localhost:1650"
-
-    client.license.activate(activation_code=activation_code)
-    client.license.add_cluster(id=license_id, address=license_server, secret=secret)
-    client.enterprise.activate(license_server=license_server, id=license_id, secret=secret)
+    auth_token = "iamroot"
 
     client.auth_token = auth_token
-    client.auth.activate(root_token=auth_token)
-    client.identity.set_identity_server_config(
-        config=IdentityServerConfig(issuer="http://localhost:1658")
-    )
+
     yield client
 
     # not redundant because auth_token could be overriden by tests
     client.auth_token = auth_token
     client.tear_down()
-    client.enterprise.deactivate()
+    #client.enterprise.deactivate()
 
 
 class TestClient(_Client):
