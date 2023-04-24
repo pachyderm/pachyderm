aab7b6a3a08016ec7e33cb78eff11ed48a2a0135
djanicekpach <daniel.janicek@pachyderm.io>
2023-04-24 13:33:43 -0700

    revert old version changes

--- etc/testing/circle/workloads/codecoverage/build-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-# Run in codecoverage folder
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -C ./collector
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -C ./extractor
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -C ./merger
-
-docker build . -t pachyderm/codecoverage:local
