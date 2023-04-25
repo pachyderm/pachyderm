5337f707e982a9a02624acab65e9c64087e08ed8
djanicekpach <daniel.janicek@pachyderm.io>
2023-04-25 12:18:48 -0700

    linter

--- etc/testing/circle/run_tests.sh
+++ etc/testing/circle/run_tests.sh
@@ -8,7 +8,8 @@ export VM_IP
 ENTERPRISE_PORT="31650"
 export ENTEPRRISE_PORT
 
-TESTFLAGS="-v -cover -test.gocoverdir='$TEST_RESULTS' -covermode=atomic -coverpkg=./... | stdbuf -i0 tee -a /tmp/results"
+testFlagsArr=(-v -cover -test.gocoverdir="$TEST_RESULTS" -covermode=atomic -coverpkg=./... | stdbuf -i0 tee -a /tmp/results)
+TESTFLAGS="${testFlagsArr[@]}"
 export TESTFLAGS
 
 # make launch-kube connects with kubernetes, so it should just be available
