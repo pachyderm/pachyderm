8f117ce4e99da970a231c6a0cfe99db582303d4d
djanicekpach <daniel.janicek@pachyderm.io>
2023-05-26 14:30:16 -0700

    DRAFT test reporting

--- src/pps/filter_test.go
+++ src/pps/filter_test.go
@@ -19,3 +19,17 @@ func TestDatumStateFilter(t *testing.T) {
 		t.Errorf("%v disallowed matching state", f)
 	}
 }
+
+func TestDatumStateFilter2(t *testing.T) {
+	var (
+		f = pps.ListDatumRequest_Filter{State: []pps.DatumState{pps.DatumState_FAILED}}
+		d = &pps.DatumInfo{State: pps.DatumState_UNKNOWN}
+	)
+	if f.Allow(d) {
+		t.Errorf("%v allowed %v", f, d.State)
+	}
+	d.State = pps.DatumState_FAILED
+	if !f.Allow(d) {
+		t.Errorf("%v disallowed matching state", f)
+	}
+}
