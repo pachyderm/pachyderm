84e07d836415becd630d9360dd58eede69ac9f45
Albert Cui <albert@pachyderm.io>
2023-05-30 19:58:00 -0400

    add initial v2.7.0 migration for core projects

---
+++ src/internal/clusterstate/v2.7.0/clusterstate.go
@@ -0,0 +1,19 @@
+package v2_7_0
+
+import (
+	"context"
+
+	"github.com/pachyderm/pachyderm/v2/src/internal/migrations"
+)
+
+func Migrate(state migrations.State) migrations.State {
+	return state.Apply("Migrate collections.projects to pfs.projects", func(ctx context.Context, env migrations.Env) error {
+		if err := SetupCore(ctx, env.Tx); err != nil {
+			return err
+		}
+		if err := SetupProjectsTable(ctx, env.Tx); err != nil {
+			return err
+		}
+		return nil
+	})
+}
---
+++ src/internal/clusterstate/v2.7.0/coredb.go
@@ -0,0 +1,51 @@
+package v2_7_0
+
+import (
+	"context"
+
+	"github.com/pachyderm/pachyderm/v2/src/internal/errors"
+	"github.com/pachyderm/pachyderm/v2/src/internal/pachsql"
+)
+
+func SetupCore(ctx context.Context, tx *pachsql.Tx) error {
+	if _, err := tx.ExecContext(ctx, `CREATE SCHEMA IF NOT EXISTS core;`); err != nil {
+		return errors.Wrap(err, "error creating core schema")
+	}
+	if _, err := tx.ExecContext(ctx, `
+		CREATE OR REPLACE FUNCTION core.set_updated_at_to_now() RETURNS TRIGGER AS $$
+		BEGIN
+			NEW.updated_at = now();
+			RETURN NEW;
+		END;
+		$$ language 'plpgsql';
+	`); err != nil {
+		return errors.Wrap(err, "error creating set_updated_at_to_now trigger function")
+	}
+
+	if err := SetupProjectsTable(ctx, tx); err != nil {
+		return errors.Wrap(err, "error creating projects table")
+	}
+	return nil
+}
+
+func SetupProjectsTable(ctx context.Context, tx *pachsql.Tx) error {
+	if _, err := tx.ExecContext(ctx, `
+		CREATE TABLE IF NOT EXISTS core.projects (
+			id bigserial PRIMARY KEY,
+			name text UNIQUE NOT NULL,
+			description text,
+			created_at timestamptz NOT NULL,
+			updated_at timestamptz NOT NULL
+		);
+	`); err != nil {
+		return errors.Wrap(err, "error creating projects table")
+	}
+	if _, err := tx.ExecContext(ctx, `
+		CREATE TRIGGER set_updated_at
+			BEFORE INSERT OR UPDATE ON core.projects
+			FOR EACH ROW EXECUTE PROCEDURE core.set_updated_at_to_now();
+	`); err != nil {
+		return errors.Wrap(err, "error creating set_updated_at trigger")
+	}
+	return nil
+}
