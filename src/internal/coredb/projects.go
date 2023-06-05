c3c20e13bf5fcfef2efe879a39b6fae0711af0f1
Syed <fahad.syed@hpe.com>
2023-06-05 13:24:03 -0400

    [PFS-91] Add library functions for create, update, and get.

--- src/internal/coredb/projects.go
+++ src/internal/coredb/projects.go
@@ -2,28 +2,95 @@ package coredb
 
 import (
 	"context"
+	"fmt"
 	"github.com/gogo/protobuf/types"
 	"github.com/pachyderm/pachyderm/v2/src/internal/errors"
 	"github.com/pachyderm/pachyderm/v2/src/internal/pachsql"
 	"github.com/pachyderm/pachyderm/v2/src/pfs"
+	"time"
 )
 
+const (
+	insertProjectQuery     = "INSERT INTO core.projects (name, description, created_at, updated_at) VALUES ($1,$2,$3,$4);"
+	updateProjectBaseQuery = "UPDATE core.projects SET name = $1, description = $2, created_at = $3, updated_at = $4"
+)
+
+// CreateProject creates an entry in the core.projects table.
 func CreateProject(ctx context.Context, tx *pachsql.Tx, project *pfs.ProjectInfo) error {
+	//todo: this can be swapped out with postgres' created_at trigger later.
 	createdAt, err := types.TimestampFromProto(project.CreatedAt)
 	if err != nil {
 		return errors.EnsureStack(err)
 	}
-	_, err = tx.ExecContext(ctx, "INSERT INTO core.projects (name, description, created_at) VALUES ($1,$2,$3);",
-		project.Project.Name, project.Description, createdAt)
+	_, err = tx.ExecContext(ctx, insertProjectQuery, project.Project.Name, project.Description, createdAt, nil)
 	//todo: insert project.authInfo into auth table.
 	return errors.EnsureStack(err)
 }
 
+// GetProject retrieves an entry from the core.projects table by project name.
 func GetProject(ctx context.Context, tx *pachsql.Tx, projectName string) (*pfs.ProjectInfo, error) {
-	row := tx.QueryRowContext(ctx, "SELECT (name, description, created_at) FROM core.projects WHERE name = $1;", projectName)
+	return getProject(ctx, tx, "where name = $1", projectName)
+}
+
+// GetProjectByID is like GetProject, but retrieves an entry using the row id.
+func GetProjectByID(ctx context.Context, tx *pachsql.Tx, id int64) (*pfs.ProjectInfo, error) {
+	return getProject(ctx, tx, "where id = $1", id)
+}
+
+func getProject(ctx context.Context, tx *pachsql.Tx, where string, whereVal interface{}) (*pfs.ProjectInfo, error) {
+	row := tx.QueryRowContext(ctx, fmt.Sprintf("SELECT name, description, created_at FROM core.projects %s", where), whereVal)
 	project := &pfs.ProjectInfo{Project: &pfs.Project{}}
-	if err := row.Scan(&project.Project.Name, &project.Description, &project.CreatedAt); err != nil {
+	var createdAt time.Time
+	var err error
+	if err = row.Scan(&project.Project.Name, &project.Description, &createdAt); err != nil {
+		return nil, errors.EnsureStack(err)
+	}
+	project.CreatedAt, err = types.TimestampProto(createdAt)
+	if err != nil {
 		return nil, errors.EnsureStack(err)
 	}
 	return project, nil
+}
+
+// UpdateProject updates all fields of an existing project entry in the core.projects table by name. If 'upsert' is set to true, UpdateProject()
+// will attempt to call CreateProject() if the entry does not exist. If updateTime is not provided, the current time will be used.
+func UpdateProject(ctx context.Context, tx *pachsql.Tx, project *pfs.ProjectInfo, upsert bool, updateTime ...*types.Timestamp) error {
+	return updateProject(ctx, tx, project, "where name = $5", project.Project.Name, upsert, updateTime...)
+}
+
+// UpdateProjectByID is like UpdateProject, but uses the row id instead of the name. It does not allow upserting.
+func UpdateProjectByID(ctx context.Context, tx *pachsql.Tx, id int64, project *pfs.ProjectInfo, updateTime ...*types.Timestamp) error {
+	return updateProject(ctx, tx, project, "where id = $5", id, false, updateTime...)
+}
+
+func updateProject(ctx context.Context, tx *pachsql.Tx, project *pfs.ProjectInfo, where string, whereVal interface{}, upsert bool, updateTime ...*types.Timestamp) error {
+	createdAt, err := types.TimestampFromProto(project.CreatedAt)
+	if err != nil {
+		return errors.EnsureStack(err)
+	}
+	updatedAt := time.Now()
+	if updateTime != nil {
+		updatedAt, err = types.TimestampFromProto(updateTime[0])
+		if err != nil {
+			return errors.EnsureStack(err)
+		}
+	}
+	res, err := tx.ExecContext(ctx, fmt.Sprintf("%s %s;", updateProjectBaseQuery, where),
+		project.Project.Name, project.Description, createdAt, updatedAt, whereVal)
+	if err != nil {
+		return errors.EnsureStack(err)
+	}
+	numRows, err := res.RowsAffected()
+	if err != nil {
+		return errors.EnsureStack(err)
+	}
+	if numRows == 0 {
+		if upsert {
+			_, err := tx.ExecContext(ctx, "INSERT INTO core.projects (name, description, created_at, updated_at) VALUES ($1,$2,$3,$4);",
+				project.Project.Name, project.Description, createdAt, updatedAt)
+			return errors.EnsureStack(err)
+		}
+		return errors.New(fmt.Sprintf("%s not found in core.projects", project.Project.Name))
+	}
+	return errors.EnsureStack(err)
 }
--- src/internal/coredb/projects_test.go
+++ src/internal/coredb/projects_test.go
@@ -8,17 +8,80 @@ import (
 	"github.com/pachyderm/pachyderm/v2/src/internal/require"
 	"github.com/pachyderm/pachyderm/v2/src/pfs"
 	"testing"
+	"time"
 )
 
-func TestCreateProject(t *testing.T) {
+const (
+	testProj     = "testproj"
+	testProjDesc = "this is a test project"
+)
+
+func TestProjectCreateAndGet(t *testing.T) {
 	ctx := pctx.TestContext(t)
 	db := dockertestenv.NewTestDB(t)
 	tx, err := db.BeginTxx(ctx, nil)
 	require.NoError(t, err, "should be able to create a tx")
 	defer tx.Rollback()
 	require.NoError(t, v2_7_0.SetupCore(ctx, tx), "should be able to set up core tables")
-	info := &pfs.ProjectInfo{Project: &pfs.Project{Name: "testproj"}, Description: "this is a test project", CreatedAt: types.TimestampNow()}
-	require.NoError(t, CreateProject(ctx, tx, info))
-	info2, err := GetProject(ctx, tx, "testproj")
+	createInfo := &pfs.ProjectInfo{Project: &pfs.Project{Name: testProj}, Description: testProjDesc, CreatedAt: types.TimestampNow()}
+	require.NoError(t, CreateProject(ctx, tx, createInfo), "should be able to create project")
+	getInfo, err := GetProject(ctx, tx, testProj)
+	require.NoError(t, err, "should be able to get a project")
+	require.NoError(t, tx.Commit())
+	require.Equal(t, createInfo.Project.Name, getInfo.Project.Name)
+	require.Equal(t, createInfo.Description, getInfo.Description)
+	require.Equal(t, createInfo.CreatedAt.Seconds, getInfo.CreatedAt.Seconds)
+}
+
+func TestGetProjectByID(t *testing.T) {
+	ctx := pctx.TestContext(t)
+	db := dockertestenv.NewTestDB(t)
+	tx, err := db.BeginTxx(ctx, nil)
 	require.NoError(t, err, "should be able to create a tx")
+	defer tx.Rollback()
+	require.NoError(t, v2_7_0.SetupCore(ctx, tx), "should be able to set up core tables")
+	createInfo := &pfs.ProjectInfo{Project: &pfs.Project{Name: testProj}, Description: testProjDesc, CreatedAt: types.TimestampNow()}
+	require.NoError(t, CreateProject(ctx, tx, createInfo), "should be able to create project")
+	getInfo, err := GetProjectByID(ctx, tx, 1)
+	require.NoError(t, err, "should be able to get a project")
+	require.NoError(t, tx.Commit())
+	require.Equal(t, createInfo.Project.Name, getInfo.Project.Name)
+	require.Equal(t, createInfo.Description, getInfo.Description)
+	require.Equal(t, createInfo.CreatedAt.Seconds, getInfo.CreatedAt.Seconds)
+}
+
+func TestUpdateProject(t *testing.T) {
+	ctx := pctx.TestContext(t)
+	db := dockertestenv.NewTestDB(t)
+	tx, err := db.BeginTxx(ctx, nil)
+	require.NoError(t, err, "should be able to create a tx")
+	defer tx.Rollback()
+	require.NoError(t, v2_7_0.SetupCore(ctx, tx), "should be able to set up core tables")
+	// test upsert correctness
+	projInfo := &pfs.ProjectInfo{Project: &pfs.Project{Name: testProj}, Description: testProjDesc, CreatedAt: types.TimestampNow()}
+	require.YesError(t, UpdateProject(ctx, tx, projInfo, false), "should not be able to create project when upsert = false")
+	require.NoError(t, UpdateProject(ctx, tx, projInfo, true), "should be able to create project when upsert = true")
+	projInfo.Description = "new desc"
+	require.NoError(t, UpdateProject(ctx, tx, projInfo, false), "should be able to update project")
+	// test update time correctness
+	currTimeProto := types.TimestampNow()
+	require.NoError(t, UpdateProject(ctx, tx, projInfo, false, currTimeProto), "should be able to update project with new time")
+	row := tx.QueryRowContext(ctx, "SELECT updated_at FROM core.projects WHERE name = $1", projInfo.Project.Name)
+	var dbTime time.Time
+	require.NoError(t, row.Scan(&dbTime), "should be able to get database updated_at time")
+	dbTimeProto, err := types.TimestampProto(dbTime)
+	require.NoError(t, err, "should be able to convert time.Time to types.Timestamp")
+	require.Equal(t, dbTimeProto.Seconds, currTimeProto.Seconds)
+}
+
+func TestUpdateProjectByID(t *testing.T) {
+	ctx := pctx.TestContext(t)
+	db := dockertestenv.NewTestDB(t)
+	tx, err := db.BeginTxx(ctx, nil)
+	require.NoError(t, err, "should be able to create a tx")
+	defer tx.Rollback()
+	require.NoError(t, v2_7_0.SetupCore(ctx, tx), "should be able to set up core tables")
+	projInfo := &pfs.ProjectInfo{Project: &pfs.Project{Name: testProj}, Description: testProjDesc, CreatedAt: types.TimestampNow()}
+	require.NoError(t, CreateProject(ctx, tx, projInfo), "should not be able to create project")
+	require.NoError(t, UpdateProjectByID(ctx, tx, 1, projInfo), "should not be able to create project")
 }
