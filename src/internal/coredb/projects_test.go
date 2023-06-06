52fe1cfe73ae4b5637d1707b66a0014b46f03ba1
Syed <fahad.syed@hpe.com>
2023-06-06 11:58:27 -0400

    [PFS-91] integrate PR feedback.

--- src/internal/coredb/projects_test.go
+++ src/internal/coredb/projects_test.go
@@ -2,10 +2,12 @@ package coredb
 
 import (
 	"github.com/gogo/protobuf/types"
-	v2_7_0 "github.com/pachyderm/pachyderm/v2/src/internal/clusterstate/v2.7.0"
+	"github.com/pachyderm/pachyderm/v2/src/internal/clusterstate"
 	"github.com/pachyderm/pachyderm/v2/src/internal/dockertestenv"
+	"github.com/pachyderm/pachyderm/v2/src/internal/migrations"
 	"github.com/pachyderm/pachyderm/v2/src/internal/pctx"
 	"github.com/pachyderm/pachyderm/v2/src/internal/require"
+	"github.com/pachyderm/pachyderm/v2/src/internal/testetcd"
 	"github.com/pachyderm/pachyderm/v2/src/pfs"
 	"testing"
 )
@@ -21,7 +23,8 @@ func TestCreateProject(t *testing.T) {
 	tx, err := db.BeginTxx(ctx, nil)
 	require.NoError(t, err, "should be able to create a tx")
 	defer tx.Rollback()
-	require.NoError(t, v2_7_0.SetupCore(ctx, tx), "should be able to set up core tables")
+	migrationEnv := migrations.Env{EtcdClient: testetcd.NewEnv(ctx, t).EtcdClient}
+	require.NoError(t, migrations.ApplyMigrations(ctx, db, migrationEnv, clusterstate.DesiredClusterState), "should be able to set up tables")
 	createInfo := &pfs.ProjectInfo{Project: &pfs.Project{Name: testProj}, Description: testProjDesc}
 	require.NoError(t, CreateProject(ctx, tx, createInfo), "should be able to create project")
 	getInfo, err := GetProject(ctx, tx, testProj)
@@ -36,7 +39,8 @@ func TestDeleteProject(t *testing.T) {
 	tx, err := db.BeginTxx(ctx, nil)
 	require.NoError(t, err, "should be able to create a tx")
 	defer tx.Rollback()
-	require.NoError(t, v2_7_0.SetupCore(ctx, tx), "should be able to set up core tables")
+	migrationEnv := migrations.Env{EtcdClient: testetcd.NewEnv(ctx, t).EtcdClient}
+	require.NoError(t, migrations.ApplyMigrations(ctx, db, migrationEnv, clusterstate.DesiredClusterState), "should be able to set up tables")
 	createInfo := &pfs.ProjectInfo{Project: &pfs.Project{Name: testProj}, Description: testProjDesc}
 	require.NoError(t, CreateProject(ctx, tx, createInfo), "should be able to create project")
 	require.NoError(t, DeleteProject(ctx, tx, createInfo.Project.Name), "should be able to delete project")
@@ -51,7 +55,8 @@ func TestGetProjectByID(t *testing.T) {
 	tx, err := db.BeginTxx(ctx, nil)
 	require.NoError(t, err, "should be able to create a tx")
 	defer tx.Rollback()
-	require.NoError(t, v2_7_0.SetupCore(ctx, tx), "should be able to set up core tables")
+	migrationEnv := migrations.Env{EtcdClient: testetcd.NewEnv(ctx, t).EtcdClient}
+	require.NoError(t, migrations.ApplyMigrations(ctx, db, migrationEnv, clusterstate.DesiredClusterState), "should be able to set up tables")
 	createInfo := &pfs.ProjectInfo{Project: &pfs.Project{Name: testProj}, Description: testProjDesc, CreatedAt: types.TimestampNow()}
 	require.NoError(t, CreateProject(ctx, tx, createInfo), "should be able to create project")
 	getInfo, err := GetProjectByID(ctx, tx, 1)
@@ -68,7 +73,8 @@ func TestUpdateProject(t *testing.T) {
 	tx, err := db.BeginTxx(ctx, nil)
 	require.NoError(t, err, "should be able to create a tx")
 	defer tx.Rollback()
-	require.NoError(t, v2_7_0.SetupCore(ctx, tx), "should be able to set up core tables")
+	migrationEnv := migrations.Env{EtcdClient: testetcd.NewEnv(ctx, t).EtcdClient}
+	require.NoError(t, migrations.ApplyMigrations(ctx, db, migrationEnv, clusterstate.DesiredClusterState), "should be able to set up tables")
 	// test upsert correctness
 	projInfo := &pfs.ProjectInfo{Project: &pfs.Project{Name: testProj}, Description: testProjDesc, CreatedAt: types.TimestampNow()}
 	require.YesError(t, UpdateProject(ctx, tx, projInfo, false), "should not be able to create project when upsert = false")
@@ -83,7 +89,8 @@ func TestUpdateProjectByID(t *testing.T) {
 	tx, err := db.BeginTxx(ctx, nil)
 	require.NoError(t, err, "should be able to create a tx")
 	defer tx.Rollback()
-	require.NoError(t, v2_7_0.SetupCore(ctx, tx), "should be able to set up core tables")
+	migrationEnv := migrations.Env{EtcdClient: testetcd.NewEnv(ctx, t).EtcdClient}
+	require.NoError(t, migrations.ApplyMigrations(ctx, db, migrationEnv, clusterstate.DesiredClusterState), "should be able to set up tables")
 	projInfo := &pfs.ProjectInfo{Project: &pfs.Project{Name: testProj}, Description: testProjDesc, CreatedAt: types.TimestampNow()}
 	require.NoError(t, CreateProject(ctx, tx, projInfo), "should be able to create project")
 	require.NoError(t, UpdateProjectByID(ctx, tx, 1, projInfo), "should be able to update project")
