bedb8872faf3269f2b2948c7a852870aeb652fd2
Albert Cui <albert@pachyderm.io>
2023-06-11 02:12:23 -0400

    tmp

--- src/internal/clusterstate/v2.7.0/clusterstate.go
+++ src/internal/clusterstate/v2.7.0/clusterstate.go
@@ -16,8 +16,20 @@ func Migrate(state migrations.State) migrations.State {
 			return nil
 		}).
 		Apply("Migrate collections.projects to core.projects", func(ctx context.Context, env migrations.Env) error {
-			if err := migrateProjectsTable(ctx, env.Tx); err != nil {
-				return errors.Wrap(err, "error migrating projects table")
+			if err := migrateProjects(ctx, env.Tx); err != nil {
+				return errors.Wrap(err, "error migrating projects")
+			}
+			return nil
+		}).
+		Apply("Create pfs schema", func(ctx context.Context, env migrations.Env) error {
+			if err := createPFSSchema(ctx, env.Tx); err != nil {
+				return errors.Wrap(err, "error creating pfs schema")
+			}
+			return nil
+		}).
+		Apply("Migrate collections.repos to pfs.repos", func(ctx context.Context, env migrations.Env) error {
+			if err := migrateRepos(ctx, env.Tx); err != nil {
+				return errors.Wrap(err, "error migrating repos")
 			}
 			return nil
 		})
---
+++ src/internal/clusterstate/v2.7.0/pfsdb.go
@@ -0,0 +1,57 @@
+package v2_7_0
+
+import (
+	"context"
+
+	"github.com/pachyderm/pachyderm/v2/src/internal/errors"
+	"github.com/pachyderm/pachyderm/v2/src/internal/pachsql"
+)
+
+func createPFSSchema(ctx context.Context, tx *pachsql.Tx) error {
+	// pfs schema already exists, but this SQL is idempotent
+	if _, err := tx.ExecContext(ctx, `CREATE SCHEMA IF NOT EXISTS pfs;`); err != nil {
+		return errors.Wrap(err, "error creating core schema")
+	}
+
+	if err := createReposTable(ctx, tx); err != nil {
+		return errors.Wrap(err, "error creating pfs.repos table")
+	}
+	return nil
+}
+
+func createReposTable(ctx context.Context, tx *pachsql.Tx) error {
+	if _, err := tx.ExecContext(ctx, `
+		CREATE TYPE pfs.repo_type AS ENUM (
+			'unknown',
+			'user',
+			'meta',
+			'spec'
+		);
+	`); err != nil {
+		return errors.Wrap(err, "error creating repo_type enum")
+	}
+	if _, err := tx.ExecContext(ctx, `
+		CREATE TABLE IF NOT EXISTS pfs.repos (
+			id bigserial PRIMARY KEY,
+			name text NOT NULL,
+			type pfs.repo_type NOT NULL,
+			project_id bigint NOT NULL REFERENCES core.projects(id),
+			created_at timestamptz DEFAULT CURRENT_TIMESTAMP NOT NULL,
+			updated_at timestamptz DEFAULT CURRENT_TIMESTAMP NOT NULL
+		);
+	`); err != nil {
+		return errors.Wrap(err, "error creating pfs.repos table")
+	}
+	if _, err := tx.ExecContext(ctx, `
+		CREATE TRIGGER set_updated_at
+			BEFORE UPDATE ON pfs.repos
+			FOR EACH ROW EXECUTE PROCEDURE core.set_updated_at_to_now();
+	`); err != nil {
+		return errors.Wrap(err, "error creating set_updated_at trigger")
+	}
+	return nil
+}
+
+func migrateRepos(ctx context.Context, tx *pachsql.Tx) error {
+	return nil
+}
