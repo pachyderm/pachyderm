7d3e358198e1a4906fd0b4b7703002ec98ea08a8
Albert Cui <albert@pachyderm.io>
2023-06-12 11:49:08 -0400

    Migrate all records from `collections.projects` to `core.projects` (#8920)
    * Copy over proto code so that this migration is immutable
    * Migrate all collections.projects to core.projects

--- src/internal/clusterstate/v2.7.0/clusterstate.go
+++ src/internal/clusterstate/v2.7.0/clusterstate.go
@@ -3,14 +3,22 @@ package v2_7_0
 import (
 	"context"
 
+	"github.com/pachyderm/pachyderm/v2/src/internal/errors"
 	"github.com/pachyderm/pachyderm/v2/src/internal/migrations"
 )
 
 func Migrate(state migrations.State) migrations.State {
-	return state.Apply("Setup core.* tables", func(ctx context.Context, env migrations.Env) error {
-		if err := SetupCore(ctx, env.Tx); err != nil {
-			return err
-		}
-		return nil
-	})
+	return state.
+		Apply("Create core schema", func(ctx context.Context, env migrations.Env) error {
+			if err := createCoreSchema(ctx, env.Tx); err != nil {
+				return errors.Wrap(err, "error creating core schema")
+			}
+			return nil
+		}).
+		Apply("Migrate collections.projects to core.projects", func(ctx context.Context, env migrations.Env) error {
+			if err := migrateProjects(ctx, env.Tx); err != nil {
+				return errors.Wrap(err, "error migrating projects table")
+			}
+			return nil
+		})
 }
