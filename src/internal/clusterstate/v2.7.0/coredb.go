58133da08c4515999538849f111beb9dfef7629c
Albert Cui <albert@pachyderm.io>
2023-06-12 11:49:08 -0400

    Migrate all records from `collections.projects` to `core.projects` (#8920)
    * Copy over proto code so that this migration is immutable
    * Migrate all collections.projects to core.projects

--- src/internal/clusterstate/v2.7.0/coredb.go
+++ src/internal/clusterstate/v2.7.0/coredb.go
@@ -2,12 +2,23 @@ package v2_7_0
 
 import (
 	"context"
+	"time"
+
+	proto "github.com/gogo/protobuf/proto"
 
 	"github.com/pachyderm/pachyderm/v2/src/internal/errors"
 	"github.com/pachyderm/pachyderm/v2/src/internal/pachsql"
 )
 
-func SetupCore(ctx context.Context, tx *pachsql.Tx) error {
+// CollectionRecord is a record in a collections table.
+type CollectionRecord struct {
+	Key       string    `db:"key"`
+	Proto     []byte    `db:"proto"`
+	CreatedAt time.Time `db:"createdat"`
+	UpdatedAt time.Time `db:"updatedat"`
+}
+
+func createCoreSchema(ctx context.Context, tx *pachsql.Tx) error {
 	if _, err := tx.ExecContext(ctx, `CREATE SCHEMA IF NOT EXISTS core;`); err != nil {
 		return errors.Wrap(err, "error creating core schema")
 	}
@@ -22,13 +33,13 @@ func SetupCore(ctx context.Context, tx *pachsql.Tx) error {
 		return errors.Wrap(err, "error creating set_updated_at_to_now trigger function")
 	}
 
-	if err := SetupProjectsTable(ctx, tx); err != nil {
+	if err := createProjectsTable(ctx, tx); err != nil {
 		return errors.Wrap(err, "error creating projects table")
 	}
 	return nil
 }
 
-func SetupProjectsTable(ctx context.Context, tx *pachsql.Tx) error {
+func createProjectsTable(ctx context.Context, tx *pachsql.Tx) error {
 	if _, err := tx.ExecContext(ctx, `
 		CREATE TABLE IF NOT EXISTS core.projects (
 			id bigserial PRIMARY KEY,
@@ -46,6 +57,33 @@ func SetupProjectsTable(ctx context.Context, tx *pachsql.Tx) error {
 			FOR EACH ROW EXECUTE PROCEDURE core.set_updated_at_to_now();
 	`); err != nil {
 		return errors.Wrap(err, "error creating set_updated_at trigger")
+	}
+	return nil
+}
+
+func migrateProjects(ctx context.Context, tx *pachsql.Tx) error {
+	insertStmt, err := tx.Preparex("INSERT INTO core.projects(name, description, created_at, updated_at) VALUES($1, $2, $3, $4)")
+	if err != nil {
+		return errors.Wrap(err, "error preparing insert projects statement")
+	}
+	defer insertStmt.Close()
+
+	projectColRows := []CollectionRecord{}
+	if err := tx.SelectContext(ctx, &projectColRows, "SELECT key, proto, createdat, updatedat FROM collections.projects ORDER BY createdat ASC"); err != nil {
+		return errors.Wrap(err, "error listing from collections.projects")
+	}
+	projectInfos := []ProjectInfo{}
+	for _, project := range projectColRows {
+		projectInfo := ProjectInfo{}
+		if err := proto.Unmarshal(project.Proto, &projectInfo); err != nil {
+			return errors.Wrap(err, "error unmarshalling project")
+		}
+		projectInfos = append(projectInfos, projectInfo)
+	}
+	for i := 0; i < len(projectColRows); i++ {
+		if _, err := insertStmt.ExecContext(ctx, projectInfos[i].Project.Name, projectInfos[i].Description, projectColRows[i].CreatedAt, projectColRows[i].UpdatedAt); err != nil {
+			return errors.Wrap(err, "error inserting project")
+		}
 	}
 	return nil
 }
--- src/internal/clusterstate/v2.7.0_test.go
+++ src/internal/clusterstate/v2.7.0_test.go
@@ -1,9 +1,13 @@
 package clusterstate
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
+	proto "github.com/gogo/protobuf/proto"
+
+	v2_7_0 "github.com/pachyderm/pachyderm/v2/src/internal/clusterstate/v2.7.0"
 	"github.com/pachyderm/pachyderm/v2/src/internal/dockertestenv"
 	"github.com/pachyderm/pachyderm/v2/src/internal/migrations"
 	"github.com/pachyderm/pachyderm/v2/src/internal/pctx"
@@ -11,7 +15,15 @@ import (
 	"github.com/pachyderm/pachyderm/v2/src/internal/testetcd"
 )
 
-func Test_v2_7_0_ClusterState(t *testing.T) {
+func Test_v2_7_0_ClusterState_Projects(t *testing.T) {
+	type project struct {
+		ID          int       `db:"id"`
+		Name        string    `db:"name"`
+		Description string    `db:"description"`
+		CreatedAt   time.Time `db:"created_at"`
+		UpdatedAt   time.Time `db:"updated_at"`
+	}
+
 	if DesiredClusterState.Number() > state_2_7_0.Number() {
 		t.Skip("skipping test because desired state is newer than this migration")
 	}
@@ -20,33 +32,45 @@ func Test_v2_7_0_ClusterState(t *testing.T) {
 	defer db.Close()
 	migrationEnv := migrations.Env{EtcdClient: testetcd.NewEnv(ctx, t).EtcdClient}
 
-	// apply the migration
+	// Pre-migration
+	// Ceate sample test data in collections.projects table
+	require.NoError(t, migrations.ApplyMigrations(ctx, db, migrationEnv, state_2_6_0))
+	tx, err := db.BeginTxx(ctx, nil)
+	require.NoError(t, err)
+	defer tx.Rollback()
+	for i := 0; i < 10; i++ {
+		name := fmt.Sprintf("project%d", i)
+		projectInfo := v2_7_0.ProjectInfo{Project: &v2_7_0.Project{Name: name}, Description: "test " + name}
+		b, err := proto.Marshal(&projectInfo)
+		require.NoError(t, err)
+		_, err = tx.ExecContext(ctx, `INSERT INTO collections.projects(key, proto) VALUES($1, $2)`, name, b)
+		require.NoError(t, err)
+	}
+	require.NoError(t, tx.Commit())
+
+	// Get all existing projects in collections.projects including the default project
+	expectedProjects := make(map[string]project)
+	collectionRecords := []v2_7_0.CollectionRecord{}
+	require.NoError(t, db.SelectContext(ctx, &collectionRecords, `SELECT key, proto, createdat, updatedat FROM collections.projects`))
+	for _, row := range collectionRecords {
+		projectInfo := v2_7_0.ProjectInfo{}
+		require.NoError(t, proto.Unmarshal(row.Proto, &projectInfo))
+		expectedProjects[projectInfo.Project.Name] = project{Name: projectInfo.Project.Name, Description: projectInfo.Description, CreatedAt: row.CreatedAt, UpdatedAt: row.UpdatedAt}
+	}
+
+	// Apply the migration of interest
 	require.NoError(t, migrations.ApplyMigrations(ctx, db, migrationEnv, state_2_7_0))
 	require.NoError(t, migrations.BlockUntil(ctx, db, state_2_7_0))
 
-	// make some assertions
-	projectNames := []string{"project1", "project2"}
-	stmt, err := db.Prepare("INSERT INTO core.projects(name) VALUES($1)")
-	require.NoError(t, err)
-	for _, name := range projectNames {
-		_, err := stmt.ExecContext(ctx, name)
-		require.NoError(t, err)
+	// Check whether all the data is migrated to core.gotProjects table
+	gotProjects := []project{}
+	require.NoError(t, db.SelectContext(ctx, &gotProjects, `SELECT id, name, description, created_at, updated_at FROM core.projects ORDER BY id`))
+	require.Equal(t, len(expectedProjects), len(gotProjects))
+	for i, p := range gotProjects {
+		require.Equal(t, i+1, p.ID)
+		require.Equal(t, expectedProjects[p.Name].Name, p.Name)
+		require.Equal(t, expectedProjects[p.Name].Description, p.Description)
+		require.Equal(t, expectedProjects[p.Name].CreatedAt, p.CreatedAt)
+		require.Equal(t, expectedProjects[p.Name].UpdatedAt, p.UpdatedAt)
 	}
-
-	rowsInserted := 0
-	rows, err := db.QueryContext(ctx, `SELECT id, name, created_at, updated_at FROM core.projects ORDER BY id`)
-	require.NoError(t, err)
-	defer rows.Close()
-	var (
-		id                     int
-		name                   string
-		created_at, updated_at time.Time
-	)
-	for rows.Next() {
-		require.NoError(t, rows.Scan(&id, &name, &created_at, &updated_at))
-		require.Equal(t, projectNames[id-1], name)
-		rowsInserted++
-		t.Log(id, name, created_at, updated_at)
-	}
-	require.Equal(t, 2, rowsInserted)
 }
