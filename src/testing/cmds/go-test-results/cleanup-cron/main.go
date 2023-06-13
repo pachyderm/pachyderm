0baa340a8e8f2195eba6ca025c992af31e98475f
Daniel Janicek <daniel.janicek@hpe.com>
2023-06-13 11:05:07 -0700

    working DB cleanup cron

--- src/testing/cmds/go-test-results/cleanup-cron/main.go
+++ src/testing/cmds/go-test-results/cleanup-cron/main.go
@@ -4,12 +4,15 @@ import (
 	"context"
 	"time"
 
+	"github.com/pachyderm/pachyderm/v2/src/internal/dbutil"
+	"github.com/pachyderm/pachyderm/v2/src/internal/errors"
 	"github.com/pachyderm/pachyderm/v2/src/internal/log"
 	"github.com/pachyderm/pachyderm/v2/src/internal/pctx"
+	gotestresults "github.com/pachyderm/pachyderm/v2/src/testing/cmds/go-test-results"
 	"go.uber.org/zap"
 )
 
-const retentionPeriod = time.Hour * 24 * 365 * 3
+var retentionPeriod = time.Now().Add(-time.Hour * 24 * 365 * 3)
 
 // This is built and runs in a pachyderm cron pipeline to clean up stale data
 func main() {
@@ -23,6 +26,24 @@ func main() {
 
 func run(ctx context.Context) error {
 	log.Info(ctx, "Running DB Cleanup")
-
+	db, err := dbutil.NewDB(
+		dbutil.WithHostPort(gotestresults.PostgresqlHost, 5432),
+		dbutil.WithDBName("ci_metrics"),
+		dbutil.WithUserPassword(gotestresults.PostgresqlUser, gotestresults.PostgresqlPassword),
+	)
+	if err != nil {
+		return errors.Wrapf(err, "Opening db connection")
+	}
+	log.Info(ctx, "Deleting logs before cutoff time", zap.String("cutoff time", retentionPeriod.String()))
+	_, err = db.Exec(`
+		DELETE FROM public.test_results 
+		USING ci_jobs 
+		WHERE public.ci_jobs.job_timestamp < $1
+		AND ci_jobs.workflow_id=test_results.workflow_id AND ci_jobs.job_id=test_results.job_id AND ci_jobs.job_executor=test_results.job_executor`,
+		retentionPeriod,
+	)
+	if err != nil {
+		return errors.Wrapf(err, "Deleting old records")
+	}
 	return nil
 }
