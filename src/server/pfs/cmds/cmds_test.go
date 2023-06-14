d64cf219fd3076223ced946d0b717b67fbc95ba1
Fahad Syed <DeadbeamNetworks@gmail.com>
2023-06-14 15:35:52 -0400

    [PFS-91] Fix cmds unit test.

--- src/server/pfs/cmds/cmds_test.go
+++ src/server/pfs/cmds/cmds_test.go
@@ -361,7 +361,7 @@ func TestProject(t *testing.T) {
 	// c := env.PachClient
 	// using xargs to trim newlines
 	require.NoError(t, tu.PachctlBashCmd(t, c, `
-                pachctl list project | xargs | match '^ACTIVE PROJECT CREATED DESCRIPTION \* default - -$'
+                pachctl list project | xargs | match '^ACTIVE PROJECT CREATED DESCRIPTION \* default ([^-]+ ago) -$'
                 pachctl create project foo
                 pachctl list project | match "foo     ([^-]+ ago) -"
 		`,
@@ -381,7 +381,7 @@ func TestProject(t *testing.T) {
                 `,
 	).Run())
 	require.NoError(t, tu.PachctlBashCmd(t, c, `
-                pachctl list project | xargs | match '^ACTIVE PROJECT CREATED DESCRIPTION \* default - -$'
+                pachctl list project | xargs | match '^ACTIVE PROJECT CREATED DESCRIPTION \* default ([^-]+ ago) -$'
                 pachctl create project foo
                 `,
 	).Run())
