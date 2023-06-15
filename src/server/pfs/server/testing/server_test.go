272101b9f985170b045377ba5a5226ef01e703f3
Alon Cohen <alonco124@gmail.com>
2023-06-15 23:38:09 +0300

    [CORE-1765] Eliminate double slashes (#8935) (#8949)
    * [CORE-1765] Eliminate double slashes (#8935)
    
    * Eliminate double slashes in test URL paths. This fixes some tests CircleCI had marked as flakey.
    
    * security: upgrade to go 1.20.5 (#8927)
    
    ---------
    
    Co-authored-by: Robert Uhl <robert.uhl@hpe.com>
    Co-authored-by: Jonathan Rockway <2367+jrockway@users.noreply.github.com>

--- src/server/pfs/server/testing/server_test.go
+++ src/server/pfs/server/testing/server_test.go
@@ -5816,10 +5816,10 @@ func TestPFS(suite *testing.T) {
 			require.NoError(t, bucket.WriteAll(ctx, path, []byte(path), nil))
 		}
 		for _, p := range paths {
-			objURL := url + "/" + p
+			objURL := url + p
 			require.NoError(t, env.PachClient.PutFileURL(commit, p, objURL, false))
 		}
-		srcURL := url + "/files"
+		srcURL := url + "files"
 		require.NoError(t, env.PachClient.PutFileURL(commit, "recursive", srcURL, true))
 		check := func() {
 			cis, err := env.PachClient.ListCommit(client.NewProjectRepo(pfs.DefaultProjectName, repo), nil, nil, 0)
