32e45498fa03932d24bb8f71025a9dbf8cf54c32
Albert Cui <albert@pachyderm.io>
2023-06-20 14:23:26 -0400

    tmp

--- src/internal/watch/postgres/impl_test.go
+++ src/internal/watch/postgres/impl_test.go
@@ -1,8 +1,32 @@
 package postgres_test
 
-import "testing"
+import (
+	"testing"
 
-func TestWatcher(t *testing.T) {
-	t.Parallel()
-	t.Skip("TODO")
+	"github.com/pachyderm/pachyderm/v2/src/internal/collection"
+	"github.com/pachyderm/pachyderm/v2/src/internal/dbutil"
+	"github.com/pachyderm/pachyderm/v2/src/internal/dockertestenv"
+	"github.com/pachyderm/pachyderm/v2/src/internal/require"
+	"github.com/pachyderm/pachyderm/v2/src/internal/testutil"
+	"github.com/pachyderm/pachyderm/v2/src/internal/watch/postgres"
+)
+
+func TestWatcRepos(t *testing.T) {
+	// ctx := pctx.TestContext(t)
+	dbOpts := dockertestenv.NewTestDBOptions(t)
+	db := testutil.OpenDB(t, dbOpts...)
+	dsn := dbutil.GetDSN(dbOpts...)
+	listener := collection.NewPostgresListener(dsn)
+	watcher, err := postgres.NewWatcher(db, listener, "test", "pfs.repos")
+	require.NoError(t, err)
+	defer watcher.Close()
+
+	// start watching for events
+	go func() {
+		for event := range watcher.Watch() {
+			t.Log(event)
+		}
+	}()
+
+	// generate events
 }
