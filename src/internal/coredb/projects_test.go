d0bfdc15e6fb8302a862210dc191d0c98f9e674b
Fahad Syed <DeadbeamNetworks@gmail.com>
2023-06-21 14:08:54 -0400

    [PFS-91] Integrate PR feedback -- stream interface + misc

--- src/internal/coredb/projects_test.go
+++ src/internal/coredb/projects_test.go
@@ -4,17 +4,16 @@ package coredb
 
 import (
 	"fmt"
-	"io"
 	"testing"
 
 	"github.com/gogo/protobuf/types"
 
 	"github.com/pachyderm/pachyderm/v2/src/internal/clusterstate"
 	"github.com/pachyderm/pachyderm/v2/src/internal/dockertestenv"
-	"github.com/pachyderm/pachyderm/v2/src/internal/errors"
 	"github.com/pachyderm/pachyderm/v2/src/internal/migrations"
 	"github.com/pachyderm/pachyderm/v2/src/internal/pctx"
 	"github.com/pachyderm/pachyderm/v2/src/internal/require"
+	"github.com/pachyderm/pachyderm/v2/src/internal/stream"
 	"github.com/pachyderm/pachyderm/v2/src/internal/testetcd"
 	"github.com/pachyderm/pachyderm/v2/src/pfs"
 )
@@ -34,7 +33,7 @@ func TestCreateProject(t *testing.T) {
 	require.NoError(t, migrations.ApplyMigrations(ctx, db, migrationEnv, clusterstate.DesiredClusterState), "should be able to set up tables")
 	createInfo := &pfs.ProjectInfo{Project: &pfs.Project{Name: testProj}, Description: testProjDesc}
 	require.NoError(t, CreateProject(ctx, tx, createInfo), "should be able to create project")
-	getInfo, err := GetProject(ctx, tx, testProj)
+	getInfo, err := GetProjectByName(ctx, tx, testProj)
 	require.NoError(t, err, "should be able to get a project")
 	require.Equal(t, createInfo.Project.Name, getInfo.Project.Name)
 	require.Equal(t, createInfo.Description, getInfo.Description)
@@ -51,12 +50,12 @@ func TestDeleteProject(t *testing.T) {
 	createInfo := &pfs.ProjectInfo{Project: &pfs.Project{Name: testProj}, Description: testProjDesc}
 	require.NoError(t, CreateProject(ctx, tx, createInfo), "should be able to create project")
 	require.NoError(t, DeleteProject(ctx, tx, createInfo.Project.Name), "should be able to delete project")
-	_, err = GetProject(ctx, tx, testProj)
+	_, err = GetProjectByName(ctx, tx, testProj)
 	require.YesError(t, err, "get project should not find row")
 	require.YesError(t, DeleteProject(ctx, tx, createInfo.Project.Name), "double delete should be an error")
 }
 
-func TestGetProjectByID(t *testing.T) {
+func TestGetProject(t *testing.T) {
 	ctx := pctx.TestContext(t)
 	db := dockertestenv.NewTestDB(t)
 	tx, err := db.BeginTxx(ctx, nil)
@@ -67,7 +66,7 @@ func TestGetProjectByID(t *testing.T) {
 	createInfo := &pfs.ProjectInfo{Project: &pfs.Project{Name: testProj}, Description: testProjDesc, CreatedAt: types.TimestampNow()}
 	require.NoError(t, CreateProject(ctx, tx, createInfo), "should be able to create project")
 	// the 'default' project is ID 1.
-	getInfo, err := GetProjectByID(ctx, tx, 2)
+	getInfo, err := GetProject(ctx, tx, 2)
 	fmt.Printf("%q %q\n", createInfo, getInfo)
 	require.NoError(t, err, "should be able to get a project")
 	require.NoError(t, tx.Commit())
@@ -91,15 +90,16 @@ func TestListProject(t *testing.T) {
 	iter, err := ListProject(ctx, db)
 	require.NoError(t, err, "should be able to list projects")
 	i := 0
-	projectInfo := &pfs.ProjectInfo{}
-	for err := iter.Next(ctx, projectInfo); !errors.Is(err, io.EOF); err = iter.Next(ctx, projectInfo) {
+
+	require.NoError(t, stream.ForEach[*pfs.ProjectInfo](ctx, iter, func(proj *pfs.ProjectInfo) error {
 		if err != nil {
 			require.NoError(t, err, "should be able to iterate over projects")
 		}
-		require.Equal(t, expectedInfos[i].Project.Name, projectInfo.Project.Name)
-		require.Equal(t, expectedInfos[i].Description, projectInfo.Description)
+		require.Equal(t, expectedInfos[i].Project.Name, proj.Project.Name)
+		require.Equal(t, expectedInfos[i].Description, proj.Description)
 		i++
-	}
+		return nil
+	}))
 }
 
 func TestDeleteAllProjects(t *testing.T) {
@@ -116,7 +116,7 @@ func TestDeleteAllProjects(t *testing.T) {
 		require.NoError(t, CreateProject(ctx, tx, createInfo), "should be able to create project")
 	}
 	require.NoError(t, DeleteAllProjects(ctx, tx))
-	_, err = GetProjectByID(ctx, tx, 1)
+	_, err = GetProject(ctx, tx, 1)
 	require.YesError(t, err, "should not have any project entries")
 }
 
@@ -130,10 +130,10 @@ func TestUpdateProject(t *testing.T) {
 	require.NoError(t, migrations.ApplyMigrations(ctx, db, migrationEnv, clusterstate.DesiredClusterState), "should be able to set up tables")
 	// test upsert correctness
 	projInfo := &pfs.ProjectInfo{Project: &pfs.Project{Name: testProj}, Description: testProjDesc, CreatedAt: types.TimestampNow()}
-	require.YesError(t, UpdateProject(ctx, tx, projInfo, false), "should not be able to create project when upsert = false")
-	require.NoError(t, UpdateProject(ctx, tx, projInfo, true), "should be able to create project when upsert = true")
+	require.YesError(t, UpdateProject(ctx, tx, 99, projInfo), "should not be able to create project when upsert = false")
+	require.NoError(t, UpsertProject(ctx, tx, projInfo), "should be able to create project when upsert = true")
 	projInfo.Description = "new desc"
-	require.NoError(t, UpdateProject(ctx, tx, projInfo, false), "should be able to update project")
+	require.NoError(t, UpsertProject(ctx, tx, projInfo), "should be able to update project")
 }
 
 func TestUpdateProjectByID(t *testing.T) {
@@ -147,5 +147,5 @@ func TestUpdateProjectByID(t *testing.T) {
 	projInfo := &pfs.ProjectInfo{Project: &pfs.Project{Name: testProj}, Description: testProjDesc, CreatedAt: types.TimestampNow()}
 	require.NoError(t, CreateProject(ctx, tx, projInfo), "should be able to create project")
 	// the 'default' project ID is 1
-	require.NoError(t, UpdateProjectByID(ctx, tx, 2, projInfo), "should be able to update project")
+	require.NoError(t, UpdateProject(ctx, tx, 2, projInfo), "should be able to update project")
 }
