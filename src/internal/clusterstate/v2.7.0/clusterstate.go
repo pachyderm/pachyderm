5d3a8b8e895bec96fc09c5acfad90d6898c1d47c
Albert Cui <albert@pachyderm.io>
2023-06-21 14:34:25 -0400

    don't conflate schema and table creation together

--- src/internal/clusterstate/v2.7.0/clusterstate.go
+++ src/internal/clusterstate/v2.7.0/clusterstate.go
@@ -11,13 +11,19 @@ func Migrate(state migrations.State) migrations.State {
 	return state.
 		Apply("Create core schema", func(ctx context.Context, env migrations.Env) error {
 			if err := createCoreSchema(ctx, env.Tx); err != nil {
-				return errors.Wrap(err, "error creating core schema")
+				return errors.Wrap(err, "creating core schema")
+			}
+			return nil
+		}).
+		Apply("Create core.projects table", func(ctx context.Context, env migrations.Env) error {
+			if err := createProjectsTable(ctx, env.Tx); err != nil {
+				return errors.Wrap(err, "creating core.projects table")
 			}
 			return nil
 		}).
 		Apply("Migrate collections.projects to core.projects", func(ctx context.Context, env migrations.Env) error {
 			if err := migrateProjects(ctx, env.Tx); err != nil {
-				return errors.Wrap(err, "error migrating projects table")
+				return errors.Wrap(err, "migrating collections.projects to core.projects")
 			}
 			return nil
 		})
