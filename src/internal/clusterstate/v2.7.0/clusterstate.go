7e3ab28dde75800f931a0deebab237fe66180092
Fahad Syed <deadbeamNetworks@gmail.com>
2023-06-27 13:27:45 -0400

    [PFS-91] Fix merge conflict before rebase.

--- src/internal/clusterstate/v2.7.0/clusterstate.go
+++ src/internal/clusterstate/v2.7.0/clusterstate.go
@@ -2,6 +2,8 @@ package v2_7_0
 
 import (
 	"context"
+	col "github.com/pachyderm/pachyderm/v2/src/internal/collection"
+	"github.com/pachyderm/pachyderm/v2/src/internal/ppsdb"
 
 	"github.com/pachyderm/pachyderm/v2/src/internal/errors"
 	"github.com/pachyderm/pachyderm/v2/src/internal/migrations"
@@ -9,6 +11,11 @@ import (
 
 func Migrate(state migrations.State) migrations.State {
 	return state.
+		Apply("create cluster defaults", func(ctx context.Context, env migrations.Env) error {
+			var cc []col.PostgresCollection
+			cc = append(cc, ppsdb.CollectionsV2_7_0()...)
+			return col.SetupPostgresCollections(ctx, env.Tx, cc...)
+		}).
 		Apply("Create core schema", func(ctx context.Context, env migrations.Env) error {
 			if err := createCoreSchema(ctx, env.Tx); err != nil {
 				return errors.Wrap(err, "creating core schema")
