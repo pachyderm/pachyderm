eebe1c2847cd25b0f27c27d6b5c1dc0f6aac37f0
Albert Cui <albert@pachyderm.io>
2023-06-28 11:19:36 -0400

    Improve tests (#9001)

--- src/internal/clusterstate/v2.7.0_test.go
+++ src/internal/clusterstate/v2.7.0_test.go
@@ -2,7 +2,6 @@ package clusterstate
 
 import (
 	"context"
-	"fmt"
 	"strings"
 	"testing"
 
@@ -19,14 +18,15 @@ import (
 	"github.com/pachyderm/pachyderm/v2/src/pfs"
 )
 
-// Create sample test data in collections.projects table
+// Create sample test data "a la collections", i.e. insert data into collections.* tables
 func setupTestData(t *testing.T, ctx context.Context, db *sqlx.DB) {
 	t.Helper()
 	tx, err := db.BeginTxx(ctx, nil)
 	require.NoError(t, err)
 	defer tx.Rollback()
-	for i := 0; i < 12; i++ {
-		projectInfo := pfs.ProjectInfo{Project: &pfs.Project{Name: fmt.Sprintf("project%d", i+1)}, Description: "test project"}
+	projectNames := []string{"testProject1", "testProject2", "testProject3", strings.Repeat("A", 51)}
+	for _, name := range projectNames {
+		projectInfo := pfs.ProjectInfo{Project: &pfs.Project{Name: name}, Description: "test project"}
 		b, err := proto.Marshal(&projectInfo)
 		require.NoError(t, err)
 		_, err = tx.ExecContext(ctx, `INSERT INTO collections.projects(key, proto) VALUES($1, $2)`, projectInfo.Project.String(), b)
@@ -35,7 +35,7 @@ func setupTestData(t *testing.T, ctx context.Context, db *sqlx.DB) {
 	require.NoError(t, tx.Commit())
 }
 
-func Test_v2_7_0_ClusterState_Projects(t *testing.T) {
+func Test_v2_7_0_ClusterState(t *testing.T) {
 	ctx := pctx.TestContext(t)
 	db, _ := dockertestenv.NewEphemeralPostgresDB(ctx, t)
 	defer db.Close()
@@ -49,10 +49,11 @@ func Test_v2_7_0_ClusterState_Projects(t *testing.T) {
 	expectedProjects, err := v2_7_0.ListProjectsFromCollection(ctx, db)
 	require.NoError(t, err)
 
-	// Migrates collections.projects to core.projects
+	// Apply 2.7.0 migration
 	require.NoError(t, migrations.ApplyMigrations(ctx, db, migrationEnv, state_2_7_0))
 	require.NoError(t, migrations.BlockUntil(ctx, db, state_2_7_0))
 
+	// Verify Projects
 	// Check whether all the data is migrated to core.projects table
 	var gotProjects []v2_7_0.Project
 	require.NoError(t, db.SelectContext(ctx, &gotProjects, `SELECT id, name, description, created_at, updated_at FROM core.projects ORDER BY id`))
@@ -60,8 +61,7 @@ func Test_v2_7_0_ClusterState_Projects(t *testing.T) {
 	if diff := cmp.Diff(expectedProjects, gotProjects); diff != "" {
 		t.Errorf("projects differ: (-want +got)\n%s", diff)
 	}
-
 	// Test project names can only be 51 characters long
-	_, err = db.ExecContext(ctx, `INSERT INTO core.projects(name, description) VALUES($1, $2)`, strings.Repeat("A", 52), "this should error")
+	_, err = db.ExecContext(ctx, `INSERT INTO core.projects(name, description) VALUES($1, $2)`, strings.Repeat("A", 52), "project name with 52 characters")
 	require.ErrorContains(t, err, "value too long for type character varying(51)")
 }
