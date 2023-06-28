156ed47e564da8ee4be5563ef8981fc320fc6c74
molinamelendezj <molinamelendezj@gmail.com>
2023-06-28 16:08:48 -0400

    make `make proto` mac friendly

--- etc/proto/build.sh
+++ etc/proto/build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 # This script build protos using the pachyderm_proto image.
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  TAR=gtar # brew install gnu-tar if on mac
+else
+  TAR=tar
+fi
+
 read -ra proto_files < <(find src -name "*.proto" -print0 | xargs -0)
-tar cf - "${proto_files[@]}" \
+$TAR cf - "${proto_files[@]}" \
  | docker run -i --rm pachyderm_proto \
- | tar xf -
+ | $TAR xf -
