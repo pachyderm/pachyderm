2826b929e00dd64e9ed17866dff208689981db75
Ben Bonenfant <ben.bonenfant@pachyderm.io>
2023-07-03 14:31:07 -0700

    fix teardown code of identity test

--- python-sdk/tests/test_identity.py
+++ python-sdk/tests/test_identity.py
@@ -17,34 +17,30 @@ class TestIdentity:
 
     @staticmethod
     def test_oidc_client(auth_client: TestClient):
+        client1 = identity.OidcClient(id="oidc1", name="pach1", secret="secret1")
+        client2 = identity.OidcClient(id="oidc2", name="pach2", secret="secret2")
         try:
-            oidc1 = auth_client.identity.create_oidc_client(
-                client=identity.OidcClient(id="oidc1", name="pach1", secret="secret1")
-            )
-            oidc2 = auth_client.identity.create_oidc_client(
-                client=identity.OidcClient(id="oidc2", name="pach2", secret="secret2")
-            )
+            oidc1 = auth_client.identity.create_oidc_client(client=client1)
+            oidc2 = auth_client.identity.create_oidc_client(client=client2)
 
             oidc_clients = auth_client.identity.list_oidc_clients().clients
             assert oidc1.client in oidc_clients
             assert oidc2.client in oidc_clients
 
-            assert auth_client.identity.get_oidc_client(id=oidc1.client.id).client.name == "pach1"
-            assert auth_client.identity.get_oidc_client(id=oidc2.client.id).client.name == "pach2"
+            assert auth_client.identity.get_oidc_client(id=client1.id).client.name == client1.name
+            assert auth_client.identity.get_oidc_client(id=client2.id).client.name == client2.name
 
-            auth_client.identity.update_oidc_client(
-                client=identity.OidcClient(id="oidc1", name="pach3", secret="secret1")
-            )
-            assert auth_client.identity.get_oidc_client(id=oidc1.client.id).client.name == "pach3"
+            client1.name = "pach3"
+            auth_client.identity.update_oidc_client(client=client1)
+            assert auth_client.identity.get_oidc_client(id=client1.id).client.name == client1.name
 
             auth_client.identity.delete_oidc_client(id=oidc1.client.id)
             oidc_clients = auth_client.identity.list_oidc_clients().clients
             assert oidc1 not in oidc_clients
 
-
         finally:
             oidc_clients = auth_client.identity.list_oidc_clients().clients
-            if oidc1 in oidc_clients:
-                auth_client.identity.delete_oidc_client(id=oidc1.client.id)
-            if oidc2 in oidc_clients:
-                auth_client.identity.delete_oidc_client(id=oidc2.client.id)
+            if client1 in oidc_clients:
+                auth_client.identity.delete_oidc_client(id=client1.id)
+            if client2 in oidc_clients:
+                auth_client.identity.delete_oidc_client(id=client2.id)
