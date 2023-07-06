5334ababe71ef4bb3a5c2b4ef2875ca9a3f5013e
Bryce McAnally <bryce@pachyderm.io>
2023-07-06 08:51:31 -0500

    Apply patch

--- src/server/pfs/server/master.go
+++ src/server/pfs/server/master.go
@@ -123,7 +123,8 @@ func (d *driver) manageRepos(ctx context.Context) error {
 					backoff.RetryUntilCancel(ctx, func() (retErr error) { //nolint:errcheck
 						ctx, cancel := context.WithCancel(ctx)
 						defer cancel()
-						lockCtx, err := ring.Lock(ctx, lockPrefix)
+						var err error
+						ctx, err = ring.Lock(ctx, lockPrefix)
 						if err != nil {
 							return errors.Wrap(err, "error locking repo lock")
 						}
@@ -135,7 +136,7 @@ func (d *driver) manageRepos(ctx context.Context) error {
 						var eg errgroup.Group
 						eg.Go(func() error {
 							return backoff.RetryUntilCancel(ctx, func() error {
-								return d.manageBranches(lockCtx, key)
+								return d.manageBranches(ctx, key)
 							}, backoff.NewInfiniteBackOff(), func(err error, d time.Duration) error {
 								log.Error(ctx, "error managing branches", zap.String("repo", key), zap.Error(err), zap.Duration("retryAfter", d))
 								return nil
@@ -143,7 +144,7 @@ func (d *driver) manageRepos(ctx context.Context) error {
 						})
 						eg.Go(func() error {
 							return backoff.RetryUntilCancel(ctx, func() error {
-								return d.finishRepoCommits(lockCtx, key)
+								return d.finishRepoCommits(ctx, key)
 							}, backoff.NewInfiniteBackOff(), func(err error, d time.Duration) error {
 								log.Error(ctx, "error finishing repo commits", zap.String("repo", key), zap.Error(err), zap.Duration("retryAfter", d))
 								return nil
