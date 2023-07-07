3060a3bd07de6c26a8a2ff711c85164c1a246687
Ben Bonenfant <ben.bonenfant@pachyderm.io>
2023-07-07 13:32:26 -0700

    [Python-SDK] Extension to DebugDump (#9049)
    Creates api/debug/extension.py that overrides the debug.dump method, routing it to the DumpV2 endpoint and providing a sensible default (matching pachctl).

--- python-sdk/tests/test_debug.py
+++ python-sdk/tests/test_debug.py
@@ -6,9 +6,9 @@ from pachyderm_sdk.api import debug
 
 
 def test_dump(client: TestClient):
-    message = next(client.debug.dump_v2())
+    message = next(client.debug.dump())
     assert isinstance(message.content.content, bytes)
-    assert len(message.content.content) > 0
+    assert message.progress.progress > 0
 
 
 def test_profile_cpu(client: TestClient):
