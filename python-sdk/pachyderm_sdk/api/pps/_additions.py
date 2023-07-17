a20d2b0ab59517cf6e5f57bf60a53eee9eeb5c1c
Kevin Yang <kevin.yang@hpe.com>
2023-07-17 13:12:55 -0400

    add docstring

--- python-sdk/pachyderm_sdk/api/pps/_additions.py
+++ python-sdk/pachyderm_sdk/api/pps/_additions.py
@@ -1,3 +1,10 @@
+""" This file patches methods onto the PPS Noun objects.
+This is done, as opposed to subclassing them, for these methods
+  to automatically be accessible on any objects included in the
+  response from the API.
+Note: These are internally patched and this file should
+  not be imported directly by users.
+"""
 from . import CreatePipelineRequest, Service, Spout
 
 def _Create_Pipeline_Request__post_init__(self: "CreatePipelineRequest") -> None:
