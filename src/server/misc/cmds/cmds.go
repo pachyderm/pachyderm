d10eba580d04483c315eb179adbef13ca15f8856
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2023-07-26 13:12:42 -0400

    Add a job to perform preflight checks (#9112)

--- src/server/misc/cmds/cmds.go
+++ src/server/misc/cmds/cmds.go
@@ -3,7 +3,6 @@ package cmds
 import (
 	"context"
 	"crypto/tls"
-	"database/sql"
 	"fmt"
 	"net"
 	"net/http"
@@ -11,24 +10,19 @@ import (
 	"net/url"
 	"os"
 	"os/signal"
-	"path/filepath"
 	"strings"
 
 	"github.com/jmoiron/sqlx"
-	"github.com/pachyderm/pachyderm/v2/src/client"
 	"github.com/pachyderm/pachyderm/v2/src/internal/archiveserver"
-	"github.com/pachyderm/pachyderm/v2/src/internal/clusterstate"
 	"github.com/pachyderm/pachyderm/v2/src/internal/cmdutil"
 	"github.com/pachyderm/pachyderm/v2/src/internal/dbutil"
 	"github.com/pachyderm/pachyderm/v2/src/internal/errors"
 	"github.com/pachyderm/pachyderm/v2/src/internal/log"
-	"github.com/pachyderm/pachyderm/v2/src/internal/migrations"
 	"github.com/pachyderm/pachyderm/v2/src/internal/pctx"
+	"github.com/pachyderm/pachyderm/v2/src/internal/preflight"
 	"github.com/pachyderm/pachyderm/v2/src/internal/promutil"
 	"github.com/pachyderm/pachyderm/v2/src/internal/signals"
 	"github.com/spf13/cobra"
-	clientv3 "go.etcd.io/etcd/client/v3"
-	"go.etcd.io/etcd/server/v3/embed"
 	"go.uber.org/zap"
 )
 
@@ -198,66 +192,10 @@ func Cmds(ctx context.Context) []*cobra.Command {
 			if err := dbutil.WaitUntilReady(ctx, db); err != nil {
 				return errors.Wrap(err, "wait for database ready")
 			}
-
-			// Create test dirs for etcd data
-			dir, err := os.MkdirTemp("", "test-migrations")
-			if err != nil {
-				return errors.Wrap(err, "create etcd server tmpdir")
+			if err := preflight.TestMigrations(ctx, db); err != nil {
+				return errors.Wrap(err, "apply migrations")
 			}
-			defer os.RemoveAll(dir)
-
-			etcdConfig := embed.NewConfig()
-			etcdConfig.MaxTxnOps = 10000
-			etcdConfig.Dir = filepath.Join(dir, "dir")
-			etcdConfig.WalDir = filepath.Join(dir, "wal")
-			etcdConfig.InitialElectionTickAdvance = false
-			etcdConfig.TickMs = 10
-			etcdConfig.ElectionMs = 50
-			etcdConfig.ListenPeerUrls = []url.URL{}
-			etcdConfig.ListenClientUrls = []url.URL{{
-				Scheme: "http",
-				Host:   "localhost:7777",
-			}}
-			log.AddLoggerToEtcdServer(ctx, etcdConfig)
-			etcd, err := embed.StartEtcd(etcdConfig)
-			if err != nil {
-				return errors.Wrap(err, "start etcd")
-			}
-			defer etcd.Close()
-
-			etcdCfg := log.GetEtcdClientConfig(ctx)
-			etcdCfg.Endpoints = []string{"http://localhost:7777"}
-			etcdCfg.DialOptions = client.DefaultDialOptions()
-			etcdClient, err := clientv3.New(etcdCfg)
-			if err != nil {
-				return errors.Wrap(err, "connect to etcd")
-			}
-			defer etcdClient.Close()
-
-			txx, err := db.BeginTxx(ctx, &sql.TxOptions{
-				Isolation: sql.LevelSerializable,
-			})
-			if err != nil {
-				return errors.Wrap(err, "start tx")
-			}
-			defer func() {
-				if err := txx.Rollback(); err != nil {
-					errors.JoinInto(&retErr, errors.Wrap(err, "rollback"))
-				}
-			}()
-			states := migrations.CollectStates(nil, clusterstate.DesiredClusterState)
-			env := migrations.MakeEnv(nil, etcdClient)
-			env.Tx = txx
-			env.WithTableLocks = false
-			var errs error
-			for _, s := range states {
-				if err := migrations.ApplyMigrationTx(ctx, env, s); err != nil {
-					log.Error(ctx, "migration did not apply; continuing", zap.Error(err))
-					errors.JoinInto(&retErr, errors.Wrapf(err, "migration %v", s.Number()))
-				}
-			}
-			log.Info(ctx, "done applying migrations")
-			return errs
+			return nil
 		}),
 	}
 	commands = append(commands, cmdutil.CreateAlias(testMigrations, "misc test-migrations"))
