03e8de2a2571cf04144dd27e4214da65c1be9a4e
Daniel Janicek <daniel.janicek@hpe.com>
2023-07-28 14:17:50 -0700

    Draft: log in to determine in tests

--- src/testing/deploy/deploy_test.go
+++ src/testing/deploy/deploy_test.go
@@ -4,6 +4,8 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"net/url"
@@ -19,7 +21,10 @@ import (
 	"github.com/pachyderm/pachyderm/v2/src/internal/minikubetestenv"
 	"github.com/pachyderm/pachyderm/v2/src/internal/require"
 	"github.com/pachyderm/pachyderm/v2/src/internal/testutil"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
+const detExternalLoginUrl = "http://%s:%d/api/v1/auth/login" // DNJ TODO don't hardcorde namespace/port? - need to rotate for parallel
 
 var valueOverrides map[string]string = make(map[string]string)
 
@@ -110,6 +115,29 @@ func TestEnterpriseServerMember(t *testing.T) {
 	mockIDPLogin(t, c)
 }
 
+func TestDeterminedInstallAndIntegration(t *testing.T) {
+	t.Parallel()
+	ns, portOffset := minikubetestenv.ClaimCluster(t)
+	k := testutil.GetKubeClient(t)
+	opts := &minikubetestenv.DeployOpts{
+		AuthUser:   auth.RootUser,
+		Enterprise: true,
+		PortOffset: portOffset,
+		Determined: true,
+	}
+	valueOverrides["pachd.replicas"] = "1"
+	opts.ValueOverrides = valueOverrides
+	minikubetestenv.PutNamespace(t, ns)
+	c := minikubetestenv.InstallRelease(t, context.Background(), ns, k, opts)
+	whoami, err := c.AuthAPIClient.WhoAmI(c.Ctx(), &auth.WhoAmIRequest{})
+	require.NoError(t, err)
+	require.Equal(t, auth.RootUser, whoami.Username)
+	c.SetAuthToken("")
+	mockIDPLogin(t, c)
+	time.Sleep(30 * time.Second) // DNJ TODO - wait for determined in deploy
+	determinedLogin(t, ns)
+}
+
 func mockIDPLogin(t testing.TB, c *client.APIClient) {
 	require.NoErrorWithinTRetryConstant(t, 60*time.Second, func() error {
 		// login using mock IDP admin
@@ -120,7 +148,6 @@ func mockIDPLogin(t testing.TB, c *client.APIClient) {
 			return errors.EnsureStack(err)
 		}
 		state := loginInfo.State
-
 		// Get the initial URL from the grpc, which should point to the dex login page
 		getResp, err := hc.Get(loginInfo.LoginUrl)
 		if err != nil {
@@ -168,6 +195,39 @@ func mockIDPLogin(t testing.TB, c *client.APIClient) {
 		}
 		return nil
 	}, 5*time.Second, "Attempting login through mock IDP")
+}
+func determinedLogin(t testing.TB, namespace string) string {
+	ctx := context.Background()
+	kube := testutil.GetKubeClient(t)
+	// DNJ TODO refactor finding URL
+	service, err := kube.CoreV1().Services(namespace).Get(ctx, fmt.Sprintf("determined-master-service-%s", namespace), v1.GetOptions{})
+	detPort := service.Spec.Ports[0].NodePort
+	require.NoError(t, err, "Fininding Determined service")
+	// DNJ TODO - find IP from nodes
+	node, err := kube.CoreV1().Nodes().Get(ctx, "minikube", v1.GetOptions{})
+	require.NoError(t, err, "Fininding node for Determined")
+	var detIp string
+	for _, addr := range node.Status.Addresses {
+		if addr.Type == "InternalIP" {
+			detIp = addr.Address
+		}
+	}
+	hc := &http.Client{Timeout: 15 * time.Second}
+	// DNJ TODO - add in retries
+	req, err := http.NewRequest("POST", fmt.Sprintf(detExternalLoginUrl, detIp, detPort), strings.NewReader(`{"username":"admin","password":""}`))
+	require.NoError(t, err, "Creating Determined login request")
+	resp, err := hc.Do(req)
+	require.NoError(t, err, "Logging into Determined")
+	require.Equal(t, 200, resp.StatusCode)
+	body, err := io.ReadAll(resp.Body)
+	require.NoError(t, err, "Reading Determined login")
+	authToken := struct {
+		Token string
+	}{}
+	err = json.Unmarshal(body, &authToken)
+	require.NoError(t, err, "Parsing Determined login")
+	t.Logf("DNJ TODO SERVICE: %v -- %v", authToken, string(body))
+	return authToken.Token
 }
 
 func createTrustedPeersFile(t testing.TB) string {
