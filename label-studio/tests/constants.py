a2d3f0f1c0567afacf56c2cbaafabe8bab43d311
Sahith Malyala <sahith.malyala@hpe.com>
2023-08-03 13:05:37 -0400

    Import storage tests

--- label-studio/tests/constants.py
+++ label-studio/tests/constants.py
@@ -1,5 +1,7 @@
 import os
 
-LABEL_STUDIO_URL = "http://localhost:8080"
-## TODO: remove default
-AUTH_TOKEN = os.environ.get("LABEL_STUDIO_USER_TOKEN", "abcdef")
+LABEL_STUDIO_URL = 'http://localhost:8080'
+# TODO: Get minikube ip
+PACHD_ADDRESS = 'host.docker.internal:30650'
+# TODO: remove default
+AUTH_TOKEN = os.environ.get('LABEL_STUDIO_USER_TOKEN', 'abcdef')
--- label-studio/tests/test_pachyderm_storage.py
+++ label-studio/tests/test_pachyderm_storage.py
@@ -5,79 +5,140 @@ from label_studio_sdk import Client as LSClient
 from pachyderm_sdk import Client as PachClient
 from pachyderm_sdk.api import pfs
 
-from tests.constants import LABEL_STUDIO_URL, AUTH_TOKEN
-from tests.utils import connect_pachyderm_import_storage
+from tests.constants import LABEL_STUDIO_URL, AUTH_TOKEN, PACHD_ADDRESS
 
 
 ## TODO: proper client config for CI
-@pytest.fixture(scope="session")
+@pytest.fixture(scope='session')
 def pach_client():
     return PachClient.from_config()
 
-
 @pytest.fixture
-def add_data_to_pachyderm(pach_client):
-    import_repo = pfs.Repo(name="images", type="user")
+def pachyderm_data(pach_client):
+    import_repo = pfs.Repo(name='images', type='user')
     pach_client.pfs.create_repo(repo=import_repo)
 
     with pach_client.pfs.commit(
-        branch=pfs.Branch(repo=import_repo, name="master")
+        branch=pfs.Branch(repo=import_repo, name='master')
     ) as commit:
         # TODO: switch out
         # commit.put_file_from_url(
-        #     path="/liberty.jpg",
-        #     url="https://docs.pachyderm.com/images/opencv/liberty.jpg",
+        #     path='/liberty.jpg',
+        #     url='https://docs.pachyderm.com/images/opencv/liberty.jpg',
         # )
 
-        f = open("/Users/malyala/Downloads/liberty.jpg", "rb")
-        commit.put_file_from_file(path="/liberty.jpg", file=f)
-
+        f = open('/Users/malyala/Downloads/liberty.jpg', 'rb')
+        commit.put_file_from_file(path='/liberty.jpg', file=f)
         commit_id = commit.id
-        print(commit_id)
 
-    export_repo = pfs.Repo(name="labels")
+    export_repo = pfs.Repo(name='labels')
     pach_client.pfs.create_repo(repo=export_repo)
 
     yield import_repo, commit_id, export_repo
 
     pach_client.pfs.delete_all()
 
-
-def test_import_from_pachyderm_storage(add_data_to_pachyderm):
-    import_repo, commit_id, export_repo = add_data_to_pachyderm
-
+@pytest.fixture
+def project():
     ls_client = LSClient(url=LABEL_STUDIO_URL, api_key=AUTH_TOKEN)
     ls_client.check_connection()
 
     project = ls_client.start_project(
-        title="Image Annotation",
-        label_config="""
+        title='Image Annotation',
+        label_config='''
     <View>
-        <Image name="image" value="$image"/>
-        <RectangleLabels name="label" toName="image">
-            <Label value="Statue" background="green"/>
+        <Image name='image' value='$image'/>
+        <RectangleLabels name='label' toName='image'>
+            <Label value='Statue' background='green'/>
         </RectangleLabels>
     </View>
-    """,
+    ''',
     )
+
+    return project
+
+@pytest.fixture
+def import_storage(pachyderm_data, project):
+    import_repo, commit_id, _ = pachyderm_data
 
     # TODO: pachd address to minikube ip
-    storage = connect_pachyderm_import_storage(
-        project,
-        import_repo.name,
-        commit_id,
-        pachd_address="host.docker.internal:30650",
-        title="Images",
-    )
+    # Create import storage
+    payload = {
+        'pach_repo': import_repo.name,
+        'pach_commit': commit_id,
+        'pachd_address': PACHD_ADDRESS,
+        'use_blob_urls': True,
+        'title': 'Pachyderm images',
+        'project': project.id,
+    }
+    storage = project.make_request('POST', '/api/storages/pachyderm', json=payload)
 
-    print(storage)
+    assert storage.ok
+    assert storage.json()['type'] == 'pachyderm'
+    storage_id = storage.json()["id"]
+
+    # Get all storages
+    response = project.make_request('GET', f'/api/storages/pachyderm?project={storage_id}')
+
+    assert response.ok
+    assert len(response.json()) == 1
+
+    yield storage.json()
+
+    # Delete storage
+    response = project.make_request('DELETE', f'/api/storages/pachyderm/{storage_id}')
+
+    assert response.ok
+
+    # Get all storages
+    response = project.make_request('GET', f'/api/storages/pachyderm?project={storage_id}')
+
+    assert response.ok
+    assert len(response.json()) == 0
+
+
+# Label Studio API: https://labelstud.io/api
+
+def test_pachyderm_import_storage(pachyderm_data, project, import_storage):
+    import_repo, commit_id, _ = pachyderm_data
+
+    # Validate storage
+    payload = {
+        'pach_repo': import_repo.name,
+        'pach_commit': commit_id,
+        'pachd_address': PACHD_ADDRESS,
+        'use_blob_urls': True,
+        'title': 'Pachyderm images',
+        'project': project.id,
+    }
+    response = project.make_request('POST', '/api/storages/pachyderm/validate', json=payload)
+
+    assert response.ok
+
+    # Update storage
+    payload = {
+        'pach_repo': import_repo.name,
+        'pach_commit': commit_id,
+        'pachd_address': PACHD_ADDRESS,
+        'use_blob_urls': True,
+        'title': 'Pachyderm images (updated)'
+    }
+    response = project.make_request('PATCH', f'/api/storages/pachyderm/{import_storage["id"]}?project={project.id}', json=payload)
+
+    assert response.ok
+    assert response.json()['title'] == 'Pachyderm images (updated)'
+
+    # Get storage
+    response = project.make_request('GET', f'/api/storages/pachyderm/{import_storage["id"]}')
+
+    assert response.ok
+    assert response.json()['title'] == 'Pachyderm images (updated)'
 
 
 def test_begin():
     x = requests.get(
-        f"{LABEL_STUDIO_URL}/api/current-user/whoami",
-        headers={"Authorization": f"Token {AUTH_TOKEN}"},
+        f'{LABEL_STUDIO_URL}/api/current-user/whoami',
+        headers={'Authorization': f'Token {AUTH_TOKEN}'},
     )
-    print(x.json())
 
     assert x.ok
