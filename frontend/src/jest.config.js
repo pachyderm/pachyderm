409e82ff166c342500c6974e8f4b7c67654d16d1
Logan <31484824+loganripplinger@users.noreply.github.com>
2023-08-03 07:57:51 -0700

    chore: Frontend tests end with *.test.ts* (#1467)
    * Update jest globs
    
    * Update *.test.mock.tsx to *.mock.test.tsx
    
    * Update *.test.unit.tsx to *.unit.test.tsx
    
    * Update *.test.unit.ts to *.unit.test.ts

--- frontend/src/jest.config.js
+++ frontend/src/jest.config.js
@@ -22,24 +22,24 @@ module.exports = {
   testTimeout: baseConfig.testTimeout, // testTimeout doesn't get picked up inside of projects https://github.com/jestjs/jest/issues/9696
   projects: [
     {
-      displayName: 'mock-server',
       ...baseConfig,
-      testMatch: ['**/__tests__/**/*.test.mock.tsx'],
+      displayName: 'mock-server',
+      testMatch: ['**/__tests__/**/*.mock.test.ts?(x)'],
       setupFilesAfterEnv: ['./setupTests.mock.ts'],
     },
     {
-      displayName: 'msw',
       ...baseConfig,
-      testMatch: ['**/__tests__/**/*.test.tsx'],
+      displayName: 'msw',
+      testMatch: [
+        '**/__tests__/**/*.test.tsx',
+        '!**/__tests__/**/*.mock.test.ts?(x)',
+        '!**/__tests__/**/*.unit.test.ts?(x)',
+      ],
     },
     {
-      displayName: 'unit',
       ...baseConfig,
-      testMatch: [
-        '**/__tests__/**/*.test.ts',
-        '**/__tests__/**/*.unit.ts',
-        '**/__tests__/**/*.unit.tsx',
-      ],
+      displayName: 'unit',
+      testMatch: ['**/__tests__/**/*.unit.test.ts?(x)'],
     },
   ],
 };
