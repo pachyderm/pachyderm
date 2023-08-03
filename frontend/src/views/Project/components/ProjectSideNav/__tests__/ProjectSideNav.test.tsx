08e121da4fc7aae3c3601af8cb98f435f83751a0
Logan <31484824+loganripplinger@users.noreply.github.com>
2023-08-03 14:56:50 -0700

    Testing: Migrate project side nav to MSW (#1469)
    * Rename ProjectSideNav
    
    * testing: Pull out unit tests from ProjectSideNav
    
    * Remove unused test.
    
    * Migrate test to MSW.
    
    * Remove unused test

---
+++ frontend/src/views/Project/components/ProjectSideNav/__tests__/ProjectSideNav.test.tsx
@@ -0,0 +1,48 @@
+import {render, screen} from '@testing-library/react';
+import {setupServer} from 'msw/lib/node';
+import React from 'react';
+
+import {
+  mockEmptyGetAuthorize,
+  mockGetVersionInfo,
+  mockRepos,
+} from '@dash-frontend/mocks';
+import {withContextProviders, click, type} from '@dash-frontend/testHelpers';
+
+import ProjectSideNavComponent from '../../ProjectSideNav';
+
+describe('project sidenav', () => {
+  const server = setupServer();
+
+  const ProjectSideNav = withContextProviders(ProjectSideNavComponent);
+
+  beforeAll(() => {
+    server.listen();
+    server.use(mockEmptyGetAuthorize());
+    server.use(mockGetVersionInfo());
+    server.use(mockRepos());
+  });
+
+  afterAll(() => server.close());
+
+  describe('create repo modal', () => {
+    it('should error if the repo already exists', async () => {
+      window.history.replaceState('', '', '/project/default');
+
+      render(<ProjectSideNav />);
+
+      const createButton = screen.getByText('Create Repo');
+      await click(createButton);
+
+      const nameInput = await screen.findByLabelText('Name', {
+        exact: false,
+      });
+
+      await type(nameInput, 'montage');
+
+      expect(
+        await screen.findByText('Repo name already in use'),
+      ).toBeInTheDocument();
+    });
+  });
+});
--- frontend/src/views/Project/components/ProjectSideNav/__tests__/ProjectSideNav.unit.test.tsx
+++ frontend/src/views/Project/components/ProjectSideNav/__tests__/ProjectSideNav.unit.test.tsx
@@ -1,80 +1,14 @@
-import {mockServer} from '@dash-backend/testHelpers';
-import {render, waitFor, screen, within} from '@testing-library/react';
+import {render, screen, within} from '@testing-library/react';
 import React from 'react';
 
 import {withContextProviders, click, type} from '@dash-frontend/testHelpers';
 
-import ProjectSideNavComponent from '../../ProjectSideNav';
+import ProjectSideNavComponent from '..';
 
 describe('project sidenav', () => {
   const ProjectSideNav = withContextProviders(ProjectSideNavComponent);
 
-  it('should not display notification badge for projects with no unhealthy jobs', async () => {
-    window.history.replaceState(
-      '',
-      '',
-      '/project/Solar-Power-Data-Logger-Team-Collab',
-    );
-
-    render(<ProjectSideNav />);
-
-    await waitFor(() =>
-      expect(
-        screen.queryByTestId('ProjectHeader__projectNameLoader'),
-      ).not.toBeInTheDocument(),
-    );
-    expect(screen.queryByLabelText('Number of failed')).not.toBeInTheDocument();
-  });
-
   describe('create repo modal', () => {
-    it('should allow users to create new repos', async () => {
-      window.history.replaceState('', '', '/project/Empty-Project');
-
-      render(<ProjectSideNav />);
-
-      const createButton = screen.getByText('Create Repo');
-      await click(createButton);
-
-      const nameInput = await screen.findByLabelText('Name', {exact: false});
-      const descriptionInput = await screen.findByLabelText(
-        'Description (optional)',
-        {
-          exact: false,
-        },
-      );
-      const submitButton = screen.getByText('Create');
-
-      await type(nameInput, 'newRepo');
-      await type(descriptionInput, 'newRepo Description');
-
-      expect(mockServer.getState().repos['Empty-Project']).toHaveLength(0);
-
-      await click(submitButton);
-
-      await waitFor(() =>
-        expect(mockServer.getState().repos['Empty-Project']).toHaveLength(1),
-      );
-    });
-
-    it('should error if the repo already exists', async () => {
-      window.history.replaceState('', '', '/project/Data-Cleaning-Process');
-
-      render(<ProjectSideNav />);
-
-      const createButton = screen.getByText('Create Repo');
-      await click(createButton);
-
-      const nameInput = await screen.findByLabelText('Name', {
-        exact: false,
-      });
-
-      await type(nameInput, 'training');
-
-      expect(
-        await screen.findByText('Repo name already in use'),
-      ).toBeInTheDocument();
-    });
-
     const validInputs = [
       ['good'],
       ['good-'],
