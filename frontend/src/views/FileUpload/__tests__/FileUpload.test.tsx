b4453e6be728c3625e3fb8f999bb46a8df10b1a6
Jeff Rifwald <jmcriffey@gmail.com>
2023-08-07 18:17:06 -0400

    feat(file): add more upload file tests

--- frontend/src/views/FileUpload/__tests__/FileUpload.test.tsx
+++ frontend/src/views/FileUpload/__tests__/FileUpload.test.tsx
@@ -1,27 +1,36 @@
-import {render, waitFor, screen} from '@testing-library/react';
+import {
+  render,
+  waitFor,
+  waitForElementToBeRemoved,
+  screen,
+} from '@testing-library/react';
+import {rest} from 'msw';
+import {setupServer} from 'msw/node';
 import React from 'react';
 
 import {
   type,
   withContextProviders,
   clear,
+  click,
   upload,
 } from '@dash-frontend/testHelpers';
 
 import FileUploadComponent from '..';
 import {GLOB_CHARACTERS} from '../lib/constants';
 
-describe('File Upload', () => {
-  beforeEach(() => {
-    window.history.replaceState(
-      {},
-      '',
-      '/lineage/Solar-Power-Data-Logger-Team-Collab/repos/cron/upload',
-    );
-  });
-
+describe('views/FileUpload', () => {
+  const server = setupServer();
   const FileUpload = withContextProviders(() => {
     return <FileUploadComponent />;
+  });
+
+  beforeAll(() => {
+    server.listen();
+  });
+
+  afterAll(() => {
+    server.close();
   });
 
   it('should require a path', async () => {
@@ -161,5 +170,54 @@ describe('File Upload', () => {
         `Below file names cannot contain ${GLOB_CHARACTERS}. Please rename or re-upload.`,
       ),
     ).toBeInTheDocument();
+  });
+
+  it('should allow a user to submit a file', async () => {
+    server.use(
+      rest.post('/upload/start', (req, res, ctx) => {
+        return res(ctx.json({uploadId: 1}));
+      }),
+    );
+    server.use(
+      rest.post('/upload', (req, res, ctx) => {
+        return res(ctx.json({fileName: 'hello.txt'}));
+      }),
+    );
+    server.use(
+      rest.post('/upload/finish', (req, res, ctx) => {
+        return res(ctx.json({commitId: '8ee8210b85174c9d8033c181228d0857'}));
+      }),
+    );
+
+    render(<FileUpload />);
+
+    const browseButton = screen.getByText('Browse Files');
+    const submitButton = screen.getByLabelText('Upload Selected Files');
+    const fileInput = screen.getByLabelText('Attach Files');
+    const file = new File(['hello'], 'hello.txt', {type: 'text/plain'});
+
+    await click(browseButton);
+    await waitFor(() => expect(fileInput).toBeEnabled());
+    await upload(fileInput, [file]);
+    await screen.findByText('hello.txt');
+
+    await waitFor(() =>
+      expect(screen.getByLabelText('Upload Selected Files')).toBeEnabled(),
+    );
+    await click(submitButton);
+
+    await screen.findByText(
+      'All files have been successfully uploaded, click Done to commit the files.',
+    );
+
+    const commitButton = await screen.findByLabelText('Commit Selected Files');
+
+    await click(commitButton);
+    await waitFor(() => {
+      // The upload modal should automatically close
+      expect(
+        screen.queryByLabelText('Commit Selected Files'),
+      ).not.toBeInTheDocument();
+    });
   });
 });
