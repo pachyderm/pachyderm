bc3fa17c4747f50ba2aa5e3524eb9350eaaae190
Sahith Malyala <sahith.malyala@hpe.com>
2023-08-08 08:55:35 -0400

    Remove todos

--- label-studio/tests/constants.py
+++ label-studio/tests/constants.py
@@ -1,7 +1,5 @@
 import os
 
 LABEL_STUDIO_URL = 'http://localhost:8080'
-# TODO: Get minikube ip
 PACHD_ADDRESS = os.environ.get('PACHD_ADDRESS')
-# TODO: remove default
 AUTH_TOKEN = os.environ.get('LABEL_STUDIO_USER_TOKEN')
--- label-studio/tests/test_pachyderm_storage.py
+++ label-studio/tests/test_pachyderm_storage.py
@@ -8,7 +8,6 @@ from pachyderm_sdk.api import pfs
 from tests.constants import LABEL_STUDIO_URL, AUTH_TOKEN, PACHD_ADDRESS
 
 
-# TODO: proper client config for CI
 @pytest.fixture(scope="session")
 def pach_client():
     return PachClient.from_pachd_address(PACHD_ADDRESS)
@@ -22,14 +21,10 @@ def pachyderm_data(pach_client):
     with pach_client.pfs.commit(
         branch=pfs.Branch(repo=import_repo, name="master")
     ) as commit:
-        # TODO: switch out
         commit.put_file_from_url(
             path='/liberty.jpg',
             url='https://docs.pachyderm.com/images/opencv/liberty.jpg',
         )
-
-        # f = open("/Users/malyala/Downloads/liberty.jpg", "rb")
-        # commit.put_file_from_file(path="/liberty.jpg", file=f)
         commit_id = commit.id
 
     export_repo = pfs.Repo(name="labels", type="user")
