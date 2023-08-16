7c81ec0a6a8263bf62996f47197ef31034b240b7
Peter <60992304+pleeko@users.noreply.github.com>
2023-08-16 11:40:50 -0400

    [FRON-990]: Porting download tests to msw (#1502)
    * porting download tests to msw

--- frontend/src/mocks/files.ts
+++ frontend/src/mocks/files.ts
@@ -5,6 +5,7 @@ import {
   mockGetFilesQuery,
   FileType,
   mockDeleteFilesMutation,
+  mockFileDownloadQuery,
 } from '@graphqlTypes';
 import merge from 'lodash/merge';
 
@@ -42,6 +43,8 @@ export const MOCK_IMAGES_FILES: File[] = [
     path: '/AT-AT.png',
     sizeBytes: 80590,
     sizeDisplay: '80.59 kB',
+    download:
+      'http://localhost/download/default/images/master/4a83c74809664f899261baccdb47cd90/AT-AT.png',
   }),
   buildFile({
     commitId: '4a83c74809664f899261baccdb47cd90',
@@ -50,11 +53,20 @@ export const MOCK_IMAGES_FILES: File[] = [
     commitAction: FileCommitState.ADDED,
     sizeBytes: 58650,
     sizeDisplay: '58.65 kB',
+    download:
+      'http://localhost/download/default/images/master/4a83c74809664f899261baccdb47cd90/liberty.png',
   }),
   buildFile({
     commitId: '4a83c74809664f899261baccdb47cd90',
     repoName: 'images',
     path: '/cats/',
+  }),
+  buildFile({
+    commitId: '4a83c74809664f899261baccdb47cd90',
+    repoName: 'images',
+    path: '/json_nested_arrays.json',
+    sizeBytes: 2e8,
+    sizeDisplay: '200 MB',
   }),
 ];
 
@@ -129,4 +141,22 @@ export const generatePagingFiles = ({
 export const mockDeleteFiles = () =>
   mockDeleteFilesMutation((_req, res, ctx) => {
     return res(ctx.data({deleteFiles: 'deleted'}));
+  });
+
+export const mockFileDownload = (files: string[]) =>
+  mockFileDownloadQuery((req, res, ctx) => {
+    const {projectId, repoId, commitId, paths} = req.variables.args;
+    if (
+      projectId === 'default' &&
+      repoId === 'images' &&
+      commitId === '4a83c74809664f899261baccdb47cd90' &&
+      JSON.stringify(paths) === JSON.stringify(files)
+    ) {
+      return res(
+        ctx.data({
+          fileDownload: '/archive/gCACFAwASxxgccGkVzh2qIdHkwutaaoJDgD.zip',
+        }),
+      );
+    }
+    return res(ctx.errors(['error generating download link']));
   });
--- frontend/src/views/FileBrowser/components/ListViewTable/__tests__/ListViewTable.test.tsx
+++ frontend/src/views/FileBrowser/components/ListViewTable/__tests__/ListViewTable.test.tsx
@@ -2,7 +2,7 @@ import {
   mockDeleteFilesMutation,
   mockRepoWithLinkedPipelineQuery,
 } from '@graphqlTypes';
-import {render, screen, waitFor, within} from '@testing-library/react';
+import {render, screen, within} from '@testing-library/react';
 import {setupServer} from 'msw/lib/node';
 import React from 'react';
 
@@ -11,7 +11,7 @@ import {
   mockGetVersionInfo,
   mockRepoImagesWithLinkedPipeline,
 } from '@dash-frontend/mocks';
-import {MOCK_IMAGES_FILES} from '@dash-frontend/mocks/files';
+import {MOCK_IMAGES_FILES, mockFileDownload} from '@dash-frontend/mocks/files';
 import {click, withContextProviders} from '@dash-frontend/testHelpers';
 
 import {default as ListViewTableComponent} from '../ListViewTable';
@@ -19,6 +19,8 @@ import {default as ListViewTableComponent} from '../ListViewTable';
 const ListViewTable = withContextProviders(({files}) => {
   return <ListViewTableComponent files={files} />;
 });
+
+window.open = jest.fn();
 
 describe('List View Table', () => {
   const server = setupServer();
@@ -83,142 +85,318 @@ describe('List View Table', () => {
     );
   });
 
-  it('should delete file on action click', async () => {
-    server.use(
-      mockDeleteFilesMutation((req, res, ctx) => {
-        const {projectId, repo, branch, filePaths} = req.variables.args;
-        if (
-          projectId === 'default' &&
-          repo === 'images' &&
-          branch === 'master' &&
-          JSON.stringify(filePaths) === JSON.stringify(['/AT-AT.png'])
-        ) {
-          return res(
-            ctx.data({
-              deleteFiles: '720d471659dc4682a53576fdb637a482',
-            }),
-          );
-        }
-        return res(ctx.errors(['file does not exist']));
-      }),
-    );
+  describe('Delete', () => {
+    it('should delete file on action click', async () => {
+      server.use(
+        mockDeleteFilesMutation((req, res, ctx) => {
+          const {projectId, repo, branch, filePaths} = req.variables.args;
+          if (
+            projectId === 'default' &&
+            repo === 'images' &&
+            branch === 'master' &&
+            JSON.stringify(filePaths) === JSON.stringify(['/AT-AT.png'])
+          ) {
+            return res(
+              ctx.data({
+                deleteFiles: '720d471659dc4682a53576fdb637a482',
+              }),
+            );
+          }
+          return res(ctx.errors(['file does not exist']));
+        }),
+      );
 
-    render(<ListViewTable files={MOCK_IMAGES_FILES} />);
+      render(<ListViewTable files={MOCK_IMAGES_FILES} />);
 
-    await click((await screen.findAllByTestId('DropdownButton__button'))[0]);
-    await click((await screen.findAllByText('Delete'))[0]);
+      await click((await screen.findAllByTestId('DropdownButton__button'))[0]);
+      await click((await screen.findAllByText('Delete'))[0]);
 
-    const deleteConfirm = await screen.findByTestId('ModalFooter__confirm');
-    await click(deleteConfirm);
+      const deleteConfirm = await screen.findByTestId('ModalFooter__confirm');
+      await click(deleteConfirm);
 
-    expect(window.location.pathname).toBe(
-      '/project/default/repos/images/branch/master/commit/720d471659dc4682a53576fdb637a482/',
-    );
-  });
-
-  it('should delete multiple files on action click', async () => {
-    server.use(
-      mockDeleteFilesMutation((req, res, ctx) => {
-        const {projectId, repo, branch, filePaths} = req.variables.args;
-        if (
-          projectId === 'default' &&
-          repo === 'images' &&
-          branch === 'master' &&
-          JSON.stringify(filePaths) ===
-            JSON.stringify(['/AT-AT.png', '/cats/', '/liberty.png'])
-        ) {
-          return res(
-            ctx.data({
-              deleteFiles: '720d471659dc4682a53576fdb637a482',
-            }),
-          );
-        }
-        return res(ctx.errors(['file does not exist']));
-      }),
-    );
-    render(<ListViewTable files={MOCK_IMAGES_FILES} />);
-
-    const deleteButton = screen.getByRole('button', {
-      name: /delete selected items/i,
+      expect(window.location.pathname).toBe(
+        '/project/default/repos/images/branch/master/commit/720d471659dc4682a53576fdb637a482/',
+      );
     });
-    expect(deleteButton).toBeDisabled();
 
-    await click(
-      screen.getByRole('cell', {
-        name: /at-at\.png/i,
-      }),
-    );
-    await click(
-      screen.getByRole('cell', {
-        name: /cats/i,
-      }),
-    );
-    await click(
-      screen.getByRole('cell', {
-        name: /liberty\.png/i,
-      }),
-    );
+    it('should delete multiple files on action click', async () => {
+      server.use(
+        mockDeleteFilesMutation((req, res, ctx) => {
+          const {projectId, repo, branch, filePaths} = req.variables.args;
+          if (
+            projectId === 'default' &&
+            repo === 'images' &&
+            branch === 'master' &&
+            JSON.stringify(filePaths) ===
+              JSON.stringify(['/AT-AT.png', '/cats/', '/liberty.png'])
+          ) {
+            return res(
+              ctx.data({
+                deleteFiles: '720d471659dc4682a53576fdb637a482',
+              }),
+            );
+          }
+          return res(ctx.errors(['file does not exist']));
+        }),
+      );
+      render(<ListViewTable files={MOCK_IMAGES_FILES} />);
 
-    await waitFor(() => {
+      const deleteButton = screen.getByRole('button', {
+        name: /delete selected items/i,
+      });
+      expect(deleteButton).toBeDisabled();
+
+      await click(
+        screen.getByRole('cell', {
+          name: /at-at\.png/i,
+        }),
+      );
+      await click(
+        screen.getByRole('cell', {
+          name: /cats/i,
+        }),
+      );
+      await click(
+        screen.getByRole('cell', {
+          name: /liberty\.png/i,
+        }),
+      );
+
       expect(deleteButton).toBeEnabled();
+
+      await click(deleteButton);
+
+      const modal = await screen.findByRole('dialog');
+      expect(await within(modal).findByRole('list')).toHaveTextContent(
+        '/AT-AT.png/cats//liberty.png',
+      );
+      const deleteConfirm = await within(modal).findByRole('button', {
+        name: /delete/i,
+      });
+      await click(deleteConfirm);
+
+      expect(window.location.pathname).toBe(
+        '/project/default/repos/images/branch/master/commit/720d471659dc4682a53576fdb637a482/',
+      );
     });
 
-    await click(deleteButton);
+    it('should not allow file deletion for outputRepos', async () => {
+      server.use(
+        mockRepoWithLinkedPipelineQuery((req, res, ctx) => {
+          const {projectId, id} = req.variables.args;
+          if (projectId === 'default' && id === 'images') {
+            return res(
+              ctx.data({
+                repo: {
+                  branches: [
+                    {
+                      name: 'master',
+                      __typename: 'Branch',
+                    },
+                  ],
+                  createdAt: 1690221504,
+                  description: '',
+                  id: 'images',
+                  name: 'images',
+                  sizeDisplay: '0 B',
+                  sizeBytes: 14783,
+                  access: true,
+                  projectId: 'default',
+                  linkedPipeline: {
+                    id: 'default_pipeline',
+                    name: 'pipeline',
+                    __typename: 'Pipeline',
+                  },
+                  authInfo: null,
+                  __typename: 'Repo',
+                },
+              }),
+            );
+          }
+          return res();
+        }),
+      );
 
-    const modal = await screen.findByRole('dialog');
-    expect(await within(modal).findByRole('list')).toHaveTextContent(
-      '/AT-AT.png/cats//liberty.png',
-    );
-    const deleteConfirm = await within(modal).findByRole('button', {
-      name: /delete/i,
+      render(<ListViewTable files={MOCK_IMAGES_FILES} />);
+
+      const deleteButton = screen.getByRole('button', {
+        name: /delete selected items/i,
+      });
+      expect(deleteButton).toBeDisabled();
+
+      await click(
+        screen.getByRole('cell', {
+          name: /at-at\.png/i,
+        }),
+      );
+
+      expect(deleteButton).toBeDisabled();
+
+      await click((await screen.findAllByTestId('DropdownButton__button'))[0]);
+      expect(screen.queryByText('Delete')).not.toBeInTheDocument();
     });
-    await click(deleteConfirm);
-
-    expect(window.location.pathname).toBe(
-      '/project/default/repos/images/branch/master/commit/720d471659dc4682a53576fdb637a482/',
-    );
   });
 
-  it('should not allow file deletion for outputRepos', async () => {
-    server.use(
-      mockRepoWithLinkedPipelineQuery((req, res, ctx) => {
-        const {projectId, id} = req.variables.args;
-        if (projectId === 'default' && id === 'images') {
-          return res(
-            ctx.data({
-              repo: {
-                branches: [
-                  {
-                    name: 'master',
-                    __typename: 'Branch',
-                  },
-                ],
-                createdAt: 1690221504,
-                description: '',
-                id: 'images',
-                name: 'images',
-                sizeDisplay: '0 B',
-                sizeBytes: 14783,
-                access: true,
-                projectId: 'default',
-                linkedPipeline: {
-                  id: 'default_pipeline',
-                  name: 'pipeline',
-                  __typename: 'Pipeline',
-                },
-                authInfo: null,
-                __typename: 'Repo',
-              },
-            }),
-          );
-        }
-        return res();
-      }),
-    );
+  describe('Download', () => {
+    beforeAll(() => {
+      process.env.REACT_APP_RUNTIME_PACHYDERM_PUBLIC_HOST = 'localhost';
+      process.env.REACT_APP_RUNTIME_PACHYDERM_PUBLIC_TLS = 'false';
+    });
 
-    render(<ListViewTable files={MOCK_IMAGES_FILES} />);
+    it('should download a small file directly from pachd', async () => {
+      const spy = jest.spyOn(window, 'open');
 
-    await click((await screen.findAllByTestId('DropdownButton__button'))[0]);
-    expect(screen.queryByText('Delete')).not.toBeInTheDocument();
+      render(<ListViewTable files={MOCK_IMAGES_FILES} />);
+
+      const hamburger = screen.getByRole('button', {
+        name: /at-at\.png - 80\.59 kb/i,
+      });
+      await click(hamburger);
+      const downloadButton = within(hamburger).getByText(/Download/i);
+      await click(downloadButton);
+
+      expect(spy).toHaveBeenLastCalledWith(
+        'http://localhost/download/default/images/master/4a83c74809664f899261baccdb47cd90/AT-AT.png',
+      );
+    });
+
+    describe('Port Forward', () => {
+      beforeAll(() => {
+        process.env.REACT_APP_RUNTIME_PACHYDERM_PUBLIC_HOST = '';
+      });
+
+      it('should disable multi file download', async () => {
+        render(<ListViewTable files={MOCK_IMAGES_FILES} />);
+
+        const downloadButton = screen.getByRole('button', {
+          name: /download selected items/i,
+        });
+
+        expect(downloadButton).toBeDisabled();
+
+        await click(
+          screen.getByRole('cell', {
+            name: /at-at\.png/i,
+          }),
+        );
+        await click(
+          screen.getByRole('cell', {
+            name: /cats/i,
+          }),
+        );
+        await click(
+          screen.getByRole('cell', {
+            name: /liberty\.png/i,
+          }),
+        );
+        expect(downloadButton).toBeDisabled();
+      });
+
+      it('should disable single file download for large files', async () => {
+        process.env.REACT_APP_RUNTIME_PACHYDERM_PUBLIC_HOST = '';
+
+        render(<ListViewTable files={MOCK_IMAGES_FILES} />);
+
+        const hamburger = screen.getByRole('button', {
+          name: /json_nested_arrays\.json - 200 mb/i,
+        });
+        await click(hamburger);
+
+        const downloadButton = within(hamburger).getByText(
+          /download \(file too large to download\)/i,
+        );
+        expect(downloadButton.closest('button')).toBeDisabled();
+      });
+    });
+
+    describe('Proxy', () => {
+      beforeEach(() => {
+        process.env.REACT_APP_RUNTIME_PACHYDERM_PUBLIC_HOST = 'localhost';
+        process.env.REACT_APP_RUNTIME_PACHYDERM_PUBLIC_TLS = '';
+      });
+
+      it('should download multiple files on action click', async () => {
+        const spy = jest.spyOn(window, 'open');
+
+        server.use(
+          mockFileDownload([
+            '/AT-AT.png',
+            '/cats/',
+            '/json_nested_arrays.json',
+          ]),
+        );
+
+        render(<ListViewTable files={MOCK_IMAGES_FILES} />);
+
+        const downloadButton = screen.getByRole('button', {
+          name: /download selected items/i,
+        });
+
+        expect(downloadButton).toBeDisabled();
+
+        await click(
+          screen.getByRole('cell', {
+            name: /at-at\.png/i,
+          }),
+        );
+        await click(
+          screen.getByRole('cell', {
+            name: /cats/i,
+          }),
+        );
+        await click(
+          screen.getByRole('cell', {
+            name: /json_nested_arrays\.json/i,
+          }),
+        );
+        expect(downloadButton).toBeEnabled();
+        await click(downloadButton);
+        expect(spy).toHaveBeenLastCalledWith(
+          'http://localhost/archive/gCACFAwASxxgccGkVzh2qIdHkwutaaoJDgD.zip',
+        );
+      });
+
+      it('should allow download for large files using zip', async () => {
+        const spy = jest.spyOn(window, 'open');
+
+        server.use(mockFileDownload(['/json_nested_arrays.json']));
+
+        render(<ListViewTable files={MOCK_IMAGES_FILES} />);
+
+        const hamburger = screen.getByRole('button', {
+          name: /json_nested_arrays\.json - 200 mb/i,
+        });
+        await click(hamburger);
+
+        const downloadButton = within(hamburger).getByText(/Download Zip/i);
+
+        await click(downloadButton);
+        expect(spy).toHaveBeenLastCalledWith(
+          'http://localhost/archive/gCACFAwASxxgccGkVzh2qIdHkwutaaoJDgD.zip',
+        );
+      });
+
+      it('should generate a download link with TLS enabled', async () => {
+        process.env.REACT_APP_RUNTIME_PACHYDERM_PUBLIC_TLS = 'true';
+
+        const spy = jest.spyOn(window, 'open');
+
+        server.use(mockFileDownload(['/json_nested_arrays.json']));
+
+        render(<ListViewTable files={MOCK_IMAGES_FILES} />);
+
+        const hamburger = screen.getByRole('button', {
+          name: /json_nested_arrays\.json - 200 mb/i,
+        });
+        await click(hamburger);
+
+        const downloadButton = within(hamburger).getByText(/Download Zip/i);
+
+        await click(downloadButton);
+        expect(spy).toHaveBeenLastCalledWith(
+          'https://localhost/archive/gCACFAwASxxgccGkVzh2qIdHkwutaaoJDgD.zip',
+        );
+      });
+    });
   });
 });
