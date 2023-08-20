e27cc22c144ec55b63ed614cc653b152881711c2
Iris Wang <125112276+lemontree-ca@users.noreply.github.com>
2023-08-20 00:01:51 -0700

    updated return value for DeleteRepo

--- src/client/transaction.go
+++ src/client/transaction.go
@@ -298,7 +298,9 @@ func (c *pfsBuilderClient) CreateRepo(ctx context.Context, req *pfs.CreateRepoRe
 }
 func (c *pfsBuilderClient) DeleteRepo(ctx context.Context, req *pfs.DeleteRepoRequest, opts ...grpc.CallOption) (*pfs.DeleteRepoResponse, error) {
 	c.tb.requests = append(c.tb.requests, &transaction.TransactionRequest{DeleteRepo: req})
-	return true, nil
+	response := &pfs.DeleteRepoResponse{}
+	response.Deleted = true
+	return response, nil
 }
 func (c *pfsBuilderClient) StartCommit(ctx context.Context, req *pfs.StartCommitRequest, opts ...grpc.CallOption) (*pfs.Commit, error) {
 	// Note that since we are batching requests (no extra round-trips), we do not
