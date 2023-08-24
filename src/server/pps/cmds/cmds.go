dbf95ae2cb3955a75142ece8ad45b061cf805e23
Robert A. Uhl <robert.uhl@hpe.com>
2023-08-24 15:26:06 -0400

    Use SpecReader in pachctl edit pipeline

--- src/server/pps/cmds/cmds.go
+++ src/server/pps/cmds/cmds.go
@@ -1008,12 +1008,13 @@ func Cmds(mainCtx context.Context, pachCtx *config.Context, pachctlCfg *pachctl.
 				return err
 			}
 			defer r.Close()
-			pipelineReader, err := ppsutil.NewPipelineManifestReader(r)
+			specReader := ppsutil.NewSpecReader(r)
+			spec, err := specReader.Next()
 			if err != nil {
 				return err
 			}
-			request, err := pipelineReader.NextCreatePipelineRequest()
-			if err != nil {
+			var request = new(pps.CreatePipelineRequest)
+			if err := protojson.Unmarshal([]byte(spec), request); err != nil {
 				return err
 			}
 			if proto.Equal(createPipelineRequest, request) {
