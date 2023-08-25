909ab57866c85f4feb1705c52974c3e234812727
Robert Uhl <robert.uhl@hpe.com>
2023-08-25 11:45:57 -0400

    [CORE-1927] Support dry run in pachctl [create|delete|update] defaults (#9243)

--- src/server/pps/cmds/cmds.go
+++ src/server/pps/cmds/cmds.go
@@ -1596,7 +1596,7 @@ func Cmds(mainCtx context.Context, pachCtx *config.Context, pachctlCfg *pachctl.
 				if err != nil {
 					return errors.Wrapf(err, "could not open path %q for reading", pathname)
 				}
-				return setClusterDefaults(mainCtx, pachctlCfg, rc, regenerate, reprocess)
+				return setClusterDefaults(mainCtx, pachctlCfg, rc, regenerate, reprocess, dryRun)
 			}
 			return errors.New("--cluster must be specified")
 		}),
@@ -1606,6 +1606,7 @@ func Cmds(mainCtx context.Context, pachCtx *config.Context, pachctlCfg *pachctl.
 	createDefaults.Flags().BoolVar(&regenerate, "regenerate", false, "Regenerate pipeline specs from new defaults.")
 	createDefaults.Flags().BoolVar(&reprocess, "reprocess", false, "Reprocess regenerated pipelines.  Implies --regenerate")
 	createDefaults.Flags().StringVarP(&pathname, "file", "f", "-", "A JSON file containing cluster defaults.  \"-\" reads from stdin (the default behavior.)")
+	createDefaults.Flags().BoolVar(&dryRun, "dry-run", false, "Do not actually delete defaults.")
 	commands = append(commands, cmdutil.CreateAliases(createDefaults, "create defaults"))
 
 	deleteDefaults := &cobra.Command{
@@ -1614,7 +1615,7 @@ func Cmds(mainCtx context.Context, pachCtx *config.Context, pachctlCfg *pachctl.
 		Long:  "Delete defaults.",
 		Run: cmdutil.RunFixedArgs(0, func(args []string) error {
 			if cluster {
-				return setClusterDefaults(mainCtx, pachctlCfg, io.NopCloser(strings.NewReader(`{}`)), regenerate, reprocess)
+				return setClusterDefaults(mainCtx, pachctlCfg, io.NopCloser(strings.NewReader(`{}`)), regenerate, reprocess, dryRun)
 			}
 			return errors.New("--cluster must be specified")
 		}),
@@ -1623,6 +1624,7 @@ func Cmds(mainCtx context.Context, pachCtx *config.Context, pachctlCfg *pachctl.
 	deleteDefaults.Flags().BoolVar(&cluster, "cluster", false, "Delete cluster defaults.")
 	deleteDefaults.Flags().BoolVar(&regenerate, "regenerate", false, "Regenerate pipeline specs deleted (i.e., empty) defaults.")
 	deleteDefaults.Flags().BoolVar(&reprocess, "reprocess", false, "Reprocess regenerated pipelines.  Implies --regenerate")
+	deleteDefaults.Flags().BoolVar(&dryRun, "dry-run", false, "Do not actually delete defaults.")
 	commands = append(commands, cmdutil.CreateAliases(deleteDefaults, "delete defaults"))
 
 	updateDefaults := &cobra.Command{
@@ -1635,7 +1637,7 @@ func Cmds(mainCtx context.Context, pachCtx *config.Context, pachctlCfg *pachctl.
 				if err != nil {
 					return errors.Wrapf(err, "could not open path %q for reading", pathname)
 				}
-				return setClusterDefaults(mainCtx, pachctlCfg, rc, regenerate, reprocess)
+				return setClusterDefaults(mainCtx, pachctlCfg, rc, regenerate, reprocess, dryRun)
 			}
 			return errors.New("--cluster must be specified")
 		}),
@@ -1644,14 +1646,15 @@ func Cmds(mainCtx context.Context, pachCtx *config.Context, pachctlCfg *pachctl.
 	updateDefaults.Flags().BoolVar(&cluster, "cluster", false, "Update cluster defaults.")
 	updateDefaults.Flags().StringVarP(&pathname, "file", "f", "-", "A JSON file containing cluster defaults.  \"-\" reads from stdin (the default behavior.)")
 	updateDefaults.Flags().BoolVar(&regenerate, "regenerate", false, "Regenerate pipeline specs from new defaults.")
-	updateDefaults.Flags().BoolVar(&reprocess, "reprocess", false, "Reprocess regenerated pipelines.  Implies --regenerate")
+	updateDefaults.Flags().BoolVar(&reprocess, "reprocess", false, "Reprocess regenerated pipelines.  Implies --regenerate.")
+	updateDefaults.Flags().BoolVar(&dryRun, "dry-run", false, "Do not actually update defaults.")
 	commands = append(commands, cmdutil.CreateAliases(updateDefaults, "update defaults"))
 
 	return commands
 }
 
 // setClusterDefaults sets the cluster defaults to the result of reading from r.  Reprocess implies regenerate.
-func setClusterDefaults(ctx context.Context, pachctlCfg *pachctl.Config, r io.ReadCloser, regenerate, reprocess bool) error {
+func setClusterDefaults(ctx context.Context, pachctlCfg *pachctl.Config, r io.ReadCloser, regenerate, reprocess, dryRun bool) error {
 	if reprocess {
 		regenerate = true
 	}
@@ -1669,6 +1672,7 @@ func setClusterDefaults(ctx context.Context, pachctlCfg *pachctl.Config, r io.Re
 		ClusterDefaultsJson: string(b),
 		Regenerate:          regenerate,
 		Reprocess:           reprocess,
+		DryRun:              dryRun,
 	}
 
 	client, err := pachctlCfg.NewOnUserMachine(ctx, false)
