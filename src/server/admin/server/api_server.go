1a7c9b80457571bfc8b736622b9de0e25164c0ff
Robert A. Uhl <robert.uhl@hpe.com>
2023-08-29 10:57:57 -0400

    Don’t use k8s for admin server tests
    It’s not currently needed.

--- src/server/admin/server/api_server.go
+++ src/server/admin/server/api_server.go
@@ -4,16 +4,18 @@ import (
 	"context"
 	"fmt"
 
+	"go.uber.org/zap"
+	"google.golang.org/protobuf/proto"
+
 	"github.com/pachyderm/pachyderm/v2/src/admin"
+	"github.com/pachyderm/pachyderm/v2/src/pfs"
+	"github.com/pachyderm/pachyderm/v2/src/version"
+	"github.com/pachyderm/pachyderm/v2/src/version/versionpb"
+
 	"github.com/pachyderm/pachyderm/v2/src/internal/errors"
 	"github.com/pachyderm/pachyderm/v2/src/internal/log"
 	"github.com/pachyderm/pachyderm/v2/src/internal/pachconfig"
 	"github.com/pachyderm/pachyderm/v2/src/internal/serviceenv"
-	"github.com/pachyderm/pachyderm/v2/src/pfs"
-	"github.com/pachyderm/pachyderm/v2/src/version"
-	"github.com/pachyderm/pachyderm/v2/src/version/versionpb"
-	"go.uber.org/zap"
-	"google.golang.org/protobuf/proto"
 )
 
 // Env is the set of dependencies required by an APIServer
