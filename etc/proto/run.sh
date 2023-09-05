54f5fd5927f50f08d3ae57511a062a2347975274
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2023-09-05 18:50:35 -0400

    Allow only protojson field names in example pipelines (#9304)
    This deprecates the use of "snake_case" and string integers in the
    examples. pachctl still allows them and that will not change.
    
    I also ran 'deno fmt' on all the files that can be autoformatted (our
    proprietary `{} --- {}` format doesn't autoformat, that's actually YAML
    not JSON).
    
    Finally, I added a CI rule to ensure that every `.pipeline.json` meets
    the spec. That way if you change the protos, you will know what
    pipelines you break!
    
    This also satisfies CORE-1772 enough for me. That ticket wants us to run
    `pachctl validate` but `yajsv` is much faster and is 90% as good, so I'm
    going to close it.

--- etc/proto/run.sh
+++ etc/proto/run.sh
@@ -52,7 +52,7 @@ protoc \
     --jsonschema_opt="enums_as_strings_only" \
     --jsonschema_opt="disallow_bigints_as_strings" \
     --jsonschema_opt="prefix_schema_files_with_package" \
-    --jsonschema_opt="proto_and_json_fieldnames" \
+    --jsonschema_opt="json_fieldnames" \
     --doc_opt="json,proto-docs.json" \
     --doc2_opt="markdown,proto-docs.md" \
     "${PROTOS[@]}" > /dev/stderr
