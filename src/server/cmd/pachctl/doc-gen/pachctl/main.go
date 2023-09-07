2f6c07231a487a95d3831b429ac91378f6f7355a
L.B <mrlawrencelane@gmail.com>
2023-09-07 13:34:45 -0400

    docgen formatting updates

--- src/server/cmd/pachctl/doc-gen/pachctl/main.go
+++ src/server/cmd/pachctl/doc-gen/pachctl/main.go
@@ -40,7 +40,7 @@ func do(ctx context.Context, _ *pachconfig.EmptyConfig) error {
 	const fmTemplate = `---
 date: %s
 title: "%s"
-slug: "Learn about the %s command"
+description: "Learn about the %s command"
 ---
 
 `
@@ -54,13 +54,49 @@ slug: "Learn about the %s command"
 
 	linkHandler := func(name string) string {
 		base := strings.TrimSuffix(name, filepath.Ext(name))
-		return "/commands/" + strings.ToLower(base) + "/"
+		url := "../" + strings.ToLower(base)
+		return url
 	}
 
 	err = doc.GenMarkdownTreeCustom(rootCmd, path, filePrepender, linkHandler)
 
 	if err != nil {
 		return errors.Wrap(err, "generate Markdown documentation")
+	}
+
+	if err := reformatMarkdownOutput(path); err != nil {
+		return errors.Wrap(err, "replace ./ in Markdown files")
+	}
+
+	return nil
+}
+
+//  replace any instance of "./" in the generated markdown files with no space ""
+
+func reformatMarkdownOutput(path string) error {
+	files, err := os.ReadDir(path)
+	if err != nil {
+		return errors.Wrap(err, "read directory")
+	}
+
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), ".md") {
+			filePath := filepath.Join(path, file.Name())
+			content, err := os.ReadFile(filePath)
+			if err != nil {
+				return errors.Wrap(err, "read file")
+			}
+
+			// Replace any instance of "./" in the generated markdown files
+			updatedContent := strings.Replace(string(content), "./pachctl", "pachctl", -1)
+			// Replace any instance of "	-" in the generated markdown examples
+			updatedContent2 := strings.Replace(updatedContent, "	-", "", -1)
+
+			// Write the updated content back to the file
+			if err := os.WriteFile(filePath, []byte(updatedContent2), os.ModePerm); err != nil {
+				return errors.Wrap(err, "write file")
+			}
+		}
 	}
 
 	return nil
