9e7382868aa2570ff8844cbfe28409df03dd5faa
Robert A. Uhl <robert.uhl@hpe.com>
2023-09-12 15:11:44 -0400

    Add regeneration test

--- src/server/pps/cmds/defaults_test.go
+++ src/server/pps/cmds/defaults_test.go
@@ -110,3 +110,80 @@ func TestCreatePipeline_defaults(t *testing.T) {
 		"PipelineName", "pipeline",
 	).Run())
 }
+
+func TestCreatePipeline_regenerate(t *testing.T) {
+
+	if testing.Short() {
+		t.Skip("Skipping integration tests in short mode")
+	}
+
+	c, _ := minikubetestenv.AcquireCluster(t)
+	require.NoError(t, tu.PachctlBashCmd(t, c, `
+		pachctl create defaults --cluster <<EOF
+		{
+			"create_pipeline_request": {
+				"datumTries": 17,
+				"autoscaling": true
+			}
+		}
+		EOF
+		pachctl create repo {{.RepoName}}
+		pachctl create pipeline <<EOF
+		{
+			"pipeline": {
+				"project": {
+					"name": "{{.ProjectName | js}}"
+				},
+				"name": "{{.PipelineName | js}}"
+			},
+			"transform": {
+				"cmd": ["cp", "r", "/pfs/in", "/pfs/out"]
+			},
+			"input": {
+				"pfs": {
+					"project": "default",
+					"repo": "{{.RepoName | js}}",
+					"glob": "/*",
+					"name": "in"
+				}
+			},
+			"resource_requests": {
+				"cpu": null,
+				"disk": "187Mi"
+			},
+			"autoscaling": false
+		}
+		EOF
+		pachctl inspect pipeline {{.PipelineName}} --raw | jq -r .details.resource_requests.disk | match 187Mi
+		pachctl inspect pipeline {{.PipelineName}} --raw | jq -r .details.resource_requests.memory | match null
+		# the raw format currently marshals to snake case
+		pachctl inspect pipeline {{.PipelineName}} --raw | jq -r .details.datum_tries | match 17
+		pachctl inspect pipeline {{.PipelineName}} --raw | jq -r .details.autoscaling | match null
+		# the specs marshal to camelCase
+		pachctl inspect pipeline {{.PipelineName}} --raw | jq -r .effective_spec_json | jq -r .datumTries | match 17
+		pachctl inspect pipeline {{.PipelineName}} --raw | jq -r .effective_spec_json | jq -r .autoscaling | match false
+		# updating defaults without --regenerate does not change the pipeline
+		pachctl create defaults --cluster <<EOF
+		{
+			"create_pipeline_request": {
+				"datumTries": 18,
+				"autoscaling": true
+			}
+		}
+		EOF
+		pachctl inspect pipeline {{.PipelineName}} --raw | jq -r .effective_spec_json | jq -r .datumTries | match 17
+		# updating defaults with --regenerate does change the pipeline
+		pachctl create defaults --cluster --regenerate <<EOF
+		{
+			"create_pipeline_request": {
+				"datumTries": 8,
+				"autoscaling": true
+			}
+		}
+		EOF
+		pachctl inspect pipeline {{.PipelineName}} --raw | jq -r .effective_spec_json | jq -r .datumTries | match 8
+		`,
+		"ProjectName", pfs.DefaultProjectName,
+		"RepoName", "input",
+		"PipelineName", "pipeline").Run())
+}
