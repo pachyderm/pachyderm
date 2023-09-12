0772e712bc290ace9c1bdb2b20f2bd215674972a
Brendon Carroll <7072757+brendoncarroll@users.noreply.github.com>
2023-09-12 15:48:54 -0400

    Move PFS Load Tests to Debug Server (#9253)
    These changes move the PFS Load Test methods to the debug server. The
    motivation is to remove the pach client as a dependency of PFS.
    Currently the way a service does a self RPC is by importing a pach
    client, which also means it has to depend on all of the APIs.
    
    The Debug server is a good place to move this functionality because it
    is, and always will be, at the top of the dependency graph (without
    anything depending on it).
    Adding the Task Service as a dependency of the Debug Server makes sense
    because it would allow us to parallelize our collection of debug dumps.

--- src/server/pfs/server/server.go
+++ src/server/pfs/server/server.go
@@ -3,13 +3,10 @@ package server
 import (
 	"context"
 
-	"github.com/pachyderm/pachyderm/v2/src/internal/client"
 	col "github.com/pachyderm/pachyderm/v2/src/internal/collection"
 	"github.com/pachyderm/pachyderm/v2/src/internal/obj"
 	"github.com/pachyderm/pachyderm/v2/src/internal/pachconfig"
 	"github.com/pachyderm/pachyderm/v2/src/internal/pachsql"
-	"github.com/pachyderm/pachyderm/v2/src/internal/pctx"
-	"github.com/pachyderm/pachyderm/v2/src/internal/pfsload"
 	"github.com/pachyderm/pachyderm/v2/src/internal/task"
 	txnenv "github.com/pachyderm/pachyderm/v2/src/internal/transactionenv"
 	"github.com/pachyderm/pachyderm/v2/src/internal/transactionenv/txncontext"
@@ -35,8 +32,6 @@ type Env struct {
 
 	AuthServer           authserver.APIServer
 	GetPipelineInspector func() PipelineInspector
-	// TODO: remove this, the load tests need a pachClient
-	GetPachClient func(ctx context.Context) *client.APIClient
 
 	BackgroundContext context.Context
 	StorageConfig     pachconfig.StorageConfiguration
@@ -49,29 +44,5 @@ func NewAPIServer(env Env) (pfsserver.APIServer, error) {
 	if err != nil {
 		return nil, err
 	}
-	go func() {
-		pfsload.Worker(env.GetPachClient(pctx.Child(env.BackgroundContext, "pfsload")), env.TaskService) //nolint:errcheck
-	}()
-	return newValidatedAPIServer(a, env.AuthServer), nil
-}
-
-// TODO: remove this after load tests have been moved to Debug Server
-func NewSidecarAPIServer(env Env) (pfsserver.APIServer, error) {
-	a, err := newAPIServer(env)
-	if err != nil {
-		return nil, err
-	}
-	return newValidatedAPIServer(a, env.AuthServer), nil
-}
-
-// NewPachwAPIServer is used when running pachd in Pachw Mode.
-// In Pachw Mode, a pachd instance processes storage and URl related tasks via the task service.
-// TODO: remove this after load tests have been moved to Debug server.
-func NewPachwAPIServer(env Env) (pfsserver.APIServer, error) {
-	a, err := newAPIServer(env)
-	if err != nil {
-		return nil, err
-	}
-	go func() { pfsload.Worker(env.GetPachClient(env.BackgroundContext), env.TaskService) }() //nolint:errcheck
 	return newValidatedAPIServer(a, env.AuthServer), nil
 }
