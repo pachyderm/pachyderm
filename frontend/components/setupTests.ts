375e17758acb9126472fd2cc0ef13842308a081b
Jeff Rifwald <2831294+jeffrifwald@users.noreply.github.com>
2023-09-15 10:12:51 -0400

    chore(tests): update @testing-library/jest-dom (#1576)

--- frontend/components/setupTests.ts
+++ frontend/components/setupTests.ts
@@ -1,5 +1,4 @@
 import '@testing-library/jest-dom';
-import '@testing-library/jest-dom/extend-expect';
 
 import {randomBytes} from 'crypto';
 
--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -1,6 +1,5 @@
 import 'cross-fetch';
 import '@testing-library/jest-dom';
-import '@testing-library/jest-dom/extend-expect';
 
 import {randomBytes} from 'crypto';
 
