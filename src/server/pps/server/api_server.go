55f6f0fed659284381fde37a95ee19656357a88c
acohen4 <alonco124@gmail.com>
2023-09-21 17:04:39 +0300

    Spout commits should be closed by PPS

--- src/server/pps/server/api_server.go
+++ src/server/pps/server/api_server.go
@@ -2543,7 +2543,12 @@ func (a *apiServer) CreatePipelineInTransaction(ctx context.Context, txnCtx *txn
 	}); err != nil {
 		return errors.Wrapf(err, "could not create/update output branch")
 	}
-
+	if request.Spout == nil {
+		c := &pfs.Commit{Branch: outputBranch, Repo: outputBranch.Repo}
+		if err := a.env.PFSServer.FinishCommitInTransaction(ctx, txnCtx, &pfs.FinishCommitRequest{Commit: c, Description: "close spout commit"}); err != nil {
+			return errors.Wrapf(err, "could not create/update output branch")
+		}
+	}
 	if visitErr := pps.VisitInput(request.Input, func(input *pps.Input) error {
 		if input.Pfs != nil && input.Pfs.Trigger != nil {
 			var prevHead *pfs.Commit
