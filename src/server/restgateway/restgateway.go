4e637bea959638cee8f410f1649111f2e50a03f7
Zartaj Majeed <zmajeed@users.noreply.github.com>
2023-09-22 13:29:21 -0500

    rest api server (#9358)

---
+++ src/server/restgateway/restgateway.go
@@ -0,0 +1,189 @@
+package restgateway
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/url"
+  "flag"
+
+	"github.com/pachyderm/pachyderm/v2/src/internal/client"
+	"github.com/pachyderm/pachyderm/v2/src/internal/errors"
+	"github.com/pachyderm/pachyderm/v2/src/internal/log"
+	"go.uber.org/zap"
+
+  "github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+  "google.golang.org/grpc"
+  "google.golang.org/grpc/credentials/insecure"
+
+	ppsgw "github.com/pachyderm/pachyderm/v2/src/pps"
+	pfsgw "github.com/pachyderm/pachyderm/v2/src/pfs"
+
+	workergw "github.com/pachyderm/pachyderm/v2/src/worker"
+	proxygw "github.com/pachyderm/pachyderm/v2/src/proxy"
+	admingw "github.com/pachyderm/pachyderm/v2/src/admin"
+	authgw "github.com/pachyderm/pachyderm/v2/src/auth"
+	licensegw "github.com/pachyderm/pachyderm/v2/src/license"
+	identitygw "github.com/pachyderm/pachyderm/v2/src/identity"
+	debuggw "github.com/pachyderm/pachyderm/v2/src/debug"
+	enterprisegw "github.com/pachyderm/pachyderm/v2/src/enterprise"
+	transactiongw "github.com/pachyderm/pachyderm/v2/src/transaction"
+	versiongw "github.com/pachyderm/pachyderm/v2/src/version/versionpb"
+
+)
+
+var (
+  // pachd-grpc server port 1650
+  grpcServerEndpoint = flag.String("grpc-server-endpoint",  "localhost:1650", "gRPC server endpoint")
+)
+
+// Server is an http.Server that serves public requests.
+type Server struct {
+	mux    runtime.ServeMux
+	server *http.Server
+}
+
+// New creates a new API server, with an http.Server to actually serve traffic.
+func New(port uint16, pachClientFactory func(ctx context.Context) *client.APIClient) *Server {
+  ctx := context.Background()
+  ctx, cancel := context.WithCancel(ctx)
+  defer cancel()
+
+  mux := runtime.NewServeMux()
+
+	// Health check.
+	//mux.HandlePath("POST", "/healthz", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	mux.HandlePath("GET", "/healthz", func(w http.ResponseWriter, r *http.Request, _ map[string]string) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("healthy\n")) //nolint:errcheck
+	})
+
+  opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+
+  err := ppsgw.RegisterAPIHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
+  if err != nil {
+    return nil
+  }
+
+  err = pfsgw.RegisterAPIHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
+  if err != nil {
+    return nil
+  }
+
+  err = workergw.RegisterWorkerHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
+  if err != nil {
+    return nil
+  }
+
+  err = proxygw.RegisterAPIHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
+  if err != nil {
+    return nil
+  }
+
+  err = admingw.RegisterAPIHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
+  if err != nil {
+    return nil
+  }
+
+  err = authgw.RegisterAPIHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
+  if err != nil {
+    return nil
+  }
+
+  err = licensegw.RegisterAPIHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
+  if err != nil {
+    return nil
+  }
+
+  err = identitygw.RegisterAPIHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
+  if err != nil {
+    return nil
+  }
+
+  err = debuggw.RegisterDebugHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
+  if err != nil {
+    return nil
+  }
+
+  err = enterprisegw.RegisterAPIHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
+  if err != nil {
+    return nil
+  }
+
+  err = transactiongw.RegisterAPIHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
+  if err != nil {
+    return nil
+  }
+
+  err = versiongw.RegisterAPIHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
+  if err != nil {
+    return nil
+  }
+
+	return &Server{
+		mux: *mux,
+		server: &http.Server{
+			Addr:    fmt.Sprintf(":%d", port),
+			Handler: mux,
+		},
+	}
+}
+
+// CSRFWrapper is an http.Handler that provides CSRF protection to the underlying handler.
+func CSRFWrapper(h http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		origin := r.Header.Get("origin")
+		if origin != "" {
+			u, err := url.Parse(origin)
+			if err != nil {
+				origin = "error(origin): " + err.Error()
+			} else if u.Host == "" {
+				origin = "error(origin): no host"
+			} else {
+				origin = u.Host
+			}
+		} else {
+			// No Origin header, try Referer.
+			if r := r.Header.Get("referer"); r != "" {
+				u, err := url.Parse(r)
+				if err != nil {
+					origin = "error(referer): " + err.Error() // We must deny in this case.
+				} else if u.Host == "" {
+					origin = "error(referer): no host"
+				} else {
+					origin = u.Host
+				}
+			}
+			// Neither header exists.
+		}
+		if origin == "" {
+			log.Debug(r.Context(), "csrf: no origin or referer header; assuming cli; allow")
+			h.ServeHTTP(w, r)
+			return
+		}
+		if origin != r.Host {
+			log.Info(r.Context(), "csrf: origin/host mismatch; deny", zap.String("resolved_origin", origin), zap.Strings("origin", r.Header.Values("origin")), zap.Strings("referer", r.Header.Values("referer")), zap.String("host", r.Host))
+			http.Error(w, "csrf: origin/host mismatch", http.StatusForbidden)
+			return
+		}
+		// Origin and Host match; allow.
+		h.ServeHTTP(w, r)
+	}
+}
+
+// ListenAndServe begins serving the server, and returns when the context is canceled or the server
+// dies on its own.
+func (h *Server) ListenAndServe(ctx context.Context) error {
+	log.AddLoggerToHTTPServer(ctx, "restgateway", h.server)
+	errCh := make(chan error, 1)
+	go func() {
+		errCh <- h.server.ListenAndServe()
+	}()
+	select {
+	case <-ctx.Done():
+		log.Info(ctx, "restgateway server exiting", zap.Error(context.Cause(ctx)))
+		return errors.EnsureStack(h.server.Shutdown(ctx))
+	case err := <-errCh:
+		return err
+	}
+}
