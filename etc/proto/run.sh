309aa7949e66094200904a796c62c94482375b26
Jeff Rifwald <jmcriffey@gmail.com>
2023-09-26 16:25:31 -0400

    generate swagger v2 docs from protos

--- etc/proto/run.sh
+++ etc/proto/run.sh
@@ -59,6 +59,8 @@ protoc \
     --grpc-gateway_opt logtostderr=true \
     --grpc-gateway_opt paths=source_relative \
     --grpc-gateway_opt generate_unbound_methods=true \
+    --openapiv2_out v2/src \
+    --openapiv2_opt logtostderr=true \
     "${PROTOS[@]}" > /dev/stderr
 
 pushd v2 > /dev/stderr
