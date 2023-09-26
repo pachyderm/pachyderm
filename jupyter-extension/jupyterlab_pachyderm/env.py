61bcf1428d59a2e2e133f76e94c07af6c112876a
Ben Bonenfant <ben.bonenfant@pachyderm.io>
2023-09-26 13:02:43 -0700

    [Jupyter] Enable networking over UNIX domain sockets with pycurl (#9366)
    Development on #9219 moved to this branch to run changes through our CI.
    See that PR description for more information, but the TLDR is that HPC
    environments need to use sockets and the pycurl is supported best by the
    jupyter ecosystem. Additionally the `pycurl` package is already included
    in the base image of our notebooks-worker image.
    
    ---------
    
    Co-authored-by: Pankaj Mandal <mandalpa@cray.com>
    Co-authored-by: pankaj-mandal <pankaj.mandal.ku@gmail.com>
    Co-authored-by: pankaj-mandal <pankaj.mandal@hpe.com>

--- jupyter-extension/jupyterlab_pachyderm/env.py
+++ jupyter-extension/jupyterlab_pachyderm/env.py
@@ -1,7 +1,11 @@
 import os
 from distutils.util import strtobool
 
+HTTP_UNIX_SOCKET_SCHEMA="http_unix"
+HTTP_SCHEMA="http"
 PFS_MOUNT_DIR = os.environ.get("PFS_MOUNT_DIR", "/pfs")
+PFS_SOCK_PATH = os.environ.get("PFS_SOCK_PATH", "/tmp/pfs.sock")
+DEFAULT_SCHEMA = os.environ.get("DEFAULT_SCHEMA", HTTP_UNIX_SOCKET_SCHEMA)
 SIDECAR_MODE = strtobool(os.environ.get("SIDECAR_MODE", "False").lower())
 MOUNT_SERVER_LOG_DIR = os.environ.get("MOUNT_SERVER_LOG_DIR") # defaults to stdout/stderr
 NONPRIV_CONTAINER = os.environ.get("NONPRIV_CONTAINER") # Unset assume --privileged container
