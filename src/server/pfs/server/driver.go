e5c15cc087454496c6fc2ea672beedf0f1a36e71
acohen4 <alonco124@gmail.com>
2023-09-28 16:20:49 +0300

    better

--- src/server/pfs/server/driver.go
+++ src/server/pfs/server/driver.go
@@ -1994,11 +1994,8 @@ func (d *driver) makeEmptyCommit(ctx context.Context, txnCtx *txncontext.Transac
 	// Input repos want a closed head commit, so decide if we leave
 	// it open by the presence of branch provenance.
 	closed := true
-	for _, prov := range directProvenance {
-		if prov.Repo.Type != pfs.SpecRepoType {
-			closed = false
-			break
-		}
+	if len(directProvenance) > 0 {
+		closed = false
 	}
 	commit := branch.NewCommit(txnCtx.CommitSetID)
 	commit.Repo = branch.Repo
