b2c850e2766becf2448fc252e19f0208a5f7fcd9
Pankaj Mandal <pankaj.mandal@hpe.com>
2023-10-06 13:55:09 -0700

    add default value

--- jupyter-extension/jupyterlab_pachyderm/env.py
+++ jupyter-extension/jupyterlab_pachyderm/env.py
@@ -8,7 +8,7 @@ PFS_SOCK_PATH = os.environ.get("PFS_SOCK_PATH", "/tmp/pfs.sock")
 DEFAULT_SCHEMA = os.environ.get("DEFAULT_SCHEMA", HTTP_UNIX_SOCKET_SCHEMA)
 SIDECAR_MODE = strtobool(os.environ.get("SIDECAR_MODE", "False").lower())
 MOUNT_SERVER_LOG_DIR = os.environ.get("MOUNT_SERVER_LOG_DIR") # defaults to stdout/stderr
-NONPRIV_CONTAINER = strtobool(os.environ.get("NONPRIV_CONTAINER").lower()) # Unset assume --privileged container
+NONPRIV_CONTAINER = strtobool(os.environ.get("NONPRIV_CONTAINER", "False").lower()) # Unset assume --privileged container
 DET_RESOURCES_TYPE = os.environ.get("DET_RESOURCES_TYPE") # Assume MLDE if set
 SLURM_JOB="slurm-job" # For launcher HPC this is set in DispatcherRM
 
