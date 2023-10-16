39c0cfa1a8aae97436082b42ea5e2ac47a00ec36
Matthew Steffen <msteffen@pachyderm.io>
2023-10-16 11:49:26 -0700

    Switch from custom ResponseWriter to httpsnoop

--- src/internal/log/http.go
+++ src/internal/log/http.go
@@ -7,59 +7,10 @@ import (
 	"net/http"
 	"strings"
 
+	"github.com/felixge/httpsnoop"
 	"go.uber.org/zap"
 	"google.golang.org/grpc/metadata"
 )
-
-// loggingResponseWriter logs the first 1kb of HTTP response data if the HTTP
-// header is an error status (4xx or 5xx)
-type loggingResponseWriter struct {
-	// w is the inner HTTP ResponseWriter that sends data to the client
-	w http.ResponseWriter
-	// statusCode is the response's HTTP error code--determines whether the body
-	// content is logged.
-	statusCode int
-	// buf will hold the first 1k of the response body (for logging) if the server
-	// returns an error response
-	buf []byte
-}
-
-func newLoggingResponseWriter(w http.ResponseWriter) *loggingResponseWriter {
-	return &loggingResponseWriter{
-		w: w,
-	}
-}
-
-func (l *loggingResponseWriter) Header() http.Header {
-	return l.w.Header()
-}
-
-func (l *loggingResponseWriter) Write(bs []byte) (int, error) {
-	if l.statusCode == 0 {
-		l.statusCode = 200
-	}
-	if l.statusCode >= 400 {
-		if l.buf == nil {
-			// TODO would it be better to statically alloc this? (e.g. [1000]byte in
-			// the struct)
-			l.buf = make([]byte, 0, 1000)
-		}
-		if len(l.buf) < cap(l.buf) {
-			m := len(l.buf)
-			n := min(len(bs), cap(l.buf)-m)
-			l.buf = l.buf[:m+n] // extend l.buf
-			copy(l.buf[m:], bs[:n])
-		}
-	}
-	return l.w.Write(bs)
-}
-
-func (l *loggingResponseWriter) WriteHeader(statusCode int) {
-	if l.statusCode == 0 {
-		l.statusCode = statusCode
-	}
-	l.w.WriteHeader(statusCode)
-}
 
 // AddLoggerToHTTPServer mutates the provided *http.Server so that (*http.Request).Context() returns
 // a context that can be logged to.
@@ -95,10 +46,42 @@ func AddLoggerToHTTPServer(rctx context.Context, name string, s *http.Server) {
 			ctx = ChildLogger(ctx, "", WithFields(id))
 			ctx = metadata.NewOutgoingContext(ctx, metadata.MD{"x-request-id": requestID})
 			r = r.WithContext(ctx)
-			lw := newLoggingResponseWriter(w)
-			orig.ServeHTTP(lw, r)
+
+			var respStatusCode int
+			var errbuf []byte
+			w = httpsnoop.Wrap(w, httpsnoop.Hooks{
+				WriteHeader: func(next httpsnoop.WriteHeaderFunc) httpsnoop.WriteHeaderFunc {
+					return func(code int) {
+						respStatusCode = code
+						next(code)
+					}
+				},
+				Write: func(next httpsnoop.WriteFunc) httpsnoop.WriteFunc {
+					return func(b []byte) (int, error) {
+						if respStatusCode == 0 {
+							respStatusCode = 200
+						}
+						if respStatusCode >= 400 {
+							if errbuf == nil {
+								// TODO would it be better to statically alloc this? (e.g.
+								// [1000]byte above?)
+								errbuf = make([]byte, 0, 1000)
+							}
+							if len(errbuf) < cap(errbuf) {
+								m := len(errbuf)
+								n := min(len(b), cap(errbuf)-m)
+								errbuf = errbuf[:m+n] // extend errbuf
+								copy(errbuf[m:], b[:n])
+							}
+						}
+						return next(b)
+					}
+				},
+			})
+
+			orig.ServeHTTP(w, r)
 			if ua := r.Header.Get("user-agent"); ua != "Envoy/HC" {
-				Info(ctx, "http response", zap.String("method", r.Method), zap.String("host", r.Host), zap.String("path", url), zap.String("peer", r.RemoteAddr), id, zap.Int("status-code", lw.statusCode), zap.ByteString("error-msg", lw.buf))
+				Info(ctx, "http response", zap.String("method", r.Method), zap.String("host", r.Host), zap.String("path", url), zap.String("peer", r.RemoteAddr), id, zap.Int("status-code", respStatusCode), zap.ByteString("error-msg", errbuf))
 			}
 		})
 	}
--- src/internal/log/http_test.go
+++ src/internal/log/http_test.go
@@ -47,6 +47,10 @@ func TestHTTPServerError(t *testing.T) {
 	ctx, h := testWithCaptureParallel(t, zap.Development())
 	server := &http.Server{
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.URL.Path == "/tea" {
+				w.Write([]byte("OK"))
+				return
+			}
 			// test that response log lines only include the first 1000b of the
 			// response body.
 			http.Error(w, strings.Repeat("a", 2000), http.StatusTeapot)
@@ -62,27 +66,73 @@ func TestHTTPServerError(t *testing.T) {
 	go server.Serve(l)                                          //nolint:errcheck
 	t.Cleanup(func() { server.Shutdown(context.Background()) }) //nolint:errcheck
 
-	if _, err := http.Get("http://" + l.Addr().String() + "/brew?drink=coffee"); err != nil {
-		t.Fatalf("get: %v", err)
-	}
+	t.Run("HealthyResponsesDontLogBody", func(t *testing.T) {
+		h.Clear()
+		if _, err := http.Get("http://" + l.Addr().String() + "/tea"); err != nil {
+			t.Fatalf("get: %v", err)
+		}
 
-	want := []string{
-		"http: info: incoming http request",
-		"http: info: http response",
-	}
-	if diff := cmp.Diff(h.Logs(), want, formatLogs(simple)); diff != "" {
-		t.Errorf("logs (-got +want):\n%s", diff)
-	}
-	for _, l := range h.Logs() {
-		if errmsg, ok := l.Keys["err-msg"]; ok {
-			errmsg, ok := errmsg.(string)
-			if !ok {
-				t.Errorf("expected http error msg to be string, but was: %T", errmsg)
+		want := []string{
+			"http: info: incoming http request",
+			"http: info: http response",
+		}
+		if diff := cmp.Diff(h.Logs(), want, formatLogs(simple)); diff != "" {
+			t.Errorf("logs (-got +want):\n%s", diff)
+		}
+		for _, l := range h.Logs() {
+			if rawStatus, ok := l.Keys["status-code"]; ok {
+				status, ok := rawStatus.(float64)
+				if !ok {
+					t.Errorf("expected http error msg to be float64, but was: %T", rawStatus)
+				}
+				if status != 200 {
+					// can't use require.Equal due to import cycle
+					t.Errorf("expected status-code 200, but was: %d", int(status))
+				}
 			}
-			if errmsg != strings.Repeat("a", 1000) {
-				// can't use require.Equal due to import cycle
-				t.Errorf("expected http error msg to be a*1000, but was:\n%s\n(len: %d)", errmsg, len(errmsg))
+			if rawErrmsg, ok := l.Keys["err-msg"]; ok {
+				errmsg, ok := rawErrmsg.(string)
+				if !ok {
+					t.Errorf("expected http error msg to be string, but was: %T", rawErrmsg)
+				}
+				if errmsg != "" {
+					t.Errorf("expected empty http error msg, but was:\n%s\n(len: %d)", errmsg, len(errmsg))
+				}
 			}
 		}
-	}
+	})
+	t.Run("ErrorResponsesLogged", func(t *testing.T) {
+		h.Clear()
+		if _, err := http.Get("http://" + l.Addr().String() + "/coffee"); err != nil {
+			t.Fatalf("get: %v", err)
+		}
+
+		want := []string{
+			"http: info: incoming http request",
+			"http: info: http response",
+		}
+		if diff := cmp.Diff(h.Logs(), want, formatLogs(simple)); diff != "" {
+			t.Errorf("logs (-got +want):\n%s", diff)
+		}
+		for _, l := range h.Logs() {
+			if rawStatus, ok := l.Keys["status-code"]; ok {
+				status, ok := rawStatus.(float64)
+				if !ok {
+					t.Errorf("expected http error msg to be float64, but was: %T", rawStatus)
+				}
+				if status != http.StatusTeapot {
+					t.Errorf("expected status-code %d, but was: %d", http.StatusTeapot, int(status))
+				}
+			}
+			if rawErrmsg, ok := l.Keys["err-msg"]; ok {
+				errmsg, ok := rawErrmsg.(string)
+				if !ok {
+					t.Errorf("expected http error msg to be string, but was: %T", rawErrmsg)
+				}
+				if errmsg != strings.Repeat("a", 1000) {
+					t.Errorf("expected http error msg to be a*1000, but was:\n%s\n(len: %d)", errmsg, len(errmsg))
+				}
+			}
+		}
+	})
 }
