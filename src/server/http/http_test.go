92ce9960a4a4c321239bd8c64c5fb668373bfc3a
Zartaj Majeed <zartaj.majeed@hpe.com>
2023-10-18 13:27:01 -0500

    move routing test back to http but rename package to avoid import cycle

--- src/server/http/http_test.go
+++ src/server/http/http_test.go
@@ -1,12 +1,66 @@
-package http
+package http_test
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/pachyderm/pachyderm/v2/src/internal/client"
+	"github.com/pachyderm/pachyderm/v2/src/internal/dockertestenv"
+	"github.com/pachyderm/pachyderm/v2/src/internal/log"
 	"github.com/pachyderm/pachyderm/v2/src/internal/pctx"
+	"github.com/pachyderm/pachyderm/v2/src/internal/testpachd/realenv"
+	pachdhttp "github.com/pachyderm/pachyderm/v2/src/server/http"
 )
+
+func TestRouting(t *testing.T) {
+	testData := []struct {
+		name     string
+		method   string
+		url      string
+		wantCode int
+	}{
+		{
+			name:     "test not found status",
+			method:   "GET",
+			url:      "http://pachyderm.example.com/",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "health",
+			method:   "GET",
+			url:      "http://pachyderm.example.com/healthz",
+			wantCode: http.StatusOK,
+		},
+		{
+			name:     "CreatePipelineRequest JSON schema",
+			method:   "GET",
+			url:      "http://pachyderm.example.com/jsonschema/pps_v2/CreatePipelineRequest.schema.json",
+			wantCode: http.StatusOK,
+		},
+	}
+	for _, test := range testData {
+		t.Run(test.name, func(t *testing.T) {
+			ctx := pctx.TestContext(t)
+			s := pachdhttp.New(ctx, 0, func(ctx context.Context) *client.APIClient {
+				env := realenv.NewRealEnv(ctx, t, dockertestenv.NewTestDBConfig(t))
+				client := env.PachClient
+				return client
+			})
+			log.AddLoggerToHTTPServer(ctx, test.name, s.Server)
+
+			req := httptest.NewRequest(test.method, test.url, nil)
+			req = req.WithContext(ctx)
+			rec := httptest.NewRecorder()
+
+			s.Server.Handler.ServeHTTP(rec, req)
+			if got, want := rec.Code, test.wantCode; got != want {
+				t.Errorf("response code:\n  got: %v\n want: %v", got, want)
+			}
+		})
+	}
+}
 
 func TestCSRFWrapper(t *testing.T) {
 	testData := []struct {
@@ -61,7 +115,7 @@ func TestCSRFWrapper(t *testing.T) {
 		},
 	}
 
-	f := CSRFWrapper(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	f := pachdhttp.CSRFWrapper(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("ok")) //nolint:errcheck
 	}))
