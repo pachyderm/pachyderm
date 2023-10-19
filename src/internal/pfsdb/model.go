bbba7b3f428f43f76cc24b59cc093843fe60eec6
Fahad Syed <deadbeamNetworks@gmail.com>
2023-10-19 15:23:06 -0400

    [PFS-94-138] remove debug comment.

--- src/internal/pfsdb/model.go
+++ src/internal/pfsdb/model.go
@@ -41,10 +41,6 @@ func (repo Repo) GetCreatedAtUpdatedAt() CreatedAtUpdatedAt {
 	return repo.CreatedAtUpdatedAt
 }
 
-func (repo Repo) GetCreatedAtUpdatedAt() CreatedAtUpdatedAt {
-	return repo.CreatedAtUpdatedAt
-}
-
 func (repo *Repo) Pb() *pfs.Repo {
 	return &pfs.Repo{
 		Name:    repo.Name,
