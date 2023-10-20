8c245cc2f5ffb7b82accfff03634eb2cc23b8076
Daniel Janicek <daniel.janicek@hpe.com>
2023-10-20 10:51:31 -0700

    debug info

--- src/testing/cmds/harness/test-runner/main.go
+++ src/testing/cmds/harness/test-runner/main.go
@@ -134,7 +134,7 @@ func runTest(pkg string, testNames []string, tags string, gotestsumArgs string, 
 	findTestArgs := []string{
 		fmt.Sprintf("--packages=%s", pkg),
 		"--rerun-fails",
-		"--rerun-fails-max-failures=2",
+		"--rerun-fails-max-failures=1",
 		"--format=testname",
 		"--debug",
 	}
