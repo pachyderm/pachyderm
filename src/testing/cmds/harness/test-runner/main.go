a8b605f8dcf1cb2c0a86a66b7910b2fda45c713c
Daniel Janicek <daniel.janicek@hpe.com>
2023-10-24 14:34:03 -0700

    test perf check

--- src/testing/cmds/harness/test-runner/main.go
+++ src/testing/cmds/harness/test-runner/main.go
@@ -151,7 +151,7 @@ func runTest(pkg string, testNames []string, tags string, gotestsumArgs string, 
 
 	findTestArgs = append(findTestArgs, "--",
 		"-timeout=30m",
-		"-parallel=4", // DNJ TODO allow this to change with args
+		"-parallel=2", // DNJ TODO allow this to change with args
 		fmt.Sprintf("-tags=%s", tags),
 		fmt.Sprintf("-run=%s", testRegex.String()))
 	if gotestArgs != "" {
