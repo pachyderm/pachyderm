7a348726232b2ffedd0dc5ec25a614579ba3918f
Matthew Steffen <msteffen@pachyderm.io>
2023-10-28 12:37:38 -0700

    Clean up testing loose ends (enable server logging, add comments)

--- src/server/pfs/fuse/helpers_test.go
+++ src/server/pfs/fuse/helpers_test.go
@@ -52,9 +52,17 @@ func withServerMount(tb testing.TB, c *client.APIClient, sopts *ServerOptions, f
 		require.ErrorIs(tb, mountErr, http.ErrServerClosed)
 	}()
 	defer func() {
-		// recover because panics leave the mount in a weird state that makes
-		// it hard to rerun the tests, mostly relevent when you're iterating on
-		// these tests, or the code they test.
+		// panic() leaves the mount in a weird state that makes it hard to rerun the
+		// tests (mostly relevent when you're iterating on these tests, or the code
+		// they test).
+		//
+		// N.B. (msteffen): AFAICT, recover() returns nil unless called in the
+		// _same_ goroutine as panic(). Go's HTTP server handles each request in a
+		// separate goroutine, so if panic() is called inside a handler, this call
+		// to recover() will return nil, and tb.Fatal() will not be called. In
+		// general, I don't know of a way to determine, at this location, whether
+		// the program is exiting due to a panic in another goro or exiting
+		// normally.
 		if r := recover(); r != nil {
 			tb.Fatal(r)
 		}
