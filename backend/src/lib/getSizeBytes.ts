c62450789e6427a62e350eab0dc43fff5040acc8
Logan <31484824+loganripplinger@users.noreply.github.com>
2023-10-31 10:39:39 -0700

    fix: Correctly show zero size commits instead of sizeBytesUpperBound (#1688)

--- backend/src/lib/getSizeBytes.ts
+++ backend/src/lib/getSizeBytes.ts
@@ -1,7 +1,7 @@
 import {CommitInfo, RepoInfo} from '@dash-backend/proto';
 
 const getSizeBytes = (obj: CommitInfo.AsObject | RepoInfo.AsObject) => {
-  return obj.details?.sizeBytes || obj.sizeBytesUpperBound || 0;
+  return obj.details?.sizeBytes ?? obj.sizeBytesUpperBound ?? 0;
 };
 
 export default getSizeBytes;
