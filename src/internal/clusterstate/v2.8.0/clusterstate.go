bdeddf86aad89816f0b5a3035d6f1dd248f6217f
Fahad Syed <deadbeamNetworks@gmail.com>
2023-11-02 12:29:46 -0400

    [PFS-164] remove temp migration stuff.

--- src/internal/clusterstate/v2.8.0/clusterstate.go
+++ src/internal/clusterstate/v2.8.0/clusterstate.go
@@ -12,13 +12,10 @@ func Migrate(state migrations.State) migrations.State {
 		Apply("Migrate collections.repos to pfs.repos", migrateRepos, migrations.Squash).
 		Apply("Migrate collections.branches to pfs.branches", migrateBranches, migrations.Squash).
 		Apply("Migrate collections.commits to pfs.commits", migrateCommits, migrations.Squash).
+		Apply("alter pfs.commits schema post data migration", alterCommitsTablePostDataMigration, migrations.Squash).
 		Apply("Synthesize cluster defaults from environment variables", synthesizeClusterDefaults, migrations.Squash).
 		Apply("Synthesize user and effective specs from their pipeline details", synthesizeSpecs, migrations.Squash).
 		Apply("create project defaults", func(ctx context.Context, env migrations.Env) error {
 			return setupPostgresCollections(ctx, env.Tx, ppsCollections()...)
 		})
-}
-
-func PostMigrate(state migrations.State) migrations.State {
-	return state.Apply("alter pfs.commits schema post data migration", alterCommitsTablePostDataMigration)
 }
