abe61168a1fe4b1aee8ba55537f93e558d4fe378
Daniel Janicek <daniel.janicek@hpe.com>
2023-11-03 09:15:45 -0700

    increase deploy pool

--- src/internal/minikubetestenv/client_factory.go
+++ src/internal/minikubetestenv/client_factory.go
@@ -36,7 +36,7 @@ var (
 		sem:               *semaphore.NewWeighted(int64(*poolSize)),
 	}
 	setup               sync.Once
-	poolSize            *int  = flag.Int("clusters.pool", 8, "maximum size of managed pachyderm clusters")
+	poolSize            *int  = flag.Int("clusters.pool", 9, "maximum size of managed pachyderm clusters")
 	useLeftoverClusters *bool = flag.Bool("clusters.reuse", true, "reuse leftover pachyderm clusters if available")
 	cleanupDataAfter    *bool = flag.Bool("clusters.data.cleanup", false, "cleanup the data following each test")
 	forceLocal          *bool = flag.Bool("clusters.local", false, "use whatever is in your pachyderm context as the target")
