0c212547ffdd237abb6d4234ff1d1be309fb0ee1
Daniel Janicek <daniel.janicek@hpe.com>
2023-11-14 12:22:22 -0800

    collector processes increase

--- src/testing/cmds/harness/test-collector/main.go
+++ src/testing/cmds/harness/test-collector/main.go
@@ -95,8 +95,6 @@ func testNames(ctx context.Context, pkg string, addtlCmdArgs ...string) (map[str
 	if err != nil {
 		return nil, errors.EnsureStack(err)
 	}
-	cmd.Env = os.Environ()
-	cmd.Env = append(cmd.Env, "GOMAXPROCS=12")
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
 		return nil, errors.EnsureStack(err)
