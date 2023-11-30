840434bdf3d5eb670a1bfa6be8b9f91aa093b0eb
Daniel Janicek <daniel.janicek@hpe.com>
2023-11-30 15:55:42 -0800

    Revert "code coverage format folder"
    This reverts commit 619cad69c986419010b2543a5c4f27241952acdb.

--- etc/testing/circle/format-coverage.sh
+++ etc/testing/circle/format-coverage.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
 
-cover_folder="${TEST_RESULTS}/cover" 
-find "$cover_folder" -name "*.tgz" -type f -exec tar xvf {} --transform="s/.*\///" -C "$TEST_RESULTS" \; # extract all covdata to test-results folder
-go tool covdata textfmt -i="$cover_folder" -o "$TEST_RESULTS/coverage.txt"
-find "$cover_folder" -name "*covcounters.*" -type f -exec rm {} \; # cleanup so we don't store tons of extra files in CI artifacts
-find "$cover_folder" -name "*covmeta.*" -type f -exec rm {} \; 
+ls "$TEST_RESULTS" 
+find "$TEST_RESULTS" -name "*.tgz" -type f -exec tar xvf {} --transform="s/.*\///" -C "$TEST_RESULTS" \; # extract all covdata to test-results folder
+go tool covdata textfmt -i="$TEST_RESULTS" -o "$TEST_RESULTS/coverage.txt"
+find "$TEST_RESULTS" -name "*covcounters.*" -type f -exec rm {} \; # cleanup so we don't store tons of extra files in CI artifacts
+find "$TEST_RESULTS" -name "*covmeta.*" -type f -exec rm {} \; 
