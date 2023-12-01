418968c3a7e8db63e4dda291abf841b8fbe308e6
Daniel Janicek <daniel.janicek@hpe.com>
2023-12-01 06:49:43 -0800

    Revert "check cover folder"
    This reverts commit d6227ab2c51b8011545ac35d015d72e6a1f25f33.

--- etc/testing/circle/format-coverage.sh
+++ etc/testing/circle/format-coverage.sh
@@ -3,8 +3,6 @@ set -euxo pipefail
 
 ls "$TEST_RESULTS" 
 find "$TEST_RESULTS" -name "*.tgz" -type f -exec tar xvf {} --transform="s/.*\///" -C "$TEST_RESULTS" \; # extract all covdata to test-results folder
-ls "$TEST_RESULTS"
-ls "$TEST_RESULTS/cover"
-go tool covdata textfmt -i="$TEST_RESULTS/cover" -o "$TEST_RESULTS/coverage.txt"
-find "$TEST_RESULTS/cover" -name "*covcounters.*" -type f -exec rm {} \; # cleanup so we don't store tons of extra files in CI artifacts
-find "$TEST_RESULTS/cover" -name "*covmeta.*" -type f -exec rm {} \; 
+go tool covdata textfmt -i="$TEST_RESULTS" -o "$TEST_RESULTS/coverage.txt"
+find "$TEST_RESULTS" -name "*covcounters.*" -type f -exec rm {} \; # cleanup so we don't store tons of extra files in CI artifacts
+find "$TEST_RESULTS" -name "*covmeta.*" -type f -exec rm {} \; 
