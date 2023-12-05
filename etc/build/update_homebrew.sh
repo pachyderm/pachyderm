0a616863864aaf4090de3f3ed9592bafebd694c9
molinamelendezj <molinamelendezj@gmail.com>
2023-12-05 15:43:55 -0500

    Update update_homebrew.sh

--- etc/build/update_homebrew.sh
+++ etc/build/update_homebrew.sh
@@ -19,31 +19,29 @@ git clone git@github.com:pachyderm/homebrew-tap
 
 
 # figure out highest major/minor pair currently released in homebrew
-pushd homebrew-tap
 [[ $MAJOR_MINOR =~ ([0-9]+)\.([0-9]+) ]];
 major="${BASH_REMATCH[1]}"
 minor="${BASH_REMATCH[2]}"
 for s in *.rb; do
     if [[ $s =~ ^pachctl@([0-9]+)\.([0-9]+)\.rb ]]; then
-	if [[ $major < "${BASH_REMATCH[1]}" ]]; then
+    if [[ $major < "${BASH_REMATCH[1]}" ]]; then
             major="${BASH_REMATCH[1]}";
-	    minor="${BASH_REMATCH[2]}";
-	elif [[ $major == "${BASH_REMATCH[1]}" && $minor < "${BASH_REMATCH[2]}" ]]; then
-	    major="${BASH_REMATCH[1]}";
-	    minor="${BASH_REMATCH[2]}";	 
-	fi;
+        minor="${BASH_REMATCH[2]}";
+    elif [[ $major == "${BASH_REMATCH[1]}" && $minor < "${BASH_REMATCH[2]}" ]]; then
+        major="${BASH_REMATCH[1]}";
+        minor="${BASH_REMATCH[2]}";	 
+    fi;
     fi;
 done;
 
-
-
+pushd homebrew-tap
     git checkout -b "$BRANCH" || git checkout "$BRANCH"
     VERSION=$version ./update-formula.sh
     git add "pachctl@$MAJOR_MINOR.rb"
     if [[ $stable == 1 ]]; then
 	if [[ "${major}.${minor}" == "$MAJOR_MINOR" ]]; then 
             cp "pachctl@$MAJOR_MINOR.rb" pachctl.rb
-            sed -i '' -E 's/class PachctlAT\([0-9]*\)/class Pachctl/g' pachctl.rb
+            sed -i -E 's/class PachctlAT\([0-9]*\)/class Pachctl/g' pachctl.rb
             git add pachctl.rb
 	fi;
     fi;
