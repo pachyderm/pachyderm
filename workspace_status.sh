f8594523832ac0316a832b08c59706e6f76f83dd
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2023-12-13 16:31:05 +0000

    stamp pachctl

--- workspace_status.sh
+++ workspace_status.sh
@@ -11,5 +11,8 @@ echo "GIT_BRANCH $git_branch"
 git_tree_status=$(git diff-index --quiet HEAD -- && echo 'Clean' || echo 'Modified')
 echo "GIT_TREE_STATUS $git_tree_status"
 
-most_recent_tag=$(git tag -l 'v*' --sort=creatordate | tail -n1)
-echo "STABLE_VERSION $most_recent_tag"
+# TODO(jrockway): Read these from the git tags when the branch is named "2.x".
+echo "STABLE_APP_VERSION 2.9.0"
+
+additional_version=$(git diff-index --quiet HEAD -- && echo "-${commit_sha}" || echo "-${commit_sha}+dirty")
+echo "STABLE_ADDITIONAL_VERSION $additional_version"
