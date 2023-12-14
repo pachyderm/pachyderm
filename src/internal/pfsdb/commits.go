0c2cd2e10d7de4f032b291d14136d136f8d6334d
Albert Cui <albert@pachyderm.io>
2023-12-14 14:03:29 -0500

    Better error checking

--- src/internal/pfsdb/commits.go
+++ src/internal/pfsdb/commits.go
@@ -196,12 +196,17 @@ func CreateCommit(ctx context.Context, tx *pachsql.Tx, commitInfo *pfs.CommitInf
 	if err := validateCommitInfo(commitInfo); err != nil {
 		return 0, err
 	}
-	commit, getCommitErr := GetCommitWithIDByKey(ctx, tx, commitInfo.Commit)
-	if getCommitErr == nil {
+	commit, err := GetCommitWithIDByKey(ctx, tx, commitInfo.Commit)
+	if err == nil {
 		return 0, &CommitAlreadyExistsError{CommitID: commit.CommitInfo.Commit.Key()}
 	}
-	if getCommitErr != nil && (errors.As(getCommitErr, new(*ProjectNotFoundError)) || errors.As(getCommitErr, new(*RepoNotFoundError))) {
-		return 0, getCommitErr
+	if err != nil {
+		if errors.As(err, new(*ProjectNotFoundError)) || errors.As(err, new(*RepoNotFoundError)) {
+			return 0, err
+		}
+		if !errors.As(err, new(*CommitNotFoundError)) {
+			return 0, err
+		}
 	}
 	opt := AncestryOpt{}
 	if len(opts) > 0 {
@@ -240,7 +245,7 @@ func CreateCommit(ctx context.Context, tx *pachsql.Tx, commitInfo *pfs.CommitInf
 		insert.FinishedTime, insert.CompactingTime, insert.ValidatingTime, insert.Size, insert.Error)
 	lastInsertId := 0
 	if err := row.Scan(&lastInsertId); err != nil {
-		return 0, errors.Wrap(errors.Join(getCommitErr, err), "scanning id from create commitInfo")
+		return 0, errors.Wrap(err, "scanning id from create commitInfo")
 	}
 	if commitInfo.ParentCommit != nil && !opt.SkipParent {
 		if err := CreateCommitParent(ctx, tx, commitInfo.ParentCommit, CommitID(lastInsertId)); err != nil {
