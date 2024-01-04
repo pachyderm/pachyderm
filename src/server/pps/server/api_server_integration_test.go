9f53b64d5d8a58d7995a4b30b7a88b1c1bf469bf
Robert A. Uhl <robert.uhl@hpe.com>
2024-01-04 09:42:29 -0500

    Add a test that sidecar metrics are properly served

--- src/server/pps/server/api_server_integration_test.go
+++ src/server/pps/server/api_server_integration_test.go
@@ -4,16 +4,20 @@ package server_test
 
 import (
 	"bytes"
+	"strings"
 	"testing"
+	"text/template"
+
+	"google.golang.org/protobuf/encoding/protojson"
+
+	"github.com/pachyderm/pachyderm/v2/src/pfs"
+	"github.com/pachyderm/pachyderm/v2/src/pps"
 
 	"github.com/pachyderm/pachyderm/v2/src/internal/client"
 	"github.com/pachyderm/pachyderm/v2/src/internal/minikubetestenv"
 	"github.com/pachyderm/pachyderm/v2/src/internal/pctx"
 	"github.com/pachyderm/pachyderm/v2/src/internal/require"
 	tu "github.com/pachyderm/pachyderm/v2/src/internal/testutil"
-	"github.com/pachyderm/pachyderm/v2/src/pfs"
-	"github.com/pachyderm/pachyderm/v2/src/pps"
-	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func TestRerunPipeline(t *testing.T) {
@@ -136,4 +140,95 @@ func TestRerunPipeline(t *testing.T) {
 	require.Equal(t, int64(0), jobInfo.DataSkipped)
 	require.Equal(t, int64(0), jobInfo.DataRecovered)
 	require.Equal(t, int64(0), jobInfo.DataFailed)
+}
+
+// TestSidecarMetrics tests that sidecar metrics are available.
+func TestSidecarMetrics(t *testing.T) {
+	ctx := pctx.TestContext(t)
+	t.Parallel()
+	c, namespace := minikubetestenv.AcquireCluster(t)
+
+	inputRepo := "input"
+	require.NoError(t, c.CreateRepo(pfs.DefaultProjectName, inputRepo))
+
+	var pipelineTemplate1 = `{
+		"pipeline": {
+			"project": {
+				"name": "{{.ProjectName | js}}"
+			},
+			"name": "{{.PipelineName | js}}"
+		},
+		"transform": {
+			"cmd": ["cp", "-r", "/pfs/in/foo", "/pfs/out"]
+		},
+		"input": {
+			"pfs": {
+				"project": "default",
+				"repo": "{{.RepoName | js}}",
+				"glob": "/*",
+				"name": "in"
+			}
+		},
+		"autoscaling": false
+	}`
+	tmpl1, err := template.New("pipeline").Parse(pipelineTemplate1)
+	require.NoError(t, err, "template must parse")
+	var buf bytes.Buffer
+	require.NoError(t, tmpl1.Execute(&buf, struct {
+		ProjectName, PipelineName, RepoName string
+	}{pfs.DefaultProjectName, "pipeline1", inputRepo}), "template must execute")
+	_, err = c.PpsAPIClient.CreatePipelineV2(ctx, &pps.CreatePipelineV2Request{
+		CreatePipelineRequestJson: buf.String(),
+	})
+	require.NoError(t, err, "creating pipeline1 must succeed")
+
+	// pipeline2 pulls metrics from pipeline1
+	var pipelineTemplate2 = `{
+		"pipeline": {
+			"project": {
+				"name": "{{.ProjectName | js}}"
+			},
+			"name": "{{.PipelineName | js}}"
+		},
+		"transform": {
+			"image": "curlimages/curl:8.5.0",
+			"cmd": ["sh", "-c", "curl http://default-pipeline1-v1.{{.Namespace}}.svc.cluster.local:9091/metrics > /pfs/out/output"]
+		},
+		"input": {
+			"pfs": {
+				"project": "default",
+				"repo": "{{.RepoName | js}}",
+				"glob": "/*",
+				"name": "in"
+			}
+		},
+		"autoscaling": false
+	}`
+	tmpl2, err := template.New("pipeline").Parse(pipelineTemplate2)
+	require.NoError(t, err, "template must parse")
+	var buf2 bytes.Buffer
+	require.NoError(t, tmpl2.Execute(&buf2, struct {
+		ProjectName, PipelineName, RepoName, Namespace string
+	}{pfs.DefaultProjectName, "pipeline2", "pipeline1", namespace}), "template must execute")
+	_, err = c.PpsAPIClient.CreatePipelineV2(ctx, &pps.CreatePipelineV2Request{
+		CreatePipelineRequestJson: buf2.String(),
+	})
+	require.NoError(t, err, "creating pipeline2 must succeed")
+
+	// put a file into the input repo, which should trigger pipeline1, whose completion should trigger pipeline2
+	commit1, err := c.StartCommit(pfs.DefaultProjectName, inputRepo, "master")
+	require.NoError(t, err)
+	require.NoError(t, c.PutFile(commit1, "/foo", strings.NewReader("foo")))
+	require.NoError(t, c.FinishCommit(pfs.DefaultProjectName, inputRepo, "master", commit1.Id))
+
+	_, err = c.WaitCommitSetAll(commit1.Id)
+	require.NoError(t, err, "commit set must finish")
+
+	branch, err := c.InspectBranch(pfs.DefaultProjectName, "pipeline2", "master")
+
+	var buf3 bytes.Buffer
+	err = c.GetFile(branch.Head, "/output", &buf3)
+	require.NoError(t, err, "get file must succeed")
+	// this is a bit brute force, but it does work
+	require.True(t, strings.Contains(buf3.String(), "\npachyderm_pachd_pps"))
 }
