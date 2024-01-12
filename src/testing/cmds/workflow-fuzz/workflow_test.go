442b1e18a50aa6358200ba59a20cac4031de6798
Daniel Janicek <daniel.janicek@hpe.com>
2024-01-12 15:47:22 -0800

    technically a successful pipeline create

--- src/testing/cmds/workflow-fuzz/workflow_test.go
+++ src/testing/cmds/workflow-fuzz/workflow_test.go
@@ -8,7 +8,6 @@ import (
 	"fmt"
 	"math/rand"
 	"path"
-	"regexp"
 	"slices"
 	"strings"
 	"testing"
@@ -28,37 +27,85 @@ import (
 
 const (
 	namespace            = "fuzz-cluster-1"
-	useSucccessValWeight = .7
+	useSucccessValWeight = .8
 	maxRepeatedCount     = 10
-	maxProtoDepth        = 50
 	// usNilWeight          = .2
 )
 
 var (
-	run               = flag.Bool("testgen.run", true, "be default, don't run workflow data generation") // DNJ TODO - just hide in test harness? set back to false at least
-	alphaNumericRegex = regexp.MustCompile(`^[a-zA-Z0-9]*$`)
-	successInputs     = map[string][]protoreflect.Value{}
+	runDefault    = flag.Bool("gen.default", true, "use the default data generators") // DNJ TODO - just hide in test harness? set back to false at least
+	successInputs = map[string][]protoreflect.Value{}
 )
 
 var protosUnderTest = map[protoreflect.FileDescriptor][]string{ // DNJ TODO - make own rangeRPC function from rpc_fuzz_test? at least move list of apis to separate input file(s)
 	pfs.File_pfs_pfs_proto: {
 		pfs.API_CreateRepo_FullMethodName,
 		pfs.API_CreateProject_FullMethodName,
-		pfs.API_DeleteRepo_FullMethodName,
-		pfs.API_DeleteProject_FullMethodName, // DNJ TODO - weighting?
-		pfs.API_InspectProjectV2_FullMethodName,
-		pfs.API_InspectRepo_FullMethodName,
-		pfs.API_ListRepo_FullMethodName,
-		pfs.API_ListBranch_FullMethodName,
+		// pfs.API_DeleteRepo_FullMethodName,
+		// pfs.API_DeleteProject_FullMethodName, // DNJ TODO - weighting?
+		// pfs.API_InspectProjectV2_FullMethodName,
+		// pfs.API_InspectRepo_FullMethodName,
+		// pfs.API_ListRepo_FullMethodName,
+		// pfs.API_ListBranch_FullMethodName,
 	},
 	pps.File_pps_pps_proto: {
 		pps.API_CreatePipeline_FullMethodName,
 	},
 }
 
-type genData struct { // DNJ TODO - awful name
-	r     *rand.Rand
-	field protoreflect.FieldDescriptor
+type GeneratorIndex struct {
+	KindGenerators map[protoreflect.Kind]Generator
+	NameGenerators map[string]Generator
+}
+
+type generatorSource struct { // DNJ TODO - awful name
+	r        *rand.Rand
+	field    protoreflect.FieldDescriptor
+	ancestry []string
+	index    GeneratorIndex
+}
+
+type Generator func(generatorSource) protoreflect.Value
+
+func subinput(genSource generatorSource) protoreflect.Value {
+	return protoreflect.ValueOf(nil) // DNJ TODO allow 1-2 layers
+}
+
+func noValue(genSource generatorSource) protoreflect.Value {
+	return protoreflect.ValueOf(nil)
+}
+
+func repoType(genSource generatorSource) protoreflect.Value {
+	return protoreflect.ValueOf("user")
+}
+
+func serviceType(genSource generatorSource) protoreflect.Value {
+	return protoreflect.ValueOf("NodePort")
+}
+
+func randInt64Positive(genSource generatorSource) protoreflect.Value { // DNJ  TODO parametrize and return function?
+	return protoreflect.ValueOf(genSource.r.Int63n(2000))
+}
+
+func randNanos(genSource generatorSource) protoreflect.Value {
+	return protoreflect.ValueOf(genSource.r.Int31n(2000))
+}
+
+func randJson(genSource generatorSource) protoreflect.Value {
+	return protoreflect.ValueOf("") // DNJ TODO - random json generator
+}
+
+func randReproccessSpec(genSource generatorSource) protoreflect.Value {
+	if genSource.r.Float32() < .5 {
+		return protoreflect.ValueOf("until_success")
+	}
+	return protoreflect.ValueOf("every_job")
+}
+
+func randTolerationOperator(genSource generatorSource) protoreflect.Value {
+	enum := genSource.field.Enum().Values()
+	selected := genSource.r.Intn(enum.Len()-1) + 1 // 0 is invalid for create pipeline
+	return protoreflect.ValueOf(enum.Get(selected).Number())
 }
 
 // DNJ TODO - rpc fuzz test
@@ -110,32 +157,62 @@ func randString(r *rand.Rand, minlength int, maxLength int) string {
 	var characters strings.Builder
 	length := r.Intn(maxLength) + minlength
 	for i := 0; i < length; i++ {
-		characters.WriteByte(byte(r.Intn(127)))
+		characters.WriteByte(byte(32 + r.Intn(94))) // printable ascii
 	}
 	return characters.String()
 }
 
-type generator func(genData) protoreflect.Value
-
-func inputGenerator(ctx context.Context, r *rand.Rand, msgDesc protoreflect.MessageDescriptor, potentialInputs map[string][]protoreflect.Value) *dynamicpb.Message { // DNJ TODO - generics maybe?
+func inputGenerator(ctx context.Context, r *rand.Rand, msgDesc protoreflect.MessageDescriptor, potentialInputs map[string][]protoreflect.Value, prevSource generatorSource) *dynamicpb.Message { // DNJ TODO - generics maybe?
 	msg := dynamicpb.NewMessage(msgDesc)
-	// DNJ TODO Inject and save successful field values
 	// DNJ TODO type enum?
-	defaultGenerators := map[protoreflect.Kind]generator{ // DNJ TODO - full custom inputs, how to handle for specific fields? Maybe do my own hash with field name or have a field name map of generators that is checlked first?
-		protoreflect.BoolKind: func(gen genData) protoreflect.Value {
-			return protoreflect.ValueOf(gen.r.Float32() < .5)
-		}, // DNJ TODO - Generics and/or pass context for error?
-		protoreflect.StringKind: func(gen genData) protoreflect.Value {
-			return protoreflect.ValueOf(randString(gen.r, 0, 5))
+	defaultGenerators := GeneratorIndex{ // DNJ TODO - full custom inputs, how to handle for specific fields? Maybe do my own hash with field name or have a field name map of generators that is checlked first?
+		KindGenerators: map[protoreflect.Kind]Generator{
+			protoreflect.BoolKind: func(gen generatorSource) protoreflect.Value {
+				return protoreflect.ValueOf(gen.r.Float32() < .5)
+			}, // DNJ TODO - Generics and/or pass context for error?
+			protoreflect.StringKind: func(gen generatorSource) protoreflect.Value {
+				return protoreflect.ValueOf(randString(gen.r, 1, 5))
+			},
+			protoreflect.Int64Kind: func(gen generatorSource) protoreflect.Value {
+				return protoreflect.ValueOf(gen.r.Int63n(20) - 10)
+			},
+			protoreflect.Int32Kind: func(gen generatorSource) protoreflect.Value {
+				return protoreflect.ValueOf(gen.r.Int31n(20) - 10)
+			},
+			protoreflect.Uint32Kind: func(gen generatorSource) protoreflect.Value {
+				return protoreflect.ValueOf(gen.r.Uint32())
+			},
+			protoreflect.FloatKind: func(gen generatorSource) protoreflect.Value {
+				return protoreflect.ValueOf(gen.r.Float32()*2 - 1)
+			},
+			protoreflect.EnumKind: func(gen generatorSource) protoreflect.Value {
+				enum := gen.field.Enum().Values()
+				selected := gen.r.Intn(enum.Len())
+				return protoreflect.ValueOf(enum.Get(selected).Number())
+			},
+			protoreflect.MessageKind: func(gen generatorSource) protoreflect.Value {
+				return protoreflect.ValueOf(inputGenerator(ctx, gen.r, gen.field.Message(), potentialInputs, gen)) // DNJ TODO - this depth lineage is too confusing
+			},
 		},
-		protoreflect.Int64Kind: func(gen genData) protoreflect.Value {
-			return protoreflect.ValueOf(gen.r.Int63n(20) - 10)
-		},
-		protoreflect.FloatKind: func(gen genData) protoreflect.Value {
-			return protoreflect.ValueOf(gen.r.Float32()*2 - 1)
-		},
-		protoreflect.MessageKind: func(gen genData) protoreflect.Value {
-			return protoreflect.ValueOf(inputGenerator(ctx, gen.r, gen.field.Message(), potentialInputs))
+		NameGenerators: map[string]Generator{
+			"pps_v2.Input.join":                           subinput,
+			"pps_v2.Input.cross":                          subinput,
+			"pps_v2.Input.union":                          subinput,
+			"pps_v2.Input.group":                          subinput,
+			"google.protobuf.Int64Value.value":            randInt64Positive, // DNJ TODO - read ancestry to decide?
+			"google.protobuf.Duration.seconds":            randInt64Positive,
+			"google.protobuf.Duration.nanos":              randNanos,
+			"google.protobuf.Timestamp.seconds":           randInt64Positive,
+			"google.protobuf.Timestamp.nanos":             randNanos,
+			"pps_v2.CreatePipelineRequest.tf_job":         noValue,
+			"pps_v2.CreatePipelineRequest.reprocess_spec": randReproccessSpec,
+			"pps_v2.DatumSetSpec.size_bytes":              randInt64Positive,
+			"pps_v2.DatumSetSpec.number":                  randInt64Positive,
+			"pps_v2.Toleration.operator":                  randTolerationOperator,
+			"pps_v2.CreatePipelineRequest.pod_spec":       randJson,
+			"pps_v2.CreatePipelineRequest.pod_patch":      randJson,
+			"pfs_v2.Repo.type":                            repoType,
+			"pps_v2.Service.type":                         serviceType, //could not marshal CreatePipelineRequest to JSON
 		},
 	}
 	for i := 0; i < msgDesc.Fields().Len(); i++ { // DNJ TODO fuzz each type and recurse
@@ -145,14 +222,13 @@ func inputGenerator(ctx context.Context, r *rand.Rand, msgDesc protoreflect.Mess
 			r.Float32() < useSucccessValWeight {
 			fieldVal = successVals[r.Intn(len(successVals))]
 		} else {
-			gen := genData{r: r, field: field}                         // DNJ TODO gen is a bad name.
-			if string(field.Name()) == "type" && field.Enum() == nil { // DNJ TODO - make type an enum? handle more generically with generators? This cannot be in final code...
-				fieldVal = protoreflect.ValueOf("user")
-			} else if field.IsList() { // fall-back to normal assignment if 1 or fewer selected
+			genSource := generatorSource{r: r, field: field, index: defaultGenerators}
+			genSource.ancestry = append(prevSource.ancestry, string(field.Name()))
+			if field.IsList() { // fall-back to normal assignment if 1 or fewer selected
 				count := r.Intn(maxRepeatedCount)
 				vals := msg.NewField(field).List()
 				for i := 0; i < count; i++ {
-					v := getDataForType(ctx, gen, defaultGenerators)
+					v := getValueForField(ctx, genSource)
 					if v.IsValid() {
 						vals.Append(v)
 					} else {
@@ -166,27 +242,34 @@ func inputGenerator(ctx context.Context, r *rand.Rand, msgDesc protoreflect.Mess
 				log.Info(ctx, "Map message type not yet implemented in value generation",
 					zap.Any("field", field.FullName()))
 			} else {
-				fieldVal = getDataForType(ctx, gen, defaultGenerators)
+				fieldVal = getValueForField(ctx, genSource)
 			}
 		}
+		if string(field.Name()) == "operator" {
+			log.Info(ctx, "DNJ TODO Field for input what?!",
+				zap.Any("field", field.FullName()),
+				zap.Any("fieldVal", fieldVal.Interface()))
+		}
 		if fieldVal.IsValid() {
-			// log.Info(ctx, "Setting field val for input",
-			// 	zap.Any("field", field.FullName()),
-			// 	zap.Any("fieldVal", fieldVal.Interface()))
 			msg.Set(field, fieldVal)
 		}
 	}
 	return msg
 }
 
-func getDataForType(ctx context.Context, gen genData, generators map[protoreflect.Kind]generator) protoreflect.Value {
-	genFunc, ok := generators[gen.field.Kind()]
-	if ok {
-		return genFunc(gen)
+func getValueForField(ctx context.Context, genSource generatorSource) protoreflect.Value {
+	if nameGenFunc, ok := genSource.index.NameGenerators[string(genSource.field.FullName())]; ok {
+		return nameGenFunc(genSource)
+	} else if kindGenFunc, ok := genSource.index.KindGenerators[genSource.field.Kind()]; ok {
+		log.Info(ctx, "DNJ TODO - field kind log",
+			zap.Any("field", genSource.field.FullName()),
+			zap.Any("field", genSource.field.Kind()),
+		)
+		return kindGenFunc(genSource)
 	} else {
-		log.Info(ctx, "Input generator not provided for requested data type.",
-			zap.Any("field", gen.field.FullName()),
-			zap.Any("data type", gen.field.Kind()))
+		log.Info(ctx, "Input generator not provided for requested field.",
+			zap.Any("field", genSource.field.FullName()),
+			zap.Any("data type", genSource.field.Kind()))
 		return protoreflect.ValueOf(nil)
 	}
 }
@@ -229,7 +312,7 @@ func storeSuccessVals(ctx context.Context, msg *dynamicpb.Message, out map[strin
 func fuzzGrpc(ctx context.Context, c *client.APIClient, seed int64) { // DNJ TODO - can and/or should this be public?
 	r := rand.New(rand.NewSource(seed)) // DNJ TODO - accept intrface that Rand stisifies to allow custom value sourcing? move successvals logic there! needs Float32 and Intn right now
 	rangeRPCsList(protosUnderTest, func(fd protoreflect.FileDescriptor, sd protoreflect.ServiceDescriptor, md protoreflect.MethodDescriptor) {
-		input := inputGenerator(ctx, r, md.Input(), successInputs)
+		input := inputGenerator(ctx, r, md.Input(), successInputs, generatorSource{})
 		reply := dynamicpb.NewMessage(md.Output())
 		fullName := "/" + path.Join(string(sd.FullName()), string(md.Name())) // DNJ TODO move to rpc fuzz test and make function for this.
 		if err := c.ClientConn().Invoke(ctx, fullName, input, reply); err != nil {
@@ -253,7 +336,7 @@ func fuzzGrpc(ctx context.Context, c *client.APIClient, seed int64) { // DNJ TOD
 
 // DNJ TODO context and logger?
 func FuzzGrpcWorkflow(f *testing.F) { // DNJ TODO - better name
-	if !*run {
+	if !*runDefault {
 		f.Skip()
 	}
 	// We need a file logger since Go runs Fuzz in seperate procs
