dd48645847f4ec35072d02820ea92907035a67ed
Logan <31484824+loganripplinger@users.noreply.github.com>
2024-01-17 08:47:08 -0800

    fix: Do not throw unfinished error to error boundary (#1848)

--- frontend/src/hooks/useDatums.ts
+++ frontend/src/hooks/useDatums.ts
@@ -1,6 +1,7 @@
 import {useQuery} from '@tanstack/react-query';
 
 import {ListDatumRequest, listDatumPaged} from '@dash-frontend/api/pps';
+import {isErrorWithMessage, isUnknown} from '@dash-frontend/api/utils/error';
 import getErrorMessage from '@dash-frontend/lib/getErrorMessage';
 import queryKeys from '@dash-frontend/lib/queryKeys';
 
@@ -20,6 +21,14 @@ export const useDatumsPaged = (req: ListDatumRequest, enabled = true) => {
       }),
     ],
     queryFn: () => listDatumPaged(req),
+    throwOnError: (e) =>
+      // This can trigger if you try to listDatum right after you create a pipeline
+      !(
+        isUnknown(e) &&
+        isErrorWithMessage(e) &&
+        e?.message.includes('base commit ') &&
+        e?.message.includes(' unfinished')
+      ),
     enabled,
     refetchInterval: false,
   });
