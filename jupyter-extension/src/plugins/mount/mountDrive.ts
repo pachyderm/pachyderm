eec4ef851a7de11c05721e05a58007b91587b4ea
Emma Adams <emma.adams@hpe.com>
2024-01-19 18:16:29 +0000

    Update mountDrive to support grabbing all entities

--- jupyter-extension/src/plugins/mount/mountDrive.ts
+++ jupyter-extension/src/plugins/mount/mountDrive.ts
@@ -23,8 +23,10 @@ const DEFAULT_CONTENT_MODEL: Contents.IModel = {
 };
 
 export class MountDrive implements Contents.IDrive {
-  public _registry: DocumentRegistry;
+  _registry: DocumentRegistry;
+  modelDBFactory?: ModelDB.IFactory | undefined;
   readonly _model: Paging.IModel;
+
   private _fileChanged = new Signal<this, Contents.IChangedArgs>(this);
   private _loading = new Signal<this, boolean>(this);
   private _serverSettings = ServerConnection.makeSettings();
@@ -32,14 +34,15 @@ export class MountDrive implements Contents.IDrive {
   private _cache: {key: string | null; contents: any};
   private _path: string;
   private _name_suffix: string;
-  modelDBFactory?: ModelDB.IFactory | undefined;
+  private _refreshFileBrowser: () => Promise<void>; // A function that refresh the file browser 
 
-  constructor(registry: DocumentRegistry, path: string, name_suffix: string) {
+  constructor(registry: DocumentRegistry, path: string, name_suffix: string, refreshFileBrowser: () => Promise<void>) {
     this._registry = registry;
     this._model = {page: 0, max_page: 0};
     this._cache = {key: null, contents: DEFAULT_CONTENT_MODEL};
     this._path = path;
     this._name_suffix = name_suffix;
+    this._refreshFileBrowser = refreshFileBrowser
   }
 
   get name(): string {
@@ -105,13 +108,19 @@ export class MountDrive implements Contents.IDrive {
     // If we don't have contents cached, then we fetch them and cache the results.
     if (localPath !== this._cache.key || !localPath) {
       this._loading.emit(true);
-      const response = await this._get(url, {...options, content});
+      const getOptions = {
+        ...options,
+        number: MAX_NUM_CONTENTS_PAGE,
+        content,
+      };
+      const response = await this._get(url, getOptions);
       this._loading.emit(false);
       this._model.page = 1;
       this._model.max_page = Math.ceil(
         response.content.length / MAX_NUM_CONTENTS_PAGE,
       );
       this._cache = {key: localPath, contents: response.content};
+      this._fetchNextPage(response, localPath, url, getOptions);
     }
 
     return {
@@ -121,6 +130,41 @@ export class MountDrive implements Contents.IDrive {
         this._model.page * MAX_NUM_CONTENTS_PAGE,
       ),
     };
+  }
+
+  _fetchNextPage(previousResponse: Contents.IModel, key: string, url: string, getOptions: PartialJSONObject): void {
+    // Stop fetching pages if we recieve a page less than expected file count of a page
+    if (previousResponse?.content?.length < MAX_NUM_CONTENTS_PAGE) {
+      return;
+    }
+
+    // Queuing a promise without returning it to await lets us start the process of fetching the next pages in the background while
+    // continuing to render the the first page.
+    (async () => {
+      const nextResponse: Contents.IModel = await new Promise((resolve, reject) => {
+        return this._get(url, {
+          ...getOptions,
+          pagination_marker: previousResponse.content.slice(-1)[0].file_uri
+        })
+          .then((nextResponse) => resolve(nextResponse))
+          .catch((e) => reject(e))
+      })
+
+      // Check to make sure that the cache file path has not changed so that we stop updating the contents and requesting
+      // new pages
+      if (this._cache.key !== key) {
+        return
+      }
+
+      // Update the cache contents and refresh the FileBrowser. Note this will not cause any changes in the current scroll position or file selection.
+      // It will just add new pages and update the page selector without disrupting the user.
+      this._cache.contents = this._cache.contents.concat(nextResponse.content);
+      this._model.max_page = Math.ceil(
+        this._cache.contents.length / MAX_NUM_CONTENTS_PAGE,
+      );
+      await this._refreshFileBrowser()
+      this._fetchNextPage(nextResponse, key, url, getOptions)
+    })();
   }
 
   getDownloadUrl(localPath: string): Promise<string> {
