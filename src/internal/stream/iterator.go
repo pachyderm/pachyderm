0b4bf24dc16e872bdba67c8f1e26c327fa9672d9
Sahith Malyala <sahith.malyala@hpe.com>
2024-02-06 11:48:25 -0500

    Stop after reaching max

--- src/internal/stream/iterator.go
+++ src/internal/stream/iterator.go
@@ -80,8 +80,8 @@ func Read[T any](ctx context.Context, it Iterator[T], buf []T) (n int, _ error) 
 // Collect reads at most max from the iterator into a buffer and returns it.
 func Collect[T any](ctx context.Context, it Iterator[T], max int) (ret []T, _ error) {
 	for {
-		if len(ret) > max {
-			return nil, errors.Errorf("stream.Collect: iterator produced too many elements. max=%d", max)
+		if len(ret) == max {
+			break
 		}
 		if err := appendNext(ctx, it, &ret); err != nil {
 			if IsEOS(err) {
