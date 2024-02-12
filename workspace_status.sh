1828ed42e418d345beaf0d797b89895b28e1b6ff
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2024-02-12 20:11:07 +0000

    adjust workspace_status to respect tags

--- workspace_status.sh
+++ workspace_status.sh
@@ -11,14 +11,11 @@ echo "GIT_BRANCH $git_branch"
 git_tree_status=$(git diff-index --quiet HEAD -- && echo 'Clean' || echo 'Modified')
 echo "GIT_TREE_STATUS $git_tree_status"
 
-most_recent_tag=$(git tag -l 'v*' --sort=creatordate | tail -n1)
-echo "STABLE_VERSION $most_recent_tag"
+exact=$(git describe --exact-match 2>/dev/null | sed -e s/^v// | cut -d - -f 1 || echo 2.10.0)
+echo "STABLE_APP_VERSION $exact"
 
-# TODO(jrockway): Read these from the git tags when the branch is named "2.x".
-echo "STABLE_APP_VERSION 2.9.0"
-
-additional_version=$(git diff-index --quiet HEAD -- && echo "-${commit_sha}" || echo "-${commit_sha}+dirty")
-echo "STABLE_ADDITIONAL_VERSION $additional_version"
+additional_version=$(git describe --exact-match 2>/dev/null | cut -d - -f 2- || echo "pre.$(git describe --long --dirty=x | rev | cut -d - -f 1 | rev)")
+echo "STABLE_ADDITIONAL_VERSION -$additional_version"
 
 ci_runner_image_version="$(date +%Y%m%d)-${commit_sha}"
 echo "STABLE_CI_RUNNER_IMAGE_VERSION ${ci_runner_image_version}"
