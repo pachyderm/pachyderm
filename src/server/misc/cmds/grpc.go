ecab0959e51807d82701d74c8c4522c3f1f2bd65
Daniel Janicek <daniel.janicek@hpe.com>
2024-02-14 13:09:16 -0800

    revert now uneeded changes

--- src/server/misc/cmds/grpc.go
+++ src/server/misc/cmds/grpc.go
@@ -44,10 +44,6 @@ type gRPCParams struct {
 	Headers []string
 }
 
-func GrpcFromAddress(address string) gRPCParams {
-	return gRPCParams{Address: address}
-}
-
 func (p gRPCParams) Run(ctx context.Context, pachctlCfg *pachctl.Config, w io.Writer, args []string) error {
 	handlers := buildRPCs(
 		admin.File_admin_admin_proto,
@@ -72,7 +68,7 @@ func (p gRPCParams) Run(ctx context.Context, pachctlCfg *pachctl.Config, w io.Wr
 		methods := maps.Keys(handlers)
 		sort.Strings(methods)
 		for _, m := range methods {
-			fmt.Fprint(w, m)
+			fmt.Println(m)
 		}
 		return nil
 	}
