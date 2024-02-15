36a155123b16908db8f834a707852919a9cf7fb5
Robert A. Uhl <robert.uhl@hpe.com>
2024-02-15 10:01:41 -0500

    Add an incremental test

--- src/server/worker/datum/datum_benchmark_test.go
+++ src/server/worker/datum/datum_benchmark_test.go
@@ -13,18 +13,49 @@ import (
 	"github.com/pachyderm/pachyderm/v2/src/pps"
 )
 
-func BenchmarkCreate(b *testing.B) {
+func BenchmarkPipeline(b *testing.B) {
 	c, _ := minikubetestenv.AcquireCluster(b)
 
 	b.ResetTimer()
 	require.NoError(b, c.CreateRepo(pfs.DefaultProjectName, "input"))
 	commit, err := c.StartCommit(pfs.DefaultProjectName, "input", "master")
+	require.NoError(b, err, "should be able to create commit")
 	for i := 0; i < b.N; i++ {
-		require.NoError(b, err, "should be able to create commit")
-		for j := 0; j < 1000; j++ {
+		for j := 0; j < 125; j++ {
 			require.NoError(b, c.PutFile(commit, fmt.Sprintf("/file%d-%d", i, j), strings.NewReader("test content here")))
 		}
 	}
 	require.NoError(b, c.CreatePipeline(pfs.DefaultProjectName, "first", "", []string{"/bin/bash"}, []string{"cp /pfs/input/* /pfs/out"}, &pps.ParallelismSpec{Constant: 8}, &pps.Input{Pfs: &pps.PFSInput{Glob: "/*", Repo: "input"}}, "master", false))
 	require.NoError(b, c.FinishCommit(pfs.DefaultProjectName, "input", "master", commit.Id))
+}
+
+func BenchmarkIncrementalPipeline(b *testing.B) {
+	c, _ := minikubetestenv.AcquireCluster(b)
+	require.NoError(b, c.CreateRepo(pfs.DefaultProjectName, "input"))
+	commit, err := c.StartCommit(pfs.DefaultProjectName, "input", "master")
+	require.NoError(b, err, "should be able to create commit")
+	txn, err := c.StartTransaction()
+	require.NoError(b, err, "should be able to create transaction")
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < 125; j++ {
+			require.NoError(b, c.PutFile(commit, fmt.Sprintf("/file%d-%d", i, j), strings.NewReader("new test content here")))
+		}
+	}
+	require.NoError(b, c.CreatePipeline(pfs.DefaultProjectName, "first", "", []string{"/bin/bash"}, []string{"cp /pfs/input/* /pfs/out"}, &pps.ParallelismSpec{Constant: 8}, &pps.Input{Pfs: &pps.PFSInput{Glob: "/*", Repo: "input"}}, "master", false))
+	require.NoError(b, c.FinishCommit(pfs.DefaultProjectName, "input", "master", commit.Id))
+	_, err = c.FinishTransaction(txn)
+	require.NoError(b, err, "should finish transaction")
+
+	b.ResetTimer()
+
+	commit, err = c.StartCommit(pfs.DefaultProjectName, "input", "master")
+	require.NoError(b, err, "should be able to create commit")
+	txn, err = c.StartTransaction()
+	require.NoError(b, err, "should be able to create transaction")
+	for i := 0; i < b.N; i++ {
+		require.NoError(b, c.PutFile(commit, fmt.Sprintf("/file%d-%d", i, i), strings.NewReader("incremental test content here")))
+	}
+	require.NoError(b, c.FinishCommit(pfs.DefaultProjectName, "input", "master", commit.Id))
+	_, err = c.FinishTransaction(txn)
+	require.NoError(b, err, "should finish transaction")
 }
