316d6cb24c706c2897ad7b588a6e23c5db917415
Emma Adams <emma.adams@hpe.com>
2024-03-07 17:40:54 +0000

    Fail on console Change eslint warn errors to to errors that fail

--- jupyter-extension/scripts/jest.setup.js
+++ jupyter-extension/scripts/jest.setup.js
@@ -1,42 +1,41 @@
-const originalError = console.error.bind(console.error)
+import failOnConsole from 'jest-fail-on-console'
+
 const ignoredErrors = [
   // I am not exactly sure what causes this error to occur. This typically occurs when findByTestId is not properly awaited which is not happening.
   // The tests seem fine and pass. I think we should revisit this error once we can update all the testing libraries, but for now it can be silenced.
-  (msg, optionalParams) => msg.includes('Warning: An update to %s inside a test was not wrapped in act(...)') && optionalParams[0] === 'Datum'
+  (msg) => msg.includes('Warning: An update to Datum inside a test was not wrapped in act(...)')
 ]
-const originalWarn = console.warn.bind(console.warn)
 const ignoredWarnings = [
-    // For some reason JupyterLab's own LabIcons from @jupyterlab/ui-components complain they are malformed. This issue is fixed in future versions
-    // of the library, but for now can be safely ignored.
-    (msg, optionalParams) => msg.includes('SVG HTML was malformed for LabIcon instance.') && msg.includes('name: ui-components:caret-up'),
-    (msg, optionalParams) => msg.includes('SVG HTML was malformed for LabIcon instance.') && msg.includes('name: ui-components:caret-down'),
-    (msg, optionalParams) => msg.includes('SVG HTML was malformed for LabIcon instance.') && msg.includes('name: ui-components:caret-right'),
-    (msg, optionalParams) => msg.includes('SVG HTML was malformed for LabIcon instance.') && msg.includes('name: ui-components:caret-left'),
-    (msg, optionalParams) => msg.includes('SVG HTML was malformed for LabIcon instance.') && msg.includes('name: ui-components:close'),
-    (msg, optionalParams) => msg.includes('SVG HTML was malformed for LabIcon instance.') && msg.includes('name: ui-components:folder'),
-    (msg, optionalParams) => msg.includes('SVG HTML was malformed for LabIcon instance.') && msg.includes('name: ui-components:ellipses')
+  // For some reason JupyterLab's own LabIcons from @jupyterlab/ui-components complain they are malformed. This issue is fixed in future versions
+  // of the library, but for now can be safely ignored.
+  (msg) => msg.includes('SVG HTML was malformed for LabIcon instance.') && msg.includes('name: ui-components:caret-up'),
+  (msg) => msg.includes('SVG HTML was malformed for LabIcon instance.') && msg.includes('name: ui-components:caret-down'),
+  (msg) => msg.includes('SVG HTML was malformed for LabIcon instance.') && msg.includes('name: ui-components:caret-right'),
+  (msg) => msg.includes('SVG HTML was malformed for LabIcon instance.') && msg.includes('name: ui-components:caret-left'),
+  (msg) => msg.includes('SVG HTML was malformed for LabIcon instance.') && msg.includes('name: ui-components:close'),
+  (msg) => msg.includes('SVG HTML was malformed for LabIcon instance.') && msg.includes('name: ui-components:folder'),
+  (msg) => msg.includes('SVG HTML was malformed for LabIcon instance.') && msg.includes('name: ui-components:ellipses')
 ]
 
+failOnConsole({
+  shouldFailOnWarn: true,
+  silenceMessage: (message, methodName) => {
+    if (methodName === 'error') {
+      for (const ignoredError of ignoredErrors) {
+        if (ignoredError(message)) {
+            return true;
+        }
+      }
+    }
 
-beforeAll(() => {
-  console.error = (msg, ...optionalParams) => {
-    for (const ignoredError of ignoredErrors) {
-        if (ignoredError(msg, optionalParams)) {
-            return;
+    if (methodName === 'warn') {
+      for (const ignoredWarning of ignoredWarnings) {
+        if (ignoredWarning(message)) {
+            return true;
         }
+      }
     }
-    originalError(msg)
-  }
-  console.warn = (msg, ...optionalParams) => {
-    for (const ignoredWarning of ignoredWarnings) {
-        if (ignoredWarning(msg, optionalParams)) {
-            return;
-        }
-    }
-    originalWarn(msg)
-  }
-})
-afterAll(() => {
-  console.warn = originalWarn
-  console.error = originalError
+
+    return false;
+  },
 })
