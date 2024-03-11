33fb87633019c0967e30d206a7e6afaa4c1e41da
Emma Adams <emma.adams@hpe.com>
2024-03-11 23:27:26 +0000

    Remove unnecessary import

--- jupyter-extension/src/utils/components/DropdownCombobox/__tests__/DropdownCombobox.test.tsx
+++ jupyter-extension/src/utils/components/DropdownCombobox/__tests__/DropdownCombobox.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render, fireEvent} from '@testing-library/react';
 
-import DropdownCombobox, {DropdownComboboxProps} from '../DropdownCombobox';
+import DropdownCombobox from '../DropdownCombobox';
 
 describe('DropdownCombobox', () => {
   it('should render placeholder', async () => {
