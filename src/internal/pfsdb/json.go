6ee291917e6f492c0c8405f69149404d1739d379
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2024-03-14 16:58:44 +0000

    spellcheck

--- src/internal/pfsdb/json.go
+++ src/internal/pfsdb/json.go
@@ -29,10 +29,10 @@ func (c *jsonMap) Scan(src any) error {
 	// migration) and add data that won't unmarshal into that type.  Then all getters on the
 	// data with this column would hard-fail and be unfixable without manually editing the
 	// database.  We would rather not fail hard under these conditions, so we do a two-step
-	// unmarshal; get all the key -> json pairs, and then interpet each value as a string.  If
-	// the value unmarhsals into a string, then it's a Go string.  If it doesn't, then the Go
+	// unmarshal; get all the key -> json pairs, and then interpret each value as a string.  If
+	// the value unmarshals into a string, then it's a Go string.  If it doesn't, then the Go
 	// string is just the JSON bytes that were at that value.  This would let the user at least
-	// use Pachyderm APIs to replace the content of the fautly key.
+	// use Pachyderm APIs to replace the content of the faulty key.
 	//
 	// This will not let users use the API to store arbitrary JSON data in the column; the
 	// marshaling step below still requires a map[string]string and map[string]string{"key":
