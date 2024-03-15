19516e6b23a8384af728117fc9732b5403c4d459
Peter <60992304+pleeko@users.noreply.github.com>
2024-03-15 10:07:15 -0400

    fix enterprise e2e (#1964)

--- cypress/e2e/authenticated/EnterpriseFeatures.ts
+++ cypress/e2e/authenticated/EnterpriseFeatures.ts
@@ -7,22 +7,24 @@ describe('Enterprise Features', () => {
     before(() => {
       cy.exec('echo "pizza" | pachctl auth use-auth-token');
       cy.deleteReposAndPipelines();
-      cy.login();
       cy.exec(`
     pachctl create repo data
     echo '{"pipeline": {"name": "workers"}, "parallelismSpec": {"constant": 999} ,"transform": {"cmd": ["sh"],"stdin": [""]},"input": {"pfs": {"glob": "/*","repo": "data"}}}'  | pachctl create pipeline
     `);
     });
 
-    beforeEach(() => {
-      cy.visit('/lineage/default');
-    });
     after(() => {
       cy.exec('echo "pizza" | pachctl auth use-auth-token');
       cy.deleteReposAndPipelines();
     });
 
+    beforeEach(() => {
+      cy.login();
+    });
+
     it('should show truncate parallelism info in footer of dag node', () => {
+      cy.visit('/lineage/default');
+
       cy.get(
         'g[id="GROUP_e2dc1146a182e5c1d924aab7badb60b37f7538c1_parallelism"]',
       )
