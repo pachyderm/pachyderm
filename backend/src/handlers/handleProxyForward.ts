402236b1917f399f68d0f3c25e49c4c03f811939
Peter <60992304+pleeko@users.noreply.github.com>
2024-03-15 15:36:39 -0400

    [FRON-1428]: Proxy port bug (#1971)

--- backend/src/handlers/handleProxyForward.ts
+++ backend/src/handlers/handleProxyForward.ts
@@ -5,6 +5,8 @@ export default async (req: Request, res: Response) => {
   const isTest = process.env.NODE_ENV === 'test';
   const isDevelopment = process.env.NODE_ENV === 'development';
 
+  const proxyPort = process.env.PACHYDERM_PROXY_SERVICE_PORT;
+
   try {
     const requestUrl = req.originalUrl.slice('/proxyForward'.length);
     let fullUrl = '';
@@ -16,9 +18,13 @@ export default async (req: Request, res: Response) => {
 
       // pachyderm-proxy is the name of the service that gets exposed in kubernetes
       if (req.hostname !== 'localhost' && req.hostname !== '127.0.0.1') {
-        fullUrl = `${req.protocol}://${req.hostname}${requestUrl}?authn-token=${authToken}`;
+        fullUrl = `${req.protocol}://${req.get(
+          'host',
+        )}${requestUrl}?authn-token=${authToken}`;
       } else {
-        fullUrl = `${req.protocol}://pachyderm-proxy${requestUrl}?authn-token=${authToken}`;
+        fullUrl = `${req.protocol}://pachyderm-proxy${
+          proxyPort ? `:${proxyPort}` : ''
+        }${requestUrl}?authn-token=${authToken}`;
       }
     }
 
