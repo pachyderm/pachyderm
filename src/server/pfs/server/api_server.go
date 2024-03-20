1ec938ae1c05c02ed1f952e8a686174b15fb590c
acohen4 <alonco124@gmail.com>
2024-03-20 12:37:56 +0200

    basic ReposSummary

--- src/server/pfs/server/api_server.go
+++ src/server/pfs/server/api_server.go
@@ -984,3 +984,42 @@ func (a *apiServer) Egress(ctx context.Context, req *pfs.EgressRequest) (*pfs.Eg
 	}
 	return nil, errors.Errorf("egress failed")
 }
+
+func (a *apiServer) ReposSummary(ctx context.Context, request *pfs.ReposSummaryRequest) (*pfs.ReposSummaryResponse, error) {
+	summaries := make(map[string]*pfs.ReposSummary)
+	var repos []*pfs.RepoInfo
+	var err error
+	if err := errors.Wrap(dbutil.WithTx(ctx, a.env.DB, func(ctx context.Context, tx *pachsql.Tx) error {
+		return a.driver.txnEnv.WithReadContext(ctx, func(txnCxt *txncontext.TransactionContext) error {
+			repos, err = a.driver.listRepoInTransaction(ctx, txnCxt, true, "user", request.Projects, nil)
+			if err != nil {
+				return err
+			}
+			for _, r := range repos {
+				project := r.Repo.Project.String()
+				summary, ok := summaries[project]
+				if !ok {
+					summary = &pfs.ReposSummary{Project: r.Repo.Project}
+					summaries[project] = summary
+				}
+				summary.UserRepoCount++
+				size, err := a.driver.repoSize(ctx, txnCtx, r)
+				if err != nil {
+					return errors.Wrapf(err, "get size of repo %q", r.Repo.String()
+				}
+				summary.SizeBytes += size
+			}
+			return nil
+		})
+	}, dbutil.WithReadOnly()), "list repo"); err != nil {
+		return nil, err
+	}
+	resp := &pfs.ReposSummaryResponse{}
+	for _, p := range request.Projects {
+		if summary, ok := summaries[p.String()]; ok {
+			resp.Summaries = append(resp.Summaries, summary)
+		}
+	}
+	return resp, nil
+
+}
