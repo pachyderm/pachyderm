599230e6ca6a0511912b38a0435b3f88da930a85
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2024-03-26 18:10:42 +0000

    listeners

--- src/internal/pachd/testpachd_test.go
+++ src/internal/pachd/testpachd_test.go
@@ -1,13 +1,49 @@
 package pachd_test
 
 import (
+	"bytes"
+	"context"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/pachyderm/pachyderm/v2/src/internal/pachd"
+	"github.com/pachyderm/pachyderm/v2/src/internal/pctx"
+	"github.com/pachyderm/pachyderm/v2/src/internal/require"
+	"github.com/pachyderm/pachyderm/v2/src/pfs"
 )
 
 func TestNewTestPachd(t *testing.T) {
-	pachd.NewTestPachd(t)
+	c := pachd.NewTestPachd(t)
+	ctx, done := context.WithTimeout(pctx.TestContext(t), 5*time.Second)
+	defer done()
+	c = c.WithCtx(ctx)
+	repo := &pfs.Repo{
+		Name: "test",
+		Project: &pfs.Project{
+			Name: pfs.DefaultProjectName,
+		},
+		Type: pfs.UserRepoType,
+	}
+	commit := &pfs.Commit{
+		Repo: repo,
+		Branch: &pfs.Branch{
+			Repo: repo,
+			Name: "master",
+		},
+	}
+	filename := "test.txt"
+	want := "hello, world!\n"
+	_, err := c.PfsAPIClient.CreateRepo(c.Ctx(), &pfs.CreateRepoRequest{Repo: repo})
+	require.NoError(t, err, "should create repo")
+	if err := c.PutFile(commit, filename, strings.NewReader(want)); err != nil {
+		t.Fatalf("put file: %v", err)
+	}
+	var buf bytes.Buffer
+	if err := c.GetFile(commit, filename, &buf); err != nil {
+		t.Fatalf("get file: %v", err)
+	}
+	require.Equal(t, want, buf.String(), "file content should be equal")
 }
 
 func TestNewTestPachd_underscore(t *testing.T) {
