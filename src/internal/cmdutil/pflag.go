0d7fc6b9475edcd2347599e62721c3ae923bad07
Robert A. Uhl <robert.uhl@hpe.com>
2024-03-29 14:23:32 -0400

    Comment new type and methods

--- src/internal/cmdutil/pflag.go
+++ src/internal/cmdutil/pflag.go
@@ -6,12 +6,18 @@ import (
 	"github.com/pachyderm/pachyderm/v2/src/internal/errors"
 )
 
+// TimeFlag implements the pflag.Value interface, enabling RFC3339 (with
+// optional nanoseconds) flag arguments.
 type TimeFlag time.Time
 
+// String implements the pflag.Value interface.  It returns the value of the
+// timestamp in RFC3339 (with optional nanoseconds) format.
 func (value *TimeFlag) String() string {
 	return time.Time(*value).Format(time.RFC3339Nano)
 }
 
+// Set implements the pflag.Value interface.  It sets the value of the flag to
+// its parsed argument, if it is acceptable RFC 3339 (with optional nanoseconds) format.
 func (value *TimeFlag) Set(s string) error {
 	ts, err := time.Parse(time.RFC3339Nano, s)
 	if err != nil {
@@ -21,10 +27,8 @@ func (value *TimeFlag) Set(s string) error {
 	return nil
 }
 
+// Type implements the pflag.Value interface.  It simply returns a string
+// describing the format.
 func (value *TimeFlag) Type() string {
-	return "RF3339 date (with nanoseconds)"
-}
-
-func (value *TimeFlag) AsTime() time.Time {
-	return time.Time(*value)
+	return "RF3339 date (with optional nanoseconds)"
 }
