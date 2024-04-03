06b482f36b5b2fa5745a6d11394f366a9f089726
Zhang-Muyang <kryptonian314@outlook.com>
2024-04-03 19:04:44 +0000

    fix s3 test

--- src/server/pfs/s3/project_master_test.go
+++ src/server/pfs/s3/project_master_test.go
@@ -367,7 +367,7 @@ func projectMasterListObjectsPaginated(t *testing.T, pachClient *client.APIClien
 	checkListObjects(t, ch, &startTime, &endTime, expectedFiles, []string{"dir/"})
 
 	// Query by commit.branch.repo
-	ch = minioClient.ListObjects(fmt.Sprintf("%s.%s.%s.%s", commit.Id, commit.Branch.Name, repo, pfs.DefaultProjectName), "", false, make(chan struct{}))
+	ch = minioClient.ListObjects(fmt.Sprintf("%s.%s.%s.%s", commit.Id, "master", repo, pfs.DefaultProjectName), "", false, make(chan struct{}))
 	checkListObjects(t, ch, &startTime, &endTime, expectedFiles, []string{"dir/"})
 
 	// Request that will list all files in master starting with 1
