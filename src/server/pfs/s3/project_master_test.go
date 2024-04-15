9210ae4974842e312da1238f9ec337b8dd5929bb
Zhang-Muyang <kryptonian314@outlook.com>
2024-04-15 19:45:58 +0000

    fix a simple test

--- src/server/pfs/s3/project_master_test.go
+++ src/server/pfs/s3/project_master_test.go
@@ -350,7 +350,7 @@ func projectMasterListObjectsPaginated(t *testing.T, pachClient *client.APIClien
 		return nil
 	}))
 
-	require.NoError(t, pachClient.FinishCommit(pfs.DefaultProjectName, repo, commit.Branch.Name, commit.Id))
+	require.NoError(t, pachClient.FinishCommit(pfs.DefaultProjectName, repo, "", commit.Id))
 
 	endTime := time.Now().Add(time.Duration(5) * time.Minute)
 
