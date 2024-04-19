82636e6580ae6f0cdf8df5563743c4d3d5ed2f79
Zartaj Majeed <zartaj.majeed@hpe.com>
2024-04-18 21:59:38 -0500

    fix pipeline logs test

--- src/server/logs/logs_test.go
+++ src/server/logs/logs_test.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 	"testing"
 	"time"
+	"encoding/base64"
 
 	"github.com/pachyderm/pachyderm/v2/src/logs"
 	logservice "github.com/pachyderm/pachyderm/v2/src/server/logs"
@@ -202,7 +203,7 @@ func TestGetDatumLogs(t *testing.T) {
 
 }
 
-func TestGetPipelineLogs(t *testing.T) {
+func TestPipelineLogs(t *testing.T) {
 	ctx := pctx.TestContext(t)
 	aloki, err := testloki.New(ctx, t.TempDir())
 	if err != nil {
@@ -213,7 +214,6 @@ func TestGetPipelineLogs(t *testing.T) {
 			t.Fatalf("clean up loki: %v", err)
 		}
 	})
-  ts := time.Now()
   labels := map[string]string{
     "app": "pipeline",
     "component": "worker",
@@ -222,32 +222,43 @@ func TestGetPipelineLogs(t *testing.T) {
     "pipelineVersion": "1",
     "suite": "pachyderm",
   }
+  hash, err := base64.StdEncoding.DecodeString("Vp/ZEXxcM96lYfLUnnuaFECJ1j4tuvla7TsY6XGF7qU=")
+  if err != nil {
+    t.Fatalf("base64 error")
+  }
   messageTexts := []string{
     "/usr/local/lib/python3.4/dist-packages/matplotlib/font_manager.py:273: UserWarning: Matplotlib is building the font cache using fc-list. This may take a moment. 1",
-    "/usr/local/lib/python3.4/dist-packages/matplotlib/font_manager.py:273: UserWarning: Matplotlib is building the font cache using fc-list. This may take a moment. 2",
+    "  warnings.warn('Matplotlib is building the font cache using fc-list. This may take a moment.') 2",
     "/usr/local/lib/python3.4/dist-packages/matplotlib/font_manager.py:273: UserWarning: Matplotlib is building the font cache using fc-list. This may take a moment. 3",
-    "/usr/local/lib/python3.4/dist-packages/matplotlib/font_manager.py:273: UserWarning: Matplotlib is building the font cache using fc-list. This may take a moment. 4",
+    "  warnings.warn('Matplotlib is building the font cache using fc-list. This may take a moment.') 4",
+  }
+  now := time.Now().Add(-(time.Duration(len(messageTexts)) + 1) * time.Second)
+  ts := []time.Time{
+    now,
+    now.Add(time.Second),
+    now.Add(2 * time.Second),
+    now.Add(3 * time.Second),
   }
   pipelineLogs := []*testloki.Log{
     {
-      Time: ts,
+      Time: ts[0],
       Labels: labels,
-      Message: `{"severity":"info","ts":"2024-04-16T21:10:37.234151650Z","message": {{ index .messageTexts 0 }},"workerId":"default-edges-v1-8sx6n","projectName":"default","pipelineName":"edges","jobId":"c4cae897bc914bd4bdb6262db038ff15","data":[{"path":"/liberty.jpg","hash":"Vp/ZEXxcM96lYfLUnnuaFECJ1j4tuvla7TsY6XGF7qU="}],"datumId":"cef4a52be60465b328ea783037b6c46531ad7cc9f4e190f9c0e548f473cd1fd1","user":true,"stream":"stderr"}`,
+      Message: `{"severity":"info","ts":"2024-04-16T21:10:37.234151650Z","message":"` + messageTexts[0] + `","workerId":"default-edges-v1-8sx6n","projectName":"default","pipelineName":"edges","jobId":"c4cae897bc914bd4bdb6262db038ff15","data":[{"path":"/liberty.jpg","hash":"Vp/ZEXxcM96lYfLUnnuaFECJ1j4tuvla7TsY6XGF7qU="}],"datumId":"cef4a52be60465b328ea783037b6c46531ad7cc9f4e190f9c0e548f473cd1fd1","user":true,"stream":"stderr"}`,
     },
     {
-      Time: ts,
+      Time: ts[1],
       Labels: labels,
-      Message: `{"severity":"info","ts":"2024-04-16T21:10:37.234183251Z","message":"  warnings.warn('Matplotlib is building the font cache using fc-list. This may take a moment.')","workerId":"default-edges-v1-8sx6n","projectName":"default","pipelineName":"edges","jobId":"c4cae897bc914bd4bdb6262db038ff15","data":[{"path":"/liberty.jpg","hash":"Vp/ZEXxcM96lYfLUnnuaFECJ1j4tuvla7TsY6XGF7qU="}],"datumId":"cef4a52be60465b328ea783037b6c46531ad7cc9f4e190f9c0e548f473cd1fd1","user":true,"stream":"stderr"}`,
+      Message: `{"severity":"info","ts":"2024-04-16T21:10:37.234183251Z","message":"` + messageTexts[1] + `","workerId":"default-edges-v1-8sx6n","projectName":"default","pipelineName":"edges","jobId":"c4cae897bc914bd4bdb6262db038ff15","data":[{"path":"/liberty.jpg","hash":"Vp/ZEXxcM96lYfLUnnuaFECJ1j4tuvla7TsY6XGF7qU="}],"datumId":"cef4a52be60465b328ea783037b6c46531ad7cc9f4e190f9c0e548f473cd1fd1","user":true,"stream":"stderr"}`,
     },
     {
-      Time: ts,
+      Time: ts[2],
       Labels: labels,
-      Message: `{"severity":"info","ts":"2024-04-16T21:10:37.234186851Z","message":"/usr/local/lib/python3.4/dist-packages/matplotlib/font_manager.py:273: UserWarning: Matplotlib is building the font cache using fc-list. This may take a moment.","workerId":"default-edges-v1-8sx6n","projectName":"default","pipelineName":"edges","jobId":"c4cae897bc914bd4bdb6262db038ff15","data":[{"path":"/liberty.jpg","hash":"Vp/ZEXxcM96lYfLUnnuaFECJ1j4tuvla7TsY6XGF7qU="}],"datumId":"cef4a52be60465b328ea783037b6c46531ad7cc9f4e190f9c0e548f473cd1fd1","user":true,"stream":"stderr"}`,
+      Message: `{"severity":"info","ts":"2024-04-16T21:10:37.234186851Z","message":"` + messageTexts[2] + `","workerId":"default-edges-v1-8sx6n","projectName":"default","pipelineName":"edges","jobId":"c4cae897bc914bd4bdb6262db038ff15","data":[{"path":"/liberty.jpg","hash":"Vp/ZEXxcM96lYfLUnnuaFECJ1j4tuvla7TsY6XGF7qU="}],"datumId":"cef4a52be60465b328ea783037b6c46531ad7cc9f4e190f9c0e548f473cd1fd1","user":true,"stream":"stderr"}`,
     },
     {
-      Time: ts,
+      Time: ts[3],
       Labels: labels,
-      Message: `{"severity":"info","ts":"2024-04-16T21:10:37.234188451Z","message":"  warnings.warn('Matplotlib is building the font cache using fc-list. This may take a moment.')","workerId":"default-edges-v1-8sx6n","projectName":"default","pipelineName":"edges","jobId":"c4cae897bc914bd4bdb6262db038ff15","data":[{"path":"/liberty.jpg","hash":"Vp/ZEXxcM96lYfLUnnuaFECJ1j4tuvla7TsY6XGF7qU="}],"datumId":"cef4a52be60465b328ea783037b6c46531ad7cc9f4e190f9c0e548f473cd1fd1","user":true,"stream":"stderr"}`,
+      Message: `{"severity":"info","ts":"2024-04-16T21:10:37.234188451Z","message":"` + messageTexts[3] + `","workerId":"default-edges-v1-8sx6n","projectName":"default","pipelineName":"edges","jobId":"c4cae897bc914bd4bdb6262db038ff15","data":[{"path":"/liberty.jpg","hash":"Vp/ZEXxcM96lYfLUnnuaFECJ1j4tuvla7TsY6XGF7qU="}],"datumId":"cef4a52be60465b328ea783037b6c46531ad7cc9f4e190f9c0e548f473cd1fd1","user":true,"stream":"stderr"}`,
     },
   }
 
@@ -310,9 +321,10 @@ func TestGetPipelineLogs(t *testing.T) {
       JobId: "c4cae897bc914bd4bdb6262db038ff15",
       WorkerId: "default-edges-v1-8sx6n",
       DatumId: "cef4a52be60465b328ea783037b6c46531ad7cc9f4e190f9c0e548f473cd1fd1",
+      Data: []*pps.InputFile{{Path: "/liberty.jpg", Hash: hash}},
       User: true,
       Ts: wants[i].GetLog().NativeTimestamp,
-      Message: "/usr/local/lib/python3.4/dist-packages/matplotlib/font_manager.py:273: UserWarning: Matplotlib is building the font cache using fc-list. This may take a moment.",
+      Message: messageTexts[i],
     }
     if err := aloki.AddLog(ctx, log); err != nil {
       t.Fatalf("add log: %v", err)
