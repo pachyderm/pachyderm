81c153ceb2c31ee48cdda9926cee548dccf4f2ab
Robert A. Uhl <robert.uhl@hpe.com>
2024-05-10 16:43:31 -0400

    Add pipeline user test

--- src/server/logs/cmds/cmds_test.go
+++ src/server/logs/cmds/cmds_test.go
@@ -74,7 +74,6 @@ func TestGetLogs_default_noauth(t *testing.T) {
 	).Run())
 }
 
-// TODO: need to replace this test with real loki that can filter by data for specific pipelines and non-admin users
 func TestGetLogs_default_nonadmin(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Skipping integration tests in short mode")
@@ -226,66 +225,87 @@ func TestGetLogs_pipeline_noauth(t *testing.T) {
 	).Run())
 }
 
-// TODO: need to replace this test with real loki that can filter by data for specific pipelines and non-admin users
-// func TestGetLogs_pipeline_user(t *testing.T) {
-// 	if testing.Short() {
-// 		t.Skip("Skipping integration tests in short mode")
-// 	}
+func TestGetLogs_pipeline_user(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping integration tests in short mode")
+	}
 
-// 	var (
-// 		ctx          = pctx.TestContext(t)
-// 		buildEntries = func() []loki.Entry {
-// 			var entries []loki.Entry
-// 			for i := -99; i <= 0; i++ {
-// 				entries = append(entries, loki.Entry{
-// 					Timestamp: time.Now().Add(time.Duration(i) * time.Second),
-// 					Line:      fmt.Sprintf("%v foo", i),
-// 				})
-// 			}
-// 			return entries
-// 		}
-// 		env = realEnvWithLoki(ctx, t, buildEntries())
-// 		c   = env.PachClient
-// 	)
-// 	mockInspectCluster(env)
-// 	peerPort := strconv.Itoa(int(env.ServiceEnv.Config().PeerPort))
-// 	alice := testutil.UniqueString("robot:alice")
-// 	aliceClient := testutil.AuthenticatedPachClient(t, c, alice, peerPort)
+	ctx := pctx.TestContext(t)
+	aloki, err := testloki.New(ctx, t.TempDir())
+	if err != nil {
+		t.Fatalf("could not create test Loki: %v", err)
+	}
+	for i := -99; i <= 0; i++ {
+		aloki.AddLog(ctx, &testloki.Log{
+			Time:    time.Now().Add(time.Duration(i) * time.Second),
+			Message: fmt.Sprintf(`{"user":true, "message":"%d foo"}`, i),
+			Labels: map[string]string{
+				"suite":           "pachyderm",
+				"app":             "pipeline",
+				"pipelineProject": pfs.DefaultProjectName,
+				"pipelineName":    "pipeline",
+				"container":       "user",
+			},
+		})
+	}
 
-// 	require.NoError(t, testutil.PachctlBashCmdCtx(aliceClient.Ctx(), t, aliceClient, `
-// 		pachctl create repo {{.RepoName}}
-// 		pachctl create pipeline <<EOF
-// 		{
-// 			"pipeline": {
-// 				"project": {
-// 					"name": "{{.ProjectName | js}}"
-// 				},
-// 				"name": "{{.PipelineName | js}}"
-// 			},
-// 			"transform": {
-// 				"cmd": ["cp", "r", "/pfs/in", "/pfs/out"]
-// 			},
-// 			"input": {
-// 				"pfs": {
-// 					"project": "default",
-// 					"repo": "{{.RepoName | js}}",
-// 					"glob": "/*",
-// 					"name": "in"
-// 				}
-// 			},
-// 			"resource_requests": {
-// 				"cpu": null,
-// 				"disk": "187Mi"
-// 			},
-// 			"autoscaling": false
-// 		}
-// 		EOF
-// 		pachctl logs2 --pipeline pipeline --project default| match "64 foo"`,
-// 		"ProjectName", pfs.DefaultProjectName,
-// 		"RepoName", "input",
-// 		"PipelineName", "pipeline",
-// 	).Run())
-// }
+	env := realenv.NewRealEnvWithIdentity(ctx, t, dockertestenv.NewTestDBConfig(t).PachConfigOption,
+		func(c *pachconfig.Configuration) {
+			u, err := url.Parse(aloki.Client.Address)
+			if err != nil {
+				panic(err)
+			}
+			c.LokiHost, c.LokiPort = u.Hostname(), u.Port()
+		})
+	c := env.PachClient
+	peerPort := strconv.Itoa(int(env.ServiceEnv.Config().PeerPort))
+	testutil.ActivateAuthClient(t, c, peerPort)
+	alice := testutil.UniqueString("robot:alice")
+	if _, err := c.ModifyRoleBinding(c.Ctx(), &auth.ModifyRoleBindingRequest{
+		Principal: alice,
+		Roles:     []string{auth.LokiLogReaderRole},
+		Resource: &auth.Resource{
+			Type: auth.ResourceType_CLUSTER,
+		},
+	}); err != nil {
+		t.Fatalf("give alice reader on default/test: %v", err)
+	}
+	aliceClient := testutil.AuthenticatedPachClient(t, c, alice, peerPort)
+
+	require.NoError(t, testutil.PachctlBashCmdCtx(aliceClient.Ctx(), t, aliceClient, `
+		pachctl create repo {{.RepoName}}
+		pachctl create pipeline <<EOF
+		{
+			"pipeline": {
+				"project": {
+					"name": "{{.ProjectName | js}}"
+				},
+				"name": "{{.PipelineName | js}}"
+			},
+			"transform": {
+				"cmd": ["cp", "r", "/pfs/in", "/pfs/out"]
+			},
+			"input": {
+				"pfs": {
+					"project": "default",
+					"repo": "{{.RepoName | js}}",
+					"glob": "/*",
+					"name": "in"
+				}
+			},
+			"resource_requests": {
+				"cpu": null,
+				"disk": "187Mi"
+			},
+			"autoscaling": false
+		}
+		EOF
+		pachctl logs2 --pipeline pipeline --project default| match "64 foo"`,
+		"ProjectName", pfs.DefaultProjectName,
+		"RepoName", "input",
+		"PipelineName", "pipeline",
+	).Run())
+}
 
 func TestGetLogs_project_noauth(t *testing.T) {
 	if testing.Short() {
