2588b9893e02ff75b0eebbe081277cb369f9db24
Ben Bonenfant <ben.bonenfant@pachyderm.io>
2024-05-22 16:26:45 -0700

    [Jupyter] Bugfix: notebook path relative to server_root_dir (#10073)
    Jupyterlab users can configure the server_root_dir to be different than
    the CWD of the backend process

--- jupyter-extension/jupyterlab_pachyderm/pps_client.py
+++ jupyter-extension/jupyterlab_pachyderm/pps_client.py
@@ -226,12 +226,16 @@ def upload_environment(
 class PPSClient:
     """Client interface for the PPS extension backend."""
 
-    def __init__(self, client: Client):
+    def __init__(self, client: Client, root_dir: Path):
+        """
+        client: The pachyderm client.
+        root_dir: The root path from which all data and notebook files are relative.
+        """
         self.nbconvert = PythonExporter()
         self.client = client
+        self.root_dir = root_dir
 
-    @staticmethod
-    async def generate(path):
+    async def generate(self, path):
         """Generates the pipeline spec from the Notebook file specified.
 
         Args:
@@ -239,7 +243,7 @@ class PPSClient:
         """
         get_logger().debug(f"path: {path}")
 
-        path = Path(path.lstrip("/"))
+        path = self.root_dir.joinpath(path.lstrip("/")).resolve()
         if not path.exists():
             raise HTTPError(status_code=400, reason=f"notebook does not exist: {path}")
 
@@ -259,7 +263,7 @@ class PPSClient:
         """
         get_logger().debug(f"path: {path} | body: {body}")
 
-        path = Path(path.lstrip("/"))
+        path = self.root_dir.joinpath(path.lstrip("/")).resolve()
         if not path.exists():
             raise HTTPError(status_code=400, reason=f"notebook does not exist: {path}")
 
