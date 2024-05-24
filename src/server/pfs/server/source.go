31e76611dafa5a725a6313b9d7b54fc2a0651a1f
Zhang-Muyang <kryptonian314@outlook.com>
2024-05-24 19:56:56 +0000

    fix comment

--- src/server/pfs/server/source.go
+++ src/server/pfs/server/source.go
@@ -31,14 +31,14 @@ type source struct {
 
 // NewSource creates a Source which emits FileInfos with the information from commit, and the entries return from fileSet.
 func NewSource(commitInfo *pfs.CommitInfo, fs fileset.FileSet, opts ...SourceOption) Source {
-	validatedFS := fileset.NewPathValidator(fs)
+	fs = fileset.NewPathValidator(fs)
 	sc := &sourceConfig{}
 	for _, opt := range opts {
 		opt(sc)
 	}
 	s := &source{
 		commitInfo: commitInfo,
-		fileSet:    fileset.NewDirInserter(validatedFS, sc.prefix),
+		fileSet:    fileset.NewDirInserter(fs, sc.prefix),
 		dirIndexOpts: []index.Option{
 			index.WithPrefix(sc.prefix),
 			index.WithDatum(sc.datum),
