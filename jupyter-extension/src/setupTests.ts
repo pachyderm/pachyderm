3f8ad5eb482d6dc81b26d622b29dd15a7ff6d738
Kevin Yang <kevin.yang@hpe.com>
2024-06-28 11:36:52 -0400

    wip

--- jupyter-extension/src/setupTests.ts
+++ jupyter-extension/src/setupTests.ts
@@ -1,5 +1,26 @@
 import '@testing-library/jest-dom';
-import '@testing-library/jest-dom/extend-expect';
 import {enableFetchMocks} from 'jest-fetch-mock';
+
+//import 'jest-preset-angular';
+
+Object.defineProperty(window, 'DragEvent', {
+    value: class DragEvent {}
+});
+
+Object.defineProperty(window, 'matchMedia', {
+writable: true,
+    value: jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+    })),
+});
+
+globalThis.crypto ??= require("node:crypto").webcrypto
 
 enableFetchMocks();
