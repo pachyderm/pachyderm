4cdc47351838b5d3181a52b9b398c7b6b1089ce5
Emma Adams <emma.adams@hpe.com>
2024-07-11 17:32:56 +0000

    Comment out failOnConsole Update skipped error message

--- jupyter-extension/jest.config.js
+++ jupyter-extension/jest.config.js
@@ -11,7 +11,8 @@ const esModules = [
 baseConfig.transformIgnorePatterns = [`/node_modules/(?!${esModules}).+`];
 baseConfig.preset = 'ts-jest/presets/js-with-babel';
 baseConfig.testEnvironment = 'jsdom';
-baseConfig.setupFilesAfterEnv.push('<rootDir>/scripts/jest.setup.js');
+// TODO: Reenable this once we figure out why it is causing issues with stack traces being inaccurate and general display bugs.
+// baseConfig.setupFilesAfterEnv.push('<rootDir>/scripts/jest.setup.js');
 
 // On the current version of Jest we are using it will warn us about real timers being cleared when using `advanceTimersByTime`
 // regardless of if we setup fake timers or not. In future versions of jest timers are rewritten again and should not have this issue.
--- jupyter-extension/scripts/jest.setup.js
+++ jupyter-extension/scripts/jest.setup.js
@@ -1,8 +1,12 @@
+// NOTE: This is temporarily unused see `jest.config.js` for where it is commented out.
+
 import failOnConsole from 'jest-fail-on-console'
 
 const ignoredErrors = [
-  // I am not exactly sure what causes this error to occur. This typically occurs when findByTestId is not properly awaited which is not happening.
-  // The tests seem fine and pass. I think we should revisit this error once we can update all the testing libraries, but for now it can be silenced.
+  // This error is caused because we are running two different versions of react in our application. JupyterLab depends on a higher
+  // version of react than our extension does. React supports this behavior by design, but this is something we should eventually address by
+  // updating our version of React. Two different versions of React can cause some unexpected problems given the difference between the two is a major version and
+  // the ability to run two different versions of react is meant primarily for entirely separate areas of an application not tightly coupled like JupyterLab and our extension.
   (msg) => msg.includes('Warning: An update to Datum inside a test was not wrapped in act(...)')
 ]
 const ignoredWarnings = [
