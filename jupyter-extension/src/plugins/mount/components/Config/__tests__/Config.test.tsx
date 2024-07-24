c828d73af6884d437861d8ac9878ace15e4a0f5a
Ben Bonenfant <ben.bonenfant@pachyderm.io>
2024-07-24 14:53:37 -0700

    fix test and revert test file

--- jupyter-extension/src/plugins/mount/components/Config/__tests__/Config.test.tsx
+++ jupyter-extension/src/plugins/mount/components/Config/__tests__/Config.test.tsx
@@ -6,6 +6,8 @@ import {mockedRequestAPI} from 'utils/testUtils';
 import userEvent from '@testing-library/user-event';
 import {AuthConfig, HealthCheck} from 'plugins/mount/types';
 import Config from '../Config';
+import {ReadonlyJSONObject} from "@lumino/coreutils";
+import {ServerConnection} from "@jupyterlab/services";
 jest.mock('../../../../../handler');
 
 describe('config screen', () => {
@@ -281,9 +283,16 @@ describe('config screen', () => {
       };
 
       mockRequestAPI.requestAPI.mockImplementation(
-        mockedRequestAPI({
-          status: 'HEALTHY_INVALID_CLUSTER',
-        }),
+        (
+          _endPoint?: string,
+          method?: string,
+          body?: ReadonlyJSONObject | null,
+          namespace?: string,
+        ) => {
+          throw new ServerConnection.ResponseError(
+            new Response(null, {status: 400}),
+          );
+        },
       );
 
       const {getByTestId, findByText} = render(
@@ -300,6 +309,7 @@ describe('config screen', () => {
 
       userEvent.type(input, 'grpc://test.com:31400');
       submit.click();
+      // await new Promise((r) => setTimeout(r, 2000));
       await findByText('Invalid address.');
       expect(mockRequestAPI.requestAPI).toHaveBeenCalledTimes(1);
 
