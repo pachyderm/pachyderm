b19b75ecbd587a73ef9f7db55f70e5d98b3c66e7
Fahad Syed <fahad.syed@pachyderm.io>
2024-07-29 14:47:25 -0400

    [PFS-263] Disable PJS Schema creation (#10219)
    We're disabling the PJS Schema migration from being applied in 2.11 to
    avoid locking ourselves into the design until more of it is implemented.
    The code is still in the repo, but it isn't getting called.

--- src/internal/clusterstate/v2.11.0/clusterstate.go
+++ src/internal/clusterstate/v2.11.0/clusterstate.go
@@ -10,7 +10,6 @@ func Migrate(state migrations.State) migrations.State {
 		Apply("Normalize commit diffs", normalizeCommitDiffs, migrations.Squash).
 		Apply("Add auth principals table", addAuthPrincipals, migrations.Squash).
 		Apply("Add project created_by", addProjectCreatedBy, migrations.Squash).
-		Apply("Create PJS schema", createPJSSchema, migrations.Squash).
 		Apply("Add commit created_by", addCommitCreatedBy, migrations.Squash).
 		Apply("Add branch created_by", addBranchCreatedBy, migrations.Squash)
 }
