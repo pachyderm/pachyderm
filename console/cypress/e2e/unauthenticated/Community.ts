3d745d2d951e2066b7ed012d71f7a4e5e1a96ca0
Peter <60992304+pleeko@users.noreply.github.com>
2024-07-29 09:50:25 -0400

    [FRON-1543]: CI tests and build (#10210)

--- console/cypress/e2e/unauthenticated/Community.ts
+++ console/cypress/e2e/unauthenticated/Community.ts
@@ -11,6 +11,14 @@ describe('Community Edition Banner', () => {
 
     it('should show a community edition banner', () => {
       cy.findByText('Community Edition').should('exist');
+      cy.findByRole('link', {
+        name: 'Upgrade to Enterprise',
+      }).should(
+        'have.attr',
+        'href',
+        'https://www.pachyderm.com/trial-console/?utm_source=console',
+      );
+
       cy.findByText('Pipelines: 0/16').should('exist');
 
       cy.exec('jq -r .pachReleaseCommit version.json').then((res) => {
@@ -20,35 +28,6 @@ describe('Community Edition Banner', () => {
       });
 
       cy.findByText('Pipelines: 1/16', {timeout: 12000}).should('exist');
-    });
-  });
-
-  describe('With Enterprise', () => {
-    beforeEach(() => {
-      cy.visit('/');
-    });
-
-    after(() => {
-      cy.exec('echo y | pachctl auth deactivate', {failOnNonZeroExit: false})
-        .exec('pachctl enterprise deactivate')
-        .exec('pachctl license delete-all');
-    });
-
-    it('should remove the community edition banner when an enterprise license is entered', () => {
-      cy.findByText('Community Edition').should('exist');
-      cy.findByRole('link', {
-        name: 'Upgrade to Enterprise',
-      }).should(
-        'have.attr',
-        'href',
-        'https://www.pachyderm.com/trial-console/?utm_source=console',
-      );
-
-      cy.exec('echo $PACHYDERM_ENTERPRISE_KEY | pachctl license activate');
-
-      cy.reload();
-      cy.findByText('Project Preview', {timeout: 12000}).should('exist');
-      cy.findByText('Community Edition').should('not.exist');
     });
   });
 });
