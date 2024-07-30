f251f5674b9277864a01e40af627576c2dc273f1
Ben Bonenfant <ben.bonenfant@pachyderm.io>
2024-07-30 11:27:09 -0700

    [Python-SDK] Enable datum batching tests in CI (#10233)
    these tests were accidentally getting skipping in CI

--- python-sdk/tests/test_worker.py
+++ python-sdk/tests/test_worker.py
@@ -37,7 +37,7 @@ def generate_stdin(func: Callable[[], None]):
     ]
 
 
-@pytest.mark.skipif(IMAGE_NAME=None, reason="Image not specified")
+@pytest.mark.skipif(not IMAGE_NAME, reason="Image not specified")
 def test_datum_batching(client: TestClient):
     """Test that exceptions within the user code is caught, reported to the
     worker binary, and iteration continues.
@@ -102,7 +102,7 @@ def test_datum_batching(client: TestClient):
             client.pps.delete_pipeline(pipeline=pipeline, force=True)
 
 
-@pytest.mark.skipif(IMAGE_NAME=None, reason="Image not specified")
+@pytest.mark.skipif(not IMAGE_NAME, reason="Image not specified")
 def test_datum_batching_errors(client: TestClient):
     """Test that exceptions within the user code is caught, reported to the
     worker binary, and iteration continues.
