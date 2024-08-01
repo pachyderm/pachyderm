4d0f48f089902550c850e28b87f0209592cf4b95
Robert A. Uhl <robert.uhl@hpe.com>
2024-08-01 08:32:43 -0400

    Switch from Pachyderm to Bitnami PostgreSQL image

--- src/testing/deploy/deploy_arm64_test.go
+++ src/testing/deploy/deploy_arm64_test.go
@@ -4,7 +4,7 @@ package main
 
 func init() {
 	globalValueOverrides = map[string]string{
-		"postgresql.image.repository": "pachyderm/postgresql",
+		"postgresql.image.repository": "bitnami/postgresql",
 		"postgresql.image.tag":        "13.3.0",
 		"etcd.image.tag":              "v3.5.5",
 		"pgbouncer.image.tag":         "1.16.1",
