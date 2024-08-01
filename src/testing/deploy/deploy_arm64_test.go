43d0fbd3e76165fabb00339401808a48692afc9f
Robert A. Uhl <robert.uhl@hpe.com>
2024-08-01 16:47:18 -0400

    Update Bitnami PostgreSQL image to 14.12.0
    Also switch to pgbouncer image which supports SCRAM auth.

--- src/testing/deploy/deploy_arm64_test.go
+++ src/testing/deploy/deploy_arm64_test.go
@@ -5,7 +5,7 @@ package main
 func init() {
 	globalValueOverrides = map[string]string{
 		"postgresql.image.repository": "bitnami/postgresql",
-		"postgresql.image.tag":        "13.15.0",
+		"postgresql.image.tag":        "14.12.0",
 		"etcd.image.tag":              "v3.5.5",
 		"pgbouncer.image.tag":         "1.16.1",
 		"kubeEventTail.image.tag":     "0.0.7",
