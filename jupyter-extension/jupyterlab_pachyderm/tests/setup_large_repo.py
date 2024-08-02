f0d0d5e68362e3ee4c1539916fb76cdd129e65c6
Emma Adams <emma.adams@hpe.com>
2024-08-02 09:56:48 -0700

    [INT-1297] Large repo infinite scroll testing documentation (#10247)
    I wrote down some further notes about how to test the infinite
    scrolling. The actual implementation is documented already in
    MountDrive.tsx. These are just some of the behaviors I am looking for
    when I manually test this feature.
    
    It could be possible to write an automated test for this, but since it
    can take a while to actually create the repo we might need to use faked
    responses from the server. This should still meaningfully test the
    frontend.

--- jupyter-extension/jupyterlab_pachyderm/tests/setup_large_repo.py
+++ jupyter-extension/jupyterlab_pachyderm/tests/setup_large_repo.py
@@ -1,6 +1,25 @@
 # This is small script to setup a test repo in a new project (test_large_repo_project/test_large_repo).
 # This test repo has a large number of files as defined by NUMBER_OF_FILES created in it. It is useful
 # for testing how the Jupyterlab extension scales with any number of files.
+#
+# Once this script is run you should see a new project and repo created in the Pachyderm cluster with
+# the url `test_large_repo_project/test_large_repo`. You can then use the Jupyterlab extension to mount
+# this repo with the master branch and see how it behaves with a large number of files.
+#
+# There are some important behaviors that should be tested
+# * While loading the files the text for each file should be grey, but each file can still be opened.
+# * Changing directories while loading should stop the loading of the rest of the files. You can verify this by
+#   opening up the developer tools and seeing that the requests for more files has stopped.
+# * Changing directories back and forth to a large directory should not result in any files from the previous attempt appearing
+#   twice.
+# * While loading you should be able to use the scroll bar to navigate through files while loading. Since files are constantly
+#   being added the same location in the scroll bar should not be the same file until finished loading.
+# * While loading if you are not interacting with the scroll bar the files shown should not change.
+# * Once all the files are loaded the text for all files should be black. Also the scroll bar should be able to navigate
+#   to the same set of files when jumping to and from the same point in the scroll bar. You can verify this either by trying to click
+#   on the same location in the scroll bar or just setting the scroll bar to exact values with the developer tools.
+# * The local file cache should not hold on to any files once the directory is changed. You can verify this by checking the
+#   MountDrive._cache in the developer tools.
 
 import math
 
