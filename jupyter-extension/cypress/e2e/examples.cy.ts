227de36e3bc49607a48e2d7a3410175486c09752
kevyang <kevyang@umich.edu>
2024-08-05 09:49:36 -0700

    Update dependencies for jupyterlab v4.0 (#10250)
    Upgrade our JupyterLab base image to 4.0.11 and update all of our
    dependency usage to be up to date with that version.
    
    ---------
    
    Co-authored-by: Kevin Yang <kevin.yang@hpe.com>

--- jupyter-extension/cypress/e2e/examples.cy.ts
+++ jupyter-extension/cypress/e2e/examples.cy.ts
@@ -5,14 +5,14 @@ describe('examples', () => {
   });
 
   it('Should open the pachyderm tutorial example notebook.', () => {
-    cy.jupyterlabCommand('filebrowser:create-main-launcher');
+    cy.jupyterlabCommand('launcher:create');
     cy.findByText('Pachyderm Examples');
     cy.findAllByText('Intro to Pachyderm').first().click();
     cy.contains('h1', 'Intro to Pachyderm Tutorial Header');
   });
 
   it('Should open the pachyderm mount example notebook.', () => {
-    cy.jupyterlabCommand('filebrowser:create-main-launcher');
+    cy.jupyterlabCommand('launcher:create');
     cy.findByText('Pachyderm Examples');
     cy.findAllByText('Mounting Data Repos').first().click();
     cy.contains('h1', 'Mounting Data Repos in Notebooks Header');
--- jupyter-extension/src/setupTests.ts
+++ jupyter-extension/src/setupTests.ts
@@ -1,5 +1,25 @@
 import '@testing-library/jest-dom';
-import '@testing-library/jest-dom/extend-expect';
 import {enableFetchMocks} from 'jest-fetch-mock';
+
+Object.defineProperty(window, 'DragEvent', {
+  value: class DragEvent {},
+});
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+globalThis.crypto ??= require('node:crypto').webcrypto;
 
 enableFetchMocks();
