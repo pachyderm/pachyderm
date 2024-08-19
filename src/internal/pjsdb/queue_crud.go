abbc4f1d4d766206c505a0bc75edb3ff4a0b6ae4
Zhang-Muyang <73475399+Zhang-Muyang@users.noreply.github.com>
2024-08-19 07:11:49 -0600

    [MLDM-29] Queue CRUD (#10268)

--- src/internal/pjsdb/queue_crud.go
+++ src/internal/pjsdb/queue_crud.go
@@ -2,6 +2,7 @@ package pjsdb
 
 import (
 	"context"
+	"database/sql"
 
 	"github.com/pachyderm/pachyderm/v2/src/internal/errors"
 	"github.com/pachyderm/pachyderm/v2/src/internal/pachsql"
@@ -20,4 +21,31 @@ func ListQueues(ctx context.Context, db *pachsql.DB, req IterateQueuesRequest) (
 		return nil, errors.Wrap(err, "list queue")
 	}
 	return queues, nil
+}
+
+// DequeueAndProcess processes the first job in a given queue, and removes that element from queue
+func DequeueAndProcess(ctx context.Context, tx *pachsql.Tx, programHash []byte) (JobID, error) {
+	ctx = pctx.Child(ctx, "dequeue and process")
+	var jobID JobID
+	if err := tx.QueryRowxContext(ctx, `
+		WITH updated AS (
+			SELECT id
+			FROM pjs.jobs
+			WHERE processing IS NULL AND done IS NULL AND queued IS NOT NULL AND program_hash = $1
+			ORDER BY queued
+			LIMIT 1
+			FOR UPDATE
+		)
+		UPDATE pjs.jobs
+		SET processing = CURRENT_TIMESTAMP
+		FROM updated
+		WHERE pjs.jobs.id = updated.id
+		RETURNING pjs.jobs.id
+	`, programHash).Scan(&jobID); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, &DequeueFromEmptyQueueError{ID: string(programHash)}
+		}
+		return 0, errors.Wrap(err, "dequeue and process")
+	}
+	return jobID, nil
 }
--- src/internal/pjsdb/queue_crud_test.go
+++ src/internal/pjsdb/queue_crud_test.go
@@ -3,8 +3,10 @@ package pjsdb_test
 import (
 	"testing"
 
+	"github.com/pachyderm/pachyderm/v2/src/internal/errors"
 	"github.com/pachyderm/pachyderm/v2/src/internal/pjsdb"
 	"github.com/pachyderm/pachyderm/v2/src/internal/require"
+	"github.com/pachyderm/pachyderm/v2/src/internal/storage/fileset"
 )
 
 func TestForEachQueue(t *testing.T) {
@@ -29,4 +31,56 @@ func TestForEachQueue(t *testing.T) {
 	})
 	require.NoError(t, err)
 	require.Equal(t, 3, num)
+}
+
+func TestDequeue(t *testing.T) {
+	ctx, db := DB(t)
+	s := FilesetStorage(t, db)
+	var prog1, prog2, prog3 fileset.PinnedFileset
+	// create 3 queues and put in 25 jobs in each queue
+	const NumOfPrograms = 25
+	withTx(t, ctx, db, s, func(d dependencies) {
+		prog1 = mockFileset(t, d, "/program", "#!/bin/bash; echo 'hello';")
+		prog2 = mockFileset(t, d, "/program", "#!/bin/bash; echo 'hi';")
+		prog3 = mockFileset(t, d, "/program", "#!/bin/bash; echo 'hey';")
+		for i := 0; i < NumOfPrograms; i++ {
+			createJobWithFilesets(t, d, 0, prog1)
+			createJobWithFilesets(t, d, 0, prog2)
+			createJobWithFilesets(t, d, 0, prog3)
+		}
+	})
+
+	var dequeued uint64 = 0
+	for i := 0; i < NumOfPrograms; i++ {
+		withTx(t, ctx, db, s, func(d dependencies) {
+			dequeued++
+			// for now the program hash is also the program
+			jobID, err := pjsdb.DequeueAndProcess(ctx, d.tx, []byte(fileset.ID(prog1).HexString()))
+			require.NoError(t, err)
+			// the job is dequeued in FIFO order
+			require.Equal(t, uint64(jobID), dequeued)
+			dequeued++
+			jobID, err = pjsdb.DequeueAndProcess(ctx, d.tx, []byte(fileset.ID(prog2).HexString()))
+			require.NoError(t, err)
+			require.Equal(t, uint64(jobID), dequeued)
+			dequeued++
+			jobID, err = pjsdb.DequeueAndProcess(ctx, d.tx, []byte(fileset.ID(prog3).HexString()))
+			require.NoError(t, err)
+			require.Equal(t, uint64(jobID), dequeued)
+		})
+		withTx(t, ctx, db, s, func(d dependencies) {
+			err := pjsdb.ForEachQueue(ctx, db, pjsdb.IterateQueuesRequest{}, func(queue pjsdb.Queue) error {
+				require.Len(t, queue.Jobs, NumOfPrograms-int(dequeued)/3)
+				require.Len(t, queue.Programs, NumOfPrograms-int(dequeued)/3)
+				return nil
+			})
+			require.NoError(t, err)
+		})
+	}
+	withTx(t, ctx, db, s, func(d dependencies) {
+		// All three queues are empty. An error is expected if more dequeue operation is performed
+		_, err := pjsdb.DequeueAndProcess(ctx, d.tx, []byte(fileset.ID(prog1).HexString()))
+		require.YesError(t, err)
+		require.True(t, errors.As(err, &pjsdb.DequeueFromEmptyQueueError{}))
+	})
 }
