579770159c60eed148286e4cc7f4bcb5d09d15a4
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2024-08-23 16:30:16 -0400

    workspace_status: work on mac os, upgrade hard-coded master version (#10280)

--- workspace_status.sh
+++ workspace_status.sh
@@ -11,10 +11,16 @@ echo "GIT_BRANCH $git_branch"
 git_tree_status=$(git diff-index --quiet HEAD -- && echo 'Clean' || echo 'Modified')
 echo "GIT_TREE_STATUS $git_tree_status"
 
-exact=$(git describe --exact-match 2>/dev/null | sed -e s/^v// | cut -d - -f 1 || echo 2.11.0)
+exact=$(git describe --exact-match 2>/dev/null | sed -e s/^v// | cut -d - -f 1 || echo 2.12.0)
 echo "STABLE_APP_VERSION $exact"
 
-additional_version=$(git describe --exact-match 2>/dev/null | cut -d - -f 2- || echo "pre.$(git describe --long --dirty=".$(git diff HEAD | sha256sum | cut -c 1-10)" | rev | cut -d - -f 1 | rev)")
+SHA256SUM="sha256sum"
+if command -v shasum &> /dev/null; then
+    # Mac OS ships shasum instead of sha256sum.
+    SHA256SUM="shasum -a 256"
+fi
+
+additional_version=$(git describe --exact-match 2>/dev/null | cut -d - -f 2- || echo "pre.$(git describe --long --dirty=".$(git diff HEAD | $SHA256SUM | cut -c 1-10)" | rev | cut -d - -f 1 | rev)")
 echo "STABLE_ADDITIONAL_VERSION -$additional_version"
 
 ci_runner_image_version="$(date +%Y%m%d)-${commit_sha}"
