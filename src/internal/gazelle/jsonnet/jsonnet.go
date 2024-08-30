3fbf8ec5979b0205964420d958febffecaed7e9e
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2024-08-30 00:47:45 +0000

    handle deleting rules

--- src/internal/gazelle/jsonnet/jsonnet.go
+++ src/internal/gazelle/jsonnet/jsonnet.go
@@ -26,10 +26,8 @@ const (
 	ruleImport = "//private/rules/jsonnet-lint:jsonnet-lint.bzl"
 )
 
-func (_ Jsonnet) Name() string { return lang }
-
-func (_ Jsonnet) Kinds() map[string]rule.KindInfo {
-	return map[string]rule.KindInfo{
+var (
+	kindInfo = map[string]rule.KindInfo{
 		lintRule: {
 			MatchAny:   false,
 			MatchAttrs: []string{"src"},
@@ -43,10 +41,15 @@ func (_ Jsonnet) Kinds() map[string]rule.KindInfo {
 			ResolveAttrs: map[string]bool{},
 		},
 	}
+)
+
+func (_ Jsonnet) Name() string { return lang }
+
+func (_ Jsonnet) Kinds() map[string]rule.KindInfo {
+	return kindInfo
 }
 
 func (_ Jsonnet) Imports(c *config.Config, r *rule.Rule, f *rule.File) []resolve.ImportSpec {
-	log.Printf("looking at rule %v (%v)", r.Name(), r.Kind())
 	switch r.Kind() {
 	case lintRule:
 		return []resolve.ImportSpec{
@@ -73,17 +76,24 @@ func (j *Jsonnet) GenerateRules(args language.GenerateArgs) (result language.Gen
 	}
 	var deps []string
 	var rules []*rule.Rule
+	seen := make(map[string]struct{})
 
+	// Find jsonnet and libsonnet files.
 	for _, e := range entries {
+		// Directories can't be jsonnet files.
 		if e.IsDir() {
 			continue
 		}
+
+		// Path is a pretty filename for verbose logs.
 		fullPath := filepath.Join(args.Dir, e.Name())
 		path, err := filepath.Rel(args.Config.RepoRoot, fullPath)
 		if err != nil {
 			log.Printf("jsonnet: error: filepath.Rel(%v, %v): %v", args.Config.RepoRoot, fullPath, err)
-			continue
+			path = "<unknown>/" + e.Name()
 		}
+
+		// Generate a rule name; also collect a libsonnet file as a dependency.
 		var name string
 		ext := filepath.Ext(path)
 		switch ext {
@@ -100,20 +110,51 @@ func (j *Jsonnet) GenerateRules(args language.GenerateArgs) (result language.Gen
 		if j.Verbose {
 			log.Printf("jsonnet: emitting rules for %v", path)
 		}
+
+		// Create the rule without deps.  Deps will be added after this loop, so we know
+		// we've seen them all.
 		ruleName := fmt.Sprintf("%s_lint_test", strings.ReplaceAll(name, "-", "_"))
 		r := rule.NewRule(lintRule, ruleName)
 		r.SetPrivateAttr("filename", e.Name())
+		seen[e.Name()] = struct{}{}
 		r.SetAttr("size", "small")
 		r.SetAttr("src", e.Name())
 		rules = append(rules, r)
 	}
 
+	// Emit the rules, merging in deps if there are any.
 	for _, r := range rules {
-		if len(deps) > 0 {
-			r.SetAttr("deps", deps)
+		var localDeps []string
+		for _, d := range deps {
+			if d != r.PrivateAttr("filename") {
+				// A file shouldn't depend on itself.  It wouldn't be harmful, just
+				// confusing and ugly.
+				localDeps = append(localDeps, d)
+			}
+		}
+		if len(localDeps) > 0 {
+			r.SetAttr("deps", localDeps)
 		}
 		result.Gen = append(result.Gen, r)
 		result.Imports = append(result.Imports, r.PrivateAttr("filename"))
+	}
+
+	// Do a final pass over existing rules, to see if there are any that need to be deleted
+	// because the underlying source file was deleted.
+	if args.File != nil {
+		for _, r := range args.File.Rules {
+			if r.Kind() != lintRule {
+				continue
+			}
+			src := r.AttrString("src")
+			if _, ok := seen[src]; !ok {
+				r.DelAttr("src")
+				result.Empty = append(result.Empty, r)
+				if j.Verbose {
+					log.Printf("jsonnet: deleting dead rule in %v: %v(%v)", args.Dir, r.Name(), r.Kind())
+				}
+			}
+		}
 	}
 	return
 }
