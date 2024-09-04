29adc7295093b51ec2feece3590ef44eeb17dd9c
Robert A. Uhl <robert.uhl@hpe.com>
2024-09-04 12:32:15 -0400

    Gazelle & check in

---
+++ src/server/pps/internal/worker/fs.go
@@ -0,0 +1,41 @@
+package worker
+
+import (
+	"fmt"
+	"io/fs"
+	"testing/fstest"
+
+	"github.com/pachyderm/pachyderm/v2/src/internal/pjs"
+)
+
+var (
+	workerFS   fstest.MapFS
+	workerHash []byte
+)
+
+func init() {
+	workerFS = fstest.MapFS{
+		"name":    &fstest.MapFile{Data: []byte("pps-worker")},
+		"version": &fstest.MapFile{Data: []byte("1")},
+	}
+	var err error
+	if workerHash, err = pjs.HashFS(workerFS); err != nil {
+		panic(fmt.Sprintf("could not hash workerFS: %v", err))
+	}
+}
+
+// ProgramFS returns a filesystem representing the PPS worker program.
+func ProgramFS() fs.FS {
+	var w = make(fstest.MapFS, len(workerFS))
+	for k, v := range workerFS {
+		var vv = new(fstest.MapFile)
+		*vv = *v
+		w[k] = vv
+	}
+	return w
+}
+
+// ProgramHash returns a hash representing the PPS worker program.
+func ProgramHash() []byte {
+	return workerHash[:]
+}
