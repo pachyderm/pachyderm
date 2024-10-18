774e16866ca8c7882ed29a374120583fabd7d3ae
Robert A. Uhl <robert.uhl@hpe.com>
2024-10-18 14:53:31 -0400

    Set up real storage

--- src/internal/pachd/restore.go
+++ src/internal/pachd/restore.go
@@ -6,12 +6,10 @@ import (
 	"github.com/jmoiron/sqlx"
 
 	"github.com/pachyderm/pachyderm/v2/src/internal/errors"
+	"github.com/pachyderm/pachyderm/v2/src/internal/obj"
 	"github.com/pachyderm/pachyderm/v2/src/internal/pachconfig"
 	"github.com/pachyderm/pachyderm/v2/src/internal/recovery"
-	"github.com/pachyderm/pachyderm/v2/src/internal/storage/chunk"
-	"github.com/pachyderm/pachyderm/v2/src/internal/storage/fileset"
-	"github.com/pachyderm/pachyderm/v2/src/internal/storage/kv"
-	"github.com/pachyderm/pachyderm/v2/src/internal/storage/track"
+	"github.com/pachyderm/pachyderm/v2/src/internal/storage"
 )
 
 type RestoreSnapshotEnv struct {
@@ -32,11 +30,24 @@ func NewRestoreSnapshot(env RestoreSnapshotEnv, config pachconfig.PachdRestoreSn
 		printVersion(),
 		awaitDB(env.DB),
 		setupStep{
-			Name: "HelloWorld",
+			Name: "restore",
 			Fn: func(ctx context.Context) error {
-				tracker := track.NewPostgresTracker(env.DB)
-				storage := fileset.NewStorage(fileset.NewPostgresStore(env.DB), tracker, chunk.NewStorage(kv.NewMemStore(), nil, env.DB, tracker))
-				s := &recovery.Snapshotter{DB: env.DB, Storage: storage}
+				//tracker := track.NewPostgresTracker(env.DB)
+
+				bucket, err := obj.NewBucket(ctx, config.StorageBackend, config.StorageRoot, config.StorageURL)
+				if err != nil {
+					return errors.Wrap(err, "storage env")
+				}
+				sEnv := storage.Env{
+					Bucket: bucket,
+					DB:     env.DB,
+					Config: config.StorageConfiguration,
+				}
+				storage, err := storage.New(ctx, sEnv)
+				if err != nil {
+					return errors.Wrapf(err, "could not configure new storage")
+				}
+				s := &recovery.Snapshotter{DB: env.DB, Storage: storage.Filesets}
 				snapshotID := recovery.SnapshotID(config.SnapshotID)
 				if err := s.RestoreSnapshot(ctx, snapshotID, recovery.RestoreSnapshotOptions{}); err != nil {
 					return errors.Wrapf(err, "could not restore snapshot %s", snapshotID)
