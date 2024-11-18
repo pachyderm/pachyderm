e4db7f8354e2d89d48832649a102f71b7df44c7f
Robert A. Uhl <robert.uhl@hpe.com>
2024-11-18 08:52:09 -0500

    Double wait time

--- etc/testing/circle/wait_for_docker_images.sh
+++ etc/testing/circle/wait_for_docker_images.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-for _ in $(seq 1 120); do
+for _ in $(seq 1 240); do
     if docker manifest inspect "pachyderm/pachd:$TEST_IMAGE_SHA"; then
         echo "manifest exists"
         exit 0
@@ -11,5 +11,5 @@ for _ in $(seq 1 120); do
     sleep 5
 done
 
-echo "images not available after 600 seconds; giving up"
+echo "images not available after 1,200 seconds; giving up"
 exit 1
