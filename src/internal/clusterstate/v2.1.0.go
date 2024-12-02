e98b01e516a3596d4aec03d7f5feb8f26cee9032
MuyangZhangHPE <muyang.zhang@hpe.com>
2024-12-02 15:29:52 +0000

    remove clusterstate dependency

--- src/internal/clusterstate/v2.1.0.go
+++ src/internal/clusterstate/v2.1.0.go
@@ -5,18 +5,12 @@ import (
 
 	"github.com/pachyderm/pachyderm/v2/src/internal/migrations"
 	"github.com/pachyderm/pachyderm/v2/src/internal/storage/fileset"
-	enterpriseserver "github.com/pachyderm/pachyderm/v2/src/server/enterprise/server"
 )
 
 var state_2_1_0 migrations.State = state_2_0_0.
-	Apply("Move EnterpriseConfig from etcd -> postgres", func(ctx context.Context, env migrations.Env) error {
-		return enterpriseserver.EnterpriseConfigPostgresMigration(ctx, env.Tx, env.EtcdClient)
-	}).
-	Apply("Remove old EnterpriseConfig record from etcd", func(ctx context.Context, env migrations.Env) error {
-		return enterpriseserver.DeleteEnterpriseConfigFromEtcd(ctx, env.EtcdClient)
-	}).
 	Apply("create pfs cache v1", func(ctx context.Context, env migrations.Env) error {
 		return fileset.CreatePostgresCacheV1(ctx, env.Tx)
 	}, migrations.Squash)
+
 	// DO NOT MODIFY THIS STATE
 	// IT HAS ALREADY SHIPPED IN A RELEASE
