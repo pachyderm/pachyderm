820f9156cc16a6176258c54a9c34611ec6c4466d
Pierre Lamarche <pierrelamarche5@gmail.com>
2024-12-06 11:54:29 -0400

    chore: set console always in enterprise mode

--- console/cypress/e2e/unauthenticated/Header.ts
+++
@@ -1,6 +0,0 @@
-describe('Header', () => {
-  it('when in Community Edition it should show the correct app name', () => {
-    cy.visit('/');
-    cy.findByRole('banner').findByRole('heading', {name: 'Console'});
-  });
-});
