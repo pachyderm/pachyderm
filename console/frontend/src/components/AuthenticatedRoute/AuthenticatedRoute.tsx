e4a2cc1aec20923b0016b0e62e31d43ae8112775
Pierre Lamarche <pierrelamarche5@gmail.com>
2024-12-06 12:57:27 -0400

    chore: set console always in enterprise mode

--- console/frontend/src/components/AuthenticatedRoute/AuthenticatedRoute.tsx
+++ console/frontend/src/components/AuthenticatedRoute/AuthenticatedRoute.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import CommunityEditionBannerProvider from '@dash-frontend/components/CommunityEditionBanner/CommunityEditionBannerProvider';
 import AuthenticatedRouteErrorView from '@dash-frontend/views/ErrorView/AuthenticatedRouteErrorView';
 
 import useAuthenticatedRoute from './hooks/useAuthenticatedRoute';
@@ -17,7 +16,6 @@ const AuthenticatedRoute = <T,>(Component: React.ComponentType<T>) => {
       return (
         <>
           <Component {...props} />
-          <CommunityEditionBannerProvider />
         </>
       );
     }
